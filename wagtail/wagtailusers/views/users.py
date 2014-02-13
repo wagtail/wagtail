8daf6b3407aebff2d7ea3fcfc6e0bc6b259353e5
Dave Cranwell <david@torchbox.com>
2014-02-13 15:47:19 +0000

    added user searching

--- wagtail/wagtailusers/views/users.py
+++ wagtail/wagtailusers/views/users.py
@@ -3,15 +3,31 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import permission_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib import messages
+from django.db.models import Q
 
+from wagtail.wagtailadmin.forms import SearchForm
 from wagtail.wagtailusers.forms import UserCreationForm, UserEditForm
-
 
 @permission_required('auth.change_user')
 def index(request):
+    q = None
     p = request.GET.get("p", 1)
+    is_searching = False
 
-    users = User.objects.order_by('last_name', 'first_name')
+    if 'q' in request.GET:
+        form = SearchForm(request.GET, placeholder_suffix="users")
+        if form.is_valid():
+            q = form.cleaned_data['q']
+
+            is_searching = True
+            users = User.objects.filter(Q(username__icontains=q) | Q(first_name__icontains=q) | Q(last_name__icontains=q) | Q(email__icontains=q))       
+    else:
+        form = SearchForm(placeholder_suffix="users")
+
+    if not is_searching:
+        users = User.objects
+
+    users = users.order_by('last_name', 'first_name')
 
     if 'ordering' in request.GET:
         ordering = request.GET['ordering']
@@ -31,11 +47,21 @@ def index(request):
     except EmptyPage:
         users = paginator.page(paginator.num_pages)
 
-    return render(request, 'wagtailusers/index.html', {
-        'users': users,
-        'ordering': ordering,
-    })
-
+    if request.is_ajax():
+        return render(request, "wagtailusers/results.html", {
+            'users': users,
+            'is_searching': is_searching,
+            'search_query': q,
+            'ordering': ordering,
+        })
+    else:
+        return render(request, "wagtailusers/index.html", {
+            'search_form': form,
+            'users': users,
+            'is_searching': is_searching,
+            'ordering': ordering,
+            'search_query': q,
+        })
 
 @permission_required('auth.change_user')
 def create(request):
