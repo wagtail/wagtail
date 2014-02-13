f1e9a4703d9102702b5dbdd46fc76302aa62bc02
Matt Westcott <matt@west.co.tt>
2014-02-13 12:53:18 +0000

    apparently we don't need imports in tests/__init__.py

--- wagtail/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-import wagtail.wagtailcore.tests
-import wagtail.wagtailadmin.tests
-import wagtail.wagtaildocs.tests
-import wagtail.wagtailembeds.tests
-import wagtail.wagtailimages.tests
-import wagtail.wagtailredirects.tests
-import wagtail.wagtailsearch.tests
-import wagtail.wagtailsnippets.tests
