a4dcbb12fcb43873242ad7dbf4ca04d7fc165c36
Tom Dyson <tom@torchbox.com>
2014-02-18 18:05:50 +0000

    More RTD-related Sphinx config changes

--- docs/conf.py
+++ docs/conf.py
@@ -110,8 +110,7 @@ pygments_style = 'sphinx'
 # documentation.
 #html_theme_options = {}
 
-# Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
