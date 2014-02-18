5bbb62d2cf682a7c742b589b7fcf5e45bd952139
Tom Dyson <tom@torchbox.com>
2014-02-18 17:59:36 +0000

    Don't specify theme when building on RTD
    See https://github.com/snide/sphinx_rtd_theme/issues/68

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,14 @@
 
 import sys
 import os
-import sphinx_rtd_theme
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -97,9 +104,6 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
