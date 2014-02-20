553d718e5a82ec69a8e74381f85f0e10b41d979c
Karl Hobley <karlhobley10@gmail.com>
2014-02-20 22:08:44 +0000

    Revert "Added ElasticSearch backend to tests settings"
    This reverts commit 28a77c6071faaa717dff990c06cc12b14858215a.

--- runtests.py
+++ runtests.py
@@ -62,16 +62,7 @@ if not settings.configured:
             'wagtail.wagtailsearch',
             'wagtail.wagtailredirects',
             'wagtail.tests',
-        ],
-        WAGTAILSEARCH_BACKENDS={
-            'default': {
-                'BACKEND': 'wagtail.wagtailsearch.backends.db.DBSearch',
-            },
-            'elasticsearch': {
-                'BACKEND': 'wagtail.wagtailsearch.backends.elasticsearch.ElasticSearch',
-                'RUN_TESTS': True,
-            }
-        }
+        ]
     )
 
 
