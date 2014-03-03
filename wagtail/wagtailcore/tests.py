2851e3fea16c5f9e6f59c5e92408b2cd3fd08b03
Matt Westcott <matt@west.co.tt>
2014-03-03 15:50:45 +0000

    Add test fixtures and tests for URL construction in wagtailcore

--- wagtail/wagtailcore/tests.py
+++ wagtail/wagtailcore/tests.py
@@ -1,19 +1,40 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-import unittest
-
 from django.test import TestCase
+from wagtail.wagtailcore.models import Page, Site
 
 
-@unittest.skip("Need real tests")
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestRouting(TestCase):
+    fixtures = ['test.json']
+
+    def test_urls(self):
+        default_site = Site.objects.get(is_default_site=True)
+        homepage = Page.objects.get(url_path='/home/')
+        christmas_page = Page.objects.get(url_path='/home/events/christmas/')
+
+        # Basic installation only has one site configured, so page.url will return local URLs
+        self.assertEqual(homepage.full_url, 'http://localhost/')
+        self.assertEqual(homepage.url, '/')
+        self.assertEqual(homepage.relative_url(default_site), '/')
+
+        self.assertEqual(christmas_page.full_url, 'http://localhost/events/christmas/')
+        self.assertEqual(christmas_page.url, '/events/christmas/')
+        self.assertEqual(christmas_page.relative_url(default_site), '/events/christmas/')
+
+    def test_urls_with_multiple_sites(self):
+        events_page = Page.objects.get(url_path='/home/events/')
+        events_site = Site.objects.create(hostname='events.example.com', root_page=events_page)
+
+        default_site = Site.objects.get(is_default_site=True)
+        homepage = Page.objects.get(url_path='/home/')
+        christmas_page = Page.objects.get(url_path='/home/events/christmas/')
+
+        # with multiple sites, page.url will return full URLs to ensure that
+        # they work across sites
+        self.assertEqual(homepage.full_url, 'http://localhost/')
+        self.assertEqual(homepage.url, 'http://localhost/')
+        self.assertEqual(homepage.relative_url(default_site), '/')
+        self.assertEqual(homepage.relative_url(events_site), 'http://localhost/')
+
+        self.assertEqual(christmas_page.full_url, 'http://events.example.com/christmas/')
+        self.assertEqual(christmas_page.url, 'http://events.example.com/christmas/')
+        self.assertEqual(christmas_page.relative_url(default_site), 'http://events.example.com/christmas/')
+        self.assertEqual(christmas_page.relative_url(events_site), '/christmas/')
