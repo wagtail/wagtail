d61ac002c0f320d74af1c3ab7f3e6599427da762
Dave Cranwell <david@torchbox.com>
2014-03-18 12:08:23 +0000

    updated animation and js requirements

--- wagtail/wagtailadmin/static/wagtailadmin/js/edit_bird.js
+++ wagtail/wagtailadmin/static/wagtailadmin/js/edit_bird.js
@@ -1,3 +1,11 @@
-$(function(){
-	$('body').addClass('ready');
-})
+/* No jQuery to speed up loading */
+document.addEventListener('DOMContentLoaded', function(){
+    var body = document.querySelectorAll('body')[0];
+    var className = 'ready';
+
+    if (body.classList){
+        body.classList.add(className);
+    }else{
+        body.className += ' ' + className;
+    }
+});
