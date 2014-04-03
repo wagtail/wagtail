wagtail/wagtail

commit a1c9224b7adeae41ee649416023239cd5f22c23c

Add check for slug when editing page (fixes #119)

wagtail/wagtailadmin/views/pages.py
@@ -217,6 +217,8 @@ def create(request, content_type_app_name, content_type_model_name, parent_page_
 def edit(request, page_id):
     latest_revision = get_object_or_404(Page, id=page_id).get_latest_revision()
     page = get_object_or_404(Page, id=page_id).get_latest_revision_as_page()
+    parent = page.get_parent()
+
     page_perms = page.permissions_for_user(request.user)
     if not page_perms.can_edit():
         raise PermissionDenied
@@ -228,6 +230,14 @@ def edit(request, page_id):
 
     if request.POST:
         form = form_class(request.POST, request.FILES, instance=page)
+
+        # Stick an extra validator into the form to make sure that the slug is not already in use
+        def clean_slug(slug):
+            # Make sure the slug isn't already in use
+            if parent.get_children().filter(slug=slug).count() > 0:
+                raise ValidationError(_("This slug is already in use"))
+            return slug
+        form.fields['slug'].clean = clean_slug
 
         if form.is_valid():
             is_publishing = bool(request.POST.get('action-publish')) and page_perms.can_publish()

commit 60873318748ce388c88898988965865153009b2c

Fix bug introduced by a1c9224
The a1c9224 commit allowed page editing only when their slug had been
changed!

wagtail/wagtailadmin/views/pages.py
@@ -234,7 +234,7 @@ def edit(request, page_id):
         # Stick an extra validator into the form to make sure that the slug is not already in use
         def clean_slug(slug):
             # Make sure the slug isn't already in use
-            if parent.get_children().filter(slug=slug).count() > 0:
+            if parent.get_children().filter(slug=slug).exclude(id=page_id).count() > 0:
                 raise ValidationError(_("This slug is already in use"))
             return slug
         form.fields['slug'].clean = clean_slug