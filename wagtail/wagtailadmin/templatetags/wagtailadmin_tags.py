1d092eb1da50d89df27780f298cf470f1c8fe6f5
Matt Westcott <matt@west.co.tt>
2014-04-04 14:24:58 +0100

    move ellipsistrim filter to wagtailadmin_tags

--- wagtail/wagtailadmin/templatetags/wagtailadmin_tags.py
+++ wagtail/wagtailadmin/templatetags/wagtailadmin_tags.py
@@ -73,3 +73,13 @@ def main_nav(context):
         'menu_items': sorted(menu_items, key=lambda i: i.order),
         'request': request,
     }
+
+
+@register.filter("ellipsistrim")
+def ellipsistrim(value, max_length):
+    if len(value) > max_length:
+        truncd_val = value[:max_length]
+        if not len(value) == max_length+1 and value[max_length+1] != " ":
+            truncd_val = truncd_val[:truncd_val.rfind(" ")]
+        return truncd_val + "..."
+    return value
