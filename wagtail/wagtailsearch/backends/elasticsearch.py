4328f59c3cb9e244781306b9cb578ff8c450655a
Karl Hobley <karl@torchbox.com>
2014-04-15 16:22:42 +0100

    Copy results cache when ElasticSearchResults is sliced

--- wagtail/wagtailsearch/backends/elasticsearch.py
+++ wagtail/wagtailsearch/backends/elasticsearch.py
@@ -209,7 +209,10 @@ class ElasticSearchResults(object):
 
     def count(self):
         if self._hit_count is None:
-            self._hit_count = self._do_count()
+            if self._results_cache is not None:
+                self._hit_count = len(self._results_cache)
+            else:
+                self._hit_count = self._do_count()
         return self._hit_count
 
     def _do_search(self):
@@ -240,9 +243,16 @@ class ElasticSearchResults(object):
             stop = int(key.stop) if key.stop else None
             new.set_limits(start, stop)
 
+            # Copy results cache
+            if self._results_cache is not None:
+                new._results_cache = self._results_cache[key]
+
             return new
         else:
             # Return a single item
+            if self._results_cache is not None:
+                return self._results_cache[key]
+
             new.start = key
             new.stop = key + 1
             return list(new)[0]
