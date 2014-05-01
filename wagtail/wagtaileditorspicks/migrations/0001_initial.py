cd6110facac8127f193b561437b652eecc197ea5
Karl Hobley <karlhobley10@gmail.com>
2014-05-01 15:30:40 +0100

    Created wagtaileditorspicks and moved editors picks models from search

---
+++ wagtail/wagtaileditorspicks/migrations/0001_initial.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    depends_on = (
+        ('wagtailsearch', '0003_auto__del_searchtestchild__del_searchtest'),
+    )
+
+    def forwards(self, orm):
+        db.rename_table('wagtailsearch_querydailyhits', 'wagtaileditorspicks_querydailyhits')
+        db.rename_table('wagtailsearch_query', 'wagtaileditorspicks_query')
+        db.rename_table('wagtailsearch_editorspick', 'wagtaileditorspicks_editorspick')
+
+    def backwards(self, orm):
+        db.rename_table('wagtaileditorspicks_querydailyhits', 'wagtailsearch_querydailyhits')
+        db.rename_table('wagtaileditorspicks_query', 'wagtailsearch_query')
+        db.rename_table('wagtaileditorspicks_editorspick', 'wagtailsearch_editorspick')
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'wagtailcore.page': {
+            'Meta': {'object_name': 'Page'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'pages'", 'to': u"orm['contenttypes.ContentType']"}),
+            'depth': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'has_unpublished_changes': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'live': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'numchild': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'owned_pages'", 'null': 'True', 'to': u"orm['auth.User']"}),
+            'path': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'search_description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'seo_title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'show_in_menus': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'url_path': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        },
+        u'wagtaileditorspicks.editorspick': {
+            'Meta': {'ordering': "('sort_order',)", 'object_name': 'EditorsPick'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'page': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'+'", 'to': u"orm['wagtailcore.Page']"}),
+            'query': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'editors_picks'", 'to': u"orm['wagtaileditorspicks.Query']"}),
+            'sort_order': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'wagtaileditorspicks.query': {
+            'Meta': {'object_name': 'Query'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'query_string': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        u'wagtaileditorspicks.querydailyhits': {
+            'Meta': {'unique_together': "(('query', 'date'),)", 'object_name': 'QueryDailyHits'},
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'hits': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'query': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'daily_hits'", 'to': u"orm['wagtaileditorspicks.Query']"})
+        }
+    }
+
+    complete_apps = ['wagtaileditorspicks']
---
+++ wagtail/wagtaileditorspicks/models.py
@@ -0,0 +1,89 @@
+from django.db import models
+from django.utils import timezone
+
+import datetime
+import string
+
+
+class Query(models.Model):
+    query_string = models.CharField(max_length=255, unique=True)
+
+    def save(self, *args, **kwargs):
+        # Normalise query string
+        self.query_string = self.normalise_query_string(self.query_string)
+
+        super(Query, self).save(*args, **kwargs)
+
+    def add_hit(self, date=None):
+        if date is None:
+            date = timezone.now().date()
+        daily_hits, created = QueryDailyHits.objects.get_or_create(query=self, date=date)
+        daily_hits.hits = models.F('hits') + 1
+        daily_hits.save()
+
+    def __unicode__(self):
+        return self.query_string
+
+    @property
+    def hits(self):
+        hits = self.daily_hits.aggregate(models.Sum('hits'))['hits__sum']
+        return hits if hits else 0
+
+    @classmethod
+    def garbage_collect(cls):
+        """
+        Deletes all Query records that have no daily hits or editors picks
+        """
+        cls.objects.filter(daily_hits__isnull=True, editors_picks__isnull=True).delete()
+
+    @classmethod
+    def get(cls, query_string):
+        return cls.objects.get_or_create(query_string=cls.normalise_query_string(query_string))[0]
+
+    @classmethod
+    def get_most_popular(cls, date_since=None):
+        # TODO: Implement date_since
+        return cls.objects.filter(daily_hits__isnull=False).annotate(_hits=models.Sum('daily_hits__hits')).distinct().order_by('-_hits')
+
+    @staticmethod
+    def normalise_query_string(query_string):
+        # Convert query_string to lowercase
+        query_string = query_string.lower()
+
+        # Strip punctuation characters
+        query_string = ''.join([c for c in query_string if c not in string.punctuation])
+
+        # Remove double spaces
+        query_string = ' '.join(query_string.split())
+
+        return query_string
+
+
+class QueryDailyHits(models.Model):
+    query = models.ForeignKey(Query, db_index=True, related_name='daily_hits')
+    date = models.DateField()
+    hits = models.IntegerField(default=0)
+
+    @classmethod
+    def garbage_collect(cls):
+        """
+        Deletes all QueryDailyHits records that are older than 7 days
+        """
+        min_date = timezone.now().date() - datetime.timedelta(days=7)
+
+        cls.objects.filter(date__lt=min_date).delete()
+
+    class Meta:
+        unique_together = (
+            ('query', 'date'),
+        )
+
+
+class EditorsPick(models.Model):
+    query = models.ForeignKey(Query, db_index=True, related_name='editors_picks')
+    page = models.ForeignKey('wagtailcore.Page', related_name='+')
+    sort_order = models.IntegerField(null=True, blank=True, editable=False)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ('sort_order', )
