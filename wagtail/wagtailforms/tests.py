9b93d7edd8b950082d0524777e4223a217b52ef0
Matt Westcott <matt@west.co.tt>
2014-05-07 17:33:13 +0100

    Add unit tests for wagtailforms backend

--- wagtail/wagtailforms/tests.py
+++ wagtail/wagtailforms/tests.py
@@ -28,3 +28,36 @@ class TestFormSubmission(TestCase):
         form_page = Page.objects.get(url_path='/home/contact-us/')
 
         self.assertTrue(FormSubmission.objects.filter(page=form_page, form_data__contains='hello world').exists())
+
+
+class TestFormsBackend(TestCase):
+    fixtures = ['test.json']
+
+    def test_cannot_see_forms_without_permission(self):
+        form_page = Page.objects.get(url_path='/home/contact-us/')
+
+        self.client.login(username='eventeditor', password='password')
+        response = self.client.get('/admin/forms/')
+        self.assertFalse(response.context['form_pages'].filter(id=form_page.id).exists())
+
+    def test_can_see_forms_with_permission(self):
+        form_page = Page.objects.get(url_path='/home/contact-us/')
+
+        self.client.login(username='siteeditor', password='password')
+        response = self.client.get('/admin/forms/')
+        self.assertTrue(response.context['form_pages'].filter(id=form_page.id).exists())
+
+    def test_can_get_submissions(self):
+        form_page = Page.objects.get(url_path='/home/contact-us/')
+
+        self.client.login(username='siteeditor', password='password')
+
+        response = self.client.get('/admin/forms/submissions/%d/' % form_page.id)
+        self.assertEqual(len(response.context['data_rows']), 2)
+
+        response = self.client.get('/admin/forms/submissions/%d/?date_from=01%%2F01%%2F2014' % form_page.id)
+        self.assertEqual(len(response.context['data_rows']), 1)
+
+        response = self.client.get('/admin/forms/submissions/%d/?date_from=01%%2F01%%2F2014&action=CSV' % form_page.id)
+        data_line = response.content.split("\n")[1]
+        self.assertTrue('new@example.com' in data_line)
