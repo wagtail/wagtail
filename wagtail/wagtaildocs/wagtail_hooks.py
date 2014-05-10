fd737f0ab0688f0a9423239b53d49421503da1b7
Matt Westcott <matt@west.co.tt>
2014-05-10 19:35:51 +0100

    Have wagtaildocs, wagtailembeds, wagtailimages and wagtailsnippets register their page editor JS components through the insert_editor_js hook, rather than hard-coding them into _editor_js.html

--- wagtail/wagtaildocs/wagtail_hooks.py
+++ wagtail/wagtaildocs/wagtail_hooks.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.core import urlresolvers
+from django.utils.html import format_html_join
 from django.utils.translation import ugettext_lazy as _
 
 from wagtail.wagtailadmin import hooks
@@ -21,3 +23,14 @@ def construct_main_menu(request, menu_items):
             MenuItem(_('Documents'), urlresolvers.reverse('wagtaildocs_index'), classnames='icon icon-doc-full-inverse', order=400)
         )
 hooks.register('construct_main_menu', construct_main_menu)
+
+
+def editor_js():
+    js_files = [
+        'wagtaildocs/js/hallo-plugins/hallo-wagtaildoclink.js',
+        'wagtaildocs/js/document-chooser.js',
+    ]
+    return format_html_join('\n', '<script src="{0}{1}"></script>',
+        ((settings.STATIC_URL, filename) for filename in js_files)
+    )
+hooks.register('insert_editor_js', editor_js)
