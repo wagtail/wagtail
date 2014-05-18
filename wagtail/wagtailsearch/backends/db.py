c55067fc463331cec2f6af0476b5b2063f235ba4
Karl Hobley <karlhobley10@gmail.com>
2014-05-18 16:08:12 +0100

    Added DBSearchResults class to DB search backend

--- wagtail/wagtailsearch/backends/db.py
+++ wagtail/wagtailsearch/backends/db.py
@@ -1,7 +1,47 @@
 from django.db import models
 
-from wagtail.wagtailsearch.backends.base import BaseSearch
+from wagtail.wagtailsearch.backends.base import BaseSearch, BaseSearchResults
 from wagtail.wagtailsearch.indexed import Indexed
+
+import string
+
+
+class DBSearchResults(BaseSearchResults):
+    def _do_search(self):
+        queryset = self.queryset
+
+        # Get terms
+        terms = self.query_string.split()
+        if not terms:
+            return queryset.none()
+
+        # Get fields
+        fields = self.fields
+        if fields is None:
+            fields = queryset.model.get_search_fields(exclude_filter=True).keys()
+
+        # Filter by terms
+        for term in terms:
+            term_query = None
+            for field_name in fields:
+                # Check if the field exists (this will filter out indexed callables)
+                try:
+                    queryset.model._meta.get_field_by_name(field_name)
+                except:
+                    continue
+
+                # Filter on this field
+                field_filter = {'%s__icontains' % field_name: term}
+                if term_query is None:
+                    term_query = models.Q(**field_filter)
+                else:
+                    term_query |= models.Q(**field_filter)
+            queryset = queryset.filter(term_query)
+
+        return queryset[slice(self.start, self.stop)]
+
+    def _do_count(self):
+        return self._do_search().count()
 
 
 class DBSearch(BaseSearch):
@@ -31,35 +71,13 @@ class DBSearch(BaseSearch):
         if not issubclass(query_set.model, Indexed):
             return query_set.none()
 
-        # Get terms
-        terms = query_string.split()
-        if not terms:
+        # Clean up query string
+        if query_string is not None:
+            query_string = "".join([c for c in query_string if c not in string.punctuation])
+
+        # Don't search using blank query strings (this upsets ElasticSearch)
+        if query_string == "":
             return query_set.none()
 
-        # Get fields
-        if fields is None:
-            fields = query_set.model.get_search_fields(exclude_filter=True).keys()
-
-        # Return nothing if there are no fields
-        if not fields:
-            return query_set.none()
-
-        # Filter by terms
-        for term in terms:
-            term_query = None
-            for field_name in fields:
-                # Check if the field exists (this will filter out indexed callables)
-                try:
-                    query_set.model._meta.get_field_by_name(field_name)
-                except:
-                    continue
-
-                # Filter on this field
-                field_filter = {'%s__icontains' % field_name: term}
-                if term_query is None:
-                    term_query = models.Q(**field_filter)
-                else:
-                    term_query |= models.Q(**field_filter)
-            query_set = query_set.filter(term_query)
-
-        return query_set
+        # Return search results
+        return DBSearchResults(self, query_set, query_string, fields=fields)
