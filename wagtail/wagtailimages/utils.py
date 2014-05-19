b0c069d44611e9213be1f60e403956d6dd683d35
Karl Hobley <karl@torchbox.com>
2014-05-19 09:56:49 +0100

    More robust image file validation Previously, it was possible to upload PSD files by simply renaming them to have a '.jpg' extension.
    This causes issues as wagtailimages cannot resize PSD files causing crashes.
    
    This commit fixes this by validating that the internal format of the file matches the extension.

---
+++ wagtail/wagtailimages/utils.py
@@ -0,0 +1,27 @@
+import os
+
+from PIL import Image
+
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy  as _
+
+
+def validate_image_format(f):
+    # Check file extension
+    extension = os.path.splitext(f.name)[1].lower()[1:]
+
+    if extension == 'jpg':
+        extension = 'jpeg'
+
+    if extension not in ['gif', 'jpeg', 'png']:
+        raise ValidationError(_("Not a valid image. Please use a gif, jpeg or png file with the correct file extension."))
+
+    # Open image file
+    file_position = f.tell()
+    f.seek(0)
+    image = Image.open(f)
+    f.seek(file_position)
+
+    # Check that the internal format matches the extension
+    if image.format.upper() != extension.upper():
+        raise ValidationError(_("Not a valid %s image. Please use a gif, jpeg or png file with the correct file extension.") % (extension.upper()))
