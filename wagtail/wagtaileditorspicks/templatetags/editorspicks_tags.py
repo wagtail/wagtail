83876544b510d3cefb999917fa1df05d8fef1a9e
Karl Hobley <karlhobley10@gmail.com>
2014-05-24 20:02:35 +0100

    Removed stray print statements

--- wagtail/wagtaileditorspicks/templatetags/editorspicks_tags.py
+++ wagtail/wagtaileditorspicks/templatetags/editorspicks_tags.py
@@ -6,6 +6,4 @@ register = template.Library()
 
 @register.assignment_tag
 def get_editors_picks(query_string):
-    print query_string
-    print Query.get(query_string).editors_picks.all()
     return Query.get(query_string).editors_picks.all()
