a5072c768df546641311a0085aed6e6ae8debb57
Tom Talbot <tom.talbot@torchbox.com>
2014-05-29 15:02:56 +0100

    Fix #94. Panels are now hidden on page load if they are marked as deleted after a form validation failure.

--- wagtail/wagtailadmin/views/pages.py
+++ wagtail/wagtailadmin/views/pages.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.decorators import permission_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.utils.translation import ugettext as _ 
+from django.utils.translation import ugettext as _
 from django.views.decorators.vary import vary_on_headers
 
 from wagtail.wagtailadmin.edit_handlers import TabbedInterface, ObjectList
