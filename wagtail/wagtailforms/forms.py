1683287ee7b5c0780024ecb055513762f8828c34
Karl Hobley <karl@torchbox.com>
2014-06-03 11:15:12 +0100

    Change wagtailforms SelectDateForm to use DateTimeFields
    This is because DateFields are not timezone aware which causes warnings to be raised by django.
    
    The widgets are still set to "DateInput" so the look and behaviour should not change

--- wagtail/wagtailforms/forms.py
+++ wagtail/wagtailforms/forms.py
@@ -100,11 +100,11 @@ class FormBuilder(object):
 
 
 class SelectDateForm(django.forms.Form):
-    date_from = django.forms.DateField(
+    date_from = django.forms.DateTimeField(
         required=False,
         widget=django.forms.DateInput(attrs={'placeholder': 'Date from'})
     )
-    date_to = django.forms.DateField(
+    date_to = django.forms.DateTimeField(
         required=False,
         widget=django.forms.DateInput(attrs={'placeholder': 'Date to'})
     )
