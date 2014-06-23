ee7aebc9daf4517f2540e80ebe30c53685af3067
Karl Hobley <karl@torchbox.com>
2014-06-23 13:43:34 +0100

    Added tests for sitemaps

--- wagtail/tests/urls.py
+++ wagtail/tests/urls.py
@@ -4,6 +4,7 @@ from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 from wagtail.wagtailsearch.urls import frontend as wagtailsearch_frontend_urls
+from wagtail.contrib.wagtailsitemaps.views import sitemap
 
 # Signal handlers
 from wagtail.wagtailsearch import register_signal_handlers as wagtailsearch_register_signal_handlers
@@ -14,6 +15,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^search/', include(wagtailsearch_frontend_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
+
+    url(r'^sitemap\.xml$', sitemap),
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's serving mechanism
