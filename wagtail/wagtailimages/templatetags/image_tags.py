50a5182794b4a4e2336e9e40c471e617da136dac
Karl Hobley <karl@torchbox.com>
2014-06-30 10:37:02 +0100

    Fixed typos in a couple of deprecation warnings

--- wagtail/wagtailimages/templatetags/image_tags.py
+++ wagtail/wagtailimages/templatetags/image_tags.py
@@ -1,7 +1,7 @@
 import warnings
 
 warnings.warn(
-    "The image_tags tag library has been moved to wagtailcore_tags. "
+    "The image_tags tag library has been moved to wagtailimages_tags. "
     "Use {% load wagtailimages_tags %} instead.", DeprecationWarning)
 
 
