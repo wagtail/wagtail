99178828dc2adadec4c24292b06830f93d18c2ff
Nick Smith <nick.smith@torchbox.com>
2014-07-02 12:20:55 +0100

    Add groups to wagtail admin interface

---
+++ wagtail/wagtailusers/views/groups.py
@@ -0,0 +1,114 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import permission_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib import messages
+from django.db.models import Q
+from django.utils.translation import ugettext as _
+from django.views.decorators.vary import vary_on_headers
+
+from wagtail.wagtailadmin.forms import SearchForm
+from wagtail.wagtailusers.forms import GroupForm
+from wagtail.wagtailcore.compat import AUTH_USER_APP_LABEL, AUTH_USER_MODEL_NAME
+
+User = get_user_model()
+
+# Typically we would check the permission 'auth.change_user' for user
+# management actions, but this may vary according to the AUTH_USER_MODEL
+# setting
+change_user_perm = "{0}.change_{1}".format(AUTH_USER_APP_LABEL, AUTH_USER_MODEL_NAME.lower())
+
+
+@permission_required(change_user_perm)
+@vary_on_headers('X-Requested-With')
+def index(request):
+    q = None
+    p = request.GET.get("p", 1)
+    is_searching = False
+
+    if 'q' in request.GET:
+        form = SearchForm(request.GET, placeholder=_("Search groups"))
+        if form.is_valid():
+            q = form.cleaned_data['q']
+
+            is_searching = True
+            groups = Group.objects.filter(name__icontains=q)
+    else:
+        form = SearchForm(placeholder=_("Search groups"))
+
+    if not is_searching:
+        groups = Group.objects
+
+    groups = groups.order_by('name')
+
+    if 'ordering' in request.GET:
+        ordering = request.GET['ordering']
+
+        if ordering in ['name', 'username']:
+            if ordering != 'name':
+                users = users.order_by(ordering)
+    else:
+        ordering = 'name'
+
+    paginator = Paginator(groups, 20)
+
+    try:
+        groups = paginator.page(p)
+    except PageNotAnInteger:
+        groups = paginator.page(1)
+    except EmptyPage:
+        groups = paginator.page(paginator.num_pages)
+
+    if request.is_ajax():
+        return render(request, "wagtailusers/groups/results.html", {
+            'groups': groups,
+            'is_searching': is_searching,
+            'query_string': q,
+            'ordering': ordering,
+        })
+    else:
+        return render(request, "wagtailusers/groups/index.html", {
+            'search_form': form,
+            'groups': groups,
+            'is_searching': is_searching,
+            'ordering': ordering,
+            'query_string': q,
+        })
+
+@permission_required(change_user_perm)
+def create(request):
+    if request.POST:
+        form = GroupForm(request.POST)
+        if form.is_valid():
+            group = form.save()
+            messages.success(request, _("Group '{0}' created.").format(group))
+            return redirect('wagtailusers_groups_index')
+        else:
+            messages.error(request, _("The group could not be created due to errors.") )
+    else:
+        form = GroupForm()
+
+    return render(request, 'wagtailusers/groups/create.html', {
+        'form': form,
+    })
+
+
+@permission_required(change_user_perm)
+def edit(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    if request.POST:
+        form = GroupForm(request.POST, instance=group)
+        if form.is_valid():
+            group = form.save()
+            messages.success(request, _("Group '{0}' updated.").format(group))
+            return redirect('wagtailusers_groups_index')
+        else:
+            messages.error(request, _("The group could not be saved due to errors."))
+    else:
+        form = GroupForm(instance=group)
+
+    return render(request, 'wagtailusers/groups/edit.html', {
+        'group': group,
+        'form': form,
+    })
