79de8bf88439fe56f6e24ae186c1f9365837ee3f
Matt Westcott <matt@west.co.tt>
2014-07-02 21:19:29 +0100

    replace assertRegexpMatches with six.assertRegex to prevent deprecation warnings on python 3

--- wagtail/wagtailimages/tests.py
+++ wagtail/wagtailimages/tests.py
@@ -1,4 +1,5 @@
 from mock import MagicMock
+from django.utils import six
 
 from django.test import TestCase
 from django import template
@@ -453,16 +454,14 @@ class TestFormat(TestCase):
             self.image,
             'test alt text'
         )
-        self.assertRegexpMatches(
-            result,
+        six.assertRegex(self, result,
             '<img data-embedtype="image" data-id="0" data-format="test name" data-alt="test alt text" class="test classnames" src="[^"]+" width="1" height="1" alt="test alt text">',
-            )
+        )
 
     def test_image_to_html_no_classnames(self):
         self.format.classnames = None
         result = self.format.image_to_html(self.image, 'test alt text')
-        self.assertRegexpMatches(
-            result,
+        six.assertRegex(self, result,
             '<img src="[^"]+" width="1" height="1" alt="test alt text">'
         )
         self.format.classnames = 'test classnames'
