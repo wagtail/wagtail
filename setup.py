85a68a34f1812d11eb36cc9f3899206b61d21687
Karl Hobley <karl@torchbox.com>
2014-07-02 10:22:08 +0100

    Requirements changes  - Removed Beautiful Soup 3  - Added six

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ install_requires = [
     "beautifulsoup4>=4.3.2",
     "lxml>=3.3.0",
     "Unidecode>=0.04.14",
-    "BeautifulSoup==3.2.1",  # django-compressor gets confused if we have lxml but not BS3 installed
+    "six==1.7.3",
 ]
 
 
