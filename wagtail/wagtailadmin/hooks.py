1a3558cabd2751a975fcabd11979874ba17a54ee
Karl Hobley <karl@torchbox.com>
2014-07-03 10:25:26 +0100

    Added DeprecationWarning to wagtailadmin.hooks

--- wagtail/wagtailadmin/hooks.py
+++ wagtail/wagtailadmin/hooks.py
@@ -1,4 +1,11 @@
 # The 'hooks' module is now part of wagtailcore.
 # Imports are provided here for backwards compatibility
 
+import warnings
+
+warnings.warn(
+    "The wagtail.wagtailadmin.hooks module has been moved. "
+    "Use wagtail.wagtailcore.hooks instead.", DeprecationWarning)
+
+
 from wagtail.wagtailcore.hooks import register, get_hooks
