afe5283bef37dd55b866615eebb699e983c7e05a
Nick Smith <nick.smith@torchbox.com>
2014-07-04 11:35:08 +0100

    Register 'access wagtail admin' permission

---
+++ wagtail/wagtailadmin/wagtail_hooks.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import Permission
+from wagtail.wagtailadmin import hooks
+
+
+def register_permissions():
+    return Permission.objects.filter(content_type__app_label='wagtailadmin', codename='access_admin')
+hooks.register('register_permissions', register_permissions)
