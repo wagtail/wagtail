74c202287752960f021564851fc7629b42a72d30
Nick Smith <nick.smith@torchbox.com>
2014-07-11 10:30:45 +0100

    Add page chooser panel for site form root page field

--- wagtail/wagtailsites/forms.py
+++ wagtail/wagtailsites/forms.py
@@ -4,6 +4,10 @@ from wagtail.wagtailcore.models import Site
 
 
 class SiteForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(SiteForm, self).__init__(*args, **kwargs)
+        self.fields['root_page'].widget = forms.HiddenInput()
+
     required_css_class = "required"
 
     class Meta:
