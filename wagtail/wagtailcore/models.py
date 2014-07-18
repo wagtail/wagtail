b310fcad017c171d6e99643cdf67aad1d851e575
Dave Cranwell <david@torchbox.com>
2014-07-18 16:04:50 +0100

    styled list of page permissions to more resemble other permissions above

--- wagtail/wagtailcore/models.py
+++ wagtail/wagtailcore/models.py
@@ -944,9 +944,9 @@ class PageRevision(models.Model):
 
 
 PAGE_PERMISSION_TYPE_CHOICES = [
-    ('add', 'Add'),
-    ('edit', 'Edit'),
-    ('publish', 'Publish'),
+    ('add', 'Add / edit pages you own'),
+    ('edit', 'Add / edit any page'),
+    ('publish', 'Publish pages'),
 ]
 
 
