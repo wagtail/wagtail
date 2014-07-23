77673d7bf89e60281cf0e79ee3bcab767b9a6f3f
Karl Hobley <karl@torchbox.com>
2014-07-23 15:16:59 +0100

    Replaced all DeprecationWarnngs with RemovedInWagtail06Warnings

--- wagtail/wagtailadmin/hooks.py
+++ wagtail/wagtailadmin/hooks.py
@@ -3,9 +3,12 @@
 
 import warnings
 
+from wagtail.utils.deprecation import RemovedInWagtail06Warning
+
+
 warnings.warn(
     "The wagtail.wagtailadmin.hooks module has been moved. "
-    "Use wagtail.wagtailcore.hooks instead.", DeprecationWarning)
+    "Use wagtail.wagtailcore.hooks instead.", RemovedInWagtail06Warning)
 
 
 from wagtail.wagtailcore.hooks import register, get_hooks
