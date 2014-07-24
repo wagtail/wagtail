e8dd039dce2c6f62d84c0e99de4ec574ecd0d46c
Dave Cranwell <david@torchbox.com>
2014-07-24 11:10:42 +0100

    ongoing tweaks

--- wagtail/wagtailadmin/static/wagtailadmin/js/core.js
+++ wagtail/wagtailadmin/static/wagtailadmin/js/core.js
@@ -22,9 +22,12 @@ $(function(){
     });
 
     $('.nav-main a').on('click', function(){
-        $('.nav-wrapper').toggleClass('submenu-active')
         $(this).closest('li').toggleClass('active');
-        return false
+        
+        if($(this).closest('li').find('.submenu')){
+            $('.nav-wrapper').toggleClass('submenu-active')
+            return false
+        }
     })
 
     // Enable swishy section navigation menu
