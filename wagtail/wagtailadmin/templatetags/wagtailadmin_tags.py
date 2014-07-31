de70f2ab89fdf99c54088c64956b00a3621fdd51
Karl Hobley <karl@torchbox.com>
2014-07-31 12:19:09 +0100

    Improved implementation of `usage_count_enabled`
    Thanks to @gasman for spotting

--- wagtail/wagtailadmin/templatetags/wagtailadmin_tags.py
+++ wagtail/wagtailadmin/templatetags/wagtailadmin_tags.py
@@ -126,9 +126,4 @@ def hook_output(hook_name):
 
 @register.assignment_tag
 def usage_count_enabled():
-    if hasattr(
-            settings, 'WAGTAIL_USAGE_COUNT_ENABLED'
-    ) and settings.WAGTAIL_USAGE_COUNT_ENABLED:
-        return True
-    else:
-        return False
+    return getattr(settings, 'WAGTAIL_USAGE_COUNT_ENABLED', False)
