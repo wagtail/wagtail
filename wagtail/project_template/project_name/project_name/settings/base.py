03700d65ed8a27f8e63e39532d84b543e73d7813
Matt Westcott <matt@west.co.tt>
2014-07-31 16:11:02 +0100

    Use sqlite by default

--- wagtail/project_template/project_name/project_name/settings/base.py
+++ wagtail/project_template/project_name/project_name/settings/base.py
@@ -87,17 +87,26 @@ WSGI_APPLICATION = SITE_NAME + '.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#databases
 
+# SQLite (simplest install)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '{{ project_name }}',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',  # Set to empty string for localhost.
-        'PORT': '',  # Set to empty string for default.
-        'CONN_MAX_AGE': 600,  # number of seconds database connections should persist for
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': join(PROJECT_ROOT, 'db.sqlite3'),
     }
 }
+
+# PostgreSQL (Recommended, but requires the psycopg2 library and Postgresql development headers)
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': '{{ project_name }}',
+#         'USER': 'postgres',
+#         'PASSWORD': '',
+#         'HOST': '',  # Set to empty string for localhost.
+#         'PORT': '',  # Set to empty string for default.
+#         'CONN_MAX_AGE': 600,  # number of seconds database connections should persist for
+#     }
+# }
 
 
 # Internationalization
