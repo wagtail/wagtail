afec066013346b943852e823fc7d8f7c0b25c794
Karl Hobley <karl@torchbox.com>
2014-09-08 14:36:37 +0100

    Imports cleanup

--- wagtail/wagtailimages/views/focal_point_chooser.py
+++ wagtail/wagtailimages/views/focal_point_chooser.py
@@ -1,15 +1,8 @@
-import json
-
-from django.shortcuts import get_object_or_404, render
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import permission_required
 
 from wagtail.wagtailadmin.modal_workflow import render_modal_workflow
-from wagtail.wagtailadmin.forms import SearchForm
-
 from wagtail.wagtailimages.models import get_image_model
-from wagtail.wagtailimages.forms import get_image_form, ImageInsertionForm
-from wagtail.wagtailimages.formats import get_image_format
 
 
 @permission_required('wagtailadmin.access_admin')
