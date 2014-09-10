74a57077dbef6398f2b10219c64f57288229e985
Dave Cranwell <david@torchbox.com>
2014-09-10 13:04:28 +0100

    styling and responsive solution for focal point chooser

--- wagtail/wagtailimages/static/wagtailimages/js/focal-point-chooser.js
+++ wagtail/wagtailimages/static/wagtailimages/js/focal-point-chooser.js
@@ -1,48 +1,75 @@
-$(function() {
-    var $chooser = $('div.focal-point-chooser');
-    var $indicator = $('.current-focal-point-indicator', $chooser);
-    var $image = $('img', $chooser);
-    var $focalPointXField = $('input.focal_point_x');
-    var $focalPointYField = $('input.focal_point_y');
-    var $focalPointWidthField = $('input.focal_point_width');
-    var $focalPointHeightField = $('input.focal_point_height');
+var jcropapi;
 
-    var originalWidth = $image.data('originalWidth');
-    var originalHeight = $image.data('originalHeight');
-
-    var focalPointXOriginal = $chooser.data('focalPointX');
-    var focalPointYOriginal = $chooser.data('focalPointY');
-    var focalPointWidthOriginal = $chooser.data('focalPointWidth');
-    var focalPointHeightOriginal = $chooser.data('focalPointHeight');
-
-    $image.Jcrop({
-        trueSize: [originalWidth, originalHeight],
+function setupJcrop(image, original, focalPointOriginal, fields){
+    image.Jcrop({
+        trueSize: [original.width, original.height],
         onSelect: function(box) {
             var x = Math.floor((box.x + box.x2) / 2);
             var y = Math.floor((box.y + box.y2) / 2);
             var w = Math.floor(box.w);
             var h = Math.floor(box.h);
 
-            $focalPointXField.val(x);
-            $focalPointYField.val(y);
-            $focalPointWidthField.val(w);
-            $focalPointHeightField.val(h);
+            fields.x.val(x);
+            fields.y.val(y);
+            fields.width.val(w);
+            fields.height.val(h);
         },
         onRelease: function() {
-            $focalPointXField.val(focalPointXOriginal);
-            $focalPointYField.val(focalPointYOriginal);
-            $focalPointWidthField.val(focalPointWidthOriginal);
-            $focalPointHeightField.val(focalPointHeightOriginal);
+            fields.x.val(focalPointOriginal.x);
+            fields.y.val(focalPointOriginal.y);
+            fields.width.val(focalPointOriginal.width);
+            fields.height.val(focalPointOriginal.height);
         },
+    }, function(){
+        jcropapi = this
     });
+}
 
-    var left = focalPointXOriginal - focalPointWidthOriginal / 2
-    var top = focalPointYOriginal - focalPointHeightOriginal / 2
-    var width = focalPointWidthOriginal;
-    var height = focalPointHeightOriginal;
+$(function() {
+    var $chooser = $('div.focal-point-chooser');
+    var $indicator = $('.current-focal-point-indicator', $chooser);
+    var $image = $('img', $chooser);
 
-    $indicator.css('left', (left * 100 / originalWidth) + '%');
-    $indicator.css('top', (top * 100 / originalHeight) + '%');
-    $indicator.css('width', (width * 100 / originalWidth) + '%');
-    $indicator.css('height', (height * 100 / originalHeight) + '%');
+    var original = {
+        width: $image.data('originalWidth'),
+        height: $image.data('originalHeight')
+    }
+
+    var focalPointOriginal = {
+        x: $chooser.data('focalPointX'),
+        y: $chooser.data('focalPointY'),
+        width: $chooser.data('focalPointWidth'),
+        height: $chooser.data('focalPointHeight')
+    }
+
+    var fields = {
+        x: $('input.focal_point_x'),
+        y: $('input.focal_point_y'),
+        width: $('input.focal_point_width'),
+        height: $('input.focal_point_height')
+    }
+
+    var left = focalPointOriginal.x - focalPointOriginal.width / 2
+    var top = focalPointOriginal.y - focalPointOriginal.height / 2
+    var width = focalPointOriginal.width;
+    var height = focalPointOriginal.height;
+
+    $indicator.css('left', (left * 100 / original.width) + '%');
+    $indicator.css('top', (top * 100 / original.height) + '%');
+    $indicator.css('width', (width * 100 / original.width) + '%');
+    $indicator.css('height', (height * 100 / original.height) + '%');
+
+    var params = [$image, original, focalPointOriginal, fields];
+
+    setupJcrop.apply(this, params)
+
+    $(window).resize($.debounce(300, function(){
+        // jcrop doesn't support responsive images so to cater for resizing the browser
+        // we have to destroy() it, which doesn't properly do it,
+        // so destory it some more, then re-apply it
+        jcropapi.destroy();
+        $image.removeAttr('style');
+        $('.jcrop-holder').remove();
+        setupJcrop.apply(this, params)
+    }));
 });
