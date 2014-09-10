c221a04afac2b4348dc4792b9c01c6757b103fb0
Matt Westcott <matt@west.co.tt>
2014-09-10 17:38:19 +0100

    fix Vagrant config for new project structure

--- wagtail/project_template/vagrant/provision.sh
+++ wagtail/project_template/vagrant/provision.sh
@@ -3,7 +3,6 @@
 PROJECT_NAME=$1
 
 PROJECT_DIR=/home/vagrant/$PROJECT_NAME
-DJANGO_DIR=$PROJECT_DIR/$PROJECT_NAME
 VIRTUALENV_DIR=/home/vagrant/.virtualenvs/$PROJECT_NAME
 
 PYTHON=$VIRTUALENV_DIR/bin/python
@@ -23,17 +22,17 @@ echo "workon $PROJECT_NAME" >> /home/vagrant/.bashrc
 
 
 # Set execute permissions on manage.py as they get lost if we build from a zip file
-chmod a+x $DJANGO_DIR/manage.py
+chmod a+x $PROJECT_DIR/manage.py
 
 
 # Run syncdb/migrate/update_index
-su - vagrant -c "$PYTHON $DJANGO_DIR/manage.py syncdb --noinput && \
-                 $PYTHON $DJANGO_DIR/manage.py migrate --noinput && \
-                 $PYTHON $DJANGO_DIR/manage.py update_index"
+su - vagrant -c "$PYTHON $PROJECT_DIR/manage.py syncdb --noinput && \
+                 $PYTHON $PROJECT_DIR/manage.py migrate --noinput && \
+                 $PYTHON $PROJECT_DIR/manage.py update_index"
 
 
 # Add a couple of aliases to manage.py into .bashrc
 cat << EOF >> /home/vagrant/.bashrc
-alias dj="$PYTHON $DJANGO_DIR/manage.py"
+alias dj="$PYTHON $PROJECT_DIR/manage.py"
 alias djrun="dj runserver 0.0.0.0:8000"
 EOF
