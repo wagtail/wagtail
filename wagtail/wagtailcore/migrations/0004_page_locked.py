65d0846c4ab98401b95cf2c4c359e434a37e30a8
Karl Hobley <karl@torchbox.com>
2014-10-06 09:15:31 +0100

    Fixed migration conflicts

--- wagtail/wagtailcore/migrations/0004_page_locked.py
+++ wagtail/wagtailcore/migrations/0004_page_locked.py
@@ -7,14 +7,14 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wagtailcore', '0002_initial_data'),
+        ('wagtailcore', '0003_add_uniqueness_constraint_on_group_page_permission'),
     ]
 
     operations = [
         migrations.AddField(
             model_name='page',
             name='locked',
-            field=models.BooleanField(editable=False, default=False),
+            field=models.BooleanField(default=False, editable=False),
             preserve_default=True,
         ),
     ]
