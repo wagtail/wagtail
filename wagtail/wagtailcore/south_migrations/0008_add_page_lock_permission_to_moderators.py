98437e8721f6be17361d96cc6e5e69a8422f9948
Karl Hobley <karl@torchbox.com>
2014-10-06 12:48:41 +0100

    Fixed missing import

--- wagtail/wagtailcore/south_migrations/0008_add_page_lock_permission_to_moderators.py
+++ wagtail/wagtailcore/south_migrations/0008_add_page_lock_permission_to_moderators.py
@@ -5,6 +5,7 @@ from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import DataMigration
 from django.db import models, connection
+from django.db.transaction import set_autocommit
 
 
 class Migration(DataMigration):
