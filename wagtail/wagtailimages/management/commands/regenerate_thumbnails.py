7a47c96907d1de7ddb4157de67bcd2f7b55f5615
Damian Moore <damian@epixstudios.co.uk>
2014-10-27 18:14:15 +0000

    Management command to generate thumbnails that may be missing

---
+++ wagtail/wagtailimages/management/commands/regenerate_thumbnails.py
@@ -0,0 +1,18 @@
+mport os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from wagtail.wagtailimages.models import Rendition
+
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        print 'Checking thumbnails'
+        for ren in Rendition.objects.all():
+            if not os.path.exists(ren.file.path):
+                print 'Regenerating: ' + ren.file.path
+                thumb = ren.filter.process_image(ren.image.file)
+                with open(ren.file.path, 'w') as output:
+                    output.write(thumb.read())
+                    output.close()
+
