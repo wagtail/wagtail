089352217f14e1d7d46e6f9e77d3a08f347744f9
Karl Hobley <karl@torchbox.com>
2014-10-27 15:56:25 +0000

    Moved feature detection code into babel

--- wagtail/wagtailimages/babel.py
+++ wagtail/wagtailimages/babel.py
@@ -1,6 +1,8 @@
 import os.path
 from io import BytesIO
 
+from wagtail.wagtailimages.rect import Rect
+
 
 class ImageBabel(object):
     operations = []
@@ -208,22 +210,62 @@ try:
 
 
     class OpenCVState(object):
-        def __init__(self, image):
-            self.image = image
+        def __init__(self, image_mode, image_size, image_data):
+            self.image_mode = image_mode
+            self.image_size = image_size
+            self.image_data = image_data
+
+        def to_buffer(self):
+            return self.image_mode, self.image_size, self.image_data
 
         @classmethod
         def from_buffer(cls, buf):
-            pass
+            mode, size, data = buf
+            return cls(mode, size, data)
 
+        def opencv_grey_image(self):
+            image = cv.CreateImageHeader(self.image_size, cv.IPL_DEPTH_8U, 3)
+            cv.SetData(image, self.image_data)
 
-    @ImageBabel.operation(OpenCVState, 'detect_faces')
-    def opencv_detect_faces(state):
-        pass
+            grey_image = cv.CreateImage(self.image_size, 8, 1)
+            convert_mode = getattr(cv, 'CV_%s2GRAY' % self.image_mode)
+            cv.CvtColor(image, grey_image, convert_mode)
+
+            return grey_image
 
 
     @ImageBabel.operation(OpenCVState, 'detect_features')
     def opencv_detect_features(state):
-        pass
+        image = state.opencv_grey_image()
+        rows = state.image_size[0]
+        cols = state.image_size[1]
+
+        eig_image = cv.CreateMat(rows, cols, cv.CV_32FC1)
+        temp_image = cv.CreateMat(rows, cols, cv.CV_32FC1)
+        points = cv.GoodFeaturesToTrack(image, eig_image, temp_image, 20, 0.04, 1.0, useHarris=False)
+
+        return points
+
+
+    @ImageBabel.operation(OpenCVState, 'detect_faces')
+    def detect_faces(state):
+        cascade_filename = os.path.join(os.path.dirname(__file__), 'face_detection', 'haarcascade_frontalface_alt2.xml')
+        cascade = cv.Load(cascade_filename)
+        image = state.opencv_grey_image()
+
+        cv.EqualizeHist(image, image)
+
+        min_size = (40, 40)
+        haar_scale = 1.1
+        min_neighbors = 3
+        haar_flags = 0
+
+        faces = cv.HaarDetectObjects(
+            image, cascade, cv.CreateMemStorage(0),
+            haar_scale, min_neighbors, haar_flags, min_size
+        )
+
+        return [Rect(face[0][0], face[0][1], face[0][0] + face[0][2], face[0][1] + face[0][3]) for face in faces]
 
 except ImportError:
     OpenCVState = None
