bbcde0e408ceadbe3ce5c4988802f7264e7800cd
Damian Moore <damian@epixstudios.co.uk>
2014-10-28 09:52:41 +0000

    Typo fixes and tidy-ups

--- wagtail/wagtailimages/management/commands/regenerate_thumbnails.py
+++ wagtail/wagtailimages/management/commands/regenerate_thumbnails.py
@@ -1,4 +1,5 @@
-mport os
+from __future__ import print_function
+import os
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
@@ -7,12 +8,11 @@ from wagtail.wagtailimages.models import Rendition
 
 class Command(BaseCommand):
     def handle(self, **options):
-        print 'Checking thumbnails'
+        print('Checking thumbnails')
         for ren in Rendition.objects.all():
             if not os.path.exists(ren.file.path):
-                print 'Regenerating: ' + ren.file.path
+                print('Regenerating: ' + ren.file.path)
                 thumb = ren.filter.process_image(ren.image.file)
                 with open(ren.file.path, 'w') as output:
                     output.write(thumb.read())
-                    output.close()
 
