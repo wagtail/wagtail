09e4add28a0cb1da9a826d6cc3e2f6117efb3931
Matt Westcott <matt@west.co.tt>
2014-11-11 11:51:47 +0000

    Fix page chooser on sites interface

--- wagtail/wagtailsites/forms.py
+++ wagtail/wagtailsites/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 
 from wagtail.wagtailcore.models import Site
+from wagtail.wagtailadmin.widgets import AdminPageChooser
 
 
 class SiteForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(SiteForm, self).__init__(*args, **kwargs)
-        self.fields['root_page'].widget = forms.HiddenInput()
+        self.fields['root_page'].widget = AdminPageChooser()
 
     required_css_class = "required"
 
--- wagtail/wagtailsites/templates/wagtailsites/includes/site_form.js
+++
@@ -1,5 +0,0 @@
-(function() {
-   function fixPrefix(str) { return str; }
-   createPageChooser(fixPrefix('id_root_page'));
-
-})();
