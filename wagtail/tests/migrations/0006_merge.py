c52b48c9f2fc281792a926ff760fc523cbfb58c4
Matt Westcott <matt@west.co.tt>
2014-11-13 13:02:40 +0000

    merge parallel migrations in tests

---
+++ wagtail/tests/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tests', '0005_auto_20141113_0642'),
+        ('tests', '0005_auto_20141008_0122'),
+    ]
+
+    operations = [
+    ]
