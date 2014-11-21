f85372e4c5cdd40301e0b3e0631ddeb79d965f24
Julien Antony <julien.antony@scopart.fr>
2014-11-21 16:26:03 +0100

    [fix] mysql support #541

--- wagtail/wagtailimages/migrations/0004_make_focal_point_key_not_nullable.py
+++ wagtail/wagtailimages/migrations/0004_make_focal_point_key_not_nullable.py
@@ -5,11 +5,18 @@ from django.db import models, migrations
 
 
 def remove_duplicate_renditions(apps, schema_editor):
-    schema_editor.execute("""
-        DELETE FROM wagtailimages_rendition WHERE image_id || '-' || filter_id IN (
-            SELECT image_id || '-' || filter_id FROM wagtailimages_rendition WHERE focal_point_key IS NULL GROUP BY image_id, filter_id HAVING COUNT(*) > 1
-        ) AND focal_point_key IS NULL
-    """)
+    if schema_editor.connection.vendor == 'mysql':
+        schema_editor.execute("""
+            DELETE FROM `wagtailimages_rendition` WHERE CONCAT(image_id, '-', filter_id) IN (
+                SELECT CONCAT(image_id, '-', filter_id) FROM (SELECT * FROM `wagtailimages_rendition`) as x WHERE `focal_point_key` IS NULL GROUP BY image_id, filter_id HAVING COUNT(*) > 1
+            ) AND `focal_point_key` IS NULL
+        """)
+    else:
+        schema_editor.execute("""
+            DELETE FROM wagtailimages_rendition WHERE image_id || '-' || filter_id IN (
+                SELECT image_id || '-' || filter_id FROM wagtailimages_rendition WHERE focal_point_key IS NULL GROUP BY image_id, filter_id HAVING COUNT(*) > 1
+            ) AND focal_point_key IS NULL
+        """)
 
 class Migration(migrations.Migration):
 
