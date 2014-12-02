0e61e9a0c484a8c7aed83b21a7c8c5e83ab82072
Karl Hobley <karlhobley10@gmail.com>
2014-12-02 09:27:46 +0000

    Updated deprecation warnings

--- wagtail/utils/deprecation.py
+++ wagtail/utils/deprecation.py
@@ -1,6 +1,6 @@
-class RemovedInWagtail09Warning(DeprecationWarning):
+class RemovedInWagtail10Warning(DeprecationWarning):
     pass
 
 
-class RemovedInWagtail10Warning(PendingDeprecationWarning):
+class RemovedInWagtail11Warning(PendingDeprecationWarning):
     pass
