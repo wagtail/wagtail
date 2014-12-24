896654de425fd6c205f493665608c08d2c5163a4
Karl Hobley <karl@torchbox.com>
2014-12-24 13:56:11 +0000

    Failing tests for users without username

--- wagtail/tests/models.py
+++ wagtail/tests/models.py
@@ -80,6 +80,48 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         return self.first_name
 
 
+class CustomUserWithoutUsernameManager(BaseUserManager):
+    def _create_user(self, email, password,
+                     is_staff, is_superuser, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        email = self.normalize_email(email)
+        user = self.model(email=email,
+                          is_staff=is_staff, is_active=True,
+                          is_superuser=is_superuser, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        return self._create_user(email, password, False, False,
+                                 **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True,
+                                 **extra_fields)
+
+
+class CustomUserWithoutUsername(AbstractBaseUser):
+    email = models.EmailField(max_length=255, blank=True)
+    is_staff = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+
+    USERNAME_FIELD = 'email'
+
+    objects = CustomUserWithoutUsernameManager()
+
+    def get_full_name(self):
+        return self.first_name + ' ' + self.last_name
+
+    def get_short_name(self):
+        return self.first_name
+
+
 # Link fields
 
 class LinkFields(models.Model):
