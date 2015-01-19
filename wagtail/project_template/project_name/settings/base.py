5d3dc6fcbe1a79bf0ad6117f7e40b82d97ce67c3
Karl Hobley <karlhobley10@gmail.com>
2015-01-19 14:05:13 +0000

    No longer need to specify LOGIN_URL

--- wagtail/project_template/project_name/settings/base.py
+++ wagtail/project_template/project_name/settings/base.py
@@ -143,7 +143,6 @@ TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
 
 # Wagtail settings
 
-LOGIN_URL = 'wagtailadmin_login'
 LOGIN_REDIRECT_URL = 'wagtailadmin_home'
 
 WAGTAIL_SITE_NAME = "{{ project_name }}"
