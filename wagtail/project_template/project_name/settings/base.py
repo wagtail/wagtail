32f2dddaa359324b312bbb087369c6186ecfd1b8
Karl Hobley <karlhobley10@gmail.com>
2015-01-19 14:05:55 +0000

    Set default login redirect to dashboard
    No longer need to specify LOGIN_REDIRECT_URL

--- wagtail/project_template/project_name/settings/base.py
+++ wagtail/project_template/project_name/settings/base.py
@@ -143,8 +143,6 @@ TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
 
 # Wagtail settings
 
-LOGIN_REDIRECT_URL = 'wagtailadmin_home'
-
 WAGTAIL_SITE_NAME = "{{ project_name }}"
 
 # Use Elasticsearch as the search backend for extra performance and better search results:
