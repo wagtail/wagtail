7c2f61d017842147244d12131a483ad652e25540
Karl Hobley <karlhobley10@gmail.com>
2015-01-28 10:07:52 +0000

    End Python 3.2 support. Fixes #931

--- setup.py
+++ setup.py
@@ -71,7 +71,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Framework :: Django',
