f56d927be08ccba228163aac389c3fe6cbfee49f
Matt Westcott <matt@west.co.tt>
2015-02-06 16:40:51 +0000

    provide a render_html method on WidgetWithScript so that subclasses can override the HTML part of the rendering too

--- wagtail/utils/widgets.py
+++ wagtail/utils/widgets.py
@@ -5,16 +5,20 @@ from django.utils.safestring import mark_safe
 
 
 class WidgetWithScript(Widget):
+    def render_html(self, name, value, attrs):
+        """Render the HTML (non-JS) portion of the field markup"""
+        return super(WidgetWithScript, self).render(name, value, attrs)
+
     def render(self, name, value, attrs=None):
-        widget = super(WidgetWithScript, self).render(name, value, attrs)
+        widget_html = self.render_html(name, value, attrs)
 
         final_attrs = self.build_attrs(attrs, name=name)
         id_ = final_attrs.get('id', None)
         if id_ is None:
-            return widget
+            return widget_html
 
         js = self.render_js_init(id_, name, value)
-        out = '{0}<script>{1}</script>'.format(widget, js)
+        out = '{0}<script>{1}</script>'.format(widget_html, js)
         return mark_safe(out)
 
     def render_js_init(self, id_, name, value):
