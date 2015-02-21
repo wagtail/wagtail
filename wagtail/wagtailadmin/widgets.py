550d4e94daa98fd3c53548f3a6b91109619d1423
Matt Westcott <matt@west.co.tt>
2015-02-21 17:37:14 +0000

    Standardise date/time formats between admin datetime widgets and Javascript code.
    The JS for the datetime picker was previously updated to include seconds, to match Django's default format and thus fix #479 - however, the same bug also affected the time picker, which was left unfixed. Since we typically don't want seconds in our datetimes anyhow, we reverse that fix here, and instead update AdminDateWidget / AdminTimeWidget / AdminDateTimeWidget to explicitly use the seconds-less format that the JS expects. This also ensures that the JS behaviour will not break as a result of a site owner setting a non-standard value for DATE_INPUT_FORMATS / TIME_INPUT_FORMATS / DATETIME_INPUT_FORMATS in their site settings.

--- wagtail/wagtailadmin/widgets.py
+++ wagtail/wagtailadmin/widgets.py
@@ -28,16 +28,28 @@ class AdminAutoHeightTextInput(WidgetWithScript, widgets.Textarea):
         return '$("#{0}").autosize();'.format(id_)
 
 class AdminDateInput(WidgetWithScript, widgets.DateInput):
+    # Set a default date format to match the one that our JS date picker expects -
+    # it can still be overridden explicitly, but this way it won't be affected by
+    # the DATE_INPUT_FORMATS setting
+    def __init__(self, attrs=None, format='%Y-%m-%d'):
+        super(AdminDateInput, self).__init__(attrs=attrs, format=format)
+
     def render_js_init(self, id_, name, value):
         return 'initDateChooser({0});'.format(json.dumps(id_))
 
 
 class AdminTimeInput(WidgetWithScript, widgets.TimeInput):
+    def __init__(self, attrs=None, format='%H:%M'):
+        super(AdminTimeInput, self).__init__(attrs=attrs, format=format)
+
     def render_js_init(self, id_, name, value):
         return 'initTimeChooser({0});'.format(json.dumps(id_))
 
 
 class AdminDateTimeInput(WidgetWithScript, widgets.DateTimeInput):
+    def __init__(self, attrs=None, format='%Y-%m-%d %H:%M'):
+        super(AdminDateTimeInput, self).__init__(attrs=attrs, format=format)
+
     def render_js_init(self, id_, name, value):
         return 'initDateTimeChooser({0});'.format(json.dumps(id_))
 
