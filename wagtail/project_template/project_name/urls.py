97d2c06a7e747481feea0358f3c9e54a9453c271
Karl Hobley <karlhobley10@gmail.com>
2015-04-02 13:09:34 +0100

    Removed patterns() from project template

--- wagtail/project_template/project_name/urls.py
+++ wagtail/project_template/project_name/urls.py
@@ -1,6 +1,6 @@
 import os
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
@@ -11,7 +11,7 @@ from wagtail.wagtaildocs import urls as wagtaildocs_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^django-admin/', include(admin.site.urls)),
 
     url(r'^admin/', include(wagtailadmin_urls)),
@@ -19,7 +19,7 @@ urlpatterns = patterns('',
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'', include(wagtail_urls)),
-)
+]
 
 
 if settings.DEBUG:
