09835326d799cd05200856d6114b2df126d21bfb
Karl Hobley <karlhobley10@gmail.com>
2015-04-09 14:07:41 +0100

    Revert "Make subpage_urls a property on RoutablePageTest"
    This reverts commit d80a92cfe45907b9f91fd212a3b06fa0b2321364.

--- wagtail/tests/routablepage/models.py
+++ wagtail/tests/routablepage/models.py
@@ -8,14 +8,12 @@ def routable_page_external_view(request, arg):
     return HttpResponse("EXTERNAL VIEW: " + arg)
 
 class RoutablePageTest(RoutablePage):
-    @property
-    def subpage_urls(self):
-        return (
-            url(r'^$', self.main, name='main'),
-            url(r'^archive/year/(\d+)/$', self.archive_by_year, name='archive_by_year'),
-            url(r'^archive/author/(?P<author_slug>.+)/$', self.archive_by_author, name='archive_by_author'),
-            url(r'^external/(.+)/$', routable_page_external_view, name='external_view')
-        )
+    subpage_urls = (
+        url(r'^$', 'main', name='main'),
+        url(r'^archive/year/(\d+)/$', 'archive_by_year', name='archive_by_year'),
+        url(r'^archive/author/(?P<author_slug>.+)/$', 'archive_by_author', name='archive_by_author'),
+        url(r'^external/(.+)/$', routable_page_external_view, name='external_view')
+    )
 
     def archive_by_year(self, request, year):
         return HttpResponse("ARCHIVE BY YEAR: " + str(year))
