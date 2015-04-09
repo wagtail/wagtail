122bbc3c3f855d7cf2a6b0ca2676a0d0f098df51
Dave Cranwell <david@torchbox.com>
2015-04-09 16:14:56 +0100

    removed js used for early testing

--- wagtail/wagtailadmin/static/wagtailadmin/js/blocks/chooser.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    window.Chooser = function(definitionPrefix) {
-        return function(elementPrefix) {
-            $('#' + elementPrefix + '-button').click(function() {
-                alert('hello, I am a chooser for ' + elementPrefix + ', which is of type ' + definitionPrefix);
-            });
-        };
-    };
-})(jQuery);
