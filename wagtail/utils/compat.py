115ffb22128e12a0cc88b7c0cd1dd9bde04fb768
Matt Westcott <matt@west.co.tt>
2015-04-13 19:46:59 +0100

    Check Django version instead of hasattr

--- wagtail/utils/compat.py
+++ wagtail/utils/compat.py
@@ -1,5 +1,11 @@
+import django
+
+
 def get_related_model(rel):
     # In Django 1.7 and under, the related model is accessed by doing: rel.model
     # This was renamed in Django 1.8 to rel.related_model. rel.model now returns
     # the base model.
-    return getattr(rel, 'related_model', rel.model)
+    if django.VERSION >= (1, 8):
+        return rel.related_model
+    else:
+        return rel.model
