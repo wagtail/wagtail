2b1eb6560a339f6c81ca93d58eb75d41e51f70ad
Karl Hobley <karlhobley10@gmail.com>
2015-04-13 15:34:52 +0100

    Search tests improvements

--- wagtail/wagtailsearch/tests/test_db_backend.py
+++ wagtail/wagtailsearch/tests/test_db_backend.py
@@ -11,3 +11,7 @@ class TestDBBackend(BackendTests, TestCase):
     @unittest.expectedFailure
     def test_callable_indexed_field(self):
         super(TestDBBackend, self).test_callable_indexed_field()
+
+    @unittest.expectedFailure
+    def test_update_index_command(self):
+        super(TestDBBackend, self).test_update_index_command()
