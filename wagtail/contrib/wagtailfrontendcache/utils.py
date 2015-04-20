4a9a23cc01acdece1f744fd048e878fade069c23
Matt Westcott <matt@west.co.tt>
2015-04-20 20:02:21 +0100

    Skip purge_page_from_cache if page has no routable URL - fixes #1208

--- wagtail/contrib/wagtailfrontendcache/utils.py
+++ wagtail/contrib/wagtailfrontendcache/utils.py
@@ -61,8 +61,12 @@ def purge_url_from_cache(url, backend_settings=None, backends=None):
 
 
 def purge_page_from_cache(page, backend_settings=None, backends=None):
+    page_url = page.full_url
+    if page_url is None:  # nothing to be done if the page has no routable URL
+        return
+
     for backend_name, backend in get_backends(backend_settings=backend_settings, backends=backends).items():
         # Purge cached paths from cache
         for path in page.specific.get_cached_paths():
-            logger.info("[%s] Purging URL: %s", backend_name, page.full_url + path[1:])
-            backend.purge(page.full_url + path[1:])
+            logger.info("[%s] Purging URL: %s", backend_name, page_url + path[1:])
+            backend.purge(page_url + path[1:])
