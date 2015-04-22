e0c8af0f39163256edcb7c6adc6f0d665c3df984
Karl Hobley <karlhobley10@gmail.com>
2015-04-22 11:20:36 +0100

    Copied project template from Django 1.8.0

---
+++ wagtail/project_template/project_name/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', '{{ project_name }}.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+]
