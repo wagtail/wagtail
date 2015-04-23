f70eb09d072afad91bc131d7599ffc718852499f
Karl Hobley <karlhobley10@gmail.com>
2015-04-23 12:33:29 +0100

    Added streaming sendfile backend
    The simple sendfile backend doesn't stream responses. This commit adds a backend that does.

--- wagtail/wagtaildocs/views/serve.py
+++ wagtail/wagtaildocs/views/serve.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from django.conf import settings
 
 from wagtail.utils.sendfile import sendfile
+from wagtail.utils import sendfile_streaming_backend
 
 from wagtail.wagtaildocs.models import Document, document_served
 
@@ -15,7 +16,5 @@ def serve(request, document_id, document_filename):
     if hasattr(settings, 'SENDFILE_BACKEND'):
         return sendfile(request, doc.file.path, attachment=True, attachment_filename=doc.filename)
     else:
-        # Fallback to simple backend if user hasn't specified SENDFILE_BACKEND (will crash by default)
-        from sendfile.backends.simple import sendfile as simple_sendfile_backend
-
-        return sendfile(request, doc.file.path, attachment=True, attachment_filename=doc.filename, backend=simple_sendfile_backend)
+        # Fallback to streaming backend if user hasn't specified SENDFILE_BACKEND
+        return sendfile(request, doc.file.path, attachment=True, attachment_filename=doc.filename, backend=sendfile_streaming_backend.sendfile)
