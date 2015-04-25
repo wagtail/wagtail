312033ba4e10063b58848a6575139d9d8317f990
Frank Wiles <frank@revsys.com>
2015-04-25 13:01:58 -0500

    Update docs copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Wagtail'
-copyright = u'2014, Torchbox'
+copyright = u'2015, Torchbox'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
