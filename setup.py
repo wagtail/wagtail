d0abaa77fe8f575fe402ac9c0e4c7fde0be51c09
Matt Westcott <matt@west.co.tt>
2015-04-27 15:25:05 +0100

    support Django 1.8 in setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ PY3 = sys.version_info[0] == 3
 
 
 install_requires = [
-    "Django>=1.7.1,<1.8",
+    "Django>=1.7.1,<1.9",
     "django-compressor>=1.4",
     "django-libsass>=0.2",
     "django-modelcluster>=0.6",
