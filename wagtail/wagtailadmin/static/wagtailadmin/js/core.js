e810bf47e8a79aa073d6e0f0449f3c8d924f3544
Mac Chapman <mac@veryhappythings.co.uk>
2015-05-14 10:19:56 +0100

    Change quotes to match code style

--- wagtail/wagtailadmin/static/wagtailadmin/js/core.js
+++ wagtail/wagtailadmin/static/wagtailadmin/js/core.js
@@ -13,7 +13,7 @@ function escapeHtml(text) {
         '<': '&lt;',
         '>': '&gt;',
         '"': '&quot;',
-        "'": '&#039;'
+        '\'': '&#039;'
     };
 
     return text.replace(/[&<>"']/g, function(m) {
