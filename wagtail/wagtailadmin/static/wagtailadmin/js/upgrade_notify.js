1954baa18c0d65a0433cd044bcbf454587eddcb9
Dave Cranwell <david@torchbox.com>
2015-05-15 15:34:43 +0100

    linted

--- wagtail/wagtailadmin/static/wagtailadmin/js/upgrade_notify.js
+++ wagtail/wagtailadmin/static/wagtailadmin/js/upgrade_notify.js
@@ -1,42 +1,48 @@
-$(function(){
+$(function() {
     'use strict';
 
-    /* 
+    /*
     *   Expected JSON payload:
     *   {
     *       "version" : "1.2.3",    // Version number. Can only contain numbers and decimal point.
     *       "url" : "https://wagtail.io"  // Absolute URL to page/file containing release notes or actual package. It's up to you.
     *   }
-    */    
+    */
 
     function cmpVersion(a, b) {
-        var i, cmp, len, re = /(\.0)+[^\.]*$/;
+        var i;
+        var cmp;
+        var len;
+        var re = /(\.0)+[^\.]*$/;
+
         a = (a + '').replace(re, '').split('.');
         b = (b + '').replace(re, '').split('.');
         len = Math.min(a.length, b.length);
-        for( i = 0; i < len; i++ ) {
+        for (i = 0; i < len; i++) {
             cmp = parseInt(a[i], 10) - parseInt(b[i], 10);
-            if( cmp !== 0 ) {
+            if (cmp !== 0) {
                 return cmp;
             }
         }
+
         return a.length - b.length;
     }
+
     function gtVersion(a, b) {
         return cmpVersion(a, b) > 0;
     }
 
-    var trackingUrl = "https://releases.wagtail.io/latest.txt";
+    var trackingUrl = 'https://releases.wagtail.io/latest.txt';
     var currentVersion = window.wagtailVersion;
 
     $.getJSON(trackingUrl, function(data) {
-        try{
-            if(data.version && gtVersion(data.version, currentVersion)){
+        try {
+            if (data.version && gtVersion(data.version, currentVersion)) {
                 var $container = $('.panel-upgrade-notification')
                 $('.newversion', $container).html(data.version);
                 $('.releasenotes-link', $container).attr('href', data.url);
                 $container.show();
             }
-        } catch (e){}
+        } catch (e) {}
     });
 });
