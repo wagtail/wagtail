f3f8e13af3d7167235024f1fd0b85e8006931632
Karl Hobley <karlhobley10@gmail.com>
2015-05-17 12:02:15 +0100

    Tidy up render_modal_workflow
    Also, now returns valid JSON with correct content type header

--- wagtail/wagtailadmin/modal_workflow.py
+++ wagtail/wagtailadmin/modal_workflow.py
@@ -6,20 +6,18 @@ from wagtail.utils.compat import render_to_string
 
 
 def render_modal_workflow(request, html_template, js_template, template_vars=None):
-    """"
+    """
     Render a response consisting of an HTML chunk and a JS onload chunk
     in the format required by the modal-workflow framework.
     """
-    response_keyvars = []
+    response_keyvars = {}
 
     if html_template:
         html = render_to_string(html_template, template_vars or {}, request=request)
-        response_keyvars.append("'html': %s" % json.dumps(html))
+        response_keyvars['html'] = html
 
     if js_template:
         js = render_to_string(js_template, template_vars or {}, request=request)
-        response_keyvars.append("'onload': %s" % js)
+        response_keyvars['onload'] = js
 
-    response_text = "{%s}" % ','.join(response_keyvars)
-
-    return HttpResponse(response_text, content_type="text/javascript")
+    return HttpResponse(json.dumps(response_keyvars), content_type="application/json")
