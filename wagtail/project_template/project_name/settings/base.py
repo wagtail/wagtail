dc6a53a5fb825bca1a53fbed3ba08b240166d34e
Karl Hobley <karlhobley10@gmail.com>
2015-05-26 16:12:53 +0100

    Missing comma

--- wagtail/project_template/project_name/settings/base.py
+++ wagtail/project_template/project_name/settings/base.py
@@ -123,7 +123,7 @@ STATICFILES_FINDERS = (
 )
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, 'static')
+    os.path.join(PROJECT_DIR, 'static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
