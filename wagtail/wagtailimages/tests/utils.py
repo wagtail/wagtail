52ee5aa4defadbcd8c763c7fc07e528600b75219
Ben Emery <ben.s.emery@gmail.com>
2015-06-04 11:05:38 +0100

    Allow for overriding image upload path

--- wagtail/wagtailimages/tests/utils.py
+++ wagtail/wagtailimages/tests/utils.py
@@ -9,8 +9,8 @@ from wagtail.wagtailimages.models import get_image_model
 Image = get_image_model()
 
 
-def get_test_image_file(filename='test.png'):
+def get_test_image_file(filename='test.png', colour='white', size=(640, 480)):
     f = BytesIO()
-    image = PIL.Image.new('RGB', (640, 480), 'white')
+    image = PIL.Image.new('RGB', size, colour)
     image.save(f, 'PNG')
     return ImageFile(f, name=filename)
