109f1d799bf1b16b64fbdff77dd0eb20a4a3a4c9
Dave Cranwell <david@torchbox.com>
2015-06-04 17:44:08 +0100

    added new gulpfile directory with modularised tasks

---
+++ gulpfile.js/lib/simplyCopy.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var gutil = require('gulp-util');
+var path = require('path');
+var config = require('../config');
+
+/*
+ * Simple copy task - just copoes files from the source to the destination,
+ * with no compilation, minification, or other intelligence
+ *
+ */
+ 
+var renameSrcToDest = function() {
+    return rename(function(filePath) {
+        filePath.dirname = filePath.dirname.replace(
+            '/' + config.srcDir + '/',
+            '/' + config.destDir + '/');
+    });
+};
+
+
+var simpleCopyTask = function(glob) {
+    return function() {
+        var sources = config.apps.map(function(app) {
+            return path.join(app.sourceFiles, app.appName, glob);
+        });
+
+        return gulp.src(sources, {base: '.'})
+            .pipe(renameSrcToDest())
+            .pipe(gulp.dest('.'))
+            .on('error', gutil.log);
+    };
+};
+
+module.exports = simpleCopyTask;
