4c51d97ae5f91b4d77e1ba0ad7a6980a87cb059a
Karl Hobley <karlhobley10@gmail.com>
2015-06-05 14:23:15 +0100

    Catch all embed exceptions in frontend code

--- wagtail/wagtailembeds/format.py
+++ wagtail/wagtailembeds/format.py
@@ -20,7 +20,7 @@ def embed_to_frontend_html(url):
             'embed': embed,
             'ratio': ratio,
         })
-    except embeds.EmbedNotFoundException:
+    except embeds.EmbedException:
         return ''
 
 
@@ -32,6 +32,6 @@ def embed_to_editor_html(url):
         return render_to_string('wagtailembeds/embed_editor.html', {
             'embed': embed,
         })
-    except embeds.EmbedNotFoundException:
+    except embeds.EmbedException:
         # Could be replaced with a nice error message
         return ''
