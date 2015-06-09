2a5535e5da44fc260b183bd7a625587b82b8f84e
Tim Heap <tim@timheap.me>
2015-06-09 15:27:53 +1000

    Add support for user models with no username
    The User forms were the only known sections to fail when using a swapped
    User model that did not have a `username` field, using an `email` field
    instead for example.
    
    This commit makes the User forms work with `User.USERNAME_FIELD` instead
    of hardcoding `"username"`.
    
    Truly strange user models are not yet supported.
    
    The tests do not test this custom user functionality yet, as this is
    quite difficult to do. Lots of code does not support dynamically
    changing the `AUTH_USER_MODEL` setting for the tests, so only one custom
    user model for most of the tests.
    
    Fixes #1345
    See #1380

--- wagtail/wagtailusers/forms.py
+++ wagtail/wagtailusers/forms.py
@@ -1,7 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm
-from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import Group, Permission
 from django.forms.models import inlineformset_factory
 
@@ -13,9 +12,37 @@ from wagtail.wagtailcore.models import UserPagePermissionsProxy, GroupPagePermis
 
 User = get_user_model()
 
+# The standard fields each user model is expected to have, as a minimum.
+standard_fields = set(['email', 'first_name', 'last_name', 'is_superuser', 'groups'])
 
-# extend Django's UserCreationForm with an 'is_superuser' field
-class UserCreationForm(BaseUserCreationForm):
+
+class UsernameForm(forms.ModelForm):
+    """
+    Intelligently sets up the username field if it is infact a username. If the
+    User model has been swapped out, and the username field is an email or
+    something else, dont touch it.
+    """
+    def __init__(self, *args, **kwargs):
+        super(UsernameForm, self).__init__(*args, **kwargs)
+        if User.USERNAME_FIELD == 'username':
+            field = self.fields['username']
+            field.regex = r"^[\w.@+-]+$"
+            field.help_text = _("Required. 30 characters or fewer. Letters, "
+                                "digits and @/./+/-/_ only.")
+            field.error_messages = field.error_messages.copy()
+            field.error_messages.update({
+                'invalid': _("This value may contain only letters, numbers "
+                             "and @/./+/-/_ characters.")})
+
+    @property
+    def username_field(self):
+        return self[User.USERNAME_FIELD]
+
+    def separate_username_field(self):
+        return User.USERNAME_FIELD not in standard_fields
+
+
+class UserCreationForm(UsernameForm):
 
     required_css_class = "required"
     is_superuser = forms.BooleanField(
@@ -24,26 +51,32 @@ class UserCreationForm(BaseUserCreationForm):
         help_text=_("If ticked, this user has the ability to manage user accounts.")
     )
 
+    password1 = forms.CharField(
+        label=_("Password"),
+        required=False,
+        widget=forms.PasswordInput,
+        help_text=_("Leave blank if not changing."))
+    password2 = forms.CharField(
+        label=_("Password confirmation"), required=False,
+        widget=forms.PasswordInput,
+        help_text=_("Enter the same password as above, for verification."))
+
     email = forms.EmailField(required=True, label=_("Email"))
     first_name = forms.CharField(required=True, label=_("First Name"))
     last_name = forms.CharField(required=True, label=_("Last Name"))
 
     class Meta:
         model = User
-        fields = ("username", "email", "first_name", "last_name", "is_superuser", "groups")
+        fields = set([User.USERNAME_FIELD]) | standard_fields
         widgets = {
             'groups': forms.CheckboxSelectMultiple
         }
 
     def clean_username(self):
-        # Method copied from parent
-
-        username = self.cleaned_data["username"]
+        username_field = User.USERNAME_FIELD
+        username = self.cleaned_data[username_field]
         try:
-            # When called from BaseUserCreationForm, the method fails if using a AUTH_MODEL_MODEL,
-            # This is because the following line tries to perform a lookup on
-            # the default "auth_user" table.
-            User._default_manager.get(username=username)
+            User._default_manager.get(**{username_field: username})
         except User.DoesNotExist:
             return username
         raise forms.ValidationError(
@@ -51,8 +84,19 @@ class UserCreationForm(BaseUserCreationForm):
             code='duplicate_username',
         )
 
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
+
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
 
         # users can access django-admin iff they are a superuser
         user.is_staff = user.is_superuser
@@ -65,21 +109,13 @@ class UserCreationForm(BaseUserCreationForm):
 
 # Largely the same as django.contrib.auth.forms.UserCreationForm, but with enough subtle changes
 # (to make password non-required) that it isn't worth inheriting...
-class UserEditForm(forms.ModelForm):
+class UserEditForm(UsernameForm):
     required_css_class = "required"
 
     error_messages = {
         'duplicate_username': _("A user with that username already exists."),
         'password_mismatch': _("The two password fields didn't match."),
     }
-    username = forms.RegexField(
-        label=_("Username"),
-        max_length=30,
-        regex=r'^[\w.@+-]+$',
-        help_text=_("Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only."),
-        error_messages={
-            'invalid': _("This value may contain only letters, numbers and @/./+/-/_ characters.")
-        })
 
     email = forms.EmailField(required=True, label=_("Email"))
     first_name = forms.CharField(required=True, label=_("First Name"))
@@ -103,7 +139,7 @@ class UserEditForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ("username", "email", "first_name", "last_name", "is_active", "is_superuser", "groups")
+        fields = set([User.USERNAME_FIELD, "is_active"]) | standard_fields
         widgets = {
             'groups': forms.CheckboxSelectMultiple
         }
@@ -112,8 +148,10 @@ class UserEditForm(forms.ModelForm):
         # Since User.username is unique, this check is redundant,
         # but it sets a nicer error message than the ORM. See #13147.
         username = self.cleaned_data["username"]
+        username_field = User.USERNAME_FIELD
         try:
-            User._default_manager.exclude(id=self.instance.id).get(username=username)
+            User._default_manager.exclude(id=self.instance.id).get(**{
+                username_field: username})
         except User.DoesNotExist:
             return username
         raise forms.ValidationError(self.error_messages['duplicate_username'])
