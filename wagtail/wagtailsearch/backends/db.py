98301838cdf0277355e39510e660134ff6015fa7
Karl Hobley <karlhobley10@gmail.com>
2015-07-03 16:54:55 +0100

    Fix incorrect import

--- wagtail/wagtailsearch/backends/db.py
+++ wagtail/wagtailsearch/backends/db.py
@@ -1,3 +1,3 @@
-from .db import DatabaseSearchQuery as DBSearchQuery, DatabaseSearchResults as DBSearchResults, DatabaseSearchBackend as DBSearch  # noqa
+from .database import DatabaseSearchQuery as DBSearchQuery, DatabaseSearchResults as DBSearchResults, DatabaseSearchBackend as DBSearch  # noqa
 
 SearchBackend = DBSearch
