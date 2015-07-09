cb2146930b17d9546c877c86f88316b96f9ac1c6
Tim Heap <tim@timheap.me>
2015-07-09 13:00:05 +1000

    Refactor search index chunk updating

--- wagtail/wagtailsearch/management/commands/update_index.py
+++ wagtail/wagtailsearch/management/commands/update_index.py
@@ -2,6 +2,7 @@ from optparse import make_option
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from django.db import transaction
 
 from wagtail.wagtailsearch.index import get_indexed_models
 from wagtail.wagtailsearch.backends import get_search_backend
@@ -46,44 +47,13 @@ class Command(BaseCommand):
             rebuilder.add_model(model)
 
             # Add items (1000 at a time)
-            i = 0
             count = 0
-            self.stdout.write(INDENT, ending='')
-            while True:
-                # Get next batch of items
-                items = list(queryset[i * 1000:][:1000])
-                i += 1
+            for chunk in self.progress(self.queryset_chunks(queryset)):
+                rebuilder.add_items(model, chunk)
+                count += len(chunk)
 
-                # Exit loop if there are no items left to index
-                if not items:
-                    self.stdout.write('')
-                    self.stdout.write(INDENT + "Indexed %d %s" % (
-                        count,
-                        model._meta.verbose_name_plural,
-                    ))
-                    self.stdout.write('')
-
-                    break
-
-                # Add the items
-                rebuilder.add_items(model, items)
-
-                # Increment count
-                count += len(items)
-
-                # Print a progress dot
-                self.stdout.write('.', ending='')
-
-                # Print space after every 10 dots
-                if i % 10 == 0:
-                    self.stdout.write(' ', ending='')
-
-                # Print newline after every 50 dots
-                if i % 50 == 0:
-                    self.stdout.write('')
-                    self.stdout.write(INDENT, ending='')
-
-                self.stdout.flush()
+            self.stdout.write(INDENT + "Indexed %d %s" % (
+                count, model._meta.verbose_name_plural))
 
         # Finish rebuild
         self.stdout.write(backend_name + ": Finishing rebuild")
@@ -116,3 +86,44 @@ class Command(BaseCommand):
         # Update backends
         for backend_name in backend_names:
             self.update_backend(backend_name, object_list)
+
+    def progress(self, iterable, gap=10, new_line=5, indent=INDENT):
+        """
+        Print a progress meter while iterating over an iterable. Use it as part
+        of a ``for`` loop::
+
+            for item in self.progress(big_long_list):
+                self.do_expensive_computation(item)
+
+        A ``.`` character is printed for every value in the iterable,
+        a space every ``gap`` items, and a new line every ``new_line`` gaps.
+        Each line of output is prefixed with ``indent``
+        """
+        self.stdout.write(indent, ending='')
+        for i, value in enumerate(iterable, start=1):
+            yield value
+            self.stdout.write('.', ending='')
+            if i % (gap * new_line) == 0:
+                self.stdout.write('')
+                self.stdout.write(indent, ending='')
+            elif i % gap == 0:
+                self.stdout.write(' ', ending='')
+            self.stdout.flush()
+        self.stdout.write('')
+
+    # Atomic so the count of models doesnt change as it is iterated
+    @transaction.atomic
+    def queryset_chunks(self, qs, chunk_size=1000):
+        """
+        Yield a queryset in chunks of at most ``chunk_size``. The chunk yeilded
+        will be a list, not a queryset. Iterating over the chunks is done in a
+        transaction so that the order and count of items in the queryset
+        remains stable.
+        """
+        i = 0
+        while True:
+            items = list(qs[i * chunk_size:][:chunk_size])
+            if not items:
+                break
+            yield items
+            i += 1
