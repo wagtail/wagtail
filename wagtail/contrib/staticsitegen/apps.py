8442c99fd1160396dc29933be535e421b4afaf1c
Karl Hobley <karlhobley10@gmail.com>
2015-07-09 17:12:19 +0100

    Renamed wagtailmedusa to staticsitegen

---
+++ wagtail/contrib/staticsitegen/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WagtailStaticSiteGenAppConfig(AppConfig):
+    name = 'wagtail.contrib.staticsitegen'
+    label = 'wagtailstaticsitegen'
+    verbose_name = "Wagtail static site generator"
