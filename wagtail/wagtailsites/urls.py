d53540a944518992d8183aa5838c528fffce8e32
Karl Hobley <karlhobley10@gmail.com>
2015-07-10 11:53:47 +0100

    Put all wagtailsites URLs into a namespace

--- wagtail/wagtailsites/urls.py
+++ wagtail/wagtailsites/urls.py
@@ -3,9 +3,9 @@ from wagtail.wagtailsites import views
 
 urlpatterns = [
 
-    url(r'^$', views.index, name='wagtailsites_index'),
-    url(r'^new/$', views.create, name='wagtailsites_create'),
-    url(r'^(\d+)/$', views.edit, name='wagtailsites_edit'),
-    url(r'^(\d+)/delete/$', views.delete, name='wagtailsites_delete'),
+    url(r'^$', views.index, name='index'),
+    url(r'^new/$', views.create, name='create'),
+    url(r'^(\d+)/$', views.edit, name='edit'),
+    url(r'^(\d+)/delete/$', views.delete, name='delete'),
 
 ]
