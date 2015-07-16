d4fe6fad542b8b2547b43302a3e797fd29ae672d
Richard Mitchell <richard.j.mitchell@gmail.com>
2015-07-16 16:01:01 +0100

    Add option for custom editor CSS.

--- wagtail/wagtailhallo/wagtail_hooks.py
+++ wagtail/wagtailhallo/wagtail_hooks.py
@@ -5,6 +5,16 @@ from wagtail.wagtailadmin.templatetags.wagtailadmin_tags import hook_output
 from wagtail.wagtailcore import hooks
 
 
+@hooks.register('insert_editor_css')
+def insert_editor_css():
+    css_files = [
+    ]
+    css_includes = format_html_join('\n', '<link rel="stylesheet" href="{0}/{1}">',
+        ((settings.STATIC_URL, filename) for filename in css_files),
+        )
+    return css_includes + hook_output('insert_hallo_css')
+
+
 @hooks.register('insert_editor_js')
 def insert_editor_js():
     js_files = [
