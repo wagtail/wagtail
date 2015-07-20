59038ac0074c66017591850b4197dafaa861c335
Karl Hobley <karlhobley10@gmail.com>
2015-07-20 13:07:31 +0100

    Updated chooser views to support multiple page types

--- wagtail/wagtailadmin/views/chooser.py
+++ wagtail/wagtailadmin/views/chooser.py
@@ -12,7 +12,7 @@ from wagtail.wagtailcore.models import Page
 
 def get_querystring(request):
     return urlencode({
-        'page_type': request.GET.get('page_type', ''),
+        'page_types': request.GET.get('page_types', ''),
         'allow_external_link': request.GET.get('allow_external_link', ''),
         'allow_email_link': request.GET.get('allow_email_link', ''),
         'prompt_for_link_text': request.GET.get('prompt_for_link_text', ''),
@@ -22,25 +22,29 @@ def get_querystring(request):
 def browse(request, parent_page_id=None):
     ITEMS_PER_PAGE = 25
 
-    page_type = request.GET.get('page_type') or 'wagtailcore.page'
-    content_type_app_name, content_type_model_name = page_type.split('.')
+    page_types = request.GET.get('page_types', 'wagtailcore.page').split(',')
 
-    try:
-        content_type = ContentType.objects.get_by_natural_key(content_type_app_name, content_type_model_name)
-    except ContentType.DoesNotExist:
-        raise Http404
-    desired_class = content_type.model_class()
+    desired_classes = []
+    for page_type in page_types:
+        content_type_app_name, content_type_model_name = page_type.split('.')
+
+        try:
+            content_type = ContentType.objects.get_by_natural_key(content_type_app_name, content_type_model_name)
+        except ContentType.DoesNotExist:
+            raise Http404
+
+        desired_classes.append(content_type.model_class())
 
     if parent_page_id:
         parent_page = get_object_or_404(Page, id=parent_page_id)
     else:
         parent_page = Page.get_first_root_node()
 
-    parent_page.can_choose = issubclass(parent_page.specific_class, desired_class)
+    parent_page.can_choose = issubclass(parent_page.specific_class, tuple(desired_classes))
     search_form = SearchForm()
     pages = parent_page.get_children()
 
-    if desired_class == Page:
+    if desired_classes == [Page]:
         # apply pagination first, since we know that the page listing won't
         # have to be filtered, and that saves us walking the entire list
         p = request.GET.get('p', 1)
@@ -63,7 +67,7 @@ def browse(request, parent_page_id=None):
 
         shown_pages = []
         for page in pages:
-            page.can_choose = issubclass(page.specific_class or Page, desired_class)
+            page.can_choose = issubclass(page.specific_class or Page, tuple(desired_classes))
             page.can_descend = page.get_children_count()
 
             if page.can_choose or page.can_descend:
@@ -86,29 +90,43 @@ def browse(request, parent_page_id=None):
         'parent_page': parent_page,
         'pages': pages,
         'search_form': search_form,
-        'page_type_string': page_type,
-        'page_type_name': desired_class.get_verbose_name(),
+        'page_type_string': ','.join(page_types),
+        'page_type_names': [desired_class.get_verbose_name() for desired_class in desired_classes],
         'page_types_restricted': (page_type != 'wagtailcore.page')
     })
 
 
 def search(request, parent_page_id=None):
-    page_type = request.GET.get('page_type') or 'wagtailcore.page'
-    content_type_app_name, content_type_model_name = page_type.split('.')
+    page_types = request.GET.get('page_types')
+    content_types = []
 
-    try:
-        content_type = ContentType.objects.get_by_natural_key(content_type_app_name, content_type_model_name)
-    except ContentType.DoesNotExist:
-        raise Http404
-    desired_class = content_type.model_class()
+    # Convert page_types string into list of ContentType objects
+    if page_types:
+        for page_type in page_types.split(','):
+            content_type_app_name, content_type_model_name = page_type.split('.')
+
+            try:
+                content_types.append(ContentType.objects.get_by_natural_key(content_type_app_name, content_type_model_name))
+            except ContentType.DoesNotExist:
+                raise Http404
 
     search_form = SearchForm(request.GET)
     if search_form.is_valid() and search_form.cleaned_data['q']:
-        pages = desired_class.objects.exclude(
+        pages = Page.objects.exclude(
             depth=1  # never include root
-        ).filter(title__icontains=search_form.cleaned_data['q'])[:10]
+        )
+
+        # Restrict content types
+        if content_types:
+            pages = pages.filter(content_type__in=content_types)
+
+        # Do search
+        pages = pages.filter(title__icontains=search_form.cleaned_data['q'])
+
+        # Truncate results
+        pages = pages[:10]
     else:
-        pages = desired_class.objects.none()
+        pages = Page.objects.none()
 
     shown_pages = []
     for page in pages:
