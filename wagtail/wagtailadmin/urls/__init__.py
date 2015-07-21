6a9ee7dfe6d59de67e4fcea007ddfc3ea693d9e1
Matt Westcott <matt@west.co.tt>
2015-07-21 11:59:35 +0100

    Move wagtailadmin /pages URLs into a submodule

--- wagtail/wagtailadmin/urls/__init__.py
+++ wagtail/wagtailadmin/urls/__init__.py
@@ -4,7 +4,8 @@ from django.contrib.auth import views as django_auth_views
 from django.views.decorators.cache import cache_control
 
 from wagtail.wagtailadmin.forms import PasswordResetForm
-from wagtail.wagtailadmin.views import account, chooser, home, pages, tags, userbar, page_privacy
+from wagtail.wagtailadmin.urls import pages as wagtailadmin_pages_urls
+from wagtail.wagtailadmin.views import account, chooser, home, pages, tags, userbar
 from wagtail.wagtailcore import hooks
 from wagtail.utils.urlpatterns import decorate_urlpatterns
 
@@ -20,40 +21,7 @@ urlpatterns = [
     url(r'^pages/$', pages.index, name='wagtailadmin_explore_root'),
     url(r'^pages/(\d+)/$', pages.index, name='wagtailadmin_explore'),
 
-    url(r'^pages/', include([
-        url(r'^add/(\w+)/(\w+)/(\d+)/$', pages.create, name='add'),
-        url(r'^add/(\w+)/(\w+)/(\d+)/preview/$', pages.preview_on_create, name='preview_on_add'),
-        url(r'^usage/(\w+)/(\w+)/$', pages.content_type_use, name='type_use'),
-
-        url(r'^(\d+)/edit/$', pages.edit, name='edit'),
-        url(r'^(\d+)/edit/preview/$', pages.preview_on_edit, name='preview_on_edit'),
-
-        url(r'^preview/$', pages.preview, name='preview'),
-        url(r'^preview_loading/$', pages.preview_loading, name='preview_loading'),
-
-        url(r'^(\d+)/view_draft/$', pages.view_draft, name='view_draft'),
-        url(r'^(\d+)/add_subpage/$', pages.add_subpage, name='add_subpage'),
-        url(r'^(\d+)/delete/$', pages.delete, name='delete'),
-        url(r'^(\d+)/unpublish/$', pages.unpublish, name='unpublish'),
-
-        url(r'^search/$', pages.search, name='search'),
-
-        url(r'^(\d+)/move/$', pages.move_choose_destination, name='move'),
-        url(r'^(\d+)/move/(\d+)/$', pages.move_choose_destination, name='move_choose_destination'),
-        url(r'^(\d+)/move/(\d+)/confirm/$', pages.move_confirm, name='move_confirm'),
-        url(r'^(\d+)/set_position/$', pages.set_page_position, name='set_page_position'),
-
-        url(r'^(\d+)/copy/$', pages.copy, name='copy'),
-
-        url(r'^moderation/(\d+)/approve/$', pages.approve_moderation, name='approve_moderation'),
-        url(r'^moderation/(\d+)/reject/$', pages.reject_moderation, name='reject_moderation'),
-        url(r'^moderation/(\d+)/preview/$', pages.preview_for_moderation, name='preview_for_moderation'),
-
-        url(r'^(\d+)/privacy/$', page_privacy.set_privacy, name='set_privacy'),
-
-        url(r'^(\d+)/lock/$', pages.lock, name='lock'),
-        url(r'^(\d+)/unlock/$', pages.unlock, name='unlock'),
-    ], namespace='wagtailadmin_pages')),
+    url(r'^pages/', include(wagtailadmin_pages_urls, namespace='wagtailadmin_pages')),
 
     # TODO: Move into wagtailadmin_pages namespace
     url(r'^choose-page/$', chooser.browse, name='wagtailadmin_choose_page'),
