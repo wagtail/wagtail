55b16ced56ec2b89dede6335a88832e035383f30
jordij <jordi.joan@gmail.com>
2015-07-27 12:38:28 +1200

    Embed serach and upload modal working.

--- wagtail/wagtailembeds/models.py
+++ wagtail/wagtailembeds/models.py
@@ -1,8 +1,13 @@
 from __future__ import unicode_literals
 
+from taggit.managers import TaggableManager
+
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
+
+from wagtail.wagtailsearch import index
+from wagtail.wagtailadmin.taggable import TagSearchable
 
 
 EMBED_TYPES = (
@@ -14,7 +19,7 @@ EMBED_TYPES = (
 
 
 @python_2_unicode_compatible
-class Embed(models.Model):
+class Embed(models.Model, TagSearchable):
     url = models.URLField()
     max_width = models.SmallIntegerField(null=True, blank=True)
     type = models.CharField(max_length=10, choices=EMBED_TYPES)
@@ -26,6 +31,14 @@ class Embed(models.Model):
     width = models.IntegerField(null=True, blank=True)
     height = models.IntegerField(null=True, blank=True)
     last_updated = models.DateTimeField(auto_now=True)
+    tags = TaggableManager(help_text=None, blank=True, verbose_name=_('Tags'))
+
+    search_fields = TagSearchable.search_fields + (
+        index.SearchField('author_name', partial_match=True, boost=10),
+        index.SearchField('provider_name', partial_match=False, boost=10),
+        index.FilterField('last_updated'),
+        index.FilterField('type'),
+    )
 
     class Meta:
         unique_together = ('url', 'max_width')
--- wagtail/wagtailembeds/templates/wagtailembeds/chooser/chooser.js
+++ wagtail/wagtailembeds/templates/wagtailembeds/chooser/chooser.js
@@ -1,4 +1,52 @@
 function(modal) {
+
+    var searchUrl = $('form.embed-search', modal.body).attr('action');
+
+    function ajaxifyLinks (context) {
+        $('.listing a', context).click(function() {
+            modal.loadUrl(this.href);
+            return false;
+        });
+
+        $('.pagination a', context).click(function() {
+            var page = this.getAttribute("data-page");
+            setPage(page);
+            return false;
+        });
+    }
+
+    function search() {
+        $.ajax({
+            url: searchUrl,
+            data: {q: $('#id_q').val()},
+            success: function(data, status) {
+                $('#embed-results').html(data);
+                ajaxifyLinks($('#embed-results'));
+            }
+        });
+        return false;
+    }
+
+    function setPage(page) {
+        if($('#id_q').val().length){
+            dataObj = {q: $('#id_q').val(), p: page};
+        }else{
+            dataObj = {p: page};
+        }
+
+        $.ajax({
+            url: searchUrl,
+            data: dataObj,
+            success: function(data, status) {
+                $('#embed-results').html(data);
+                ajaxifyLinks($('#embed-results'));
+            }
+        });
+        return false;
+    }
+
+    ajaxifyLinks(modal.body);
+
     $('form.embed-form', modal.body).submit(function() {
         var formdata = new FormData(this);
 
@@ -16,9 +64,24 @@ function(modal) {
 
         return false;
     });
-    $('ul.embeds a.embed-choice').click(function(event){
-        event.preventDefault();
-        modal.loadUrl(this.href);
+
+    $('form.embed-search', modal.body).submit(search);
+
+    $('#id_q').on('input', function() {
+        clearTimeout($.data(this, 'timer'));
+        var wait = setTimeout(search, 200);
+        $(this).data('timer', wait);
+    });
+    $('a.suggested-tag').click(function() {
+        $('#id_q').val($(this).text());
+        search();
         return false;
+    });
+
+    {% url 'wagtailembed_tag_autocomplete' as autocomplete_url %}
+
+    /* Add tag entry interface (with autocompletion) to the tag field of the embed upload form */
+    $('#id_tags', modal.body).tagit({
+        autocomplete: {source: "{{ autocomplete_url|addslashes }}"}
     });
 }
