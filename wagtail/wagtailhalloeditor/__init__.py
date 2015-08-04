2c06d143083f118c160f569d6f8663a83f72363c
Richard Mitchell <richard.j.mitchell@gmail.com>
2015-08-04 16:41:12 +0100

    Add app config like other wagtail apps.

---
+++ wagtail/wagtailhalloeditor/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'wagtail.wagtailhalloeditor.apps.WagtailHalloEditorAppConfig'
---
+++ wagtail/wagtailhalloeditor/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WagtailHalloEditorAppConfig(AppConfig):
+    name = 'wagtail.wagtailhalloeditor'
+    label = 'wagtailhalloeditor'
+    verbose_name = "Wagtail Hallo editor"
