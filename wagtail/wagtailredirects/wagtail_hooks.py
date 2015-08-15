63511a7ee6c9a3d171233fa35dd47f5c691b352c
Matt Westcott <matt@west.co.tt>
2015-08-15 21:21:51 +0200

    Enforce redirect permissions correctly, and register them with the group permissions UI

--- wagtail/wagtailredirects/wagtail_hooks.py
+++ wagtail/wagtailredirects/wagtail_hooks.py
@@ -1,6 +1,7 @@
 from django.core import urlresolvers
 from django.conf.urls import include, url
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import Permission
 
 from wagtail.wagtailcore import hooks
 from wagtail.wagtailredirects import urls
@@ -17,9 +18,19 @@ def register_admin_urls():
 
 class RedirectsMenuItem(MenuItem):
     def is_shown(self, request):
-        # TEMPORARY: Only show if the user is a superuser
-        return request.user.is_superuser
+        return (
+            request.user.has_perm('wagtailredirects.add_redirect')
+            or request.user.has_perm('wagtailredirects.change_redirect')
+            or request.user.has_perm('wagtailredirects.delete_redirect')
+        )
+
 
 @hooks.register('register_settings_menu_item')
 def register_redirects_menu_item():
     return RedirectsMenuItem(_('Redirects'), urlresolvers.reverse('wagtailredirects:index'), classnames='icon icon-redirect', order=800)
+
+
+@hooks.register('register_permissions')
+def register_permissions():
+    return Permission.objects.filter(content_type__app_label='wagtailredirects',
+        codename__in=['add_redirect', 'change_redirect', 'delete_redirect'])
