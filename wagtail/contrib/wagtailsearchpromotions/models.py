e59d146c9cd258164c01f42a2f863b229ec72fdc
Karl Hobley <karlhobley10@gmail.com>
2015-08-20 11:55:39 +0100

    Removed import
    This prevents a circular import

--- wagtail/contrib/wagtailsearchpromotions/models.py
+++ wagtail/contrib/wagtailsearchpromotions/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from wagtail.wagtailsearch.models import Query
-
 
 class SearchPromotion(models.Model):
-    query = models.ForeignKey(Query, db_index=True, related_name='editors_picks')
+    query = models.ForeignKey('wagtailsearch.Query', db_index=True, related_name='editors_picks')
     page = models.ForeignKey('wagtailcore.Page', verbose_name=_('Page'))
     sort_order = models.IntegerField(null=True, blank=True, editable=False)
     description = models.TextField(verbose_name=_('Description'), blank=True)
