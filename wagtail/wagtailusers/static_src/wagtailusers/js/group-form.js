755f9916278547fcd8915e5999984b79db140cc6
Matt Westcott <matt@west.co.tt>
2015-09-09 15:00:05 +0100

    fix jscs warning

--- wagtail/wagtailusers/static_src/wagtailusers/js/group-form.js
+++ wagtail/wagtailusers/static_src/wagtailusers/js/group-form.js
@@ -1,6 +1,6 @@
 $(function() {
     buildExpandingFormset('id_page_permissions', {
-        'onInit': function(index) {
+        onInit: function(index) {
             var deleteInputId = 'id_page_permissions-' + index + '-DELETE';
             var childId = 'inline_child_page_permissions-' + index;
             $('#' + deleteInputId + '-button').click(function() {
