02e4288b15f059881aeea1cbd942670f34f79e8b
Tim Heap <tim@timheap.me>
2015-10-01 14:59:13 +1000

    Add `wagtailuserbar()` Jinja function

---
+++ wagtail/wagtailadmin/templatetags/jinja2.py
@@ -0,0 +1,17 @@
+import jinja2
+from jinja2.ext import Extension
+
+from .wagtailuserbar import wagtailuserbar
+
+
+class WagtailUserbarExtension(Extension):
+    def __init__(self, environment):
+        super(WagtailUserbarExtension, self).__init__(environment)
+
+        self.environment.globals.update({
+            'wagtailuserbar': jinja2.contextfunction(wagtailuserbar),
+        })
+
+
+# Nicer import names
+userbar = WagtailUserbarExtension
