0c6b4e568e6a96d09ea94c8e9c6b5c0627d927f5
Tim Heap <tim@timheap.me>
2015-10-06 11:25:16 +1100

    Add test helpers for Wagtail sites
    `wagtail.tests.utils.WagtailPageTests` is a new `TestCase` subclass that
    helps developers write tests for their Wagtail sites. It currently
    includes three assert methods:
    
    `assertCanCreateAt(parent_model, child_model)`, which asserts that a
    child page of a certain type could be created underneath a parent page.
    This is useful for making assertions around the business rules of your
    site.
    
    `assertCanNotCreateAt(parent_model, child_model)` is the inverse of the
    above.
    
    `assertCanCreate(parent, child_model, data)` asserts that a child page
    can be added underneath the given parent page, by POSTing `data` at the
    correct page in the Wagtail admin. This checks that the developer has
    correctly configured their `content_panels` and related options.
    
    These methods are just a start, and could be expanded further. More
    methods could be added, asserting that Snippets can be created, for
    example. The current methods could be extended further, to validate more
    about the Wagtail admin page editor.

--- wagtail/tests/utils.py
+++ wagtail/tests/utils.py
@@ -1,9 +1,17 @@
-from contextlib import contextmanager
-import warnings
+from __future__ import absolute_import, unicode_literals
+
 import sys
+import warnings
+from contextlib import contextmanager
 
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.core.urlresolvers import reverse
+from django.test import TestCase
 from django.utils import six
+from django.utils.text import slugify
+
+from wagtail.wagtailcore.models import Page
 
 
 class WagtailTestUtils(object):
@@ -51,3 +59,85 @@ class WagtailTestUtils(object):
         for mod in list(sys.modules.values()):
             if hasattr(mod, key):
                 getattr(mod, key).clear()
+
+
+class WagtailPageTests(WagtailTestUtils, TestCase):
+    """
+    A set of asserts to help write tests for your own Wagtail site.
+    """
+    def setUp(self):
+        super(WagtailPageTests, self).setUp()
+        self.login()
+
+    def _testCanCreateAt(self, parent_model, child_model):
+        child_ct = ContentType.objects.get_for_model(child_model)
+        parent_ct = ContentType.objects.get_for_model(parent_model)
+
+        return all([
+            child_ct in parent_model.allowed_subpage_types(),
+            # Anything can be created under a Page
+            parent_model is Page or parent_ct in child_model.allowed_parent_page_types()])
+
+    def assertCanCreateAt(self, parent_model, child_model, msg=None):
+        """
+        Assert a particular child Page type can be created under a parent
+        Page type.
+        """
+        if not self._testCanCreateAt(parent_model, child_model):
+            msg = self._formatMessage(msg, "Can not create a %s.%s under a %s.%s" % (
+                child_model._meta.app_label, child_model._meta.model_name,
+                parent_model._meta.app_label, parent_model._meta.model_name))
+            raise self.failureException(msg)
+
+    def assertCanNotCreateAt(self, parent_model, child_model, msg=None):
+        """
+        Assert a particular child Page type can not be created under a parent
+        Page type.
+        """
+        if self._testCanCreateAt(parent_model, child_model):
+            msg = self._formatMessage(msg, "Can create a %s.%s under a %s.%s" % (
+                child_model._meta.app_label, child_model._meta.model_name,
+                parent_model._meta.app_label, parent_model._meta.model_name))
+            raise self.failureException(msg)
+
+    def assertCanCreate(self, parent, child_model, data, msg=None):
+        """
+        Assert that a child of the given Page type can be created under the
+        parent, using the supplied POST data
+        """
+        self.assertCanCreateAt(parent.specific_class, child_model)
+
+        if 'slug' not in data and 'title' in data:
+            data['slug'] = slugify(data['title'])
+        data['action-publish'] = 'action-publish'
+
+        add_url = reverse('wagtailadmin_pages:add', args=[
+            child_model._meta.app_label, child_model._meta.model_name, parent.pk])
+        response = self.client.post(add_url, data, follow=True)
+
+        if response.status_code != 200:
+            msg = self._formatMessage(msg, 'Creating a %s.%s returned a %d' % (
+                child_model._meta.app_label, child_model._meta.model_name, response.status_code))
+            raise self.failureException(msg)
+
+        if response.redirect_chain == []:
+            if 'form' not in response.context:
+                msg = self._formatMessage(msg, 'Creating a page failed unusually')
+                raise self.failureException(msg)
+            form = response.context['form']
+            if not form.errors:
+                msg = self._formatMessage(msg, 'Creating a page failed for an unknown reason')
+                raise self.failureException(msg)
+
+            errors = '\n'.join('  %s:\n    %s' % (field, '\n    '.join(errors))
+                               for field, errors in sorted(form.errors.items()))
+            msg = self._formatMessage(msg, 'Validation errors found when creating a %s.%s:\n%s' % (
+                child_model._meta.app_label, child_model._meta.model_name, errors))
+            raise self.failureException(msg)
+
+        explore_url = 'http://testserver' + reverse('wagtailadmin_explore', args=[parent.pk])
+        if response.redirect_chain != [(explore_url, 302)]:
+            msg = self._formatMessage(msg, 'Creating a page %s.%s didnt redirect the user to the explorer, but to %s' % (
+                child_model._meta.app_label, child_model._meta.model_name,
+                response.redirect_chain))
+            raise self.failureException(msg)
