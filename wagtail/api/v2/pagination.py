ff15b2f6e3a4768a1c3a43c990196e66a3823246
Karl Hobley <karlhobley10@gmail.com>
2015-10-08 20:36:21 +0100

    feat(api/2): Added new module for WagtailAPI v2

---
+++ wagtail/api/v2/pagination.py
@@ -0,0 +1,45 @@
+from collections import OrderedDict
+
+from django.conf import settings
+
+from rest_framework.pagination import BasePagination
+from rest_framework.response import Response
+
+from .utils import BadRequestError
+
+
+class WagtailPagination(BasePagination):
+    def paginate_queryset(self, queryset, request, view=None):
+        limit_max = getattr(settings, 'WAGTAILAPI_LIMIT_MAX', 20)
+
+        try:
+            offset = int(request.GET.get('offset', 0))
+            assert offset >= 0
+        except (ValueError, AssertionError):
+            raise BadRequestError("offset must be a positive integer")
+
+        try:
+            limit = int(request.GET.get('limit', min(20, limit_max)))
+
+            if limit > limit_max:
+                raise BadRequestError("limit cannot be higher than %d" % limit_max)
+
+            assert limit >= 0
+        except (ValueError, AssertionError):
+            raise BadRequestError("limit must be a positive integer")
+
+        start = offset
+        stop = offset + limit
+
+        self.view = view
+        self.total_count = queryset.count()
+        return queryset[start:stop]
+
+    def get_paginated_response(self, data):
+        data = OrderedDict([
+            ('meta', OrderedDict([
+                ('total_count', self.total_count),
+            ])),
+            (self.view.name, data),
+        ])
+        return Response(data)
---
+++ wagtail/api/v2/signal_handlers.py
@@ -0,0 +1,54 @@
+from django.core.urlresolvers import reverse
+from django.db.models.signals import post_save, post_delete
+
+from wagtail.wagtailcore.signals import page_published, page_unpublished
+from wagtail.wagtailcore.models import get_page_models
+from wagtail.wagtailimages.models import get_image_model
+from wagtail.wagtaildocs.models import Document
+
+from wagtail.contrib.wagtailfrontendcache.utils import purge_url_from_cache
+
+from .utils import get_base_url
+
+
+def purge_page_from_cache(instance, **kwargs):
+    base_url = get_base_url()
+    purge_url_from_cache(base_url + reverse('wagtailapi_v2:pages:detail', args=(instance.id, )))
+
+
+def purge_image_from_cache(instance, **kwargs):
+    if not kwargs.get('created', False):
+        base_url = get_base_url()
+        purge_url_from_cache(base_url + reverse('wagtailapi_v2:images:detail', args=(instance.id, )))
+
+
+def purge_document_from_cache(instance, **kwargs):
+    if not kwargs.get('created', False):
+        base_url = get_base_url()
+        purge_url_from_cache(base_url + reverse('wagtailapi_v2:documents:detail', args=(instance.id, )))
+
+
+def register_signal_handlers():
+    Image = get_image_model()
+
+    for model in get_page_models():
+        page_published.connect(purge_page_from_cache, sender=model)
+        page_unpublished.connect(purge_page_from_cache, sender=model)
+
+    post_save.connect(purge_image_from_cache, sender=Image)
+    post_delete.connect(purge_image_from_cache, sender=Image)
+    post_save.connect(purge_document_from_cache, sender=Document)
+    post_delete.connect(purge_document_from_cache, sender=Document)
+
+
+def unregister_signal_handlers():
+    Image = get_image_model()
+
+    for model in get_page_models():
+        page_published.disconnect(purge_page_from_cache, sender=model)
+        page_unpublished.disconnect(purge_page_from_cache, sender=model)
+
+    post_save.disconnect(purge_image_from_cache, sender=Image)
+    post_delete.disconnect(purge_image_from_cache, sender=Image)
+    post_save.disconnect(purge_document_from_cache, sender=Document)
+    post_delete.disconnect(purge_document_from_cache, sender=Document)
