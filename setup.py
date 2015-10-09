bce8bf1a5cd21c57b0cc86a5ffdd02934f29a53d
Matt Westcott <matt@west.co.tt>
2015-10-09 15:47:49 +0100

    Bump django-modelcluster version to 1.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ except ImportError:
 install_requires = [
     "Django>=1.7.1,<1.9",
     "django-compressor>=1.4",
-    "django-modelcluster>=0.6",
+    "django-modelcluster>=1.0",
     "django-taggit>=0.13.0",
     "django-treebeard==3.0",
     "djangorestframework>=3.1.3",
