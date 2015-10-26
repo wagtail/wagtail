1d1563ccb9664055f1eaf0edcfedca14569710b7
Andy Babic <ababic@rkh.co.uk>
2015-10-26 16:57:52 +0000

    Major changes to ModelAdminMiddleware. - Removed all references to snippet-related urls, as no longer useful (wagtailmodeladmin's own views are used for managing snippets and non-page models) - Evaluate namespace and url_name on the referer url to match Wagtail 1.1 namespace and names for page-related urls

--- wagtail/contrib/wagtailmodeladmin/middleware.py
+++ wagtail/contrib/wagtailmodeladmin/middleware.py
@@ -5,43 +5,35 @@ from django.core.urlresolvers import resolve, Resolver404
 
 class ModelAdminMiddleware(object):
     """
-    Whenever loading wagtail's wagtailadmin_explore or wagtailsnippets_list
-    urls, we check the session for a 'return_to_list_url' value (set by an
-    AppModelAdmin, PageModelAdmin or SnippetModelAdmin instance via the
-    'construct_main_menu' webhook)
-
-    If it looks like we should be returned to a listing page that isn't the
-    wagtailadmin_explore or wagtailsnippets_list, we hijack the request and
-    redirect to our listing page.
+    Whenever loading wagtail's wagtailadmin_explore url, we check the session
+    for a 'return_to_list_url' value (set by some views), to see if the user
+    should be redirected to a custom list view instead, and if so, redirect
+    them to it.
     """
 
     def process_request(self, request):
         referer_url = request.META.get('HTTP_REFERER')
         return_to_index_url = request.session.get('return_to_index_url')
 
-        """
-        There's no point doing anything unless we have a referer_url,
-        and return_to_index_url has been set
-        """
-        if referer_url and return_to_index_url:
+        try:
+            resolver_match = resolve(request.path)
 
-            try:
-                resolver_match = resolve(request.path)
-                if resolver_match.url_name in ['wagtailadmin_explore',
-                                               'wagtailsnippets_list']:
-                    referer_match = resolve(urlparse(referer_url).path)
-                    if referer_match.url_name in (
-                        'wagtailadmin_pages_create',
-                        'wagtailadmin_pages_edit',
-                        'wagtailadmin_pages_delete',
-                        'wagtailadmin_pages_unpublish',
-                        'wagtailsnippets_create',
-                        'wagtailsnippets_edit',
-                        'wagtailsnippets_delete',
-                    ):
-                        del request.session['return_to_index_url']
-                        return HttpResponseRedirect(return_to_index_url)
-            except Resolver404:
-                pass
+            if all((
+                return_to_index_url,
+                referer_url,
+                resolver_match.url_name == 'wagtailadmin_explore',
+            )):
+                referer_match = resolve(urlparse(referer_url).path)
+                referer_url_name = referer_match.url_name
+                referer_ns = referer_match.namespace
+
+                if referer_ns == 'wagtailadmin_pages' and referer_url_name in (
+                    'add', 'edit', 'delete', 'unpublish', 'copy'
+                ):
+                    del request.session['return_to_index_url']
+                    return HttpResponseRedirect(return_to_index_url)
+
+        except Resolver404:
+            pass
 
         return None
