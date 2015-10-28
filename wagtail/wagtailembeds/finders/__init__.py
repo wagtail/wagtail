12b8c60a8fa45a0e3fa93eacadbafc437f220510
Karl Hobley <karlhobley10@gmail.com>
2015-10-28 16:14:38 +0000

    Remap old embed paths to new ones
    For backwards compatibility

--- wagtail/wagtailembeds/finders/__init__.py
+++ wagtail/wagtailembeds/finders/__init__.py
@@ -5,10 +5,21 @@ from wagtail.wagtailembeds.finders.oembed import oembed
 from wagtail.wagtailembeds.finders.embedly import embedly
 
 
+MOVED_FINDERS = {
+    'wagtail.wagtailembeds.embeds.embedly': 'wagtail.wagtailembeds.finders.embedly.embedly',
+    'wagtail.wagtailembeds.embeds.oembed': 'wagtail.wagtailembeds.finders.oembed.oembed',
+}
+
+
 def get_default_finder():
     # Check if the user has set the embed finder manually
     if hasattr(settings, 'WAGTAILEMBEDS_EMBED_FINDER'):
-        return import_string(settings.WAGTAILEMBEDS_EMBED_FINDER)
+        finder_name = settings.WAGTAILEMBEDS_EMBED_FINDER
+
+        if finder_name in MOVED_FINDERS:
+            finder_name = MOVED_FINDERS[finder_name]
+
+        return import_string(finder_name)
 
     # Use embedly if the embedly key is set
     if hasattr(settings, 'WAGTAILEMBEDS_EMBEDLY_KEY'):
