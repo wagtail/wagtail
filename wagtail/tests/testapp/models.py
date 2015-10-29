bd6a7c768f9ce030e4b502c8ceaace2f51077cf6
Matt Westcott <matt@west.co.tt>
2015-10-29 17:16:07 +0000

    Use the live database record as the result of get_latest_revision_as_page if there are no draft edits.
    This ensures that the live database state will be reflected in the page editor, which provides two benefits:
    * any changes made directly at the model / database level (e.g. automated data imports) will be visible in the editor
    * inline child objects will be associated with their actual database IDs even if this information is missing from the revision record. This ensures that their IDs will be preserved on next publish, rather than the records being deleted and recreated (#1853)

--- wagtail/tests/testapp/models.py
+++ wagtail/tests/testapp/models.py
@@ -118,6 +118,11 @@ class RelatedLink(LinkFields):
 class SimplePage(Page):
     content = models.TextField()
 
+    content_panels = [
+        FieldPanel('title', classname="full title"),
+        FieldPanel('content'),
+    ]
+
 
 class PageWithOldStyleRouteMethod(Page):
     """
