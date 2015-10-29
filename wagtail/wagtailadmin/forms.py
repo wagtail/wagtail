3370d9ac69dfb3984cdc1a13fe9d68a3aca7d87f
Tim Heap <tim@timheap.me>
2015-10-29 15:51:30 +1100

    Move page validation to WagtailAdminPageForm class
    Page validation is now done through the WagtailAdminPageForm class,
    instead of through a separate function.

--- wagtail/wagtailadmin/forms.py
+++ wagtail/wagtailadmin/forms.py
@@ -1,11 +1,21 @@
+from __future__ import unicode_literals
+
+import copy
+
+import django
 from django import forms
-from django.core import validators
-from django.forms.widgets import TextInput
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm
+from django.core import validators
+from django.db import models
+from django.forms.widgets import TextInput
+from django.utils import timezone
 from django.utils.translation import ugettext as _
-from django.utils.translation import ungettext, ugettext_lazy
-from wagtail.wagtailadmin.widgets import AdminPageChooser
+from django.utils.translation import ugettext_lazy, ungettext
+from modelcluster.forms import ClusterForm, ClusterFormMetaclass
+from taggit.managers import TaggableManager
+
+from wagtail.wagtailadmin import widgets
 from wagtail.wagtailcore.models import Page
 
 
@@ -120,7 +130,7 @@ class CopyForm(forms.Form):
         self.fields['new_parent_page'] = forms.ModelChoiceField(
             initial=self.page.get_parent(),
             queryset=Page.objects.all(),
-            widget=AdminPageChooser(can_choose_root=True),
+            widget=widgets.AdminPageChooser(can_choose_root=True),
             label=_("New parent page"),
             help_text=_("This copy will be a child of this given parent page.")
         )
@@ -186,5 +196,118 @@ class PageViewRestrictionForm(forms.Form):
         if cleaned_data.get('restriction_type') == 'password' and not cleaned_data.get('password'):
             self._errors["password"] = self.error_class([_('This field is required.')])
             del cleaned_data['password']
+
+        return cleaned_data
+
+
+# Form field properties to override whenever we encounter a model field
+# that matches one of these types - including subclasses
+FORM_FIELD_OVERRIDES = {
+    models.DateField: {'widget': widgets.AdminDateInput},
+    models.TimeField: {'widget': widgets.AdminTimeInput},
+    models.DateTimeField: {'widget': widgets.AdminDateTimeInput},
+    TaggableManager: {'widget': widgets.AdminTagWidget},
+}
+
+# Form field properties to override whenever we encounter a model field
+# that matches one of these types exactly, ignoring subclasses.
+# (This allows us to override the widget for models.TextField, but leave
+# the RichTextField widget alone)
+DIRECT_FORM_FIELD_OVERRIDES = {
+    models.TextField: {'widget': widgets.AdminAutoHeightTextInput},
+}
+
+
+# Callback to allow us to override the default form fields provided for each model field.
+def formfield_for_dbfield(db_field, **kwargs):
+    # adapted from django/contrib/admin/options.py
+
+    overrides = None
+
+    # If we've got overrides for the formfield defined, use 'em. **kwargs
+    # passed to formfield_for_dbfield override the defaults.
+    if db_field.__class__ in DIRECT_FORM_FIELD_OVERRIDES:
+        overrides = DIRECT_FORM_FIELD_OVERRIDES[db_field.__class__]
+    else:
+        for klass in db_field.__class__.mro():
+            if klass in FORM_FIELD_OVERRIDES:
+                overrides = FORM_FIELD_OVERRIDES[klass]
+                break
+
+    if overrides:
+        kwargs = dict(copy.deepcopy(overrides), **kwargs)
+
+    return db_field.formfield(**kwargs)
+
+
+class WagtailAdminModelFormMetaclass(ClusterFormMetaclass):
+    # Override the behaviour of the regular ModelForm metaclass -
+    # which handles the translation of model fields to form fields -
+    # to use our own formfield_for_dbfield function to do that translation.
+    # This is done by sneaking a formfield_callback property into the class
+    # being defined (unless the class already provides a formfield_callback
+    # of its own).
+
+    # while we're at it, we'll also set extra_form_count to 0, as we're creating
+    # extra forms in JS
+    extra_form_count = 0
+
+    def __new__(cls, name, bases, attrs):
+        if 'formfield_callback' not in attrs or attrs['formfield_callback'] is None:
+            attrs['formfield_callback'] = formfield_for_dbfield
+
+        new_class = super(WagtailAdminModelFormMetaclass, cls).__new__(cls, name, bases, attrs)
+        return new_class
+
+WagtailAdminModelForm = WagtailAdminModelFormMetaclass(str('WagtailAdminModelForm'), (ClusterForm,), {})
+
+# Now, any model forms built off WagtailAdminModelForm instead of ModelForm should pick up
+# the nice form fields defined in FORM_FIELD_OVERRIDES.
+
+
+class WagtailAdminPageForm(WagtailAdminModelForm):
+
+    class Meta:
+        # (dealing with Treebeard's tree-related fields that really should have
+        # been editable=False)
+        exclude = ['content_type', 'path', 'depth', 'numchild']
+
+    def __init__(self, data=None, files=None, parent_page=None, *args, **kwargs):
+        super(WagtailAdminPageForm, self).__init__(data, files, *args, **kwargs)
+        self.parent_page = parent_page
+
+    if django.VERSION < (1, 9):
+        def clean_title(self):
+            return self.cleaned_data['title'].strip()
+
+        def clean_seo_title(self):
+            return self.cleaned_data['seo_title'].strip()
+
+    def clean(self):
+        cleaned_data = super(WagtailAdminPageForm, self).clean()
+        if 'slug' in self.cleaned_data:
+            # Get siblings for the page
+            siblings = self.parent_page.get_children()
+            if self.instance and self.instance.id:
+                siblings = siblings.exclude(id=self.instance.id)
+
+            # Make sure the slug isn't being used by a sibling
+            if siblings.filter(slug=cleaned_data['slug']).exists():
+                self.add_error('slug', forms.ValidationError(_("This slug is already in use")))
+
+        # Check scheduled publishing fields
+        go_live_at = cleaned_data.get('go_live_at')
+        expire_at = cleaned_data.get('expire_at')
+
+        # Go live must be before expire
+        if go_live_at and expire_at:
+            if go_live_at > expire_at:
+                msg = _('Go live date/time must be before expiry date/time')
+                self.add_error('go_live_at', forms.ValidationError(msg))
+                self.add_error('expire_at', forms.ValidationError(msg))
+
+        # Expire at must be in the future
+        if expire_at and expire_at < timezone.now():
+            self.add_error('expire_at', forms.ValidationError(_('Expiry date/time must be in the future')))
 
         return cleaned_data
