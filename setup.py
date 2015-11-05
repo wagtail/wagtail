5f7592f4a82076102ac795d9b762c65965ff59fb
Karl Hobley <karlhobley10@gmail.com>
2015-11-05 16:19:52 +0000

    Advertise Python 3.5 support

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Framework :: Django',
         'Topic :: Internet :: WWW/HTTP :: Site Management',
     ],
