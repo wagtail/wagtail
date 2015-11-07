3ee9c76f1592189213332caa2469cc6dbd592060
Karl Hobley <karlhobley10@gmail.com>
2015-11-07 17:48:36 +0000

    Moved generate/verify signature into ServeView

--- wagtail/wagtailimages/tests/urls.py
+++ wagtail/wagtailimages/tests/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^actions/serve/(.*)/(\d*)/(.*)/[^/]*', ServeView.as_view(action='serve'), name='wagtailimages_serve_action_serve'),
     url(r'^actions/redirect/(.*)/(\d*)/(.*)/[^/]*', ServeView.as_view(action='redirect'), name='wagtailimages_serve_action_redirect'),
     url(r'^actions/unknown/(.*)/(\d*)/(.*)/[^/]*', ServeView.as_view(action='unknown'), name='wagtailimages_serve_action_unknown'),
+    url(r'^custom_key/(.*)/(\d*)/(.*)/[^/]*', ServeView.as_view(key='custom'), name='wagtailimages_serve_custom_key'),
 ]
--- wagtail/wagtailimages/views/serve.py
+++ wagtail/wagtailimages/views/serve.py
@@ -1,26 +1,56 @@
 from __future__ import absolute_import, unicode_literals
 
+import base64
+import hashlib
+import hmac
 import imghdr
 from wsgiref.util import FileWrapper
 
+from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured, PermissionDenied
 from django.http import HttpResponse, HttpResponsePermanentRedirect, StreamingHttpResponse
 from django.shortcuts import get_object_or_404
+from django.utils.six import text_type
 from django.views.generic import View
 
 from wagtail.wagtailimages.exceptions import InvalidFilterSpecError
 from wagtail.wagtailimages.models import SourceImageIOError, get_image_model
-from wagtail.wagtailimages.utils import verify_signature
 
 
 class ServeView(View):
     model = get_image_model()
     action = 'serve'
+    key = None
+
+    def get_key(self):
+        if self.key is not None:
+            return self.key
+        else:
+            return settings.SECRET_KEY
+
+    def generate_signature(self, image_id, filter_spec):
+        key = self.get_key()
+
+        # Key must be a bytes object
+        if isinstance(key, text_type):
+            key = key.encode()
+
+        # Get primary key of image instance
+        if isinstance(image_id, self.model):
+            image_id = image_id.id
+
+        # Based on libthumbor hmac generation
+        # https://github.com/thumbor/libthumbor/blob/b19dc58cf84787e08c8e397ab322e86268bb4345/libthumbor/crypto.py#L50
+        url = str(image_id) + '/' + str(filter_spec) + '/'
+        return base64.urlsafe_b64encode(hmac.new(key, url.encode(), hashlib.sha1).digest())
+
+    def verify_signature(self, signature, image_id, filter_spec):
+        return signature == self.generate_signature(image_id, filter_spec)
 
     def get(self, request, signature, image_id, filter_spec):
         image = get_object_or_404(self.model, id=image_id)
 
-        if not verify_signature(signature.encode(), image_id, filter_spec):
+        if not self.verify_signature(signature.encode(), image_id, filter_spec):
             raise PermissionDenied
 
         try:
