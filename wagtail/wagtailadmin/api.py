4a78d17ab0bdfb9ed0ac248ef39eb486c5af9a3b
Karl Hobley <karlhobley10@gmail.com>
2015-12-01 14:32:45 +0000

    feat(admin/api): Show all pages in admin API (except root)

--- wagtail/wagtailadmin/api.py
+++ wagtail/wagtailadmin/api.py
@@ -1,9 +1,36 @@
 from wagtail.api.shared.router import WagtailAPIRouter
+from wagtail.api.shared.utils import BadRequestError, page_models_from_string, filter_page_type
 from wagtail.api.v2.endpoints import PagesAPIEndpoint, ImagesAPIEndpoint, DocumentsAPIEndpoint
+
+from wagtail.wagtailcore.models import Page
 
 
 class PagesAdminAPIEndpoint(PagesAPIEndpoint):
-    pass
+    def get_queryset(self):
+        request = self.request
+
+        # Allow pages to be filtered to a specific type
+        try:
+            models = page_models_from_string(request.GET.get('type', 'wagtailcore.Page'))
+        except (LookupError, ValueError):
+            raise BadRequestError("type doesn't exist")
+
+        if not models:
+            models = [Page]
+
+        if len(models) == 1:
+            queryset = models[0].objects.all()
+        else:
+            queryset = Page.objects.all()
+
+            # Filter pages by specified models
+            queryset = filter_page_type(queryset, models)
+
+        # Hide root page
+        # TODO: Add "include_root" flag
+        queryset = queryset.exclude(depth=1)
+
+        return queryset
 
 
 class ImagesAdminAPIEndpoint(ImagesAPIEndpoint):
