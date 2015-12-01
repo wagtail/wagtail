518a600d96a34410128921308e3e03569c0f5ce8
Karl Hobley <karlhobley10@gmail.com>
2015-12-01 15:43:32 +0000

    feat(admin/api): Added thumbnail to images

--- wagtail/wagtailadmin/api/serializers.py
+++ wagtail/wagtailadmin/api/serializers.py
@@ -1,4 +1,11 @@
-from wagtail.api.v2.serializers import PageMetaField, PageSerializer
+from collections import OrderedDict
+
+from wagtail.api.v2.serializers import (
+    PageMetaField, PageSerializer,
+    MetaField, ImageSerializer,
+)
+
+from wagtail.wagtailimages.models import SourceImageIOError
 
 
 class AdminPageMetaField(PageMetaField):
@@ -24,3 +31,54 @@ class AdminPageMetaField(PageMetaField):
 
 class AdminPageSerializer(PageSerializer):
     meta = AdminPageMetaField()
+
+
+class AdminImageMetaField(MetaField):
+    """
+    A subclass of ImageMetaField for the admin API.
+
+    This adds the "thumbnail" field contain a URL/width/height of a max-165x165
+    rendition of the image.
+
+    Example:
+    "meta": {
+        ...
+
+        "thumbnail": {
+            "url": "/media/images/myimage.max-165x165.jpg",
+            "width": 165,
+            "height": 100
+        }
+    }
+
+    If there is an error with the source image. The "thumbnail" field will be
+    set to null and a new field "source_image_error" will be added and set to
+    true.
+
+    "meta": {
+        ...
+
+        "thumbnail": null,
+        "source_image_error": true
+    }
+    """
+    def to_representation(self, image):
+        data = super(AdminImageMetaField, self).to_representation(image)
+
+        try:
+            thumbnail = image.get_rendition('max-165x165')
+
+            data['thumbnail'] = OrderedDict([
+                ('url', thumbnail.url),
+                ('width', thumbnail.width),
+                ('height', thumbnail.height),
+            ])
+        except SourceImageIOError:
+            data['thumbnail'] = None
+            data['source_image_error'] = True
+
+        return data
+
+
+class AdminImageSerializer(ImageSerializer):
+    meta = AdminImageMetaField()
