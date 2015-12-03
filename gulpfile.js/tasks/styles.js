2df578af25a02e9d7f70cd297512bf72cf6802f7
Dave Cranwell <david@torchbox.com>
2015-12-03 17:11:08 +0000

    removed unnecessary whitelist of sass files to compile. sass knows not to compile partials, which leaves all the _other_ files which it should compile

--- gulpfile.js/tasks/styles.js
+++ gulpfile.js/tasks/styles.js
@@ -25,6 +25,8 @@ gulp.task('styles:sass', function () {
     // its own Sass files that need to be compiled.
     var sources = flatten(config.apps.map(function(app) { return app.scssSources(); }));
 
+    console.log(sources);
+
     return gulp.src(sources)
         .pipe(sass({
             errLogToConsole: true,
