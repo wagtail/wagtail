3bcb722a5f6065a93f12e7451c4b0c5a57a8e88c
Karl Hobley <karlhobley10@gmail.com>
2015-12-07 13:34:35 +0000

    refactor(search/elastic): Allow rebuilder classes to be swapped

--- wagtail/wagtailsearch/backends/elasticsearch.py
+++ wagtail/wagtailsearch/backends/elasticsearch.py
@@ -577,6 +577,8 @@ class ElasticSearch(BaseSearch):
     query_class = ElasticSearchQuery
     results_class = ElasticSearchResults
     mapping_class = ElasticSearchMapping
+    basic_rebuilder_class = ElasticSearchIndexRebuilder
+    atomic_rebuilder_class = ElasticSearchAtomicIndexRebuilder
 
     def __init__(self, params):
         super(ElasticSearch, self).__init__(params)
@@ -587,9 +589,9 @@ class ElasticSearch(BaseSearch):
         self.es_timeout = params.pop('TIMEOUT', 10)
 
         if params.pop('ATOMIC_REBUILD', False):
-            self.rebuilder_class = ElasticSearchAtomicIndexRebuilder
+            self.rebuilder_class = self.atomic_rebuilder_class
         else:
-            self.rebuilder_class = ElasticSearchIndexRebuilder
+            self.rebuilder_class = self.basic_rebuilder_class
 
         # If HOSTS is not set, convert URLS setting to HOSTS
         es_urls = params.pop('URLS', ['http://localhost:9200'])
