7e68f1c5efb9168e9ee8fbc9904334b7199caa66
Dave Cartwright <hit.dave.cartwright@gmail.com>
2016-01-31 22:43:40 +1300

    added unit tests for render_js_init get_panel on widget BaseTextAreaWidget and RichTextArea

--- wagtail/wagtailcore/tests/test_widgets.py
+++ wagtail/wagtailcore/tests/test_widgets.py
@@ -1,12 +1,43 @@
 from django.test import TestCase
-from wagtail.wagtailcore.fields import BaseTextAreaWidget
+from wagtail.wagtailcore.fields import BaseTextAreaWidget, RichTextArea
 
 
 class TestBaseTextAreaWidget(TestCase):
 
-    def test_is_abstract(self):
+    def test_get_panel_implemented(self):
         with self.assertRaises(NotImplementedError):
-            fail = BaseTextAreaWidget()
-            fail.get_panel()
+            widget = BaseTextAreaWidget()
+            widget.get_panel()
+
+    def test_render_js_init(self):
+        widget = BaseTextAreaWidget()
+        js_init = widget.render_js_init('test-id', 'test', None)
+        self.assertIsInstance(js_init, unicode)
+        self.assertEqual(len(js_init), 0)
+
+
+class TestRichTextAreaWidget(TestCase):
+
+    def test_get_panel_implemented(self):
+        from wagtail.wagtailadmin.edit_handlers import RichTextFieldPanel
+        widget = RichTextArea()
+        panel_proxy = widget.get_panel()
+        panel = panel_proxy(None)
+        self.assertIsInstance(panel, RichTextFieldPanel)
+
+    def test_default_render_js_init(self):
+        widget = RichTextArea()
+        js_init = widget.render_js_init('test-id', 'test', None)
+        self.assertIsInstance(js_init, unicode)
+        self.assertEqual(js_init, "makeRichTextEditable(\"test-id\", null);")
+
+    def test_custom_widget_render_js_init(self):
+        custom_widget = RichTextArea(editor_config={'plugins': {'halloheadings': {'formatBlocks': ['p', 'h2']}}})
+        js_init = custom_widget.render_js_init('test-id', 'test', None)
+        self.assertIsInstance(js_init, unicode)
+        self.assertEqual(js_init, "makeRichTextEditable(\"test-id\", {"
+                                  "\"plugins\": {\"halloheadings\": {\"formatBlocks\": [\"p\", \"h2\"]}}});"
+                         )
+
 
 
