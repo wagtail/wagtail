767a29c2358b43493d05b27e09dd7e439b79bdc0
Razi Alsayyed <razi.sayed@gmail.com>
2016-01-31 17:56:52 +0200

    use get_search_backend
    get_search_backend is language aware now.
    
    note that we change the backend parameter default value to None.

--- wagtail/wagtailsearch/queryset.py
+++ wagtail/wagtailsearch/queryset.py
@@ -1,12 +1,12 @@
-from wagtail.wagtailsearch.backends import get_language_aware_search_backend
+from wagtail.wagtailsearch.backends import get_search_backend
 
 
 class SearchableQuerySetMixin(object):
     def search(self, query_string, fields=None,
-               operator=None, order_by_relevance=True, backend='default'):
+               operator=None, order_by_relevance=True, backend=None):
         """
         This runs a search query on all the items in the QuerySet
         """
-        search_backend = get_language_aware_search_backend(backend)
+        search_backend = get_search_backend(backend)
         return search_backend.search(query_string, self, fields=fields,
                                      operator=operator, order_by_relevance=order_by_relevance)
