395cecc64225d87d009169c4fb30548a4f0cbcdf
Matt Westcott <matt@west.co.tt>
2016-02-09 16:53:49 +0000

    Fixes to page ordering logic in explorer:
    - keep ordering and pagination as two distinct operations
    - only perform the 'annotate' hack when ordering by latest_revision_created_at
    - entries with a null latest_revision_created_at should appear at the top when
      ordering oldest first
    - don't uselessly call order_by twice
    - actually test that the 'ordering' parameter orders the results :-)

--- wagtail/wagtailadmin/tests/test_pages_views.py
+++ wagtail/wagtailadmin/tests/test_pages_views.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 import mock
 
 import django
@@ -45,6 +45,21 @@ class TestPageExplorer(TestCase, WagtailTestUtils):
         )
         self.root_page.add_child(instance=self.child_page)
 
+        # more child pages to test ordering
+        self.old_page = StandardIndex(
+            title="Old page",
+            slug="old-page",
+            latest_revision_created_at=datetime(2010, 1, 1)
+        )
+        self.root_page.add_child(instance=self.old_page)
+
+        self.new_page = SimplePage(
+            title="New page",
+            slug="new-page",
+            latest_revision_created_at=datetime(2016, 1, 1)
+        )
+        self.root_page.add_child(instance=self.new_page)
+
         # Login
         self.login()
 
@@ -53,7 +68,11 @@ class TestPageExplorer(TestCase, WagtailTestUtils):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'wagtailadmin/pages/index.html')
         self.assertEqual(self.root_page, response.context['parent_page'])
-        self.assertTrue(response.context['pages'].paginator.object_list.filter(id=self.child_page.id).exists())
+
+        # child pages should be most recent first
+        # (with null latest_revision_created_at at the end)
+        page_ids = [page.id for page in response.context['pages']]
+        self.assertEqual(page_ids, [self.new_page.id, self.old_page.id, self.child_page.id])
 
     def test_explore_root(self):
         response = self.client.get(reverse('wagtailadmin_explore_root'))
@@ -63,22 +82,66 @@ class TestPageExplorer(TestCase, WagtailTestUtils):
         self.assertTrue(response.context['pages'].paginator.object_list.filter(id=self.root_page.id).exists())
 
     def test_ordering(self):
-        response = self.client.get(reverse('wagtailadmin_explore_root'), {'ordering': 'content_type'})
+        response = self.client.get(
+            reverse('wagtailadmin_explore', args=(self.root_page.id, )),
+            {'ordering': 'title'}
+        )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'wagtailadmin/pages/index.html')
-        self.assertEqual(response.context['ordering'], 'content_type')
+        self.assertEqual(response.context['ordering'], 'title')
+
+        # child pages should be ordered by title
+        page_ids = [page.id for page in response.context['pages']]
+        self.assertEqual(page_ids, [self.child_page.id, self.new_page.id, self.old_page.id])
+
+    def test_reverse_ordering(self):
+        response = self.client.get(
+            reverse('wagtailadmin_explore', args=(self.root_page.id, )),
+            {'ordering': '-title'}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'wagtailadmin/pages/index.html')
+        self.assertEqual(response.context['ordering'], '-title')
+
+        # child pages should be ordered by title
+        page_ids = [page.id for page in response.context['pages']]
+        self.assertEqual(page_ids, [self.old_page.id, self.new_page.id, self.child_page.id])
+
+    def test_ordering_by_last_revision_forward(self):
+        response = self.client.get(
+            reverse('wagtailadmin_explore', args=(self.root_page.id, )),
+            {'ordering': 'latest_revision_created_at'}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'wagtailadmin/pages/index.html')
+        self.assertEqual(response.context['ordering'], 'latest_revision_created_at')
+
+        # child pages should be oldest revision first
+        # (with null latest_revision_created_at at the start)
+        page_ids = [page.id for page in response.context['pages']]
+        self.assertEqual(page_ids, [self.child_page.id, self.old_page.id, self.new_page.id])
 
     def test_invalid_ordering(self):
-        response = self.client.get(reverse('wagtailadmin_explore_root'), {'ordering': 'invalid_order'})
+        response = self.client.get(
+            reverse('wagtailadmin_explore', args=(self.root_page.id, )),
+            {'ordering': 'invalid_order'}
+        )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'wagtailadmin/pages/index.html')
         self.assertEqual(response.context['ordering'], '-latest_revision_created_at')
 
     def test_reordering(self):
-        response = self.client.get(reverse('wagtailadmin_explore_root'), {'ordering': 'ord'})
+        response = self.client.get(
+            reverse('wagtailadmin_explore', args=(self.root_page.id, )),
+            {'ordering': 'ord'}
+        )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'wagtailadmin/pages/index.html')
         self.assertEqual(response.context['ordering'], 'ord')
+
+        # child pages should be ordered by native tree order (i.e. by creation time)
+        page_ids = [page.id for page in response.context['pages']]
+        self.assertEqual(page_ids, [self.child_page.id, self.old_page.id, self.new_page.id])
 
         # Pages must not be paginated
         self.assertNotIsInstance(response.context['pages'], paginator.Page)
