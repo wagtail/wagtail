35fc03c8a05cb34b5f654492ae7c9fdc8ccb3865
Matt Westcott <matt@west.co.tt>
2016-02-16 17:14:29 +0000

    fixup! Implement document permissions panel on group edit page

--- wagtail/wagtaildocs/wagtail_hooks.py
+++ wagtail/wagtaildocs/wagtail_hooks.py
@@ -2,7 +2,6 @@ from django.conf.urls import include, url
 from django.core import urlresolvers
 from django.utils.html import format_html, format_html_join
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.models import Permission
 from django.contrib.staticfiles.templatetags.staticfiles import static
 
 from wagtail.wagtailcore import hooks
