9cc6e985e1a2a5b82900f64a09dff82ccdce6712
Matt Westcott <matt@west.co.tt>
2016-02-16 17:16:29 +0000

    fixup! Move operation to fix collation on Collection.path to a separate migration

--- wagtail/wagtailcore/migrations/0027_fix_collection_path_collation.py
+++ wagtail/wagtailcore/migrations/0027_fix_collection_path_collation.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import migrations
 
 
 def set_collection_path_collation(apps, schema_editor):
