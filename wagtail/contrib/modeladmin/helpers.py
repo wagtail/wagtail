8bf896b17983cde806b93998bbf7facf09e54ca5
Andy Babic <ababic@rkh.co.uk>
2016-02-21 20:05:21 +0000

    backtracked on button style changes

--- wagtail/contrib/modeladmin/helpers.py
+++ wagtail/contrib/modeladmin/helpers.py
@@ -175,7 +175,7 @@ def get_url_name(model_meta, action='index'):
 
 class ButtonHelper(object):
 
-    default_button_classname = 'button button-small bicolor icon'
+    default_button_classname = 'button button-small button-secondary'
 
     def __init__(self, model, permission_helper, user):
         self.user = user
@@ -194,7 +194,7 @@ class ButtonHelper(object):
         return {
             'url': self.get_action_url('create'),
             'label': _('Add %s') % self.model_name,
-            'classname': 'button bicolor icon icon-plus',
+            'classname': self.default_button_classname,
             'title': _('Add a new %s') % self.model_name,
         }
 
@@ -202,7 +202,7 @@ class ButtonHelper(object):
         return {
             'url': self.get_action_url('edit', pk),
             'label': _('Edit'),
-            'classname': self.default_button_classname + ' icon-edit',
+            'classname': self.default_button_classname,
             'title': _('Edit this %s') % self.model_name,
         }
 
@@ -210,7 +210,7 @@ class ButtonHelper(object):
         return {
             'url': self.get_action_url('confirm_delete', pk),
             'label': _('Delete'),
-            'classname': self.default_button_classname + ' no icon-bin',
+            'classname': self.default_button_classname + ' no',
             'title': _('Delete this %s') % self.model_name,
         }
 
