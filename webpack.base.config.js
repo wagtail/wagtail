26d889821ea49ca995973f3239a8f9abf6f75d80
justinvdm <justinvdm.0.1.0@gmail.com>
2016-02-24 17:18:39 +0200

    Lint webpack configs

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -3,44 +3,44 @@ var path = require('path');
 var glob = require('glob').sync;
 
 
-module.exports = function(env) {
-    return {
-        entry: entryPoints('./wagtail/**/static_src/**/app/*.entry.js'),
-        output: {
-          path: './',
-          filename: '[name].js'
-        },
-        module: {
-            loaders: [{
-                test: /\.js$/,
-                loader: 'babel',
-            }, {
-                test: /\.jsx$/,
-                loader: 'babel',
-            }]
-        },
-    };
-};
-
-
-function entryPoints(paths) {
-    return _(glob(paths))
-        .map(entryPoint)
-        .fromPairs()
-        .value();
+function appName(filename) {
+  return _(filename)
+    .split(path.sep)
+    .get(2);
 }
 
 
 function entryPoint(filename) {
-    var name = appName(filename);
-    var entryName = path.basename(filename, '.entry.js');
-    var outputPath = path.join('wagtail', name, 'static', name, 'js', entryName);
-    return [outputPath, filename];
+  var name = appName(filename);
+  var entryName = path.basename(filename, '.entry.js');
+  var outputPath = path.join('wagtail', name, 'static', name, 'js', entryName);
+  return [outputPath, filename];
 }
 
 
-function appName(filename) {
-    return _(filename)
-        .split(path.sep)
-        .get(2);
+function entryPoints(paths) {
+  return _(glob(paths))
+    .map(entryPoint)
+    .fromPairs()
+    .value();
 }
+
+
+module.exports = function exports() {
+  return {
+    entry: entryPoints('./wagtail/**/static_src/**/app/*.entry.js'),
+    output: {
+      path: './',
+      filename: '[name].js'
+    },
+    module: {
+      loaders: [{
+        test: /\.js$/,
+        loader: 'babel',
+      }, {
+        test: /\.jsx$/,
+        loader: 'babel',
+      }]
+    },
+  };
+};
