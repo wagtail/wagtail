57f5126f906ea3b4965880be6d1e41188047bbeb
Matt Westcott <matt@west.co.tt>
2016-02-25 16:00:51 +0200

    Remove slug assignment from a bunch of tests that no longer need it

--- wagtail/wagtailadmin/tests/test_privacy.py
+++ wagtail/wagtailadmin/tests/test_privacy.py
@@ -15,14 +15,12 @@ class TestSetPrivacyView(TestCase, WagtailTestUtils):
 
         self.public_page = self.homepage.add_child(instance=SimplePage(
             title="Public page",
-            slug='public-page',
             content="hello",
             live=True,
         ))
 
         self.private_page = self.homepage.add_child(instance=SimplePage(
             title="Private page",
-            slug='private-page',
             content="hello",
             live=True,
         ))
@@ -30,7 +28,6 @@ class TestSetPrivacyView(TestCase, WagtailTestUtils):
 
         self.private_child_page = self.private_page.add_child(instance=SimplePage(
             title="Private child page",
-            slug='private-child-page',
             content="hello",
             live=True,
         ))
@@ -140,14 +137,12 @@ class TestPrivacyIndicators(TestCase, WagtailTestUtils):
 
         self.public_page = self.homepage.add_child(instance=SimplePage(
             title="Public page",
-            slug='public-page',
             content="hello",
             live=True,
         ))
 
         self.private_page = self.homepage.add_child(instance=SimplePage(
             title="Private page",
-            slug='private-page',
             content="hello",
             live=True,
         ))
@@ -155,7 +150,6 @@ class TestPrivacyIndicators(TestCase, WagtailTestUtils):
 
         self.private_child_page = self.private_page.add_child(instance=SimplePage(
             title="Private child page",
-            slug='private-child-page',
             content="hello",
             live=True,
         ))
