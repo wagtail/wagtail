e06f6acaba9200f712b57372d897682d5ac91f81
Matt Westcott <matt@west.co.tt>
2016-03-07 18:17:42 +0000

    Tweak dependencies to allow Willow 0.3b4 to be installed, via either tox or setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ install_requires = [
     "beautifulsoup4>=4.3.2",
     "html5lib>=0.999,<1",
     "Unidecode>=0.04.14",
-    "Willow>=0.3,<0.4",
+    "Willow>=0.3b4,<0.4",
 ]
 
 # Testing dependencies
