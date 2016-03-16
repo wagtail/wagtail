920a79f223a0f0cddbcb01b3bdc58d4cd85bd903
Matt Westcott <matt@west.co.tt>
2016-03-16 11:34:04 +0000

    Set version number to 1.4

--- wagtail/wagtailcore/__init__.py
+++ wagtail/wagtailcore/__init__.py
@@ -1,6 +1,6 @@
-__version__ = '1.4rc1'
+__version__ = '1.4'
 # Required for npm package for frontend
-__semver__ = '1.4.0-rc.1'
+__semver__ = '1.4.0'
 default_app_config = 'wagtail.wagtailcore.apps.WagtailCoreAppConfig'
 
 
