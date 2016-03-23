90bcabd9cd6d88fbe40f434d75a0d67ae87681af
Tim Heap <tim@timheap.me>
2016-03-23 17:24:27 +1100

    Use field.rel over field.related, rel.model over rel.to

--- wagtail/wagtailsnippets/edit_handlers.py
+++ wagtail/wagtailsnippets/edit_handlers.py
@@ -40,7 +40,7 @@ class BaseSnippetChooserPanel(BaseChooserPanel):
                         .format(cls.__name__, cls.snippet_type)
                     )
             else:
-                cls._target_model = cls.model._meta.get_field(cls.field_name).rel.to
+                cls._target_model = cls.model._meta.get_field(cls.field_name).rel.model
 
         return cls._target_model
 
