edc236cb06413bb645f2a480ae7eb19b2c2aa75a
Tim Heap <tim@timheap.me>
2016-03-24 11:34:31 +1100

    Verify SSL certificates for Elasticsearch connections by default
    Making developers opt out of extra security is better than making them
    opt in, especially when they may not be aware of the security they are
    missing out on.

--- wagtail/wagtailsearch/backends/elasticsearch.py
+++ wagtail/wagtailsearch/backends/elasticsearch.py
@@ -686,6 +686,7 @@ class ElasticSearch(BaseSearch):
                     'port': port,
                     'url_prefix': parsed_url.path,
                     'use_ssl': use_ssl,
+                    'verify_certs': use_ssl,
                     'http_auth': http_auth,
                 })
 
