a484abda0a65df240a1887003d5174708680fe1e
Karl Hobley <karlhobley10@gmail.com>
2016-03-28 12:08:33 +0100

    Catch errors raised during search index auto update
    Fixes #2323
    
    This prevents the admin interface crashing if there was an error while updating the search index.
    
    The error is now caught and logged instead

--- wagtail/wagtailsearch/signal_handlers.py
+++ wagtail/wagtailsearch/signal_handlers.py
@@ -1,9 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 
+import logging
+
 from django.db.models.signals import post_delete, post_save
 
-from wagtail.wagtailsearch.backends import get_search_backends
+from wagtail.wagtailsearch.backends import get_search_backends_with_name
 from wagtail.wagtailsearch.index import get_indexed_models
+
+logger = logging.getLogger('wagtail.search.auto_reindex')
 
 
 def get_indexed_instance(instance):
@@ -22,16 +26,24 @@ def post_save_signal_handler(instance, **kwargs):
     indexed_instance = get_indexed_instance(instance)
 
     if indexed_instance:
-        for backend in get_search_backends(with_auto_update=True):
-            backend.add(indexed_instance)
+        for backend_name, backend in get_search_backends_with_name(with_auto_update=True):
+            try:
+                backend.add(indexed_instance)
+            except Exception:
+                # Catch and log all errors
+                logger.exception("Exception raised while adding %r into the '%s' search backend", indexed_instance, backend_name)
 
 
 def post_delete_signal_handler(instance, **kwargs):
     indexed_instance = get_indexed_instance(instance)
 
     if indexed_instance:
-        for backend in get_search_backends(with_auto_update=True):
-            backend.delete(indexed_instance)
+        for backend_name, backend in get_search_backends_with_name(with_auto_update=True):
+            try:
+                backend.delete(indexed_instance)
+            except Exception:
+                # Catch and log all errors
+                logger.exception("Exception raised while deleting %r from the '%s' search backend", indexed_instance, backend_name)
 
 
 def register_signal_handlers():
