885c4ec28ddca98ef3c6c1cda9c1706b7a2dd472
Tim Heap <tim@timheap.me>
2016-03-29 06:34:25 +0200

    Add tests for snippet edit handler form classes

---
+++ wagtail/tests/snippets/forms.py
@@ -0,0 +1,7 @@
+from wagtail.wagtailadmin.forms import WagtailAdminModelForm
+
+
+class FancySnippetForm(WagtailAdminModelForm):
+    """
+    A custom form class for FancySnippets in the admin
+    """
