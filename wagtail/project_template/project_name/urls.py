196250fd47291adbe9d2d40e9cd5a7b5d7de84da
Tomas Olander <tomasolander@gmail.com>
2016-04-09 18:32:39 +0200

    Remove unused import in project template (#2466)

--- wagtail/project_template/project_name/urls.py
+++ wagtail/project_template/project_name/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-    from django.views.generic import TemplateView
 
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
