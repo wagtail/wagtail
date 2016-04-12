0ec0edd8d3d22920293fbe1534224e060c42c32c
Karl Hobley <karlhobley10@gmail.com>
2016-04-12 21:01:51 +0100

    Added 'jpegquality' image filter
    This allows the developer to adjust the output quality of JPEG images on a per-placement basis

--- wagtail/wagtailimages/image_operations.py
+++ wagtail/wagtailimages/image_operations.py
@@ -216,3 +216,14 @@ class WidthHeightOperation(Operation):
             return
 
         return willow.resize((width, height))
+
+
+class JPEGQualityOperation(Operation):
+    def construct(self, quality):
+        self.quality = int(quality)
+
+        if self.quality > 100:
+            raise ValueError("JPEG quality must not be higher than 100")
+
+    def run(self, willow, image, env):
+        env['jpeg-quality'] = self.quality
