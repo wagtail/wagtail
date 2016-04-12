f58eb6ef28b874bca06d47a0c1cca2af6cd402de
Karl Hobley <karlhobley10@gmail.com>
2016-04-12 21:10:17 +0100

    Added 'format' template filter
    This allows the developer to change the output format of an image placement

--- wagtail/wagtailimages/image_operations.py
+++ wagtail/wagtailimages/image_operations.py
@@ -227,3 +227,14 @@ class JPEGQualityOperation(Operation):
 
     def run(self, willow, image, env):
         env['jpeg-quality'] = self.quality
+
+
+class FormatOperation(Operation):
+    def construct(self, fmt):
+        self.format = fmt
+
+        if self.format not in ['jpeg', 'png', 'gif']:
+            raise ValueError("Format must be either 'jpeg', 'png' or 'gif'")
+
+    def run(self, willow, image, env):
+        env['output-format'] = self.format
