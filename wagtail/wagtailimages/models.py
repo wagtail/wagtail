1b89d77a865cbbf8486e2d9398c30ab5bcef5c0f
Karl Hobley <karlhobley10@gmail.com>
2016-04-12 20:57:19 +0100

    Backwards compatibility for 3-argument run methods

--- wagtail/wagtailimages/models.py
+++ wagtail/wagtailimages/models.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 
 import hashlib
+import inspect
 import os.path
+import warnings
 from collections import OrderedDict
 from contextlib import contextmanager
 
@@ -406,7 +408,25 @@ class Filter(models.Model):
 
             env = {}
             for operation in self.operations:
-                willow = operation.run(willow, image, env) or willow
+                # Check that the operation can take the new "env" argument
+                # (added in Wagtail 1.5)
+                try:
+                    inspect.getcallargs(operation.run, willow, image, env)
+                    accepts_env = True
+                except TypeError:
+                    # Check that the paramters fit the old style, so we don't
+                    # raise a warning if there is a coding error
+                    inspect.getcallargs(operation.run, willow, image)
+                    accepts_env = False
+                    warnings.warn("ImageOperation run methods should take 4 "
+                                  "arguments. %d.run only takes 3.",
+                                  RemovedInWagtail17Warning)
+
+                # Call operation
+                if accepts_env:
+                    willow = operation.run(willow, image, env) or willow
+                else:
+                    willow = operation.run(willow, image) or willow
 
             if original_format == 'jpeg':
                 # Allow changing of JPEG compression quality
