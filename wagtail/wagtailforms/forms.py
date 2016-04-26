6c68d36399870192a220db77638b170553f63df1
Mikalai Radchuk <m1kola@users.noreply.github.com>
2016-04-26 19:18:31 +0300

    Useless return

--- wagtail/wagtailforms/forms.py
+++ wagtail/wagtailforms/forms.py
@@ -8,7 +8,7 @@ import django.forms
 class BaseForm(django.forms.Form):
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('label_suffix', '')
-        return super(BaseForm, self).__init__(*args, **kwargs)
+        super(BaseForm, self).__init__(*args, **kwargs)
 
 
 class FormBuilder(object):
