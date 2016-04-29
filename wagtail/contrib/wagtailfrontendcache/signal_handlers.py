ecd5ba2ff0027006cfb37709caf398345ca6fe10
Vincent Audebert <vincent.audebert@yahoo.fr>
2016-04-29 15:53:25 +1200

    Fixing #2527 Old cached page is purged if page is moved or slug changed. TODO: Write tests

--- wagtail/contrib/wagtailfrontendcache/signal_handlers.py
+++ wagtail/contrib/wagtailfrontendcache/signal_handlers.py
@@ -1,9 +1,21 @@
 from __future__ import absolute_import, unicode_literals
 
+import logging
+
 from django.apps import apps
 
-from wagtail.contrib.wagtailfrontendcache.utils import purge_page_from_cache
+from wagtail.contrib.wagtailfrontendcache.utils import purge_page_from_cache, purge_url_from_cache
 from wagtail.wagtailcore.signals import page_published, page_unpublished
+from django.db.models.signals import post_save, pre_save
+
+logger = logging.getLogger('wagtail.frontendcache')
+
+
+def has_path_changed(instance, field):
+    if not instance.pk:
+        return False
+    old_value = instance.__class__._default_manager.filter(pk=instance.pk).values(field).get()[field]
+    return not getattr(instance, field) == old_value
 
 
 def page_published_signal_handler(instance, **kwargs):
@@ -12,6 +24,19 @@ def page_published_signal_handler(instance, **kwargs):
 
 def page_unpublished_signal_handler(instance, **kwargs):
     purge_page_from_cache(instance)
+
+
+def page_pre_saved_signal_handler(instance, **kwargs):
+    if has_path_changed(instance, 'url_path'):
+        instance.old_url = instance.url
+    else:
+        instance.old_url = None
+
+
+def page_post_saved_signal_handler(instance, **kwargs):
+    if not kwargs['update_fields']:
+        if instance.old_url:
+            purge_url_from_cache(instance.old_url)
 
 
 def register_signal_handlers():
@@ -23,3 +48,5 @@ def register_signal_handlers():
     for model in indexed_models:
         page_published.connect(page_published_signal_handler, sender=model)
         page_unpublished.connect(page_unpublished_signal_handler, sender=model)
+        pre_save.connect(page_pre_saved_signal_handler, sender=model)
+        post_save.connect(page_post_saved_signal_handler, sender=model)
