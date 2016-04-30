105256e4158d2ce70233a9aac745a50b667dcd8f
Lucas Moeskops <lucas@fabrique.nl>
2016-04-30 19:40:06 +0200

    Support for foreign related objects for Elasticsearch backend search.

---
+++ wagtail/tests/search/migrations/0002_subobject.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('searchtests', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='SearchTestSubObject',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('parent', models.ForeignKey(to='searchtests.SearchTest')),
+                ('name', models.CharField(max_length=255)),
+            ],
+            bases=(models.Model,),
+        ),
+    ]
