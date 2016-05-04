a30dc4edb0c1fb21cff1ea391b7f6f6f0a8f1469
Robert Rollins <rrollins@caltech.edu>
2016-05-03 17:53:25 -0700

    A few fixes to make flake8 and isort happy.

--- wagtail/wagtailcore/models.py
+++ wagtail/wagtailcore/models.py
@@ -7,7 +7,6 @@ from collections import defaultdict
 from django import VERSION as DJANGO_VERSION
 
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.core import checks
--- wagtail/wagtailcore/request_cache.py
+++ wagtail/wagtailcore/request_cache.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import, unicode_literals
+
 from uuid import uuid4
 
 from django.core.cache.backends.base import BaseCache
