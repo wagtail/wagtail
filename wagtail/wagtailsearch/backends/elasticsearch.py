f7320bde3c437612f7f4e36f86ed681ba161a7f8
Karl Hobley <karlhobley10@gmail.com>
2016-05-05 10:45:13 +0100

    Elasticsearch: Replaced usage of index_analyzer
    index_analyzer has been removed in Elasticsearch 2.0. But the new way works in older versions as well so I think it's worth switching to the new way now.
    
    https://www.elastic.co/guide/en/elasticsearch/reference/current/breaking_20_mapping_changes.html#_analyzer_mappings

--- wagtail/wagtailsearch/backends/elasticsearch.py
+++ wagtail/wagtailsearch/backends/elasticsearch.py
@@ -62,7 +62,8 @@ class ElasticSearchMapping(object):
                     mapping['boost'] = field.boost
 
                 if field.partial_match:
-                    mapping['index_analyzer'] = 'edgengram_analyzer'
+                    mapping['analyzer'] = 'edgengram_analyzer'
+                    mapping['search_analyzer'] = 'standard'
 
                 mapping['include_in_all'] = True
 
@@ -81,7 +82,7 @@ class ElasticSearchMapping(object):
         fields = {
             'pk': dict(type='string', index='not_analyzed', store='yes', include_in_all=False),
             'content_type': dict(type='string', index='not_analyzed', include_in_all=False),
-            '_partials': dict(type='string', index_analyzer='edgengram_analyzer', include_in_all=False),
+            '_partials': dict(type='string', analyzer='edgengram_analyzer', search_analyzer='standard', include_in_all=False),
         }
 
         fields.update(dict(
