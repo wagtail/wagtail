57e476641780feedcf6fa05f8f8f5e71fd1ea92d
Mikalai Radchuk <radchuk.m@gmail.com>
2016-05-11 19:56:55 +0300

    Fixes `RichTextBlock`
    `self.editor` should be passed into `get_rich_text_editor`

--- wagtail/wagtailcore/blocks/field_block.py
+++ wagtail/wagtailcore/blocks/field_block.py
@@ -327,7 +327,7 @@ class RichTextBlock(FieldBlock):
     @cached_property
     def field(self):
         from wagtail.wagtailadmin.rich_text import get_rich_text_editor
-        return forms.CharField(widget=get_rich_text_editor(), **self.field_options)
+        return forms.CharField(widget=get_rich_text_editor(self.editor), **self.field_options)
 
     def value_for_form(self, value):
         # Rich text editors take the source-HTML string as input (and takes care
