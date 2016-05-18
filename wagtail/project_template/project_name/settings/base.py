7f8cf2539f52581465ea7a1e0591a49c76ea7ac8
Matt Westcott <matt@west.co.tt>
2016-05-18 22:11:38 +0100

    Add BASE_URL setting back to project template

--- wagtail/project_template/project_name/settings/base.py
+++ wagtail/project_template/project_name/settings/base.py
@@ -136,3 +136,7 @@ MEDIA_URL = '/media/'
 # Wagtail settings
 
 WAGTAIL_SITE_NAME = "{{ project_name }}"
+
+# Base URL to use when referring to full URLs within the Wagtail admin backend -
+# e.g. in notification emails. Don't include '/admin' or a trailing slash
+BASE_URL = 'http://example.com'
