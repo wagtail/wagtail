57ab79e00bf7c7e7864aa2d37c9c9935a9a12605
Matt Westcott <matt@west.co.tt>
2016-05-19 14:36:16 +0100

    Prepare for 1.4.5 release

--- wagtail/wagtailcore/__init__.py
+++ wagtail/wagtailcore/__init__.py
@@ -1,6 +1,6 @@
-__version__ = '1.4.4'
+__version__ = '1.4.5'
 # Required for npm package for frontend
-__semver__ = '1.4.4'
+__semver__ = '1.4.5'
 default_app_config = 'wagtail.wagtailcore.apps.WagtailCoreAppConfig'
 
 
