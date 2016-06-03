f282fc2022b776c090f16c865e1d27ec8f8bb6f5
riceyrice <stephen.rice@isotoma.com>
2016-06-03 17:23:43 +0100

    Add setting to use wagtailsearchpromotions to manage queries
    WAGTAILSEARCHPROMOTIONS_QUERY_EDITOR=True in settings changes the UI to:
    - show all saved queries whether they are promoted or not
    - allow queries to be deleted

--- wagtail/contrib/wagtailsearchpromotions/views.py
+++ wagtail/contrib/wagtailsearchpromotions/views.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 
+from django.conf import settings
 from django.core.urlresolvers import reverse
+from django.db.models import Q
+from django.db.models.aggregates import Sum
+from django.db.models.functions import Coalesce
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.translation import ugettext as _
 from django.views.decorators.vary import vary_on_headers
@@ -14,6 +18,25 @@ from wagtail.wagtailsearch import forms as search_forms
 from wagtail.wagtailsearch.models import Query
 
 
+query_editor_setting = 'WAGTAILSEARCHPROMOTIONS_QUERY_EDITOR'
+
+# If acting as query editor, allow saving a query with no promotions
+if getattr(settings, query_editor_setting, False):
+    forms.SearchPromotionsFormSet.minimum_forms = 0
+
+
+# Factored out for reuse as public-facing "top searches"
+def get_hit_queryset():
+    queryset = Query.objects.filter(
+        Q(daily_hits__isnull=False) | Q(editors_picks__isnull=False))
+    queryset = Query.objects.filter(pk__in=queryset).annotate(_hits=Coalesce(Sum('daily_hits__hits'), 0))
+    return queryset.order_by('-_hits', 'query_string')
+
+
+def get_promoted_queryset():
+    return Query.objects.filter(editors_picks__isnull=False).distinct()
+
+
 @any_permission_required(
     'wagtailsearchpromotions.add_searchpromotion',
     'wagtailsearchpromotions.change_searchpromotion',
@@ -21,31 +44,41 @@ from wagtail.wagtailsearch.models import Query
 )
 @vary_on_headers('X-Requested-With')
 def index(request):
+    is_editor = getattr(settings, query_editor_setting, False)
     is_searching = False
+    queries_count = None
     query_string = request.GET.get('q', "")
 
-    queries = Query.objects.filter(editors_picks__isnull=False).distinct()
+    if is_editor:
+        queries = get_hit_queryset()
+    else:
+        queries = get_promoted_queryset()
 
     # Search
     if query_string:
         queries = queries.filter(query_string__icontains=query_string)
         is_searching = True
+        queries_count = queries.count()
 
     paginator, queries = paginate(request, queries)
 
     if request.is_ajax():
         return render(request, "wagtailsearchpromotions/results.html", {
+            'is_editor': is_editor,
             'is_searching': is_searching,
             'queries': queries,
+            'queries_count': queries_count,
             'query_string': query_string,
         })
     else:
         return render(request, 'wagtailsearchpromotions/index.html', {
+            'is_editor': is_editor,
             'is_searching': is_searching,
             'queries': queries,
             'query_string': query_string,
             'search_form': SearchForm(
-                data=dict(q=query_string) if query_string else None, placeholder=_("Search promoted results")
+                data=dict(q=query_string) if query_string else None,
+                placeholder=_("Search all terms" if is_editor else "Search promoted results")
             ),
         })
 
@@ -145,13 +178,18 @@ def edit(request, query_id):
 
 @permission_required('wagtailsearchpromotions.delete_searchpromotion')
 def delete(request, query_id):
+    is_editor = getattr(settings, query_editor_setting, False)
     query = get_object_or_404(Query, id=query_id)
 
     if request.method == 'POST':
-        query.editors_picks.all().delete()
+        if is_editor:
+            query.delete()
+        else:
+            query.editors_picks.all().delete()
         messages.success(request, _("Editor's picks deleted."))
         return redirect('wagtailsearchpromotions:index')
 
     return render(request, 'wagtailsearchpromotions/confirm_delete.html', {
+        'is_editor': is_editor,
         'query': query,
     })
