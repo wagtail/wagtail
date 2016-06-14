3b7a2442db9458fccb11c1a07e268d4a25d29012
Karl Hobley <karl@torchbox.com>
2016-06-14 12:12:30 +0100

    Fixed isort errors

--- wagtail/wagtailadmin/edit_handlers.py
+++ wagtail/wagtailadmin/edit_handlers.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 
+import math
+import re
 import warnings
 
 import django
-import re
-import math
 from django import forms
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ImproperlyConfigured
