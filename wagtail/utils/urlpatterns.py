9a9f4a7bec685f9b9fbafe08da27cfd63736817b
Paul J Stevens <p.stevens@lukkien.com>
2016-06-16 15:32:36 +0200

    fix decorated urlpatterns

--- wagtail/utils/urlpatterns.py
+++ wagtail/utils/urlpatterns.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, unicode_literals
-
 from functools import update_wrapper
+from django import VERSION as DJANGO_VERSION
 
 
 def decorate_urlpatterns(urlpatterns, decorator):
@@ -8,7 +8,11 @@ def decorate_urlpatterns(urlpatterns, decorator):
         if hasattr(pattern, 'url_patterns'):
             decorate_urlpatterns(pattern.url_patterns, decorator)
 
-        if hasattr(pattern, '_callback'):
-            pattern._callback = update_wrapper(decorator(pattern.callback), pattern.callback)
+        if DJANGO_VERSION <= (1, 9):
+            if hasattr(pattern, '_callback'):
+                pattern._callback = update_wrapper(decorator(pattern.callback), pattern.callback)
+        else:
+            if hasattr(pattern, 'callback') and callable(pattern.callback):
+                pattern.callback = update_wrapper(decorator(pattern.callback), pattern.callback)
 
     return urlpatterns
