6b58a7104697b526797204cbb5f6364faf9c8f26
Robert Rollins <rrollins@caltech.edu>
2016-06-20 12:27:57 -0700

    RequestCacheMiddleware adds a local memory cache instance as request.cache.
    This is useful for those times when a single request will need to perform a very
    expensive operation multiples times, but the results of that opperation are
    specific to the current request. Unlike all other caching mechanisms provided
    by Django, RequestCache's data will be garbage collected alongside the request
    object.
    
    This PR is part of the functionality from #2463, which @gasman asked me to split
    into multiple PRs to make it easier to review.

--- wagtail/wagtailcore/middleware.py
+++ wagtail/wagtailcore/middleware.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 
 from wagtail.wagtailcore.models import Site
+from wagtail.wagtailcore.request_cache import RequestCache
 
 
 class SiteMiddleware(object):
+
     def process_request(self, request):
         """
         Set request.site to contain the Site object responsible for handling this request,
@@ -13,3 +15,12 @@ class SiteMiddleware(object):
             request.site = Site.find_for_request(request)
         except Site.DoesNotExist:
             request.site = None
+
+
+class RequestCacheMiddleware(object):
+    """
+    Creates a fresh cache instance as request.cache. The cache instance lives only as long as request does.
+    """
+
+    def process_request(self, request):
+        request.cache = RequestCache()
