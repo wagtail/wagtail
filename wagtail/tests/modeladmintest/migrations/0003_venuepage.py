d9b1b4796dc79c72fde3bac2c825ff475c38cc29
Andy Babic <ababic@rkh.co.uk>
2016-06-22 14:18:56 +0100

    - Restore functionality to ModelAdmin class that didn't apply properly from patch - Fix migration to silence 'test__migrations' test fail

--- wagtail/tests/modeladmintest/migrations/0003_venuepage.py
+++ wagtail/tests/modeladmintest/migrations/0003_venuepage.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='VenuePage',
             fields=[
-                ('page_ptr', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, primary_key=True, to='wagtailcore.Page')),
+                ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
                 ('address', models.CharField(max_length=300)),
                 ('capacity', models.IntegerField()),
             ],
