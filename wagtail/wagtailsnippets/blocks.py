3674eadf8c31933dc9d5a8a875f05ac0110bef48
Nick Smith <nick.smith@torchbox.com>
2016-06-22 10:43:23 +0100

    Allow referencing models by string representation in SnippetChooserBlock

--- wagtail/wagtailsnippets/blocks.py
+++ wagtail/wagtailsnippets/blocks.py
@@ -3,12 +3,17 @@ from __future__ import absolute_import, unicode_literals
 from django.utils.functional import cached_property
 
 from wagtail.wagtailcore.blocks import ChooserBlock
+from wagtail.wagtailcore.utils import resolve_model_string
 
 
 class SnippetChooserBlock(ChooserBlock):
     def __init__(self, target_model, **kwargs):
         super(SnippetChooserBlock, self).__init__(**kwargs)
-        self.target_model = target_model
+        self._target_model = target_model
+
+    @cached_property
+    def target_model(self):
+        return resolve_model_string(self._target_model)
 
     @cached_property
     def widget(self):
