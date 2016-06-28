7b86009365796426beef136c6622faac995bc56d
Eraldo Energy <eraldo@eraldo.org>
2016-06-29 00:13:39 +0200

    Renamed `document` model attribute to `attachment` to avoid conflicts with wagtail.

--- wagtail/tests/customuser/migrations/0002_added_file_field.py
+++ wagtail/tests/customuser/migrations/0002_added_file_field.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddField(
             model_name='CustomUser',
-            name='document',
+            name='attachment',
             field=models.FileField(blank=True),
         ),
     ]
--- wagtail/tests/customuser/models.py
+++ wagtail/tests/customuser/models.py
@@ -41,7 +41,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
     country = models.CharField(max_length=100, blank=True)
-    document = models.FileField(blank=True)
+    attachment = models.FileField(blank=True)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
