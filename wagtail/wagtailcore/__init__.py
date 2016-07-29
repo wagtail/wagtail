3cc4e3ea250d21f7fa0ee13f7e2494712c35eb02
Tim Heap <tim@timheap.me>
2016-07-29 11:20:06 +1000

    Add a VERSION tuple, and some version helpers from Django

--- wagtail/wagtailcore/__init__.py
+++ wagtail/wagtailcore/__init__.py
@@ -1,7 +1,15 @@
-__version__ = '1.7a0'
+from __future__ import absolute_import, unicode_literals
+
+from wagtail.utils.version import get_semver_version, get_version
+
+# major.minor.patch.release.number
+# release must be one of alpha, beta, rc, or final
+VERSION = (1, 7, 0, 'alpha', 0)
+
+__version__ = get_version(VERSION)
+
 # Required for npm package for frontend
-__semver__ = '1.7.0-alpha'
-default_app_config = 'wagtail.wagtailcore.apps.WagtailCoreAppConfig'
+__semver__ = get_semver_version(VERSION)
 
 
 def setup():
