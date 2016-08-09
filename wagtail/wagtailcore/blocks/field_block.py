bb44c0349411732a5d36820f0df7986eb9004cce
Andreas Nüßlein <nutz@noova.de>
2016-08-09 11:50:04 +0200

    ChoiceBlock should accept "include_blank=False"
    Just like the referenced `modelfield`, ChoiceBlock should be able to NOT inlude a blank option.

--- wagtail/wagtailcore/blocks/field_block.py
+++ wagtail/wagtailcore/blocks/field_block.py
@@ -316,7 +316,7 @@ class ChoiceBlock(FieldBlock):
 
     choices = ()
 
-    def __init__(self, choices=None, required=True, help_text=None, **kwargs):
+    def __init__(self, choices=None, required=True, help_text=None, include_blank=True, **kwargs):
         if choices is None:
             # no choices specified, so pick up the choice list defined at the class level
             choices = list(self.choices)
@@ -334,21 +334,22 @@ class ChoiceBlock(FieldBlock):
         # If choices does not already contain a blank option, insert one
         # (to match Django's own behaviour for modelfields:
         # https://github.com/django/django/blob/1.7.5/django/db/models/fields/__init__.py#L732-744)
-        has_blank_choice = False
-        for v1, v2 in choices:
-            if isinstance(v2, (list, tuple)):
-                # this is a named group, and v2 is the value list
-                has_blank_choice = any([value in ('', None) for value, label in v2])
-                if has_blank_choice:
-                    break
-            else:
-                # this is an individual choice; v1 is the value
-                if v1 in ('', None):
-                    has_blank_choice = True
-                    break
-
-        if not has_blank_choice:
-            choices = BLANK_CHOICE_DASH + choices
+        if include_blank:
+            has_blank_choice = False
+            for v1, v2 in choices:
+                if isinstance(v2, (list, tuple)):
+                    # this is a named group, and v2 is the value list
+                    has_blank_choice = any([value in ('', None) for value, label in v2])
+                    if has_blank_choice:
+                        break
+                else:
+                    # this is an individual choice; v1 is the value
+                    if v1 in ('', None):
+                        has_blank_choice = True
+                        break
+    
+            if not has_blank_choice:
+                choices = BLANK_CHOICE_DASH + choices
 
         self.field = forms.ChoiceField(choices=choices, required=required, help_text=help_text)
         super(ChoiceBlock, self).__init__(**kwargs)
