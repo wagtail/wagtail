24e243f3fba8640149e4b18327f5eb994ffb9ebd
Matt Westcott <matt@west.co.tt>
2016-08-26 11:26:34 +0100

    version bump for 1.6.1

--- wagtail/wagtailcore/__init__.py
+++ wagtail/wagtailcore/__init__.py
@@ -1,6 +1,6 @@
-__version__ = '1.6'
+__version__ = '1.6.1'
 # Required for npm package for frontend
-__semver__ = '1.6.0'
+__semver__ = '1.6.1'
 default_app_config = 'wagtail.wagtailcore.apps.WagtailCoreAppConfig'
 
 
