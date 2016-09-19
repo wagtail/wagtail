d58d3579e58aec10c23741b20e39ccd5cdfcb288
Thibaud Colas <thibaudcolas@gmail.com>
2016-09-19 22:50:55 +0300

    Add missing test stubs & document them

--- client/tests/stubs.js
+++ client/tests/stubs.js
@@ -1,3 +1,9 @@
+/**
+ * Test stubs to mirror available global variables.
+ * Those variables usually come from the back-end via templates.
+ * See /wagtailadmin/templates/wagtailadmin/admin_base.html.
+ */
+
 global.wagtailConfig = {
   api: {
     documents: '/admin/api/v1beta/documents/',
@@ -6,7 +12,13 @@ global.wagtailConfig = {
   },
   urls: {
     pages: '/admin/pages/',
-  }
+  },
+  strings: {
+    EXPLORER: 'Explorer',
+    LOADING: 'Loading...',
+    NO_RESULTS: 'No results',
+    SEE_CHILDREN: 'See Children',
+  },
 };
 
 global.wagtailVersion = '1.6a1';
