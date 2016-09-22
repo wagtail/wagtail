76d61f621e90113a2f4a8e2d6a4c5fe50ba3c513
Thibaud Colas <thibaudcolas@gmail.com>
2016-09-22 23:09:24 +0300

    Use more advanced production uglify config

--- webpack.prd.config.js
+++ webpack.prd.config.js
@@ -4,17 +4,24 @@ var config = base('production');
 
 
 // production overrides go here
-config.watch = false;
-
 config.plugins.push(new webpack.DefinePlugin({
   'process.env': {
     NODE_ENV: JSON.stringify('production'),
   },
 }));
 
+// See https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/config/webpack.config.prod.js.
 config.plugins.push(new webpack.optimize.UglifyJsPlugin({
   compress: {
-    warnings: false,
+    screw_ie8: true,
+    warnings: false
+  },
+  mangle: {
+    screw_ie8: true
+  },
+  output: {
+    comments: false,
+    screw_ie8: true
   },
 }));
 
