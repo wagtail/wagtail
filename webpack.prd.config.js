d0f2b9b42cd70784d73d5fec46c73138b140efa7
Thibaud Colas <thibaudcolas@gmail.com>
2016-09-22 22:54:57 +0300

    Add basic env-specific webpack configuration

--- webpack.prd.config.js
+++ webpack.prd.config.js
@@ -1,8 +1,21 @@
+var webpack = require('webpack');
 var base = require('./webpack.base.config');
 var config = base('production');
 
 
 // production overrides go here
+config.watch = false;
 
+config.plugins.push(new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production'),
+  },
+}));
+
+config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+  compress: {
+    warnings: false,
+  },
+}));
 
 module.exports = config;
