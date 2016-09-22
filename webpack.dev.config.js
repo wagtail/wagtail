87b400d00bc74150dd6a669d1b37301a9527dc3f
Thibaud Colas <thibaudcolas@gmail.com>
2016-09-22 22:54:57 +0300

    Refine webpack configuration per env
    Add env-specific webpack configuration
    
    Add basic env-specific webpack configuration
    
    Use more advanced production uglify config
    
    Simplify webpack loader config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,3 +1,4 @@
+var webpack = require('webpack');
 var base = require('./webpack.base.config');
 var config = base('development');
 
@@ -6,5 +7,12 @@ var config = base('development');
 config.watch = true;
 // See http://webpack.github.io/docs/configuration.html#devtool
 config.devtool = 'inline-source-map';
+
+// Set process.env.NODE_ENV to development to enable JS development aids.
+config.plugins.push(new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('development'),
+  },
+}));
 
 module.exports = config;
