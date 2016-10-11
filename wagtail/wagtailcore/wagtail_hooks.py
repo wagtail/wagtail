5559358e50190c35485f0528bf6d5142cf7d2f86
Matt Westcott <matt@west.co.tt>
2016-10-11 15:17:06 +0100

    Implement front-end logic for group and login-based permissions

--- wagtail/wagtailcore/wagtail_hooks.py
+++ wagtail/wagtailcore/wagtail_hooks.py
@@ -1,8 +1,17 @@
 from __future__ import absolute_import, unicode_literals
 
+from django.conf import settings
+from django.contrib.auth.views import redirect_to_login
 from django.core.urlresolvers import reverse
 
+from wagtail.utils.compat import user_is_authenticated
 from wagtail.wagtailcore import hooks
+from wagtail.wagtailcore.models import PageViewRestriction
+
+
+def require_wagtail_login(next):
+    login_url = getattr(settings, 'WAGTAIL_FRONTEND_LOGIN_URL', reverse('wagtailcore_login'))
+    return redirect_to_login(next, login_url)
 
 
 @hooks.register('before_serve_page')
@@ -19,9 +28,19 @@ def check_view_restrictions(page, request, serve_args, serve_kwargs):
     if restrictions:
         passed_restrictions = request.session.get('passed_page_view_restrictions', [])
         for restriction in restrictions:
-            if restriction.id not in passed_restrictions:
-                from wagtail.wagtailcore.forms import PasswordPageViewRestrictionForm
-                form = PasswordPageViewRestrictionForm(instance=restriction,
-                                                       initial={'return_url': request.get_full_path()})
-                action_url = reverse('wagtailcore_authenticate_with_password', args=[restriction.id, page.id])
-                return page.serve_password_required_response(request, form, action_url)
+            if restriction.restriction_type == PageViewRestriction.PASSWORD:
+                if restriction.id not in passed_restrictions:
+                    from wagtail.wagtailcore.forms import PasswordPageViewRestrictionForm
+                    form = PasswordPageViewRestrictionForm(instance=restriction,
+                                                           initial={'return_url': request.get_full_path()})
+                    action_url = reverse('wagtailcore_authenticate_with_password', args=[restriction.id, page.id])
+                    return page.serve_password_required_response(request, form, action_url)
+            elif restriction.restriction_type == PageViewRestriction.LOGIN:
+                if not user_is_authenticated(request.user):
+                    return require_wagtail_login(next=request.get_full_path())
+            elif restriction.restriction_type == PageViewRestriction.GROUPS:
+                if not request.user.is_superuser:
+                    current_user_groups = request.user.groups.all()
+
+                    if not any(group in current_user_groups for group in restriction.groups.all()):
+                        return require_wagtail_login(next=request.get_full_path())
