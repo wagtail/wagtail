60c9d7c26c03b029d961bb4a53fb2cbd7a12c70b
Karl Hobley <karl@torchbox.com>
2016-11-08 10:46:59 +0000

    Switch query combinators to use "bool" queries

--- wagtail/wagtailsearch/backends/elasticsearch5.py
+++ wagtail/wagtailsearch/backends/elasticsearch5.py
@@ -14,6 +14,57 @@ class Elasticsearch5Mapping(Elasticsearch2Mapping):
 class Elasticsearch5SearchQuery(Elasticsearch2SearchQuery):
     mapping_class = Elasticsearch5Mapping
 
+    def _connect_filters(self, filters, connector, negated):
+        if filters:
+            if len(filters) == 1:
+                filter_out = filters[0]
+            elif connector == 'AND':
+                filter_out = {
+                    'bool': {
+                        'must': [
+                            fil for fil in filters if fil is not None
+                        ]
+                    }
+                }
+            elif connector == 'OR':
+                filter_out = {
+                    'bool': {
+                        'should': [
+                            fil for fil in filters if fil is not None
+                        ]
+                    }
+                }
+
+            if negated:
+                filter_out = {
+                    'bool': {
+                        'mustNot': filter_out
+                    }
+                }
+
+            return filter_out
+
+    def get_query(self):
+        inner_query = self.get_inner_query()
+        filters = self.get_filters()
+
+        if len(filters) == 1:
+            return {
+                'bool': {
+                    'must': inner_query,
+                    'filter': filters[0],
+                }
+            }
+        elif len(filters) > 1:
+            return {
+                'bool': {
+                    'must': inner_query,
+                    'filter': filters,
+                }
+            }
+        else:
+            return inner_query
+
 
 class Elasticsearch5SearchResults(Elasticsearch2SearchResults):
     fields_param_name = 'stored_fields'
