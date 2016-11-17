35c850d8ec3867d9a2131c33ccaa02e3c97259a6
Karl Hobley <karl@torchbox.com>
2016-11-17 10:57:20 +0000

    Update URLs in install_elasticsearch5.sh

--- scripts/travis/install_elasticsearch5.sh
+++ scripts/travis/install_elasticsearch5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get autoremove --purge elasticsearch
-wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-echo "deb http://packages.elastic.co/elasticsearch/5.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elk.list
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
 sudo apt-get update && sudo apt-get install elasticsearch -y
 sudo service elasticsearch start
