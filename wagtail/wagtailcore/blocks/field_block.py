600f317fa77d86064ffcebffeb69a63945dfecc8
Scot Hacker <shacker@birdhouse.org>
2016-11-28 14:56:26 -0800

    Omit unneeded __init__ and get_searchable_content from BlockQuoteBlock

--- wagtail/wagtailcore/blocks/field_block.py
+++ wagtail/wagtailcore/blocks/field_block.py
@@ -141,12 +141,6 @@ class TextBlock(FieldBlock):
 
 class BlockQuoteBlock(TextBlock):
 
-    def __init__(self, required=True, help_text=None, max_length=None, min_length=None, **kwargs):
-        super(BlockQuoteBlock, self).__init__(**kwargs)
-
-    def get_searchable_content(self, value):
-        return [force_text(value)]
-
     def render_basic(self, value, context=None):
         if value:
             return format_html('<blockquote>{0}</blockquote>', value)
