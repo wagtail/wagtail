7a0d66b06d214ed1aaf25d36b40443046870096d
MattRijk <MattRijk@users.noreply.github.com>
2016-12-07 16:12:30 -0500

    fixes issue 3202 Updated setup.py removed from installed_requires to doc extras

--- setup.py
+++ setup.py
@@ -31,9 +31,7 @@ install_requires = [
     "html5lib>=0.999,<1",
     "Unidecode>=0.04.14",
     "Willow>=0.4,<0.5",
-    "requests>=2.11.1,<3.0",
-    "pyenchant==1.6.6",
-    "sphinxcontrib-spelling>=2.3.0", 
+    "requests>=2.11.1,<3.0", 
 ]
 
 # Testing dependencies
@@ -55,6 +53,8 @@ testing_extras = [
 
 # Documentation dependencies
 documentation_extras = [
+    'pyenchant==1.6.6',
+    'sphinxcontrib-spelling>=2.3.0',
     'Sphinx>=1.3.1',
     'sphinx-autobuild>=0.5.2',
     'sphinx_rtd_theme>=0.1.8',
