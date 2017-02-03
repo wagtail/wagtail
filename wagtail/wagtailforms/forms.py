129cac46088a21d524f61043e4d8d20fa802fe20
Ross Crawford-d'Heureuse <r.crawford@douglas.de>
2017-02-03 13:14:35 +0100

    Added hiddenfield

--- wagtail/wagtailforms/forms.py
+++ wagtail/wagtailforms/forms.py
@@ -69,6 +69,9 @@ class FormBuilder(object):
     def create_checkbox_field(self, field, options):
         return django.forms.BooleanField(**options)
 
+    def create_hidden_field(self, field, options):
+        return django.forms.CharField(widget=django.forms.HiddenInput, **options)
+
     FIELD_TYPES = {
         'singleline': create_singleline_field,
         'multiline': create_multiline_field,
@@ -81,6 +84,7 @@ class FormBuilder(object):
         'radio': create_radio_field,
         'checkboxes': create_checkboxes_field,
         'checkbox': create_checkbox_field,
+        'hiddenfield': create_hidden_field,
     }
 
     @property
