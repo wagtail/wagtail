367b32afd798f6267ac77437fa89b64086a8e692
Karl Hobley <karl@torchbox.com>
2017-02-10 15:24:29 +0000

    Rename project_template homepage to "Home"

--- wagtail/project_template/home/migrations/0002_create_homepage.py
+++ wagtail/project_template/home/migrations/0002_create_homepage.py
@@ -21,7 +21,7 @@ def create_homepage(apps, schema_editor):
 
     # Create a new homepage
     homepage = HomePage.objects.create(
-        title="Homepage",
+        title="Home",
         slug='home',
         content_type=homepage_content_type,
         path='00010001',
