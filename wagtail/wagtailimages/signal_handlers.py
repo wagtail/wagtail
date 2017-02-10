361df2035ef8be08ceb75d24afa745207d2e4d6e
Mike Dingjan <m.dingjan@lukkien.com>
2017-02-10 17:06:08 +0100

    Implement ``get_image_model`` for image signals

--- wagtail/wagtailimages/signal_handlers.py
+++ wagtail/wagtailimages/signal_handlers.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 from django.conf import settings
 from django.db.models.signals import post_delete, pre_save
 
-from wagtail.wagtailimages.models import Image, Rendition
+from wagtail.wagtailimages import get_image_model
 
 
 def post_delete_file_cleanup(instance, **kwargs):
@@ -20,6 +20,9 @@ def pre_save_image_feature_detection(instance, **kwargs):
 
 
 def register_signal_handlers():
+    Image = get_image_model()
+    Rendition = Image.get_rendition_model()
+
     pre_save.connect(pre_save_image_feature_detection, sender=Image)
     post_delete.connect(post_delete_file_cleanup, sender=Image)
     post_delete.connect(post_delete_file_cleanup, sender=Rendition)
