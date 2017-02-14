c162ec3e51e857d22c35aa13ff518f6d3ab0dd54
Emlyn Clay <eclay101@gmail.com>
2017-02-14 20:04:51 +0000

    Use movenodeform form in the PageAdmin changelist
    Treebeard comes with a useful tree-editing form widget, let's use that rather than a less useful MPTT node position text boxes.

--- wagtail/wagtailcore/admin.py
+++ wagtail/wagtailcore/admin.py
@@ -4,10 +4,18 @@ from django.contrib import admin
 from django.contrib.auth.admin import GroupAdmin
 from django.contrib.auth.models import Group
 
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
+
 from wagtail.wagtailcore.models import GroupPagePermission, Page, Site
 
 admin.site.register(Site)
-admin.site.register(Page)
+
+# Use the Treebeard treeadmin.
+class PageAdmin(TreeAdmin):
+    form = movenodeform_factory(Page)
+
+admin.site.register(Page, PageAdmin)
 
 
 # Extend GroupAdmin to include page permissions as an inline
