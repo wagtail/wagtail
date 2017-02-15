24b0d2bbd2100863a2c8bf05709a3082d64a185e
Emlyn Clay <eclay101@gmail.com>
2017-02-15 15:08:53 +0000

    Check if treebeard is in the the INSTALLED_APPS
    Check if treebeard is in the the INSTALLED_APPS else just use the vanilla django changelist

--- wagtail/wagtailcore/admin.py
+++ wagtail/wagtailcore/admin.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 from django.contrib import admin
 from django.contrib.auth.admin import GroupAdmin
 from django.contrib.auth.models import Group
+from django.apps import apps
 
 from treebeard.admin import TreeAdmin
 from treebeard.forms import movenodeform_factory
@@ -12,12 +13,15 @@ from wagtail.wagtailcore.models import GroupPagePermission, Page, Site
 admin.site.register(Site)
 
 
-# Use the Treebeard treeadmin.
-class PageAdmin(TreeAdmin):
-    form = movenodeform_factory(Page)
+# Use the Treebeard treeadmin form widget if installed
+# else just register the model with the vanilla changelist
+if apps.is_installed('treebeard'):
+    class PageAdmin(TreeAdmin):
+        form = movenodeform_factory(Page)
 
-
-admin.site.register(Page, PageAdmin)
+    admin.site.register(Page, PageAdmin)
+else:
+    admin.site.register(Page)
 
 
 # Extend GroupAdmin to include page permissions as an inline
