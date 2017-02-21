b69bf0e9bb5c1f8dceb2a12e03f809c10a5b21ae
David Ray <davidray@gmail.com>
2017-02-21 08:46:21 -0500

    Updating Sphinx and other doc dep versions (#3386)
    * Updating Sphinx and other doc dep versions
    * Don't bother to install docs dependencies on Drone

--- setup.py
+++ setup.py
@@ -53,11 +53,11 @@ testing_extras = [
 
 # Documentation dependencies
 documentation_extras = [
-    'pyenchant==1.6.6',
+    'pyenchant==1.6.8',
     'sphinxcontrib-spelling>=2.3.0',
-    'Sphinx>=1.3.1',
-    'sphinx-autobuild>=0.5.2',
-    'sphinx_rtd_theme>=0.1.8',
+    'Sphinx>=1.5.2',
+    'sphinx-autobuild>=0.6.0',
+    'sphinx_rtd_theme>=0.1.9',
 ]
 
 setup(
