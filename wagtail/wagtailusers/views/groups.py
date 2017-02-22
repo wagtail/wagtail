6fb2876aec574a4ab788707193d36863e31df430
tholliday <tholliday@morristechnology.com>
2017-02-22 17:44:28 -0500

    Created collection management forms for the group admin, so that the management of collections can be controlled per group.

--- wagtail/wagtailusers/views/groups.py
+++ wagtail/wagtailusers/views/groups.py
@@ -11,7 +11,7 @@ from wagtail.wagtailadmin import messages
 from wagtail.wagtailadmin.forms import SearchForm
 from wagtail.wagtailadmin.utils import any_permission_required, permission_required
 from wagtail.wagtailcore import hooks
-from wagtail.wagtailusers.forms import GroupForm, GroupPagePermissionFormSet
+from wagtail.wagtailusers.forms import GroupForm, GroupPagePermissionFormSet, GroupCollectionPermissionFormSet
 
 _permission_panel_classes = None
 
@@ -19,7 +19,7 @@ _permission_panel_classes = None
 def get_permission_panel_classes():
     global _permission_panel_classes
     if _permission_panel_classes is None:
-        _permission_panel_classes = [GroupPagePermissionFormSet]
+        _permission_panel_classes = [GroupPagePermissionFormSet, GroupCollectionPermissionFormSet, ]
         for fn in hooks.get_hooks('register_group_permission_panel'):
             _permission_panel_classes.append(fn())
 
