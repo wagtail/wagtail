a01684125fff0e7cd6e8b1ca2fcbcfc609c727c7
tholliday <tholliday@morristechnology.com>
2017-02-24 17:16:57 -0500

    Created templates and js files to create and display the collection chooser modal.

---
+++ wagtail/wagtailadmin/static_src/wagtailadmin/js/collection-chooser.js
@@ -0,0 +1,38 @@
+function createCollectionChooser(id, openAtParentId, canChooseRoot) {
+    var chooserElement = $('#' + id + '-chooser');
+    var collectionTitle = chooserElement.find('.title');
+    var input = $('#' + id);
+    var editLink = chooserElement.find('.edit-link');
+
+    $('.action-choose', chooserElement).click(function() {
+        var initialUrl = window.chooserUrls.collectionChooser;
+        if (openAtParentId) {
+            initialUrl += openAtParentId + '/';
+        }
+
+        var urlParams = {modal: 'true'};
+        if (canChooseRoot) {
+            urlParams.can_choose_root = 'true';
+        }
+
+        ModalWorkflow({
+            url: initialUrl,
+            urlParams: urlParams,
+            responses: {
+                collectionChosen: function(collectionData) {
+                    input.val(collectionData.id);
+                    openAtParentId = collectionData.parentId;
+                    collectionTitle.text(collectionData.name);
+                    chooserElement.removeClass('blank');
+                    editLink.attr('href', collectionData.editUrl);
+                }
+            }
+        });
+    });
+
+    $('.action-clear', chooserElement).click(function() {
+        input.val('');
+        openAtParentId = null;
+        chooserElement.addClass('blank');
+    });
+}
