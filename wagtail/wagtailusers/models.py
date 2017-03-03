434f793e7a6682a7ea0954bc8ebb128010205417
Daniel Chimeno <dchimeno@gmail.com>
2017-03-03 13:18:46 +0100

    first working attempt to make gravatar optional

--- wagtail/wagtailusers/models.py
+++ wagtail/wagtailusers/models.py
@@ -4,10 +4,25 @@ from django.conf import settings
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from wagtail.wagtailusers.utils import get_gravatar_url
+
+
+def upload_avatar_to(instance, filename):
+    return "avatar_images/avatar_{}_{}".format(instance.id, filename)
 
 
 @python_2_unicode_compatible
 class UserProfile(models.Model):
+    DEFAULT = 'Default'
+    CUSTOM = 'Custom'
+    GRAVATAR = 'Gravatar'
+    AVATAR_CHOICES = (
+        (DEFAULT, _('Default')),
+        (CUSTOM, _('Custom')),
+        (GRAVATAR, 'Gravatar')
+    )
+
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='wagtail_userprofile'
     )
@@ -30,12 +45,39 @@ class UserProfile(models.Model):
         help_text=_("Receive notification when your page edit is rejected")
     )
 
+    avatar_choice = models.CharField(
+        verbose_name=_('Select avatar backend'),
+        default=DEFAULT,
+        choices=AVATAR_CHOICES,
+        max_length=10
+    )
+
+    avatar = models.ImageField(
+        verbose_name=_('Upload your custom avatar'),
+        upload_to=upload_avatar_to,
+        blank=True,
+    )
+
     @classmethod
     def get_for_user(cls, user):
         return cls.objects.get_or_create(user=user)[0]
 
     def __str__(self):
         return self.user.get_username()
+
+    def get_avatar_url(self):
+        if self.avatar_choice == self.DEFAULT:
+            return static('wagtailadmin/images/default-user-avatar.svg')
+        elif self.avatar_choice == self.CUSTOM:
+            try:
+                return self.avatar.url
+            except ValueError:
+                return static('wagtailadmin/images/default-user-avatar.svg')
+        else:
+            if self.user.email:
+                return get_gravatar_url(self.user.email, default=None, size=50)
+            else:
+                return static('wagtailadmin/images/default-user-avatar.svg')
 
     class Meta:
         verbose_name = _('user profile')
--- wagtail/wagtailusers/utils.py
+++ wagtail/wagtailusers/utils.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 
 from wagtail.wagtailcore.compat import AUTH_USER_APP_LABEL, AUTH_USER_MODEL_NAME
-
+import hashlib
+from django.utils.six.moves.urllib.parse import urlencode
 
 delete_user_perm = "{0}.delete_{1}".format(AUTH_USER_APP_LABEL, AUTH_USER_MODEL_NAME.lower())
 
@@ -19,3 +20,10 @@ def user_can_delete_user(current_user, user_to_delete):
         return False
 
     return True
+
+
+def get_gravatar_url(email, default, size=50):
+    gravatar_url = "https://www.gravatar.com/avatar/" + hashlib.md5(email.lower().encode('utf-8')).hexdigest() + "?"
+    gravatar_url += urlencode({'d': default, 's': str(size)})
+    return gravatar_url
+
