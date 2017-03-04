856e1f45162cd0b4a100e56bcf542f9d3b8d329b
Daniel Chimeno <dchimeno@gmail.com>
2017-03-04 15:05:59 +0100

    make user required in tag

--- wagtail/wagtailadmin/templatetags/user_avatar.py
+++ wagtail/wagtailadmin/templatetags/user_avatar.py
@@ -8,11 +8,11 @@ def user_avatar_url(context, *args, **kwargs):
     """
     A template tag that receives a user an size and return
     the appropiate avatar for that user.
-    If user is not passed, the user in the request is used.
     Usage: {% avatar_url user=request.user size=50 %}
     """
-    user = getattr(kwargs, 'user', context['user'])
-    size = kwargs['size']
+
+    user = kwargs.get('user')
+    size = kwargs.get('size')
 
     return user.wagtail_userprofile.get_avatar_url(size=size)
 
