6b58ec6af0c65058fa834756db820592824e491f
Gordon Pendleton <gordon@internal.gandssecurityincorporated.com>
2017-03-13 18:44:39 -0400

    add field that requires `get_db_prep_value` for query lookup values
    references
    https://github.com/django/django/blob/d6eaf7c0183cd04b78f2a55e1d60bb7e59598310/tests/custom_pk/fields.py
    
    https://github.com/wagtail/wagtail/pull/3448

---
+++ wagtail/tests/customuser/fields.py
@@ -0,0 +1,67 @@
+import random
+
+from django.db import models
+from django.utils.encoding import force_text
+
+LOWER_BOUND = -2147483648
+UPPER_BOUND =  2147483647
+SHIFT     = 92147483647
+
+class ConvertedValue:
+    def __init__(self, value):
+        value = int(value)
+        if UPPER_BOUND < value:
+            self.display_value = value
+            self.db_value = value - SHIFT
+        else:
+            self.db_value = value
+            self.display_value = value + SHIFT
+
+    def __repr__(self):
+        return "<%s: %s>" % (self.__class__.__name__, self.db_value)
+
+    def __str__(self):
+        return force_text(self.display_value)
+    
+    def __int__(self):
+        return self.display_value
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.db_value == other.db_value
+        return self.db_value == other
+
+
+class ConvertedValueField(models.IntegerField):
+    '''
+        Roughly copied from https://github.com/django/django/blob/d6eaf7c0183cd04b78f2a55e1d60bb7e59598310/tests/custom_pk/fields.py
+    '''
+
+    def pre_save(self, instance, add):
+        value = getattr(instance, self.attname, None)
+        if not value:
+            value = ConvertedValue(random.randint(LOWER_BOUND, UPPER_BOUND))
+            setattr(instance, self.attname, value)
+        return value
+
+    def to_python(self, value):
+        if not value:
+            return
+        if not isinstance(value, ConvertedValue):
+            value = ConvertedValue(value)
+        return value
+
+    def from_db_value(self, value, expression, connection, context):
+        if not value:
+            return
+        return ConvertedValue(value)
+
+    def get_db_prep_save(self, value, connection):
+        if not value:
+            return
+        return ConvertedValue(value).db_value
+
+    def get_db_prep_value(self, value, connection, prepared=False):
+        if not value:
+            return
+        return ConvertedValue(value).db_value
