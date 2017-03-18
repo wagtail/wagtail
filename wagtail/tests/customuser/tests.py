2246d0502ad38405448975e89d6552b0074e62ef
Gordon Pendleton <gordon@internal.gandssecurityincorporated.com>
2017-03-18 12:49:47 -0400

    verify the json string uses the display value and not the db value

--- wagtail/tests/customuser/tests.py
+++ wagtail/tests/customuser/tests.py
@@ -14,11 +14,11 @@ class TestConvertedValueField(TestCase, WagtailTestUtils):
         
         User = self.user.__class__
         self.pk_field = User._meta.get_field(User._meta.pk.name)
+        self.pk_db_value = self.pk_field.get_db_prep_value(self.user.pk, connections['default'])
     
     def test_db_value_is_different(self):
-        db_value = self.pk_field.get_db_prep_value(self.user.pk, connections['default'])
-        self.assertEqual(self.user.pk, db_value)
-        self.assertNotEqual(text_type(self.user.pk), text_type(db_value))
+        self.assertEqual(self.user.pk, self.pk_db_value)
+        self.assertNotEqual(text_type(self.user.pk), text_type(self.pk_db_value))
     
     def test_custom_user_primary_key_is_hashable(self):
         hash(self.user.pk)
@@ -26,6 +26,9 @@ class TestConvertedValueField(TestCase, WagtailTestUtils):
     def test_custom_user_primary_key_is_jsonable(self):
         json_str = json.dumps({'pk': self.user.pk}, separators=(',',':'))
         self.assertEqual(json_str, '{"pk":"%s"}' % self.user.pk)
+        
+        # verify the json string uses the display value and not the db value
+        self.assertNotEqual(json_str, '{"pk":"%s"}' % self.pk_db_value)
     
     def test_custom_user_primary_key(self):
         self.assertIsInstance(self.user.pk, ConvertedValue)
