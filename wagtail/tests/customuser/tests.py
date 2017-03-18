fb2b808d83602c8bff05109468de09a3ff06fe9b
Gordon Pendleton <gordon@internal.gandssecurityincorporated.com>
2017-03-18 12:12:01 -0400

    test converted value is hashable and jsonable

--- wagtail/tests/customuser/tests.py
+++ wagtail/tests/customuser/tests.py
@@ -1,3 +1,5 @@
+import json
+
 from django.test import TestCase
 from wagtail.tests.utils import WagtailTestUtils
 
@@ -7,6 +9,13 @@ from .fields import ConvertedValue, ConvertedValueField
 class TestConvertedValueField(TestCase, WagtailTestUtils):
     def setUp(self):
         self.user = self.login()
+    
+    def test_custom_user_primary_key_is_hashable(self):
+        hash(self.user.pk)
+    
+    def test_custom_user_primary_key_is_jsonable(self):
+        json_str = json.dumps({'pk': self.user.pk}, separators=(',',':'))
+        self.assertEqual(json_str, '{"pk":"%s"}' % self.user.pk)
     
     def test_custom_user_primary_key(self):
         self.assertIsInstance(self.user.pk, ConvertedValue)
