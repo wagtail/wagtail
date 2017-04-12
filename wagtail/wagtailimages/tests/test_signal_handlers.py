0e0a43ff4401debd873d02e9aa8f1dc6a796a42c
Gordon Pendleton <gordon@internal.gandssecurityincorporated.com>
2017-04-12 12:53:49 -0400

    remove redundant tests

--- wagtail/wagtailimages/tests/test_signal_handlers.py
+++ wagtail/wagtailimages/tests/test_signal_handlers.py
@@ -7,36 +7,6 @@ from wagtail.wagtailimages import get_image_model
 from wagtail.wagtailimages.tests.utils import get_test_image_file
 
 
-class TestFilesNotDeletedForDefaultModels(TestCase):
-    '''
-    Checking that the files are not deleted in this test case means that the
-    delete signal handler is waiting for on_commit per the following documentation:
-    
-        Django's TestCase class wraps each test in a transaction and rolls back that
-        transaction after each test, in order to provide test isolation. This means
-        that no transaction is ever actually committed, thus your on_commit()
-        callbacks will never be run. If you need to test the results of an
-        on_commit() callback, use a TransactionTestCase instead.
-        https://docs.djangoproject.com/en/1.10/topics/db/transactions/#use-in-tests
-    '''
-    
-    def setUp(self):
-        self.image = get_image_model().objects.create(title="Test Image", file=get_test_image_file())
-        self.rendition = self.image.get_rendition('original')
-
-    def test_image_file_not_deleted(self):
-        file_name = self.image.file.name
-        self.assertTrue(self.image.file.storage.exists(file_name))
-        self.image.delete()
-        self.assertTrue(self.image.file.storage.exists(file_name))
-    
-    def test_rendition_file_not_deleted(self):
-        file_name = self.rendition.file.name
-        self.assertTrue(self.rendition.file.storage.exists(file_name))
-        self.rendition.delete()
-        self.assertTrue(self.rendition.file.storage.exists(file_name))
-
-
 class TestFilesDeletedForDefaultModels(TransactionTestCase):
     '''
     Because we expect file deletion to only happen once a transaction is
