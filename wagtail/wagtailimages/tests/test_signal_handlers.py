f11c6e476e8c10f328f0dc37929abe47e147f32d
Gordon Pendleton <gordon@internal.gandssecurityincorporated.com>
2017-04-12 09:22:05 -0400

    test for rendition file delete too

--- wagtail/wagtailimages/tests/test_signal_handlers.py
+++ wagtail/wagtailimages/tests/test_signal_handlers.py
@@ -8,24 +8,34 @@ from wagtail.wagtailimages import get_image_model, signal_handlers
 from wagtail.wagtailimages.tests.utils import get_test_image_file
 
 
-class TestSignalHandlers(TestCase):
+class TestDefaultModelSignalHandlers(TestCase):
 
-    def test_image_signal_handlers(self):
+    def test_image_file_delete_signal_handler(self):
         image = get_image_model().objects.create(title="Test Image", file=get_test_image_file())
         image_path = image.file.path
+        
+        self.assertTrue(os.path.exists(image_path))
+        
         image.delete()
 
         self.assertFalse(os.path.exists(image_path))
+    
+    def test_rendition_file_delete_signal_handler(self):
+        image = get_image_model().objects.create(title="Test Image", file=get_test_image_file())
+        rendition = image.get_rendition('original')
+        rendition_path = rendition.file.path
+        
+        self.assertTrue(os.path.exists(rendition_path))
+        
+        rendition.delete()
 
-    @override_settings(WAGTAILIMAGES_IMAGE_MODEL='tests.CustomImage')
-    def test_custom_image_signal_handlers(self):
+        self.assertFalse(os.path.exists(rendition_path))
+
+
+@override_settings(WAGTAILIMAGES_IMAGE_MODEL='tests.CustomImage')
+class TestCustomModelSignalHandlers(TestDefaultModelSignalHandlers):
+    def setUp(self):
         #: Sadly signal receivers only get connected when starting django.
         #: We will re-attach them here to mimic the django startup behavior
         #: and get the signals connected to our custom model..
         signal_handlers.register_signal_handlers()
-
-        image = get_image_model().objects.create(title="Test CustomImage", file=get_test_image_file())
-        image_path = image.file.path
-        image.delete()
-
-        self.assertFalse(os.path.exists(image_path))
