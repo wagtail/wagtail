23ffd1ce481ec10de745ac05e8e6a95efb6ad70c
Bertrand Bordage <bordage.bertrand@gmail.com>
2017-04-28 14:12:06 +0200

    [Postgres search] Improves and tests weight calculus.

--- wagtail/contrib/postgres_search/tests/test_backend.py
+++ wagtail/contrib/postgres_search/tests/test_backend.py
@@ -7,6 +7,8 @@ from django.utils.six import StringIO
 from wagtail.tests.search.models import SearchTest
 from wagtail.wagtailsearch.tests.test_backends import BackendTests
 
+from ..utils import BOOSTS_WEIGHTS, WEIGHTS_VALUES, determine_boosts_weights, get_weight
+
 
 class TestPostgresSearchBackend(BackendTests, TestCase):
     backend_path = 'wagtail.contrib.postgres_search.backend'
@@ -40,3 +42,31 @@ class TestPostgresSearchBackend(BackendTests, TestCase):
         results = self.backend.search('world', SearchTest)
         self.assertSetEqual(set(results), {self.testa,
                                            self.testd.searchtest_ptr})
+
+    def test_weights(self):
+        self.assertListEqual(BOOSTS_WEIGHTS,
+                             [(10, 'A'), (2, 'B'), (0, 'C'), (0, 'D')])
+        self.assertListEqual(WEIGHTS_VALUES, [0, 0, 0.2, 1.0])
+
+        self.assertEqual(get_weight(15), 'A')
+        self.assertEqual(get_weight(10), 'A')
+        self.assertEqual(get_weight(9.9), 'B')
+        self.assertEqual(get_weight(2), 'B')
+        self.assertEqual(get_weight(1.9), 'C')
+        self.assertEqual(get_weight(0), 'C')
+        self.assertEqual(get_weight(-1), 'D')
+
+        self.assertListEqual(determine_boosts_weights([1]),
+                             [(1, 'A'), (0, 'B'), (0, 'C'), (0, 'D')])
+        self.assertListEqual(determine_boosts_weights([-1]),
+                             [(-1, 'A'), (-1, 'B'), (-1, 'C'), (-1, 'D')])
+        self.assertListEqual(determine_boosts_weights([-1, 1, 2]),
+                             [(2, 'A'), (1, 'B'), (-1, 'C'), (-1, 'D')])
+        self.assertListEqual(determine_boosts_weights([0, 1, 2, 3]),
+                             [(3, 'A'), (2, 'B'), (1, 'C'), (0, 'D')])
+        self.assertListEqual(determine_boosts_weights([0, 0.25, 0.75, 1, 1.5]),
+                             [(1.5, 'A'), (1, 'B'), (0.5, 'C'), (0, 'D')])
+        self.assertListEqual(determine_boosts_weights([0, 1, 2, 3, 4, 5, 6]),
+                             [(6, 'A'), (4, 'B'), (2, 'C'), (0, 'D')])
+        self.assertListEqual(determine_boosts_weights([-2, -1, 0, 1, 2, 3, 4]),
+                             [(4, 'A'), (2, 'B'), (0, 'C'), (-2, 'D')])
