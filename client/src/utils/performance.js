463e1861128333be6ffb3541fece6bfa4a792459
Thibaud Colas <thibaudcolas@gmail.com>
2017-06-07 22:12:04 +0300

    Add tooling for performance measurement of React components

--- client/src/utils/performance.js
+++ client/src/utils/performance.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+
 /* eslint-disable import/no-mutable-exports */
 let perfMiddleware;
 
@@ -28,6 +30,57 @@ if (process.env.NODE_ENV !== 'production') {
   };
 }
 
+let perfComponent;
+
+if (process.env.NODE_ENV !== 'production') {
+  /**
+   * Wraps the passed in `Component` in a higher-order component. It can then
+   * measure the performance of every render of the `Component`.
+   *
+   * Can also be used as an ES2016 decorator.
+   * @param  {ReactComponent} Component the component to wrap
+   * @return {ReactComponent}           the wrapped component
+   * See https://github.com/sheepsteak/react-perf-component
+   */
+  perfComponent = (Target) => {
+    if (process.env.NODE_ENV === 'production') {
+      return Target;
+    }
+
+    // eslint-disable-next-line global-require
+    const ReactPerf = require('react-addons-perf');
+
+    class Perf extends Component {
+      componentDidMount() {
+        ReactPerf.start();
+      }
+
+      componentDidUpdate() {
+        ReactPerf.stop();
+
+        const measurements = ReactPerf.getLastMeasurements();
+
+        ReactPerf.printWasted(measurements);
+        ReactPerf.start();
+      }
+
+      componentWillUnmount() {
+        ReactPerf.stop();
+      }
+
+      render() {
+        return <Target {...this.props} />;
+      }
+    }
+
+    Perf.displayName = `perf(${Target.displayName || Target.name || 'Component'})`;
+    Perf.WrappedComponent = Target;
+
+    return Perf;
+  };
+}
+
 export {
-  perfMiddleware
+  perfMiddleware,
+  perfComponent,
 };
