3bf4e10902b132682201c55edfc2a39437587ec4
Thibaud Colas <thibaudcolas@gmail.com>
2017-06-07 16:21:12 +0300

    Add best practice node modules aliasing

--- client/webpack/base.config.js
+++ client/webpack/base.config.js
@@ -61,5 +61,12 @@ module.exports = function exports() {
       // Add webpack version information
       version: false,
     },
+    // Some libraries import Node modules but don't use them in the browser.
+    // Tell Webpack to provide empty mocks for them so importing them works.
+    node: {
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty',
+    },
   };
 };
