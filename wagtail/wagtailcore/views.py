60f40f10eeeb95443dea9d964660dc477c0bd8ea
Matt Westcott <matt@west.co.tt>
2017-06-07 23:48:06 +0100

    Move 'mark view restriction as passed' logic to the BaseViewRestriction model

--- wagtail/wagtailcore/views.py
+++ wagtail/wagtailcore/views.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.http import Http404, HttpResponse
 from django.shortcuts import get_object_or_404, redirect
@@ -38,15 +37,7 @@ def authenticate_with_password(request, page_view_restriction_id, page_id):
     if request.method == 'POST':
         form = PasswordPageViewRestrictionForm(request.POST, instance=restriction)
         if form.is_valid():
-            has_existing_session = (settings.SESSION_COOKIE_NAME in request.COOKIES)
-            passed_restrictions = request.session.setdefault('passed_page_view_restrictions', [])
-            if restriction.id not in passed_restrictions:
-                passed_restrictions.append(restriction.id)
-                request.session['passed_page_view_restrictions'] = passed_restrictions
-            if not has_existing_session:
-                # if this is a session we've created, set it to expire at the end
-                # of the browser session
-                request.session.set_expiry(0)
+            restriction.mark_as_passed(request)
 
             return redirect(form.cleaned_data['return_url'])
     else:
