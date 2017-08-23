44cd99f0929924896a25de6adfc74f3736e8f252
LB <mail@lb.ee>
2017-08-23 17:20:01 +0200

    rework delete function to handle cases of multiple snippets

--- wagtail/snippets/urls.py
+++ wagtail/snippets/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     url(r'^(\w+)/(\w+)/$', snippets.list, name='list'),
     url(r'^(\w+)/(\w+)/add/$', snippets.create, name='add'),
     url(r'^(\w+)/(\w+)/([^/]+?)/$', snippets.edit, name='edit'),
+    url(r'^(\w+)/(\w+)/multiple/delete/$', snippets.delete, name='delete-multiple'),
     url(r'^(\w+)/(\w+)/([^/]+?)/delete/$', snippets.delete, name='delete'),
     url(r'^(\w+)/(\w+)/([^/]+?)/usage/$', snippets.usage, name='usage'),
 ]
