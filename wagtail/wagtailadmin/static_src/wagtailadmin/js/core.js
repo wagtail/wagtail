c148cc5caa482e16acc839969c974d0abce8af9d
Jack P <jacp3@outlook.com>
2017-08-30 11:48:50 +0100

    Update core.js

--- wagtail/wagtailadmin/static_src/wagtailadmin/js/core.js
+++ wagtail/wagtailadmin/static_src/wagtailadmin/js/core.js
@@ -173,6 +173,10 @@ $(function() {
                 'background-color': '#0f2828',
                 'border': '2px solid #000'
             });
+
+            $('.listing').css({
+                'color': '#000'
+            });
         };
     };
     applyHighContrastMode();
