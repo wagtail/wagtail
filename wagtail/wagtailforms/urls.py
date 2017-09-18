5de889a23ad7cce87f6900feda9fdd0c55229d8a
Johan Arensman <johan@bdrck.nl>
2017-09-18 21:17:12 +0200

    Converted the `index`, `delete_submissions` and `list_submissions` function views to proper class based views. Extracted several getters and setters to allow easy extending and overrides.

--- wagtail/wagtailforms/urls.py
+++ wagtail/wagtailforms/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import url
 from wagtail.wagtailforms import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^submissions/(\d+)/$', views.list_submissions, name='list_submissions'),
-    url(r'^submissions/(\d+)/delete/$', views.delete_submissions, name='delete_submissions')
+    url(r'^$', views.ListFormPages.as_view(), name='index'),
+    url(r'^submissions/(?P<page_id>\d+)/$', views.ListSubmissions.as_view(), name='list_submissions'),
+    url(r'^submissions/(?P<page_id>\d+)/delete/$', views.DeleteSubmissions.as_view(), name='delete_submissions')
 ]
