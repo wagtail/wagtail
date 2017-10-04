f8bbd9b9b53d6d55fc98757df08ea723e701f3aa
Matt Westcott <matt@west.co.tt>
2017-10-04 15:59:58 +0100

    Ensure example.doc is deleted on TestServeView teardown

--- wagtail/wagtaildocs/tests/test_views.py
+++ wagtail/wagtaildocs/tests/test_views.py
@@ -77,7 +77,9 @@ class TestServeView(TestCase):
         self.document.file.save('example.doc', ContentFile("A boring example document"))
 
     def tearDown(self):
-        self.document.delete()
+        # delete the FieldFile directly because the TestCase does not commit
+        # transactions to trigger transaction.on_commit() in the signal handler
+        self.document.file.delete()
 
     def get(self):
         return self.client.get(reverse('wagtaildocs_serve', args=(self.document.id, self.document.filename)))
