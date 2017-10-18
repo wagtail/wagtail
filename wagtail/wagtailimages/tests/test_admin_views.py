3a19f0c9210cf242ce5a4aca1cff96b533832efb
Kees Hink <keeshink@gmail.com>
2017-10-18 16:51:37 +0100

    Test that Used N times is shown on delete page if WAGTAIL_USAGE_COUNT_ENABLED

--- wagtail/wagtailimages/tests/test_admin_views.py
+++ wagtail/wagtailimages/tests/test_admin_views.py
@@ -488,6 +488,13 @@ class TestImageDeleteView(TestCase, WagtailTestUtils):
         self.assertTemplateUsed(response, 'wagtailimages/images/confirm_delete.html')
         self.assertNotIn('Used ', str(response.content))
 
+    @override_settings(WAGTAIL_USAGE_COUNT_ENABLED=True)
+    def test_usage_link(self):
+        response = self.get()
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'wagtailimages/images/confirm_delete.html')
+        self.assertIn('Used 0 times', str(response.content))
+
     def test_delete(self):
         response = self.post()
 
