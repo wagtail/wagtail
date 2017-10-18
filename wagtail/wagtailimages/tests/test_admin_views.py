e48c38b5565c3b70e4fac7b8894a909ad5cc2476
Kees Hink <keeshink@gmail.com>
2017-10-18 16:51:05 +0100

    Adapt existing test, set WAGTAIL_USAGE_COUNT_ENABLED=False explicitly

--- wagtail/wagtailimages/tests/test_admin_views.py
+++ wagtail/wagtailimages/tests/test_admin_views.py
@@ -481,10 +481,12 @@ class TestImageDeleteView(TestCase, WagtailTestUtils):
     def post(self, post_data={}):
         return self.client.post(reverse('wagtailimages:delete', args=(self.image.id,)), post_data)
 
+    @override_settings(WAGTAIL_USAGE_COUNT_ENABLED=False)
     def test_simple(self):
         response = self.get()
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'wagtailimages/images/confirm_delete.html')
+        self.assertNotIn('Used ', str(response.content))
 
     def test_delete(self):
         response = self.post()
