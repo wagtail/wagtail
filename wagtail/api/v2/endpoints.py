90153e31c8da9ff285ce9372692a6328b68d0c9d
Karl Hobley <karl@kaed.uk>
2017-10-26 09:51:08 +0100

    Fix AttributeError in listing_view endpoint
    Fixes #3967
    
    If there was no site configured, the listing views in the API would
    break with an AttributeError.
    
    Now, the following happens if there is no site record:
     - In the Admin API, all pages are returned but links in the response do
    not include a domain name
     - In the Public API, no pages are returned

--- wagtail/api/v2/endpoints.py
+++ wagtail/api/v2/endpoints.py
@@ -403,7 +403,11 @@ class PagesAPIEndpoint(BaseAPIEndpoint):
         queryset = queryset.public().live()
 
         # Filter by site
-        queryset = queryset.descendant_of(request.site.root_page, inclusive=True)
+        if request.site:
+            queryset = queryset.descendant_of(request.site.root_page, inclusive=True)
+        else:
+            # No sites configured
+            queryset = queryset.none()
 
         return queryset
 
--- wagtail/api/v2/utils.py
+++ wagtail/api/v2/utils.py
@@ -12,7 +12,7 @@ class BadRequestError(Exception):
 
 
 def get_base_url(request=None):
-    base_url = getattr(settings, 'WAGTAILAPI_BASE_URL', request.site.root_url if request else None)
+    base_url = getattr(settings, 'WAGTAILAPI_BASE_URL', request.site.root_url if request and request.site else None)
 
     if base_url:
         # We only want the scheme and netloc
