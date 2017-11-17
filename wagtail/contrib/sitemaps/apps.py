58ea7f824acc808466dfa34b1ace0ccae3ea0d45
Karl Hobley <karl@kaed.uk>
2017-11-17 11:50:05 +0000

    Renamed wagtail.contrib.wagtailsitemaps to wagtail.contrib.sitemaps

--- wagtail/contrib/sitemaps/apps.py
+++ wagtail/contrib/sitemaps/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 
 
 class WagtailSitemapsAppConfig(AppConfig):
-    name = 'wagtail.contrib.wagtailsitemaps'
+    name = 'wagtail.contrib.sitemaps'
     label = 'wagtailsitemaps'
     verbose_name = "Wagtail sitemaps"
