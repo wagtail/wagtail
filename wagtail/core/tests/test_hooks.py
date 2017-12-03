496dc07e30014801b37f2cb7267b4ec12e5e5a99
Balazs Endresz <balazs.endresz@gmail.com>
2017-12-03 11:04:26 +0100

    fixup! Use hook_output tag via separate template include. Fixes #4080

--- wagtail/core/tests/test_hooks.py
+++ wagtail/core/tests/test_hooks.py
@@ -1,7 +1,17 @@
-from django.test import TestCase
+from copy import deepcopy
 
+from django.conf import settings
+from django.test import TestCase, override_settings
+from django.urls import reverse
+
+from wagtail.tests.testapp.models import SimplePage
 from wagtail.tests.utils import WagtailTestUtils
 from wagtail.core import hooks
+from wagtail.core.models import Site
+
+
+TEMPLATES_DEBUG_FALSE = deepcopy(settings.TEMPLATES)
+TEMPLATES_DEBUG_FALSE[0]['OPTIONS']['debug'] = False
 
 
 def test_hook():
@@ -10,6 +20,12 @@ def test_hook():
 
 class TestLoginView(TestCase, WagtailTestUtils):
     fixtures = ['test.json']
+
+    def setUp(self):
+        super().setUp()
+        site = Site.objects.get(is_default_site=True)
+        self.root = site.root_page.specific
+        self.login()
 
     @classmethod
     def setUpClass(cls):
@@ -34,3 +50,35 @@ class TestLoginView(TestCase, WagtailTestUtils):
         with self.register_hook('test_hook_name', after_hook, order=1):
             hook_fns = hooks.get_hooks('test_hook_name')
             self.assertEqual(hook_fns, [test_hook, after_hook])
+
+
+    @override_settings(DEBUG=False, TEMPLATES=TEMPLATES_DEBUG_FALSE)
+    def test_exceptions_limited_to_hooks(self):
+        """
+        Test behaviour of hooks when one of them raises an exception.
+        This test will be no longer necessary starting with Django 2.1.
+        See https://github.com/wagtail/wagtail/issues/4080
+        """
+
+        def hook_raising_exception():
+            raise Exception
+
+        with self.register_hook('insert_editor_js', test_hook):
+            with self.register_hook('insert_editor_js', hook_raising_exception):
+                # hook_output.html is used for both insert_editor_css and insert_editor_js
+                with self.assertTemplateUsed('wagtailadmin/shared/hook_output.html', count=2):
+                    add_url = reverse('wagtailadmin_pages:add', args=[
+                        SimplePage._meta.app_label, SimplePage._meta.model_name, self.root.pk
+                    ])
+                    response = self.client.get(add_url)
+
+                    # the rest of the _editor_js.html include is still rendered
+                    self.assertContains(response, 'page-editor.js')
+
+                    # the insert_editor_css hook works too
+                    self.assertContains(response, '<!-- hook_output insert_editor_css -->')
+
+                    # The insert_editor_js hook raised an exception,
+                    # so its hook_output.html template doesn't get rendered at all.
+                    # In fact, none of the insert_editor_js hooks will be rendered.
+                    self.assertNotContains(response, '<!-- hook_output insert_editor_js -->')
