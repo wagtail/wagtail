38c095bb632b75dea193874e8218ebc56c06a455
Trent Holliday <tholliday@morristechnology.com>
2017-12-05 13:59:43 -0500

    Added helper methods for navigating collections, i.e. building the breadcrumbs

--- wagtail/admin/navigation.py
+++ wagtail/admin/navigation.py
@@ -1,4 +1,4 @@
-from wagtail.core.models import Page
+from wagtail.core.models import Collection, Page
 
 
 def get_pages_with_direct_explore_permission(user):
@@ -13,6 +13,18 @@ def get_pages_with_direct_explore_permission(user):
         )
 
 
+def get_collections_with_direct_explore_permission(user):
+    # Get all collections that the user has permissions for
+    if user.is_superuser:
+        # superuser has implicit permission on the root node
+        return Collection.objects.filter(depth=1)
+    else:
+        return Collection.objects.filter(
+            group_manage_permissions__group__in=user.groups.all(),
+            group_manage_permissions__permission_type__in=['add', 'edit']
+        ).distinct()
+
+
 def get_explorable_root_page(user):
     # Get the highest common explorable ancestor for the given user. If the user
     # has no permissions over any pages, this method will return None.
@@ -21,5 +33,18 @@ def get_explorable_root_page(user):
         return pages.first_common_ancestor(
             include_self=True,
             strict=True)
+    else:
+        return None
+
+
+def get_explorable_root_collection(user):
+    # Get the highest common explorable ancestor for the given user. If the user
+    # has no permissions over any collections, this method will return None.
+    collections = get_collections_with_direct_explore_permission(user)
+    if collections:
+        return collections.first_common_ancestor(
+            include_self=True,
+            strict=True
+        )
     else:
         return None
