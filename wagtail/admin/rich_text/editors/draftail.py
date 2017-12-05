8ce14cfb0044d90f2503a7bd940a7f6401c15db2
Thibaud Colas <thibaudcolas@gmail.com>
2017-12-05 19:51:07 +0200

    Integrate Draftail-related assets with Django widget

--- wagtail/admin/rich_text/editors/draftail.py
+++ wagtail/admin/rich_text/editors/draftail.py
@@ -1,4 +1,6 @@
-from django.forms import widgets
+import json
+
+from django.forms import Media, widgets
 
 from wagtail.admin.edit_handlers import RichTextFieldPanel
 from wagtail.admin.rich_text.converters.contentstate import ContentstateConverter
@@ -30,8 +32,20 @@ class DraftailRichTextArea(widgets.Textarea):
             translated_value = self.converter.from_database_format(value)
         return super().render(name, translated_value, attrs)
 
+    def render_js_init(self, id_, name, value):
+        return "window.draftail.initEditor('{name}', {opts})".format(
+            name=name, opts=json.dumps(self.options))
+
     def value_from_datadict(self, data, files, name):
         original_value = super().value_from_datadict(data, files, name)
         if original_value is None:
             return None
         return self.converter.to_database_format(original_value)
+
+    @property
+    def media(self):
+        return Media(js=[
+            'wagtailadmin/js/draftail.js',
+        ], css={
+            'all': ['wagtailadmin/css/panels/dratail.css']
+        })
