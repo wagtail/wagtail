4508c8c990d868e2a50c45e07f32d50d5d30c432
Bertrand Bordage <bordage.bertrand@gmail.com>
2017-12-15 12:58:35 +0100

    Fixes a test migration for SQLite.

--- wagtail/tests/testapp/migrations/0025_rename_alwaysshowinmenuspage.py
+++ wagtail/tests/testapp/migrations/0025_rename_alwaysshowinmenuspage.py
@@ -2,6 +2,8 @@ from django.db.migrations import Migration as BaseMigration, RenameModel
 
 
 class Migration(BaseMigration):
+    atomic = False
+
     dependencies = [('tests', '0024_tableblockstreampage')]
 
     operations = [RenameModel('AlwaysShowInMenusPage', 'NeverShowInMenusPage')]
