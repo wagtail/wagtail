925b0bd02197f67fbf8c6f6eb990e11b2ff6c6ad
Matt Westcott <matt@west.co.tt>
2018-01-02 23:57:47 +0000

    Reinstate error reporting on image upload from #2167

--- wagtail/images/templates/wagtailimages/chooser/chooser.js
+++ wagtail/images/templates/wagtailimages/chooser/chooser.js
@@ -76,6 +76,14 @@ function(modal) {
                 dataType: 'text',
                 success: function(response){
                     modal.loadResponseText(response);
+                },
+                error: function(response, textStatus, errorThrown) {
+                    {% trans "Server Error" as error_label %}
+                    {% trans "Report this error to your webmaster with the following information:" as error_message %}
+                    message = '{{ error_message|escapejs }}<br />' + errorThrown + ' - ' + response.status;
+                    $('#upload').append(
+                        '<div class="help-block help-critical">' +
+                        '<strong>{{ error_label|escapejs }}: </strong>' + message + '</div>');
                 }
             });
         }
