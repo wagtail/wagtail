61462401c7e4623f02c37a5b2a8995c4616748c8
Matt Westcott <matt@west.co.tt>
2018-01-03 15:08:49 +0000

    Add missing appconfs for settings, table_block and sites

---
+++ wagtail/contrib/table_block/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WagtailTableBlockAppConfig(AppConfig):
+    name = 'wagtail.contrib.table_block'
+    label = 'wagtailtableblock'
+    verbose_name = "Wagtail table block"
