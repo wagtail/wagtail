d69f231cdd895c28d2a6519fb89471fc426b1c64
Matt Westcott <matt@west.co.tt>
2018-01-09 12:28:11 +0000

    Add appconf for ModelAdmin

---
+++ wagtail/contrib/modeladmin/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WagtailModelAdminAppConfig(AppConfig):
+    name = 'wagtail.contrib.modeladmin'
+    label = 'wagtailmodeladmin'
+    verbose_name = "Wagtail ModelAdmin"
