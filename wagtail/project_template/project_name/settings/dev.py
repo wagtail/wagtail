1d22bbf814059f04566cebd0e4ec9bed66ae0105
Tom Dyson <tom@torchbox.com>
2018-01-11 15:46:20 +0000

    Allow any host in development

--- wagtail/project_template/project_name/settings/dev.py
+++ wagtail/project_template/project_name/settings/dev.py
@@ -6,6 +6,8 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '{{ secret_key }}'
 
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
