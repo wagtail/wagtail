6ad1bc420d0ccef7830499a482b3183b447b5db2
LB <mail@lb.ee>
2018-01-30 08:15:16 +1000

    clearer check name

--- wagtail/contrib/modeladmin/checks.py
+++ wagtail/contrib/modeladmin/checks.py
@@ -2,7 +2,7 @@ from django.core import checks
 
 
 @checks.register('panels')
-def modeladmin_model_check(app_configs, **kwargs):
+def modeladmin_model_panels_check(app_configs, **kwargs):
     """Checks panels configuration in all models used within modeladmin."""
     from wagtail.admin.checks import check_panels_in_model
     from .options import get_modeladmin_models
