51c7bb3b0fb013051216253afb31e721d48886e2
Sander Tuit <sandertuit@gmail.com>
2018-03-15 13:23:35 +0100

    Fix argument description for icon tag

--- wagtail/admin/templatetags/wagtailui_tags.py
+++ wagtail/admin/templatetags/wagtailui_tags.py
@@ -7,8 +7,9 @@ register = template.Library()
                         takes_context=False)
 def wagtail_icon(classnames, title='', show_label=True, decorative=True):
     """
-    Usage: {% wagtail_icon title show_label decorative %}
+    Usage: {% wagtail_icon classnames title show_label decorative %}
 
+    The css classes necessary to show the icon are provided as a first argument.
     The 'title' argument is optional, but recommended. When 'show_label' is True, the title is shown next to the icon.
     When an icon is marked decorative, it is hidden for screenreaders (a11y). If 'decorative' is False, the title is
     visible for screenreaders, regardless whether 'show_label' is True or not.
