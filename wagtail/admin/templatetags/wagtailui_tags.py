69a4de15f3dafea4d1ccb6901aacf91968486ea2
Sander Tuit <sandertuit@gmail.com>
2018-03-15 11:59:42 +0100

    Add icon template tag with accessibility options

---
+++ wagtail/admin/templatetags/wagtailui_tags.py
@@ -0,0 +1,17 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag("wagtailadmin/shared/icon.html",
+                        takes_context=False)
+def wagtail_icon(classnames, title='', show_label=True, decorative=True):
+    """
+    Usage: {% wagtail_icon title show_label decorative %}
+
+    The 'title' argument is optional, but recommended. When 'show_label' is True, the title is shown next to the icon.
+    When an icon is marked decorative, it is hidden for screenreaders (a11y). If 'decorative' is False, the title is
+    visible for screenreaders, regardless whether 'show_label' is True or not.
+
+    """
+    return {'classnames': classnames, 'title': title, 'show_label': show_label, 'decorative': decorative}
