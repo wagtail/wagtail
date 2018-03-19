dc80af4148f841b81ea8e20352185ee36bf9c6bc
Thibaud Colas <thibaudcolas@gmail.com>
2018-03-20 00:00:57 +0200

    Remove code branch that is never executed

--- client/src/components/Draftail/DraftUtils.js
+++ client/src/components/Draftail/DraftUtils.js
@@ -22,24 +22,23 @@
   * See https://github.com/jpuri/draftjs-utils/blob/e81c0ae19c3b0fdef7e0c1b70d924398956be126/js/block.js#L106.
   */
   export const getSelectionText = (editorState) => {
-    let selectedText = '';
     const selection = editorState.getSelection();
     let start = selection.getAnchorOffset();
     let end = selection.getFocusOffset();
     const selectedBlocks = getSelectedBlocksList(editorState);
 
-    if (selectedBlocks.size > 0) {
-      if (selection.getIsBackward()) {
-        const temp = start;
-        start = end;
-        end = temp;
-      }
-
-      for (let i = 0; i < selectedBlocks.size; i += 1) {
-        const blockStart = i === 0 ? start : 0;
-        const blockEnd = i === (selectedBlocks.size - 1) ? end : selectedBlocks.get(i).getText().length;
-        selectedText += selectedBlocks.get(i).getText().slice(blockStart, blockEnd);
-      }
+    if (selection.getIsBackward()) {
+      const temp = start;
+      start = end;
+      end = temp;
     }
+
+    let selectedText = '';
+    for (let i = 0; i < selectedBlocks.size; i += 1) {
+      const blockStart = i === 0 ? start : 0;
+      const blockEnd = i === (selectedBlocks.size - 1) ? end : selectedBlocks.get(i).getText().length;
+      selectedText += selectedBlocks.get(i).getText().slice(blockStart, blockEnd);
+    }
+
     return selectedText;
   };
