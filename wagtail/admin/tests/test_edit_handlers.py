630eaade3737dc48a22f06be15f7edf9a02ea893
Karl Hobley <karl@kaed.uk>
2018-04-03 14:16:29 +0100

    Rename assertEquals to assertEqual

--- wagtail/admin/tests/test_edit_handlers.py
+++ wagtail/admin/tests/test_edit_handlers.py
@@ -212,7 +212,7 @@ class TestExtractPanelDefinitionsFromModelClass(TestCase):
     def test_exclude_with_defined_panels(self):
         Site.panels = [FieldPanel('hostname')]
         panels = extract_panel_definitions_from_model_class(Site, exclude=['hostname'])
-        self.assertEquals([], panels)
+        self.assertEqual([], panels)
 
     def test_can_build_panel_list(self):
         # EventPage has no 'panels' definition, so one should be derived from the field list
