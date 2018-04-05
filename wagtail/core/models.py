0db74c88d72d5e3f60b1231946268288cfaeb2b9
Codie Roelf <codie@praekelt.com>
2018-04-05 12:46:56 +0200

    use better sorting

--- wagtail/core/models.py
+++ wagtail/core/models.py
@@ -160,7 +160,8 @@ class Site(models.Model):
         if result is None:
             result = [
                 (site.id, site.root_page.url_path, site.root_url)
-                for site in Site.objects.select_related('root_page').order_by('-root_page__url_path')
+                for site in Site.objects.select_related('root_page').order_by(
+                    '-root_page__url_path', 'is_default_site', 'hostname')
             ]
             cache.set('wagtail_site_root_paths', result, 3600)
 
