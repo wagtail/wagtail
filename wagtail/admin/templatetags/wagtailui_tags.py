f87279e1c183f69e0a675a31465d0a54e06929ee
Thibaud Colas <thibaudcolas@gmail.com>
2018-04-08 22:57:38 +0300

    Update icon template tag to use the same props as React component

--- wagtail/admin/templatetags/wagtailui_tags.py
+++ wagtail/admin/templatetags/wagtailui_tags.py
@@ -3,16 +3,15 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag("wagtailadmin/shared/icon.html",
-                        takes_context=False)
-def wagtail_icon(classnames, title='', show_label=True, decorative=True):
+@register.inclusion_tag("wagtailadmin/shared/wagtail_icon.html", takes_context=False)
+def wagtail_icon(name=None, classname='', title=None):
     """
-    Usage: {% wagtail_icon classnames title show_label decorative %}
+    Usage: {% wagtail_icon name="cogs" classname="icon--red" title="Settings" %}
 
-    The css classes necessary to show the icon are provided as a first argument.
-    The 'title' argument is optional, but recommended. When 'show_label' is True, the title is shown next to the icon.
-    When an icon is marked decorative, it is hidden for screenreaders (a11y). If 'decorative' is False, the title is
-    visible for screenreaders, regardless whether 'show_label' is True or not.
-
+    First load the tags with {% load wagtailui_tags %}
     """
-    return {'classnames': classnames, 'title': title, 'show_label': show_label, 'decorative': decorative}
+    return {
+        'name': name,
+        'classname': classname,
+        'title': title,
+    }
