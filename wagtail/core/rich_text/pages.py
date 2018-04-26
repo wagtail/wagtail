986abd10ea961afbd63fbf84772d1aa6b38dd888
Matt Westcott <matt@west.co.tt>
2018-04-26 19:26:10 +0100

    Implement get_model / get_instance on link/embed handlers

--- wagtail/core/rich_text/pages.py
+++ wagtail/core/rich_text/pages.py
@@ -1,13 +1,22 @@
 from django.utils.html import escape
 
 from wagtail.core.models import Page
+from wagtail.core.rich_text import LinkHandler
 
 
-class PageLinkHandler:
+class PageLinkHandler(LinkHandler):
     @staticmethod
-    def expand_db_attributes(attrs):
+    def get_model():
+        return Page
+
+    @classmethod
+    def get_instance(cls, attrs):
+        return super().get_instance(attrs).specific
+
+    @classmethod
+    def expand_db_attributes(cls, attrs):
         try:
-            page = Page.objects.get(id=attrs['id'])
+            page = cls.get_instance(attrs)
             return '<a href="%s">' % escape(page.specific.url)
         except Page.DoesNotExist:
             return "<a>"
