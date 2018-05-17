a69f205a661792e51a3224458ae5a91eedb013a0
Matt Westcott <matt@west.co.tt>
2018-05-17 16:08:42 +0100

    Remove frontend search view

--- wagtail/search/urls/frontend.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from wagtail.search.views import search
-
-app_name = 'wagtailsearch_frontend'
-urlpatterns = [
-    url(r'^$', search, name='wagtailsearch_search'),
-    url(r'^suggest/$', search, {'use_json': True}, name='wagtailsearch_suggest'),
-]
