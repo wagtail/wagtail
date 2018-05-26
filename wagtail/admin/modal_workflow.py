97caea7ffb22fd01305ad2b105a2ee7d62cf289b
Matt Westcott <matthew@west.co.tt>
2018-05-26 09:46:02 +0100

    Support additional data in render_modal_workflow responses
    This allows us to eliminate template tags in JS handlers, since
    dynamic data can now be passed in rather than injected.

--- wagtail/admin/modal_workflow.py
+++ wagtail/admin/modal_workflow.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 
 
-def render_modal_workflow(request, html_template, js_template, template_vars=None):
+def render_modal_workflow(request, html_template, js_template, template_vars=None, json_data=None):
     """"
     Render a response consisting of an HTML chunk and a JS onload chunk
     in the format required by the modal-workflow framework.
@@ -18,6 +18,10 @@ def render_modal_workflow(request, html_template, js_template, template_vars=Non
     if js_template:
         js = render_to_string(js_template, template_vars or {}, request=request)
         response_keyvars.append("'onload': %s" % js)
+
+    if json_data:
+        for key, val in json_data.items():
+            response_keyvars.append("%s: %s" % (json.dumps(key), json.dumps(val)))
 
     response_text = "{%s}" % ','.join(response_keyvars)
 
