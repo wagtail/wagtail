0b442a47cb7d51763ce6900ef2bb21633e5a6381
Rhys <rhys@squarehost.co.uk>
2018-05-31 14:12:25 +0100

    - forgot js

--- wagtail/admin/static_src/wagtailadmin/js/submenu.js
+++ wagtail/admin/static_src/wagtailadmin/js/submenu.js
@@ -3,12 +3,23 @@ $(function() {
         if ($(this).closest('li').find('.nav-submenu').length) {
 
             // Close other active submenus first, if any
-            if ($('.nav-wrapper.submenu-active').length && !$(this).closest('li').hasClass('submenu-active')) {
+            var hasC = $(this).closest('li').find('.nav-submenu').length > 0;
+            if ($(this).closest('li').parent().parent().hasClass('nav-main')) {
+                hasC = false;
+            }
+
+            if (hasC) {
+                $(this).closest('li').siblings('.submenu-active').removeClass('submenu-active');
+            }
+
+            if ($('.nav-wrapper.submenu-active').length && !$(this).closest('li').hasClass('submenu-active') && !hasC) {
                 $('.nav-main .submenu-active, .nav-wrapper').removeClass('submenu-active');
             }
 
             $(this).closest('li').toggleClass('submenu-active');
-            $('.nav-wrapper').toggleClass('submenu-active');
+            if (!$('.nav-wrapper').hasClass('submenu-active')) {
+                $('.nav-wrapper').addClass('submenu-active');
+            }
             return false;
         }
     });
