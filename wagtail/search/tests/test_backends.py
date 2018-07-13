dca01c3f4dec02cc163bc7faf2d826fb6842556f
Karl Hobley <karl@kaed.uk>
2018-07-13 10:21:55 +0100

    Reindex books when adding tags

--- wagtail/search/tests/test_backends.py
+++ wagtail/search/tests/test_backends.py
@@ -420,10 +420,17 @@ class BackendTests(WagtailTestUtils):
         # The test data doesn't contain any tags, add some
         FANTASY_BOOKS = [1, 2, 3, 4, 5, 6, 7]
         SCIFI_BOOKS = [10]
-        for book_id in FANTASY_BOOKS:
-            models.Book.objects.get(id=book_id).tags.add('Fantasy')
-        for book_id in SCIFI_BOOKS:
-            models.Book.objects.get(id=book_id).tags.add('Science Fiction')
+        for book in models.Book.objects.filter(id__in=FANTASY_BOOKS + SCIFI_BOOKS):
+            if book.id in FANTASY_BOOKS:
+                book.tags.add('Fantasy')
+            if book.id in SCIFI_BOOKS:
+                book.tags.add('Science Fiction')
+
+            self.backend.add(book)
+
+        index = self.backend.get_index_for_model(models.Book)
+        if index:
+            index.refresh()
 
         fantasy_tag = Tag.objects.get(name='Fantasy')
         scifi_tag = Tag.objects.get(name='Science Fiction')
