f62d03bde7bf4178296578cf4dcfa1f6c60d3d05
Matt Westcott <matt@west.co.tt>
2018-08-13 14:58:35 +0100

    Pin Beautifulsoup to 4.6.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ install_requires = [
     "djangorestframework>=3.7.4,<4.0",
     "draftjs_exporter>=2.0,<3.0",
     "Pillow>=4.0.0,<6.0",
-    "beautifulsoup4>=4.5.1,<5.0,!=4.6.1",
+    "beautifulsoup4>=4.5.1,<4.6.1",
     "html5lib>=0.999,<2",
     "Unidecode>=0.04.14,<2.0",
     "Willow>=1.1,<1.2",
