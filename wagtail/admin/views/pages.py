4d2ffc02b9232135def19e388da459a818bf43dc
Matt Westcott <matt@west.co.tt>
2018-09-19 12:32:20 +0100

    Replace the page_action_menu template tag with a PageActionMenu class to manage rendering

--- wagtail/admin/views/pages.py
+++ wagtail/admin/views/pages.py
@@ -18,7 +18,7 @@ from django.views.decorators.vary import vary_on_headers
 from django.views.generic import View
 
 from wagtail.admin import messages, signals
-from wagtail.admin.action_menu import _get_action_menu_items
+from wagtail.admin.action_menu import PageActionMenu
 from wagtail.admin.forms import CopyForm, SearchForm
 from wagtail.admin.navigation import get_explorable_root_page
 from wagtail.admin.utils import send_notification, user_has_any_page_permission, user_passes_test
@@ -303,7 +303,7 @@ def create(request, content_type_app_name, content_type_model_name, parent_page_
         'page_class': page_class,
         'parent_page': parent_page,
         'edit_handler': edit_handler,
-        'action_menu_items': _get_action_menu_items(),
+        'action_menu': PageActionMenu(request, view='create', parent_page=parent_page),
         'preview_modes': page.preview_modes,
         'form': form,
         'next': next_url,
@@ -527,13 +527,12 @@ def edit(request, page_id):
         page_for_status = page
 
     return render(request, 'wagtailadmin/pages/edit.html', {
-        'view_type': 'edit',
         'page': page,
         'page_for_status': page_for_status,
         'content_type': content_type,
         'edit_handler': edit_handler,
         'errors_debug': errors_debug,
-        'action_menu_items': _get_action_menu_items(),
+        'action_menu': PageActionMenu(request, view='edit', page=page),
         'preview_modes': page.preview_modes,
         'form': form,
         'next': next_url,
@@ -1137,14 +1136,13 @@ def revisions_revert(request, page_id, revision_id):
     ))
 
     return render(request, 'wagtailadmin/pages/edit.html', {
-        'view_type': 'revisions_revert',
         'page': page,
         'revision': revision,
         'is_revision': True,
         'content_type': content_type,
         'edit_handler': edit_handler,
         'errors_debug': None,
-        'action_menu_items': _get_action_menu_items(),
+        'action_menu': PageActionMenu(request, view='revisions_revert', page=page),
         'preview_modes': page.preview_modes,
         'form': form,  # Used in unit tests
     })
