8c82d66efa3b2979c380a78b528652a4a264d78f
Bertrand Bordage <bordage.bertrand@gmail.com>
2018-12-05 05:29:44 +0100

    Removes unused files.

--- wagtail/admin/static_src/wagtailadmin/js/blocks/struct.js
+++
@@ -1,9 +0,0 @@
-window.StructBlock = function(childInitializersByName) {
-    return function(prefix) {
-        for (var childName in childInitializersByName) {
-            var childInitializer = childInitializersByName[childName];
-            var childPrefix = prefix + '-' + childName;
-            childInitializer(childPrefix);
-        }
-    };
-};
