06fe46a76b68f77c86ad7bbe65e01282a0f2f89c
Thomas Kremmel <thomas.kremmel@simpleloop.com>
2019-01-17 11:10:56 +0100

    restrict TestExtractPanelDefinitionsFromModelAdmin testcase to check for form fields via create and verifying if the correct edit_handlers are called

--- wagtail/contrib/modeladmin/tests/test_modeladmin_edit_handlers.py
+++ wagtail/contrib/modeladmin/tests/test_modeladmin_edit_handlers.py
@@ -1,6 +1,7 @@
+import mock
+
 from django.test import TestCase
 
-from wagtail.tests.modeladmintest.models import Contributor, Friend, Person, Visitor
 from wagtail.tests.utils import WagtailTestUtils
 
 
@@ -11,52 +12,31 @@ class TestExtractPanelDefinitionsFromModelAdmin(TestCase, WagtailTestUtils):
         self.login()
 
     def test_model_edit_handler(self):
-        # tests that edit_handler defintion from model is used to create
-        # a model instance if edit_handler is set on the model
-        response = self.client.post('/admin/modeladmintest/person/create/', {
-            'first_name': "John",
-            'last_name': "Doe",
-            'address': "123 Main St Anytown",
-            'phone_number': "+123456789"
-        })
-        # Should redirect back to index
-        self.assertRedirects(response, '/admin/modeladmintest/person/')
-
-        # Check that the person was created
-        self.assertEqual(Person.objects.filter(
-            first_name="John",
-            last_name="Doe",
-            phone_number="+123456789"
-        ).count(), 1)
-
-        # verify that form fields are returned which have been defined
-        # in Person.edit_handler
+        # loads the 'create' view and verifies that form fields are returned
+        # which have been defined via model Person.edit_handler
         response = self.client.get('/admin/modeladmintest/person/create/')
         self.assertEqual(
             [field_name for field_name in response.context['form'].fields],
             ['first_name', 'last_name', 'phone_number']
         )
 
+    @mock.patch('wagtail.contrib.modeladmin.views.ModelFormView.get_edit_handler')
+    def test_model_form_view_edit_handler_called(self, mock_modelformview_get_edit_handler):
+        # loads the 'create' view and verifies
+        # that modelformview edit_handler is called
+        self.client.get('/admin/modeladmintest/person/create/')
+        mock_modelformview_get_edit_handler.assert_called()
+
+    @mock.patch('wagtail.contrib.modeladmin.options.ModelAdmin.get_edit_handler')
+    def test_model_admin_edit_handler_called(self, mock_modeladmin_get_edit_handler):
+        # loads the 'create' view and verifies
+        # that modeladmin edit_handler is called
+        self.client.get('/admin/modeladmintest/person/create/')
+        mock_modeladmin_get_edit_handler.assert_called()
+
     def test_model_panels(self):
-        # tests that panel definition from model is used to create
-        # a model instance if no edit_handler is set on the model
-        response = self.client.post('/admin/modeladmintest/friend/create/', {
-            'first_name': "John",
-            'last_name': "Doe",
-            'address': "123 Main St Anytown",
-            'phone_number': "+123456789"
-        })
-        # Should redirect back to index
-        self.assertRedirects(response, '/admin/modeladmintest/friend/')
-
-        # Check that the friend was created
-        self.assertEqual(Friend.objects.filter(
-            first_name="John",
-            phone_number="+123456789"
-        ).count(), 1)
-
-        # verify that form fields are returned which have been defined
-        # in Friend.panels
+        # loads the 'create' view and verifies that form fields are returned
+        # which have been defined via model Friend.panels
         response = self.client.get('/admin/modeladmintest/friend/create/')
         self.assertEqual(
             [field_name for field_name in response.context['form'].fields],
@@ -64,56 +44,47 @@ class TestExtractPanelDefinitionsFromModelAdmin(TestCase, WagtailTestUtils):
         )
 
     def test_model_admin_edit_handler(self):
-        # tests that edit_handler definition from modeladmin is used to create
-        # a model instance if no edit_handler is set on the model
-        response = self.client.post('/admin/modeladmintest/visitor/create/', {
-            'first_name': "John",
-            'last_name': "Doe",
-            'address': "123 Main St Anytown",
-            'phone_number': "+123456789"
-        })
-        # Should redirect back to index
-        self.assertRedirects(response, '/admin/modeladmintest/visitor/')
-
-        # Check that the person was created
-        self.assertEqual(Visitor.objects.filter(
-            last_name="Doe",
-            phone_number="+123456789",
-            address="123 Main St Anytown"
-        ).count(), 1)
-
-        # verify that form fields are returned which have been defined
-        # in Person.edit_handler
+        # loads the 'create' view and verifies that form fields are returned
+        # which have been defined via model VisitorAdmin.edit_handler
         response = self.client.get('/admin/modeladmintest/visitor/create/')
-
         self.assertEqual(
             [field_name for field_name in response.context['form'].fields],
             ['last_name', 'phone_number', 'address']
         )
 
+    @mock.patch('wagtail.contrib.modeladmin.views.ModelFormView.get_edit_handler')
+    def test_model_admin_edit_handler_form_view_edit_handler_called(self, mock_modelformview_get_edit_handler):
+        # loads the 'create' view and verifies
+        # that modelformview edit_handler is called
+        self.client.get('/admin/modeladmintest/contributor/create/')
+        mock_modelformview_get_edit_handler.assert_called()
+
+    @mock.patch('wagtail.contrib.modeladmin.options.ModelAdmin.get_edit_handler')
+    def test_model_admin_edit_handler_edit_handler_called(self, mock_modeladmin_get_edit_handler):
+        # loads the 'create' view and verifies
+        # that modeladmin edit_handler is called
+        self.client.get('/admin/modeladmintest/contributor/create/')
+        mock_modeladmin_get_edit_handler.assert_called()
+
     def test_model_admin_panels(self):
-        # tests that panel definition from modeladmin is used to create
-        # a model instance if no edit_handler is set on the model and modeladmin
-        response = self.client.post('/admin/modeladmintest/contributor/create/', {
-            'first_name': "John",
-            'last_name': "Doe",
-            'address': "123 Main St Anytown",
-            'phone_number': "+123456789"
-        })
-        # Should redirect back to index
-        self.assertRedirects(response, '/admin/modeladmintest/contributor/')
-
-        # Check that the person was created
-        self.assertEqual(Contributor.objects.filter(
-            last_name="Doe",
-            phone_number="+123456789",
-            address="123 Main St Anytown"
-        ).count(), 1)
-
-        # verify that form fields are returned which have been defined
-        # in Person.edit_handler
+        # loads the 'create' view and verifies that form fields are returned
+        # which have been defined via model ContributorAdmin.panels
         response = self.client.get('/admin/modeladmintest/contributor/create/')
         self.assertEqual(
             [field_name for field_name in response.context['form'].fields],
             ['last_name', 'phone_number', 'address']
         )
+
+    @mock.patch('wagtail.contrib.modeladmin.views.ModelFormView.get_edit_handler')
+    def test_model_admin_panels_form_view_edit_handler_called(self, mock_modelformview_get_edit_handler):
+        # loads the 'create' view and verifies
+        # that modelformview edit_handler is called
+        self.client.get('/admin/modeladmintest/contributor/create/')
+        mock_modelformview_get_edit_handler.assert_called()
+
+    @mock.patch('wagtail.contrib.modeladmin.options.ModelAdmin.get_edit_handler')
+    def test_model_admin_panels_admin_edit_handler_called(self, mock_modeladmin_get_edit_handler):
+        # loads the 'create' view and verifies
+        # that modeladmin edit_handler is called
+        self.client.get('/admin/modeladmintest/contributor/create/')
+        mock_modeladmin_get_edit_handler.assert_called()
