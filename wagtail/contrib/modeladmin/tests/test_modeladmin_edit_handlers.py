30a981075b7f93537e408dbf9dcf5fe1b9f154e9
Thomas Kremmel <thomas.kremmel@simpleloop.com>
2019-01-17 11:12:11 +0100

    TestExtractPanelDefinitionsFromModelAdmin test case fixes

--- wagtail/contrib/modeladmin/tests/test_modeladmin_edit_handlers.py
+++ wagtail/contrib/modeladmin/tests/test_modeladmin_edit_handlers.py
@@ -56,14 +56,14 @@ class TestExtractPanelDefinitionsFromModelAdmin(TestCase, WagtailTestUtils):
     def test_model_admin_edit_handler_form_view_edit_handler_called(self, mock_modelformview_get_edit_handler):
         # loads the 'create' view and verifies
         # that modelformview edit_handler is called
-        self.client.get('/admin/modeladmintest/contributor/create/')
+        self.client.get('/admin/modeladmintest/visitor/create/')
         mock_modelformview_get_edit_handler.assert_called()
 
     @mock.patch('wagtail.contrib.modeladmin.options.ModelAdmin.get_edit_handler')
     def test_model_admin_edit_handler_edit_handler_called(self, mock_modeladmin_get_edit_handler):
         # loads the 'create' view and verifies
         # that modeladmin edit_handler is called
-        self.client.get('/admin/modeladmintest/contributor/create/')
+        self.client.get('/admin/modeladmintest/visitor/create/')
         mock_modeladmin_get_edit_handler.assert_called()
 
     def test_model_admin_panels(self):
