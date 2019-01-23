d2ddcd5646a31803c06cd94e7d562ea94b58a08b
Andy Babic <ababic@rkh.co.uk>
2019-01-23 14:24:19 +0000

    Added docstring to module, and tweaked comments/variable names for consistency/readability

--- wagtail/utils/contenttypes.py
+++ wagtail/utils/contenttypes.py
@@ -4,6 +4,20 @@ from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q
 
+"""
+This module is a temporary measure to help support proxy models throughout
+Wagtail, by ensuring each has it's own ``ContentType``, and that ``Permission``
+objects are correctly associated with any newly created ``ContentType`` objects
+(rather than that of the concrete model).
+
+Once Wagtail reaches a point where it supports only Django versions >= 2.2,
+this module should be removed, and the wider project updated to use the
+following:
+
+- ``ContentType.objects.get_for_model(model, for_concrete_model=False)``
+- ``ContentType.objects.get_for_models(*models, for_concrete_models=False)``
+"""
+
 manager = ContentType.objects
 db = ContentType.objects.db
 
@@ -60,7 +74,7 @@ def create_for_model(model):
     Creates and returns a ``ContentType`` object for the provided ``model``. If
     the model happens to be a proxy model, any related ``Permission`` objects
     will be correctly associated with the new object, matching the approach
-    taken by Django itself from v2.2.
+    taken by Django itself from 2.2.
     """
     opts = model._meta
     # get_or_create() used her to handle potential race conditions
@@ -68,18 +82,21 @@ def create_for_model(model):
         app_label=opts.app_label, model=opts.model_name
     )
     if created and opts.proxy and django.VERSION < (2, 2):
-        permission_codenames = [
+        # To avoid any consistent/compatibilty issues, the approach below
+        # matches that taken by Django in `0011_update_proxy_permissions.py` in
+        # https://github.com/django/django/commit/181fb60159e54d442d3610f4afba6f066a6dac05,
+        default_permissions_codenames = [
             '%s_%s' % (action, opts.model_name)
             for action in opts.default_permissions
         ]
-        permissions_query = Q(codename__in=permission_codenames)
+        permissions_query = Q(codename__in=default_permissions_codenames)
         for codename, name in opts.permissions:
-            permissions_query |= Q(codename=codename, name=name)
+            permissions_query = permissions_query | Q(codename=codename, name=name)
 
-        concrete_ctype = manager.get_for_model(model, for_concrete_model=True)
+        concrete_content_type = manager.get_for_model(model, for_concrete_model=True)
 
         Permission.objects.filter(
-            permissions_query, content_type=concrete_ctype
+            permissions_query, content_type=concrete_content_type
         ).update(content_type=content_type)
 
     manager._add_to_cache(db, content_type)
