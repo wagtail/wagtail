10c8aab8ccd98e79e897f10ffbcc6fc0b6b0dd18
Thibaud Colas <thibaudcolas@gmail.com>
2019-04-24 14:07:39 +0100

    Set up react-axe to report accessibility issues automatically while developing

--- wagtail/admin/static_src/wagtailadmin/app/wagtailadmin.entry.js
+++ wagtail/admin/static_src/wagtailadmin/app/wagtailadmin.entry.js
@@ -1,4 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { initExplorer, Icon, Portal } from 'wagtail-client';
+
+if (process.env.NODE_ENV === 'development') {
+  // Run react-axe in development only, so it does not affect performance
+  // in production, and does not break unit tests either.
+  // eslint-disable-next-line global-require
+  const axe = require('react-axe');
+  axe(React, ReactDOM, 1000);
+}
 
 // Expose components as globals for third-party reuse.
 window.wagtail.components = {
