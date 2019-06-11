16e86e8e462d030a14714eb7d656f239a64ba0c3
Jonny Scholes <jonnyscholes@gmail.com>
2019-06-11 11:04:23 +1000

    Fix linting issues

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,15 @@
  * Re-exports components and other modules via a cleaner API.
  */
 
-import Button from "./components/Button/Button";
-import Icon from "./components/Icon/Icon";
-import PublicationStatus from "./components/PublicationStatus/PublicationStatus";
-import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
-import Portal from "./components/Portal/Portal";
-import Transition from "./components/Transition/Transition";
-import Explorer, { ExplorerToggle, initExplorer } from "./components/Explorer";
-import { initFocusOutline } from "./utils/focus";
-import { initSubmenus } from "./includes/initSubmenus";
+import Button from './components/Button/Button';
+import Icon from './components/Icon/Icon';
+import PublicationStatus from './components/PublicationStatus/PublicationStatus';
+import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
+import Portal from './components/Portal/Portal';
+import Transition from './components/Transition/Transition';
+import Explorer, { ExplorerToggle, initExplorer } from './components/Explorer';
+import { initFocusOutline } from './utils/focus';
+import { initSubmenus } from './includes/initSubmenus';
 
 export {
   Button,
