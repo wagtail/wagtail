246ce3113ea1abef02148a222fb33636898d3434
mien <mikael.pj.engstrom@gmail.com>
2019-08-01 14:22:00 +0200

    Add support for phone links in rich text
    This is mainly copy paste of Liam Brenner work (#3776) but with
    a few fixes to make it work with Draftail. Since mailto:-links
    is supported i think it is reasonable to support tel:-links as
    well

--- wagtail/admin/views/chooser.py
+++ wagtail/admin/views/chooser.py
@@ -2,7 +2,8 @@ from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
-from wagtail.admin.forms.choosers import EmailLinkChooserForm, ExternalLinkChooserForm
+from wagtail.admin.forms.choosers import (
+    EmailLinkChooserForm, ExternalLinkChooserForm, PhoneLinkChooserForm)
 from wagtail.admin.forms.search import SearchForm
 from wagtail.admin.modal_workflow import render_modal_workflow
 from wagtail.core import hooks
@@ -19,6 +20,7 @@ def shared_context(request, extra_context=None):
         'parent_page_id': request.GET.get('parent_page_id'),
         'allow_external_link': request.GET.get('allow_external_link'),
         'allow_email_link': request.GET.get('allow_email_link'),
+        'allow_phone_link': request.GET.get('allow_phone_link'),
     }
     if extra_context:
         context.update(extra_context)
@@ -253,4 +255,38 @@ def email_link(request):
         shared_context(request, {
             'form': form,
         }), json_data={'step': 'email_link'}
+    )
+
+
+def phone_link(request):
+    initial_data = {
+        'link_text': request.GET.get('link_text', ''),
+        'phone_number': request.GET.get('link_url', ''),
+    }
+
+    if request.method == 'POST':
+        form = PhoneLinkChooserForm(request.POST, initial=initial_data, prefix='phone-link-chooser')
+
+        if form.is_valid():
+            result = {
+                'url': 'tel:' + form.cleaned_data['phone_number'],
+                'title': form.cleaned_data['link_text'].strip() or form.cleaned_data['phone_number'],
+                # If the user has explicitly entered / edited something in the link_text field,
+                # always use that text. If not, we should favour keeping the existing link/selection
+                # text, where applicable.
+                'prefer_this_title_as_link_text': ('link_text' in form.changed_data),
+            }
+            return render_modal_workflow(
+                request, None, None,
+                None, json_data={'step': 'external_link_chosen', 'result': result}
+            )
+    else:
+        form = PhoneLinkChooserForm(initial=initial_data, prefix='phone-link-chooser')
+
+    return render_modal_workflow(
+        request,
+        'wagtailadmin/chooser/phone_link.html', None,
+        shared_context(request, {
+            'form': form,
+        }), json_data={'step': 'phone_link'}
     )
