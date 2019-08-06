740f052904ab9025e8896f8a29517724e38f56a4
Iman Syed <iman.syed@torchbox.com>
2019-08-06 11:08:43 +0100

    When no thumbnail, 'providername - authorname' displays in draftail editor embeds

--- client/src/components/Draftail/blocks/EmbedBlock.js
+++ client/src/components/Draftail/blocks/EmbedBlock.js
@@ -10,10 +10,7 @@ import MediaBlock from '../blocks/MediaBlock';
  */
 const EmbedBlock = props => {
   const { entity, onRemoveEntity } = props.blockProps;
-  const { url, title, thumbnail } = entity.getData();
-
-  const username = url.split("/").slice()[3]
-  const profile_image = 'https://twitter.com/' + username + '/profile_image?size=original'
+  const { url, title, thumbnail, providerName, authorName } = entity.getData();
   
   if (thumbnail) {
     return (
@@ -39,23 +36,15 @@ const EmbedBlock = props => {
 
   else {
     return (
-      <MediaBlock {...props} src={profile_image} alt="">
-        {url ? (
-          <a
-            className="Tooltip__link EmbedBlock__link"
-            href={url}
-            title={url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {title}
-          </a>
-        ) : null}
+      <strong>
+        <a href={url} target={'_blank'}>
+          {providerName + ' - ' + authorName + '  '}
+        </a>
 
-        <button className="button button-secondary no Tooltip__button" onClick={onRemoveEntity}>
+        <button className="button button-secondary no Tooltip__button" onClick={onRemoveEntity} >
           {STRINGS.DELETE}
         </button>
-      </MediaBlock>
+      </strong>
     );
   };
 };
