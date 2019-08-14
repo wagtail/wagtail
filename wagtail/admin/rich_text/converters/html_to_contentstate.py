7edc8fcb45fa0a4dab8627a25f1fc7c87d713ef4
Iman Syed <iman.syed@torchbox.com>
2019-08-14 09:09:55 +0100

    Rich text preserves whitespace in <pre> tag

--- wagtail/admin/rich_text/converters/html_to_contentstate.py
+++ wagtail/admin/rich_text/converters/html_to_contentstate.py
@@ -315,9 +315,11 @@ class HtmlToContentStateHandler(HTMLParser):
             element_handler.handle_endtag(name, self.state, self.contentstate)
 
     def handle_data(self, content):
-        # normalise whitespace sequences to a single space
+        # normalise whitespace sequences to a single space unless it is contained in <pre> tag, in
+        # which case leave it alone
         # This is in line with https://www.w3.org/TR/html4/struct/text.html#h-9.1
-        content = re.sub(WHITESPACE_RE, ' ', content)
+        if not (content.startswith('<pre>') and content.endswith('</pre>')):
+            content = re.sub(WHITESPACE_RE, ' ', content)
 
         if self.state.current_block is None:
             if content == ' ':
