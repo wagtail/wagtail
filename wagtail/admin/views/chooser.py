dc2611691ab3fdd68bc7ca693575bc375f683215
Iman Syed <iman.syed@torchbox.com>
2019-08-14 15:46:11 +0100

    Sort imports

--- wagtail/admin/views/chooser.py
+++ wagtail/admin/views/chooser.py
@@ -2,7 +2,8 @@ from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
-from wagtail.admin.forms.choosers import EmailLinkChooserForm, ExternalLinkChooserForm, AnchorLinkChooserForm
+from wagtail.admin.forms.choosers import (
+    AnchorLinkChooserForm, EmailLinkChooserForm, ExternalLinkChooserForm)
 from wagtail.admin.forms.search import SearchForm
 from wagtail.admin.modal_workflow import render_modal_workflow
 from wagtail.core import hooks
