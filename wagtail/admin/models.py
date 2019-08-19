0b51caf8b04b523fbfa30866fb1e13cf4f6abf54
Matt Westcott <matt@west.co.tt>
2019-08-19 16:31:24 +0100

    Update references to wagtailadmin in comment

--- wagtail/admin/models.py
+++ wagtail/admin/models.py
@@ -5,8 +5,8 @@ from modelcluster.fields import ParentalKey
 from taggit.models import Tag
 
 # The edit_handlers module extends Page with some additional attributes required by
-# wagtailadmin (namely, base_form_class and get_edit_handler). Importing this within
-# wagtailadmin.models ensures that this happens in advance of running wagtailadmin's
+# wagtail admin (namely, base_form_class and get_edit_handler). Importing this within
+# wagtail.admin.models ensures that this happens in advance of running wagtail.admin's
 # system checks.
 from wagtail.admin import edit_handlers  # NOQA
 from wagtail.core.models import Page
