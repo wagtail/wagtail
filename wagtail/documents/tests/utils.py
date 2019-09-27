f12d371a0a7fea24b6bd2bcc37ce8281371c17db
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2019-09-27 20:25:05 +0500

    Deduplicate creation of fake file in document tests

---
+++ wagtail/documents/tests/utils.py
@@ -0,0 +1,7 @@
+from django.core.files.base import ContentFile
+
+
+def get_test_document_file():
+    fake_file = ContentFile(b"A boring example document")
+    fake_file.name = 'test.txt'
+    return fake_file
