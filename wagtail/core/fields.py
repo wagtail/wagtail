464884440c7be7684cfd9ce177bf5c8f6618ca0e
Paulo <commonzenpython@gmail.com>
2019-10-27 19:34:41 -0400

    Fixed #5537 -- Fetch deferred stream fields from db

--- wagtail/core/fields.py
+++ wagtail/core/fields.py
@@ -31,7 +31,12 @@ class Creator:
     def __get__(self, obj, type=None):
         if obj is None:
             return self
-        return obj.__dict__[self.field.name]
+        field_name = self.field.name
+
+        if field_name not in obj.__dict__:
+            # Field is deferred. Fetch it from db.
+            obj.refresh_from_db(fields=[field_name])
+        return obj.__dict__[field_name]
 
     def __set__(self, obj, value):
         obj.__dict__[self.field.name] = self.field.to_python(value)
