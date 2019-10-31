149fdc84517aa45fdffde50e1cae6b6ace5c602a
jacobtm <jacobtm@torchbox.com>
2019-10-31 10:05:38 +0000

    Create base ReportView and refactor locked_pages into subclass LockedPagesView, with a base template base_reports.html for simple listing reports

--- wagtail/admin/views/reports.py
+++ wagtail/admin/views/reports.py
@@ -1,14 +1,30 @@
-from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.utils.translation import ugettext as _
+from django.views.generic.base import TemplateResponseMixin
+from django.views.generic.list import BaseListView
+
 from wagtail.core.models import UserPagePermissionsProxy
 
 
-def locked_pages(request):
-    pages = UserPagePermissionsProxy(request.user).editable_pages().filter(locked=True)
+class ReportView(TemplateResponseMixin, BaseListView):
+    header_icon = ''
+    page_kwarg = 'p'
+    template_name = None
+    title = ''
+    paginate_by = 10
 
-    paginator = Paginator(pages, per_page=10)
-    pages = paginator.get_page(request.GET.get('p'))
+    def get_context_data(self, *args, object_list=None, **kwargs):
+        context = super().get_context_data(*args, object_list=object_list, **kwargs)
+        context['title'] = self.title
+        context['header_icon'] = self.header_icon
+        return context
 
-    return render(request, 'wagtailadmin/reports/locked_pages.html', {
-        'pages': pages,
-    })
+
+class LockedPagesView(ReportView):
+    template_name = 'wagtailadmin/reports/locked_pages.html'
+    title = _('Locked Pages')
+    header_icon = 'locked'
+
+    def get_queryset(self):
+        pages = UserPagePermissionsProxy(self.request.user).editable_pages().filter(locked=True)
+        self.queryset = pages
+        return super().get_queryset()
