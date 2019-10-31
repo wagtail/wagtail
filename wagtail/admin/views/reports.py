b98816ff0bd0f8da502abb778c5ba2f4f57fbb65
jacobtm <jacobtm@torchbox.com>
2019-10-31 13:06:09 +0000

    Improve translation compatibility, and allow base_report's 'no pages found' message to be customised using a block

--- wagtail/admin/views/reports.py
+++ wagtail/admin/views/reports.py
@@ -1,4 +1,4 @@
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 from django.views.generic.base import TemplateResponseMixin
 from django.views.generic.list import BaseListView
 
