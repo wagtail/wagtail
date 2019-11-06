db9fc378dff9f82b8f8bd24335dfa632c78fbc94
jacobtm <jacobtm@torchbox.com>
2019-11-06 11:28:06 +0000

    Add index for workflows

--- wagtail/core/permissions.py
+++ wagtail/core/permissions.py
@@ -1,5 +1,6 @@
-from wagtail.core.models import Collection, Site
+from wagtail.core.models import Collection, Site, Workflow
 from wagtail.core.permission_policies import ModelPermissionPolicy
 
 site_permission_policy = ModelPermissionPolicy(Site)
 collection_permission_policy = ModelPermissionPolicy(Collection)
+workflow_permission_policy = ModelPermissionPolicy(Workflow)
