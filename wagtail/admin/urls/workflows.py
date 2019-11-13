a247bee91c8ac781ad0314e70ef43df1f1e45911
jacobtm <jacobtm@torchbox.com>
2019-11-13 15:19:46 +0000

    First draft of add to page form with confirmation message

--- wagtail/admin/urls/workflows.py
+++ wagtail/admin/urls/workflows.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from wagtail.admin.views import workflows
 
+
 app_name = 'wagtailadmin_workflows'
 urlpatterns = [
     path('', workflows.Index.as_view(), name='index'),
     path('add/', workflows.create, name='add'),
-    path('edit/<int:pk>', workflows.edit, name='edit'),
-    path('remove/<int:pk>', workflows.remove_workflow, name='remove')
+    path('edit/<int:pk>/', workflows.edit, name='edit'),
+    path('remove/<int:pk>/', workflows.remove_workflow, name='remove'),
+    path('add_to_page/<int:workflow_pk>/', workflows.add_to_page, name='add_to_page'),
 ]
