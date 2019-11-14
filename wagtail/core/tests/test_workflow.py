19e5e258bb1836f11b82b83f98b862410791b3c2
jacobtm <jacobtm@torchbox.com>
2019-11-14 15:23:56 +0000

    Update Workflow link to Page to use OneToOneField through WorkflowPage, rather than ForeignKey. Update get_workflow and tests to match new relation.

--- wagtail/core/tests/test_workflow.py
+++ wagtail/core/tests/test_workflow.py
@@ -2,14 +2,9 @@ from django.contrib.auth.models import Group
 
 from django.test import TestCase
 
-from wagtail.core.models import GroupApprovalTask, Page, Task, Workflow, WorkflowTask
+from wagtail.core.models import GroupApprovalTask, Page, Task, Workflow, WorkflowPage, WorkflowTask
 
-from wagtail.tests.testapp.models import (
-    AbstractPage, Advert, AlwaysShowInMenusPage, BlogCategory, BlogCategoryBlogPage, BusinessChild,
-    BusinessIndex, BusinessNowherePage, BusinessSubIndex, CustomManager, CustomManagerPage,
-    CustomPageQuerySet, EventCategory, EventIndex, EventPage, GenericSnippetPage, ManyToManyBlogPage,
-    MTIBasePage, MTIChildPage, MyCustomPage, OneToOnePage, PageWithExcludedCopyField, SimpleChildPage,
-    SimplePage, SimpleParentPage, SingleEventPage, SingletonPage, StandardIndex, TaggedPage)
+from wagtail.tests.testapp.models import SimplePage
 
 class TestWorkflows(TestCase):
     fixtures = ['test.json']
@@ -36,36 +31,39 @@ class TestWorkflows(TestCase):
         self.assertIn(workflow, Workflow.objects.filter(workflow_tasks__task=task))
 
     def test_add_workflow_to_page(self):
+        # test adding a Workflow to a Page via WorkflowPage
         workflow = Workflow.objects.create(name='test_workflow')
         homepage = Page.objects.get(url_path='/home/')
-        homepage.workflow = workflow
-        homepage.save()
+        WorkflowPage.objects.create(page=homepage, workflow=workflow)
         homepage.refresh_from_db()
-        self.assertEqual(homepage.workflow, workflow)
+        self.assertEqual(homepage.workflowpage.workflow, workflow)
 
     def test_get_specific_task(self):
+        # test ability to get instance of subclassed Task type using Task.specific
         group_approval_task = GroupApprovalTask.objects.create(name='test_group_approval', group=Group.objects.first())
         task = Task.objects.get(name='test_group_approval')
         specific_task = task.specific
         self.assertIsInstance(specific_task, GroupApprovalTask)
 
     def test_get_workflow_from_parent(self):
+        # test ability to use Page.get_workflow() to retrieve a workflow from a parent page if none is set directly
         workflow = Workflow.objects.create(name='test_workflow')
         homepage = Page.objects.get(url_path='/home/')
-        homepage.workflow = workflow
-        homepage.save()
+        WorkflowPage.objects.create(page=homepage, workflow=workflow)
         hello_page = SimplePage(title="Hello world", slug='hello-world', content="hello")
         homepage.add_child(instance=hello_page)
         self.assertEqual(hello_page.get_workflow(), workflow)
 
     def test_get_workflow_from_closest_ancestor(self):
+        # test that using Page.get_workflow() tries to get the workflow from itself, then the closest ancestor, and does
+        # not get workflows from further up the page tree first
         workflow_1 = Workflow.objects.create(name='test_workflow_1')
-        workflow_2 = Workflow.objects.create(name='test_workflow_1')
+        workflow_2 = Workflow.objects.create(name='test_workflow_2')
         homepage = Page.objects.get(url_path='/home/')
-        homepage.workflow = workflow_1
-        homepage.save()
-        hello_page = SimplePage(title="Hello world", slug='hello-world', content="hello", workflow=workflow_2)
+        WorkflowPage.objects.create(page=homepage, workflow=workflow_1)
+        hello_page = SimplePage(title="Hello world", slug='hello-world', content="hello")
         homepage.add_child(instance=hello_page)
+        WorkflowPage.objects.create(page=hello_page, workflow=workflow_2)
         goodbye_page = SimplePage(title="Goodbye world", slug='goodbye-world', content="goodbye")
         hello_page.add_child(instance=goodbye_page)
         self.assertEqual(hello_page.get_workflow(), workflow_2)
