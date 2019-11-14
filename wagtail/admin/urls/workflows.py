edf5c272f708ea738962ae08d45343d63c641479
jacobtm <jacobtm@torchbox.com>
2019-11-14 17:18:17 +0000

    Add additional check for workflow being removed on remove_workflow view. Fix bug in add_to_page when models are missing. Move workflow edit fbv to cbv. Change permissions checks to use policy

--- wagtail/admin/urls/workflows.py
+++ wagtail/admin/urls/workflows.py
@@ -7,7 +7,8 @@ app_name = 'wagtailadmin_workflows'
 urlpatterns = [
     path('', workflows.Index.as_view(), name='index'),
     path('add/', workflows.Create.as_view(), name='add'),
-    path('edit/<int:pk>/', workflows.edit, name='edit'),
-    path('remove/<int:pk>/', workflows.remove_workflow, name='remove'),
+    path('edit/<int:pk>/', workflows.Edit.as_view(), name='edit'),
+    path('remove/<int:page_pk>/', workflows.remove_workflow, name='remove'),
+    path('remove/<int:page_pk>/<int:workflow_pk>/', workflows.remove_workflow, name='remove'),
     path('add_to_page/<int:workflow_pk>/', workflows.add_to_page, name='add_to_page'),
 ]
