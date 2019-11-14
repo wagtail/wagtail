823e7af07b4a4b69bf5286af8a903f8753b8ca68
jacobtm <jacobtm@torchbox.com>
2019-11-14 16:38:43 +0000

    Move create workflow to generic CreateView

--- wagtail/admin/urls/workflows.py
+++ wagtail/admin/urls/workflows.py
@@ -6,7 +6,7 @@ from wagtail.admin.views import workflows
 app_name = 'wagtailadmin_workflows'
 urlpatterns = [
     path('', workflows.Index.as_view(), name='index'),
-    path('add/', workflows.create, name='add'),
+    path('add/', workflows.Create.as_view(), name='add'),
     path('edit/<int:pk>/', workflows.edit, name='edit'),
     path('remove/<int:pk>/', workflows.remove_workflow, name='remove'),
     path('add_to_page/<int:workflow_pk>/', workflows.add_to_page, name='add_to_page'),
