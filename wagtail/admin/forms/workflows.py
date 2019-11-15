401efb3d276ed87df6e20a592db52d4388894a29
jacobtm <jacobtm@torchbox.com>
2019-11-15 14:37:36 +0000

    Add further explanatory comments, and add help text to 'active' field on Workflow

--- wagtail/admin/forms/workflows.py
+++ wagtail/admin/forms/workflows.py
@@ -6,6 +6,10 @@ from wagtail.core.models import Page, Workflow, WorkflowPage
 
 
 class AddWorkflowToPageForm(forms.Form):
+    """
+    A form to assign a Workflow instance to a Page. It is designed to work with a confirmation step if a the chosen Page
+    is assigned to an existing Workflow - the result of which is stored in overwrite_existing.
+    """
     page = forms.ModelChoiceField(queryset=Page.objects.all(), widget=widgets.AdminPageChooser(
             target_models=[Page],
             can_choose_root=True))
@@ -17,6 +21,10 @@ class AddWorkflowToPageForm(forms.Form):
         try:
             existing_workflow = page.workflowpage.workflow
             if not self.errors and existing_workflow != self.cleaned_data['workflow'] and not self.cleaned_data['overwrite_existing']:
+                # If the form has no errors, Page has an existing Workflow assigned, that Workflow is not
+                # the selected Workflow, and overwrite_existing is not True, add a new error. This should be used to
+                # trigger the confirmation message in the view. This is why this error is only added if there are no
+                # other errors - confirmation should be the final step.
                 self.add_error('page', ValidationError(_("This page already has workflow '{0}' assigned. Do you want to overwrite the existing workflow?").format(existing_workflow), code='needs_confirmation'))
         except AttributeError:
             pass
