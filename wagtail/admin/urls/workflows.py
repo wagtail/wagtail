f64f7af3f18ad6339d4e1ea6d24f9c844df7c9aa
jacobtm <jacobtm@torchbox.com>
2019-11-18 14:03:46 +0000

    Add generic views and templates for selecting and editing tasks

--- wagtail/admin/urls/workflows.py
+++ wagtail/admin/urls/workflows.py
@@ -11,4 +11,8 @@ urlpatterns = [
     path('remove/<int:page_pk>/', workflows.remove_workflow, name='remove'),
     path('remove/<int:page_pk>/<int:workflow_pk>/', workflows.remove_workflow, name='remove'),
     path('add_to_page/<int:workflow_pk>/', workflows.add_to_page, name='add_to_page'),
+    path('tasks/add/<str:app_label>/<str:model_name>/', workflows.CreateTask.as_view(), name='add_task'),
+    path('tasks/select_type/', workflows.select_task_type, name='select_task_type'),
+    path('tasks/index/', workflows.TaskIndex.as_view(), name='task_index'),
+    path('tasks/edit/<int:pk>/', workflows.EditTask.as_view(), name='edit_task')
 ]
