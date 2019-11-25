9aa34a0cfcd7adc36f964453a515d658b168c97a
Karl Hobley <karl@torchbox.com>
2019-11-25 15:14:42 +0000

    Disable "Submit for Moderation" if no workflow has been configured

--- wagtail/core/models.py
+++ wagtail/core/models.py
@@ -1556,6 +1556,9 @@ class Page(AbstractPage, index.Indexed, ClusterableModel, metaclass=PageBase):
 
         return obj
 
+    def has_workflow(self):
+        return self.get_ancestors(inclusive=True).filter(workflowpage__isnull=False).exists()
+
     def get_workflow(self):
         if hasattr(self, 'workflowpage'):
             return self.workflowpage.workflow
@@ -1986,6 +1989,9 @@ class PagePermissionTester:
             return False
 
         return self.user.is_superuser or ('publish' in self.permissions)
+
+    def can_submit_for_moderation(self):
+        return not self.page_locked() and self.page.has_workflow()
 
     def can_set_view_restrictions(self):
         return self.can_publish()
