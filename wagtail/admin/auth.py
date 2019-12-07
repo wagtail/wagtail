351c4105fac8bff527d1ee13bcd76af9cb6f4d0b
Andy Babic <andyjbabic@gmail.com>
2019-12-07 20:36:26 +0000

    Ensure TemplateResponses returned by views are rendered within the context of translation.override()

--- wagtail/admin/auth.py
+++ wagtail/admin/auth.py
@@ -9,8 +9,7 @@ from django.db.models import Q
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.timezone import activate as activate_tz
-from django.utils.translation import ugettext as _
-from django.utils.translation import override
+from django.utils.translation import override, ugettext as _
 
 from wagtail.admin import messages
 from wagtail.core.models import GroupPagePermission
@@ -154,22 +153,25 @@ def require_admin_access(view_func):
         if user.is_anonymous:
             return reject_request(request)
 
-        if user.has_perms(['wagtailadmin.access_admin']):
-            preferred_language = None
-            if hasattr(user, 'wagtail_userprofile'):
-                preferred_language = user.wagtail_userprofile.get_preferred_language()
-                l18n.set_language(preferred_language)
-                time_zone = user.wagtail_userprofile.get_current_time_zone()
-                activate_tz(time_zone)
-            if preferred_language:
-                with override(preferred_language):
-                    return view_func(request, *args, **kwargs)
-            else:
-                return view_func(request, *args, **kwargs)
+        if not user.has_perm('wagtailadmin.access_admin'):
+            if not request.is_ajax():
+                messages.error(request, _('You do not have permission to access the admin'))
+            return reject_request(request)
 
-        if not request.is_ajax():
-            messages.error(request, _('You do not have permission to access the admin'))
-
-        return reject_request(request)
+        preferred_language = None
+        if hasattr(user, 'wagtail_userprofile'):
+            preferred_language = user.wagtail_userprofile.get_preferred_language()
+            l18n.set_language(preferred_language)
+            time_zone = user.wagtail_userprofile.get_current_time_zone()
+            activate_tz(time_zone)
+        if preferred_language:
+            with override(preferred_language):
+                response = view_func(request, *args, **kwargs)
+                # forcing rendering of reponse here so that
+                # language override applies
+                if hasattr(response, 'render'):
+                    return response.render()
+                return response
+        return view_func(request, *args, **kwargs)
 
     return decorated_view
