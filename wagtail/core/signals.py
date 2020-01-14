3096fe69a5bd1e93b94a04a3798fc44e88894d72
jacobtm <jacobtm@torchbox.com>
2020-01-14 09:55:14 +0000

    Fix errors preventing mail sending; configure to work with signals sent on workflow events

--- wagtail/core/signals.py
+++ wagtail/core/signals.py
@@ -2,3 +2,13 @@ from django.dispatch import Signal
 
 page_published = Signal(providing_args=['instance', 'revision'])
 page_unpublished = Signal(providing_args=['instance'])
+
+workflow_approved = Signal(providing_args=['instance'])
+workflow_rejected = Signal(providing_args=['instance'])
+workflow_cancelled = Signal(providing_args=['instance'])
+workflow_submitted = Signal(providing_args=['instance'])
+
+task_approved = Signal(providing_args=['instance'])
+task_rejected = Signal(providing_args=['instance'])
+task_submitted = Signal(providing_args=['instance'])
+
