b85c8900f7aa33b56223aa5b4093366e95feb08b
Martin Coote <martin@martincoote.com>
2020-01-24 15:31:28 +0000

    lint fixes

--- client/src/includes/initSkipLink.js
+++ client/src/includes/initSkipLink.js
@@ -1,23 +1,23 @@
 const initSkipLink = () => {
-    // Inspired by https://github.com/selfthinker/dokuwiki_template_writr/blob/master/js/skip-link-focus-fix.js
+  // Inspired by https://github.com/selfthinker/dokuwiki_template_writr/blob/master/js/skip-link-focus-fix.js
 
-    const skiplink = document.querySelector('.skiplink');
-    const main = document.querySelector('main');
+  const skiplink = document.querySelector('.skiplink');
+  const main = document.querySelector('main');
 
-    const handleClick = () => {
-        main.setAttribute('tabindex', -1);
-        main.addEventListener('blur', handleBlur);
-        main.addEventListener('focusout', handleBlur);
-        main.focus();
-    };
+  const handleBlur = () => {
+    main.removeAttribute('tabindex');
+    main.removeEventListener('blur', handleBlur);
+    main.removeEventListener('focusout', handleBlur);
+  };
 
-    const handleBlur = () => {
-        main.removeAttribute('tabindex');
-        main.removeEventListener('blur', handleBlur);
-        main.removeEventListener('focusout', handleBlur);
-    };
+  const handleClick = () => {
+    main.setAttribute('tabindex', -1);
+    main.addEventListener('blur', handleBlur);
+    main.addEventListener('focusout', handleBlur);
+    main.focus();
+  };
 
-    skiplink.addEventListener('click', handleClick);
+  skiplink.addEventListener('click', handleClick);
 };
 
 export { initSkipLink };
