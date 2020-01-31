bdaa695575ae9333215a2e0afa411ef11f050605
jacobtm <jacobtm@torchbox.com>
2020-01-31 09:54:00 +0000

    Make signals work with new Notifier subclasses

--- wagtail/admin/signal_handlers.py
+++ wagtail/admin/signal_handlers.py
@@ -1,17 +1,17 @@
-from wagtail.admin.mail import GroupApprovalTaskStateSubmissionNotifier, WorkflowStateApprovalNotifier, WorkflowStateRejectionNotifier, WorkflowStateSubmissionNotifier
+from wagtail.admin.mail import GroupApprovalTaskStateSubmissionEmailNotifier, WorkflowStateApprovalEmailNotifier, WorkflowStateRejectionEmailNotifier, WorkflowStateSubmissionEmailNotifier
 from wagtail.core.models import TaskState, WorkflowState
 from wagtail.core.signals import task_submitted, workflow_approved, workflow_rejected, workflow_submitted
 
 
-task_submission_notifier = GroupApprovalTaskStateSubmissionNotifier()
-workflow_submission_notifier = WorkflowStateSubmissionNotifier()
-workflow_approval_notifier = WorkflowStateApprovalNotifier()
-workflow_rejection_notifier = WorkflowStateRejectionNotifier()
+task_submission_email_notifier = GroupApprovalTaskStateSubmissionEmailNotifier()
+workflow_submission_email_notifier = WorkflowStateSubmissionEmailNotifier()
+workflow_approval_email_notifier = WorkflowStateApprovalEmailNotifier()
+workflow_rejection_email_notifier = WorkflowStateRejectionEmailNotifier()
 
 
 def register_signal_handlers():
-    task_submitted.connect(task_submission_notifier, sender=TaskState, dispatch_uid='group_approval_task_submitted_email_notification')
+    task_submitted.connect(task_submission_email_notifier, sender=TaskState, dispatch_uid='group_approval_task_submitted_email_notification')
 
-    workflow_submitted.connect(workflow_submission_notifier, sender=WorkflowState, dispatch_uid='workflow_state_submitted_email_notification')
-    workflow_rejected.connect(workflow_rejection_notifier, sender=WorkflowState, dispatch_uid='workflow_state_rejected_email_notification')
-    workflow_approved.connect(workflow_approval_notifier, sender=WorkflowState, dispatch_uid='workflow_state_approved_email_notification')
+    workflow_submitted.connect(workflow_submission_email_notifier, sender=WorkflowState, dispatch_uid='workflow_state_submitted_email_notification')
+    workflow_rejected.connect(workflow_rejection_email_notifier, sender=WorkflowState, dispatch_uid='workflow_state_rejected_email_notification')
+    workflow_approved.connect(workflow_approval_email_notifier, sender=WorkflowState, dispatch_uid='workflow_state_approved_email_notification')
