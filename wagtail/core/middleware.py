96cd5c066d2c8aa400ed0da80497f5eeee4b7702
Matt Westcott <matt@west.co.tt>
2020-02-05 19:46:49 +0000

    Update wording
    Also move warning so it occurs on server startup rather than first request

--- wagtail/core/middleware.py
+++ wagtail/core/middleware.py
@@ -5,18 +5,20 @@ from wagtail.core.models import Site
 from wagtail.utils.deprecation import RemovedInWagtail211Warning
 
 
+warnings.warn(
+    'wagtail.core.middleware.SiteMiddleware and the use of request.site is deprecated. '
+    'Please update your code to use Site.get_for_request(request) in place of request.site, '
+    'and remove wagtail.core.middleware.SiteMiddleware from MIDDLEWARES',
+    RemovedInWagtail211Warning
+)
+
+
 class SiteMiddleware(MiddlewareMixin):
     def process_request(self, request):
         """
         Set request.site to contain the Site object responsible for handling this request,
         according to hostname matching rules
         """
-        warnings.warn(
-            'Wagtail SiteMiddleware and the use of request.site is deprecated '
-            'and will be removed in Wagtail 2.11. Update your middleware settings.',
-            RemovedInWagtail211Warning, stacklevel=2
-        )
-
         try:
             request.site = Site.find_for_request(request)
         except Site.DoesNotExist:
