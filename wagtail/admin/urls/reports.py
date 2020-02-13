8fa3ee016c6d8343508d97adbda8431616c0c757
Karl Hobley <karl@torchbox.com>
2020-02-13 16:01:40 +0000

    Initial work on workflow report

--- wagtail/admin/urls/reports.py
+++ wagtail/admin/urls/reports.py
@@ -4,5 +4,7 @@ from wagtail.admin.views import reports
 
 app_name = 'wagtailadmin_reports'
 urlpatterns = [
-    url(r'^locked/$', reports.LockedPagesView.as_view(), name='locked_pages')
+    url(r'^locked/$', reports.LockedPagesView.as_view(), name='locked_pages'),
+    url(r'^workflow/$', reports.WorkflowView.as_view(), name='workflow'),
+    url(r'^workflow/tasks/$', reports.WorkflowTasksView.as_view(), name='workflow_tasks'),
 ]
