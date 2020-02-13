a0097fdcbd809a5cae1008f1f16b6a0bfa277594
Karl Hobley <karl@torchbox.com>
2020-02-13 11:31:01 +0000

    Initial work on workflow history

--- wagtail/admin/urls/pages.py
+++ wagtail/admin/urls/pages.py
@@ -42,4 +42,6 @@ urlpatterns = [
     url(r'^(\d+)/revisions/(\d+)/revert/$', pages.revisions_revert, name='revisions_revert'),
     url(r'^(\d+)/revisions/(\d+)/unschedule/$', pages.revisions_unschedule, name='revisions_unschedule'),
     url(r'^(\d+)/revisions/compare/(live|earliest|\d+)\.\.\.(live|latest|\d+)/$', pages.revisions_compare, name='revisions_compare'),
+
+    url(r'^(\d+)/workflow_history/$', pages.workflow_history, name='workflow_history'),
 ]
