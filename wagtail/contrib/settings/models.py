12db25ae70752bc54bb679e5f1cf7595b23e5c9f
Andy Babic <andyjbabic@gmail.com>
2020-02-14 10:34:12 +0000

    Add BaseSetting.for_request() to facilitate efficient repeat access of settings when accessed before rendering

--- wagtail/contrib/settings/models.py
+++ wagtail/contrib/settings/models.py
@@ -22,7 +22,31 @@ class BaseSetting(models.Model):
     @classmethod
     def for_site(cls, site):
         """
-        Get an instance of this setting for the site.
+        Get or create an instance of this setting for the site.
         """
         instance, created = cls.objects.get_or_create(site=site)
         return instance
+
+    @classmethod
+    def for_request(cls, request):
+        """
+        Get or create an instance of this model for the request,
+        and cache the result on the request for faster repeat access.
+        """
+        attr_name = cls.get_cache_attr_name()
+        if hasattr(request, attr_name):
+            return getattr(request, attr_name)
+        site = Site.find_for_request(request)
+        site_settings = cls.for_site(site)
+        setattr(request, attr_name, site_settings)
+        return site_settings
+
+    @classmethod
+    def get_cache_attr_name(cls):
+        """
+        Returns the name of the attribute that should be used to store
+        a reference to the fetched/created object on a request.
+        """
+        return "_{}.{}".format(
+            cls._meta.app_label, cls._meta.model_name
+        ).lower()
