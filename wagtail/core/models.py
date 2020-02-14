171cf2c38322855996141fce028e8b43b17485dc
Storm Heg <storm.heg@torchbox.com>
2020-02-14 16:21:42 +0000

    Fix regression where Page.get_site() was not not working

--- wagtail/core/models.py
+++ wagtail/core/models.py
@@ -835,7 +835,7 @@ class Page(AbstractPage, index.Indexed, ClusterableModel, metaclass=PageBase):
             page_path = reverse(
                 'wagtail_serve', args=(self.url_path[len(root_path):],))
         except NoReverseMatch:
-            return None
+            return (site_id, None, None)
 
         # Remove the trailing slash from the URL reverse generates if
         # WAGTAIL_APPEND_SLASH is False and we're not trying to serve
@@ -849,7 +849,7 @@ class Page(AbstractPage, index.Indexed, ClusterableModel, metaclass=PageBase):
         """Return the full URL (including protocol / domain) to this page, or None if it is not routable"""
         url_parts = self.get_url_parts(request=request)
 
-        if url_parts is None:
+        if url_parts is None or url_parts[1] is None and url_parts[2] is None:
             # page is not routable
             return
 
@@ -881,7 +881,7 @@ class Page(AbstractPage, index.Indexed, ClusterableModel, metaclass=PageBase):
             current_site = site
         url_parts = self.get_url_parts(request=request)
 
-        if url_parts is None:
+        if url_parts is None or url_parts[1] is None and url_parts[2] is None:
             # page is not routable
             return
 
