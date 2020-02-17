7d6f7bb4ba32b7b34b58f90dcbc7a65151140ba5
Kalob Taulien <kalob.taulien@gmail.com>
2020-02-17 11:05:54 -0700

    Toggle permission columns on/off

--- wagtail/users/static_src/wagtailusers/js/group-form.js
+++ wagtail/users/static_src/wagtailusers/js/group-form.js
@@ -10,4 +10,18 @@ $(function() {
             });
         }
     });
+
+    const toggleBoxes = document.querySelectorAll('.js-toggle-permission');
+
+    toggleBoxes.forEach(node => {
+        node.addEventListener('change', function() {
+            const type = this.dataset.for;
+            const boxes = document.querySelectorAll(`.${type}-column input[type="checkbox"]`);
+            if(this.checked) {
+                boxes.forEach(element => element.checked = true);
+            } else {
+                boxes.forEach(element => element.checked = false);
+            }
+        });
+    });
 });
