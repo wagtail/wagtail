926c24f172e195504ce794c2a642651957013974
Karl Hobley <karl@torchbox.com>
2020-02-21 09:23:40 +0000

    Retrieve tasks in order they are on the workflow

--- wagtail/core/models.py
+++ wagtail/core/models.py
@@ -2603,7 +2603,7 @@ class Workflow(ClusterableModel):
 
     @property
     def tasks(self):
-        return Task.objects.filter(workflow_tasks__workflow=self)
+        return Task.objects.filter(workflow_tasks__workflow=self).order_by('workflow_tasks__sort_order')
 
     @transaction.atomic
     def start(self, page, user):
