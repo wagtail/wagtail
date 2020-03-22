067adfbbc82bb9427c881addc75590f7b5f0d067
Kalob Taulien <kalob.taulien@gmail.com>
2020-03-22 12:01:27 -0600

    IE11 friendly JavaScript

--- wagtail/users/static_src/wagtailusers/js/group-form.js
+++ wagtail/users/static_src/wagtailusers/js/group-form.js
@@ -11,17 +11,19 @@ $(function() {
         }
     });
 
+    // "Check all" boxes for the Group edit view.
     const toggleBoxes = document.querySelectorAll('.js-toggle-permission');
-
-    toggleBoxes.forEach(node => {
-        node.addEventListener('change', function() {
-            const type = this.dataset.for;
-            const boxes = document.querySelectorAll(`.${type}-column input[type="checkbox"]`);
-            if(this.checked) {
-                boxes.forEach(element => element.checked = true);
-            } else {
-                boxes.forEach(element => element.checked = false);
-            }
-        });
-    });
+    for(let toggleBox = 0; toggleBox < toggleBoxes.length; toggleBox++) {
+      const node = toggleBoxes[toggleBox];
+      node.addEventListener('change', function() {
+        const type = this.dataset.for;
+        const selector = '.' + type + '-column input[type="checkbox"]';
+        const boxes = document.querySelectorAll(selector);
+        const checked = this.checked ? true : false;
+          for(let i = 0; i < boxes.length; i++) {
+              const element = boxes[i];
+              element.checked = checked;
+          }
+      });
+    }
 });
