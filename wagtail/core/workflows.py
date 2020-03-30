9af37817187d21d1fed73185bb3eb235793dd534
jacobtoppm <jacobtm@torchbox.com>
2020-03-30 17:39:32 +0100

    Make selection of a task type to create take account of the potential full inheritance tree rather than just the next level, and prevent creation of abstract tasks

--- wagtail/core/workflows.py
+++ wagtail/core/workflows.py
@@ -1,3 +1,27 @@
+from wagtail.core.models import Task
+
+
+TASK_TYPES = []
+
+def get_concrete_descendants(model_class, inclusive=True):
+    """Retrieves non-abstract descendants of the given model class. If `inclusive` is set to
+    True, includes model_class"""
+    subclasses = model_class.__subclasses__()
+    if subclasses:
+        for subclass in subclasses:
+            yield from get_concrete_descendants(subclass)
+    if inclusive and not model_class._meta.abstract:
+        yield model_class
+
+
+def get_task_types(task_class=None):
+    global TASK_TYPES
+    if TASK_TYPES:
+        return TASK_TYPES
+    TASK_TYPES = list(get_concrete_descendants(Task, inclusive=False))
+    return TASK_TYPES
+
+
 def publish_workflow_state(workflow_state):
     # publish the Page associated with a WorkflowState
     if workflow_state.current_task_state:
