c54c15ee429b41626b6aedd11de8bd5fe749fa03
Matt Westcott <matt@west.co.tt>
2020-04-22 12:14:40 +0100

    Version bump to start work on 2.10

--- wagtail/__init__.py
+++ wagtail/__init__.py
@@ -5,7 +5,7 @@ from wagtail.utils.version import get_semver_version, get_version
 
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (2, 9, 0, 'alpha', 0)
+VERSION = (2, 10, 0, 'alpha', 0)
 
 __version__ = get_version(VERSION)
 
