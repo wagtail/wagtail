b85879b21063d31db7c811f825141a96a05bc67b
Matt Westcott <matt@west.co.tt>
2020-04-29 23:47:12 +0100

    Move button select widgets to wagtail.admin.widgets

--- wagtail/admin/widgets/__init__.py
+++ wagtail/admin/widgets/__init__.py
@@ -1,5 +1,6 @@
 from wagtail.admin.widgets.auto_height_text import *  # NOQA
 from wagtail.admin.widgets.button import *  # NOQA
+from wagtail.admin.widgets.button_select import *  # NOQA
 from wagtail.admin.widgets.chooser import *  # NOQA
 from wagtail.admin.widgets.datetime import *  # NOQA
 from wagtail.admin.widgets.tags import *  # NOQA
