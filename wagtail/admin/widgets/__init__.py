3314c5d95feca175485c2eb2cf8ad0dd2287c88a
Matt Westcott <matt@west.co.tt>
2020-04-30 17:33:59 +0100

    Move FilteredSelect to wagtail.admin.widgets

--- wagtail/admin/widgets/__init__.py
+++ wagtail/admin/widgets/__init__.py
@@ -3,4 +3,5 @@ from wagtail.admin.widgets.button import *  # NOQA
 from wagtail.admin.widgets.button_select import *  # NOQA
 from wagtail.admin.widgets.chooser import *  # NOQA
 from wagtail.admin.widgets.datetime import *  # NOQA
+from wagtail.admin.widgets.filtered_select import *  # NOQA
 from wagtail.admin.widgets.tags import *  # NOQA
