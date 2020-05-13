581da602fe0124b815c6d8122a7c86c43d57da9e
Karl Hobley <karl@torchbox.com>
2020-05-13 16:03:16 +0100

    Make ADD/MULL/AND/OR operators combine query classes in a balanced way
    To prevent recursion errors

--- wagtail/search/tests/test_queries.py
+++ wagtail/search/tests/test_queries.py
@@ -9,7 +9,7 @@ from wagtail.contrib.search_promotions.models import SearchPromotion
 from wagtail.search import models
 from wagtail.search.query import And, Or, Phrase, PlainText
 from wagtail.search.utils import (
-    normalise_query_string, parse_query_string, separate_filters_from_query)
+    balanced_reduce, normalise_query_string, parse_query_string, separate_filters_from_query)
 from wagtail.tests.utils import WagtailTestUtils
 
 
@@ -266,3 +266,108 @@ class TestParseQueryString(SimpleTestCase):
         filters, query = parse_query_string('"hello world" "hi earth"')
 
         self.assertEqual(repr(query), repr(And([Phrase("hello world"), Phrase("hi earth")])))
+
+
+class TestBalancedReduce(SimpleTestCase):
+    # For simple values, this should behave exactly the same as Pythons reduce()
+    # So I've copied its tests: https://github.com/python/cpython/blob/21cdb711e3b1975398c54141e519ead02670610e/Lib/test/test_functools.py#L771
+
+    def test_reduce(self):
+        class Squares:
+            def __init__(self, max):
+                self.max = max
+                self.sofar = []
+
+            def __len__(self):
+                return len(self.sofar)
+
+            def __getitem__(self, i):
+                if not 0 <= i < self.max:
+                    raise IndexError
+                n = len(self.sofar)
+                while n <= i:
+                    self.sofar.append(n * n)
+                    n += 1
+                return self.sofar[i]
+
+        def add(x, y):
+            return x + y
+
+        self.assertEqual(balanced_reduce(add, ['a', 'b', 'c'], ''), 'abc')
+        self.assertEqual(
+            balanced_reduce(add, [['a', 'c'], [], ['d', 'w']], []),
+            ['a', 'c', 'd', 'w']
+        )
+        self.assertEqual(balanced_reduce(lambda x, y: x * y, range(2, 8), 1), 5040)
+        self.assertEqual(
+            balanced_reduce(lambda x, y: x * y, range(2, 21), 1),
+            2432902008176640000
+        )
+        self.assertEqual(balanced_reduce(add, Squares(10)), 285)
+        self.assertEqual(balanced_reduce(add, Squares(10), 0), 285)
+        self.assertEqual(balanced_reduce(add, Squares(0), 0), 0)
+        self.assertRaises(TypeError, balanced_reduce)
+        self.assertRaises(TypeError, balanced_reduce, 42, 42)
+        self.assertRaises(TypeError, balanced_reduce, 42, 42, 42)
+        self.assertEqual(balanced_reduce(42, "1"), "1")  # func is never called with one item
+        self.assertEqual(balanced_reduce(42, "", "1"), "1")  # func is never called with one item
+        self.assertRaises(TypeError, balanced_reduce, 42, (42, 42))
+        self.assertRaises(TypeError, balanced_reduce, add, [])  # arg 2 must not be empty sequence with no initial value
+        self.assertRaises(TypeError, balanced_reduce, add, "")
+        self.assertRaises(TypeError, balanced_reduce, add, ())
+        self.assertRaises(TypeError, balanced_reduce, add, object())
+
+        class TestFailingIter:
+            def __iter__(self):
+                raise RuntimeError
+
+        self.assertRaises(RuntimeError, balanced_reduce, add, TestFailingIter())
+
+        self.assertEqual(balanced_reduce(add, [], None), None)
+        self.assertEqual(balanced_reduce(add, [], 42), 42)
+
+        class BadSeq:
+            def __getitem__(self, index):
+                raise ValueError
+
+        self.assertRaises(ValueError, balanced_reduce, 42, BadSeq())
+
+    # Test reduce()'s use of iterators.
+    def test_iterator_usage(self):
+        class SequenceClass:
+            def __init__(self, n):
+                self.n = n
+
+            def __getitem__(self, i):
+                if 0 <= i < self.n:
+                    return i
+                else:
+                    raise IndexError
+
+        from operator import add
+        self.assertEqual(balanced_reduce(add, SequenceClass(5)), 10)
+        self.assertEqual(balanced_reduce(add, SequenceClass(5), 42), 52)
+        self.assertRaises(TypeError, balanced_reduce, add, SequenceClass(0))
+        self.assertEqual(balanced_reduce(add, SequenceClass(0), 42), 42)
+        self.assertEqual(balanced_reduce(add, SequenceClass(1)), 0)
+        self.assertEqual(balanced_reduce(add, SequenceClass(1), 42), 42)
+
+        d = {"one": 1, "two": 2, "three": 3}
+        self.assertEqual(balanced_reduce(add, d), "".join(d.keys()))
+
+    # This test is specific to balanced_reduce
+    def test_is_balanced(self):
+        # Tests that balanced_reduce returns the object as a balanced tree
+        class CombinedNode:
+            def __init__(self, a, b):
+                self.a = a
+                self.b = b
+
+            def __repr__(self):
+                return '(%s %s)' % (self.a, self.b)
+
+        self.assertEqual(
+            repr(balanced_reduce(CombinedNode, ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'])),
+            '(((A B) (C D)) ((E F) (G H)))'
+            # Note: functools.reduce will return '(((((((A B) C) D) E) F) G) H)'
+        )
