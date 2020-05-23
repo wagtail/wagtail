08edb435ff43157e42eacefef3b66348bf2a3e08
Vasilis Mavromatis <8668731+vmavromatis@users.noreply.github.com>
2020-05-23 13:13:29 +0200

    Add ReadOnlyPanel
    https://github.com/wagtail/wagtail/issues/2893

--- wagtail/admin/edit_handlers.py
+++ wagtail/admin/edit_handlers.py
@@ -8,6 +8,7 @@ from django.forms.formsets import DELETION_FIELD_NAME, ORDERING_FIELD_NAME
 from django.forms.models import fields_for_model
 from django.template.loader import render_to_string
 from django.utils.functional import cached_property
+from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy
 from taggit.managers import TaggableManager
@@ -844,3 +845,38 @@ class StreamFieldPanel(FieldPanel):
     def on_model_bound(self):
         super().on_model_bound()
         self.block_def = self.db_field.stream_block
+
+
+class ReadOnlyPanel(EditHandler):
+    def __init__(self, attr, *args, **kwargs):
+        self.attr = attr
+        super().__init__(*args, **kwargs)
+
+    def clone(self):
+        return self.__class__(
+            attr=self.attr,
+            heading=self.heading,
+            classname=self.classname,
+            help_text=self.help_text,
+        )
+
+    def render(self):
+        value = getattr(self.instance, self.attr)
+        if callable(value):
+            value = value()
+        return format_html('<div style="padding-top: 1.2em;">{}</div>', value)
+
+    def render_as_object(self):
+        return format_html(
+            '<fieldset><legend>{}</legend>'
+            '<ul class="fields"><li><div class="field">{}</div></li></ul>'
+            '</fieldset>',
+            self.heading, self.render())
+
+    def render_as_field(self):
+        return format_html(
+            '<div class="field">'
+            '<label>{}{}</label>'
+            '<div class="field-content">{}</div>'
+            '</div>',
+            self.heading, ':', self.render())
