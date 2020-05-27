32e1bb112e77caa065e5c7ea09658df0888f4e19
Thibaud Colas <thibaudcolas@gmail.com>
2020-05-27 07:56:34 +0100

    Fix RestaurantTag migration tests introducing an extra migration (#6075)
    Due to the verbose_name being lowercased in django-taggit v1.3.0, https://github.com/jazzband/django-taggit/commit/90c7224018c941b9a260c8e8bed166536f5870df.

--- wagtail/tests/testapp/migrations/0047_restaurant_tags.py
+++ wagtail/tests/testapp/migrations/0047_restaurant_tags.py
@@ -28,8 +28,8 @@ class Migration(migrations.Migration):
             name='RestaurantTag',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100, unique=True, verbose_name='Name')),
-                ('slug', models.SlugField(max_length=100, unique=True, verbose_name='Slug')),
+                ('name', models.CharField(max_length=100, unique=True, verbose_name='name')),
+                ('slug', models.SlugField(max_length=100, unique=True, verbose_name='slug')),
             ],
             options={
                 'verbose_name': 'Tag',
