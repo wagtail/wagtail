a9e1d617f96cb51bc918254ec7e4f460b3aa4e3d
Dan Braghis <dan.braghis@torchbox.com>
2020-05-29 16:23:56 +0100

    Rename simpletask test migration

--- wagtail/tests/testapp/migrations/0052_simpletask.py
+++ wagtail/tests/testapp/migrations/0052_simpletask.py
@@ -8,7 +8,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('wagtailcore', '0046_add_workflow_models'),
-        ('tests', '0049_rawhtmlblock'),
+        ('tests', '0051_tag_verbose_name'),
     ]
 
     operations = [
