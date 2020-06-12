1945ffcc5ef9e8389408379b64e105114fa7d643
Dan Braghis <dan.braghis@torchbox.com>
2020-06-12 15:45:35 +0100

    Re-label approve/reject

--- wagtail/core/tests/test_workflow.py
+++ wagtail/core/tests/test_workflow.py
@@ -259,3 +259,13 @@ class TestWorkflows(TestCase):
         self.assertEqual(
             list(task.active_workflows), [workflow]
         )
+
+    def test_is_at_final_task(self):
+        # test that a Workflow rejected on its second Task can be resumed on the second task
+        data = self.start_workflow_on_homepage()
+        workflow_state = data['workflow_state']
+
+        self.assertFalse(workflow_state.is_at_final_task)
+        workflow_state.current_task_state.approve(user=None)
+        workflow_state.refresh_from_db()
+        self.assertTrue(workflow_state.is_at_final_task)
