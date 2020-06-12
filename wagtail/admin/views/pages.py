399c48159dee79095e65ae6fc33dcc70765c560b
jacobtoppm <55137073+jacobtoppm@users.noreply.github.com>
2020-06-12 17:09:46 +0100

    [Workflow] Commenting (#6136)
    * Add comment to TaskState
    
    * Add ability for Task on_action to accept kwargs, and a get_form_for_action method to request a form to enter addition kwargs for an action
    
    * Refactor workflow_action to take more arguments rather than getting them from POST data, and add a GET version that returns a modal. Use this to add a way to add approve/reject comments on tasks
    
    * Make tasks set finished_by
    
    * Make workflow history timeline view the default, and fix comment display
    
    * Add link to workflow history/comments on 'needs changes' message
    
    * Fix tests to use new workflow action url
    
    * Remove now-unnecessary check for TaskState copying on notifier
    
    * Add comments to rejection emails
    
    * Change needs changes banner to warning style, and reverse timeline order to show more recent events and crucially comments first
    
    * Create template include for Wagtail standard form rendering, and use it in workflow action modal

--- wagtail/admin/views/pages.py
+++ wagtail/admin/views/pages.py
@@ -28,6 +28,7 @@ from wagtail.admin.auth import user_has_any_page_permission, user_passes_test
 from wagtail.admin.forms.pages import CopyForm
 from wagtail.admin.forms.search import SearchForm
 from wagtail.admin.mail import send_notification
+from wagtail.admin.modal_workflow import render_modal_workflow
 from wagtail.admin.navigation import get_explorable_root_page
 from wagtail.core import hooks
 from wagtail.core.models import (
@@ -428,23 +429,28 @@ def edit(request, page_id):
                     reverse('wagtailadmin_pages:revisions_compare', args=(page.id, 'live', latest_revision.id)),
                     _('Compare with live version')
                 ))
+
             if workflow_state.status == WorkflowState.STATUS_NEEDS_CHANGES:
                 workflow_info = _("Changes were requested on this page")
-            # Check for revisions still undergoing moderation and warn
-            elif len(workflow_tasks) == 1:
-                # If only one task in workflow, show simple message
-                workflow_info = _("This page is currently awaiting moderation")
-            elif current_task_number:
-                workflow_info = format_html(_("<b>Page '{}'</b> is on <b>Task {} of {}: '{}'</b> in <b>Workflow '{}'</b>. "), page.get_admin_display_title(), current_task_number, len(workflow_tasks), task.name, workflow.name)
+                buttons = [messages.button(reverse('wagtailadmin_pages:workflow_history_detail', args=(page.id, workflow_state.id,)), _('See comments / history')), ] + buttons
+                messages.warning(request, workflow_info, buttons=buttons, extra_tags='workflow')
             else:
-                workflow_info = format_html(_("<b>Page '{}'</b> is on <b>Task '{}'</b> in <b>Workflow '{}'</b>. "), page.get_admin_display_title(), current_task_number, len(workflow_tasks), task.name, workflow.name)
 
-            if task.page_locked_for_user(page, request.user):
-                messages.error(request, mark_safe(workflow_info + _("Only reviewers for this task can edit the page.")), buttons=buttons, extra_tags="lock")
-            elif task_has_been_approved and getattr(settings, 'WAGTAIL_WORKFLOW_REQUIRE_REAPPROVAL_ON_EDIT', False):
-                messages.warning(request, mark_safe(workflow_info + _("Editing this Page will cause completed Tasks to need re-approval.")), buttons=buttons, extra_tags="workflow")
-            else:
-                messages.success(request, workflow_info, buttons=buttons, extra_tags="workflow")
+                # Check for revisions still undergoing moderation and warn
+                if len(workflow_tasks) == 1:
+                    # If only one task in workflow, show simple message
+                    workflow_info = _("This page is currently awaiting moderation")
+                elif current_task_number:
+                    workflow_info = format_html(_("<b>Page '{}'</b> is on <b>Task {} of {}: '{}'</b> in <b>Workflow '{}'</b>. "), page.get_admin_display_title(), current_task_number, len(workflow_tasks), task.name, workflow.name)
+                else:
+                    workflow_info = format_html(_("<b>Page '{}'</b> is on <b>Task '{}'</b> in <b>Workflow '{}'</b>. "), page.get_admin_display_title(), current_task_number, len(workflow_tasks), task.name, workflow.name)
+
+                if task.page_locked_for_user(page, request.user):
+                    messages.error(request, mark_safe(workflow_info + _("Only reviewers for this task can edit the page.")), buttons=buttons, extra_tags="lock")
+                elif task_has_been_approved and getattr(settings, 'WAGTAIL_WORKFLOW_REQUIRE_REAPPROVAL_ON_EDIT', False):
+                    messages.warning(request, mark_safe(workflow_info + _("Editing this Page will cause completed Tasks to need re-approval.")), buttons=buttons, extra_tags="workflow")
+                else:
+                    messages.success(request, workflow_info, buttons=buttons, extra_tags="workflow")
     else:
         # Show last workflow state
         workflow_state = page.workflow_states.order_by('created_at').last()
@@ -1267,8 +1273,9 @@ def reject_moderation(request, revision_id):
     return redirect('wagtailadmin_home')
 
 
-@require_POST
-def workflow_action(request, page_id):
+def workflow_action(request, page_id, action_name, task_state_id):
+    """Provides a modal view to enter additional data for the specified workflow action on GET,
+    or perform the specified action on POST"""
     page = get_object_or_404(Page, id=page_id)
 
     redirect_to = request.POST.get('next', None)
@@ -1279,25 +1286,65 @@ def workflow_action(request, page_id):
         messages.error(request, _("The page '{0}' is not currently awaiting moderation.").format(page.get_admin_display_title()))
         return redirect(redirect_to)
 
-    task_state_id = request.POST.get('task_state_id', None)
-
-    task_state = TaskState.objects.get(id=task_state_id) if task_state_id else page.current_workflow_task_state
+    task_state = get_object_or_404(TaskState, id=task_state_id)
     task_state = task_state.specific
 
     task = task_state.task.specific
 
     actions = task.get_actions(page, request.user)
+    action_verbose_name = ''
+    action_available = False
+    action_modal = False
 
-    action_name = request.POST.get('action')
-
-    if action_name not in set(action[0] for action in actions):
+    for name, verbose_name, modal in actions:
+        if name == action_name:
+            action_available = True
+            if modal:
+                action_modal = True
+                # if two actions have the same name, use the verbose name of the one allowing modal data entry
+                # within the modal
+                action_verbose_name = verbose_name
+    if not action_available:
         raise PermissionDenied
 
-    response = task.on_action(task_state, request.user, action_name)
-    if response:
-        return response
+    form_class = task.get_form_for_action(action_name)
 
-    return redirect(redirect_to)
+    if request.method == 'POST':
+        if form_class:
+            form = form_class(request.POST)
+            if form.is_valid():
+                redirect_to = task.on_action(task_state, request.user, action_name, **form.cleaned_data) or redirect_to
+            elif action_modal and request.is_ajax:
+                # show form errors
+                return render_modal_workflow(
+                    request, 'wagtailadmin/pages/workflow_action_modal.html', None, {
+                        'page': page,
+                        'form': form,
+                        'action': action_name,
+                        'action_verbose': action_verbose_name,
+                        'task_state': task_state,
+                    },
+                    json_data={'step': 'action'}
+                )
+        else:
+            redirect_to = task.on_action(task_state, request.user, action_name) or redirect_to
+
+        if request.is_ajax:
+            return render_modal_workflow(request, '', None, {}, json_data={'step': 'success', 'redirect': redirect_to})
+        return redirect(redirect_to)
+    else:
+        form = form_class()
+        return render_modal_workflow(
+            request, 'wagtailadmin/pages/workflow_action_modal.html', None, {
+                'page': page,
+                'form': form,
+                'action': action_name,
+                'action_verbose': action_verbose_name,
+                'task_state': task_state,
+            },
+            json_data={'step': 'action'}
+        )
+
 
 
 @require_GET
@@ -1602,7 +1649,7 @@ def workflow_history_detail(request, page_id, workflow_state_id):
     page_revisions = PageRevision.objects.filter(
         page=page,
         id__in=TaskState.objects.filter(workflow_state=workflow_state).values_list('page_revision_id', flat=True)
-    ).order_by('created_at')
+    ).order_by('-created_at')
 
     # Now get QuerySet of tasks completed for each revision
     task_states_by_revision_task = [
@@ -1644,7 +1691,7 @@ def workflow_history_detail(request, page_id, workflow_state_id):
         }
     ]
 
-    if workflow_state.status != WorkflowState.STATUS_IN_PROGRESS:
+    if workflow_state.status not in (WorkflowState.STATUS_IN_PROGRESS, WorkflowState.STATUS_NEEDS_CHANGES):
         last_task = completed_task_states.order_by('finished_at').last()
         if last_task:
             timeline.append({
@@ -1668,6 +1715,7 @@ def workflow_history_detail(request, page_id, workflow_state_id):
         })
 
     timeline.sort(key=lambda t: t['time'])
+    timeline.reverse()
 
     return TemplateResponse(request, 'wagtailadmin/pages/workflow_history/detail.html', {
         'page': page,
