319dc4cb0f8827b978974e33b2e5cadc4a47b60e
Karl Hobley <karl@torchbox.com>
2020-06-24 14:59:14 +0100

    Implemented task chooser modal (#6169)

--- wagtail/core/utils.py
+++ wagtail/core/utils.py
@@ -16,6 +16,17 @@ def camelcase_to_underscore(str):
     return re.sub('(((?<=[a-z])[A-Z])|([A-Z](?![A-Z]|$)))', '_\\1', str).lower().strip('_')
 
 
+def get_model_string(model):
+    """
+    Returns a string that can be used to identify the specified model.
+
+    The format is: `app_label.ModelName`
+
+    This an be reversed with the `resolve_model_string` function
+    """
+    return model._meta.app_label + '.' + model.__name__
+
+
 def resolve_model_string(model_string, default_app=None):
     """
     Resolve an 'app_label.model_name' string into an actual model class.
