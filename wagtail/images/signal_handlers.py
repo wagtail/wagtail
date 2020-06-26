849bee6f7233abdaf73a8239ec52c7cf0059f5c8
Matt Westcott <matt@west.co.tt>
2020-06-26 12:54:37 +0100

    Purge image rendition cache on rendition deletion
    Fixes #6096

--- wagtail/images/signal_handlers.py
+++ wagtail/images/signal_handlers.py
@@ -10,6 +10,10 @@ def post_delete_file_cleanup(instance, **kwargs):
     transaction.on_commit(lambda: instance.file.delete(False))
 
 
+def post_delete_purge_rendition_cache(instance, **kwargs):
+    instance.purge_from_cache()
+
+
 def pre_save_image_feature_detection(instance, **kwargs):
     if getattr(settings, 'WAGTAILIMAGES_FEATURE_DETECTION_ENABLED', False):
         # Make sure the image doesn't already have a focal point
@@ -25,3 +29,4 @@ def register_signal_handlers():
     pre_save.connect(pre_save_image_feature_detection, sender=Image)
     post_delete.connect(post_delete_file_cleanup, sender=Image)
     post_delete.connect(post_delete_file_cleanup, sender=Rendition)
+    post_delete.connect(post_delete_purge_rendition_cache, sender=Rendition)
