01c7a3bd34d3d5310bd4257ad7cb2a3338e682e1
Matt Westcott <matt@west.co.tt>
2020-07-03 17:05:04 +0100

    Use new-style URL paths for password reset views
    This matches what Django has done in the corresponding views:
    https://github.com/django/django/blob/5d4b9c1cab03f0d057f0c7751862df0302c65cf9/django/contrib/auth/urls.py
    and prevents it from breaking on Django 3.1 (because the token is now longer than the 13+20 chars allowed by the original regexp).

--- wagtail/admin/urls/password_reset.py
+++ wagtail/admin/urls/password_reset.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path
+from django.urls import path
 
 from wagtail.admin.views import account
 
@@ -9,8 +9,8 @@ urlpatterns = [
     path(
         'done/', account.PasswordResetDoneView.as_view(), name='wagtailadmin_password_reset_done'
     ),
-    re_path(
-        r'^confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    path(
+        'confirm/<uidb64>/<token>/',
         account.PasswordResetConfirmView.as_view(), name='wagtailadmin_password_reset_confirm',
     ),
     path(
