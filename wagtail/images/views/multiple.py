b5d2233fe28d58c10020164611946206b5ade5d4
Matt Westcott <matt@west.co.tt>
2020-07-06 12:34:50 +0100

    fixup! Replace a few instances of re_path with path

--- wagtail/images/views/multiple.py
+++ wagtail/images/views/multiple.py
@@ -207,7 +207,7 @@ def create_from_uploaded_image(request, uploaded_image_id):
 
     image = Image()
     form = ImageForm(
-        request.POST, request.FILES, instance=image, prefix='uploaded-image-' + uploaded_image_id, user=request.user
+        request.POST, request.FILES, instance=image, prefix='uploaded-image-%d' % uploaded_image_id, user=request.user
     )
 
     if form.is_valid():
