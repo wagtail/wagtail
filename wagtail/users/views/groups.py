3fde2999d4132eb21fdd30ec0e3201338080d532
Matt Westcott <matt@west.co.tt>
2020-07-06 12:00:17 +0100

    fixup! Replace calls to django.conf.urls.url with django.urls.path or django.urls.re_path

--- wagtail/users/views/groups.py
+++ wagtail/users/views/groups.py
@@ -1,5 +1,5 @@
-from django.urls import re_path
 from django.contrib.auth.models import Group
+from django.urls import re_path
 from django.utils.translation import gettext as _
 
 from wagtail.admin.views import generic, mixins
@@ -7,6 +7,7 @@ from wagtail.admin.viewsets.model import ModelViewSet
 from wagtail.core import hooks
 from wagtail.users.forms import GroupForm, GroupPagePermissionFormSet
 from wagtail.users.views.users import index
+
 _permission_panel_classes = None
 
 
