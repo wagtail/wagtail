10312cb4da29b842ec04ed3c6f318664a5116749
Andy Babic <andy.babic@torchbox.com>
2020-07-07 16:30:53 +0100

    Raise a custom exception when attempting to edit a page who's model class cannot be found in the codebase

---
+++ wagtail/core/exceptions.py
@@ -0,0 +1,8 @@
+class PageClassNotFoundError(ImportError):
+    """
+    Raised when a model class referenced by a page object's ``content_type``
+    value cannot be found in the codebase. Usually, this is as a result of
+    switching to a different git branch without first running/reverting
+    migrations.
+    """
+    pass
