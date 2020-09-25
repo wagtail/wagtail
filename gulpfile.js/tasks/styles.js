ae533dfc21f90c3e75e93766ae9c498291ebaa20
JNaftali <jmarantz@thelabnyc.com>
2020-09-24 22:58:45 -0400

    Implement CSS variables for admin color theming

--- gulpfile.js/tasks/styles.js
+++ gulpfile.js/tasks/styles.js
@@ -4,6 +4,8 @@ var sass = require('gulp-dart-sass');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
+var postcssCustomProperties = require('postcss-custom-properties');
+var postcssCalc = require('postcss-calc');
 var sourcemaps = require('gulp-sourcemaps');
 var size = require('gulp-size');
 var config = require('../config');
@@ -69,6 +71,8 @@ gulp.task('styles:sass', function () {
         .pipe(postcss([
           cssnano(cssnanoConfig),
           autoprefixer(autoprefixerConfig),
+          postcssCustomProperties(),
+          postcssCalc(),
         ]))
         .pipe(size({ title: 'Wagtail CSS' }))
         .pipe(config.isProduction ? gutil.noop() : sourcemaps.write())
