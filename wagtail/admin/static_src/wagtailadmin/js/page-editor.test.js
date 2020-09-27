aa235a40fdf50af9a885872f3caf021c11d17783
Matt Westcott <matt@west.co.tt>
2020-09-27 13:50:48 +0100

    Take out stopword removal, as per https://github.com/django/django/commit/62f1655a64795d055f72e53557fb8404c5430963
    Fixes #4899

--- wagtail/admin/static_src/wagtailadmin/js/page-editor.test.js
+++ wagtail/admin/static_src/wagtailadmin/js/page-editor.test.js
@@ -13,10 +13,10 @@ describe('page-editor tests', () => {
       /* true triggers to use django's urlify */
       expect(cleanForSlug('Before', true)).toBe('before');
       expect(cleanForSlug('The', true)).toBe('the');
-      expect(cleanForSlug('Before the sun rises', true)).toBe('sun-rises');
+      expect(cleanForSlug('Before the sun rises', true)).toBe('before-the-sun-rises');
       expect(cleanForSlug('ON', true)).toBe('on');
-      expect(cleanForSlug('ON this day in november', true)).toBe('day-november');
-      expect(cleanForSlug('This & That', true)).toBe('this--that');
+      expect(cleanForSlug('ON this day in november', true)).toBe('on-this-day-in-november');
+      expect(cleanForSlug('This & That', true)).toBe('this-that');
     });
 
     it('should return a correct slug which is escaped by urlify', () => {
