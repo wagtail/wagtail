00c5445b28aad973e01d4dbfedace0673eaf219b
Karl Hobley <karl@kaed.uk>
2020-10-02 17:02:08 +0100

    Update pyenchant

--- setup.py
+++ setup.py
@@ -75,8 +75,8 @@ testing_extras = [
 
 # Documentation dependencies
 documentation_extras = [
-    'pyenchant==1.6.8',
-    'sphinxcontrib-spelling>=2.3.0',
+    'pyenchant>=3.1.1,<4',
+    'sphinxcontrib-spelling>=5.4.0,<6',
     'Sphinx>=1.5.2',
     'sphinx-autobuild>=0.6.0',
     'sphinx_rtd_theme>=0.1.9',
