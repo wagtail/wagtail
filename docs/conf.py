74eda068df517e55d577094eaa3750ce606e45cc
Karl Hobley <karl@kaed.uk>
2020-10-04 15:57:00 +0100

    Change docs code block style to 'default'

--- docs/conf.py
+++ docs/conf.py
@@ -115,7 +115,7 @@ exclude_patterns = ['_build']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
