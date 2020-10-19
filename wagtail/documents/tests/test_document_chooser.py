45527164d91029a12d4051a0e8584c3e48ed5c6c
Robert Rollins <rrollins@caltech.edu>
2020-10-19 15:31:36 -0700

    Added tests for Choose permissions to the Images and Documents test suites

--- wagtail/documents/tests/test_document_chooser.py
+++ wagtail/documents/tests/test_document_chooser.py
@@ -46,6 +46,35 @@ class TestChooser(TestCase, WagtailTestUtils):
         # Log in as a non-superuser editor
         self.login(user)
 
+    def login_as_baker(self):
+        # Create group with access to admin and Chooser permission on one Collection, but not another.
+        bakers_group = Group.objects.create(name='Bakers')
+        access_admin_perm = Permission.objects.get(
+            content_type__app_label='wagtailadmin',
+            codename='access_admin'
+        )
+        bakers_group.permissions.add(access_admin_perm)
+        # Create the "Bakery" Collection and grant "choose" permission to the Bakers group.
+        root = Collection.objects.get(id=get_root_collection_id())
+        bakery_collection = root.add_child(instance=Collection(name='Bakery'))
+        GroupCollectionPermission.objects.create(
+            group=bakers_group,
+            collection=bakery_collection,
+            permission=Permission.objects.get(
+                content_type__app_label='wagtaildocs',
+                codename='choose_document'
+            )
+        )
+        # Create the "Office" Collection and _don't_ grant any permissions to the Bakers group.
+        root.add_child(instance=Collection(name='Office'))
+
+        # Create a Baker user.
+        user = self.create_user(username="baker", password="password")
+        user.groups.add(bakers_group)
+
+        # Log in as the baker.
+        self.login(user)
+
     def get(self, params=None):
         return self.client.get(reverse('wagtaildocs:chooser'), params or {})
 
@@ -68,6 +97,63 @@ class TestChooser(TestCase, WagtailTestUtils):
         self.assertNotContains(response, self._NO_DOCS_TEXT)
         self.assertNotContains(response, self._NO_COLLECTION_DOCS_TEXT)
         self.assertNotContains(response, self._UPLOAD_ONE_TEXT)
+
+    def test_chooser_only_docs_in_chooseable_collection_appear(self):
+        # Log in as a baker, who has choose permission on the Bakery but not the Office.
+        self.login_as_baker()
+        # And a document to the Bakery and to the Office.
+        bun_recipe_title = 'bun_recipe.pdf'
+        Document.objects.create(
+            title=bun_recipe_title, collection=Collection.objects.get(name='Bakery')
+        )
+        payroll_title = 'payroll.xlsx'
+        Document.objects.create(
+            title=payroll_title, collection=Collection.objects.get(name='Office')
+        )
+
+        # Open the doc chooser.
+        response = self.get()
+
+        # Confirm that the chooser opened successfully.
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'wagtaildocs/chooser/chooser.html')
+        # Ensure that the bun recipe is visible, but the payroll is not.
+        self.assertContains(response, bun_recipe_title)
+        self.assertNotContains(response, payroll_title)
+
+    def test_chooser_collection_selector_appears_only_if_multiple_collections_are_choosable(self):
+        # Log in as a baker, who has choose permission on the Bakery but not the Office.
+        self.login_as_baker()
+
+        # Open the doc chooser.
+        response = self.get()
+
+        # Confirm that the chooser opened successfully.
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'wagtaildocs/chooser/chooser.html')
+        # Ensure that the Collection chooser is not visible, because the Baker cannot
+        # choose from multiple Collections.
+        self.assertNotContains(response, 'Collection:')
+
+        # Let the Baker choose from the Office Collection.
+        GroupCollectionPermission.objects.create(
+            group=Group.objects.get(name='Bakers'),
+            collection=Collection.objects.get(name='Office'),
+            permission=Permission.objects.get(
+                content_type__app_label='wagtaildocs',
+                codename='choose_document'
+            )
+        )
+
+        # Open the doc chooser again.
+        response = self.get()
+
+        # Confirm that the chooser opened successfully.
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'wagtaildocs/chooser/chooser.html')
+        # Ensure that the Collection chooser IS visible, because the Baker can now
+        # choose from multiple Collections.
+        self.assertContains(response, 'Collection:')
 
     def test_chooser_no_docs_upload_allowed(self):
         # given a superuser and no documents in the database
