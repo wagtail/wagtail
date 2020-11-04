4452de453bb0d46eaf1f7fab84df365e3901a27c
Matt Westcott <matt@west.co.tt>
2020-11-04 18:53:42 +0000

    Drop travis entirely

--- scripts/travis/install_elasticsearch2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -O https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.4.6/elasticsearch-2.4.6.deb && sudo dpkg -i --force-confnew elasticsearch-2.4.6.deb && sudo service elasticsearch restart
