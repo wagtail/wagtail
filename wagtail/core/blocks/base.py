f4bef30c5c35c7871d6c30af61e2a8b81450fe1a
Ojas Kale <ojasok@gmail.com>
2020-11-09 19:47:31 +0530

    pickle fix

--- wagtail/core/blocks/base.py
+++ wagtail/core/blocks/base.py
@@ -6,11 +6,11 @@ from importlib import import_module
 from django import forms
 from django.core import checks
 from django.core.exceptions import ImproperlyConfigured
+from django.forms.fields import CallableChoiceIterator
 from django.template.loader import render_to_string
 from django.utils.encoding import force_str
 from django.utils.safestring import mark_safe
 from django.utils.text import capfirst
-
 
 __all__ = ['BaseBlock', 'Block', 'BoundBlock', 'DeclarativeSubBlocksMetaclass', 'BlockWidget', 'BlockField']
 
@@ -441,6 +441,36 @@ class Block(metaclass=BaseBlock):
 
         return (self.name == other.name) and (self.deconstruct() == other.deconstruct())
 
+    def __getstate__(self):
+        """Hook to allow choosing the attributes to pickle."""
+        state = self.__dict__
+
+        if "meta" in state:
+            state["meta"] = state["meta"].__dict__
+
+        if "dependencies" in state:
+            state["dependencies"] = list(state["dependencies"])
+
+        if (
+            "field" in state
+            and hasattr(state["field"], "choices")
+            and isinstance(state["field"].choices, CallableChoiceIterator)
+        ):
+            state["field"].choices = list(state["field"].choices)
+
+        return state
+
+    def __reduce__(self):
+        """Reducer to make this class picklable."""
+        block_class = self.__class__
+        args = self._constructor_args[0]
+        kwargs = self._constructor_args[1]
+        return block_unpickle, (block_class, args, kwargs)
+
+
+def block_unpickle(block_class, args, kwargs):
+    """Used to unpickle Block subclasses."""
+    return block_class(*args, **kwargs)
 
 class BoundBlock:
     def __init__(self, block, value, prefix=None, errors=None):
