80c14217d99c6e5f9e542a1f7189c8834166abf4
Dan Braghis <dan.braghis@torchbox.com>
2020-11-18 10:25:28 +0000

    Fix missing locale in the project template's homepage migration

--- wagtail/project_template/home/migrations/0002_create_homepage.py
+++ wagtail/project_template/home/migrations/0002_create_homepage.py
@@ -8,6 +8,7 @@ def create_homepage(apps, schema_editor):
     Page = apps.get_model('wagtailcore.Page')
     Site = apps.get_model('wagtailcore.Site')
     HomePage = apps.get_model('home.HomePage')
+    Locale = apps.get_model('wagtailcore.Locale')
 
     # Delete the default homepage
     # If migration is run multiple times, it may have already been deleted
@@ -27,6 +28,7 @@ def create_homepage(apps, schema_editor):
         depth=2,
         numchild=0,
         url_path='/home/',
+        locale=Locale.objects.first()  # cannot use Locale.get_default() here, to taking the first locale.
     )
 
     # Create a site with the new homepage set as the root
