9a48221d66dfb5e7579682dce3c5569a41925fd8
jacobtoppm <jacobtm@torchbox.com>
2020-11-27 17:00:02 +0000

    Change comments to use ParentalKeys, so they can easily be saved with the page. Ensure they are treated on the 'page' rather than 'revision' level. Remove unnecessary revision_resolved field, as resolved comments will now simply be deleted

--- wagtail/core/tests/test_comments.py
+++ wagtail/core/tests/test_comments.py
@@ -22,17 +22,6 @@ class CommentTestingUtils:
         )
 
 
-class TestCommentModels(CommentTestingUtils, TestCase):
-    fixtures = ['test.json']
-
-    def test_revision_resolved_not_after_revision_created_raises_error(self):
-        with self.assertRaises(ValidationError):
-            self.create_comment(self.revision_2, revision_resolved=self.revision_1)
-
-        with self.assertRaises(ValidationError):
-            self.create_comment(self.revision_1, revision_resolved=self.revision_1)
-
-
 class TestRevisionDeletion(CommentTestingUtils, TestCase):
     fixtures = ['test.json']
 
@@ -40,8 +29,6 @@ class TestRevisionDeletion(CommentTestingUtils, TestCase):
         super().setUp()
         self.revision_3 = self.page.save_revision()
         self.old_comment = self.create_comment(self.revision_1)
-        self.old_resolved_comment = self.create_comment(self.revision_1, revision_resolved=self.revision_2)
-        self.newly_resolved_comment = self.create_comment(self.revision_1, revision_resolved=self.revision_3)
         self.new_comment = self.create_comment(self.revision_3)
 
     def test_deleting_old_revision_moves_comment_revision_created_forwards(self):
@@ -56,21 +43,3 @@ class TestRevisionDeletion(CommentTestingUtils, TestCase):
         self.revision_3.delete()
         with self.assertRaises(Comment.DoesNotExist):
             self.new_comment.refresh_from_db()
-
-    def test_revision_deletion_when_revision_resolved_is_next_revision(self):
-        # test that when a comment's revision_created and revision_resolved are neighbouring revisions, deleting the revision_created
-        # deletes the comment, as a comment's revision_resolved must be after its revision_created
-        self.revision_1.delete()
-        with self.assertRaises(Comment.DoesNotExist):
-            self.old_resolved_comment.refresh_from_db()
-
-    def test_deleting_resolving_revision(self):
-        # test that when a revision linked to a comment via its revision_resolved is deleted, the comment's revision_resolved is
-        # moved to the next revision, or unresolved if there is none
-        self.revision_2.delete()
-        self.old_resolved_comment.refresh_from_db()
-        self.assertEqual(self.old_resolved_comment.revision_resolved, self.revision_3)
-
-        self.revision_3.delete()
-        self.newly_resolved_comment.refresh_from_db()
-        self.assertIsNone(self.newly_resolved_comment.revision_resolved)
