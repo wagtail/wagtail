1ca48eb31f9a3bcf673d78379f5f23f7d727c639
jacobtoppm <jacobtm@torchbox.com>
2020-11-27 17:01:57 +0000

    Add structure of CommentPanel to initialize the comment app in the frontend, and add forms to validate comments on save. (Comments are not yet output or written to the db)

---
+++ wagtail/admin/forms/comments.py
@@ -0,0 +1,52 @@
+from django.forms import ValidationError
+from django.forms.formsets import DELETION_FIELD_NAME
+from django.utils.translation import gettext as _
+
+from .models import WagtailAdminModelForm
+
+
+class CommentReplyForm(WagtailAdminModelForm):
+    user = None
+
+    class Meta:
+        fields = ('text',)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        user = self.user
+        if self.instance.pk and self.instance.user != user:
+            # trying to edit someone else's comment reply
+            if any(field for field in self.changed_data):
+                # includes DELETION_FIELD_NAME, as users cannot delete each other's individual comment replies
+                # if deleting a whole thread, this should be done by deleting the parent Comment instead
+                self.add_error(None, error=ValidationError(_("You cannot edit another user's comment")))
+        return cleaned_data
+
+    def save(self, *args, **kwargs):
+        if not self.instance.pk:
+            self.instance.user = self.user
+        return super().save(*args, **kwargs)
+
+
+class CommentForm(WagtailAdminModelForm):
+    """
+    This is designed to be subclassed and have the user overidden to enable user-based validation within the edit handler system
+    """
+    user = None
+
+    def clean(self):
+        cleaned_data = super().clean()
+        user = self.user
+
+        if self.instance.pk and self.instance.user != user:
+            # trying to edit someone else's comment
+            if any(field for field in self.changed_data if field != DELETION_FIELD_NAME):
+                # users can delete each other's base comments, as this is just the "resolve" action
+                self.add_error(None, error=ValidationError(_("You cannot edit another user's comment")))
+
+        return cleaned_data
+
+    def save(self, *args, **kwargs):
+        if not self.instance.pk:
+            self.instance.user = self.user
+        return super().save(*args, **kwargs)
