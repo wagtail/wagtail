d1751a6eb4905d4fd8d7db7ba655cfa15c4169b0
Matt Westcott <matt@west.co.tt>
2020-11-30 17:56:57 +0000

    Add initial Telepath code and integrate with BlockWidget
    This is sufficient to render a CharBlock with `StreamField(blocks.CharBlock())`

---
+++ wagtail/core/widget_adapters.py
@@ -0,0 +1,34 @@
+"""
+Register Telepath adapters for core Django form widgets, so that they can
+have corresponding Javascript objects with the ability to render new instances
+and extract field values.
+"""
+
+from django import forms
+
+from wagtail.core.telepath import Adapter, register
+
+
+class WidgetAdapter(Adapter):
+    js_constructor = 'wagtail.widgets.Widget'
+
+    def js_args(self, widget, context):
+        return [
+            widget.render('__NAME__', None, attrs={'id': '__ID__'}),
+            widget.id_for_label('__ID__'),
+        ]
+
+    class Media:
+        js = ['wagtailadmin/js/telepath/widgets.js']
+
+
+register(WidgetAdapter(), forms.widgets.Input)
+register(WidgetAdapter(), forms.Textarea)
+register(WidgetAdapter(), forms.Select)
+
+
+class RadioSelectAdapter(WidgetAdapter):
+    js_constructor = 'wagtail.widgets.RadioSelect'
+
+
+register(RadioSelectAdapter(), forms.RadioSelect)
