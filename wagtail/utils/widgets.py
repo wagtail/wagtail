b951ea690985e232a83a3085ebda24d3c8ae27bf
Matt Westcott <matt@west.co.tt>
2020-12-12 18:03:00 +0000

    Add a preprocessing step to WidgetWithScript where database lookups can happen
    This avoids separate lookups during render_html and render_js_init, and means that shared logic such as building the edit-view URL can be accessed outside of template rendering (so that we can pass it to chooser.setState in the Javascript chooser API, for example).

--- wagtail/utils/widgets.py
+++ wagtail/utils/widgets.py
@@ -7,6 +7,14 @@ class WidgetWithScript(Widget):
         """Render the HTML (non-JS) portion of the field markup"""
         return super().render(name, value, attrs)
 
+    def get_value_data(self, value):
+        # Perform any necessary preprocessing on the value passed to render() before it is passed
+        # on to render_html / render_js_init. This is a good place to perform database lookups
+        # that are needed by both render_html and render_js_init. Return value is arbitrary
+        # (we only care that render_html / render_js_init can accept it), but will typically be
+        # a dict of data needed for rendering: id, title etc.
+        return value
+
     def render(self, name, value, attrs=None, renderer=None):
         # no point trying to come up with sensible semantics for when 'id' is missing from attrs,
         # so let's make sure it fails early in the process
@@ -15,9 +23,10 @@ class WidgetWithScript(Widget):
         except (KeyError, TypeError):
             raise TypeError("WidgetWithScript cannot be rendered without an 'id' attribute")
 
-        widget_html = self.render_html(name, value, attrs)
+        value_data = self.get_value_data(value)
+        widget_html = self.render_html(name, value_data, attrs)
 
-        js = self.render_js_init(id_, name, value)
+        js = self.render_js_init(id_, name, value_data)
         out = '{0}<script>{1}</script>'.format(widget_html, js)
         return mark_safe(out)
 
