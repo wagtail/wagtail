f42ac4b41068ac3061463ed6de179c12eeaed7f1
Karl Hobley <karl@kaed.uk>
2020-12-20 13:27:38 +0000

    Switch to 2-space indentation

--- client/src/entrypoints/workflow-status.js
+++ client/src/entrypoints/workflow-status.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
 $(function() {
-    /* Interface to view the workflow status from the explorer / editor */
-    $('button.action-workflow-status').on('click', function() {
-        ModalWorkflow({
-            url: this.getAttribute('data-url'),
-        });
-        return false;
+  /* Interface to view the workflow status from the explorer / editor */
+  $('button.action-workflow-status').on('click', function() {
+    ModalWorkflow({
+      url: this.getAttribute('data-url'),
     });
+    return false;
+  });
 });
