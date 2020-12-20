a7baa2c8171c7d7b6662548c353420c8a596e0fc
Karl Hobley <karl@kaed.uk>
2020-12-20 13:27:38 +0000

    Switch to 2-space indentation

--- client/src/entrypoints/blocks/struct.js
+++ client/src/entrypoints/blocks/struct.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
 window.StructBlock = function(childInitializersByName) {
-    return function(prefix) {
-        for (var childName in childInitializersByName) {
-            var childInitializer = childInitializersByName[childName];
-            var childPrefix = prefix + '-' + childName;
-            childInitializer(childPrefix);
-        }
-    };
+  return function(prefix) {
+    for (var childName in childInitializersByName) {
+      var childInitializer = childInitializersByName[childName];
+      var childPrefix = prefix + '-' + childName;
+      childInitializer(childPrefix);
+    }
+  };
 };
