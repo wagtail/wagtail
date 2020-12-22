662761b74bb7cffdbd2ccca7a7217f66cf8917e3
Matt Westcott <matt@west.co.tt>
2020-12-22 20:32:35 +0000

    Refactor into get/post methods

--- wagtail/documents/views/multiple.py
+++ wagtail/documents/views/multiple.py
@@ -7,7 +7,6 @@ from django.template.loader import render_to_string
 from django.template.response import TemplateResponse
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.decorators.http import require_POST
 from django.views.decorators.vary import vary_on_headers
 from django.views.generic import View
 
@@ -27,100 +26,104 @@ class AddView(View):
     @method_decorator(permission_checker.require('add'))
     @method_decorator(vary_on_headers('X-Requested-With'))
     def dispatch(self, request):
-        Document = get_document_model()
-        DocumentForm = get_document_form(Document)
-        DocumentMultiForm = get_document_multi_form(Document)
+        self.model = get_document_model()
+        self.form_class = get_document_form(self.model)
+        self.edit_form_class = get_document_multi_form(self.model)
+
+        return super().dispatch(request)
+
+    def post(self, request):
+        if not request.is_ajax():
+            return HttpResponseBadRequest("Cannot POST to this view without AJAX")
+
+        if not request.FILES:
+            return HttpResponseBadRequest("Must upload a file")
+
+        # Build a form for validation
+        form = self.form_class({
+            'title': request.FILES['files[]'].name,
+            'collection': request.POST.get('collection'),
+        }, {
+            'file': request.FILES['files[]']
+        }, user=request.user)
+
+        if form.is_valid():
+            # Save it
+            doc = form.save(commit=False)
+            doc.uploaded_by_user = request.user
+            doc.file_size = doc.file.size
+
+            # Set new document file hash
+            doc.file.seek(0)
+            doc._set_file_hash(doc.file.read())
+            doc.file.seek(0)
+
+            doc.save()
+
+            # Success! Send back an edit form for this document to the user
+            return JsonResponse({
+                'success': True,
+                'doc_id': int(doc.id),
+                'form': render_to_string('wagtaildocs/multiple/edit_form.html', {
+                    'doc': doc,  # only used for tests
+                    'edit_action': reverse('wagtaildocs:edit_multiple', args=(doc.id,)),
+                    'delete_action': reverse('wagtaildocs:delete_multiple', args=(doc.id,)),
+                    'form': self.edit_form_class(
+                        instance=doc, prefix='doc-%d' % doc.id, user=request.user
+                    ),
+                }, request=request),
+            })
+        elif 'file' in form.errors:
+            # The uploaded file is invalid; reject it now
+            return JsonResponse({
+                'success': False,
+                'error_message': '\n'.join(form.errors['file']),
+            })
+        else:
+            # Some other field of the document form has failed validation, e.g. a required metadata
+            # field on a custom document model. Store the document as an UploadedDocument instead
+            # and present the edit form so that it will become a proper Document when successfully
+            # filled in
+            uploaded_doc = UploadedDocument.objects.create(
+                file=request.FILES['files[]'], uploaded_by_user=request.user
+            )
+            doc = self.model(title=request.FILES['files[]'].name, collection_id=request.POST.get('collection'))
+
+            return JsonResponse({
+                'success': True,
+
+                'uploaded_document_id': uploaded_doc.id,
+                'form': render_to_string('wagtaildocs/multiple/edit_form.html', {
+                    'uploaded_document': uploaded_doc,  # only used for tests
+                    'edit_action': reverse('wagtaildocs:create_multiple_from_uploaded_document', args=(uploaded_doc.id,)),
+                    'delete_action': reverse('wagtaildocs:delete_upload_multiple', args=(uploaded_doc.id,)),
+                    'form': self.edit_form_class(
+                        instance=doc, prefix='uploaded-document-%d' % uploaded_doc.id, user=request.user
+                    ),
+                }, request=request),
+            })
+
+    def get(self, request):
+        # Instantiate a dummy copy of the form that we can retrieve validation messages and media from;
+        # actual rendering of forms will happen on AJAX POST rather than here
+        form = self.form_class(user=request.user)
 
         collections = permission_policy.collections_user_has_permission_for(request.user, 'add')
         if len(collections) < 2:
             # no need to show a collections chooser
             collections = None
 
-        if request.method == 'POST':
-            if not request.is_ajax():
-                return HttpResponseBadRequest("Cannot POST to this view without AJAX")
-
-            if not request.FILES:
-                return HttpResponseBadRequest("Must upload a file")
-
-            # Build a form for validation
-            form = DocumentForm({
-                'title': request.FILES['files[]'].name,
-                'collection': request.POST.get('collection'),
-            }, {
-                'file': request.FILES['files[]']
-            }, user=request.user)
-
-            if form.is_valid():
-                # Save it
-                doc = form.save(commit=False)
-                doc.uploaded_by_user = request.user
-                doc.file_size = doc.file.size
-
-                # Set new document file hash
-                doc.file.seek(0)
-                doc._set_file_hash(doc.file.read())
-                doc.file.seek(0)
-
-                doc.save()
-
-                # Success! Send back an edit form for this document to the user
-                return JsonResponse({
-                    'success': True,
-                    'doc_id': int(doc.id),
-                    'form': render_to_string('wagtaildocs/multiple/edit_form.html', {
-                        'doc': doc,  # only used for tests
-                        'edit_action': reverse('wagtaildocs:edit_multiple', args=(doc.id,)),
-                        'delete_action': reverse('wagtaildocs:delete_multiple', args=(doc.id,)),
-                        'form': DocumentMultiForm(
-                            instance=doc, prefix='doc-%d' % doc.id, user=request.user
-                        ),
-                    }, request=request),
-                })
-            elif 'file' in form.errors:
-                # The uploaded file is invalid; reject it now
-                return JsonResponse({
-                    'success': False,
-                    'error_message': '\n'.join(form.errors['file']),
-                })
-            else:
-                # Some other field of the document form has failed validation, e.g. a required metadata
-                # field on a custom document model. Store the document as an UploadedDocument instead
-                # and present the edit form so that it will become a proper Document when successfully
-                # filled in
-                uploaded_doc = UploadedDocument.objects.create(
-                    file=request.FILES['files[]'], uploaded_by_user=request.user
-                )
-                doc = Document(title=request.FILES['files[]'].name, collection_id=request.POST.get('collection'))
-
-                return JsonResponse({
-                    'success': True,
-
-                    'uploaded_document_id': uploaded_doc.id,
-                    'form': render_to_string('wagtaildocs/multiple/edit_form.html', {
-                        'uploaded_document': uploaded_doc,  # only used for tests
-                        'edit_action': reverse('wagtaildocs:create_multiple_from_uploaded_document', args=(uploaded_doc.id,)),
-                        'delete_action': reverse('wagtaildocs:delete_upload_multiple', args=(uploaded_doc.id,)),
-                        'form': DocumentMultiForm(
-                            instance=doc, prefix='uploaded-document-%d' % uploaded_doc.id, user=request.user
-                        ),
-                    }, request=request),
-                })
-        else:
-            # Instantiate a dummy copy of the form that we can retrieve validation messages and media from;
-            # actual rendering of forms will happen on AJAX POST rather than here
-            form = DocumentForm(user=request.user)
-
-            return TemplateResponse(request, 'wagtaildocs/multiple/add.html', {
-                'help_text': form.fields['file'].help_text,
-                'collections': collections,
-                'form_media': form.media,
-            })
+        return TemplateResponse(request, 'wagtaildocs/multiple/add.html', {
+            'help_text': form.fields['file'].help_text,
+            'collections': collections,
+            'form_media': form.media,
+        })
 
 
 class EditView(View):
-    @method_decorator(require_POST)
-    def dispatch(self, request, doc_id, callback=None):
+    http_method_names = ['post']
+
+    def post(self, request, doc_id, callback=None):
         Document = get_document_model()
         DocumentMultiForm = get_document_multi_form(Document)
 
@@ -161,8 +164,9 @@ class EditView(View):
 
 
 class DeleteView(View):
-    @method_decorator(require_POST)
-    def dispatch(self, request, doc_id):
+    http_method_names = ['post']
+
+    def post(self, request, doc_id):
         Document = get_document_model()
 
         doc = get_object_or_404(Document, id=doc_id)
@@ -182,8 +186,9 @@ class DeleteView(View):
 
 
 class CreateFromUploadedDocumentView(View):
-    @method_decorator(require_POST)
-    def dispatch(self, request, uploaded_document_id):
+    http_method_names = ['post']
+
+    def post(self, request, uploaded_document_id):
         Document = get_document_model()
         DocumentMultiForm = get_document_multi_form(Document)
 
@@ -237,8 +242,9 @@ class CreateFromUploadedDocumentView(View):
 
 
 class DeleteUploadView(View):
-    @method_decorator(require_POST)
-    def dispatch(self, request, uploaded_document_id):
+    http_method_names = ['post']
+
+    def post(self, request, uploaded_document_id):
         uploaded_doc = get_object_or_404(UploadedDocument, id=uploaded_document_id)
 
         if not request.is_ajax():
--- wagtail/images/views/multiple.py
+++ wagtail/images/views/multiple.py
@@ -7,7 +7,6 @@ from django.template.loader import render_to_string
 from django.template.response import TemplateResponse
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.decorators.http import require_POST
 from django.views.decorators.vary import vary_on_headers
 from django.views.generic import View
 
@@ -45,98 +44,102 @@ class AddView(View):
     @method_decorator(permission_checker.require('add'))
     @method_decorator(vary_on_headers('X-Requested-With'))
     def dispatch(self, request):
-        Image = get_image_model()
-        ImageForm = get_image_form(Image)
+        self.model = get_image_model()
+        self.form_class = get_image_form(self.model)
+
+        return super().dispatch(request)
+
+    def post(self, request):
+        if not request.is_ajax():
+            return HttpResponseBadRequest("Cannot POST to this view without AJAX")
+
+        if not request.FILES:
+            return HttpResponseBadRequest("Must upload a file")
+
+        # Build a form for validation
+        form = self.form_class({
+            'title': request.FILES['files[]'].name,
+            'collection': request.POST.get('collection'),
+        }, {
+            'file': request.FILES['files[]'],
+        }, user=request.user)
+
+        if form.is_valid():
+            # Save it
+            image = form.save(commit=False)
+            image.uploaded_by_user = request.user
+            image.file_size = image.file.size
+            image.file.seek(0)
+            image._set_file_hash(image.file.read())
+            image.file.seek(0)
+            image.save()
+
+            # Success! Send back an edit form for this image to the user
+            return JsonResponse({
+                'success': True,
+                'image_id': int(image.id),
+                'form': render_to_string('wagtailimages/multiple/edit_form.html', {
+                    'image': image,
+                    'edit_action': reverse('wagtailimages:edit_multiple', args=(image.id,)),
+                    'delete_action': reverse('wagtailimages:delete_multiple', args=(image.id,)),
+                    'form': get_image_edit_form(self.model)(
+                        instance=image, prefix='image-%d' % image.id, user=request.user
+                    ),
+                }, request=request),
+            })
+        elif 'file' in form.errors:
+            # The uploaded file is invalid; reject it now
+            return JsonResponse({
+                'success': False,
+                'error_message': '\n'.join(form.errors['file']),
+            })
+        else:
+            # Some other field of the image form has failed validation, e.g. a required metadata field
+            # on a custom image model. Store the image as an UploadedImage instead and present the
+            # edit form so that it will become a proper Image when successfully filled in
+            uploaded_image = UploadedImage.objects.create(
+                file=request.FILES['files[]'], uploaded_by_user=request.user
+            )
+            image = self.model(title=request.FILES['files[]'].name, collection_id=request.POST.get('collection'))
+
+            return JsonResponse({
+                'success': True,
+                'uploaded_image_id': uploaded_image.id,
+                'form': render_to_string('wagtailimages/multiple/edit_form.html', {
+                    'uploaded_image': uploaded_image,
+                    'edit_action': reverse('wagtailimages:create_multiple_from_uploaded_image', args=(uploaded_image.id,)),
+                    'delete_action': reverse('wagtailimages:delete_upload_multiple', args=(uploaded_image.id,)),
+                    'form': get_image_edit_form(self.model)(
+                        instance=image, prefix='uploaded-image-%d' % uploaded_image.id, user=request.user
+                    ),
+                }, request=request),
+            })
+
+    def get(self, request):
+        # Instantiate a dummy copy of the form that we can retrieve validation messages and media from;
+        # actual rendering of forms will happen on AJAX POST rather than here
+        form = self.form_class(user=request.user)
 
         collections = permission_policy.collections_user_has_permission_for(request.user, 'add')
         if len(collections) < 2:
             # no need to show a collections chooser
             collections = None
 
-        if request.method == 'POST':
-            if not request.is_ajax():
-                return HttpResponseBadRequest("Cannot POST to this view without AJAX")
-
-            if not request.FILES:
-                return HttpResponseBadRequest("Must upload a file")
-
-            # Build a form for validation
-            form = ImageForm({
-                'title': request.FILES['files[]'].name,
-                'collection': request.POST.get('collection'),
-            }, {
-                'file': request.FILES['files[]'],
-            }, user=request.user)
-
-            if form.is_valid():
-                # Save it
-                image = form.save(commit=False)
-                image.uploaded_by_user = request.user
-                image.file_size = image.file.size
-                image.file.seek(0)
-                image._set_file_hash(image.file.read())
-                image.file.seek(0)
-                image.save()
-
-                # Success! Send back an edit form for this image to the user
-                return JsonResponse({
-                    'success': True,
-                    'image_id': int(image.id),
-                    'form': render_to_string('wagtailimages/multiple/edit_form.html', {
-                        'image': image,
-                        'edit_action': reverse('wagtailimages:edit_multiple', args=(image.id,)),
-                        'delete_action': reverse('wagtailimages:delete_multiple', args=(image.id,)),
-                        'form': get_image_edit_form(Image)(
-                            instance=image, prefix='image-%d' % image.id, user=request.user
-                        ),
-                    }, request=request),
-                })
-            elif 'file' in form.errors:
-                # The uploaded file is invalid; reject it now
-                return JsonResponse({
-                    'success': False,
-                    'error_message': '\n'.join(form.errors['file']),
-                })
-            else:
-                # Some other field of the image form has failed validation, e.g. a required metadata field
-                # on a custom image model. Store the image as an UploadedImage instead and present the
-                # edit form so that it will become a proper Image when successfully filled in
-                uploaded_image = UploadedImage.objects.create(
-                    file=request.FILES['files[]'], uploaded_by_user=request.user
-                )
-                image = Image(title=request.FILES['files[]'].name, collection_id=request.POST.get('collection'))
-
-                return JsonResponse({
-                    'success': True,
-                    'uploaded_image_id': uploaded_image.id,
-                    'form': render_to_string('wagtailimages/multiple/edit_form.html', {
-                        'uploaded_image': uploaded_image,
-                        'edit_action': reverse('wagtailimages:create_multiple_from_uploaded_image', args=(uploaded_image.id,)),
-                        'delete_action': reverse('wagtailimages:delete_upload_multiple', args=(uploaded_image.id,)),
-                        'form': get_image_edit_form(Image)(
-                            instance=image, prefix='uploaded-image-%d' % uploaded_image.id, user=request.user
-                        ),
-                    }, request=request),
-                })
-        else:
-            # Instantiate a dummy copy of the form that we can retrieve validation messages and media from;
-            # actual rendering of forms will happen on AJAX POST rather than here
-            form = ImageForm(user=request.user)
-
-            return TemplateResponse(request, 'wagtailimages/multiple/add.html', {
-                'max_filesize': form.fields['file'].max_upload_size,
-                'help_text': form.fields['file'].help_text,
-                'allowed_extensions': ALLOWED_EXTENSIONS,
-                'error_max_file_size': form.fields['file'].error_messages['file_too_large_unknown_size'],
-                'error_accepted_file_types': form.fields['file'].error_messages['invalid_image_extension'],
-                'collections': collections,
-                'form_media': form.media,
-            })
+        return TemplateResponse(request, 'wagtailimages/multiple/add.html', {
+            'max_filesize': form.fields['file'].max_upload_size,
+            'help_text': form.fields['file'].help_text,
+            'allowed_extensions': ALLOWED_EXTENSIONS,
+            'error_max_file_size': form.fields['file'].error_messages['file_too_large_unknown_size'],
+            'error_accepted_file_types': form.fields['file'].error_messages['invalid_image_extension'],
+            'collections': collections,
+            'form_media': form.media,
+        })
 
 
 class EditView(View):
-    @method_decorator(require_POST)
-    def dispatch(self, request, image_id, callback=None):
+    http_method_names = ['post']
+
+    def post(self, request, image_id, callback=None):
         Image = get_image_model()
         ImageForm = get_image_edit_form(Image)
 
@@ -177,8 +180,9 @@ class EditView(View):
 
 
 class DeleteView(View):
-    @method_decorator(require_POST)
-    def dispatch(self, request, image_id):
+    http_method_names = ['post']
+
+    def post(self, request, image_id):
         image = get_object_or_404(get_image_model(), id=image_id)
 
         if not request.is_ajax():
@@ -196,8 +200,9 @@ class DeleteView(View):
 
 
 class CreateFromUploadedImageView(View):
-    @method_decorator(require_POST)
-    def dispatch(self, request, uploaded_image_id):
+    http_method_names = ['post']
+
+    def post(self, request, uploaded_image_id):
         Image = get_image_model()
         ImageForm = get_image_edit_form(Image)
 
@@ -251,8 +256,9 @@ class CreateFromUploadedImageView(View):
 
 
 class DeleteUploadView(View):
-    @method_decorator(require_POST)
-    def dispatch(self, request, uploaded_image_id):
+    http_method_names = ['post']
+
+    def post(self, request, uploaded_image_id):
         uploaded_image = get_object_or_404(UploadedImage, id=uploaded_image_id)
 
         if not request.is_ajax():
