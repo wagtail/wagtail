fd618dd697abb0f1f6a088655379bea646342876
Matt Westcott <matt@west.co.tt>
2020-12-23 18:57:51 +0000

    parameterise CreateFromUploadedDocumentView

--- wagtail/images/views/multiple.py
+++ wagtail/images/views/multiple.py
@@ -101,6 +101,14 @@ class DeleteView(BaseDeleteView):
 
 class CreateFromUploadedImageView(View):
     http_method_names = ['post']
+    edit_form_template_name = 'wagtailadmin/generic/multiple_upload/edit_form.html'
+    edit_upload_url_name = 'wagtailimages:create_multiple_from_uploaded_image'
+    delete_upload_url_name = 'wagtailimages:delete_upload_multiple'
+    upload_model = UploadedImage
+    upload_pk_url_kwarg = 'uploaded_image_id'
+    edit_upload_form_prefix = 'uploaded-image'
+    context_object_id_name = 'image_id'
+    context_upload_name = 'uploaded_image'
 
     def get_model(self):
         return get_image_model()
@@ -108,54 +116,60 @@ class CreateFromUploadedImageView(View):
     def get_edit_form_class(self):
         return get_image_multi_form(self.model)
 
-    def post(self, request, uploaded_image_id):
+    def save_object(self, form):
+        # assign the file content from uploaded_image to the image object, to ensure it gets saved to
+        # Image's storage
+
+        self.object.file.save(os.path.basename(self.upload.file.name), self.upload.file.file, save=False)
+        self.object.uploaded_by_user = self.request.user
+        self.object.file_size = self.object.file.size
+        self.object.file.open()
+        self.object.file.seek(0)
+        self.object._set_file_hash(self.object.file.read())
+        self.object.file.seek(0)
+        form.save()
+
+        # Reindex the image to make sure all tags are indexed
+        for backend in get_search_backends():
+            backend.add(self.object)
+
+    def post(self, request, *args, **kwargs):
+        upload_id = kwargs[self.upload_pk_url_kwarg]
         self.model = self.get_model()
         self.form_class = self.get_edit_form_class()
 
-        uploaded_image = get_object_or_404(UploadedImage, id=uploaded_image_id)
+        self.upload = get_object_or_404(self.upload_model, id=upload_id)
 
         if not request.is_ajax():
             return HttpResponseBadRequest("Cannot POST to this view without AJAX")
 
-        if uploaded_image.uploaded_by_user != request.user:
+        if self.upload.uploaded_by_user != request.user:
             raise PermissionDenied
 
-        image = self.model()
+        self.object = self.model()
         form = self.form_class(
-            request.POST, request.FILES, instance=image, prefix='uploaded-image-%d' % uploaded_image_id, user=request.user
+            request.POST, request.FILES,
+            instance=self.object,
+            prefix='%s-%d' % (self.edit_upload_form_prefix, upload_id),
+            user=request.user
         )
 
         if form.is_valid():
-            # assign the file content from uploaded_image to the image object, to ensure it gets saved to
-            # Image's storage
-
-            image.file.save(os.path.basename(uploaded_image.file.name), uploaded_image.file.file, save=False)
-            image.uploaded_by_user = request.user
-            image.file_size = image.file.size
-            image.file.open()
-            image.file.seek(0)
-            image._set_file_hash(image.file.read())
-            image.file.seek(0)
-            form.save()
-
-            uploaded_image.file.delete()
-            uploaded_image.delete()
-
-            # Reindex the image to make sure all tags are indexed
-            for backend in get_search_backends():
-                backend.add(image)
+            self.save_object(form)
+            self.upload.file.delete()
+            self.upload.delete()
 
             return JsonResponse({
                 'success': True,
-                'image_id': image.id,
+                self.context_object_id_name: self.object.id,
             })
         else:
             return JsonResponse({
                 'success': False,
-                'form': render_to_string('wagtailadmin/generic/multiple_upload/edit_form.html', {
-                    'uploaded_image': uploaded_image,
-                    'edit_action': reverse('wagtailimages:create_multiple_from_uploaded_image', args=(uploaded_image.id,)),
-                    'delete_action': reverse('wagtailimages:delete_upload_multiple', args=(uploaded_image.id,)),
+                'form': render_to_string(self.edit_form_template_name, {
+                    self.context_upload_name: self.upload,
+                    'edit_action': reverse(self.edit_upload_url_name, args=(self.upload.id,)),
+                    'delete_action': reverse(self.delete_upload_url_name, args=(self.upload.id,)),
                     'form': form,
                 }, request=request),
             })
