04e22d65ceb87c3af226812b7c3339251dd2d3f3
Matt Westcott <matt@west.co.tt>
2021-01-05 16:55:32 +0000

    Implement public JS API for ImageChooser

--- client/src/entrypoints/images/image-chooser.js
+++ client/src/entrypoints/images/image-chooser.js
@@ -5,33 +5,83 @@ function createImageChooser(id) {
   const previewImage = chooserElement.find('.preview-image img');
   const input = $('#' + id);
   const editLink = chooserElement.find('.edit-link');
+  const chooserBaseUrl = chooserElement.data('chooserUrl');
 
-  $('.action-choose', chooserElement).on('click', () => {
-    // eslint-disable-next-line no-undef, new-cap
-    ModalWorkflow({
-      url: chooserElement.data('chooserUrl'),
-      // eslint-disable-next-line no-undef
-      onload: IMAGE_CHOOSER_MODAL_ONLOAD_HANDLERS,
-      responses: {
-        imageChosen: (imageData) => {
-          input.val(imageData.id);
-          previewImage.attr({
-            src: imageData.preview.url,
-            width: imageData.preview.width,
-            height: imageData.preview.height,
-            alt: imageData.title,
-            title: imageData.title
-          });
-          chooserElement.removeClass('blank');
-          editLink.attr('href', imageData.edit_link);
-        }
+  /*
+  Construct initial state of the chooser from the rendered (static) HTML and arguments passed to
+  createImageChooser. State is either null (= no image chosen) or a dict of id, edit_link,
+  title and preview (= a dict of url, width, height).
+
+  The result returned from the image chooser modal (see get_image_result_data in
+  wagtail.images.views.chooser) is a superset of this, and can therefore be passed directly to
+  chooser.setState.
+  */
+  let state = null;
+  if (input.val()) {
+    state = {
+      id: input.val(),
+      edit_link: editLink.attr('href'),
+      title: previewImage.attr('alt'),
+      preview: {
+        url: previewImage.attr('src'),
+        width: previewImage.attr('width'),
+        height: previewImage.attr('height'),
       }
-    });
+    };
+  }
+
+  /* define public API functions for the chooser */
+  const chooser = {
+    getState: () => state,
+    getValue: () => state && state.id,
+    setState: (newState) => {
+      if (newState) {
+        input.val(newState.id);
+        previewImage.attr({
+          src: newState.preview.url,
+          width: newState.preview.width,
+          height: newState.preview.height,
+          alt: newState.title,
+          title: newState.title
+        });
+        chooserElement.removeClass('blank');
+        editLink.attr('href', newState.edit_link);
+      } else {
+        input.val('');
+        chooserElement.addClass('blank');
+      }
+
+      state = newState;
+    },
+
+    openChooserModal: () => {
+      // eslint-disable-next-line no-undef, new-cap
+      ModalWorkflow({
+        url: chooserBaseUrl,
+        // eslint-disable-next-line no-undef
+        onload: IMAGE_CHOOSER_MODAL_ONLOAD_HANDLERS,
+        responses: {
+          imageChosen: (result) => {
+            chooser.setState(result);
+          },
+        },
+      });
+    },
+
+    clear: () => {
+      chooser.setState(null);
+    }
+  };
+
+  /* hook up chooser API to the buttons */
+  $('.action-choose', chooserElement).on('click', () => {
+    chooser.openChooserModal();
   });
 
   $('.action-clear', chooserElement).on('click', () => {
-    input.val('');
-    chooserElement.addClass('blank');
+    chooser.clear();
   });
+
+  return chooser;
 }
 window.createImageChooser = createImageChooser;
