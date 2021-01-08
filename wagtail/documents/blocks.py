b4605eb2c8bed946f2b332cfe2ff4c874b74f03d
Matt Westcott <matt@west.co.tt>
2021-01-08 14:29:32 +0000

    Add telepath adapter for document chooser

--- wagtail/documents/blocks.py
+++ wagtail/documents/blocks.py
@@ -13,7 +13,18 @@ class DocumentChooserBlock(ChooserBlock):
     @cached_property
     def widget(self):
         from wagtail.documents.widgets import AdminDocumentChooser
-        return AdminDocumentChooser
+        return AdminDocumentChooser()
+
+    def get_form_state(self, value):
+        value_data = self.widget.get_value_data(value)
+        if value_data is None:
+            return None
+        else:
+            return {
+                'id': value_data['id'],
+                'edit_link': value_data['edit_url'],
+                'title': value_data['title'],
+            }
 
     def render_basic(self, value, context=None):
         if value:
