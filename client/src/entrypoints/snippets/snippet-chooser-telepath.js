a48eeadca4dd6a21c4308e33e4fead00dead582c
Matt Westcott <matt@west.co.tt>
2021-01-08 15:12:41 +0000

    Add telepath adapter for snippet chooser

---
+++ client/src/entrypoints/snippets/snippet-chooser-telepath.js
@@ -0,0 +1,18 @@
+class SnippetChooser {
+  constructor(html, idForLabel, model) {
+    this.html = html;
+    this.idForLabel = idForLabel;
+    this.model = model;
+  }
+
+  render(placeholder, name, id, initialState) {
+    const html = this.html.replace(/__NAME__/g, name).replace(/__ID__/g, id);
+    // eslint-disable-next-line no-param-reassign
+    placeholder.outerHTML = html;
+    /* the chooser object returned by createImageChooser also serves as the JS widget representation */
+    // eslint-disable-next-line no-undef
+    const chooser = createSnippetChooser(id, this.model);
+    chooser.setState(initialState);
+  }
+}
+window.telepath.register('wagtail.snippets.widgets.SnippetChooser', SnippetChooser);
