e5bb2618119700253ab1fd4d85eeda23297bbe41
Matt Westcott <matt@west.co.tt>
2021-01-15 00:07:47 +0000

    Support unpacking of references

--- client/src/components/Telepath/Telepath.test.js
+++ client/src/components/Telepath/Telepath.test.js
@@ -69,4 +69,90 @@ describe('Telepath', () => {
     expect(dangerouslyInLove.title).toBe('Dangerously in Love');
     expect(dangerouslyInLove.artists[0].name).toBe('Beyoncé');
   });
+
+  it('can expand object references', () => {
+    const discography = telepath.unpack([
+      {
+        _type: 'music.Album',
+        _args: [
+          'Dangerously in Love',
+          [
+            { _type: 'music.Artist', _args: ['Beyoncé'], _id: 0 },
+          ]
+        ]
+      },
+      {
+        _type: 'music.Album',
+        _args: [
+          'Everything Is Love',
+          [
+            { _ref: 0 },
+            { _type: 'music.Artist', _args: ['Jay-Z'] },
+          ]
+        ]
+      },
+    ]);
+    expect(discography[0].artists[0].name).toBe('Beyoncé');
+    expect(discography[1].artists[0].name).toBe('Beyoncé');
+    expect(discography[1].artists[1].name).toBe('Jay-Z');
+  });
+
+  it('can expand list references', () => {
+    const discography = telepath.unpack([
+      {
+        _type: 'music.Album',
+        _args: [
+          "Destiny's Child",
+          {
+            _list: [
+              { _type: 'music.Artist', _args: ['Beyoncé'] },
+              { _type: 'music.Artist', _args: ['Kelly Rowland'] },
+              { _type: 'music.Artist', _args: ['Michelle Williams'] },
+            ],
+            _id: 0,
+          }
+        ]
+      },
+      {
+        _type: 'music.Album',
+        _args: [
+          'Survivor',
+          { _ref: 0 },
+        ]
+      },
+    ]);
+    expect(discography[1].artists.length).toBe(3);
+    expect(discography[1].artists[0].name).toBe('Beyoncé');
+    expect(discography[1].artists).toBe(discography[0].artists);
+  });
+
+  it('can expand primitive value references', () => {
+    const discography = telepath.unpack([
+      {
+        _type: 'music.Album',
+        _args: [
+          'Dangerously in Love',
+          [
+            {
+              _type: 'music.Artist',
+              _args: [{ _val: 'Beyoncé', _id: 0 }],
+              _id: 1,
+            },
+          ]
+        ]
+      },
+      {
+        _type: 'music.Album',
+        _args: [
+          { _ref: 0 },
+          [
+            { _ref: 1 },
+          ]
+        ]
+      },
+    ]);
+    expect(discography[0].artists[0].name).toBe('Beyoncé');
+    expect(discography[1].title).toBe('Beyoncé');
+    expect(discography[1].artists[0].name).toBe('Beyoncé');
+  });
 });
