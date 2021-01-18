c8323fa0606f3a6b7ceb57c54d3575770086fe29
Karl Hobley <karl@torchbox.com>
2021-01-18 16:52:56 +0000

    Add unit tests for PageChooser

--- client/src/entrypoints/admin/telepath/widgets.test.js
+++ client/src/entrypoints/admin/telepath/widgets.test.js
@@ -1,3 +1,4 @@
+import '../page-chooser';
 import './telepath';
 import './widgets';
 
@@ -98,5 +99,101 @@ describe('telepath: wagtail.widgets.RadioSelect', () => {
 
     // Note: This widget always focuses the last element
     expect(document.activeElement).toBe(document.querySelector('input[value="coffee"]'));
+  });
+});
+
+describe('telepath: wagtail.widgets.PageChooser', () => {
+  let boundWidget;
+
+  beforeEach(() => {
+    // Create a placeholder to render the widget
+    document.body.innerHTML = '<div id="placeholder"></div>';
+
+    // Unpack and render a radio select widget
+    const widgetDef = window.telepath.unpack({
+      _type: 'wagtail.widgets.PageChooser',
+      _args: [
+        `<div id="__ID__-chooser" class="chooser page-chooser blank" data-chooser-url="/admin/choose-page/">
+          <div class="chosen">
+            <span class="title"></span>
+            <ul class="actions">
+              <li>
+                <button type="button" class="button action-choose button-small button-secondary">
+                  Choose another page
+                </button>
+              </li>
+              <li>
+                <a href=""
+                   class="edit-link button button-small button-secondary"
+                   target="_blank"
+                   rel="noopener noreferrer">
+                  Edit this page
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="unchosen">
+            <button type="button" class="button action-choose button-small button-secondary">
+              Choose a page
+            </button>
+          </div>
+        </div>
+        <input type="hidden" name="__NAME__" id="__ID__">`,
+        '__ID__',
+        {
+          model_names: ['wagtailcore.page'],
+          can_choose_root: false,
+          user_perms: null
+        }
+      ]
+    });
+    boundWidget = widgetDef.render($('#placeholder'), 'the-name', 'the-id', {
+      id: 60,
+      parentId: 1,
+      adminTitle: 'Welcome to the Wagtail Bakery!',
+      editUrl: '/admin/pages/60/edit/'
+    });
+  });
+
+  test('it renders correctly', () => {
+    expect(document.body.innerHTML).toMatchSnapshot();
+    expect(document.querySelector('input').value).toBe('60');
+  });
+
+  test('getValue() returns the current value', () => {
+    expect(boundWidget.getValue()).toBe(60);
+  });
+
+  test('getState() returns the current state', () => {
+    expect(boundWidget.getState()).toEqual({
+      id: 60,
+      parentId: 1,
+      adminTitle: 'Welcome to the Wagtail Bakery!',
+      editUrl: '/admin/pages/60/edit/'
+    });
+  });
+
+  test('setState() changes the current page', () => {
+    boundWidget.setState({
+      id: 34,
+      parentId: 3,
+      adminTitle: 'Anadama',
+      editUrl: '/admin/pages/34/edit/'
+    });
+    expect(document.body.innerHTML).toMatchSnapshot();
+    expect(document.querySelector('input').value).toBe('34');
+  });
+
+  test('setState() to null clears the fields', () => {
+    boundWidget.setState(null);
+    expect(document.body.innerHTML).toMatchSnapshot();
+    expect(document.querySelector('input').value).toBe('');
+  });
+
+  test('focus() focuses the input', () => {
+    boundWidget.focus();
+
+    // Note: This widget always focuses the unchosen button, even if it has a value
+    expect(document.activeElement).toBe(document.querySelector('.unchosen button'));
   });
 });
