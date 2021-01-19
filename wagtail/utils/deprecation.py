ca908356bc753b1078a3cc4f6d203dd2a96181c1
Matt Westcott <matt@west.co.tt>
2021-01-19 09:49:12 +0000

    Rotate deprecation warnings

--- wagtail/utils/deprecation.py
+++ wagtail/utils/deprecation.py
@@ -3,14 +3,14 @@ import warnings
 from importlib import import_module
 
 
-class RemovedInWagtail213Warning(DeprecationWarning):
+class RemovedInWagtail214Warning(DeprecationWarning):
     pass
 
 
-removed_in_next_version_warning = RemovedInWagtail213Warning
+removed_in_next_version_warning = RemovedInWagtail214Warning
 
 
-class RemovedInWagtail214Warning(PendingDeprecationWarning):
+class RemovedInWagtail215Warning(PendingDeprecationWarning):
     pass
 
 
