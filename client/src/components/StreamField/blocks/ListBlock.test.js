83e77984be08d0de606d9a8d607e4b4ef34d412f
Matt Westcott <matt@west.co.tt>
2021-02-02 14:48:57 +0000

    Localise the Add string

--- client/src/components/StreamField/blocks/ListBlock.test.js
+++ client/src/components/StreamField/blocks/ListBlock.test.js
@@ -68,6 +68,7 @@ describe('telepath: wagtail.blocks.ListBlock', () => {
           MOVE_UP: 'Move up',
           MOVE_DOWN: 'Move down',
           DELETE: 'Delete',
+          ADD: 'Add',
         },
       }
     );
