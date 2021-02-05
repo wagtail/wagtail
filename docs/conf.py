21049c3bff7cdef109b72af25160f97eb8f9df80
Storm Heg <storm@stormheg.co>
2021-02-05 16:43:57 +0100

    Docs: disable .rst 'show source' button

--- docs/conf.py
+++ docs/conf.py
@@ -151,6 +151,7 @@ html_theme_options = {
     "logo_url": "https://docs.wagtail.io/",
 }
 
+
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 # html_title = None
@@ -202,7 +203,7 @@ html_extra_path = ['robots.txt']
 # html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-# html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 # html_show_sphinx = True
