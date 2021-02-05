f230718dc9d1f08cb001883682676a66860da166
Coen van der Kamp <coen@fourdigits.nl>
2021-02-05 14:14:22 +0100

    Add theme

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 #     html_theme = 'sphinx_rtd_theme'
 #     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-# import sphinx_typo3_theme
+import sphinx_typo3_theme
 
 html_theme = 'sphinx_typo3_theme'
 
