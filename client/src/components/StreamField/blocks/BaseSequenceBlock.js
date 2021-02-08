4ee65760fe846188803302f44f6435cd537384d3
Matt Westcott <matt@west.co.tt>
2021-02-08 22:39:15 +0000

    Add BaseSequenceBlock superclass

--- client/src/components/StreamField/blocks/BaseSequenceBlock.js
+++ client/src/components/StreamField/blocks/BaseSequenceBlock.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-underscore-dangle */
+
 /* global $ */
 
 import { escapeHtml as h } from '../../../utils/text';
@@ -149,5 +151,220 @@ export class BaseInsertionControl {
 
   delete() {
     $(this.element).hide().attr('aria-hidden', 'true');
+  }
+}
+
+
+export class BaseSequenceBlock {
+  // eslint-disable-next-line no-unused-vars
+  _createChild(blockDef, placeholder, prefix, index, id, initialState, opts) {
+    throw new Error('not implemented');
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  _createInsertionControl(placeholder, opts) {
+    throw new Error('not implemented');
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  _getChildDataForInsertion(opts) {
+    throw new Error('not implemented');
+  }
+
+  clear() {
+    this.countInput.val(0);
+    this.sequenceContainer.empty();
+    this.children = [];
+    this.blockCounter = 0;
+
+    // Create initial insertion control
+    const placeholder = document.createElement('div');
+    this.sequenceContainer.append(placeholder);
+    this.inserters = [
+      this._createInsertionControl(
+        placeholder, {
+          index: 0,
+          onRequestInsert: (newIndex, opts) => {
+            this._onRequestInsert(newIndex, opts);
+          },
+          strings: this.blockDef.meta.strings,
+        }
+      )
+    ];
+  }
+
+  _onRequestInsert(index, opts) {
+    /* handler for an 'insert new block' action */
+    const [blockDef, initialState] = this._getChildDataForInsertion(opts);
+    const newChild = this._insert(blockDef, initialState, null, index, { animate: true });
+    newChild.focus();
+  }
+
+
+  _insert(childBlockDef, initialState, id, index, opts) {
+    const prefix = this.prefix + '-' + this.blockCounter;
+    const animate = opts && opts.animate;
+    this.blockCounter++;
+
+    /*
+    a new inserter and block will be inserted AFTER the inserter with the given index;
+    e.g if there are 3 blocks the children of sequenceContainer will be
+    [inserter 0, block 0, inserter 1, block 1, inserter 2, block 2, inserter 3]
+    and inserting a new block at index 1 will create a new block 1 and inserter 2 after the
+    current inserter 1, and increment everything after that point
+    */
+    const existingInserterElement = this.inserters[index].element;
+    const blockPlaceholder = document.createElement('div');
+    const inserterPlaceholder = document.createElement('div');
+    $(blockPlaceholder).insertAfter(existingInserterElement);
+    $(inserterPlaceholder).insertAfter(blockPlaceholder);
+
+    /* shuffle up indexes of all blocks / inserters above this index */
+    for (let i = index; i < this.children.length; i++) {
+      this.children[i].setIndex(i + 1);
+    }
+    for (let i = index + 1; i < this.inserters.length; i++) {
+      this.inserters[i].setIndex(i + 1);
+    }
+
+    const child = this._createChild(childBlockDef, blockPlaceholder, prefix, index, id, initialState, {
+      animate,
+      onRequestDuplicate: (i) => { this.duplicateBlock(i); },
+      onRequestDelete: (i) => { this.deleteBlock(i); },
+      onRequestMoveUp: (i) => { this.moveBlock(i, i - 1); },
+      onRequestMoveDown: (i) => { this.moveBlock(i, i + 1); },
+      strings: this.blockDef.meta.strings,
+    });
+    this.children.splice(index, 0, child);
+
+    const inserter = this._createInsertionControl(
+      inserterPlaceholder, {
+        index: index + 1,
+        onRequestInsert: (newIndex, inserterOpts) => {
+          this._onRequestInsert(newIndex, inserterOpts);
+        },
+        strings: this.blockDef.meta.strings,
+      }
+    );
+    this.inserters.splice(index + 1, 0, inserter);
+
+    this.countInput.val(this.blockCounter);
+
+    const isFirstChild = (index === 0);
+    const isLastChild = (index === this.children.length - 1);
+    if (!isFirstChild) {
+      child.enableMoveUp();
+      if (isLastChild) {
+        /* previous child (which was previously the last one) can now move down */
+        this.children[index - 1].enableMoveDown();
+      }
+    }
+    if (!isLastChild) {
+      child.enableMoveDown();
+      if (isFirstChild) {
+        /* next child (which was previously the first one) can now move up */
+        this.children[index + 1].enableMoveUp();
+      }
+    }
+
+    return child;
+  }
+
+  deleteBlock(index) {
+    this.children[index].markDeleted({ animate: true });
+    this.inserters[index].delete();
+    this.children.splice(index, 1);
+    this.inserters.splice(index, 1);
+
+    /* index numbers of children / inserters above this index now need updating to match
+    their array indexes */
+    for (let i = index; i < this.children.length; i++) {
+      this.children[i].setIndex(i);
+    }
+    for (let i = index; i < this.inserters.length; i++) {
+      this.inserters[i].setIndex(i);
+    }
+
+    if (index === 0 && this.children.length > 0) {
+      /* we have removed the first child; the new first child cannot be moved up */
+      this.children[0].disableMoveUp();
+    }
+    if (index === this.children.length && this.children.length > 0) {
+      /* we have removed the last child; the new last child cannot be moved down */
+      this.children[this.children.length - 1].disableMoveDown();
+    }
+  }
+
+  moveBlock(oldIndex, newIndex) {
+    if (oldIndex === newIndex) return;
+    const inserterToMove = this.inserters[oldIndex];
+    const childToMove = this.children[oldIndex];
+
+    /* move HTML elements */
+    if (newIndex > oldIndex) {
+      $(inserterToMove.element).insertAfter(this.children[newIndex].element);
+    } else {
+      $(inserterToMove.element).insertBefore(this.inserters[newIndex].element);
+    }
+    $(childToMove.element).insertAfter(inserterToMove.element);
+
+    /* reorder items in the `inserters` and `children` arrays */
+    this.inserters.splice(oldIndex, 1);
+    this.inserters.splice(newIndex, 0, inserterToMove);
+    this.children.splice(oldIndex, 1);
+    this.children.splice(newIndex, 0, childToMove);
+
+    /* update index properties of moved items */
+    if (newIndex > oldIndex) {
+      for (let i = oldIndex; i <= newIndex; i++) {
+        this.inserters[i].setIndex(i);
+        this.children[i].setIndex(i);
+      }
+    } else {
+      for (let i = newIndex; i <= oldIndex; i++) {
+        this.inserters[i].setIndex(i);
+        this.children[i].setIndex(i);
+      }
+    }
+
+    /* enable/disable up/down arrows as required */
+    const maxIndex = this.children.length - 1;
+    if (oldIndex === 0) {
+      childToMove.enableMoveUp();
+      this.children[0].disableMoveUp();
+    }
+    if (oldIndex === maxIndex) {
+      childToMove.enableMoveDown();
+      this.children[maxIndex].disableMoveDown();
+    }
+    if (newIndex === 0) {
+      childToMove.disableMoveUp();
+      this.children[1].enableMoveUp();
+    }
+    if (newIndex === maxIndex) {
+      childToMove.disableMoveDown();
+      this.children[maxIndex - 1].enableMoveDown();
+    }
+  }
+
+  setState(values) {
+    this.clear();
+    values.forEach((val, i) => {
+      this.insert(val, i);
+    });
+  }
+
+  getState() {
+    return this.children.map(child => child.getState());
+  }
+
+  getValue() {
+    return this.children.map(child => child.getValue());
+  }
+
+  focus() {
+    if (this.children.length) {
+      this.children[0].focus();
+    }
   }
 }
