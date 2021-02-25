d1c63cec8c65ab5523f75e03ee78ac527b3d74d5
Tibor Leupold <tibor.leupold@torchbox.com>
2021-02-24 22:58:12 -1000

    Add .md as configured source file extension

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ if not on_rtd:
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 # source_encoding = 'utf-8-sig'
