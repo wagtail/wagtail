7d3c28e0d54f5179e9ac78bfa463e224f30562fc
Andy Babic <andy.babic@torchbox.com>
2021-02-28 16:55:38 +0000

    Change default attribute copying behaviour, and add copy_attrs_exclude argument for Page.get_specific()

--- wagtail/core/models.py
+++ wagtail/core/models.py
@@ -1169,11 +1169,17 @@ class Page(AbstractPage, index.Indexed, ClusterableModel, metaclass=PageBase):
             )
         )
 
-    def get_specific(self, deferred=False, copy_attrs=None):
+    def get_specific(self, deferred=False, copy_attrs=None, copy_attrs_exclude=None):
         """
         .. versionadded:: 2.12
 
         Return this page in its most specific subclassed form.
+
+        .. versionchanged:: 2.13
+            * When ``copy_attrs`` is not supplied, all known non-field attribute
+              values are copied to the returned object. Previously, no non-field
+              values would be copied.
+            * The ``copy_attrs_exclude`` option was added.
 
         By default, a database query is made to fetch all field values for the
         specific object. If you only require access to custom methods or other
@@ -1182,10 +1188,17 @@ class Page(AbstractPage, index.Indexed, ClusterableModel, metaclass=PageBase):
         specific field values from the returned object will trigger additional
         database queries.
 
-        If there are attribute values on this object that you wish to be copied
-        over to the specific version (for example: evaluated relationship field
-        values, annotations or cached properties), use `copy_attrs`` to pass an
-        iterable of names of attributes you wish to be copied.
+        By default, references to all non-field attribute values are copied
+        from current object to the returned one. This includes:
+        * Values set by a queryset, for example: annotations, or values set as
+          a result of using ``select_related()`` or ``prefetch_related()``.
+        * Any ``cached_property`` values that have been evaluated.
+        * Attributes set elsewhere in Python code.
+
+        For fine-grained control over which non-field values are copied to the
+        returned object, you can use ``copy_attrs`` to specify a complete list
+        of attribute names to include. Alternatively, you can use
+        ``copy_attrs_exclude`` to specify a list of attribute names to exclude.
 
         If called on a page object that is already an instance of the most
         specific class (e.g. an ``EventPage``), the object will be returned
@@ -1225,10 +1238,18 @@ class Page(AbstractPage, index.Indexed, ClusterableModel, metaclass=PageBase):
             # Fetch object from database
             specific_obj = model_class._default_manager.get(id=self.id)
 
-        # Copy additional attribute values
-        for attr in copy_attrs or ():
-            if attr in self.__dict__:
+        # Copy non-field attribute values
+        if copy_attrs is not None:
+            for attr in (attr for attr in copy_attrs if attr in self.__dict__):
                 setattr(specific_obj, attr, getattr(self, attr))
+        else:
+            exclude = copy_attrs_exclude or ()
+            for k, v in (
+                (k, v) for k, v in self.__dict__.items()
+                if k not in exclude
+            ):
+                # only set values that haven't already been set
+                specific_obj.__dict__.setdefault(k, v)
 
         return specific_obj
 
