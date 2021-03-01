7a53effeb1ada3c57753fb6f804f52418f79a0b3
jacobtoppm <jacobtm@torchbox.com>
2021-03-01 14:59:27 +0000

    Add decorator to comments, allowing them to be focused

--- client/src/components/Draftail/CommentableEditor/CommentableEditor.js
+++ client/src/components/Draftail/CommentableEditor/CommentableEditor.js
@@ -46,9 +46,61 @@ function getCommentControl(commentApp, contentPath, fieldNode) {
   }
 }
 
+function findCommentStyleRanges(contentBlock, callback) {
+    contentBlock.findStyleRanges((metadata) => metadata.getStyle().some((style) => style.startsWith('COMMENT')), (start, end) => {callback(start, end)})
+}
+
+function getCommentDecorator(commentApp) {
+  const CommentDecorator = ({ contentState, children }) => {
+    // The comment decorator makes a comment clickable, allowing it to be focused.
+    // It does not provide styling, as draft-js imposes a 1 decorator/string limit, which would prevent comment highlights
+    // going over links/other entities
+    const blockKey = children[0].props.block.getKey()
+    const start = children[0].props.start
+    const commentId = useMemo(() => parseInt(contentState.getBlockForKey(blockKey).getInlineStyleAt(start).find((style) => style.startsWith('COMMENT')).slice(8)), [blockKey, start]);
+    const annotationNode = useRef(null);
+    useEffect(() => {
+      // Add a ref to the annotation, allowing the comment to float alongside the attached text.
+      // This adds rather than sets the ref, so that a comment may be attached across paragraphs or around entities
+      const annotation = commentApp.layout.commentAnnotations.get(commentId)
+      if (annotation) {
+        annotation.addRef(annotationNode);
+      }
+      return () => {
+        const annotation = commentApp.layout.commentAnnotations.get(commentId)
+        if (annotation) {
+          annotation.removeRef(annotationNode);
+        }
+      }
+    });
+    const onClick = () => {
+      // Pin and focus the clicked comment
+      commentApp.store.dispatch(
+        commentApp.actions.setFocusedComment(commentId)
+      );
+      commentApp.store.dispatch(
+        commentApp.actions.setPinnedComment(commentId)
+      );
+    }
+    // TODO: determine the correct way to make this accessible, allowing both editing and focus jumps
+    return (
+      <a 
+        type="button"
+        ref={annotationNode}
+        onClick={onClick}
+        data-annotation
+      >
+        {children}
+      </a>
+    )
+  }
+  return CommentDecorator
+}
+
 function CommentableEditor({commentApp, fieldNode, contentPath, rawContentState, onSave, ...options}) {
     const [editorState, setEditorState] = useState(() => createEditorStateFromRaw(rawContentState));
     const CommentControl = useMemo(() => getCommentControl(commentApp, contentPath, fieldNode), [commentApp, contentPath, fieldNode]);
+    const CommentDecorator = useMemo(() => getCommentDecorator(commentApp), [commentApp])
     const enabled = useSelector(commentApp.selectors.selectEnabled);
 
     const timeoutRef = useRef();
@@ -68,6 +120,10 @@ function CommentableEditor({commentApp, fieldNode, contentPath, rawContentState,
     onChange={setEditorState}
     editorState={editorState}
     controls={enabled ? [CommentControl] : []}
+    decorators={enabled ? [{
+      strategy: findCommentStyleRanges,
+      component: CommentDecorator 
+    }] : []}
     {...options}
   />
 
