328e7012fccb3a61aefbfe1ede9c9e61ff2b0d91
Tibor Leupold <tibor.leupold@torchbox.com>
2021-03-08 01:51:06 -1000

    Switch documentation search to Algolia DocSearch (#6833)
    Co-authored-by: Scott Cranfill <scott@scottcranfill.com>
    Co-authored-by: Thibaud Colas <thibaudcolas@gmail.com>
    Co-authored-by: Andy Chosak <andy@chosak.org>

--- docs/conf.py
+++ docs/conf.py
@@ -187,7 +187,10 @@ html_extra_path = ['robots.txt']
 # html_domain_indices = True
 
 # If false, no index is generated.
-# html_use_index = True
+# Since we are implementing search with Algolia DocSearch, we do not need Sphinx to
+# generate its own index. It might not hurt to keep the Sphinx index, but it
+# could potentially speed up the build process.
+html_use_index = False
 
 # If true, the index is split into individual pages for each letter.
 # html_split_index = False
