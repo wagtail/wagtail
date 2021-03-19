16754a40e5cfd83c29b7ac70e19a495ff5c3ef69
jacobtoppm <jacobtm@torchbox.com>
2021-03-19 17:19:24 +0000

    Move entrypoint

--- wagtail/admin/static_src/wagtailadmin/app/comments.entry.js
+++
@@ -1,9 +0,0 @@
-import comments from '../../../../../client/src/components/CommentApp/comments';
-
-/**
- * Entry point loaded when the comments system is in use.
- */
-// Expose module as a global.
-window.comments = comments;
-
-comments.initComments();
