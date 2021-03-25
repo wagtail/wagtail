cb3be0fdced5e471ac2ceda01ba1d4cf8b956f74
Jacob Topp-Mugglestone <55137073+jacobtoppm@users.noreply.github.com>
2021-03-25 09:56:27 +0000

    Feature/redux comments (#6856)
    Adds inline commenting to the Draftail editor (FE only), and refactors field level comments to subscribe to updates from the store directly
    
    
    
    * Refactor field level comments to subscribe to updates directly from the commenting store, rather than calling methods on supplied widgets and annotations directly from the commenting app
    
    * Update Draftail and React-Redux packages in preparation for Draftail inline commenting
    
    * Add CommentableEditor version of Draftail as controlled component, in preparation for manipulating state from the comments system
    
    * Only initialize CommentableEditor if comments are on and the contentpath is valid. Add a comment-adding control to CommentableEditor
    
    * Update eslint
    
    * Remove comment adding control from Draftail if comments are disabled
    
    * fixup! Only initialize CommentableEditor if comments are on and the contentpath is valid. Add a comment-adding control to CommentableEditor
    
    * Add decorator to comments, allowing them to be focused
    
    * Add inline styling to comments
    
    * Make Draftail instance accessible via the DOM node on CommentableEditor as well
    
    * Force rerender for styles and decorators when necessary, and filter out deleted comments
    
    * Remove comment styles when saving Draftail content
    
    * Fix formatting error
    
    * Remove unnecessary comment
    
    * Don't use addition for string concatenation
    
    * Newline
    
    * Add explanatory comment about save logic
    
    * fixup! Don't use addition for string concatenation
    
    * Use more idiomatic undefined check
    
    * Fix aria-label for comment button
    
    * Use span to decorate link
    
    Co-authored-by: Thibaud Colas <thibaudcolas@gmail.com>
    
    * Update getFullSelectionState comment
    
    Co-authored-by: Thibaud Colas <thibaudcolas@gmail.com>
    
    * Reorder selection state generation
    
    Co-authored-by: Thibaud Colas <thibaudcolas@gmail.com>
    
    * Remove unused argument
    
    Co-authored-by: Thibaud Colas <thibaudcolas@gmail.com>
    
    * Make draftail position comments by median annotation, and pin by clicked comment
    
    * Remove inline return
    
    * Make setPinnedComment an option on setFocusedComment
    
    * Add JSDoc comments and remove unused attribute
    
    * use decoratorRef instead of Ref for clarity in annotation
    
    * fixup! Update eslint
    
    * Update Draftail snapshot
    
    * Move entrypoint
    
    * Prettier reformat and eslint fix
    
    * Use Typescript for CommentableEditor
    
    * Install types for react-redux and draft-js
    
    * Remove unused popPage from interface
    
    * Add draftjs-filters as an explicit dependency
    
    * fixup! Use Typescript for CommentableEditor
    
    * Add explicit type for timeout
    
    Co-authored-by: Thibaud Colas <thibaudcolas@gmail.com>

--- client/src/components/CommentApp/comments.js
+++ client/src/components/CommentApp/comments.js
@@ -1,16 +1,8 @@
-import { initCommentsApp } from 'wagtail-comment-frontend';
+import { initCommentApp } from 'wagtail-comment-frontend';
 import { STRINGS } from '../../config/wagtailConfig';
 
 function initComments() {
-  // in case any widgets try to initialise themselves before the comment app,
-  // store their initialisations as callbacks to be executed when the comment app
-  // itself is finished initialising.
-  const callbacks = [];
-  window.commentApp = {
-    registerWidget: (widget) => {
-      callbacks.push(() => { window.commentApp.registerWidget(widget); });
-    }
-  };
+  window.commentApp = initCommentApp();
   document.addEventListener('DOMContentLoaded', () => {
     const commentsElement = document.getElementById('comments');
     const commentsOutputElement = document.getElementById('comments-output');
@@ -19,10 +11,9 @@ function initComments() {
       throw new Error('Comments app failed to initialise. Missing HTML element');
     }
     const data = JSON.parse(dataElement.textContent);
-    window.commentApp = initCommentsApp(
+    window.commentApp.renderApp(
       commentsElement, commentsOutputElement, data.user, data.comments, new Map(Object.entries(data.authors)), STRINGS
     );
-    callbacks.forEach((callback) => { callback(); });
   });
 }
 
@@ -41,14 +32,75 @@ function getContentPath(fieldNode) {
   return contentpaths.join('.');
 }
 
+/**
+ * Controls the positioning of a field level comment, and the display of the button
+ * used to focus and pin the attached comment
+ * `getDesiredPosition` is called by the comments app to determine the height
+ * at which to float the comment.
+ */
 class BasicFieldLevelAnnotation {
-  constructor(fieldNode, node) {
+  /**
+  * Create a field-level annotation
+  * @param {Element} fieldNode - an element to provide the comment position
+  * @param {Element} node - the button to focus/pin the comment
+  * @param commentApp - the commentApp the annotation is integrating with
+  */
+  constructor(fieldNode, node, commentApp) {
     this.node = node;
     this.fieldNode = fieldNode;
-    this.position = '';
+    this.unsubscribe = null;
+    this.commentApp = commentApp;
+  }
+  /**
+  * Subscribes the annotation to update when the state of a particular comment changes,
+  * and to focus that comment when clicked
+  * @param {number} localId - the localId of the comment to subscribe to
+  */
+  subscribeToUpdates(localId) {
+    const { selectFocused, selectEnabled } = this.commentApp.selectors;
+    const selectComment = this.commentApp.utils.selectCommentFactory(localId);
+    const store = this.commentApp.store;
+    const initialState = store.getState();
+    let focused = selectFocused(initialState) === localId;
+    let shown = selectEnabled(initialState);
+    if (focused) {
+      this.onFocus();
+    }
+    if (shown) {
+      this.show();
+    }
+    this.unsubscribe = store.subscribe(() => {
+      const state = store.getState();
+      const comment = selectComment(state);
+      if (!comment) {
+        this.onDelete();
+      }
+      const nowFocused = (selectFocused(state) === localId);
+      if (nowFocused !== focused) {
+        if (focused) {
+          this.onUnfocus();
+        } else {
+          this.onFocus();
+        }
+        focused = nowFocused;
+      }
+      if (shown !== selectEnabled(state)) {
+        if (shown) {
+          this.hide();
+        } else {
+          this.show();
+        }
+        shown = selectEnabled(state);
+      }
+    }
+    );
+    this.setOnClickHandler(localId);
   }
   onDelete() {
     this.node.remove();
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
   }
   onFocus() {
     this.node.classList.remove('button-secondary');
@@ -56,7 +108,7 @@ class BasicFieldLevelAnnotation {
   }
   onUnfocus() {
     this.node.classList.add('button-secondary');
-    this.node.ariaLabel = STRINGS.UNFOCUS_COMMENT;
+    this.node.ariaLabel = STRINGS.FOCUS_COMMENT;
     // TODO: ensure comment is focused accessibly when this is clicked,
     // and that screenreader users can return to the annotation point when desired
   }
@@ -66,8 +118,12 @@ class BasicFieldLevelAnnotation {
   hide() {
     this.node.classList.add('u-hidden');
   }
-  setOnClickHandler(handler) {
-    this.node.addEventListener('click', handler);
+  setOnClickHandler(localId) {
+    this.node.addEventListener('click', () => {
+      this.commentApp.store.dispatch(
+        this.commentApp.actions.setFocusedComment(localId, { updatePinnedComment: true })
+      );
+    });
   }
   getDesiredPosition() {
     return (
@@ -81,34 +137,73 @@ class FieldLevelCommentWidget {
   constructor({
     fieldNode,
     commentAdditionNode,
-    annotationTemplateNode
+    annotationTemplateNode,
+    commentApp
   }) {
     this.fieldNode = fieldNode;
     this.contentpath = getContentPath(fieldNode);
     this.commentAdditionNode = commentAdditionNode;
     this.annotationTemplateNode = annotationTemplateNode;
-    this.commentNumber = 0;
-    this.commentsEnabled = false;
+    this.shown = false;
+    this.commentApp = commentApp;
   }
-  onRegister(makeComment) {
-    this.commentAdditionNode.addEventListener('click', () => {
-      makeComment(this.getAnnotationForComment(), this.contentpath);
+  register() {
+    const { selectEnabled } = this.commentApp.selectors;
+    const initialState = this.commentApp.store.getState();
+    let currentlyEnabled = selectEnabled(initialState);
+    const selectCommentsForContentPath = this.commentApp.utils.selectCommentsForContentPathFactory(
+      this.contentpath
+    );
+    let currentComments = selectCommentsForContentPath(initialState);
+    this.updateVisibility(currentComments.length === 0 && currentlyEnabled);
+    const unsubscribeWidget = this.commentApp.store.subscribe(() => {
+      const state = this.commentApp.store.getState();
+      const newComments = selectCommentsForContentPath(state);
+      const newEnabled = selectEnabled(state);
+      const commentsChanged = (currentComments !== newComments);
+      const enabledChanged = (currentlyEnabled !== newEnabled);
+      if (commentsChanged) {
+        // Add annotations for any new comments
+        currentComments = newComments;
+        currentComments.filter((comment) => comment.annotation === null).forEach((comment) => {
+          const annotation = this.getAnnotationForComment(comment);
+          this.commentApp.updateAnnotation(
+            annotation,
+            comment.localId
+          );
+          annotation.subscribeToUpdates(comment.localId);
+        });
+      }
+      if (enabledChanged || commentsChanged) {
+        // If comments have been enabled or disabled, or the comments have changed
+        // check whether to show the widget (if comments are enabled and there are no existing comments)
+        currentlyEnabled = newEnabled;
+        this.updateVisibility(currentComments.length === 0 && currentlyEnabled);
+      }
     });
+    initialState.comments.comments.forEach((comment) => {
+      // Add annotations for any comments already in the store
+      if (comment.contentpath === this.contentpath) {
+        const annotation = this.getAnnotationForComment(comment);
+        this.commentApp.updateAnnotation(annotation, comment.localId);
+        annotation.subscribeToUpdates(comment.localId);
+      }
+    });
+    this.commentAdditionNode.addEventListener('click', () => {
+      // Make the widget button clickable to add a comment
+      const annotation = this.getAnnotationForComment();
+      const localId = this.commentApp.makeComment(annotation, this.contentpath);
+      annotation.subscribeToUpdates(localId);
+    });
+    return unsubscribeWidget; // TODO: listen for widget deletion and use this
   }
-  setEnabled(enabled) {
-    // Update whether comments are enabled for the page
-    this.commentsEnabled = enabled;
-    this.updateVisibility();
-  }
-  onChangeComments(comments) {
-    // Receives a list of comments for the widget's contentpath
-    this.commentNumber = comments.length;
-    this.updateVisibility();
-  }
-  updateVisibility() {
-    // if comments are disabled, or the widget already has at least one associated comment,
-    // don't show the comment addition button
-    if (!this.commentsEnabled || this.commentNumber > 0) {
+  updateVisibility(newShown) {
+    if (newShown === this.shown) {
+      return;
+    }
+    this.shown = newShown;
+
+    if (!this.shown) {
       this.commentAdditionNode.classList.add('u-hidden');
     } else {
       this.commentAdditionNode.classList.remove('u-hidden');
@@ -119,7 +214,7 @@ class FieldLevelCommentWidget {
     annotationNode.id = '';
     annotationNode.classList.remove('u-hidden');
     this.commentAdditionNode.insertAdjacentElement('afterend', annotationNode);
-    return new BasicFieldLevelAnnotation(this.fieldNode, annotationNode);
+    return new BasicFieldLevelAnnotation(this.fieldNode, annotationNode, this.commentApp);
   }
 }
 
@@ -127,14 +222,16 @@ function initFieldLevelCommentWidget(fieldElement) {
   const widget = new FieldLevelCommentWidget({
     fieldNode: fieldElement,
     commentAdditionNode: fieldElement.querySelector('[data-comment-add]'),
-    annotationTemplateNode: document.querySelector('#comment-icon')
+    annotationTemplateNode: document.querySelector('#comment-icon'),
+    commentApp: window.commentApp
   });
   if (widget.contentpath) {
-    window.commentApp.registerWidget(widget);
+    widget.register();
   }
 }
 
 export default {
+  getContentPath,
   initComments,
   FieldLevelCommentWidget,
   initFieldLevelCommentWidget
