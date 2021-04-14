cc324e09af1cdd204aaec6440dad91bb2a04ff4b
Matt Westcott <matt@west.co.tt>
2021-04-14 13:03:10 +0100

    Remove deprecated `providing_args` options on signals

--- wagtail/core/signals.py
+++ wagtail/core/signals.py
@@ -1,17 +1,46 @@
 from django.dispatch import Signal
 
 
-page_published = Signal(providing_args=['instance', 'revision'])
-page_unpublished = Signal(providing_args=['instance'])
-pre_page_move = Signal(providing_args=['instance', 'parent_page_before', 'parent_page_after', 'url_path_before', 'url_path_after'])
-post_page_move = Signal(providing_args=['instance', 'parent_page_before', 'parent_page_after', 'url_path_before', 'url_path_after'])
+# Page signals
 
-workflow_approved = Signal(providing_args=['instance', 'user'])
-workflow_rejected = Signal(providing_args=['instance', 'user'])
-workflow_cancelled = Signal(providing_args=['instance', 'user'])
-workflow_submitted = Signal(providing_args=['instance', 'user'])
+# provides args: instance, revision
+page_published = Signal()
 
-task_approved = Signal(providing_args=['instance', 'user'])
-task_rejected = Signal(providing_args=['instance', 'user'])
-task_submitted = Signal(providing_args=['instance', 'user'])
-task_cancelled = Signal(providing_args=['instance' 'user'])
+# provides args: instance
+page_unpublished = Signal()
+
+# provides args: instance, parent_page_before, parent_page_after, url_path_before, url_path_after
+pre_page_move = Signal()
+
+# provides args: instance, parent_page_before, parent_page_after, url_path_before, url_path_after
+post_page_move = Signal()
+
+
+# Workflow signals
+
+# provides args: instance, user
+workflow_approved = Signal()
+
+# provides args: instance, user
+workflow_rejected = Signal()
+
+# provides args: instance, user
+workflow_cancelled = Signal()
+
+# provides args: instance, user
+workflow_submitted = Signal()
+
+
+# Workflow task signals
+
+# provides args: instance, user
+task_approved = Signal()
+
+# provides args: instance, user
+task_rejected = Signal()
+
+# provides args: instance, user
+task_submitted = Signal()
+
+# provides args: instance, user
+task_cancelled = Signal()
