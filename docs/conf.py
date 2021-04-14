f591a9c06b1e105bcd6f34004796fc505b4e7d6d
Coen van der Kamp <coen@fourdigits.nl>
2021-04-15 00:28:20 +0200

    Set project name to 'Wagtail Documentation'

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,10 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 html_theme = 'sphinx_wagtail_theme'
 html_theme_path = [sphinx_wagtail_theme.get_html_theme_path()]
 
+html_theme_options = dict(
+    project_name="Wagtail Documentation",
+)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
