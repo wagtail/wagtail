b080f09beaaf16e034c902fd40b79cfd5877ab67
frmdstryr <frmdstryr@protonmail.com>
2021-04-23 08:40:37 -0400

    Check query count on second load

--- wagtail/contrib/modeladmin/tests/test_simple_modeladmin.py
+++ wagtail/contrib/modeladmin/tests/test_simple_modeladmin.py
@@ -2,7 +2,6 @@ from io import BytesIO
 from unittest import mock
 
 from django.contrib.auth.models import Group
-from django.contrib.contenttypes.models import ContentType
 from django.core import checks
 from django.test import TestCase
 from openpyxl import load_workbook
@@ -755,7 +754,6 @@ class TestPermissionsCached(TestCase, WagtailTestUtils):
     def test_permission_helper_cache(self):
         # Ensure that the permissions helper caches the model permissions
         # and only does one query per modeladmin
-        ContentType.objects.clear_cache()
-        Site.get_site_root_paths()
-        with self.assertNumQueries(35):
+        self.client.get('/admin/modeladmintest/author/')
+        with self.assertNumQueries(25):
             self.client.get('/admin/modeladmintest/author/')
