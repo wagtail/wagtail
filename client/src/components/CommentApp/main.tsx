c1b19376cf0e6d1b16bcf008b78d60b033f9f8ff
Karl Hobley <karl@torchbox.com>
2021-04-26 15:56:25 +0100

    Put notices straight after the relevant comment/reply

--- client/src/components/CommentApp/main.tsx
+++ client/src/components/CommentApp/main.tsx
@@ -69,7 +69,7 @@ export const defaultStrings = {
   MORE_ACTIONS: 'More actions',
   SAVE_PAGE_TO_ADD_COMMENT: 'Save the page to add this comment',
   SAVE_PAGE_TO_SAVE_COMMENT_CHANGES: 'Save the page to save this comment',
-  SAVE_PAGE_TO_SAVE_REPLY: 'Save the page to save replies',
+  SAVE_PAGE_TO_SAVE_REPLY: 'Save the page to save this reply',
 };
 
 /* eslint-disable camelcase */
