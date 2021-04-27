6e559117dc0e8c6a1b05482748260267b46cf7f0
Storm Heg <storm@stormheg.co>
2021-04-27 15:23:17 +0200

    Appease eslint

--- client/src/entrypoints/admin/userbar.js
+++ client/src/entrypoints/admin/userbar.js
@@ -2,7 +2,7 @@
 // Please stick to old JS APIs and avoid importing anything that might require a vendored module
 // More background can be found in webpack.config.js
 
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener('DOMContentLoaded', () => {
   const userbar = document.querySelector('[data-wagtail-userbar]');
   const trigger = userbar.querySelector('[data-wagtail-userbar-trigger]');
   const list = userbar.querySelector('.wagtail-userbar-items');
@@ -18,7 +18,45 @@ document.addEventListener('DOMContentLoaded', (e) => {
   window.addEventListener('pageshow', hideUserbar, false);
 
   // Listen for keyboard events
+  // eslint-disable-next-line @typescript-eslint/no-use-before-define
   window.addEventListener('keydown', handleKeyDown);
+
+
+  function showUserbar(shouldFocus) {
+    userbar.classList.add(isActiveClass);
+    trigger.setAttribute('aria-expanded', 'true');
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    list.addEventListener(clickEvent, sandboxClick, false);
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    window.addEventListener(clickEvent, clickOutside, false);
+
+    // The userbar has role=menu which means that the first link should be focused on popup
+    // For weird reasons shifting focus only works after some amount of delay
+    // Which is why we are forced to use setTimeout
+    if (shouldFocus) {
+      setTimeout(() => {
+        list.querySelector('a').focus();
+      }, 300); // Less than 300ms doesn't seem to work
+    }
+  }
+
+  function hideUserbar() {
+    userbar.classList.remove(isActiveClass);
+    trigger.setAttribute('aria-expanded', 'false');
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    list.addEventListener(clickEvent, sandboxClick, false);
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    window.removeEventListener(clickEvent, clickOutside, false);
+  }
+
+  function toggleUserbar(e2) {
+    e2.stopPropagation();
+    if (userbar.classList.contains(isActiveClass)) {
+      hideUserbar();
+    } else {
+      showUserbar(true);
+    }
+  }
 
   function setFocusToTrigger() {
     setTimeout(() => trigger.focus(), 300);
@@ -46,7 +84,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
     if (listItems.length > 0) {
       setTimeout(() => {
         listItems[listItems.length - 1].firstElementChild.focus();
-      }, 100);  // Workaround for focus bug
+      }, 100); // Workaround for focus bug
     }
   }
 
@@ -73,14 +111,14 @@ document.addEventListener('DOMContentLoaded', (e) => {
             // Focus the previous item
             listItems[idx - 1].firstElementChild.focus();
           }
-        }, 100);  // Workaround for focus bug
+        }, 100); // Workaround for focus bug
       }
     });
   }
 
   function handleKeyDown(event) {
     // Only handle keyboard input if the userbar is open
-    if(userbar.classList.contains(isActiveClass)) {
+    if (userbar.classList.contains(isActiveClass)) {
       if (event.key === 'Escape') {
         // eslint-disable-next-line @typescript-eslint/no-use-before-define
         hideUserbar();
@@ -110,61 +148,24 @@ document.addEventListener('DOMContentLoaded', (e) => {
           break;
         }
       }
+      return;
     }
     // Check if the userbar is focused (but not open yet) and should be opened by keyboard input
-    else {
-      if(trigger === document.activeElement) {
-        switch (event.key) {
-          case "ArrowUp":
-            event.preventDefault();
-            showUserbar(false);
-            setTimeout(() => setFocusToFirstItem(), 300); // Workaround for focus bug
-            break;
-          case "ArrowDown":
-            event.preventDefault();
-            showUserbar(false);
-            setTimeout(() => setFocusToLastItem(), 300); // Workaround for focus bug
-            break;
-          default:
-            break;
-        }
+    if (trigger === document.activeElement) {
+      switch (event.key) {
+      case 'ArrowUp':
+        event.preventDefault();
+        showUserbar(false);
+        setTimeout(() => setFocusToFirstItem(), 300); // Workaround for focus bug
+        break;
+      case 'ArrowDown':
+        event.preventDefault();
+        showUserbar(false);
+        setTimeout(() => setFocusToLastItem(), 300); // Workaround for focus bug
+        break;
+      default:
+        break;
       }
-    }
-  }
-
-  function showUserbar(shouldFocus) {
-    userbar.classList.add(isActiveClass);
-    trigger.setAttribute('aria-expanded', 'true');
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    list.addEventListener(clickEvent, sandboxClick, false);
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    window.addEventListener(clickEvent, clickOutside, false);
-
-    // The userbar has role=menu which means that the first link should be focused on popup
-    // For weird reasons shifting focus only works after some amount of delay
-    // Which is why we are forced to use setTimeout
-    if(shouldFocus) {
-      setTimeout(() => {
-        list.querySelector('a').focus();
-      }, 300); // Less than 300ms doesn't seem to work
-    }
-  }
-
-  function hideUserbar() {
-    userbar.classList.remove(isActiveClass);
-    trigger.setAttribute('aria-expanded', 'false');
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    list.addEventListener(clickEvent, sandboxClick, false);
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    window.removeEventListener(clickEvent, clickOutside, false);
-  }
-
-  function toggleUserbar(e2) {
-    e2.stopPropagation();
-    if (userbar.classList.contains(isActiveClass)) {
-      hideUserbar();
-    } else {
-      showUserbar(true);
     }
   }
 
