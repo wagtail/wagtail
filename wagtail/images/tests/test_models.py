c4499a45984ba7f3fd60ffe81cd3b09e2ee0c8d6
Shreyash Srivastava <shreyash.rocks.ever.10231@gmail.com>
2021-05-11 21:16:31 +0530

    Added tests for Rendition.full_url property

--- wagtail/images/tests/test_models.py
+++ wagtail/images/tests/test_models.py
@@ -250,6 +250,11 @@ class TestRenditions(TestCase):
     def test_alt_attribute(self):
         rendition = self.image.get_rendition('width-400')
         self.assertEqual(rendition.alt, "Test image")
+        
+    def test_full_url(self):
+        full_url = self.image.get_rendition('original').full_url
+        full_url_string = "http://testserver/media/images/test.original.png"
+        self.assertEqual(full_url, full_url_string)
 
     @override_settings(
         CACHES={
