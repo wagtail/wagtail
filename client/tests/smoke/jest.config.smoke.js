5c81a14675d681e8e4a5bda0c46c696a8991593d
janehughes <janeelizabethhughes@gmail.com>
2021-05-11 17:43:55 +0100

    wip: update config

--- client/tests/smoke/jest.config.smoke.js
+++ client/tests/smoke/jest.config.smoke.js
@@ -1,9 +1,10 @@
 // See https://jestjs.io/docs/en/configuration.
 module.exports = {
     moduleNameMapper: {
-        '^.+\\.(css|scss)$': '<rootDir>/client/tests/mocks/fileMock.js',
+        '^.+\\.(css|scss)$': '<rootDir>/../mocks/fileMock.js',
         '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-            '<rootDir>/client/tests/mocks/fileMock.js',
+            '<rootDir>/../mocks/fileMock.js',
+        'jquery': '<rootDir>/../../../wagtail/admin/static_src/wagtailadmin/js/vendor/jquery-3.5.1.min.js'
     },
     moduleFileExtensions: ['js', 'ts', 'tsx', 'json', 'node'],
     transform: {
@@ -11,6 +12,10 @@ module.exports = {
         // '^.+\\.(css|scss)$': 'jest-transform-css',
         '^.+\\.mdx?$': '@storybook/addon-docs/jest-transform-mdx',
     },
+    transformIgnorePatterns: [
+        '<rootDir>/../../../node_modules/(?!@dump247).+(ts|tsx|js|jsx)$',
+    ],
+    setupFilesAfterEnv: ['<rootDir>/../setupTests.ts', '<rootDir>/../adapter.js', '<rootDir>/../stubs.js', '<rootDir>/../mock-fetch.js', '<rootDir>/../mock-jquery.js'],
     globals: {
         APP_URL_PRODUCTION: 'http://localhost/',
         APP_URL_DEVELOPMENT: 'http://localhost/',
--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,18 @@
 // See https://jestjs.io/docs/en/configuration.
 module.exports = {
-    moduleNameMapper: {},
+    moduleNameMapper: {
+        'jquery': '<rootDir>/wagtail/admin/static_src/wagtailadmin/js/vendor/jquery-3.5.1.min.js'
+    },
     moduleFileExtensions: ['js', 'ts', 'tsx', 'json', 'node'],
     transform: {
         '^.+\\.(js|ts|tsx)$': 'ts-jest',
         '^.+\\.mdx?$': '@storybook/addon-docs/jest-transform-mdx',
     },
     collectCoverageFrom: ['src/**/*.{js,ts,tsx}', '!<rootDir>/node_modules/'],
-    testPathIgnorePatterns: ['/node_modules/', '/smoke/'],
-    coveragePathIgnorePatterns: ['<rootDir>/client/tests', '.stories.{js,ts,tsx}'],
-    setupFilesAfterEnv: ['<rootDir>/client/tests/setupTests.ts'],
+    testPathIgnorePatterns: ['/node_modules/', '/smoke/', '/build/'],
+    coveragePathIgnorePatterns: ['/node_modules/', '/tests/', '<rootDir>/client/tests', '.stories.{js,ts,tsx}'],
+    setupFilesAfterEnv: ['<rootDir>/client/tests/setupTests.ts', '<rootDir>/client/tests/adapter.js', '<rootDir>/client/tests/stubs.js', '<rootDir>/client/tests/mock-fetch.js', '<rootDir>/client/tests/mock-jquery.js'],
+    snapshotSerializers: ['enzyme-to-json/serializer'],
     globals: {
         APP_URL_PRODUCTION: 'http://localhost/',
         APP_URL_DEVELOPMENT: 'http://localhost/',
