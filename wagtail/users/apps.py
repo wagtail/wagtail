7fbd656a437c0b215da3a085ff888c0c693046fd
Matt Westcott <matt@west.co.tt>
2021-05-11 15:07:48 +0100

    flake8

--- wagtail/users/apps.py
+++ wagtail/users/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
+
 class WagtailUsersAppConfig(AppConfig):
     name = 'wagtail.users'
     label = 'wagtailusers'
