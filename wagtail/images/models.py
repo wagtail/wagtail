6cc002813311e0e8e1a7961953fa1eb63ffddb23
Shreyash Srivastava <shreyash.rocks.ever.10231@gmail.com>
2021-05-11 21:13:05 +0530

    Added full_url property on image renditions. #6803

--- wagtail/images/models.py
+++ wagtail/images/models.py
@@ -525,6 +525,13 @@ class AbstractRendition(models.Model):
             ('height', self.height),
             ('alt', self.alt),
         ])
+    
+    @property
+    def full_url(self):
+        url = self.url
+        if settings.BASE_URL and url.startswith("/"):
+            url = settings.BASE_URL + url
+        return url
 
     def img_tag(self, extra_attributes={}):
         attrs = self.attrs_dict.copy()
