7cd3f6f53cef0247315905d4f707c0d67571e149
Thibaud Colas <thibaudcolas@gmail.com>
2021-05-11 22:28:25 +0100

    Attempt to fix TypeScript issues

--- client/src/components/CommentApp/utils/storybook.tsx
+++ client/src/components/CommentApp/utils/storybook.tsx
@@ -56,6 +56,7 @@ export function RenderCommentsForStorybook({
       }
       comment={comment}
       isVisible={true}
+      forceFocus={false}
       isFocused={comment.localId === state.comments.focusedComment}
       strings={defaultStrings}
     />
@@ -102,7 +103,7 @@ export function addTestComment(
   );
 
   if (options.focused) {
-    store.dispatch(setFocusedComment(commentId, { updatePinnedComment: true }));
+    store.dispatch(setFocusedComment(commentId, { updatePinnedComment: true, forceFocus: false }));
   }
 
   return commentId;
