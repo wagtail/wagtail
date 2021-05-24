2f161f357b8822fe303c7903726c9553bd0a7373
Matt Westcott <matt@west.co.tt>
2021-05-24 15:32:18 +0100

    Reorganise snippet admin URLs to avoid ambiguous paths
    Snippet admin views allow for arbitrary strings as primary keys, and the current URL patterns don't adequately namespace these from reserved words like 'add' - for example, a snippet with the primary key 'add' would have an edit URL that collides with the add view at `/admin/snippets/foo/bar/add/`.
    
    This is unlikely to come up in practice, but it does mean that our urlconf is more sensitive to ordering than it needs to be. Rearrange so that the verb (add, edit, delete) consistently comes before the pk, and add redirects to handle the legacy URLs.

--- wagtail/snippets/urls.py
+++ wagtail/snippets/urls.py
@@ -13,8 +13,14 @@ urlpatterns = [
 
     path('<slug:app_label>/<slug:model_name>/', snippets.list, name='list'),
     path('<slug:app_label>/<slug:model_name>/add/', snippets.create, name='add'),
-    path('<slug:app_label>/<slug:model_name>/<str:pk>/', snippets.edit, name='edit'),
+    path('<slug:app_label>/<slug:model_name>/edit/<str:pk>/', snippets.edit, name='edit'),
     path('<slug:app_label>/<slug:model_name>/multiple/delete/', snippets.delete, name='delete-multiple'),
-    path('<slug:app_label>/<slug:model_name>/<str:pk>/delete/', snippets.delete, name='delete'),
-    path('<slug:app_label>/<slug:model_name>/<str:pk>/usage/', snippets.usage, name='usage'),
+    path('<slug:app_label>/<slug:model_name>/delete/<str:pk>/', snippets.delete, name='delete'),
+    path('<slug:app_label>/<slug:model_name>/usage/<str:pk>/', snippets.usage, name='usage'),
+
+    # legacy URLs that could potentially collide if the pk matches one of the reserved names above
+    # ('add', 'edit' etc) - redirect to the unambiguous version
+    path('<slug:app_label>/<slug:model_name>/<str:pk>/', snippets.redirect_to_edit),
+    path('<slug:app_label>/<slug:model_name>/<str:pk>/delete/', snippets.redirect_to_delete),
+    path('<slug:app_label>/<slug:model_name>/<str:pk>/usage/', snippets.redirect_to_usage),
 ]
