8487c67ebb35984f7694537e779583c7d6b92cf4
Jaap Roes <jaap.roes@gmail.com>
2021-05-24 18:20:13 +0200

    Simplify oembed dict missing key fallback (#6922)

--- wagtail/embeds/finders/oembed.py
+++ wagtail/embeds/finders/oembed.py
@@ -72,9 +72,9 @@ class OEmbedFinder(EmbedFinder):
 
         # Return embed as a dict
         return {
-            'title': oembed['title'] if 'title' in oembed else '',
-            'author_name': oembed['author_name'] if 'author_name' in oembed else '',
-            'provider_name': oembed['provider_name'] if 'provider_name' in oembed else '',
+            'title': oembed.get('title', ''),
+            'author_name': oembed.get('author_name', ''),
+            'provider_name': oembed.get('provider_name', ''),
             'type': oembed['type'],
             'thumbnail_url': oembed.get('thumbnail_url'),
             'width': oembed.get('width'),
