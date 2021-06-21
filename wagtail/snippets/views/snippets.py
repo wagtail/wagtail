1a0e53ba09e18e7986611e34097117c6a2c98cd2
Matt Westcott <matt@west.co.tt>
2021-06-21 22:06:01 +0100

    Add context manager to keep track of current user when logging
    Any `log` calls within a `with LogContext(user=request.user):` block can omit the user argument and have it picked up from the context instead.

--- wagtail/snippets/views/snippets.py
+++ wagtail/snippets/views/snippets.py
@@ -18,7 +18,7 @@ from wagtail.admin import messages
 from wagtail.admin.edit_handlers import ObjectList, extract_panel_definitions_from_model_class
 from wagtail.admin.forms.search import SearchForm
 from wagtail.core import hooks
-from wagtail.core.log_actions import log
+from wagtail.core.log_actions import LogContext, log
 from wagtail.core.models import Locale, TranslatableMixin
 from wagtail.search.backends import get_search_backend
 from wagtail.search.index import class_is_indexed
@@ -212,7 +212,8 @@ def create(request, app_label, model_name):
 
         if form.is_valid():
             form.save()
-            log(instance=instance, action='wagtail.create', user=request.user)
+            with LogContext(user=request.user):
+                log(instance=instance, action='wagtail.create')
 
             messages.success(
                 request,
