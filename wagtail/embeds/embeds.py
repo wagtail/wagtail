5a73b8cabdb5406f300e0f8b6c741f3cf2abd257
Gordon Pendleton <wgordonw1@gmail.com>
2021-06-23 09:36:11 -0400

    added null check to embed exclusion

--- wagtail/embeds/embeds.py
+++ wagtail/embeds/embeds.py
@@ -13,7 +13,7 @@ def get_embed(url, max_width=None, finder=None):
 
     # Check database
     try:
-        return Embed.objects.exclude(cache_until__lte=now()).get(hash=embed_hash)
+        return Embed.objects.exclude(cache_until__isnull=False, cache_until__lte=now()).get(hash=embed_hash)
     except Embed.DoesNotExist:
         pass
 
