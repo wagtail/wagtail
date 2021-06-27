8409a8c217065361e52080462947eddf9ca2f10e
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-06-28 00:17:17 +0200

    Export the appropriate backend

--- wagtail/search/backends/database/__init__.py
+++ wagtail/search/backends/database/__init__.py
@@ -1,8 +1,14 @@
+from django.db import connection
+
 from .fallback import DatabaseSearchBackend
+from .postgres import PostgresSearchBackend
 
 
 def SearchBackend(params):
     """
     Returns the appropriate search backend for the current 'default' database system
     """
-    return DatabaseSearchBackend(params)
+    if connection.vendor == 'postgresql':
+        return PostgresSearchBackend(params)
+    else:
+        return DatabaseSearchBackend(params)
