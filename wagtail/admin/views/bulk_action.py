74b2c03f5a98248f4094dbbef52bd24858beb3b9
Shohan <shohanduttaroy99@gmail.com>
2021-07-02 01:42:59 +0530

    [feat] Add 'Select all pages in listing' support

--- wagtail/admin/views/bulk_action.py
+++ wagtail/admin/views/bulk_action.py
@@ -2,7 +2,6 @@ from abc import ABC, abstractproperty
 
 from django.db import transaction
 from django.shortcuts import get_list_or_404, redirect
-from django.urls import reverse
 from django.views.generic.base import TemplateView
 
 from wagtail.admin import messages
