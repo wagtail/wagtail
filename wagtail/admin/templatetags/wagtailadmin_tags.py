f4b33ce7f03b53e76b9abf4bfb199e400d37f821
Shohan <shohanduttaroy99@gmail.com>
2021-07-11 15:56:09 +0530

    [refactor] Refactor after review commits
    Added following
    - Add prepare action method that runs before transaction starts
    - Add a default form with a default field
    
    Changed following
    - Move permissions check in bulk move from before hook to a method that runs before the transaction starts
    - Select all pages now doesn't fetch all ids, but passes id as all and the parent page id
    - Change no access permissions
    
    Fixed following
    - Fix tests for changes in html

--- wagtail/admin/templatetags/wagtailadmin_tags.py
+++ wagtail/admin/templatetags/wagtailadmin_tags.py
@@ -495,7 +495,7 @@ def bulk_action_filters(context):
     for hook in button_hooks:
         buttons.extend(hook())
 
-    buttons.sort()
+    buttons.sort(key=lambda x: x.priority)
 
     return {'buttons': buttons}
 
--- wagtail/admin/views/pages/bulk_actions/page_bulk_action.py
+++ wagtail/admin/views/pages/bulk_actions/page_bulk_action.py
@@ -1,7 +1,25 @@
+from django import forms
+
 from wagtail.admin.views.bulk_action import BulkAction
 from wagtail.core.models import Page
+
+
+class DefaultForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['include_descendants'] = forms.BooleanField(
+            required=False
+        )
 
 
 class PageBulkAction(BulkAction):
     model = Page
     object_key = 'page'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = DefaultForm()
+        context['pages_with_no_access'] = [
+            {'page': page, 'can_edit': page.permissions_for_user(self.request.user).can_edit()} for page in context['pages_with_no_access']
+        ]
+        return context
