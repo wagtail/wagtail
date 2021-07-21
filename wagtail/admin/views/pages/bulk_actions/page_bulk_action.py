3e942eb94c984adb965e9d66157f969943143890
Shohan <shohanduttaroy99@gmail.com>
2021-07-21 14:24:20 +0530

    [refactor] Base bulk action class now uses FormView instead of TemplateView
    Changed following
    - Most bulk actions have a form requirement. Therefore, subclassing FormView makes everything easier, the form is cleaned before the bulk action is processed, and any errors are returned with appropriate error strings. The cleaned form data is available to all methods after the form is cleaned.

--- wagtail/admin/views/pages/bulk_actions/page_bulk_action.py
+++ wagtail/admin/views/pages/bulk_actions/page_bulk_action.py
@@ -4,7 +4,7 @@ from wagtail.admin.views.bulk_action import BulkAction
 from wagtail.core.models import Page
 
 
-class DefaultForm(forms.Form):
+class DefaultPageForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['include_descendants'] = forms.BooleanField(
@@ -15,10 +15,10 @@ class DefaultForm(forms.Form):
 class PageBulkAction(BulkAction):
     model = Page
     object_key = 'page'
+    form_class = DefaultPageForm
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['form'] = DefaultForm()
         context['pages_with_no_access'] = [
             {'page': page, 'can_edit': page.permissions_for_user(self.request.user).can_edit()} for page in context['pages_with_no_access']
         ]
