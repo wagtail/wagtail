f4ad6865730951f50f692f9baecd32490f2e6fd1
Shohan <shohanduttaroy99@gmail.com>
2021-07-29 01:19:13 +0530

    [feat] Add view and template to add images to a collection in bulk

--- wagtail/images/views/bulk_actions/__init__.py
+++ wagtail/images/views/bulk_actions/__init__.py
@@ -1,5 +1,6 @@
 from .add_tags import add_tags
+from .add_to_collection import add_to_collection
 from .delete import delete
 
 
-__all__ = ['add_tags', 'delete']
+__all__ = ['add_tags', 'add_to_collection', 'delete']
