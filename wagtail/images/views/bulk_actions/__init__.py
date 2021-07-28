0143f6617ba68439826e00be74f311e90f6611bf
Shohan <shohanduttaroy99@gmail.com>
2021-07-29 01:20:44 +0530

    [feat] Add action dispatcher for images bulk actions

--- wagtail/images/views/bulk_actions/__init__.py
+++ wagtail/images/views/bulk_actions/__init__.py
@@ -1,6 +1,7 @@
 from .add_tags import add_tags
 from .add_to_collection import add_to_collection
 from .delete import delete
+from .dispatcher import index
 
 
-__all__ = ['add_tags', 'add_to_collection', 'delete']
+__all__ = ['add_tags', 'add_to_collection', 'delete', 'index']
