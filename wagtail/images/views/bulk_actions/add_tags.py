a023738614d92a60ecab526fd7bb99e476ea30d2
Shohan <shohanduttaroy99@gmail.com>
2021-07-29 01:17:40 +0530

    [feat] Add view and template to add tags to images in bulk

---
+++ wagtail/images/views/bulk_actions/add_tags.py
@@ -0,0 +1,54 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ngettext
+
+from wagtail.admin import widgets
+from wagtail.core import hooks
+from wagtail.images.views.bulk_actions.image_bulk_action import ImageBulkAction
+
+
+class TagForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['tags'] = forms.Field(
+            widget=widgets.AdminTagWidget
+        )
+
+
+class AddTagsBulkAction(ImageBulkAction):
+    display_name = _("Add tags")
+    action_type = "add_tags"
+    aria_label = _("Add tags to images")
+    template_name = "wagtailimages/bulk_actions/confirm_bulk_add_tags.html"
+    action_priority = 20
+    form_class = TagForm
+
+    def check_perm(self, image):
+        return self.permission_policy.user_has_permission_for_instance(self.request.user, 'change', image)
+
+    def get_execution_context(self):
+        return {
+            'tags': self.cleaned_form.cleaned_data['tags'].split(',')
+        }
+
+    @classmethod
+    def execute_action(cls, images, **kwargs):
+        tags = kwargs.get('tags', [])
+        for image in images:
+            cls.num_parent_objects += 1
+            image.tags.add(*tags)
+            image.save()
+
+    def get_success_message(self):
+        return ngettext(
+            "New tags have been added to %(num_parent_objects)d image",
+            "New tags have been added to %(num_parent_objects)d images",
+            self.num_parent_objects
+        ) % {
+            'num_parent_objects': self.num_parent_objects
+        }
+
+
+@hooks.register('register_image_bulk_action')
+def add_tags(request):
+    return AddTagsBulkAction(request)
