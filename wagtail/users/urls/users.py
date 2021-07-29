6ce2c33817017dc5923290fd5fc7073d0aa5f4a7
Shohan <shohanduttaroy99@gmail.com>
2021-07-30 01:46:50 +0530

    [feat] Add action dispatcher for users bulk actions

--- wagtail/users/urls/users.py
+++ wagtail/users/urls/users.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from wagtail.users.views import users
+from wagtail.users.views import bulk_actions, users
 
 
 app_name = 'wagtailusers_users'
 urlpatterns = [
     path('', users.index, name='index'),
+    path('multiple/<str:action>/', bulk_actions.index, name='user_bulk_action'),
     path('add/', users.create, name='add'),
     path('<str:user_id>/', users.edit, name='edit'),
     path('<str:user_id>/delete/', users.delete, name='delete'),
