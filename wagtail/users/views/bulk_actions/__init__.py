23a440c4495560b84db0e8a21df470924f65a911
Shohan <shohanduttaroy99@gmail.com>
2021-07-30 01:46:50 +0530

    [feat] Add action dispatcher for users bulk actions

--- wagtail/users/views/bulk_actions/__init__.py
+++ wagtail/users/views/bulk_actions/__init__.py
@@ -1,6 +1,7 @@
 from .assign_role import assign_role
 from .delete import delete
+from .dispatcher import index
 from .toggle_activity import toggle_activity
 
 
-__all__ = ['assign_role', 'delete', 'toggle_activity']
+__all__ = ['assign_role', 'delete', 'index', 'toggle_activity']
