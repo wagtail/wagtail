59181f77783ac0fda0becbed60aaab2d205c1aac
Shohan <shohanduttaroy99@gmail.com>
2021-07-30 02:01:14 +0530

    [feat] Add tests for users bulk actions

---
+++ wagtail/users/tests/test_bulk_actions/test_bulk_toggle_activity.py
@@ -0,0 +1,90 @@
+from django.contrib.auth import get_user_model
+from django.http import HttpRequest, HttpResponse
+from django.test import TestCase
+from django.urls import reverse
+
+from wagtail.tests.utils import WagtailTestUtils
+from wagtail.users.views.bulk_actions.user_bulk_action import UserBulkAction
+
+
+class TestUserToggleActivityView(TestCase, WagtailTestUtils):
+    def setUp(self):
+        # create a set of test users
+        self.test_users = [
+            self.create_user(
+                username=f'testuser-{i}',
+                email=f'testuser{i}@email.com',
+                password=f'password-{i}',
+                is_active=(i & 1)  # odd numbered users will be active
+            ) for i in range(1, 6)
+        ]
+        self.current_user = self.login()
+        self.url = reverse('wagtailusers_users:user_bulk_action', args=('toggle_activity',)) + '?'
+        self.self_toggle_url = self.url + f'id={self.current_user.pk}'
+        for user in self.test_users:
+            self.url += f'id={user.pk}&'
+        self.make_active_data = {'mark_as_active': True}
+        self.make_inactive_data = {'mark_as_active': False}
+
+    def test_simple(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'wagtailusers/bulk_actions/confirm_bulk_toggle_activity.html')
+
+    def test_bulk_toggle(self):
+        response = self.client.post(self.url, self.make_active_data)
+
+        # Should redirect back to index
+        self.assertEqual(response.status_code, 302)
+
+        # Check that the users were marked as active
+        for user in self.test_users:
+            self.assertTrue(get_user_model().objects.get(email=user.email).is_active)
+
+    def test_user_cannot_mark_self_as_inactive(self):
+        response = self.client.post(self.self_toggle_url, self.make_inactive_data)
+
+        self.assertEqual(response.status_code, 200)
+        html = response.content.decode()
+        self.assertInHTML("<p>You cannot mark yourself as inactive</p>", html)
+
+        needle = '<ul>'
+        needle += '<li>{user_email}</li>'.format(user_email=self.current_user.email)
+        needle += '</ul>'
+        self.assertInHTML(needle, html)
+
+        # Check user was not marked as inactive
+        self.assertTrue(get_user_model().objects.get(pk=self.current_user.pk).is_active)
+
+    def test_before_toggle_user_hook_post(self):
+        def hook_func(request, action_type, users, action_class_instance):
+            self.assertEqual(action_type, 'toggle_activity')
+            self.assertIsInstance(request, HttpRequest)
+            self.assertIsInstance(action_class_instance, UserBulkAction)
+            self.assertCountEqual([user.pk for user in self.test_users], [user.pk for user in users])
+
+            return HttpResponse("Overridden!")
+
+        with self.register_hook('before_bulk_action', hook_func):
+            response = self.client.post(self.url, self.make_active_data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"Overridden!")
+
+    def test_after_toggle_user_hook(self):
+        def hook_func(request, action_type, users, action_class_instance):
+            self.assertEqual(action_type, 'toggle_activity')
+            self.assertIsInstance(request, HttpRequest)
+            self.assertIsInstance(action_class_instance, UserBulkAction)
+            self.assertCountEqual([user.pk for user in self.test_users], [user.pk for user in users])
+
+            return HttpResponse("Overridden!")
+
+        with self.register_hook('after_bulk_action', hook_func):
+            response = self.client.post(self.url, self.make_active_data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"Overridden!")
+
+        for user in self.test_users:
+            self.assertTrue(get_user_model().objects.get(email=user.email).is_active)
