d3de9e82efb40e6e2e004961156766af6f8a2c7a
Shohan <shohanduttaroy99@gmail.com>
2021-07-30 01:45:46 +0530

    [feat] Add view and template to toggle user activity in bulk

--- wagtail/users/views/bulk_actions/__init__.py
+++ wagtail/users/views/bulk_actions/__init__.py
@@ -1,5 +1,6 @@
 from .assign_role import assign_role
 from .delete import delete
+from .toggle_activity import toggle_activity
 
 
-__all__ = ['assign_role', 'delete']
+__all__ = ['assign_role', 'delete', 'toggle_activity']
