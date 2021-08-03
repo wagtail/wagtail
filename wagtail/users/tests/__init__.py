3bac236240069139b56e80cd65eb0d1104fd9983
Shohan <shohanduttaroy99@gmail.com>
2021-08-03 11:38:04 +0530

    [fix] Fix failing tests
    Fixed following
    - The list of ids in bulk action view were converted to int before querying. This could have problems when the ids were not meant to be integers,
    e.g. like in the case of EmailUser where it is a uuid. Django converts strings to integers when querying wherever applicable, so explicit
    conversion was removed.

--- wagtail/users/tests/__init__.py
+++ wagtail/users/tests/__init__.py
@@ -1,1 +1,4 @@
-from .test_admin_views import *
+from .test_admin_views import CustomGroupViewSet, CustomUserCreationForm, CustomUserEditForm
+
+
+__all__ = ['CustomGroupViewSet', 'CustomUserCreationForm', 'CustomUserEditForm']
