587677b86204bd7a5c196f1cbb430082ba038c03
Shohan <shohanduttaroy99@gmail.com>
2021-08-06 17:47:00 +0530

    Bulk actions for documents (#7350)
    * [feat] Add bulk delete view and template for documents
    
    * [feat] Add view and template to add tags to documents in bulk
    
    * [feat] Add view and template to add documents to a collection in bulk
    
    * [feat] Add action dispatcher for documents bulk actions
    
    * [feat] Add bulk action ui to documents listing
    
    * [refactor] Refactor code to align with changes in bulk action base class
    
    Changed following
    - Change execute_action from classmethod to instance method
    - Use cleaned form data instead of accessing request.POST data
    - Add can_edit property to objects with no access
    
    * [refactor] Refactor code to use the reusable confirmation templates
    
    * [feat] Add tests for document bulk actions
    
    * [refactor] Change code in execute_action to align with changes in base bulk action class
    
    * [refactor] Use counter instead of num_docs in confirmation templates
    
    * [refactor] Change aria label for checkbox
    
    * [refactor] Use context instead of ctx, and change 'can_edit' to check for 'change' permission
    
    * Show filters only for pages
    
    * [refactor] Reduce width of checkbox column in documents listing

---
+++ wagtail/documents/views/bulk_actions/__init__.py
@@ -0,0 +1,7 @@
+from .add_tags import add_tags
+from .add_to_collection import add_to_collection
+from .delete import delete
+from .dispatcher import index
+
+
+__all__ = ['add_to_collection', 'add_tags', 'delete', 'index']
