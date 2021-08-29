94c0d107f41e69d57e3b82dbe51723dbcb935167
Scott Cranfill <scott.cranfill@jpl.nasa.gov>
2021-08-29 11:45:41 -0700

    Change  to  in collapsible.js, as in #6342

--- client/src/entrypoints/admin/collapsible.js
+++ client/src/entrypoints/admin/collapsible.js
@@ -3,29 +3,29 @@ import $ from "jquery";
 function initCollapsibleBlocks() {
   // eslint-disable-next-line func-names
   $(".object.collapsible").each(function () {
-    const $li = $(this);
-    const $content = $li.find(".object-layout");
+    const $target = $(this);
+    const $content = $target.find(".object-layout");
     const onAnimationComplete = () =>
       $content
         .get(0)
         .dispatchEvent(
           new CustomEvent("commentAnchorVisibilityChange", { bubbles: true })
         );
-    if ($li.hasClass("collapsed") && $li.find(".error-message").length === 0) {
+    if ($target.hasClass("collapsed") && $target.find(".error-message").length === 0) {
       $content.hide({
         complete: onAnimationComplete,
       });
     }
 
-    $li.find("> .title-wrapper").on("click", () => {
-      if (!$li.hasClass("collapsed")) {
-        $li.addClass("collapsed");
+    $target.find("> .title-wrapper").on("click", () => {
+      if (!$target.hasClass("collapsed")) {
+        $target.addClass("collapsed");
         $content.hide({
           duration: "slow",
           complete: onAnimationComplete,
         });
       } else {
-        $li.removeClass("collapsed");
+        $target.removeClass("collapsed");
         $content.show({
           duration: "slow",
           complete: onAnimationComplete,
