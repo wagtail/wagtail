9567cb71d28b638e1420eda28adfc152c60e9d5a
Scott Cranfill <scott.cranfill@jpl.nasa.gov>
2021-08-30 07:54:43 -0700

    Fix JS linting issue

--- client/src/entrypoints/admin/collapsible.js
+++ client/src/entrypoints/admin/collapsible.js
@@ -1,33 +1,33 @@
-import $ from "jquery";
+import $ from 'jquery';
 
 function initCollapsibleBlocks() {
   // eslint-disable-next-line func-names
-  $(".object.collapsible").each(function () {
+  $('.object.collapsible').each(function () {
     const $target = $(this);
-    const $content = $target.find(".object-layout");
+    const $content = $target.find('.object-layout');
     const onAnimationComplete = () =>
       $content
         .get(0)
         .dispatchEvent(
-          new CustomEvent("commentAnchorVisibilityChange", { bubbles: true })
+          new CustomEvent('commentAnchorVisibilityChange', { bubbles: true })
         );
-    if ($target.hasClass("collapsed") && $target.find(".error-message").length === 0) {
+    if ($target.hasClass('collapsed') && $target.find('.error-message').length === 0) {
       $content.hide({
         complete: onAnimationComplete,
       });
     }
 
-    $target.find("> .title-wrapper").on("click", () => {
-      if (!$target.hasClass("collapsed")) {
-        $target.addClass("collapsed");
+    $target.find('> .title-wrapper').on('click', () => {
+      if (!$target.hasClass('collapsed')) {
+        $target.addClass('collapsed');
         $content.hide({
-          duration: "slow",
+          duration: 'slow',
           complete: onAnimationComplete,
         });
       } else {
-        $target.removeClass("collapsed");
+        $target.removeClass('collapsed');
         $content.show({
-          duration: "slow",
+          duration: 'slow',
           complete: onAnimationComplete,
         });
       }
