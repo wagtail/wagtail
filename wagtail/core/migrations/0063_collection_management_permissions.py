43d1f7cc51eea29a613905470e5360bca9d3b982
Cynthia Kiser <cnk@caltech.edu>
2021-09-14 16:30:14 -0700

    Fixed forward migration
    I was always assigning all 3 permissions to a group - even if the group had originally only had a subset of permissions to manage the entire model

--- wagtail/core/migrations/0063_collection_management_permissions.py
+++ wagtail/core/migrations/0063_collection_management_permissions.py
@@ -3,60 +3,54 @@ from django.db import migrations
 
 
 def grant_instance_level_collection_management_permissions(apps, schema_editor):
+    """
+    Give the groups who currently manage all collections permission to manage root collections
+    """
     Collection = apps.get_model('wagtailcore.Collection')
+    Group = apps.get_model('auth.Group')
     GroupCollectionPermission = apps.get_model('wagtailcore.GroupCollectionPermission')
     Permission = apps.get_model('auth.Permission')
 
-    # Give all groups who currently manage collections permission to manage root collection
-    root_collection = Collection.objects.filter(depth=1).order_by('path').first()
-    stock_permissions = Permission.objects.filter(
+    groups_w_permissions = Group.objects.filter(
+        permissions__content_type__app_label='wagtailcore',
+        permissions__content_type__model='collection',
+        permissions__codename__in=['add_collection', 'change_collection', 'delete_collection']
+    ).values('id', 'name', 'permissions__id', 'permissions__codename')
+
+    for root_collection in Collection.objects.filter(depth=1).all():
+        for row in groups_w_permissions:
+            GroupCollectionPermission.objects.create(
+                group_id=row['id'],
+                permission_id=row['permissions__id'],
+                collection_id=root_collection.id,
+            )
+    # Now remove the model-level permissions for collections
+    collection_permissions = Permission.objects.filter(
         content_type__app_label='wagtailcore',
         content_type__model='collection',
-        codename__in=['add_collection', 'change_collection', 'delete_collection']
-    ).prefetch_related('group_set').all()
-
-    for perm in stock_permissions:
-        for group in perm.group_set.all():
-            gcp = GroupCollectionPermission(
-                group_id=group.id,
-                collection_id=root_collection.id,
-                permission_id=perm.id
-            )
-            gcp.full_clean()
-            gcp.save()
-            # Now remove the model-level permissions from the group
-            group.permissions.remove(perm)
+        codename__in=['add_collection', 'change_collection', 'delete_collection'],
+    )
+    for perm in collection_permissions.all():
+        perm.group_set.clear()
 
 
 def revert_to_model_level_collection_management_permissions(apps, schema_editor):
+    """
+    Give model-level permission to all groups who have that permission on the root collection
+    """
     Collection = apps.get_model('wagtailcore.Collection')
-    ContentType = apps.get_model('contenttypes.ContentType')
     GroupCollectionPermission = apps.get_model('wagtailcore.GroupCollectionPermission')
-    Permission = apps.get_model('auth.Permission')
 
-    collection_content_type = ContentType.objects.get(
-        model='collection',
-        app_label='wagtailcore'
-    )
-    root_collection = Collection.objects.filter(depth=1).order_by('path').first()
-    stock_permissions = Permission.objects.filter(
-        content_type__app_label='wagtailcore',
-        content_type__model='collection',
-        codename__in=['add_collection', 'change_collection', 'delete_collection']
-    ).all()
-
-    # Give model-level permissions to all groups who have permission on the root collection
+    root_collections = Collection.objects.filter(depth=1).all()
     group_collection_permissions = GroupCollectionPermission.objects.filter(
-        collection=root_collection,
-        permission__in=stock_permissions
-    ).prefetch_related('group')
+        permission__content_type__app_label='wagtailcore',
+        permission__content_type__model='collection',
+        permission__codename__in=['add_collection', 'change_collection', 'delete_collection'],
+        collection__in=root_collections,
+    ).prefetch_related('group', 'permission')
 
     for row in group_collection_permissions.all():
-        perm = Permission.objects.get(
-            content_type=collection_content_type,
-            codename=row.permission.codename
-        )
-        row.group.permissions.add(perm)
+        row.group.permissions.add(row.permission)
 
     # Now delete the instance-level collection management permissions
     group_collection_permissions.all().delete()
