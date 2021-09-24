202380f7124d9e324ab861dba74c6fb99131bf9b
Vince Salvino <salvino@coderedcorp.com>
2021-09-24 15:35:01 -0400

    Fix sass->css output, use system paths.

--- gulpfile.js/config.js
+++ gulpfile.js/config.js
@@ -23,16 +23,16 @@ App.prototype.scssSources = function() {
 
 // All the Wagtail apps that contain static files
 var apps = [
-    new App('wagtail/admin', {'appName': 'wagtailadmin'}),
-    new App('wagtail/documents', {'appName': 'wagtaildocs'}),
-    new App('wagtail/embeds', {'appName': 'wagtailembeds'}),
-    new App('wagtail/images', {'appName': 'wagtailimages'}),
-    new App('wagtail/search', {'appName': 'wagtailsearch'}),
-    new App('wagtail/snippets', {'appName': 'wagtailsnippets'}),
-    new App('wagtail/users', {'appName': 'wagtailusers'}),
-    new App('wagtail/contrib/styleguide', {'appName': 'wagtailstyleguide'}),
-    new App('wagtail/contrib/settings', {'appName': 'wagtailsettings'}),
-    new App('wagtail/contrib/modeladmin', {'appName': 'wagtailmodeladmin'}),
+    new App(path.join('wagtail', 'admin'), {'appName': 'wagtailadmin'}),
+    new App(path.join('wagtail', 'documents'), {'appName': 'wagtaildocs'}),
+    new App(path.join('wagtail', 'embeds'), {'appName': 'wagtailembeds'}),
+    new App(path.join('wagtail', 'images'), {'appName': 'wagtailimages'}),
+    new App(path.join('wagtail', 'search'), {'appName': 'wagtailsearch'}),
+    new App(path.join('wagtail', 'snippets'), {'appName': 'wagtailsnippets'}),
+    new App(path.join('wagtail', 'users'), {'appName': 'wagtailusers'}),
+    new App(path.join('wagtail', 'contrib', 'styleguide'), {'appName': 'wagtailstyleguide'}),
+    new App(path.join('wagtail', 'contrib', 'settings'), {'appName': 'wagtailsettings'}),
+    new App(path.join('wagtail', 'contrib', 'modeladmin'), {'appName': 'wagtailmodeladmin'}),
 ];
 
 module.exports = {
