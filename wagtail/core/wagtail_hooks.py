d9620d42f388339fdc54bb960e0612efadfc7b38
Hazh. M. Adam <hazhhuman@gmail.com>
2021-10-06 18:56:01 +0800

    Update wagtail/core/wagtail_hooks.py
    Co-authored-by: Dan Braghis <dan@zerolab.org>

--- wagtail/core/wagtail_hooks.py
+++ wagtail/core/wagtail_hooks.py
@@ -284,7 +284,7 @@ def register_core_log_actions(actions):
 
         def format_message(self, log_entry):
             try:
-                return _("Removed the '%(restriction)s' view restriction. The page is public now.") % {
+                return _("Removed the '%(restriction)s' view restriction. The page is public.") % {
                     'restriction': log_entry.data['restriction']['title'],
                 }
 
