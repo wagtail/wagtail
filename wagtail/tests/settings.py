b00d63f5bb28797999e2ad3e8c992ac0b3e1bd52
Matt Westcott <matt@west.co.tt>
2021-10-08 20:24:45 +0100

    Add server-side tests for TypedTableBlock

--- wagtail/tests/settings.py
+++ wagtail/tests/settings.py
@@ -131,6 +131,7 @@ INSTALLED_APPS = [
     'wagtail.contrib.modeladmin',
     'wagtail.contrib.table_block',
     'wagtail.contrib.forms',
+    'wagtail.contrib.typed_table_block',
     'wagtail.search',
     'wagtail.embeds',
     'wagtail.images',
