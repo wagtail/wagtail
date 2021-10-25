cd3175d5361ae56d9ea20ca9f7509ab915655881
Tibor Leupold <tibor.leupold@torchbox.com>
2021-10-25 15:04:39 -0700

    Rename variable for comprehension
    Regexs are notoriously hard to read. The purpose of the regex was not
    immedately clear and I had to use and external tool to explain what the
    regex is meant for.
    
    I hope the renaming will make this more clear now.

--- client/src/utils/version.js
+++ client/src/utils/version.js
@@ -1,7 +1,7 @@
 function compareVersion(versionA, versionB) {
-  const re = /(\.0)+[^.]*$/;
-  const va = (versionA + '').replace(re, '').split('.');
-  const vb = (versionB + '').replace(re, '').split('.');
+  const trailingZeroRe = /(\.0)+[^.]*$/;
+  const va = (versionA + '').replace(trailingZeroRe, '').split('.');
+  const vb = (versionB + '').replace(trailingZeroRe, '').split('.');
   const len = Math.min(va.length, vb.length);
   for (let i = 0; i < len; i++) {
     const cmp = parseInt(va[i], 10) - parseInt(vb[i], 10);
