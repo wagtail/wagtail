8c8220e9c5d43e902d73b317e194286316cbd717
Tibor Leupold <tibor.leupold@torchbox.com>
2021-10-26 11:25:20 -0700

    Move existing version tests form utils.tests.js
    The utils.tests.js seemed a bit unexpected location, because there is no
    `utils.js` module. Now the module defining the tests and the one
    defining the functionality are named similarly, as is common in the rest
    of the code base.

--- client/src/utils/version.test.js
+++ client/src/utils/version.test.js
@@ -1,7 +1,26 @@
-import { compareVersion } from './version';
+import { compareVersion, versionOutOfDate } from './version';
 
-it('returns something', () => {
-  const result = compareVersion('1.12', '1.11');
+describe('version.versionOutOfDate', () => {
+  it('compares 1.5 and 2.4 correctly', () => {
+    expect(versionOutOfDate('1.5', '2.4')).toBeFalsy();
+  });
+  it('compares 1.5.4 and 1.5.5 correctly', () => {
+    expect(versionOutOfDate('1.5.4', '1.5.5')).toBeFalsy();
+  });
+  it('compares 1.5 and 1.5 correctly', () => {
+    expect(versionOutOfDate('1.5', '1.5')).toBeFalsy();
+  });
+  it('compares 2.6a0 and 2.4 correctly', () => {
+    expect(versionOutOfDate('2.6a0', '2.4')).toBeTruthy();
+  });
+});
 
-  expect(result).toBe(1);
+describe('version.compareVersion', () => {
+  it('compares 1.12 and 1.11 correctly', () => {
+    expect(compareVersion('1.12', '1.11')).toBe(1);
+  });
+
+  it('compares 1 and 2 correctly', () => {
+    expect(compareVersion('2', '1')).toBe(1);
+  });
 });
