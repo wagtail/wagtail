de85f28d96f11b4a22660fddb1b701a7076bd94f
Matt Westcott <matt@west.co.tt>
2021-10-28 17:55:08 +0100

    Add mechanism for WagtailAdminModelForm to select an appropriate widget for a ForeignKey to a specific model
    This means that pages, images, documents and snippets will use the approprate chooser widget automatically without having to specify it explicitly in the form definition or use a *ChooserPanel in the edit handler.

--- wagtail/documents/apps.py
+++ wagtail/documents/apps.py
@@ -12,3 +12,13 @@ class WagtailDocsAppConfig(AppConfig):
         from wagtail.documents.signal_handlers import register_signal_handlers
 
         register_signal_handlers()
+
+        # Set up model forms to use AdminDocumentChooser for any ForeignKey to the document model
+        from wagtail.admin.forms.models import FOREIGN_KEY_MODEL_OVERRIDES
+
+        from . import get_document_model
+        from .widgets import AdminDocumentChooser
+
+        FOREIGN_KEY_MODEL_OVERRIDES[get_document_model()] = {
+            "widget": AdminDocumentChooser
+        }
