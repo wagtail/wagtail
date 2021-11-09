wagtail/wagtail

commit 90c86e76d0d6f46208eb1efb2918026d61ee3326

Add a debug logger around rendition generation (#7391)
This allows insight into which images are taking the longest to generate, which fail to render at all, and potentially which images are causing crashes (as in they start, but never stop).

The logging is intentionally only on DEBUG level, so it's opt-in, and is also reasonably quiet so it doesn't bloat logs.

wagtail/images/models.py
@@ -1,5 +1,7 @@
 import hashlib
+import logging
 import os.path
+import time
 
 from collections import OrderedDict
 from contextlib import contextmanager
@@ -27,6 +29,9 @@ from wagtail.images.image_operations import FilterOperation, ImageTransform, Tra
 from wagtail.images.rect import Rect
 from wagtail.search import index
 from wagtail.search.queryset import SearchableQuerySetMixin
+
+
+logger = logging.getLogger("wagtail.images")
 
 
 class SourceImageIOError(IOError):
@@ -303,7 +308,23 @@ class AbstractImage(CollectionMember, index.Indexed, models.Model):
             )
         except Rendition.DoesNotExist:
             # Generate the rendition image
-            generated_image = filter.run(self, BytesIO())
+            try:
+                logger.debug("Generating '%s' rendition for image %d", (
+                    filter.spec,
+                    self.pk,
+                ))
+
+                start_time = time.time()
+                generated_image = filter.run(self, BytesIO())
+
+                logger.debug("Generated '%s' rendition for image %d in %.1fms", (
+                    filter.spec,
+                    self.pk,
+                    (time.time() - start_time) * 1000
+                ))
+            except:  # noqa:B901,E722
+                logger.debug("Failed to generate '%s' rendition for image %d: %s", filter.spec, self.pk)
+                raise
 
             # Generate filename
             input_filename = os.path.basename(self.file.name)

commit 2d0529aa72b21940f292c8019b6a239011b4413e

Use correct syntax for image debug logging
Fixes an issue introduced in  #7391. The arguments are meant to be *args, not a tuple.

wagtail/images/models.py
@@ -311,19 +311,21 @@ class AbstractImage(ImageFileMixin, CollectionMember, index.Indexed, models.Mode
         except Rendition.DoesNotExist:
             # Generate the rendition image
             try:
-                logger.debug("Generating '%s' rendition for image %d", (
+                logger.debug(
+                    "Generating '%s' rendition for image %d",
                     filter.spec,
                     self.pk,
-                ))
+                )
 
                 start_time = time.time()
                 generated_image = filter.run(self, BytesIO())
 
-                logger.debug("Generated '%s' rendition for image %d in %.1fms", (
+                logger.debug(
+                    "Generated '%s' rendition for image %d in %.1fms",
                     filter.spec,
                     self.pk,
                     (time.time() - start_time) * 1000
-                ))
+                )
             except:  # noqa:B901,E722
                 logger.debug("Failed to generate '%s' rendition for image %d: %s", filter.spec, self.pk)
                 raise