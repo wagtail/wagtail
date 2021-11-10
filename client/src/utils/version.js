78bf3c51804357b6d1acba98c04ff1b849701296
Tibor Leupold <tibor.leupold@torchbox.com>
2021-11-10 14:28:34 -0800

    Remove outdated functions and tests

--- client/src/utils/version.js
+++ client/src/utils/version.js
@@ -1,27 +1,3 @@
-function versionStringToArray(versionString) {
-  const trailingZeroRe = /(\.0)+[^.]*$/;
-  return (versionString + '').replace(trailingZeroRe, '').split('.');
-}
-
-function compareVersion(versionA, versionB) {
-  const va = versionStringToArray(versionA);
-  const vb = versionStringToArray(versionB);
-  const len = Math.min(va.length, vb.length);
-  for (let i = 0; i < len; i++) {
-    const cmp = parseInt(va[i], 10) - parseInt(vb[i], 10);
-    if (cmp !== 0) {
-      return cmp;
-    }
-  }
-
-  return va.length - vb.length;
-}
-
-function versionOutOfDate(latestVersion, currentVersion) {
-  // return true;
-  return compareVersion(latestVersion, currentVersion) > 0;
-}
-
 class VersionNumberFormatError extends Error {
   constructor(versionString) {
     this.message = `Version number '${versionString}' is not formatted correctly.`;
--- client/src/utils/version.test.js
+++ client/src/utils/version.test.js
@@ -1,51 +1,9 @@
 import {
-  compareVersion,
-  versionOutOfDate,
   VersionNumberFormatError,
   NotPreReleaseVersionError,
   VersionNumber,
   VersionDeltaType,
 } from './version';
-
-describe.skip('version.compareVersion', () => {
-  it('compares 1.12 and 1.11 correctly', () => {
-    expect(compareVersion('1.12', '1.11')).toBe(1);
-  });
-
-  it('compares 2 and 1 correctly', () => {
-    expect(compareVersion('2', '1')).toBe(1);
-  });
-
-  it('compares 1 and 3 correctly', () => {
-    expect(compareVersion('1', '3')).toBe(-2);
-  });
-
-  it('compares 2.1 and 2 correctly', () => {
-    expect(compareVersion('2.1', '2')).toBe(1);
-  });
-
-  it('compares 2.1.1 and 2 correctly', () => {
-    expect(compareVersion('2.1.1', '2')).toBe(2);
-  });
-});
-
-describe.skip('version.versionOutOfDate', () => {
-  it('compares 1.5 and 2.4 correctly', () => {
-    expect(versionOutOfDate('1.5', '2.4')).toBeFalsy();
-  });
-  it('compares 1.5.4 and 1.5.5 correctly', () => {
-    expect(versionOutOfDate('1.5.4', '1.5.5')).toBeFalsy();
-  });
-  it('compares 1.5 and 1.5 correctly', () => {
-    expect(versionOutOfDate('1.5', '1.5')).toBeFalsy();
-  });
-  it('compares 2.6a0 and 2.4 correctly', () => {
-    expect(versionOutOfDate('2.6a0', '2.4')).toBeTruthy();
-  });
-  it.skip('compares 2.6a0 and 2.6 correctly', () => {
-    expect(versionOutOfDate('2.6a0', '2.6')).toBeTruthy();
-  });
-});
 
 describe('version.VersionDeltaType', () => {
   it('types equal themselves', () => {
