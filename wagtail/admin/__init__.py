bf9c37ef875c6aa05aaf21cdd117a02076e2ac8c
Tibor Leupold <tibor.leupold@torchbox.com>
2021-11-19 11:45:01 -0800

    Set default_app_config only for Django < 3.2

--- wagtail/admin/__init__.py
+++ wagtail/admin/__init__.py
@@ -1,1 +1,8 @@
-default_app_config = 'wagtail.admin.apps.WagtailAdminAppConfig'
+import django
+
+
+if django.VERSION >= (3, 2):
+    # The declaration is only needed for older Django versions
+    pass
+else:
+    default_app_config = 'wagtail.admin.apps.WagtailAdminAppConfig'
