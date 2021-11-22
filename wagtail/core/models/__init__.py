c48fc250bcba325f4f4da96dee5e703218d35921
Tidiane Dia <atdia97@gmail.com>
2021-11-22 14:58:00 +0100

    Convert publish_page_revision into a function and add execute method
    Move PageRevision.publish into an external function

--- wagtail/core/models/__init__.py
+++ wagtail/core/models/__init__.py
@@ -48,6 +48,7 @@ from treebeard.mp_tree import MP_Node
 
 from wagtail.core.actions.copy_page import CopyPageAction
 from wagtail.core.actions.delete_page import DeletePageAction
+from wagtail.core.actions.publish_page_revision import PublishPageRevisionAction
 from wagtail.core.fields import StreamField
 from wagtail.core.forms import TaskStateCommentForm
 from wagtail.core.log_actions import log
@@ -2328,148 +2329,9 @@ class PageRevision(models.Model):
         return super().delete()
 
     def publish(self, user=None, changed=True, log_action=True, previous_revision=None):
-        """
-        Publishes or schedules revision for publishing.
-
-        :param user: the publishing user
-        :param changed: indicated whether content has changed
-        :param log_action:
-            flag for the logging action. Pass False to skip logging. Cannot pass an action string as the method
-            performs several actions: "publish", "revert" (and publish the reverted revision),
-            "schedule publishing with a live revision", "schedule revision reversal publishing, with a live revision",
-            "schedule publishing", "schedule revision reversal publishing"
-        :param previous_revision: indicates a revision reversal. Should be set to the previous revision instance
-        """
-        page = self.as_page_object()
-
-        def log_scheduling_action(revision, user=None, changed=changed):
-            log(
-                instance=page,
-                action='wagtail.publish.schedule',
-                user=user,
-                data={
-                    'revision': {
-                        'id': revision.id,
-                        'created': revision.created_at.strftime("%d %b %Y %H:%M"),
-                        'go_live_at': page.go_live_at.strftime("%d %b %Y %H:%M"),
-                        'has_live_version': page.live,
-                    }
-                },
-                revision=revision,
-                content_changed=changed,
-            )
-
-        if page.go_live_at and page.go_live_at > timezone.now():
-            page.has_unpublished_changes = True
-            # Instead set the approved_go_live_at of this revision
-            self.approved_go_live_at = page.go_live_at
-            self.save()
-            # And clear the the approved_go_live_at of any other revisions
-            page.revisions.exclude(id=self.id).update(approved_go_live_at=None)
-            # if we are updating a currently live page skip the rest
-            if page.live_revision:
-                # Log scheduled publishing
-                if log_action:
-                    log_scheduling_action(self, user, changed)
-
-                return
-            # if we have a go_live in the future don't make the page live
-            page.live = False
-        else:
-            page.live = True
-            # at this point, the page has unpublished changes if and only if there are newer revisions than this one
-            page.has_unpublished_changes = not self.is_latest_revision()
-            # If page goes live clear the approved_go_live_at of all revisions
-            page.revisions.update(approved_go_live_at=None)
-        page.expired = False  # When a page is published it can't be expired
-
-        # Set first_published_at, last_published_at and live_revision
-        # if the page is being published now
-        if page.live:
-            now = timezone.now()
-            page.last_published_at = now
-            page.live_revision = self
-
-            if page.first_published_at is None:
-                page.first_published_at = now
-
-            if previous_revision:
-                previous_revision_page = previous_revision.as_page_object()
-                old_page_title = previous_revision_page.title if page.title != previous_revision_page.title else None
-            else:
-                try:
-                    previous = self.get_previous()
-                except PageRevision.DoesNotExist:
-                    previous = None
-                old_page_title = previous.page.title if previous and page.title != previous.page.title else None
-        else:
-            # Unset live_revision if the page is going live in the future
-            page.live_revision = None
-
-        page.save()
-
-        for comment in getattr(page, COMMENTS_RELATION_NAME).all().only('position'):
-            comment.save(update_fields=['position'])
-
-        self.submitted_for_moderation = False
-        page.revisions.update(submitted_for_moderation=False)
-
-        workflow_state = page.current_workflow_state
-        if workflow_state and getattr(settings, 'WAGTAIL_WORKFLOW_CANCEL_ON_PUBLISH', True):
-            workflow_state.cancel(user=user)
-
-        if page.live:
-            page_published.send(sender=page.specific_class, instance=page.specific, revision=self)
-
-            # Update alias pages
-            page.update_aliases(revision=self, user=user, _content_json=self.content_json)
-
-            if log_action:
-                data = None
-                if previous_revision:
-                    data = {
-                        'revision': {
-                            'id': previous_revision.id,
-                            'created': previous_revision.created_at.strftime("%d %b %Y %H:%M")
-                        }
-                    }
-
-                if old_page_title:
-                    data = data or {}
-                    data['title'] = {
-                        'old': old_page_title,
-                        'new': page.title,
-                    }
-
-                    log(
-                        instance=page,
-                        action='wagtail.rename',
-                        user=user,
-                        data=data,
-                        revision=self,
-                    )
-
-                log(
-                    instance=page,
-                    action=log_action if isinstance(log_action, str) else 'wagtail.publish',
-                    user=user,
-                    data=data,
-                    revision=self,
-                    content_changed=changed,
-                )
-
-            logger.info("Page published: \"%s\" id=%d revision_id=%d", page.title, page.id, self.id)
-        elif page.go_live_at:
-            logger.info(
-                "Page scheduled for publish: \"%s\" id=%d revision_id=%d go_live_at=%s",
-                page.title,
-                page.id,
-                self.id,
-                page.go_live_at.isoformat()
-            )
-
-            if log_action:
-                log_scheduling_action(self, user, changed)
+        return PublishPageRevisionAction(
+            self, user=user, changed=changed, log_action=log_action, previous_revision=previous_revision
+        ).execute()
 
     def get_previous(self):
         return self.get_previous_by_created_at(page=self.page)
