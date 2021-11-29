3ae4219327a80a90a70e82ab19dded2542d438bb
Tidiane Dia <atdia97@gmail.com>
2021-11-29 12:43:20 +0100

    Convert unpublish_page into a function and implement execute method
    Move Page.unpublish into an external function

---
+++ wagtail/core/actions/unpublish_page.py
@@ -0,0 +1,82 @@
+import logging
+
+from django.core.exceptions import PermissionDenied
+
+from wagtail.core.log_actions import log
+from wagtail.core.signals import page_unpublished
+
+
+logger = logging.getLogger("wagtail.core")
+
+
+class UnpublishPagePermissionError(PermissionDenied):
+    """
+    Raised when the page unpublish cannot be performed due to insufficient permissions.
+    """
+
+    pass
+
+
+class UnpublishPageAction:
+    def __init__(self, page, set_expired, commit, user, log_action):
+        self.page = page
+        self.set_expired = set_expired
+        self.commit = commit
+        self.user = user
+        self.log_action = log_action
+
+    def check(self, skip_permission_checks=False):
+        if (
+            self.user
+            and not skip_permission_checks
+            and not self.page.permissions_for_user(self.user).can_unpublish()
+        ):
+            raise UnpublishPagePermissionError(
+                "You do not have permission to unpublish this page"
+            )
+
+    def _unpublish_page(self, page, set_expired, commit, user, log_action):
+        """
+        Unpublish the page by setting ``live`` to ``False``. Does nothing if ``live`` is already ``False``
+        :param log_action: flag for logging the action. Pass False to skip logging. Can be passed an action string.
+            Defaults to 'wagtail.unpublish'
+        """
+        if page.live:
+            page.live = False
+            page.has_unpublished_changes = True
+            page.live_revision = None
+
+            if set_expired:
+                page.expired = True
+
+            if commit:
+                # using clean=False to bypass validation
+                page.save(clean=False)
+
+            page_unpublished.send(sender=page.specific_class, instance=page.specific)
+
+            if log_action:
+                log(
+                    instance=page,
+                    action=log_action if isinstance(log_action, str) else "wagtail.unpublish",
+                    user=user,
+                )
+
+            logger.info('Page unpublished: "%s" id=%d', page.title, page.id)
+
+            page.revisions.update(approved_go_live_at=None)
+
+            # Unpublish aliases
+            for alias in page.aliases.all():
+                alias.unpublish()
+
+    def execute(self, skip_permission_checks=False):
+        self.check(skip_permission_checks=skip_permission_checks)
+
+        return self._unpublish_page(
+            self.page,
+            set_expired=self.set_expired,
+            commit=self.commit,
+            user=self.user,
+            log_action=self.log_action,
+        )
