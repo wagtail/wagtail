d7e6382d97b2ced69e767945bebf2100bd1c1090
Matt Westcott <matt@west.co.tt>
2021-12-08 21:47:16 +0000

    Use wagtail.admin.ui.tables framework for snippet chooser

--- wagtail/snippets/views/chooser.py
+++ wagtail/snippets/views/chooser.py
@@ -8,10 +8,29 @@ from django.views.generic.base import View
 
 from wagtail.admin.forms.search import SearchForm
 from wagtail.admin.modal_workflow import render_modal_workflow
+from wagtail.admin.ui.tables import Table, TitleColumn
 from wagtail.core.models import Locale, TranslatableMixin
 from wagtail.search.backends import get_search_backend
 from wagtail.search.index import class_is_indexed
 from wagtail.snippets.views.snippets import get_snippet_model_from_url_params
+
+
+class ResultsTable(Table):
+    header_row_classname = 'table-headers'
+
+
+class SnippetTitleColumn(TitleColumn):
+    def __init__(self, name, model, **kwargs):
+        self.model_opts = model._meta
+        super().__init__(name, **kwargs)
+
+    def get_value(self, instance):
+        return str(instance)
+
+    def get_link_url(self, instance, parent_context):
+        return reverse(
+            'wagtailsnippets:chosen', args=[self.model_opts.app_label, self.model_opts.model_name, quote(instance.pk)]
+        )
 
 
 class BaseChooseView(View):
@@ -68,6 +87,10 @@ class BaseChooseView(View):
         paginator = Paginator(items, per_page=25)
         self.paginated_items = paginator.get_page(request.GET.get('p'))
 
+        self.table = ResultsTable([
+            SnippetTitleColumn('title', self.model, label=_('Title'), link_classname='snippet-choice'),
+        ], self.paginated_items)
+
         return self.render_to_response()
 
     def render_to_response(self):
@@ -83,6 +106,7 @@ class ChooseView(BaseChooseView):
             {
                 'model_opts': self.model._meta,
                 'items': self.paginated_items,
+                'table': self.table,
                 'is_searchable': self.is_searchable,
                 'search_form': self.search_form,
                 'query_string': self.search_query,
@@ -101,6 +125,7 @@ class ChooseResultsView(BaseChooseView):
         return TemplateResponse(self.request, "wagtailsnippets/chooser/results.html", {
             'model_opts': self.model._meta,
             'items': self.paginated_items,
+            'table': self.table,
             'query_string': self.search_query,
             'is_searching': self.is_searching,
         })
