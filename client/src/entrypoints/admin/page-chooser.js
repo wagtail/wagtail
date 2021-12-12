75650c5fb3de95fa9c92c6711d84e1c82e6234ee
Matt Westcott <matt@west.co.tt>
2021-12-12 16:25:41 +0000

    Move _page_title_choose.html include contents into _list_choose.html

--- client/src/entrypoints/admin/page-chooser.js
+++ client/src/entrypoints/admin/page-chooser.js
@@ -12,7 +12,7 @@ function createPageChooser(id, openAtParentId, options) {
   createPageChooser. State is either null (= no page chosen) or a dict of id, parentId,
   adminTitle (the admin display title) and editUrl.
   The result returned from the page chooser modal (which is ultimately built from the data
-  attributes in wagtailadmin/pages/listing/_page_title_choose.html) is a superset of this, and
+  attributes on the link in wagtailadmin/pages/listing/_list_choose.html) is a superset of this, and
   can therefore be passed directly to chooser.setState.
   */
   let state = null;
