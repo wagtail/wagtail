7127a9c89a5823f095345b84f4d2311fcc7e0f0c
Tidiane Dia <atdia97@gmail.com>
2022-01-05 17:04:48 +0000

    Remove changed, log_action and previous_revision from publish action Admin API

--- wagtail/admin/api/actions/publish.py
+++ wagtail/admin/api/actions/publish.py
@@ -1,6 +1,5 @@
 from django.core.exceptions import ValidationError as DjangoValidationError
-from django.shortcuts import get_object_or_404
-from rest_framework import fields, status
+from rest_framework import status
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
@@ -11,37 +10,13 @@ from wagtail.core.actions.publish_page_revision import PublishPageRevisionAction
 from .base import APIAction
 
 
-class PublishPageAPIActionSerializer(Serializer):
-    changed = fields.BooleanField(default=True, required=False)
-    log_action = fields.BooleanField(default=True, required=False)
-    previous_revision_id = fields.IntegerField(required=False)
-
-
 class PublishPageAPIAction(APIAction):
-    serializer = PublishPageAPIActionSerializer
+    serializer = Serializer
 
     def _action_from_data(self, instance, data):
-        previous_revision = None
         user = self.request.user
-        log_action = data["log_action"]
-
-        previous_revision_id = data.get("previous_revision_id", None)
-        if previous_revision_id:
-            previous_revision = get_object_or_404(
-                instance.revisions, id=previous_revision_id
-            )
-
-        revision = instance.save_revision(
-            user=user, log_action=log_action, previous_revision=previous_revision
-        )
-
-        return PublishPageRevisionAction(
-            revision,
-            user=user,
-            changed=data["changed"],
-            log_action=log_action,
-            previous_revision=previous_revision,
-        )
+        revision = instance.get_latest_revision() or instance.save_revision(user=user)
+        return PublishPageRevisionAction(revision, user=user)
 
     def execute(self, instance, data):
         try:
