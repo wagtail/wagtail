6f43b4f3e87a78f7d6849f870b8ab8f451031a02
Thibaud Colas <thibaudcolas@gmail.com>
2022-01-10 15:22:51 +0000

    Fix "click outside dismiss" implementation in slim sidebar

--- client/src/components/Sidebar/modules/MainMenu.tsx
+++ client/src/components/Sidebar/modules/MainMenu.tsx
@@ -128,10 +128,26 @@ export const Menu: React.FunctionComponent<MenuProps> = (
       }
     };
 
+    const onClickOutside = (e: MouseEvent | TouchEvent) => {
+      const sidebar = document.querySelector('[data-wagtail-sidebar]');
+
+      const isInside = sidebar && sidebar.contains(e.target as Node);
+      if (!isInside) {
+        dispatch({
+          type: 'set-navigation-path',
+          path: ''
+        });
+      }
+    };
+
     document.addEventListener('keydown', onKeydown);
+    document.addEventListener('mousedown', onClickOutside);
+    document.addEventListener('touchend', onClickOutside);
 
     return () => {
       document.removeEventListener('keydown', onKeydown);
+      document.removeEventListener('mousedown', onClickOutside);
+      document.removeEventListener('touchend', onClickOutside);
     };
   }, []);
 
