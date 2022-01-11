155fca9e7e2c8cfee8d2600268ebae8d94b2e7fe
Matt Westcott <matt@west.co.tt>
2022-01-11 17:29:10 +0000

    Add alternative warning if sqlite is >=3.19 but is missing fts5 support

--- wagtail/search/apps.py
+++ wagtail/search/apps.py
@@ -29,6 +29,11 @@ class WagtailSearchAppConfig(AppConfig):
     def check_if_sqlite_version_is_supported(app_configs, **kwargs):
         if connection.vendor == 'sqlite':
             import sqlite3
+
+            from wagtail.search.backends.database.sqlite.utils import fts5_available
+
             if sqlite3.sqlite_version_info < (3, 19, 0):
                 return [Warning('Your SQLite version is older than 3.19.0. A fallback search backend will be used instead.', hint='Upgrade your SQLite version to at least 3.19.0', id='wagtailsearch.W002', obj=WagtailSearchAppConfig)]
+            elif not fts5_available():
+                return [Warning('Your SQLite installation is missing the fts5 extension. A fallback search backend will be used instead.', hint='Upgrade your SQLite installation to a version with fts5 enabled', id='wagtailsearch.W003', obj=WagtailSearchAppConfig)]
         return []
