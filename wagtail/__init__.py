ddcdb5c1a4e807878f30538394404e0fe3e9958c
Rizwan Mansuri <rizwan@webbyfox.co.uk>
2022-01-19 14:55:00 +0000

    Increase dependencies version range to cover security vulneribility

--- wagtail/__init__.py
+++ wagtail/__init__.py
@@ -7,7 +7,7 @@ from wagtail.utils.version import get_semver_version, get_version
 
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (2, 11, 8, 'final', 1)
+VERSION = (2, 11, 9, 'final', 1)
 
 __version__ = get_version(VERSION)
 
