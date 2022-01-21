89b854f1235fd24ba5ba6121e0ae8cf93a003100
zzj <29055749+zjzh@users.noreply.github.com>
2022-01-21 21:01:55 +0800

    Update test_account_management.py
    Add set comprehension for the assertPanelActive

--- wagtail/admin/tests/test_account_management.py
+++ wagtail/admin/tests/test_account_management.py
@@ -182,10 +182,7 @@ class TestAuthentication(TestCase, WagtailTestUtils):
 
 class TestAccountSectionUtilsMixin:
     def assertPanelActive(self, response, name):
-        panels = set()
-        for panelset in response.context['panels_by_tab'].values():
-            for panel in panelset:
-                panels.add(panel.name)
+        panels = {panel.name for panelset in response.context['panels_by_tab'].values() for panel in panelset}
         self.assertIn(name, panels, "Panel %s not active in response" % name)
 
     def assertPanelNotActive(self, response, name):
