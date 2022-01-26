e0349e0a4c122a3d654d86bdc435b15120f5eda1
LB Johnston <mail@lb.ee>
2022-01-26 21:24:10 +1000

    remove Hallo editor & related hooks

--- wagtail/embeds/wagtail_hooks.py
+++ wagtail/embeds/wagtail_hooks.py
@@ -3,7 +3,6 @@ from django.utils.html import format_html
 from django.utils.translation import gettext as _
 
 import wagtail.admin.rich_text.editors.draftail.features as draftail_features
-from wagtail.admin.rich_text import HalloPlugin
 from wagtail.core import hooks
 from wagtail.embeds import urls
 from wagtail.embeds.rich_text import MediaEmbedHandler
@@ -34,19 +33,6 @@ def editor_js():
 def register_embed_feature(features):
     # define a handler for converting <embed embedtype="media"> tags into frontend HTML
     features.register_embed_type(MediaEmbedHandler)
-
-    # define a hallo.js plugin to use when the 'embed' feature is active
-    features.register_editor_plugin(
-        "hallo",
-        "embed",
-        HalloPlugin(
-            name="hallowagtailembeds",
-            js=[
-                "wagtailembeds/js/embed-chooser-modal.js",
-                "wagtailembeds/js/hallo-plugins/hallo-wagtailembeds.js",
-            ],
-        ),
-    )
 
     # define how to convert between editorhtml's representation of embeds and
     # the database representation
