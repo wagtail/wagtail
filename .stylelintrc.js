0ec32e0672cce890487b86d1df794ef612d423af
Steve <steven_ts@hotmail.com>
2022-02-16 11:13:00 -0700

    Add prefix to tailwind config, add tailwind to core and make it work with stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,24 @@
 module.exports = {
   extends: '@wagtail/stylelint-config-wagtail',
   rules: {
+    'no-invalid-position-at-import-rule': [
+      true,
+      {
+        ignoreAtRules: ['tailwind', 'import'],
+      },
+    ],
+    'scss/at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+        ],
+      },
+    ],
     // Would be valuable for strict BEM components but is too hard to enforce with legacy code.
     'no-descending-specificity': null,
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ['./wagtail/**/*.{html,js,jsx,ts,tsx}'],
+  prefix: 'w-',
   theme: {
     extend: {},
   },
