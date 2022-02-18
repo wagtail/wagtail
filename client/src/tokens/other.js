fc5f23099e5d6cbf69cda4986cff11fcb18df40a
Steve <steven_ts@hotmail.com>
2022-02-18 14:04:35 -0700

    Simplify border radius design token

--- client/src/tokens/other.js
+++ client/src/tokens/other.js
@@ -1,8 +1,7 @@
 const borderRadius = {
-  DEFAULT: '0.3125rem', // 5px
   none: '0',
-  xs: '0.1875rem', // 3px
-  sm: '0.3125rem', // 5px
+  sm: '0.1875rem', // 3px
+  DEFAULT: '0.3125rem', // 5px
   md: '0.625rem', // 10px
   full: '9999px',
 };
