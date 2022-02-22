503c52e5638064a4abc321f35195e9450dfbbccf
Steve <steven_ts@hotmail.com>
2022-02-22 16:57:25 -0700

    FontWeight, line height, letter spacing

--- client/src/tokens/typography.js
+++ client/src/tokens/typography.js
@@ -38,9 +38,45 @@ const fontSize = {
   30: '1.875rem',
 };
 
+const fontWeight = {
+  normal: 400,
+  medium: 500,
+  semibold: 600,
+  bold: 700,
+  extrabold: 800,
+};
+
+// These are set to ems to be relative to the font size
+const letterSpacing = {
+  tighter: '-0.05em',
+  tight: '-0.025em',
+  normal: '0em',
+  wide: '0.025em',
+  wider: '0.05em',
+  widest: '0.1em',
+};
+
+const lineHeight = {
+  none: '1',
+  tight: '1.25',
+  snug: '1.375',
+  normal: '1.5',
+  relaxed: '1.625',
+  loose: '2',
+  3: '.75rem',
+  4: '1rem',
+  5: '1.25rem',
+  6: '1.5rem',
+  7: '1.75rem',
+  8: '2rem',
+};
+
 module.exports = {
   systemUIFontStack,
   monoFontStack,
   fontFamilies,
   fontSize,
+  fontWeight,
+  letterSpacing,
+  lineHeight,
 };
