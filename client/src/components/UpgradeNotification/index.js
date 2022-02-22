298b36d4c46ece5f61286d3e0de9b42a672f7073
Tibor Leupold <tibor.leupold@torchbox.com>
2022-02-21 17:48:30 -0800

    Use version number class for upgrade notification
    This now implements the logic to link to the next minor's release notes
    when such a version difference is computed. If a patch release
    difference is computed, then those release notes are linked.

--- client/src/components/UpgradeNotification/index.js
+++ client/src/components/UpgradeNotification/index.js
@@ -1,4 +1,4 @@
-import { versionOutOfDate } from '../../utils/version';
+import { VersionNumber, VersionDeltaType } from '../../utils/version';
 
 const initUpgradeNotification = () => {
   const container = document.querySelector('[data-upgrade]');
@@ -8,14 +8,21 @@ const initUpgradeNotification = () => {
   }
 
   /*
-   *   Expected JSON payload:
-   *   {
-   *       "version" : "1.2.3",    // Version number. Can only contain numbers and decimal point.
-   *       "url" : "https://wagtail.org"  // Absolute URL to page/file containing release notes or actual package. It's up to you.
-   *   }
+   * Expected JSON payload:
+   *
+   * {
+   *     "version": "2.15.2",
+   *     "url":     "https://docs.wagtail.io/en/stable/releases/2.15.2.html",
+   *     "minorUrl": "https://docs.wagtail.io/en/stable/releases/2.15.html",
+   *     "lts": {
+   *         "version": "2.12.8",
+   *         "url": "https://docs.wagtail.io/en/stable/releases/2.12.8.html",
+   *         "minorUrl": "https://docs.wagtail.io/en/stable/releases/2.12.html"
+   *     }
+   * }
    */
-  const releasesUrl = 'https://releases.wagtail.org/latest.txt';
-  const currentVersion = container.dataset.wagtailVersion;
+  const releasesUrl = 'https://releases.wagtail.io/latest.txt';
+  const currentVersion = new VersionNumber(container.dataset.wagtailVersion);
 
   fetch(releasesUrl)
     .then((response) => {
@@ -29,16 +36,27 @@ const initUpgradeNotification = () => {
       return response.json();
     })
     .then((data) => {
-      if (
-        data &&
-        data.version &&
-        versionOutOfDate(data.version, currentVersion)
-      ) {
+      if (data && data.version) {
+        const latestVersion = new VersionNumber(data.version);
+        const versionDelta = currentVersion.howMuchBehind(latestVersion);
+
+        let releaseNotesUrl = undefined;
+        if (!versionDelta) {
+          return;
+        } else if (
+          versionDelta === VersionDeltaType.MAJOR ||
+          versionDelta === VersionDeltaType.MINOR
+        ) {
+          releaseNotesUrl = data.minorUrl;
+        } else {
+          releaseNotesUrl = data.url;
+        }
+
         container.querySelector('[data-upgrade-version]').innerText =
           data.version;
         container
           .querySelector('[data-upgrade-link]')
-          .setAttribute('href', data.url);
+          .setAttribute('href', releaseNotesUrl);
         container.style.display = '';
       }
     })
