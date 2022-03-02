832522e7ca4413530404bfe958d8453bd2c7be8c
Steve <steven_ts@hotmail.com>
2022-03-01 18:28:19 -0700

    Add typescale plugin inside its own component, comments inside tailwind config

--- client/src/tokens/typography.js
+++ client/src/tokens/typography.js
@@ -1,6 +1,7 @@
 // Note: Tailwind does not automatically escape font names.
 // If a font name contains an invalid identifier (like a space), we wrap it in quotes to escape the invalid characters.
 // Font stack optimised for built-in fonts of each major operating system, with support for emojis.
+
 const systemUIFontStack = [
   '-apple-system',
   'BlinkMacSystemFont',
@@ -56,17 +57,8 @@ const letterSpacing = {
 
 const lineHeight = {
   none: '1',
-  tight: '1.25',
-  snug: '1.375',
+  tight: '1.3',
   normal: '1.5',
-  relaxed: '1.625',
-  loose: '2',
-  3: '.75rem',
-  4: '1rem',
-  5: '1.25rem',
-  6: '1.5rem',
-  7: '1.75rem',
-  8: '2rem',
 };
 
 module.exports = {
