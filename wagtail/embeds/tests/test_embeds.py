5be5d0a866924fc307aced7488fe4bb5c19673d8
Luis Nell <luis.nell@codista.com>
2022-03-04 11:31:50 +0100

    adds new instagram embed urls to tests.

--- wagtail/embeds/tests/test_embeds.py
+++ wagtail/embeds/tests/test_embeds.py
@@ -634,6 +634,16 @@ class TestInstagramOEmbed(TestCase):
                 "https://www.instagram.com/p/CHeRxmnDSYe/?utm_source=ig_embed"
             )
         )
+        self.assertTrue(
+            finder.accept(
+                "https://www.instagram.com/tv/CZMkxGaIXk3/?utm_source=ig_embed"
+            )
+        )
+        self.assertTrue(
+            finder.accept(
+                "https://www.instagram.com/reel/CZMs3O_I22w/?utm_source=ig_embed"
+            )
+        )
         self.assertFalse(
             finder.accept("https://instagr.am/p/CHeRxmnDSYe/?utm_source=ig_embed")
         )
