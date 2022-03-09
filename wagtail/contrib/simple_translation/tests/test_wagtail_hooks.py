311afa8c2e4dde4eb5b63e11fe1b391d653cc3f0
Kalob Taulien <kalob.taulien@gmail.com>
2022-03-09 14:32:58 -0700

    Tests

--- wagtail/contrib/simple_translation/tests/test_wagtail_hooks.py
+++ wagtail/contrib/simple_translation/tests/test_wagtail_hooks.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group, Permission
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.urls import reverse
 
 from wagtail.admin import widgets as wagtailadmin_widgets
@@ -8,6 +8,7 @@ from wagtail.contrib.simple_translation.wagtail_hooks import (
     page_listing_more_buttons,
     register_submit_translation_permission,
 )
+from wagtail.core import hooks
 from wagtail.core.models import Locale, Page
 from wagtail.tests.i18n.models import TestPage
 from wagtail.tests.utils import WagtailTestUtils
@@ -101,4 +102,79 @@ class TestWagtailHooksButtons(Utils):
         assert isinstance(
             list(page_listing_more_buttons(blog_page, page_perms))[0],
             wagtailadmin_widgets.Button,
+        )
+
+
+@override_settings(
+    WAGTAILSIMPLETRANSLATION_SYNC_PAGE_TREE=True, WAGTAIL_I18N_ENABLED=True
+)
+class TestDeletingTranslatedPages(Utils):
+    def delete_hook(self, pages, action):
+        self.assertEqual(action, "delete")
+        self.assertIsInstance(pages, list)
+
+    def test_double_registered_hook(self):
+        # We should have two implementations of `construct_synced_page_tree_list`
+        # One in simple_translation.wagtail_hooks and the other will be
+        # registered as a temporary hook.
+        with hooks.register_temporarily(
+            "construct_synced_page_tree_list", self.delete_hook
+        ):
+            defined_hooks = hooks.get_hooks("construct_synced_page_tree_list")
+            self.assertEqual(len(defined_hooks), 2)
+
+    def test_construct_synced_page_tree_list_when_deleting(self):
+        with hooks.register_temporarily(
+            "construct_synced_page_tree_list", self.delete_hook
+        ):
+            for fn in hooks.get_hooks("construct_synced_page_tree_list"):
+                response = fn([self.en_homepage], "delete")
+                if response is not None:
+                    self.assertIsInstance(response, dict)
+                    self.assertEqual(len(response.items()), 1)
+
+    def test_delete_translated_pages(self):
+        # Login to the Wagtail admin with a superuser account
+        self.login()
+
+        # BlogIndex needs translated pages before child pages can be translated
+        self.fr_blog_index = self.en_blog_index.copy_for_translation(self.fr_locale)
+        # Create a copy of the en_blog_post object as a translated page
+        self.fr_blog_post = self.en_blog_post.copy_for_translation(self.fr_locale)
+
+        # 1. Delete the en_blog_post by making a POST request to /delete/
+        response = self.client.post(
+            reverse(
+                "wagtailadmin_pages:delete",
+                args=(self.en_blog_post.id,),
+            ),
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200)
+
+        # 2. Confirm fr_blog_post is deleted
+        self.assertIsNone(Page.objects.filter(pk=self.fr_blog_post.id).first())
+
+    def test_delete_confirmation_template(self):
+        """Test the context info is correct in the confirm_delete.html template."""
+        self.login()
+
+        # BlogIndex needs translated pages before child pages can be translated
+        self.fr_blog_index = self.en_blog_index.copy_for_translation(self.fr_locale)
+        # Create a copy of the en_blog_post object as a translated page
+        self.fr_blog_post = self.en_blog_post.copy_for_translation(self.fr_locale)
+        response = self.client.get(
+            reverse(
+                "wagtailadmin_pages:delete",
+                args=(self.en_blog_post.id,),
+            ),
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["translation_count"], 1)
+        self.assertEqual(response.context["translation_descendant_count"], 0)
+        self.assertEqual(response.context["combined_subpages"], 1)
+        self.assertIn(
+            "Deleting this page will also delete 1 translation of this page.",
+            response.content.decode("utf-8"),
         )
