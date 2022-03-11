efec8f16e99307221bca942e47841d158eef3ce9
Matt Westcott <matt@west.co.tt>
2022-03-11 18:16:14 +0000

    Remove special casing of hidden fields in FieldPanel.get_comparison_class
    There's no clear reason to put a hidden field in a FieldPanel (or why you would expect it to change between revisions, and not want to know about it in the comparison view) and if you do have some weird customisation that requires that behaviour, you could always subclass FieldPanel to return None from get_comparison_class for that one case.

--- wagtail/admin/panels.py
+++ wagtail/admin/panels.py
@@ -611,11 +611,6 @@ class FieldPanel(Panel):
         return [self.field_name]
 
     def get_comparison_class(self):
-        # Hide fields with hidden widget
-        widget_override = self.widget_overrides().get(self.field_name, None)
-        if widget_override and widget_override.is_hidden:
-            return
-
         try:
             field = self.db_field
 
