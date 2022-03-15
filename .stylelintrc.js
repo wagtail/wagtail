14280ad7ea1c742db96dcfe4ac34955e988bdd7b
Thibaud Colas <thibaudcolas@gmail.com>
2022-03-15 13:21:06 +0000

    Convert all UI code to CSS logical properties (#8051)
    - Rewrite all styles to use logical properties
    - Update Stylelint configuration to include logical properties checks

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -22,5 +22,13 @@ module.exports = {
     ],
     // Would be valuable for strict BEM components but is too hard to enforce with legacy code.
     'no-descending-specificity': null,
+    // Override stylelint-config-wagtail’s options to allow all float and clear values for now.
+    'declaration-property-value-allowed-list': {
+      // 'clear': ['both', 'none'],
+      // 'float': ['inline-start', 'inline-end', 'none', 'unset'],
+      'text-align': ['start', 'end', 'center'],
+    },
+    // Disable declaration-strict-value until we are in a position to enforce it.
+    'scale-unlimited/declaration-strict-value': null,
   },
 };
