07c31e52bd445053e84290845da2ff66d1d86ebe
Karl Hobley <karl@torchbox.com>
2022-03-17 14:38:35 +0000

    Rename WagtailCoreAppConfig to WagtailAppConfig

--- wagtail/__init__.py
+++ wagtail/__init__.py
@@ -20,7 +20,7 @@ if django.VERSION >= (3, 2):
     # The declaration is only needed for older Django versions
     pass
 else:
-    default_app_config = "wagtail.apps.WagtailCoreAppConfig"
+    default_app_config = "wagtail.apps.WagtailAppConfig"
 
 
 def setup():
