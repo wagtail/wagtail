4eef36724176362632cbdc871ee77484012fbd50
Matt Westcott <matt@west.co.tt>
2022-03-22 21:36:08 +0000

    Change BoundPanel.render method to render_html

--- wagtail/contrib/forms/tests/test_views.py
+++ wagtail/contrib/forms/tests/test_views.py
@@ -62,7 +62,7 @@ class TestFormResponsesPanel(TestCase):
             },
         )
 
-        result = self.panel.render()
+        result = self.panel.render_html()
 
         url = reverse("wagtailforms:list_submissions", args=(self.form_page.id,))
         link = '<a href="{}">1</a>'.format(url)
@@ -71,7 +71,7 @@ class TestFormResponsesPanel(TestCase):
 
     def test_render_without_submissions(self):
         """The panel should not be shown if the number of submission is zero."""
-        result = self.panel.render()
+        result = self.panel.render_html()
 
         self.assertEqual("", result)
 
@@ -114,7 +114,7 @@ class TestFormResponsesPanelWithCustomSubmissionClass(TestCase, WagtailTestUtils
         new_form_submission.submit_time = "2017-08-29T12:00:00.000Z"
         new_form_submission.save()
 
-        result = self.panel.render()
+        result = self.panel.render_html()
 
         url = reverse("wagtailforms:list_submissions", args=(self.form_page.id,))
         link = '<a href="{}">1</a>'.format(url)
@@ -123,7 +123,7 @@ class TestFormResponsesPanelWithCustomSubmissionClass(TestCase, WagtailTestUtils
 
     def test_render_without_submissions(self):
         """The panel should not be shown if the number of submission is zero."""
-        result = self.panel.render()
+        result = self.panel.render_html()
 
         self.assertEqual("", result)
 
