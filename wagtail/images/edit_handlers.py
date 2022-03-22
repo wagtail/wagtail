7cc1713c4841e11a104e7f29a7566f2b3990792b
Matt Westcott <matt@west.co.tt>
2022-03-22 12:40:17 +0000

    Formally deprecate StreamFieldPanel, BaseChooserPanel etc

--- wagtail/images/edit_handlers.py
+++ wagtail/images/edit_handlers.py
@@ -1,11 +1,20 @@
+from warnings import warn
+
 from django.template.loader import render_to_string
 
 from wagtail.admin.compare import ForeignObjectComparison
-from wagtail.admin.edit_handlers import BaseChooserPanel
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.utils.deprecation import RemovedInWagtail219Warning
 
 
-class ImageChooserPanel(BaseChooserPanel):
-    pass
+class ImageChooserPanel(FieldPanel):
+    def __init__(self, *args, **kwargs):
+        warn(
+            "wagtail.images.edit_handlers.ImageChooserPanel is obsolete and should be replaced by wagtail.admin.edit_handlers.FieldPanel",
+            category=RemovedInWagtail219Warning,
+            stacklevel=2,
+        )
+        super().__init__(*args, **kwargs)
 
 
 class ImageFieldComparison(ForeignObjectComparison):
--- wagtail/snippets/edit_handlers.py
+++ wagtail/snippets/edit_handlers.py
@@ -1,5 +1,14 @@
-from wagtail.admin.edit_handlers import BaseChooserPanel
+from warnings import warn
+
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.utils.deprecation import RemovedInWagtail219Warning
 
 
-class SnippetChooserPanel(BaseChooserPanel):
-    pass
+class SnippetChooserPanel(FieldPanel):
+    def __init__(self, *args, **kwargs):
+        warn(
+            "wagtail.snippets.edit_handlers.SnippetChooserPanel is obsolete and should be replaced by wagtail.admin.edit_handlers.FieldPanel",
+            category=RemovedInWagtail219Warning,
+            stacklevel=2,
+        )
+        super().__init__(*args, **kwargs)
