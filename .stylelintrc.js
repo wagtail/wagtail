3f2fe92cf44571405ce168d6e89827d830c49594
Steve <steven_ts@hotmail.com>
2022-03-23 17:36:26 -0600

    Header redesign start

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -17,7 +17,7 @@ module.exports = {
     'no-invalid-position-at-import-rule': [
       true,
       {
-        ignoreAtRules: ['tailwind', 'use'],
+        ignoreAtRules: ['tailwind', 'use', 'layer'],
       },
     ],
     // Would be valuable for strict BEM components but is too hard to enforce with legacy code.
