316bbf9508a851a469685b02518271f1f01776d6
LB Johnston <mail@lb.ee>
2022-03-30 07:34:13 +1000

    format migration files - black

--- wagtail/test/testapp/migrations/0063_jsonblockcountsstreammodel_and_more.py
+++ wagtail/test/testapp/migrations/0063_jsonblockcountsstreammodel_and_more.py
@@ -9,29 +9,83 @@ import wagtail.images.blocks
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tests', '0062_alter_addedstreamfieldwithemptylistdefaultpage_body_and_more'),
+        ("tests", "0062_alter_addedstreamfieldwithemptylistdefaultpage_body_and_more"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='JSONBlockCountsStreamModel',
+            name="JSONBlockCountsStreamModel",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('body', wagtail.fields.StreamField([('text', wagtail.blocks.CharBlock()), ('rich_text', wagtail.blocks.RichTextBlock()), ('image', wagtail.images.blocks.ImageChooserBlock())], use_json_field=True)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "body",
+                    wagtail.fields.StreamField(
+                        [
+                            ("text", wagtail.blocks.CharBlock()),
+                            ("rich_text", wagtail.blocks.RichTextBlock()),
+                            ("image", wagtail.images.blocks.ImageChooserBlock()),
+                        ],
+                        use_json_field=True,
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='JSONMinMaxCountStreamModel',
+            name="JSONMinMaxCountStreamModel",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('body', wagtail.fields.StreamField([('text', wagtail.blocks.CharBlock()), ('rich_text', wagtail.blocks.RichTextBlock()), ('image', wagtail.images.blocks.ImageChooserBlock())], use_json_field=True)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "body",
+                    wagtail.fields.StreamField(
+                        [
+                            ("text", wagtail.blocks.CharBlock()),
+                            ("rich_text", wagtail.blocks.RichTextBlock()),
+                            ("image", wagtail.images.blocks.ImageChooserBlock()),
+                        ],
+                        use_json_field=True,
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='JSONStreamModel',
+            name="JSONStreamModel",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('body', wagtail.fields.StreamField([('text', wagtail.blocks.CharBlock()), ('rich_text', wagtail.blocks.RichTextBlock()), ('image', wagtail.images.blocks.ImageChooserBlock())], use_json_field=True)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "body",
+                    wagtail.fields.StreamField(
+                        [
+                            ("text", wagtail.blocks.CharBlock()),
+                            ("rich_text", wagtail.blocks.RichTextBlock()),
+                            ("image", wagtail.images.blocks.ImageChooserBlock()),
+                        ],
+                        use_json_field=True,
+                    ),
+                ),
             ],
         ),
     ]
