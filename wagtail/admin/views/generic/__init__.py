971f0b14ad789c67c25dc4497eee6c1ea8ac28b8
Sage Abdullah <sage.abdullah@torchbox.com>
2022-04-14 14:31:37 +0700

    Create generic BeforeAfterHookMixin

--- wagtail/admin/views/generic/__init__.py
+++ wagtail/admin/views/generic/__init__.py
@@ -1,3 +1,3 @@
-from .hooks import HookResponseMixin  # noqa
+from .hooks import BeforeAfterHookMixin, HookResponseMixin  # noqa
 from .models import CreateView, DeleteView, EditView, IndexView  # noqa
 from .permissions import PermissionCheckedMixin  # noqa
--- wagtail/admin/views/generic/hooks.py
+++ wagtail/admin/views/generic/hooks.py
@@ -16,3 +16,30 @@ class HookResponseMixin:
             result = fn(*args, **kwargs)
             if hasattr(result, "status_code"):
                 return result
+
+
+class BeforeAfterHookMixin(HookResponseMixin):
+    before_hook_name = None
+    after_hook_name = None
+
+    def run_before_hook(self):
+        return self.run_hook(self.before_hook_name)
+
+    def run_after_hook(self):
+        return self.run_hook(self.after_hook_name)
+
+    def dispatch(self, *args, **kwargs):
+        hooks_result = self.run_before_hook()
+        if hooks_result is not None:
+            return hooks_result
+
+        return super().dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        hooks_result = self.run_after_hook()
+        if hooks_result is not None:
+            return hooks_result
+
+        return response
