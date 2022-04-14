8d11821df7c5332c70271c123cf0b4206ea4c94c
Sage Abdullah <sage.abdullah@torchbox.com>
2022-04-14 18:51:21 +0700

    Create LocaleMixin

--- wagtail/admin/views/generic/__init__.py
+++ wagtail/admin/views/generic/__init__.py
@@ -1,3 +1,4 @@
 from .hooks import BeforeAfterHookMixin, HookResponseMixin  # noqa
+from .mixins import LocaleMixin  # noqa
 from .models import CreateView, DeleteView, EditView, IndexView  # noqa
 from .permissions import PermissionCheckedMixin  # noqa
---
+++ wagtail/admin/views/generic/mixins.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+
+from wagtail.models import Locale, TranslatableMixin
+
+
+class LocaleMixin:
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+        self.locale = self.get_locale()
+
+    def get_locale(self):
+        i18n_enabled = getattr(settings, "WAGTAIL_I18N_ENABLED", False)
+        if hasattr(self, "model"):
+            i18n_enabled = i18n_enabled and issubclass(self.model, TranslatableMixin)
+
+        if not i18n_enabled:
+            return None
+
+        selected_locale = self.request.GET.get("locale")
+        if selected_locale:
+            return get_object_or_404(Locale, language_code=selected_locale)
+        return Locale.get_default()
