44b1ee6af74d04fe2fef8b00ed43b6bb3d957c01
sag᠎e <sage.abdullah@torchbox.com>
2022-04-16 16:27:18 +0700

    Refactor Snippets index view to extend from TemplateView (#8352)

--- wagtail/snippets/views/snippets.py
+++ wagtail/snippets/views/snippets.py
@@ -68,24 +68,30 @@ def get_snippet_edit_handler(model):
 # == Views ==
 
 
-def index(request):
-    snippet_model_opts = [
-        model._meta
-        for model in get_snippet_models()
-        if user_can_edit_snippet_type(request.user, model)
-    ]
-    if snippet_model_opts:
-        return TemplateResponse(
-            request,
-            "wagtailsnippets/snippets/index.html",
-            {
-                "snippet_model_opts": sorted(
-                    snippet_model_opts, key=lambda x: x.verbose_name.lower()
-                )
-            },
+class Index(TemplateView):
+    template_name = "wagtailsnippets/snippets/index.html"
+
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+        self.snippet_types = self._get_snippet_types()
+
+    def _get_snippet_types(self):
+        return [
+            model._meta
+            for model in get_snippet_models()
+            if user_can_edit_snippet_type(self.request.user, model)
+        ]
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.snippet_types:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        snippet_model_opts = sorted(
+            self.snippet_types, key=lambda x: x.verbose_name.lower()
         )
-    else:
-        raise PermissionDenied
+        return super().get_context_data(snippet_model_opts=snippet_model_opts, **kwargs)
 
 
 class ListView(TemplateView):
