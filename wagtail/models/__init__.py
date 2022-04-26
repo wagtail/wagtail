2c7af1303a11e08205a46e3f71aff6c40969b978
Sage Abdullah <sage.abdullah@torchbox.com>
2022-04-26 20:03:18 +0700

    Add nullable content_type ForeignKey and content_object GenericForeignKey in Revision

--- wagtail/models/__init__.py
+++ wagtail/models/__init__.py
@@ -18,6 +18,7 @@ from urllib.parse import urlparse
 from django import forms
 from django.conf import settings
 from django.contrib.auth.models import Group
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.core import checks
 from django.core.cache import cache
@@ -2150,10 +2151,12 @@ class SubmittedRevisionsManager(models.Manager):
 
 
 class Revision(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)
     object_id = models.CharField(
         max_length=255,
         verbose_name=_("object id"),
     )
+    content_object = GenericForeignKey("content_type", "object_id")
     submitted_for_moderation = models.BooleanField(
         verbose_name=_("submitted for moderation"), default=False, db_index=True
     )
