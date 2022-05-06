f7110f009acd167ee3f4eb0b7c23aa64ab5dac45
Matt Westcott <matt@west.co.tt>
2022-05-06 21:15:51 +0100

    Merge WidgetWithScript implementation into BaseChooser
    This is a first step towards untangling the render / render_html inheritance

--- wagtail/admin/widgets/chooser.py
+++ wagtail/admin/widgets/chooser.py
@@ -6,6 +6,7 @@ from django.core.exceptions import ImproperlyConfigured
 from django.forms import widgets
 from django.template.loader import render_to_string
 from django.urls import reverse
+from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 
 from wagtail.admin.admin_url_finder import AdminURLFinder
@@ -85,7 +86,7 @@ class AdminChooser(WidgetWithScript, widgets.Input):
             return result
 
 
-class BaseChooser(WidgetWithScript, widgets.Input):
+class BaseChooser(widgets.Input):
     choose_one_text = _("Choose an item")
     choose_another_text = _("Choose another item")
     clear_choice_text = _("Clear choice")
@@ -121,6 +122,36 @@ class BaseChooser(WidgetWithScript, widgets.Input):
             return None
         else:
             return result
+
+    def render_html(self, name, value, attrs):
+        """Render the HTML (non-JS) portion of the field markup"""
+        return super().render(name, value, attrs)
+
+    def get_value_data(self, value):
+        # Perform any necessary preprocessing on the value passed to render() before it is passed
+        # on to render_html / render_js_init. This is a good place to perform database lookups
+        # that are needed by both render_html and render_js_init. Return value is arbitrary
+        # (we only care that render_html / render_js_init can accept it), but will typically be
+        # a dict of data needed for rendering: id, title etc.
+        return value
+
+    def render(self, name, value, attrs=None, renderer=None):
+        # no point trying to come up with sensible semantics for when 'id' is missing from attrs,
+        # so let's make sure it fails early in the process
+        try:
+            id_ = attrs["id"]
+        except (KeyError, TypeError):
+            raise TypeError("BaseChooser cannot be rendered without an 'id' attribute")
+
+        value_data = self.get_value_data(value)
+        widget_html = self.render_html(name, value_data, attrs)
+
+        js = self.render_js_init(id_, name, value_data)
+        out = "{0}<script>{1}</script>".format(widget_html, js)
+        return mark_safe(out)
+
+    def render_js_init(self, id_, name, value):
+        return ""
 
 
 class AdminPageChooser(BaseChooser):
