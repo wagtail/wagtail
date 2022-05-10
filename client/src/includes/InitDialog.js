bc28d35d9d9e9b08882c3bcf4c6a1f2eeb9c5c7e
Steve <steven_ts@hotmail.com>
2022-05-10 11:48:24 -0600

    Update styleguide with dialogs

--- client/src/includes/InitDialog.js
+++ client/src/includes/InitDialog.js
@@ -3,13 +3,12 @@ import A11yDialog from 'a11y-dialog';
 class Dialog {
   constructor(node) {
     this.dialog = node;
-    // es-lin
     new A11yDialog(this.dialog);
   }
 }
 
 export const initDialog = (
-  dialog = document.querySelectorAll('[data-dialog]'),
+  dialogs = document.querySelectorAll('[data-dialog]'),
 ) => {
-  dialog.forEach((tabSet) => new Dialog(tabSet));
+  dialogs.forEach((dialog) => new Dialog(dialog));
 };
