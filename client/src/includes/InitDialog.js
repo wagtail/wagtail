3198f5a123951a58ab3add45d651cdbf22915467
Steve <steven_ts@hotmail.com>
2022-05-11 00:09:43 -0600

    Add template for dialog component

--- client/src/includes/InitDialog.js
+++ client/src/includes/InitDialog.js
@@ -1,14 +1,21 @@
 import A11yDialog from 'a11y-dialog';
 
-class Dialog {
-  constructor(node) {
-    this.dialog = node;
-    new A11yDialog(this.dialog);
-  }
-}
-
 export const initDialog = (
   dialogs = document.querySelectorAll('[data-dialog]'),
 ) => {
-  dialogs.forEach((dialog) => new Dialog(dialog));
+  dialogs.forEach((template) => {
+    const html = document.documentElement;
+    const templateContent = template.content.firstElementChild.cloneNode(true);
+    document.body.appendChild(templateContent);
+    const dialog = new A11yDialog(templateContent);
+
+    // Prevent scrolling when dialog is open
+    dialog
+      .on('show', () => {
+        html.style.overflowY = 'hidden';
+      })
+      .on('hide', () => {
+        html.style.overflowY = '';
+      });
+  });
 };
