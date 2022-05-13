b3844cee46d60f664aa4bfd6e363e7f61bd09356
Matt Westcott <matt@west.co.tt>
2022-05-13 12:13:58 +0100

    Introduce a factory class for generating the onload handlers

--- client/src/entrypoints/snippets/snippet-chooser-modal.js
+++ client/src/entrypoints/snippets/snippet-chooser-modal.js
@@ -1,36 +1,9 @@
-import $ from 'jquery';
-import { SearchController } from '../../includes/chooserModal';
+import { ChooserModalOnloadHandlerFactory } from '../../includes/chooserModal';
 
-window.SNIPPET_CHOOSER_MODAL_ONLOAD_HANDLERS = {
-  choose: (modal) => {
-    let searchController;
-
-    function ajaxifyLinks(context) {
-      $('a.snippet-choice', modal.body).on('click', (event) => {
-        modal.loadUrl(event.currentTarget.href);
-        return false;
-      });
-
-      $('.pagination a', context).on('click', (event) => {
-        searchController.fetchResults(event.currentTarget.href);
-        return false;
-      });
-    }
-
-    searchController = new SearchController({
-      form: $('form.snippet-search', modal.body),
-      resultsContainerSelector: '#search-results',
-      onLoadResults: (context) => {
-        ajaxifyLinks(context);
-      },
-    });
-    searchController.attachSearchInput('#id_q');
-    searchController.attachSearchFilter('#snippet-chooser-locale');
-
-    ajaxifyLinks(modal.body);
-  },
-  chosen: (modal, jsonData) => {
-    modal.respond('snippetChosen', jsonData.result);
-    modal.close();
-  },
-};
+window.SNIPPET_CHOOSER_MODAL_ONLOAD_HANDLERS =
+  new ChooserModalOnloadHandlerFactory({
+    chosenLinkSelector: 'a.snippet-choice',
+    searchFormSelector: 'form.snippet-search',
+    searchFilterSelectors: ['#snippet-chooser-locale'],
+    chosenResponseName: 'snippetChosen',
+  }).getOnLoadHandlers();
