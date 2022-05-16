b4a9921c8a32e6d8b32cee5145312e34fd4011df
Steve <steven_ts@hotmail.com>
2022-05-16 16:45:57 -0600

    start dialog change privacy

---
+++ client/src/entrypoints/admin/dialog-workflow.js
@@ -0,0 +1,115 @@
+/* A framework for modal popups that are loaded via AJAX, allowing navigation to other
+subpages to happen within the lightbox, and returning a response to the calling page,
+possibly after several navigation steps
+*/
+
+import $ from 'jquery';
+
+import { gettext } from '../../utils/gettext';
+
+/* eslint-disable */
+function DialogWorkflow(dialog, opts) {
+  /* options passed in 'opts':
+    'url' (required): initial
+    'responses' (optional): dict of callbacks to be called when the modal content
+      calls modal.respond(callbackName, params)
+    'onload' (optional): dict of callbacks to be called when loading a step of the workflow.
+      The 'step' field in the response identifies the callback to call, passing it the
+      modal object and response data as arguments
+  */
+
+  const self = {};
+  const responseCallbacks = opts.responses || {};
+  const errorCallback = opts.onError || function () {};
+
+  // disable the trigger element so it cannot be clicked twice while modal is loading
+  self.triggerElement = document.activeElement;
+  self.triggerElement.setAttribute('disabled', true);
+
+  // set default contents of container
+  // const iconClose =
+  //   '<svg class="icon icon-cross" aria-hidden="true"><use href="#icon-cross"></use></svg>';
+  // const container = $(
+  //   '<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">\n  <div class="modal-dialog">\n    <div class="modal-content">\n      <button type="button" class="button close button--icon text-replace" data-dismiss="modal">' +
+  //     iconClose +
+  //     gettext('Close') +
+  //     '</button>\n      <div class="modal-body"></div>\n    </div><!-- /.modal-content -->\n  </div><!-- /.modal-dialog -->\n</div>',
+  // );
+
+  // add container to body and hide it, so content can be added to it before display
+  // $('body').append(container);
+  // container.modal('hide');
+
+  // add listener - once modal is about to be hidden, re-enable the trigger
+  // container.on('hide.bs.modal', () => {
+  //   self.triggerElement.removeAttribute('disabled');
+  // });
+
+  // // add listener - once modal is fully hidden (closed & css transitions end) - re-focus on trigger and remove from DOM
+  // container.on('hidden.bs.modal', function () {
+  //   self.triggerElement.focus();
+  //   container.remove();
+  // });
+
+  self.body = dialog.find('[data-dialog-content]');
+
+  self.loadUrl = function (url, urlParams) {
+    $.get(url, urlParams, self.loadResponseText, 'text').fail(errorCallback);
+  };
+
+  self.postForm = function (url, formData) {
+    $.post(url, formData, self.loadResponseText, 'text').fail(errorCallback);
+  };
+
+  self.ajaxifyForm = function (formSelector) {
+    $(formSelector).each(function () {
+      const action = this.action;
+      if (this.method.toLowerCase() === 'get') {
+        $(this).on('submit', function () {
+          self.loadUrl(action, $(this).serialize());
+          return false;
+        });
+      } else {
+        $(this).on('submit', function () {
+          self.postForm(action, $(this).serialize());
+          return false;
+        });
+      }
+    });
+  };
+
+  self.loadResponseText = function (responseText) {
+    const response = JSON.parse(responseText);
+    self.loadBody(response);
+  };
+
+  self.loadBody = function (response) {
+    if (response.html) {
+      // if response contains an 'html' item, replace modal body with it
+      self.body.html(response.html);
+      // container.modal('show');
+    }
+
+    /* If response contains a 'step' identifier, and that identifier is found in
+    the onload dict, call that onload handler */
+    if (opts.onload && response.step && response.step in opts.onload) {
+      opts.onload[response.step](self, response);
+    }
+  };
+
+  self.respond = function (responseType) {
+    if (responseType in responseCallbacks) {
+      const args = Array.prototype.slice.call(arguments, 1);
+      responseCallbacks[responseType].apply(self, args);
+    }
+  };
+
+  self.close = function () {
+    container.modal('hide');
+  };
+
+  self.loadUrl(opts.url, opts.urlParams);
+
+  return self;
+}
+window.DialogWorkflow = DialogWorkflow;
--- client/src/includes/dialog.js
+++ client/src/includes/dialog.js
@@ -5,17 +5,111 @@ export const dialog = (
 ) => {
   dialogs.forEach((template) => {
     const html = document.documentElement;
-    const templateContent = template.content.firstElementChild.cloneNode(true);
+    console.log(template);
+
+    const templateContent = template.content.firstElementChild;
+    let dataUrl;
+
+    // Check if there is a url to ajaxify dialog content
+    if (templateContent.hasAttribute('data-url')) {
+      dataUrl = templateContent.getAttribute('data-url');
+    }
+
     document.body.appendChild(templateContent);
     const dialogTemplate = new A11yDialog(templateContent);
 
     // Prevent scrolling when dialog is open
     dialogTemplate
       .on('show', () => {
+        if (dataUrl) {
+          console.log('gary');
+          window.DialogWorkflow(templateContent, {
+            url: dataUrl,
+            onload: {
+              set_privacy(modal) {
+                // eslint-disable-next-line func-names
+                $('form', modal.body).on('submit', function () {
+                  modal.postForm(this.action, $(this).serialize());
+                  return false;
+                });
+
+                console.log(modal);
+
+                const restrictionTypePasswordField = $(
+                  "input[name='restriction_type'][value='password']",
+                  modal.body,
+                );
+                const restrictionTypeGroupsField = $(
+                  "input[name='restriction_type'][value='groups']",
+                  modal.body,
+                );
+                const passwordField = $('.password-field', modal.body);
+                const groupsFields = $('#groups-fields', modal.body);
+
+                function refreshFormFields() {
+                  if (restrictionTypePasswordField.is(':checked')) {
+                    passwordField.show();
+                    groupsFields.hide();
+                  } else if (restrictionTypeGroupsField.is(':checked')) {
+                    passwordField.hide();
+                    groupsFields.show();
+                  } else {
+                    passwordField.hide();
+                    groupsFields.hide();
+                  }
+                }
+
+                refreshFormFields();
+
+                $("input[name='restriction_type']", modal.body).on(
+                  'change',
+                  refreshFormFields,
+                );
+              },
+              set_privacy_done(modal, jsonData) {
+                modal.respond('setPermission', jsonData.is_public);
+                modal.close();
+              },
+            },
+            responses: {
+              setPermission(isPublic) {
+                if (isPublic) {
+                  // Swap the status sidebar text and icon
+                  $('[data-privacy-sidebar-public]').removeClass('w-hidden');
+                  $('[data-privacy-sidebar-private]').addClass('w-hidden');
+
+                  // Swap other privacy indicators in settings and the header live button
+                  $('.privacy-indicator')
+                    .removeClass('private')
+                    .addClass('public');
+                  $('.privacy-indicator-icon use').attr('href', '#icon-view');
+                } else {
+                  // Swap the status sidebar text and icon
+                  $('[data-privacy-sidebar-public]').addClass('w-hidden');
+                  $('[data-privacy-sidebar-private]').removeClass('w-hidden');
+
+                  // Swap other privacy indicators in settings and the headers live button icon
+                  $('.privacy-indicator')
+                    .removeClass('public')
+                    .addClass('private');
+                  $('.privacy-indicator-icon use').attr(
+                    'href',
+                    '#icon-no-view',
+                  );
+                }
+              },
+            },
+          });
+        }
+
+        // Lock body scroll
         html.style.overflowY = 'hidden';
       })
       .on('hide', () => {
         html.style.overflowY = '';
+      })
+      .on('create', () => {
+        console.log('hey i am created');
       });
   });
 };
