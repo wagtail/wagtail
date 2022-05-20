wagtail/wagtail

commit 6343a9558cd33625d7d0338f1ff703ffd976e027

Implement new reusable dialog component with a11y-dialog (#8541)
Co-authored-by: Thibaud Colas <thibaudcolas@gmail.com>

client/src/includes/dialog.js
@@ -1,0 +1,21 @@
+import A11yDialog from 'a11y-dialog';
+
+export const dialog = (
+  dialogs = document.querySelectorAll('[data-dialog]'),
+) => {
+  dialogs.forEach((template) => {
+    const html = document.documentElement;
+    const templateContent = template.content.firstElementChild.cloneNode(true);
+    document.body.appendChild(templateContent);
+    const dialogTemplate = new A11yDialog(templateContent);
+
+    // Prevent scrolling when dialog is open
+    dialogTemplate
+      .on('show', () => {
+        html.style.overflowY = 'hidden';
+      })
+      .on('hide', () => {
+        html.style.overflowY = '';
+      });
+  });
+};

.circleci/config.yml
@@ -23,7 +23,7 @@ jobs:
       - run: pipenv run isort --check-only --diff .
       - run: pipenv run black --target-version py37 --check --diff .
       - run: git ls-files '*.html' | xargs pipenv run djhtml --check
-      - run: pipenv run curlylint --parse-only wagtail
+      - run: pipenv run curlylint --exclude '(dialog.html|end_dialog.html)' --parse-only wagtail
       - run: pipenv run doc8 docs
       - run: DATABASE_NAME=wagtail.db pipenv run python -u runtests.py
 

Makefile
@@ -21,7 +21,7 @@ lint-server:
 	black --target-version py37 --check --diff .
 	flake8
 	isort --check-only --diff .
-	curlylint --parse-only wagtail
+	curlylint --exclude '(dialog.html|end_dialog.html)' --parse-only wagtail
 	git ls-files '*.html' | xargs djhtml --check
 
 lint-client:

client/scss/components/_dialog.scss
@@ -1,0 +1,144 @@
+.w-dialog {
+  position: fixed;
+  display: flex;
+  inset-inline-start: 0;
+  inset-inline-end: 0;
+  bottom: 0;
+  top: 0;
+  z-index: theme('zIndex.dialog');
+  padding: theme('spacing.4');
+
+  &[aria-hidden='true'] {
+    display: none;
+  }
+
+  &__overlay {
+    position: fixed;
+    inset-inline-start: 0;
+    inset-inline-end: 0;
+    bottom: 0;
+    top: 0;
+    opacity: theme('opacity.40');
+    background: theme('colors.black.DEFAULT');
+  }
+
+  &__box {
+    width: 100%;
+    position: relative;
+    margin: auto;
+    overflow: hidden;
+    max-width: theme('maxWidth.2xl');
+    z-index: theme('zIndex.dialog');
+    background: theme('colors.white.DEFAULT');
+    box-shadow: theme('boxShadow.DEFAULT');
+    border-radius: theme('borderRadius.md');
+    animation: theme('animation.fade-in');
+
+    @include media-breakpoint-up(sm) {
+      width: 600px;
+    }
+  }
+
+  &__close-button {
+    position: absolute;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    background: transparent;
+    padding: 0;
+    top: theme('spacing.2');
+    inset-inline-end: theme('spacing.2');
+    width: theme('spacing.12');
+    height: theme('spacing.12');
+  }
+
+  &__close-icon {
+    width: theme('spacing.4');
+    height: theme('spacing.4');
+    color: theme('colors.grey.600');
+  }
+
+  &__content {
+    // Using apply for scrollbars to avoid having to overwrite scrollbar with excess css
+    overflow: auto;
+    padding: theme('spacing.8');
+    max-height: calc(100vh - 180px);
+
+    @include media-breakpoint-up(sm) {
+      padding: theme('spacing.12');
+    }
+
+    @include media-breakpoint-up(md) {
+      padding-inline-start: theme('spacing.20');
+      padding-inline-end: theme('spacing.20');
+    }
+  }
+
+  &__icon {
+    position: absolute;
+    display: none;
+    width: theme('spacing.5');
+    height: theme('spacing.5');
+    color: theme('colors.primary.DEFAULT');
+    top: theme('spacing.[0.5]');
+    transform: translateY(theme('spacing.2'));
+    inset-inline-start: calc(0 - theme('spacing.10'));
+
+    @include media-breakpoint-up(md) {
+      display: block;
+    }
+  }
+
+  &__title {
+    position: relative;
+    margin-top: 0;
+    margin-bottom: theme('spacing.1');
+  }
+
+  &__subtitle {
+    margin-bottom: theme('spacing.4');
+  }
+
+  &__message {
+    display: flex;
+    align-items: center;
+    padding: theme('spacing.5');
+
+    &--info {
+      background: theme('colors.info.50');
+      color: theme('colors.info.100');
+    }
+
+    &--warning {
+      background: theme('colors.warning.50');
+      color: theme('colors.primary.DEFAULT');
+    }
+
+    &--critical {
+      background: theme('colors.critical.50');
+      color: theme('colors.critical.200');
+    }
+
+    &--success {
+      background: theme('colors.positive.50');
+      color: theme('colors.positive.100');
+    }
+  }
+
+  &__message-icon {
+    width: theme('spacing.5');
+    height: theme('spacing.5');
+    flex-shrink: 0;
+  }
+
+  &__message-header {
+    margin-inline-start: theme('spacing[2.5]');
+    padding-inline-end: theme('spacing.8');
+    color: theme('colors.grey.600');
+    font-size: theme('fontSize.14');
+  }
+
+  &__message-description {
+    margin-bottom: 0;
+  }
+}

client/scss/core.scss
@@ -106,6 +106,7 @@ These are classes for components.
 // Legacy
 @import 'components/icons';
 @import 'components/tabs';
+@import 'components/dialog';
 @import 'components/dropdown';
 @import 'components/dropdown.legacy';
 @import 'components/help-block';

client/src/components/Sidebar/Sidebar.scss
@@ -36,7 +36,7 @@
 
 .sidebar,
 .sidebar-loading {
-  @apply w-fixed w-flex w-flex-col w-h-full w-bg-primary w-z-[300] w-transition-sidebar;
+  @apply w-fixed w-flex w-flex-col w-h-full w-bg-primary w-z-sidebar w-transition-sidebar;
   width: $menu-width;
   inset-inline-start: 0;
 
@@ -86,9 +86,9 @@
 
 // This is a separate component as it needs to display in the header
 .sidebar-nav-toggle {
+  @apply w-z-sidebar-toggle;
   @include sidebar-toggle;
   display: none; // Nav toggle is for mobile only
-  z-index: 305;
 
   &--mobile {
     @apply w-bg-primary w-top-0 w-left-0 w-h-[50px] w-w-[50px] w-rounded-none hover:w-bg-primary-200;

client/src/entrypoints/admin/wagtailadmin.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Icon, Portal, initUpgradeNotification, initSkipLink } from '../..';
 import { initModernDropdown, initTooltips } from '../../includes/initTooltips';
 import { initTabs } from '../../includes/tabs';
+import { dialog } from '../../includes/dialog';
 
 if (process.env.NODE_ENV === 'development') {
   // Run react-axe in development only, so it does not affect performance
@@ -27,4 +28,5 @@ document.addEventListener('DOMContentLoaded', () => {
   initModernDropdown();
   initTabs();
   initSkipLink();
+  dialog();
 });

client/src/tokens/colors.js
@@ -159,7 +159,7 @@ const colors = {
   },
   warning: {
     100: {
-      hex: '#FFBF00',
+      hex: '#FAA500',
       bgUtility: 'w-bg-warning-100',
       textUtility: 'w-text-warning-100',
       usage: 'Background and icons for potentially dangerous states',

commit 4a4873763f29d36cd4f51e1b40c7acaa0ba92ca9

lint fix for dialog.js
I'm guessing this was caused by #8541 and #8543 being merged in close succession...

client/src/includes/dialog.js
@@ -1,5 +1,6 @@
 import A11yDialog from 'a11y-dialog';
 
+// eslint-disable-next-line import/prefer-default-export
 export const dialog = (
   dialogs = document.querySelectorAll('[data-dialog]'),
 ) => {