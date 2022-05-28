fb382a946eff82429a5511d4e9f670d033b5b47c
PaarthAgarwal <paarth.agarwal.mec20@itbhu.ac.in>
2022-05-28 13:16:24 +0530

    fix tests

--- client/src/includes/breadcrumbs.test.js
+++ client/src/includes/breadcrumbs.test.js
@@ -1,8 +1,8 @@
 import initCollapsibleBreadcrumbs from './breadcrumbs';
 
 describe('initCollapsibleBreadcrumbs', () => {
+  jest.spyOn(document, 'addEventListener').mockImplementation(() => {});
   it('should do nothing if there is no breadcrumbs container', () => {
-    jest.spyOn(document, 'addEventListener').mockImplementation(() => {});
     // Set up our document body
     document.body.innerHTML =
       '<div>' +
@@ -23,13 +23,11 @@ describe('initCollapsibleBreadcrumbs', () => {
       '  <button id="button" data-toggle-breadcrumbs />' +
       '  <span id="username" data-breadcrumb-item hidden/>' +
       '</div>' +
-      '</div>' +
-      '<script>' +
-      'document.getElementById("button").click();' +
-      '</script>';
+      '</div>';
+    initCollapsibleBreadcrumbs();
     document.getElementById('button').click();
     expect(document.getElementById('username').hidden).toBe(false);
   });
 
-  it('should should collapse the breadcrumbs when clicked, if expanded', () => {});
+  it('should collapse the breadcrumbs when clicked, if expanded', () => {});
 });
