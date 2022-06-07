dce74375d71f594197684f9c0c99c6a164c48881
PaarthAgarwal <paarth.agarwal.mec20@itbhu.ac.in>
2022-06-07 21:37:58 +0530

    added new breadcrumbs to pattern library (storybook)
    - resolves #8631

---
+++ wagtail/admin/templates/wagtailadmin/shared/breadcrumb-next.stories.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Pattern, generateDocs } from 'storybook-django/src/react';
+
+import initCollapsibleBreadcrumbs from '../../../../../client/src/includes/breadcrumbs';
+import template from './breadcrumb-next.html';
+
+const { docs, argTypes } = generateDocs(template);
+
+document.addEventListener('DOMContentLoaded', () => {
+  initCollapsibleBreadcrumbs();
+});
+
+export default {
+  parameters: { docs },
+  argTypes: { ...argTypes },
+};
+
+const Template = (args) => (
+  <div data-slim-header>
+    <Pattern filename={__filename} context={args} />
+  </div>
+);
+
+export const Base = Template.bind({});
+
+Base.args = {
+  pages: [
+    {
+      is_root: true,
+      id: 2,
+      get_admin_display_title: 'Root item',
+    },
+    {
+      id: 3,
+      get_admin_display_title: 'First item',
+    },
+  ],
+};
+
+export const WithTrailingTitle = Template.bind({});
+
+WithTrailingTitle.args = {
+  pages: [
+    {
+      is_root: true,
+      id: 2,
+      get_admin_display_title: 'Root item',
+    },
+    {
+      id: 3,
+      get_admin_display_title: 'First item',
+    },
+    {
+      id: 4,
+      get_admin_display_title: 'Second item',
+    },
+    {
+      id: 5,
+      get_admin_display_title: 'Third item',
+    },
+  ],
+  trailing_breadcrumb_title: 'Trailing item',
+};
+
+export const MultipleItems = Template.bind({});
+
+MultipleItems.args = {
+  pages: [
+    {
+      is_root: true,
+      id: 2,
+      get_admin_display_title: 'Root item',
+    },
+    {
+      id: 3,
+      get_admin_display_title: 'First item',
+    },
+    {
+      id: 4,
+      get_admin_display_title: 'Second item',
+    },
+    {
+      id: 5,
+      get_admin_display_title: 'Third item',
+    },
+    {
+      id: 4,
+      get_admin_display_title: 'Fourth item',
+    },
+    {
+      id: 5,
+      get_admin_display_title: 'Fifth item',
+    },
+  ],
+};
