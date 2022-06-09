8436dec4239c07f9ba7d4aacdb0d12fe884d03b0
Matt Westcott <matt@west.co.tt>
2022-06-09 14:48:13 +0100

    Make the viewset responsible for registering the chooser widget

--- wagtail/admin/viewsets/chooser.py
+++ wagtail/admin/viewsets/chooser.py
@@ -1,18 +1,33 @@
+from django.db.models import ForeignKey
 from django.urls import path
+from django.utils.functional import cached_property
 from django.utils.translation import gettext as _
 
+from wagtail.admin.forms.models import register_form_field_override
 from wagtail.admin.views.generic import chooser as chooser_views
+from wagtail.admin.widgets.chooser import BaseChooser
 
 from .base import ViewSet
 
 
 class ChooserViewSet(ViewSet):
     icon = "snippet"
-    page_title = _("Choose")
+    choose_one_text = _("Choose")
+    page_title = None
+    choose_another_text = _("Choose another")
+    edit_item_text = _("Edit")
 
     choose_view_class = chooser_views.ChooseView
     choose_results_view_class = chooser_views.ChooseResultsView
     chosen_view_class = chooser_views.ChosenView
+    base_widget_class = BaseChooser
+
+    register_widget = True
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.page_title is None:
+            self.page_title = self.choose_one_text
 
     @property
     def choose_view(self):
@@ -38,9 +53,30 @@ class ChooserViewSet(ViewSet):
             model=self.model,
         )
 
+    @cached_property
+    def widget_class(self):
+        return type(
+            "%sChooserWidget" % self.model.__name__,
+            (self.base_widget_class,),
+            {
+                "model": self.model,
+                "choose_one_text": self.choose_one_text,
+                "choose_another_text": self.choose_another_text,
+                "link_to_chosen_text": self.edit_item_text,
+                "chooser_modal_url_name": self.get_url_name("choose"),
+                "icon": self.icon,
+            },
+        )
+
     def get_urlpatterns(self):
         return super().get_urlpatterns() + [
             path("", self.choose_view, name="choose"),
             path("results/", self.choose_results_view, name="choose_results"),
             path("chosen/<str:pk>/", self.chosen_view, name="chosen"),
         ]
+
+    def on_register(self):
+        if self.register_widget:
+            register_form_field_override(
+                ForeignKey, to=self.model, override={"widget": self.widget_class}
+            )
