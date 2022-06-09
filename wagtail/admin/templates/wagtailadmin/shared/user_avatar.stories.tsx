351160ce56afc8f2d52e9ffcdeb801f488306715
Thibaud Colas <thibaudcolas@gmail.com>
2022-06-09 22:19:39 +0100

    Implement DPL tag overrides

--- wagtail/admin/templates/wagtailadmin/shared/user_avatar.stories.tsx
+++ wagtail/admin/templates/wagtailadmin/shared/user_avatar.stories.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
-import {Pattern, generateDocs} from 'storybook-django/src/react';
+import { Pattern, generateDocs } from 'storybook-django/src/react';
 
 import template from './user_avatar.html';
 
-const {docs, argTypes} = generateDocs(template);
+const { docs, argTypes } = generateDocs(template);
 
 export default {
-  parameters: {docs},
-  argTypes: {...argTypes},
+  parameters: { docs },
+  argTypes: { ...argTypes },
 };
 
-const Template = (args) => <Pattern filename={__filename} context={args}/>;
-export const Base = Template.bind({});
+const Template = ({ url }) => (
+  <Pattern
+    filename={__filename}
+    tags={{
+      avatar_url: {
+        'user size=25': {
+          raw: url,
+        },
+      },
+    }}
+  />
+);
 
-Base.args = {
-  user: {
-    pk: 1,
-    first_name: 'Timmy',
-    last_name: 'Newtron',
-    username: 'theTim',
-    wagtail_userprofile: {
-      name: 'Someone else',
-      avatar: {
-        url: "https://gravatar.com/avatar/31c3d5cc27d1faa321c2413589e8a53f?s=200&d=robohash&r=x"
-      }
-    },
-  },
-  // username: 'Timothy',
-}
+export const Uploaded = Template.bind({});
+Uploaded.args = {
+  url: 'https://source.unsplash.com/6eaMM0BuWVI/100x100',
+};
 
-
+export const Gravatar = Template.bind({});
+Gravatar.args = {
+  url: 'https://gravatar.com/avatar/31c3d5cc27d1faa321c2413589e8a53f?s=200&d=robohash&r=x',
+};
