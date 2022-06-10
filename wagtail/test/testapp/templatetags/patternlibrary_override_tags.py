600c4e7a64c58547b827552079ccfab7517da22f
Steve Steinwand <steven_ts@hotmail.com>
2022-06-10 11:34:23 -0600

    Isort for override tags

--- wagtail/test/testapp/templatetags/patternlibrary_override_tags.py
+++ wagtail/test/testapp/templatetags/patternlibrary_override_tags.py
@@ -1,5 +1,5 @@
 from pattern_library.monkey_utils import override_tag
-from wagtail.admin.templatetags.wagtailadmin_tags import register
 
+from wagtail.admin.templatetags.wagtailadmin_tags import register
 
 override_tag(register, name="avatar_url")
