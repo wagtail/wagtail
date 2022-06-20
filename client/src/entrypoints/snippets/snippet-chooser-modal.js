172b1335689307960c8ebf363611983581ea6545
Matt Westcott <matt@west.co.tt>
2022-06-20 17:53:48 +0100

    Eliminate the custom response name 'snippetChosen'
    This makes the snippet chooser modal code a completely vanilla implementation of ChooserModalOnloadHandlerFactory, so we don't need snippet-chooser-modal.js at all.

--- client/src/entrypoints/snippets/snippet-chooser-modal.js
+++
@@ -1,6 +0,0 @@
-import { ChooserModalOnloadHandlerFactory } from '../../includes/chooserModal';
-
-window.SNIPPET_CHOOSER_MODAL_ONLOAD_HANDLERS =
-  new ChooserModalOnloadHandlerFactory({
-    chosenResponseName: 'snippetChosen',
-  }).getOnLoadHandlers();
