wagtail/wagtail

commit 9d1110d349a43205407df0b5e73f33bd0a3a031e

Wrap snippet views in SnippetViewSet (#8422)

wagtail/snippets/templates/wagtailsnippets/chooser/results.html
@@ -18,7 +18,7 @@
     {% if is_searching %}
         <p role="alert">{% blocktrans trimmed %}Sorry, no snippets match "<em>{{ query_string }}</em>"{% endblocktrans %}</p>
     {% else %}
-        {% url 'wagtailsnippets:add' model_opts.app_label model_opts.model_name as wagtailsnippets_create_snippet_url %}
+        {% url view.add_url_name as wagtailsnippets_create_snippet_url %}
         <p>{% blocktrans trimmed with snippet_type_name=model_opts.verbose_name %}You haven't created any {{ snippet_type_name }} snippets. Why not <a href="{{ wagtailsnippets_create_snippet_url }}" target="_blank" rel="noreferrer">create one now</a>?{% endblocktrans %}</p>
     {% endif %}
 {% endif %}

wagtail/snippets/views/chooser.py
@@ -142,6 +142,8 @@ class ChooseView(BaseChooseView):
 class ChooseResultsView(BaseChooseView):
     # Return just the HTML fragment for the results
     def render_to_response(self):
+        app_label = self.model._meta.app_label
+        model_name = self.model._meta.model_name
         return TemplateResponse(
             self.request,
             "wagtailsnippets/chooser/results.html",
@@ -151,6 +153,7 @@ class ChooseResultsView(BaseChooseView):
                 "table": self.table,
                 "query_string": self.search_query,
                 "is_searching": self.is_searching,
+                "add_url_name": f"wagtailsnippets_{app_label}_{model_name}:add",
             },
         )
 
@@ -164,7 +167,7 @@ class ChosenView(View):
             "id": str(item.pk),
             "string": str(item),
             "edit_link": reverse(
-                "wagtailsnippets:edit", args=(app_label, model_name, quote(item.pk))
+                f"wagtailsnippets_{app_label}_{model_name}:edit", args=[quote(item.pk)]
             ),
         }
 

wagtail/snippets/templates/wagtailsnippets/snippets/results.html
@@ -12,13 +12,12 @@
 
     {% include "wagtailsnippets/snippets/list.html" %}
 
-    {% url 'wagtailsnippets:list' model_opts.app_label model_opts.model_name as wagtailsnippets_list_url %}
-    {% include "wagtailadmin/shared/pagination_nav.html" with items=items linkurl=wagtailsnippets_list_url %}
+    {% include "wagtailadmin/shared/pagination_nav.html" with items=items linkurl=index_url %}
 {% else %}
     {% if is_searching %}
         <p role="alert">{% blocktrans trimmed %}Sorry, no snippets match "<em>{{ query_string }}</em>"{% endblocktrans %}</p>
     {% else %}
-        {% url 'wagtailsnippets:add' model_opts.app_label model_opts.model_name as wagtailsnippets_create_url %}
+        {% url view.add_url_name as wagtailsnippets_create_url %}
 
         {# NOTE: The two strings in the following messages are exactly the same #}
         {# The difference is that the first variant appends '?locale={{ locale.language_code}}' to 'wagtailsnippets_create_url' #}

wagtail/admin/viewsets/__init__.py
@@ -10,7 +10,11 @@ class ViewSetRegistry:
     def populate(self):
         for fn in hooks.get_hooks("register_admin_viewset"):
             viewset = fn()
-            self.register(viewset)
+            if isinstance(viewset, (list, tuple)):
+                for vs in viewset:
+                    self.register(vs)
+            else:
+                self.register(viewset)
 
     def register(self, viewset):
         self.viewsets.append(viewset)

wagtail/contrib/simple_translation/tests/test_views.py
@@ -256,32 +256,29 @@ class TestSubmitSnippetTranslationView(WagtailTestUtils, TestCase):
         view = SubmitSnippetTranslationView()
         view.object = self.en_snippet
         view.kwargs = {
-            "app_label": "some_app",
-            "model_name": "some_model",
+            "app_label": "snippetstests",
+            "model_name": "translatablesnippet",
             "pk": 99,
         }
         self.assertEqual(
-            view.get_success_url(), "/admin/snippets/some_app/some_model/edit/99/"
+            view.get_success_url(),
+            "/admin/snippets/snippetstests/translatablesnippet/edit/99/",
         )
 
     def test_get_success_url_for_single_locale(self):
         view = SubmitSnippetTranslationView()
         view.object = self.en_snippet
         view.kwargs = {
-            "app_label": "some_app",
-            "model_name": "some_model",
+            "app_label": "snippetstests",
+            "model_name": "translatablesnippet",
             "pk": 99,
         }
 
         self.assertEqual(
             view.get_success_url(view.object),
             reverse(
-                "wagtailsnippets:edit",
-                args=[
-                    "some_app",
-                    "some_model",
-                    view.object.pk,
-                ],
+                "wagtailsnippets_snippetstests_translatablesnippet:edit",
+                args=[view.object.pk],
             ),
         )
 

wagtail/contrib/simple_translation/views.py
@@ -138,25 +138,18 @@ class SubmitSnippetTranslationView(SubmitTranslationView):
         return get_object_or_404(model, pk=unquote(self.kwargs["pk"]))
 
     def get_success_url(self, translated_snippet=None):
+        app_label = self.kwargs["app_label"]
+        model_name = self.kwargs["model_name"]
+        pk = self.kwargs["pk"]
+
         if translated_snippet:
             # If the editor chose a single locale to translate to, redirect to
             # the newly translated snippet's edit view.
-            return reverse(
-                "wagtailsnippets:edit",
-                args=[
-                    self.kwargs["app_label"],
-                    self.kwargs["model_name"],
-                    translated_snippet.pk,
-                ],
-            )
+            pk = translated_snippet.pk
 
         return reverse(
-            "wagtailsnippets:edit",
-            args=[
-                self.kwargs["app_label"],
-                self.kwargs["model_name"],
-                self.kwargs["pk"],
-            ],
+            f"wagtailsnippets_{app_label}_{model_name}:edit",
+            args=[pk],
         )
 
     def get_success_message(self, locales):

wagtail/snippets/action_menu.py
@@ -76,13 +76,11 @@ class DeleteMenuItem(ActionMenuItem):
         )
 
     def get_url(self, context):
+        app_label = context["model"]._meta.app_label
+        model_name = context["model"]._meta.model_name
         return reverse(
-            "wagtailsnippets:delete",
-            args=[
-                context["model"]._meta.app_label,
-                context["model"]._meta.model_name,
-                quote(context["instance"].pk),
-            ],
+            f"wagtailsnippets_{app_label}_{model_name}:delete",
+            args=[quote(context["instance"].pk)],
         )
 
 

commit b20446f25871d78e8533415187f7992f781b0ed7

Fix 'create one now' link on snippet choosers
Regression introduced in #8422

wagtail/snippets/templates/wagtailsnippets/chooser/results.html
@@ -18,7 +18,7 @@
     {% if is_searching %}
         <p role="alert">{% blocktrans trimmed %}Sorry, no snippets match "<em>{{ query_string }}</em>"{% endblocktrans %}</p>
     {% else %}
-        {% url view.add_url_name as wagtailsnippets_create_snippet_url %}
+        {% url add_url_name as wagtailsnippets_create_snippet_url %}
         <p>{% blocktrans trimmed with snippet_type_name=model_opts.verbose_name %}You haven't created any {{ snippet_type_name }} snippets. Why not <a href="{{ wagtailsnippets_create_snippet_url }}" target="_blank" rel="noreferrer">create one now</a>?{% endblocktrans %}</p>
     {% endif %}
 {% endif %}

wagtail/snippets/tests.py
@@ -1736,6 +1736,13 @@ class TestSnippetChoose(TestCase, WagtailTestUtils):
             response.json()["html"],
         )
 
+    def test_no_results(self):
+        Advert.objects.all().delete()
+        response = self.get()
+        self.assertTemplateUsed(response, "wagtailsnippets/chooser/choose.html")
+        response_html = response.json()["html"]
+        self.assertIn('href="/admin/snippets/tests/advert/add/"', response_html)
+
     def test_ordering(self):
         """
         Listing should be ordered by PK if no ordering has been set on the model
@@ -1784,6 +1791,32 @@ class TestSnippetChoose(TestCase, WagtailTestUtils):
 
         self.assertEqual(len(response.context["items"]), 1)
         self.assertEqual(response.context["items"][0].text, "English snippet")
+
+
+class TestSnippetChooseResults(TestCase, WagtailTestUtils):
+    fixtures = ["test.json"]
+
+    def setUp(self):
+        self.login()
+        self.url_args = ["tests", "advert"]
+
+    def get(self, params=None):
+        return self.client.get(
+            reverse("wagtailsnippets:choose_results", args=self.url_args), params or {}
+        )
+
+    def test_simple(self):
+        response = self.get()
+        self.assertTemplateUsed(response, "wagtailsnippets/chooser/results.html")
+
+    def test_no_results(self):
+        Advert.objects.all().delete()
+        response = self.get()
+        self.assertTemplateUsed(response, "wagtailsnippets/chooser/results.html")
+        self.assertContains(
+            response,
+            'href="/admin/snippets/tests/advert/add/"',
+        )
 
 
 class TestSnippetChooseWithSearchableSnippet(TestCase, WagtailTestUtils):

wagtail/snippets/views/chooser.py
@@ -116,6 +116,8 @@ class BaseChooseView(View):
 class ChooseView(BaseChooseView):
     # Return the choose view as a ModalWorkflow response
     def render_to_response(self):
+        app_label = self.model._meta.app_label
+        model_name = self.model._meta.model_name
         return render_modal_workflow(
             self.request,
             "wagtailsnippets/chooser/choose.html",
@@ -134,6 +136,7 @@ class ChooseView(BaseChooseView):
                 "locale_options": Locale.objects.all()
                 if issubclass(self.model, TranslatableMixin)
                 else [],
+                "add_url_name": f"wagtailsnippets_{app_label}_{model_name}:add",
             },
             json_data={"step": "choose"},
         )