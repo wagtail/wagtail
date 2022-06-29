9e84442ba856365cccca1a6ab97d998461a2b247
Matt Westcott <matt@west.co.tt>
2022-06-29 15:34:33 +0100

    Refactor an ImageChooser class out of createImageChooser
    Use the client/src/components/ChooserWidget Chooser class as a guide, but don't copy code from there (so that any image-specific customisations don't get lost in translation)

--- client/src/entrypoints/images/image-chooser.js
+++ client/src/entrypoints/images/image-chooser.js
@@ -1,99 +1,124 @@
 import $ from 'jquery';
 
-function createImageChooser(id) {
-  const chooserElement = $('#' + id + '-chooser');
-  const previewImage = chooserElement.find('.preview-image img');
-  const input = $('#' + id);
-  const editLink = chooserElement.find('.edit-link');
-  const chooserBaseUrl = chooserElement.data('chooserUrl');
+class ImageChooser {
+  constructor(id) {
+    this.chooserElement = $('#' + id + '-chooser');
+    this.previewImage = this.chooserElement.find('.preview-image img');
+    this.input = $('#' + id);
+    this.editLink = this.chooserElement.find('.edit-link');
+    this.chooserBaseUrl = this.chooserElement.data('chooserUrl');
 
-  /*
-  Construct initial state of the chooser from the rendered (static) HTML and arguments passed to
-  createImageChooser. State is either null (= no image chosen) or a dict of id, edit_link,
-  title and preview (= a dict of url, width, height).
+    this.state = this.getStateFromHTML();
 
-  The result returned from the image chooser modal (see get_image_result_data in
-  wagtail.images.views.chooser) is a superset of this, and can therefore be passed directly to
-  chooser.setState.
-  */
-  let state = null;
-  if (input.val()) {
-    state = {
-      id: input.val(),
-      edit_link: editLink.attr('href'),
-      title: previewImage.attr('alt'),
-      preview: {
-        url: previewImage.attr('src'),
-        width: previewImage.attr('width'),
-        height: previewImage.attr('height'),
-      },
-    };
+    /* hook up chooser API to the buttons */
+    $('.action-choose', this.chooserElement).on('click', () => {
+      this.openChooserModal();
+    });
+
+    $('.action-clear', this.chooserElement).on('click', () => {
+      this.clear();
+    });
   }
 
-  /* define public API functions for the chooser */
-  const chooser = {
-    getState: () => state,
-    getValue: () => state && state.id,
-    setState: (newState) => {
-      if (newState) {
-        input.val(newState.id);
-        previewImage.attr({
-          src: newState.preview.url,
-          width: newState.preview.width,
-          height: newState.preview.height,
-          alt: newState.title,
-          title: newState.title,
-        });
-        chooserElement.removeClass('blank');
-        editLink.attr('href', newState.edit_link);
-      } else {
-        input.val('');
-        chooserElement.addClass('blank');
-      }
+  getStateFromHTML() {
+    /*
+    Construct initial state of the chooser from the rendered (static) HTML.
+    State is either null (= no image chosen) or a dict of id, edit_link, title
+    and preview (= a dict of url, width, height).
 
-      state = newState;
-    },
-    getTextLabel: (opts) => {
-      if (!state) return null;
-      const result = state.title;
-      if (opts && opts.maxLength && result.length > opts.maxLength) {
-        return result.substring(0, opts.maxLength - 1) + '…';
-      }
-      return result;
-    },
-    focus: () => {
-      $('.action-choose', chooserElement).focus();
-    },
-
-    openChooserModal: () => {
-      // eslint-disable-next-line no-undef
-      ModalWorkflow({
-        url: chooserBaseUrl,
-        // eslint-disable-next-line no-undef
-        onload: IMAGE_CHOOSER_MODAL_ONLOAD_HANDLERS,
-        responses: {
-          imageChosen: (result) => {
-            chooser.setState(result);
-          },
+    The result returned from the image chooser modal (see get_image_result_data in
+    wagtail.images.views.chooser) is a superset of this, and can therefore be passed directly to
+    chooser.setState.
+    */
+    if (this.input.val()) {
+      return {
+        id: this.input.val(),
+        edit_link: this.editLink.attr('href'),
+        title: this.previewImage.attr('alt'),
+        preview: {
+          url: this.previewImage.attr('src'),
+          width: this.previewImage.attr('width'),
+          height: this.previewImage.attr('height'),
         },
-      });
-    },
+      };
+    } else {
+      return null;
+    }
+  }
 
-    clear: () => {
-      chooser.setState(null);
-    },
-  };
+  getState() {
+    return this.state;
+  }
 
-  /* hook up chooser API to the buttons */
-  $('.action-choose', chooserElement).on('click', () => {
-    chooser.openChooserModal();
-  });
+  getValue() {
+    return this.state && this.state.id;
+  }
 
-  $('.action-clear', chooserElement).on('click', () => {
-    chooser.clear();
-  });
+  setState(newState) {
+    this.state = newState;
+    if (newState) {
+      this.renderState(newState);
+    } else {
+      this.renderEmptyState();
+    }
+  }
 
-  return chooser;
+  clear() {
+    this.setState(null);
+  }
+
+  renderEmptyState() {
+    this.input.val('');
+    this.chooserElement.addClass('blank');
+  }
+
+  renderState(newState) {
+    this.input.val(newState.id);
+    this.previewImage.attr({
+      src: newState.preview.url,
+      width: newState.preview.width,
+      height: newState.preview.height,
+      alt: newState.title,
+      title: newState.title,
+    });
+    this.chooserElement.removeClass('blank');
+    this.editLink.attr('href', newState.edit_link);
+  }
+
+  getTextLabel(opts) {
+    if (!this.state) return null;
+    const result = this.state.title;
+    if (opts && opts.maxLength && result.length > opts.maxLength) {
+      return result.substring(0, opts.maxLength - 1) + '…';
+    }
+    return result;
+  }
+
+  focus() {
+    $('.action-choose', this.chooserElement).focus();
+  }
+
+  getModalUrl() {
+    return this.chooserBaseUrl;
+  }
+
+  openChooserModal() {
+    // eslint-disable-next-line no-undef
+    ModalWorkflow({
+      url: this.getModalUrl(),
+      // eslint-disable-next-line no-undef
+      onload: IMAGE_CHOOSER_MODAL_ONLOAD_HANDLERS,
+      responses: {
+        imageChosen: (result) => {
+          this.setState(result);
+        },
+      },
+    });
+  }
+}
+
+function createImageChooser(id) {
+  return new ImageChooser(id);
 }
 
 window.createImageChooser = createImageChooser;
