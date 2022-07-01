bde74d9023572a106c53210128b0fb32c82e30e9
Sage Abdullah <sage.abdullah@torchbox.com>
2022-07-01 15:51:22 +0700

    Move side_panels.py to wagtail/admin/ui

--- wagtail/admin/views/pages/create.py
+++ wagtail/admin/views/pages/create.py
@@ -13,7 +13,7 @@ from django.views.generic.base import ContextMixin, TemplateResponseMixin, View
 
 from wagtail.admin import messages, signals
 from wagtail.admin.action_menu import PageActionMenu
-from wagtail.admin.side_panels import PageSidePanels
+from wagtail.admin.ui.side_panels import PageSidePanels
 from wagtail.admin.views.generic import HookResponseMixin
 from wagtail.admin.views.pages.utils import get_valid_next_url_from_request
 from wagtail.models import Locale, Page, PageSubscription, UserPagePermissionsProxy
