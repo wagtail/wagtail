562a459c1dc1d1d46776c5a56569608f67b96e22
Matt Westcott <matt@west.co.tt>
2022-07-05 17:41:01 +0100

    Remove now-unnecessary save_object methods

--- wagtail/documents/views/multiple.py
+++ wagtail/documents/views/multiple.py
@@ -77,9 +77,6 @@ class EditView(BaseEditView):
     def get_edit_form_class(self):
         return get_document_multi_form(self.model)
 
-    def save_object(self, form):
-        form.save()
-
 
 class DeleteView(BaseDeleteView):
     permission_policy = permission_policy
