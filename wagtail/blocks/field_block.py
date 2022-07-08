972e44c441e3ca351dabd17dcda7d0f5606350e2
Matt Westcott <matt@west.co.tt>
2022-07-08 18:40:07 +0100

    Implement max_length validation on RichTextBlock

--- wagtail/blocks/field_block.py
+++ wagtail/blocks/field_block.py
@@ -13,7 +13,11 @@ from django.utils.translation import gettext as _
 
 from wagtail.admin.staticfiles import versioned_static
 from wagtail.coreutils import camelcase_to_underscore, resolve_model_string
-from wagtail.rich_text import RichText, get_text_for_indexing
+from wagtail.rich_text import (
+    RichText,
+    RichTextMaxLengthValidator,
+    get_text_for_indexing,
+)
 from wagtail.telepath import Adapter, register
 
 from .base import Block
@@ -642,9 +646,14 @@ class RichTextBlock(FieldBlock):
         help_text=None,
         editor="default",
         features=None,
+        max_length=None,
         validators=(),
         **kwargs,
     ):
+        if max_length is not None:
+            validators = list(validators) + [
+                RichTextMaxLengthValidator(max_length),
+            ]
         self.field_options = {
             "required": required,
             "help_text": help_text,
