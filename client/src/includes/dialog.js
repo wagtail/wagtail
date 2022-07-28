54702657370287e99605182843c67510b7de6b2c
LB Johnston <mail@lb.ee>
2022-07-29 08:43:19 +1000

    add dispatched events for modal shown/hidden

--- client/src/includes/dialog.js
+++ client/src/includes/dialog.js
@@ -10,12 +10,27 @@ export const dialog = (
     const dialogTemplate = new A11yDialog(templateContent);
 
     // Prevent scrolling when dialog is open
+    // Dispatch events to hook into behaviour of modals showing / hiding that bubble
     dialogTemplate
-      .on('show', () => {
+      .on('show', (element, event) => {
         html.style.overflowY = 'hidden';
+        templateContent.dispatchEvent(
+          new CustomEvent('wagtail:dialog-shown', {
+            bubbles: true,
+            cancelable: false,
+            detail: { event },
+          }),
+        );
       })
-      .on('hide', () => {
+      .on('hide', (element, event) => {
         html.style.overflowY = '';
+        templateContent.dispatchEvent(
+          new CustomEvent('wagtail:dialog-hidden', {
+            bubbles: true,
+            cancelable: false,
+            detail: { event },
+          }),
+        );
       });
 
     // Attach event listeners to the dialog root (element with id), so it's
