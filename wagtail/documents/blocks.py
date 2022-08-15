a6a94a9a04a70334db73672024f9cafb99f5e0c9
Matt Westcott <matt@west.co.tt>
2022-08-15 17:32:55 +0100

    Ensure DocumentChooserBlock can be deconstructed for migrations
    Fixes #8989. Now that DocumentChooserBlock is constructed dynamically via wagtail.documents.viewsets.chooser, we need to explicitly set its `__module__` attribute so that the result of calling `deconstruct()` for migrations points back to the wagtail.documents.blocks module.
    
    Also update the documentation for defining custom choosers, and add tests for deconstructing the other chooser blocks.

--- wagtail/documents/blocks.py
+++ wagtail/documents/blocks.py
@@ -1,3 +1,7 @@
 from wagtail.documents.views.chooser import viewset as chooser_viewset
 
 DocumentChooserBlock = chooser_viewset.block_class
+
+# When deconstructing a DocumentChooserBlock instance for migrations, the module path
+# used in migrations should point to this module
+DocumentChooserBlock.__module__ = "wagtail.documents.blocks"
