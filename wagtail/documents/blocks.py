b4bc6818659ae785af39965569ed4bca51f0bf0d
Matt Westcott <matt@west.co.tt>
2022-08-16 14:58:40 +0100

    Fix DocumentChooserBlock deconstruction for custom document models
    Fixes #8989. The previous fix #9004 failed for custom document models because ChooserViewset assigns an internal name for the ChooserBlock class based on the model name, and if this is anything other than Document it won't match the name DocumentChooserBlock that it's exposed under in wagtail.documents.blocks. Fix this by replacing the `block_class` property with a `get_block_class` method that lets us specify the class name. As a bonus, user code that defines chooser blocks no longer has to directly hack the `__module__` attribute.

--- wagtail/documents/blocks.py
+++ wagtail/documents/blocks.py
@@ -1,7 +1,5 @@
 from wagtail.documents.views.chooser import viewset as chooser_viewset
 
-DocumentChooserBlock = chooser_viewset.block_class
-
-# When deconstructing a DocumentChooserBlock instance for migrations, the module path
-# used in migrations should point to this module
-DocumentChooserBlock.__module__ = "wagtail.documents.blocks"
+DocumentChooserBlock = chooser_viewset.get_block_class(
+    name="DocumentChooserBlock", module_path="wagtail.documents.blocks"
+)
