d3c5689340ac734f8f9cc089b82c98918314c386
LB Johnston <mail@lb.ee>
2022-08-23 20:43:35 +1000

    Set up initial stimulus application integration

---
+++ client/src/controllers/index.ts
@@ -0,0 +1,8 @@
+import type { Definition } from '@hotwired/stimulus';
+
+/**
+ * Important: Only add default core controllers that should load with the base admin JS bundle.
+ */
+export const coreControllerDefinitions: Definition[] = [
+  /* .. */
+];
