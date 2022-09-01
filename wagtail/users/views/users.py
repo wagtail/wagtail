11cda22767b9bd573294b4e8b35740d55b3b2701
Mehrdad Moradizadeh <mhrddmoradii@gmail.com>
2022-09-01 13:11:21 -0400

    Adopt generic class based view for User index listingproblem
    - improving user_management tests
    - refactoring test_authorised_with_add_permission, test_authorised_with_change_permission, and test_authorised_with_delete_permission into a single function
    - Update wagtail/users/views/users.py
    - Update wagtail/users/templates/wagtailusers/users/results.html
    - Update wagtail/users/templates/wagtailusers/users/index.html (changing to generic/base.html)
    - relates to #8622

--- wagtail/users/views/users.py
+++ wagtail/users/views/users.py
@@ -2,21 +2,21 @@ from django.conf import settings
 from django.contrib.auth import get_user_model, update_session_auth_hash
 from django.contrib.auth.models import Group
 from django.core.exceptions import PermissionDenied
-from django.core.paginator import Paginator
 from django.db import transaction
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect
 from django.template.response import TemplateResponse
 from django.urls import reverse
 from django.utils.translation import gettext as _
-from django.views.decorators.vary import vary_on_headers
+from django.utils.translation import gettext_lazy
 
 from wagtail import hooks
 from wagtail.admin import messages
-from wagtail.admin.auth import any_permission_required, permission_required
-from wagtail.admin.forms.search import SearchForm
+from wagtail.admin.auth import permission_required
+from wagtail.admin.views.generic import IndexView
 from wagtail.compat import AUTH_USER_APP_LABEL, AUTH_USER_MODEL_NAME
 from wagtail.log_actions import log
+from wagtail.permission_policies import ModelPermissionPolicy
 from wagtail.users.forms import UserCreationForm, UserEditForm
 from wagtail.users.utils import user_can_delete_user
 from wagtail.utils.loading import get_custom_form
@@ -70,74 +70,74 @@ def get_users_filter_query(q, model_fields):
     return conditions
 
 
-@any_permission_required(add_user_perm, change_user_perm, delete_user_perm)
-@vary_on_headers("X-Requested-With")
-def index(request, *args):
-    q = None
-    is_searching = False
+class Index(IndexView):
+    """
+    Lists the users for management within the admin.
+    """
 
-    group = None
-    group_filter = Q()
-    if args:
-        group = get_object_or_404(Group, id=args[0])
-        group_filter = Q(groups=group) if args else Q()
+    any_permission_required = ["add", "change", "delete"]
+    permission_policy = ModelPermissionPolicy(User)
+    model = User
+    context_object_name = "users"
+    index_url_name = "wagtailusers_users:index"
+    add_url_name = "wagtailusers_users:add"
+    edit_url_name = "wagtailusers_users:edit"
+    default_ordering = "name"
+    paginate_by = 20
+    template_name = None
+    is_searchable = True
+    page_title = gettext_lazy("Users")
 
-    model_fields = [f.name for f in User._meta.get_fields()]
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+        setattr(self, "template_name", self.get_template())
+        self.group = get_object_or_404(Group, id=args[0]) if args else None
+        self.group_filter = Q(groups=self.group) if self.group else Q()
+        self.model_fields = [f.name for f in User._meta.get_fields()]
 
-    if "q" in request.GET:
-        form = SearchForm(request.GET, placeholder=_("Search users"))
-        if form.is_valid():
-            q = form.cleaned_data["q"]
-            is_searching = True
-            conditions = get_users_filter_query(q, model_fields)
+    def get_valid_orderings(self):
+        return ["name", "username"]
 
-            users = User.objects.filter(group_filter & conditions)
-    else:
-        form = SearchForm(placeholder=_("Search users"))
+    def get_queryset(self):
+        if self.is_searching:
+            conditions = get_users_filter_query(self.search_query, self.model_fields)
+            users = User.objects.filter(self.group_filter & conditions)
+        else:
+            users = User.objects.filter(self.group_filter)
 
-    if not is_searching:
-        users = User.objects.filter(group_filter)
+        if self.locale:
+            users = users.filter(locale=self.locale)
 
-    if "last_name" in model_fields and "first_name" in model_fields:
-        users = users.order_by("last_name", "first_name")
+        if "last_name" in self.model_fields and "first_name" in self.model_fields:
+            users = users.order_by("last_name", "first_name")
 
-    if "ordering" in request.GET:
-        ordering = request.GET["ordering"]
-
-        if ordering == "username":
+        if self.get_ordering() == "username":
             users = users.order_by(User.USERNAME_FIELD)
-    else:
-        ordering = "name"
 
-    paginator = Paginator(users.select_related("wagtail_userprofile"), per_page=20)
-    users = paginator.get_page(request.GET.get("p"))
+        return users
 
-    if request.headers.get("x-requested-with") == "XMLHttpRequest":
-        return TemplateResponse(
-            request,
-            "wagtailusers/users/results.html",
-            {
-                "users": users,
-                "is_searching": is_searching,
-                "query_string": q,
-                "ordering": ordering,
-            },
+    def get_template(self):
+        if self.request.headers.get("x-requested-with") == "XMLHttpRequest":
+            return "wagtailusers/users/results.html"
+        else:
+            return "wagtailusers/users/index.html"
+
+    def get_context_data(self, *args, object_list=None, **kwargs):
+        context_data = super().get_context_data(
+            *args, object_list=object_list, **kwargs
         )
-    else:
-        return TemplateResponse(
-            request,
-            "wagtailusers/users/index.html",
+        context_data["ordering"] = self.get_ordering()
+        context_data["group"] = self.group
+        if self.request.headers.get("x-requested-with") == "XMLHttpRequest":
+            return context_data
+
+        context_data.update(
             {
-                "group": group,
-                "search_form": form,
-                "users": users,
-                "is_searching": is_searching,
-                "ordering": ordering,
-                "query_string": q,
                 "app_label": User._meta.app_label,
                 "model_name": User._meta.model_name,
-            },
+            }
         )
+        return context_data
 
 
 @permission_required(add_user_perm)
