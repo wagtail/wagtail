36c58b16d992ce87cdf8378ef8b9704f23f80021
Mehrdad Moradizadeh <mhrddmoradii@gmail.com>
2022-09-13 23:35:57 -0400

    Refactor the user delete view into a class based view
    - adapting the new approach for the template title
    - moving the check to dispatch method
    - relates to #8622

--- wagtail/users/urls/users.py
+++ wagtail/users/urls/users.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("", users.Index.as_view(), name="index"),
     path("add/", users.Create.as_view(), name="add"),
     path("<str:user_id>/", users.edit, name="edit"),
-    path("<str:user_id>/delete/", users.delete, name="delete"),
+    path("<str:pk>/delete/", users.Delete.as_view(), name="delete"),
 ]
