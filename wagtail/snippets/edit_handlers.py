67e2d6c1d778dc92d54bf624dc276a26c2fc8f80
Matt Westcott <matt@west.co.tt>
2022-09-29 19:43:38 +0100

    Reword special-purpose FieldPanel deprecation message for clarity
    Multiple people on Slack support have asked "how do I fix this error" now, which suggests that the wording here needs improvement...
    
    * change "is obsolete" to "is no longer required", to make it sound less "your code is broken" and more "here's an improvement you can make" (but also state that it'll be removed in a future release, to make it clear that you have to do it soomer or later)
    * leave out the full module paths - they're probably coming across as internal tech gobbledegook rather than informative, and obscuring the word "Panel" which is the most important thing in the message
    * link to the release note to clear up any remaining confusion

--- wagtail/snippets/edit_handlers.py
+++ wagtail/snippets/edit_handlers.py
@@ -7,7 +7,9 @@ from wagtail.utils.deprecation import RemovedInWagtail50Warning
 class SnippetChooserPanel(FieldPanel):
     def __init__(self, *args, **kwargs):
         warn(
-            "wagtail.snippets.edit_handlers.SnippetChooserPanel is obsolete and should be replaced by wagtail.admin.panels.FieldPanel",
+            "SnippetChooserPanel is no longer required for snippet choosers, and should be replaced by FieldPanel. "
+            "SnippetChooserPanel will be removed in a future release. "
+            "See https://docs.wagtail.org/en/stable/releases/3.0.html#removal-of-special-purpose-field-panel-types",
             category=RemovedInWagtail50Warning,
             stacklevel=2,
         )
