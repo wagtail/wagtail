a55a3bb08494355ecefa8082e5a72a1ee302d4bc
Tidiane Dia <atdia97@gmail.com>
2022-10-18 15:58:32 +0100

    Remove references to wagtail/contrib/settings/static_src in build scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -136,7 +136,6 @@ module.exports = {
       files: [
         'docs/_static/**',
         'wagtail/contrib/modeladmin/static_src/wagtailmodeladmin/js/prepopulate.js',
-        'wagtail/contrib/settings/static_src/wagtailsettings/js/site-switcher.js',
         'wagtail/documents/static_src/wagtaildocs/js/add-multiple.js',
         'wagtail/embeds/static_src/wagtailembeds/js/embed-chooser-modal.js',
         'wagtail/images/static_src/wagtailimages/js/add-multiple.js',
