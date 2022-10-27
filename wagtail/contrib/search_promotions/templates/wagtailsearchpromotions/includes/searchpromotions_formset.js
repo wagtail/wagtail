cad419ea9eb295ce9a9d398cba7fde2651228b0d
LB Johnston <mail@lb.ee>
2022-10-28 08:26:12 +1000

    Prettier linting - clean up ignored file (search promotions)
    - remove specific file in .prettierignore and add inline comments to allow for linting / formatting for the searchpromotions_formset
    - Split from #9522

--- wagtail/contrib/search_promotions/templates/wagtailsearchpromotions/includes/searchpromotions_formset.js
+++ wagtail/contrib/search_promotions/templates/wagtailsearchpromotions/includes/searchpromotions_formset.js
@@ -1,13 +1,15 @@
-$(function() {
-    var panel = InlinePanel({
-        formsetPrefix: "id_{{ formset.prefix }}",
-        emptyChildFormPrefix: "{{ formset.empty_form.prefix }}",
-        canOrder: true
-    });
+$(function () {
+  // eslint-disable-next-line no-undef
+  var panel = InlinePanel({
+    formsetPrefix: 'id_{{ formset.prefix }}',
+    emptyChildFormPrefix: '{{ formset.empty_form.prefix }}',
+    canOrder: true,
+  });
 
-    {% for form in formset.forms %}
-        panel.initChildControls('{{ formset.prefix }}-{{ forloop.counter0 }}');
-    {% endfor %}
+  // {# Ensure eslint/prettier ignore the Django template syntax by treating them as comments, template for loop will still be executed by Django #}
+  // {% for form in formset.forms %}
+  panel.initChildControls('{{ formset.prefix }}-{{ forloop.counter0 }}');
+  // {% endfor %}
 
-    panel.updateMoveButtonDisabledStates();
+  panel.updateMoveButtonDisabledStates();
 });
