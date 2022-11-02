2dc8b6bd4123919cb712194a529648866ddd26dc
AdeboyeJuliet <julietadeboye01@gmail.com>
2022-11-02 19:27:53 +0100

    Migrate initSkipLink to typescript, add unit tests & JSDOC
    - Additionally clean up the exports and imports to be sorted & align with named export usage
    - Fixes #9541

--- client/src/includes/initSkipLink.ts
+++ client/src/includes/initSkipLink.ts
@@ -1,17 +1,25 @@
+/**
+ * Appears at the top left corner of the admin page with the tab button is clicked.
+ * Used to provide an accessible skip button for keyboard control so that users can
+ * easily navigate to the main content without having to navigate a long list of navigation links.
+ *
+ * Inspired by https://github.com/selfthinker/dokuwiki_template_writr/blob/master/js/skip-link-focus-fix.js
+ *
+ */
 const initSkipLink = () => {
-  // Inspired by https://github.com/selfthinker/dokuwiki_template_writr/blob/master/js/skip-link-focus-fix.js
-
   const skiplink = document.querySelector('[data-skiplink]');
   const main = document.querySelector('main');
 
   const handleBlur = () => {
+    if (!main) return;
     main.removeAttribute('tabindex');
     main.removeEventListener('blur', handleBlur);
     main.removeEventListener('focusout', handleBlur);
   };
 
   const handleClick = () => {
-    main.setAttribute('tabindex', -1);
+    if (!main) return;
+    main.setAttribute('tabindex', '-1');
     main.addEventListener('blur', handleBlur);
     main.addEventListener('focusout', handleBlur);
     main.focus();
@@ -22,4 +30,4 @@ const initSkipLink = () => {
   }
 };
 
-export default initSkipLink;
+export { initSkipLink };
