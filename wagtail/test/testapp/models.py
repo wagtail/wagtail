563f656d46e89babbf40521ac9716c03bcac9f66
Matt Westcott <matt@west.co.tt>
2022-11-04 14:17:38 +0000

    Gracefully skip reference indexing for objects with a null ParentalKey
    Fixes #9583. The use case for null ParentalKeys is a bit questionable, but if they exist, we don't want reference indexing to break on them. Since references are always stored against the parent object, there's no valid way to record these references, so gracefully skip over them instead.

--- wagtail/test/testapp/models.py
+++ wagtail/test/testapp/models.py
@@ -1975,3 +1975,14 @@ class ModelWithStringTypePrimaryKey(models.Model):
 
     custom_id = models.CharField(max_length=255, primary_key=True)
     content = models.CharField(max_length=255)
+
+
+class ModelWithNullableParentalKey(models.Model):
+    """
+    There's not really a valid use case for null parental keys, but their presence should not
+    break things outright (e.g. when determining the object ID to store things under in the
+    references index).
+    """
+
+    page = ParentalKey(Page, blank=True, null=True)
+    content = RichTextField()
