3392cd85baea8fdad6ac3ede6674b20b634edc3b
Jake Howard <jake.howard@torchbox.com>
2022-12-05 15:16:38 +0000

    Revert "Add a security.txt file"
    This reverts commit fbcdf89bc35c9fefb2d09d00a7e67d4a27197b8b.

--- docs/conf.py
+++ docs/conf.py
@@ -171,7 +171,7 @@ html_static_path = ["_static"]
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-html_extra_path = ["public"]
+html_extra_path = ["robots.txt"]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
