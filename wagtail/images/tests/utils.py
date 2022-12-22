34691bdd3a211786422fc2db1d73ac50c5b367cf
Joshua Munn <public@elysee-munn.family>
2022-12-22 16:52:32 +0000

    Add preserve-svg option to image template tag

--- wagtail/images/tests/utils.py
+++ wagtail/images/tests/utils.py
@@ -34,3 +34,16 @@ def get_test_image_file_tiff(filename="test.tiff", colour="white", size=(640, 48
     image = PIL.Image.new("RGB", size, colour)
     image.save(f, "TIFF")
     return ImageFile(f, name=filename)
+
+
+def get_test_image_file_svg(
+    filename="test.svg", width=100, height=100, view_box="0 0 100 100"
+):
+    img = f"""
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" width="{width}" height="{height}" viewBox="{view_box}">
+</svg>
+    """
+    f = BytesIO(img.strip().encode("utf-8"))
+    return ImageFile(f, filename)
