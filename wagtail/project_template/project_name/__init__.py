deb51edda7ad1a7be3c3f185ac11a952f9e428c0
yashasbharadwaj1 <yashasbharadwaj6@gmail.com>
2022-12-28 15:18:16 +0530

    solving #9790

---
+++ wagtail/project_template/project_name/__init__.py
@@ -0,0 +1,1 @@
+from custom_check import *
--- wagtail/project_template/project_name/settings/base.py
+++ wagtail/project_template/project_name/settings/base.py
@@ -164,3 +164,7 @@ WAGTAILSEARCH_BACKENDS = {
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 WAGTAILADMIN_BASE_URL = "http://example.com"
+
+# below lines when working with aws s3
+DEFAULT_FILE_STORAGE = None
+AWS_S3_FILE_OVERWRITE = None
