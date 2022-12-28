205414f5878837abf0514dea6945fd72249290f4
yashasbharadwaj1 <yashasbharadwaj6@gmail.com>
2022-12-28 15:28:40 +0530

    solving the #9790 issue

---
+++ wagtail/project_template/project_name/custom_check.py
@@ -0,0 +1,22 @@
+from wagtail.project_template.project_name.settings.base import *
+from django.core.checks import Warning, register
+
+
+@register()
+def example_check(app_configs, **kwargs):
+    errors = []
+    
+    if DEFAULT_FILE_STORAGE == "storages.backends.s3boto3.S3Boto3Storage" and AWS_S3_FILE_OVERWRITE == True:
+
+        errors.append(
+            Warning(
+                'Note that the django-storages Amazon S3 backends\n'
+                '(storages.backends.s3boto.S3BotoStorage and\n'
+                'storages.backends.s3boto3.S3Boto3Storage)\n'
+                'do not correctly handle duplicate filenames in their default configuration.\n'
+                'When using these backends, AWS_S3_FILE_OVERWRITE must be set to False.\n',
+
+
+            )
+        )
+    return errors
