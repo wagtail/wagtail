567a47317fb4e3a88a439df1b0264ff46e9b23c0
Matt Westcott <matt@west.co.tt>
2023-01-12 01:56:04 +0000

    Insert chooser widget telepath definition onto template

--- wagtail/admin/panels/multiple_chooser_panel.py
+++ wagtail/admin/panels/multiple_chooser_panel.py
@@ -1,5 +1,7 @@
 from django.core.exceptions import ImproperlyConfigured
 
+from wagtail.telepath import JSContext
+
 from .inline_panel import InlinePanel
 
 
@@ -26,8 +28,19 @@ class MultipleChooserPanel(InlinePanel):
             self.chooser_widget = self.formset.empty_form.fields[
                 self.panel.chooser_field_name
             ].widget
+            self.js_context = JSContext()
+            self.chooser_widget_telepath_definition = self.js_context.pack(
+                self.chooser_widget
+            )
 
         def get_context_data(self, parent_context=None):
             context = super().get_context_data(parent_context)
             context["chooser_field_name"] = self.panel.chooser_field_name
+            context[
+                "chooser_widget_definition"
+            ] = self.chooser_widget_telepath_definition
             return context
+
+        @property
+        def media(self):
+            return super().media + self.js_context.media
