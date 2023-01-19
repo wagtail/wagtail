wagtail/wagtail

commit 6997ff1d687c509f7180b9482ac73ac9dd70c760

Use browser URLSearchParams to determine q param (core.js)
- Fixes #9765
- removed the function for getURLParam function and replaced with URLSearchParam method
- already used in many other places in the same file

client/src/entrypoints/admin/core.js
@@ -268,8 +268,8 @@ $(() => {
     // eslint-disable-next-line func-names
     const search = function () {
       const newQuery = $input.val();
-      // eslint-disable-next-line @typescript-eslint/no-use-before-define
-      const currentQuery = getURLParam('q');
+      const searchParams = new URLSearchParams(window.location.search);
+      const currentQuery = searchParams.get('q');
       // only do the query if it has changed for trimmed queries
       // for example - " " === "" and "firstword " ==== "firstword"
       if (currentQuery.trim() !== newQuery.trim()) {
@@ -278,7 +278,6 @@ $(() => {
         const index = searchNextIndex;
 
         // Update q, reset to first page, and keep other query params
-        const searchParams = new URLSearchParams(window.location.search);
         searchParams.set('q', newQuery);
         searchParams.delete('p');
         const queryString = searchParams.toString();
@@ -302,17 +301,6 @@ $(() => {
           },
         });
       }
-    };
-
-    // eslint-disable-next-line func-names
-    const getURLParam = function (name) {
-      const results = new RegExp('[\\?&]' + name + '=([^]*)').exec(
-        window.location.search,
-      );
-      if (results) {
-        return results[1];
-      }
-      return '';
     };
   }
 

commit d24ccb72484e9d7f63da6909eb2fd1c6229cd387

Fix issue where header search would fail on initial empty values
Regression from #9768
Fixes #9918

client/src/entrypoints/admin/core.js
@@ -268,9 +268,9 @@ $(() => {
 
     // eslint-disable-next-line func-names
     const search = function () {
-      const newQuery = $input.val();
+      const newQuery = $input.val() || '';
       const searchParams = new URLSearchParams(window.location.search);
-      const currentQuery = searchParams.get('q');
+      const currentQuery = searchParams.get('q') || '';
       // only do the query if it has changed for trimmed queries
       // for example - " " === "" and "firstword " ==== "firstword"
       if (currentQuery.trim() !== newQuery.trim()) {