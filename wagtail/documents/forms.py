wagtail/wagtail

commit faf3e190f6631a0ee94e5242f443e7aa0daa04c6

Fix: Revert the behaviour in #3821 deletion will happen before save

wagtail/documents/forms.py
@@ -42,8 +42,6 @@ class BaseDocumentForm(BaseCollectionMemberForm):
         if "file" in self.changed_data:
             self.instance._set_document_file_metadata()
 
-        super().save(commit=commit)
-
         if commit:
             if "file" in self.changed_data and self.original_file:
                 # If providing a new document file, delete the old one.
@@ -54,6 +52,8 @@ class BaseDocumentForm(BaseCollectionMemberForm):
 
             # Reindex the image to make sure all tags are indexed
             search_index.insert_or_update_object(self.instance)
+
+        super().save(commit=commit)
 
         return self.instance
 

wagtail/documents/tests/test_admin_views.py
@@ -603,9 +603,8 @@ class TestDocumentEditView(TestCase, WagtailTestUtils):
         )
         self.assertRedirects(response, reverse("wagtaildocs:index"))
         self.document.refresh_from_db()
-        self.assertFalse(self.document.file.storage.exists(old_filename))
-        self.assertTrue(self.document.file.storage.exists(self.document.file.name))
-        self.assertNotEqual(self.document.file.name, "documents/" + new_name)
+        self.assertEqual(old_filename, self.document.file.name)
+        self.assertEqual(self.document.file.name, "documents/" + new_name)
         self.assertEqual(self.document.file.read(), b"An updated test content.")
 
     def test_reupload_different_name(self):

commit e37363640e9b834ba3d419ab49e94ecdcad3e0aa

Fix test failures on indexing document tags
Fix regression caused by faf3e190f6631a0ee94e5242f443e7aa0daa04c6. The save() must happen before reindexing, so that the indexer sees the updated state of the tags.

wagtail/documents/forms.py
@@ -50,10 +50,11 @@ class BaseDocumentForm(BaseCollectionMemberForm):
                 self.original_file.storage.delete(self.original_file.name)
                 self.original_file = None
 
+        super().save(commit=commit)
+
+        if commit:
             # Reindex the image to make sure all tags are indexed
             search_index.insert_or_update_object(self.instance)
-
-        super().save(commit=commit)
 
         return self.instance
 