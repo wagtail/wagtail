e463c80250941a5489d1edf53c6951218281ac45
Jake Howard <jake.howard@torchbox.com>
2023-01-23 17:08:19 +0000

    Prevent memory exhaustion when running wagtail_update_image_renditions with many renditions
    This reduces the memory overhead of the command, allowing it to be run on sites with lots of images.
    Whilst that may reduce performance, I've also added a `select_related` and massively simplified the
    `purge_only` path to counter

--- wagtail/images/tests/test_management_commands.py
+++ wagtail/images/tests/test_management_commands.py
@@ -5,20 +5,22 @@ from io import StringIO
 from django.core import management
 from django.test import TestCase
 
-from wagtail.images import get_image_model
-
 from .utils import Image, get_test_image_file
+
+# note .utils.Image already does get_image_model()
+Rendition = Image.get_rendition_model()
 
 
 class TestUpdateImageRenditions(TestCase):
-    def setUp(self):
-        self.image = Image.objects.create(
+    @classmethod
+    def setUpTestData(cls):
+        cls.image = Image.objects.create(
             title="Test image",
             file=get_test_image_file(filename="test_image.png", colour="white"),
         )
 
-        self.rendition = Image.get_rendition_model().objects.create(
-            image=self.image,
+        cls.rendition = Rendition.objects.create(
+            image=cls.image,
             filter_spec="original",
             width=1000,
             height=1000,
@@ -27,8 +29,10 @@ class TestUpdateImageRenditions(TestCase):
             ),
         )
 
+        cls.reaesc = re.compile(r"\x1b[^m]*m")
+
     def delete_renditions(self):
-        renditions = get_image_model().get_rendition_model().objects.all()
+        renditions = Rendition.objects.all()
         for rendition in renditions:
             try:
                 rendition_image = rendition.image
@@ -49,42 +53,44 @@ class TestUpdateImageRenditions(TestCase):
         self.delete_renditions()
         # checking when command is called without any arguments
         output = self.run_command()
-        self.assertEqual(output.read(), "No image renditions found.\n")
+        output_string = self.reaesc.sub("", output.read())
+        self.assertEqual(output_string, "No image renditions found.\n")
 
         # checking when command is called with '--purge-only'
         output = self.run_command(purge_only=True)
-        self.assertEqual(output.read(), "No image renditions found.\n")
+        output_string = self.reaesc.sub("", output.read())
+        self.assertEqual(output_string, "No image renditions found.\n")
 
     def test_image_renditions(self):
-        renditions = get_image_model().get_rendition_model().objects.all()
+        renditions = Rendition.objects.all()
         total_renditions = len(renditions)
         output = self.run_command()
-        reaesc = re.compile(r"\x1b[^m]*m")
-        output_string = reaesc.sub("", output.read())
+        output_string = self.reaesc.sub("", output.read())
         # checking if the number of renditions regenerated equal total_renditions
         self.assertEqual(
             output_string,
-            f"Successfully regenerated {total_renditions} image rendition(s)\n",
+            f"Regenerating {total_renditions} rendition(s)\n"
+            f"Successfully processed {total_renditions} rendition(s)\n",
         )
 
         # checking if the number of renditions now equal total_renditions
-        renditions_now = get_image_model().get_rendition_model().objects.all()
+        renditions_now = Rendition.objects.all()
         total_renditions_now = len(renditions_now)
         self.assertEqual(total_renditions_now, total_renditions)
 
     def test_image_renditions_with_purge_only(self):
-        renditions = get_image_model().get_rendition_model().objects.all()
+        renditions = Rendition.objects.all()
         total_renditions = len(renditions)
         output = self.run_command(purge_only=True)
-        reaesc = re.compile(r"\x1b[^m]*m")
-        output_string = reaesc.sub("", output.read())
+        output_string = self.reaesc.sub("", output.read())
         # checking if the number of renditions purged equal total_renditions
         self.assertEqual(
             output_string,
-            f"Successfully purged {total_renditions} image rendition(s)\n",
+            f"Purging {total_renditions} rendition(s)\n"
+            f"Successfully processed {total_renditions} rendition(s)\n",
         )
 
         # checking if the number of renditions now equal 0
-        renditions_now = get_image_model().get_rendition_model().objects.all()
+        renditions_now = Rendition.objects.all()
         total_renditions_now = len(renditions_now)
         self.assertEqual(total_renditions_now, 0)
