1c2d828e7b02a18f7961a67ddb64d4fdb2b2ec2c
Matt Westcott <matt@west.co.tt>
2023-01-30 15:25:41 +0000

    Remove deprecated createXChooser JS functions

--- client/src/entrypoints/documents/document-chooser.js
+++ client/src/entrypoints/documents/document-chooser.js
@@ -1,9 +1,3 @@
 import { DocumentChooser } from '../../components/ChooserWidget/DocumentChooserWidget';
 
 window.DocumentChooser = DocumentChooser;
-
-function createDocumentChooser(id) {
-  /* RemovedInWagtail50Warning */
-  return new DocumentChooser(id);
-}
-window.createDocumentChooser = createDocumentChooser;
--- client/src/entrypoints/images/image-chooser.js
+++ client/src/entrypoints/images/image-chooser.js
@@ -1,10 +1,3 @@
 import { ImageChooser } from '../../components/ChooserWidget/ImageChooserWidget';
 
 window.ImageChooser = ImageChooser;
-
-function createImageChooser(id) {
-  /* RemovedInWagtail50Warning */
-  return new ImageChooser(id);
-}
-
-window.createImageChooser = createImageChooser;
