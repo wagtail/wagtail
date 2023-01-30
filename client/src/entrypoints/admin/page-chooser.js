68f13263890940e69211a5e365afe270a92033a6
Matt Westcott <matt@west.co.tt>
2023-01-30 15:25:41 +0000

    Remove deprecated createXChooser JS functions

--- client/src/entrypoints/admin/page-chooser.js
+++ client/src/entrypoints/admin/page-chooser.js
@@ -1,9 +1,3 @@
 import { PageChooser } from '../../components/ChooserWidget/PageChooserWidget';
 
 window.PageChooser = PageChooser;
-
-function createPageChooser(id, parentId, options) {
-  /* RemovedInWagtail50Warning */
-  return new PageChooser(id, parentId, options);
-}
-window.createPageChooser = createPageChooser;
--- client/src/entrypoints/snippets/snippet-chooser.js
+++ client/src/entrypoints/snippets/snippet-chooser.js
@@ -1,9 +1,3 @@
 import { SnippetChooser } from '../../components/ChooserWidget/SnippetChooserWidget';
 
 window.SnippetChooser = SnippetChooser;
-
-function createSnippetChooser(id) {
-  /* RemovedInWagtail50Warning */
-  return new SnippetChooser(id);
-}
-window.createSnippetChooser = createSnippetChooser;
