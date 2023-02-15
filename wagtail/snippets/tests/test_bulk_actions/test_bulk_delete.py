608308d73c9708b3d84d1d42a1468e4f81577294
Sage Abdullah <sage.abdullah@torchbox.com>
2023-02-15 17:47:16 +0000

    Call {before,after}_delete_snippet hooks in snippets delete bulk action view

--- wagtail/snippets/tests/test_bulk_actions/test_bulk_delete.py
+++ wagtail/snippets/tests/test_bulk_actions/test_bulk_delete.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import Permission
+from django.http import HttpRequest, HttpResponse
 from django.test import TestCase
 from django.urls import reverse
 
+from wagtail.snippets.bulk_actions.delete import DeleteBulkAction
 from wagtail.test.snippets.models import StandardSnippet
 from wagtail.test.utils import WagtailTestUtils
 
@@ -75,6 +77,135 @@ class TestSnippetDeleteView(WagtailTestUtils, TestCase):
         # User should be redirected back to the index
         self.assertEqual(response.status_code, 302)
 
-        # Documents should not be deleted
+        # Snippets should not be deleted
         for snippet in self.test_snippets:
             self.assertTrue(self.snippet_model.objects.filter(pk=snippet.pk).exists())
+
+    def test_before_bulk_action_hook_get(self):
+        with self.register_hook(
+            "before_bulk_action", lambda *args: HttpResponse("Overridden!")
+        ):
+            response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+
+        # The hook was not called
+        self.assertNotEqual(response.content, b"Overridden!")
+
+        # The instances were not deleted
+        self.assertQuerysetEqual(
+            self.snippet_model.objects.filter(
+                pk__in=[snippet.pk for snippet in self.test_snippets]
+            ),
+            self.test_snippets,
+            ordered=False,
+        )
+
+    def test_before_bulk_action_hook_post(self):
+        def hook_func(request, action_type, instances, action_class_instance):
+            self.assertIsInstance(request, HttpRequest)
+            self.assertEqual(action_type, "delete")
+            self.assertQuerysetEqual(instances, self.test_snippets)
+            self.assertIsInstance(action_class_instance, DeleteBulkAction)
+            return HttpResponse("Overridden!")
+
+        with self.register_hook("before_bulk_action", hook_func):
+            response = self.client.post(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"Overridden!")
+
+        # Request intercepted before the snippets were deleted
+        self.assertQuerysetEqual(
+            self.snippet_model.objects.filter(
+                pk__in=[snippet.pk for snippet in self.test_snippets]
+            ),
+            self.test_snippets,
+            ordered=False,
+        )
+
+    def test_after_bulk_action_hook(self):
+        def hook_func(request, action_type, instances, action_class_instance):
+            self.assertIsInstance(request, HttpRequest)
+            self.assertEqual(action_type, "delete")
+            self.assertQuerysetEqual(instances, self.test_snippets)
+            self.assertIsInstance(action_class_instance, DeleteBulkAction)
+            return HttpResponse("Overridden!")
+
+        with self.register_hook("after_bulk_action", hook_func):
+            response = self.client.post(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"Overridden!")
+
+        # Request intercepted after the snippets were deleted
+        self.assertFalse(
+            self.snippet_model.objects.filter(
+                pk__in=[snippet.pk for snippet in self.test_snippets]
+            ).exists()
+        )
+
+    # Also tests that the {before,after}_delete_snippet hooks are called.
+    # These hooks have existed since before bulk actions were introduced,
+    # so we need to make sure they still work.
+
+    def test_before_delete_snippet_hook_get(self):
+        def hook_func(request, instances):
+            self.assertIsInstance(request, HttpRequest)
+            self.assertQuerysetEqual(instances, self.test_snippets)
+            return HttpResponse("Overridden!")
+
+        with self.register_hook("before_delete_snippet", hook_func):
+            response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"Overridden!")
+
+        # Request intercepted before the snippets were deleted
+        self.assertQuerysetEqual(
+            self.snippet_model.objects.filter(
+                pk__in=[snippet.pk for snippet in self.test_snippets]
+            ),
+            self.test_snippets,
+            ordered=False,
+        )
+
+    def test_before_delete_snippet_hook_post(self):
+        def hook_func(request, instances):
+            self.assertIsInstance(request, HttpRequest)
+            self.assertQuerysetEqual(instances, self.test_snippets)
+            return HttpResponse("Overridden!")
+
+        with self.register_hook("before_delete_snippet", hook_func):
+            response = self.client.post(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"Overridden!")
+
+        # Request intercepted before the snippets were deleted
+        self.assertQuerysetEqual(
+            self.snippet_model.objects.filter(
+                pk__in=[snippet.pk for snippet in self.test_snippets]
+            ),
+            self.test_snippets,
+            ordered=False,
+        )
+
+    def test_after_delete_snippet_hook(self):
+        def hook_func(request, instances):
+            self.assertIsInstance(request, HttpRequest)
+            self.assertQuerysetEqual(instances, self.test_snippets)
+            return HttpResponse("Overridden!")
+
+        with self.register_hook("after_delete_snippet", hook_func):
+            response = self.client.post(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"Overridden!")
+
+        # Request intercepted after the snippets were deleted
+        self.assertFalse(
+            self.snippet_model.objects.filter(
+                pk__in=[snippet.pk for snippet in self.test_snippets]
+            ).exists()
+        )
