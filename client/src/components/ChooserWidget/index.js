38e39271ee0925a2e23d751b401df4dfa5ec7b80
George Sakkis <george.sakkis@gmail.com>
2023-03-05 11:48:25 +0200

    ChooserWidget: Trigger manually change event when input.value is set
    - Ensure that the change events bubble as this is the default browser behaviour
    - By default, changes to hidden values will not fire a `change` event, nor those that are a result of programatic changes to `value, so instead this needs to be added manually so that other code can listen to changes to chosen fields with DOM event listeners.
    - https://stackoverflow.com/questions/6533087/jquery-detect-value-change-on-hidden-input-field/8965804#8965804
    - Closes #10187

--- client/src/components/ChooserWidget/index.js
+++ client/src/components/ChooserWidget/index.js
@@ -85,11 +85,13 @@ export class Chooser {
 
   renderEmptyState() {
     this.input.setAttribute('value', '');
+    this.input.dispatchEvent(new Event('change', { bubbles: true }));
     this.chooserElement.classList.add('blank');
   }
 
   renderState(newState) {
     this.input.setAttribute('value', newState.id);
+    this.input.dispatchEvent(new Event('change', { bubbles: true }));
     if (this.titleElement && this.titleStateKey) {
       this.titleElement.textContent = newState[this.titleStateKey];
     }
