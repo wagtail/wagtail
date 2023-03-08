f83ba30c4213d9659dffacde0d7d56f473b63158
Matt Westcott <matt@west.co.tt>
2023-03-08 12:22:07 +0000

    Change the "missing core fields in search_fields" test to check against TaggedPage instead of EventPage
    wagtail.admin.tests.pages.test_page_search runs tests against SingleEventPage, and without those core fields in place there's no way to write tests that work on both the fallback backend (which ignores search_fields and only searches on core fields) and the real ones (which respect search_fields).

--- wagtail/test/testapp/models.py
+++ wagtail/test/testapp/models.py
@@ -372,7 +372,7 @@ class EventPage(Page):
     )
     categories = ParentalManyToManyField(EventCategory, blank=True)
 
-    search_fields = [
+    search_fields = Page.search_fields + [
         index.SearchField("get_audience_display"),
         index.SearchField("location"),
         index.SearchField("body"),
@@ -1320,6 +1320,11 @@ class TaggedPage(Page):
     content_panels = [
         FieldPanel("title", classname="title"),
         FieldPanel("tags"),
+    ]
+
+    # Page.search_fields intentionally omitted to test warning
+    search_fields = [
+        index.SearchField("tags"),
     ]
 
 
