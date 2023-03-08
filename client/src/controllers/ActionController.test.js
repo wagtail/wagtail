39fa706ff2aa59b804bae264c2c120e5f663f4eb
Suyash Srivastava <95718084+suyash5053@users.noreply.github.com>
2023-03-09 01:13:27 +0530

    Update ActionController with a click method
    - Partial change towards #10167

--- client/src/controllers/ActionController.test.js
+++ client/src/controllers/ActionController.test.js
@@ -2,8 +2,15 @@ import { Application } from '@hotwired/stimulus';
 import { ActionController } from './ActionController';
 
 describe('ActionController', () => {
-  beforeEach(() => {
-    document.body.innerHTML = `
+  let app;
+
+  afterEach(() => {
+    app?.stop();
+  });
+
+  describe('post method', () => {
+    beforeEach(() => {
+      document.body.innerHTML = `
     <button
       class="button no"
       data-controller="w-action"
@@ -13,20 +20,54 @@ describe('ActionController', () => {
       Enable
     </button>
     `;
-    Application.start().register('w-action', ActionController);
+
+      app = Application.start();
+      app.register('w-action', ActionController);
+    });
+
+    it('it should allow for a form POST with created data', () => {
+      const btn = document.querySelector('[data-controller="w-action"]');
+      const submitMock = jest.fn();
+      window.HTMLFormElement.prototype.submit = submitMock;
+
+      btn.click();
+      const form = document.querySelector('form');
+
+      expect(submitMock).toHaveBeenCalled();
+      expect(form.action).toBe('https://www.github.com/');
+      expect(new FormData(form).get('csrfmiddlewaretoken')).toBe('potato');
+      expect(new FormData(form).get('next')).toBe('http://localhost/');
+    });
   });
 
-  it('it should enable the workflow, lock and Unlock button', () => {
-    const btn = document.querySelector('[data-controller="w-action"]');
-    const submitMock = jest.fn();
-    window.HTMLFormElement.prototype.submit = submitMock;
+  describe('click method', () => {
+    beforeEach(() => {
+      document.body.innerHTML = `
+      <button
+        type="button"
+        id="button"
+        data-controller="w-action"
+        data-action="some-event->w-action#click"
+      >
+        Button
+      </button>
+      `;
 
-    btn.click();
-    const form = document.querySelector('form');
+      app = Application.start();
+      app.register('w-action', ActionController);
+    });
 
-    expect(submitMock).toHaveBeenCalled();
-    expect(form.action).toBe('https://www.github.com/');
-    expect(new FormData(form).get('csrfmiddlewaretoken')).toBe('potato');
-    expect(new FormData(form).get('next')).toBe('http://localhost/');
+    it('should call click method when button is clicked via Stimulus action', () => {
+      const btn = document.getElementById('button');
+      const clickMock = jest.fn();
+      HTMLButtonElement.prototype.click = clickMock;
+
+      btn.addEventListener('some-event', btn.click());
+
+      const event = new CustomEvent('some-event');
+      btn.dispatchEvent(event);
+
+      expect(clickMock).toHaveBeenCalled();
+    });
   });
 });
