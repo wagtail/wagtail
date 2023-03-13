95a444affecee3814dd5ec51894a05b5cb279c10
Matt Westcott <matt@west.co.tt>
2023-03-13 10:09:57 +0000

    Version bump to 4.1.3

--- wagtail/__init__.py
+++ wagtail/__init__.py
@@ -6,7 +6,7 @@ from wagtail.utils.version import get_semver_version, get_version
 
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (4, 1, 2, "final", 1)
+VERSION = (4, 1, 3, "final", 1)
 
 __version__ = get_version(VERSION)
 
