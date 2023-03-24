e8ead77b7a7647988b3fc82ad646ffaba9d7d5e2
Thibaud Colas <thibaudcolas@gmail.com>
2023-03-24 14:58:13 +0000

    Remove dead styles from legacy dropdown

--- client/src/entrypoints/admin/core.js
+++ client/src/entrypoints/admin/core.js
@@ -308,11 +308,8 @@ if (!wagtail.ui) {
 const DROPDOWN_SELECTOR = '[data-dropdown]';
 const LISTING_TITLE_SELECTOR = '[data-listing-page-title]';
 const LISTING_ACTIVE_CLASS = 'listing__item--active';
-const ICON_DOWN = 'icon-arrow-down';
-const ICON_UP = 'icon-arrow-up';
 const IS_OPEN = 'is-open';
 const clickEvent = 'click';
-const TOGGLE_SELECTOR = '[data-dropdown-toggle]';
 const ARIA = 'aria-hidden';
 const keys = {
   ESC: 27,
@@ -426,15 +423,12 @@ DropDown.prototype = {
     e.preventDefault();
     const el = this.el;
     const $parent = this.$parent;
-    const toggle = el.querySelector(TOGGLE_SELECTOR);
 
     this.state.isOpen = true;
     this.registry.closeAllExcept(this);
 
     el.classList.add(IS_OPEN);
     el.setAttribute(ARIA, false);
-    toggle.classList.remove(ICON_DOWN);
-    toggle.classList.add(ICON_UP);
     document.addEventListener(clickEvent, this.clickOutsideDropDown, false);
     $parent.addClass(LISTING_ACTIVE_CLASS);
   },
@@ -444,11 +438,8 @@ DropDown.prototype = {
 
     const el = this.el;
     const $parent = this.$parent;
-    const toggle = el.querySelector(TOGGLE_SELECTOR);
     document.removeEventListener(clickEvent, this.clickOutsideDropDown, false);
     el.classList.remove(IS_OPEN);
-    toggle.classList.add(ICON_DOWN);
-    toggle.classList.remove(ICON_UP);
     el.setAttribute(ARIA, true);
     $parent.removeClass(LISTING_ACTIVE_CLASS);
   },
