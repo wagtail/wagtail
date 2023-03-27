c511a7b5e75964d3a6a97ec43a14845327673050
Sage Abdullah <sage.abdullah@torchbox.com>
2023-03-27 16:05:29 +0100

    Use Stimulus target for preview panel mode select

--- client/src/controllers/PreviewController.ts
+++ client/src/controllers/PreviewController.ts
@@ -93,13 +93,15 @@ const runAccessibilityChecks = async (
  * update the preview iframe if the form is valid.
  */
 export class PreviewController extends Controller<HTMLElement> {
-  static targets = ['size', 'newTab', 'spinner', 'refresh'];
+  static targets = ['size', 'newTab', 'spinner', 'refresh', 'mode'];
 
   declare readonly sizeTargets: HTMLInputElement[];
   declare readonly newTabTarget: HTMLAnchorElement;
   declare readonly spinnerTarget: HTMLDivElement;
   declare readonly hasRefreshTarget: boolean;
   declare readonly refreshTarget: HTMLButtonElement;
+  declare readonly hasModeTarget: boolean;
+  declare readonly modeTarget: HTMLSelectElement;
 
   connect() {
     const previewSidePanel = document.querySelector(
@@ -187,9 +189,6 @@ export class PreviewController extends Controller<HTMLElement> {
       '[data-edit-form]',
     ) as HTMLFormElement;
     const previewUrl = previewPanel.dataset.action as string;
-    const previewModeSelect = document.querySelector<HTMLSelectElement>(
-      '[data-preview-mode-select]',
-    );
     let iframe = previewPanel.querySelector<HTMLIFrameElement>(
       '[data-preview-iframe]',
     ) as HTMLIFrameElement;
@@ -210,8 +209,8 @@ export class PreviewController extends Controller<HTMLElement> {
       // Create a new invisible iframe element
       const newIframe = document.createElement('iframe');
       const url = new URL(previewUrl, window.location.href);
-      if (previewModeSelect) {
-        url.searchParams.set('mode', previewModeSelect.value);
+      if (this.hasModeTarget) {
+        url.searchParams.set('mode', this.modeTarget.value);
       }
       url.searchParams.set('in_preview_panel', 'true');
       newIframe.style.width = '0';
@@ -430,8 +429,8 @@ export class PreviewController extends Controller<HTMLElement> {
       handlePreview();
     };
 
-    if (previewModeSelect) {
-      previewModeSelect.addEventListener('change', handlePreviewModeChange);
+    if (this.hasModeTarget) {
+      this.modeTarget.addEventListener('change', handlePreviewModeChange);
     }
 
     // Remember last selected device size
