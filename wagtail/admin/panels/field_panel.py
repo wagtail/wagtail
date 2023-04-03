12364c15ee9dc5d49f5e81141334c4b295ef547a
Andy Babic <andy.babic@torchbox.com>
2023-04-03 10:26:14 +0100

    Add read_only option support to FieldPanel

--- wagtail/admin/panels/field_panel.py
+++ wagtail/admin/panels/field_panel.py
@@ -1,7 +1,9 @@
 import functools
 
 from django.core.exceptions import FieldDoesNotExist, ImproperlyConfigured
+from django.template.loader import get_template
 from django.utils.functional import cached_property
+from django.utils.text import capfirst
 
 from wagtail.admin import compare
 from wagtail.blocks import BlockField
@@ -11,15 +13,23 @@ from .base import Panel
 
 class FieldPanel(Panel):
     TEMPLATE_VAR = "field_panel"
+    read_only_output_template_name = "wagtailadmin/panels/read_only_output.html"
 
     def __init__(
-        self, field_name, widget=None, disable_comments=None, permission=None, **kwargs
+        self,
+        field_name,
+        widget=None,
+        disable_comments=None,
+        permission=None,
+        read_only=False,
+        **kwargs,
     ):
         super().__init__(**kwargs)
         self.field_name = field_name
         self.widget = widget
         self.disable_comments = disable_comments
         self.permission = permission
+        self.read_only = read_only
 
     def clone_kwargs(self):
         kwargs = super().clone_kwargs()
@@ -28,10 +38,14 @@ class FieldPanel(Panel):
             widget=self.widget,
             disable_comments=self.disable_comments,
             permission=self.permission,
+            read_only=self.read_only,
         )
         return kwargs
 
     def get_form_options(self):
+        if self.read_only:
+            return {}
+
         opts = {
             "fields": [self.field_name],
         }
@@ -93,16 +107,28 @@ class FieldPanel(Panel):
         def __init__(self, **kwargs):
             super().__init__(**kwargs)
 
+            self.bound_field = None
+            self.read_only = False
+
             if self.form is None:
-                self.bound_field = None
                 return
 
             try:
                 self.bound_field = self.form[self.field_name]
             except KeyError:
-                self.bound_field = None
+                if self.panel.read_only:
+                    self.read_only = True
+                    # Ensure heading and help_text are set to something useful
+                    self.heading = self.panel.heading or capfirst(
+                        self.panel.db_field.verbose_name
+                    )
+                    self.help_text = self.panel.help_text or capfirst(
+                        self.panel.db_field.help_text
+                    )
                 return
 
+            # Ensure heading and help_text are consistant accross
+            # Panel, BoundPanel and Field
             if self.panel.heading:
                 self.heading = self.bound_field.label = self.panel.heading
             else:
@@ -115,7 +141,11 @@ class FieldPanel(Panel):
             return self.panel.field_name
 
         def is_shown(self):
-            if self.form is not None and self.bound_field is None:
+            if (
+                self.form is not None
+                and self.bound_field is None
+                and not self.read_only
+            ):
                 # this field is missing from the form
                 return False
 
@@ -129,19 +159,29 @@ class FieldPanel(Panel):
             return True
 
         def is_required(self):
+            if self.bound_field is None:
+                return False
             return self.bound_field.field.required
 
         def classes(self):
-            is_streamfield = isinstance(self.bound_field.field, BlockField)
-            extra_classes = ["w-panel--nested"] if is_streamfield else []
-
-            return self.panel.classes() + extra_classes
+            classes = self.panel.classes()
+            if self.bound_field and isinstance(self.bound_field.field, BlockField):
+                classes.append("w-panel--nested")
+            return classes
 
         @property
         def icon(self):
             """
             Display a different icon depending on the field's type.
             """
+            if self.panel.icon:
+                return self.panel.icon
+
+            if self.bound_field is None:
+                form_field = self.panel.db_field.formfield()
+            else:
+                form_field = self.bound_field.field
+
             field_icons = {
                 # Icons previously-defined as StreamField block icons.
                 # Commented out until they can be reviewed for appropriateness in this new context.
@@ -157,30 +197,41 @@ class FieldPanel(Panel):
                 # "RegexField": "code",
                 # "BooleanField": "tick-inverse",
             }
-            field_type = self.bound_field.field.__class__.__name__
-
-            return self.panel.icon or field_icons.get(field_type, None)
+            return field_icons.get(form_field.__class__.__name__, None)
 
         def id_for_label(self):
+            if self.read_only:
+                return self.prefix
             return self.bound_field.id_for_label
 
         @property
         def comments_enabled(self):
-            if self.panel.disable_comments is None:
+            if self.panel.disable_comments is None and not self.read_only:
                 # by default, enable comments on all fields except StreamField (which has its own comment handling)
                 return not isinstance(self.bound_field.field, BlockField)
             else:
                 return not self.panel.disable_comments
 
+        @cached_property
+        def value_from_instance(self):
+            return getattr(self.instance, self.field_name)
+
         def get_context_data(self, parent_context=None):
             context = super().get_context_data(parent_context)
+            if self.read_only:
+                context.update(self.get_read_only_context_data())
+            else:
+                context.update(self.get_editable_context_data())
+            return context
+
+        def get_editable_context_data(self):
 
             widget_described_by_ids = []
-            help_text = self.bound_field.help_text
             help_text_id = "%s-helptext" % self.prefix
             error_message_id = "%s-errors" % self.prefix
 
-            if help_text:
+            widget_described_by_ids = []
+            if self.help_text:
                 widget_described_by_ids.append(help_text_id)
 
             if self.bound_field.errors:
@@ -215,19 +266,39 @@ class FieldPanel(Panel):
 
                 rendered_field = self.bound_field.as_widget(attrs=widget_attrs)
 
-            context.update(
-                {
-                    "field": self.bound_field,
-                    "rendered_field": rendered_field,
-                    "help_text": help_text,
-                    "help_text_id": help_text_id,
-                    "error_message_id": error_message_id,
-                    "show_add_comment_button": self.comments_enabled
-                    and getattr(
-                        self.bound_field.field.widget, "show_add_comment_button", True
-                    ),
-                }
-            )
+            return {
+                "field": self.bound_field,
+                "rendered_field": rendered_field,
+                "error_message_id": error_message_id,
+                "help_text": self.help_text,
+                "help_text_id": help_text_id,
+                "show_add_comment_button": self.comments_enabled
+                and getattr(
+                    self.bound_field.field.widget,
+                    "show_add_comment_button",
+                    True,
+                ),
+            }
+
+        def get_read_only_context_data(self):
+            # Define context data for BoundPanel AND read-only output rendering
+            context = {
+                "id_for_label": self.id_for_label(),
+                "help_text_id": "%s-helptext" % self.prefix,
+                "help_text": self.help_text,
+                "show_add_comment_button": self.comments_enabled,
+                "raw_value": self.value_from_instance,
+                "display_value": self.panel.format_value_for_display(
+                    self.value_from_instance
+                ),
+            }
+
+            # Render read-only output
+            template = get_template(self.panel.read_only_output_template_name)
+            rendered_field = template.render(context)
+
+            # Add rendered output to BoundPanel context data
+            context["rendered_field"] = rendered_field
             return context
 
         def get_comparison(self):
