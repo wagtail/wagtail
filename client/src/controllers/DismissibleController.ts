270475f32a9e2a3941b2e057bbd2154e45538716
Lovelyfin00 <omokaroloveth10@gmail.com>
2023-04-06 11:08:00 +0100

    Migrated initDismissibles to a Stimulus controller w-dismissible
    - Closes #10166

--- client/src/controllers/DismissibleController.ts
+++ client/src/controllers/DismissibleController.ts
@@ -1,13 +1,12 @@
+import { Controller } from '@hotwired/stimulus';
 import { WAGTAIL_CONFIG } from '../config/wagtailConfig';
 
 /**
  * Updates the server, using a PATCH request when the toggle is clicked on a dismissible
- * element initialised by initDismissibles
+ * element initialised by DismissibleController
  *
  * @param data - The dismissible represented as an object with keys as
  * the id and its new state: whether it is dismissed (boolean)
- *
- * @return {Promise<Response>}
  *
  * @example
  * const data = { 'dismissible-1': true, 'dismissible-2': false };
@@ -15,51 +14,55 @@ import { WAGTAIL_CONFIG } from '../config/wagtailConfig';
  *
  * updateDismissibles(data, wagtailConfig);
  */
-export function updateDismissibles(
+export const updateDismissibles = (
   data: Record<string, boolean>,
-): Promise<Response> {
-  return fetch(WAGTAIL_CONFIG.ADMIN_URLS?.DISMISSIBLES, {
+): Promise<Response> =>
+  fetch(WAGTAIL_CONFIG.ADMIN_URLS?.DISMISSIBLES, {
     method: 'PATCH',
     headers: {
       [WAGTAIL_CONFIG.CSRF_HEADER_NAME]: WAGTAIL_CONFIG.CSRF_TOKEN,
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json', // eslint-disable-line @typescript-eslint/naming-convention
     },
     body: JSON.stringify(data),
     mode: 'same-origin',
   });
-}
 
 /**
- * Initialise dismissibles fetched from server and add click event listeners to them.
- * @return {void}
+ * Adds the ability to make an element dismissible so that it updates it's class and makes an async request.
+ * Initialise such elements with a default handler that performs the dismissal.
+ * This only initialises elements that are rendered by the server (if they have the data attr), so elements
+ * that are rendered by the client (e.g. React) needs to be handled separately.
+ *
+ * @example
+ * <section
+ *  data-controller="w-dismissible"
+ *  data-w-dismissible-dismissed-class="w-dismissible--dismissed"
+ *  data-w-dismissible-id-value="Whats new in Wagtail"
+ * >
+ *  <button type="button" data-action="w-dismiss#dismissible">Close</button>
+ * </section>
  */
-export function initDismissibles(): void {
-  // A dismissible element is marked by the data-wagtail-dismissible-id attribute.
-  const dismissibles = document.querySelectorAll<HTMLElement>(
-    '[data-wagtail-dismissible-id]',
-  );
+export class DismissibleController extends Controller<HTMLElement> {
+  static classes = ['dismissed'];
 
-  // Initialise such elements with a default handler that performs the dismissal.
-  // This only initialises elements that are rendered by the server, so elements
-  // that are rendered by the client (e.g. React) needs to be handled separately.
-  dismissibles.forEach((dismissible: HTMLElement) => {
-    // The toggle is marked by the data-wagtail-dismissible-toggle attribute,
-    // which can either be the dismissible itself or a descendant element.
-    const toggle = dismissible.hasAttribute('data-wagtail-dismissible-toggle')
-      ? dismissible
-      : dismissible.querySelector<HTMLElement>(
-          '[data-wagtail-dismissible-toggle]',
-        );
-    const id = dismissible.dataset.wagtailDismissibleId;
-    if (!(toggle && id)) return;
+  static values = {
+    dismissed: { default: false, type: Boolean },
+    id: { default: '', type: String },
+  };
 
-    // Upon clicking the toggle, send an update to the server and add the
-    // appropriate class and data attribute optimistically. Each dismissible
-    // defines how it uses (or not) these indicators.
-    toggle.addEventListener('click', () => {
-      updateDismissibles({ [id]: true });
-      dismissible.classList.add('w-dismissible--dismissed');
-      dismissible.setAttribute('data-wagtail-dismissed', '');
-    });
-  });
+  declare dismissedValue: boolean;
+  declare idValue: string;
+  declare readonly dismissedClass: string;
+
+  /**
+   * Upon activating the toggle, send an update to the server and add the
+   * appropriate class and data attribute optimistically. Each dismissible
+   * defines how it uses (or not) these indicators.
+   */
+  toggle(): void {
+    if (!this.idValue) return;
+    this.element.classList.add(this.dismissedClass);
+    this.dismissedValue = true;
+    updateDismissibles({ [this.idValue]: true });
+  }
 }
