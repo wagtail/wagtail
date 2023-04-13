3aff35d6cc257b8f10596f251f99e6f74b883a92
Hussain Saherwala <hussainsaherwala2005@gmail.com>
2023-04-13 14:37:32 +0530

    Stimulus code added to message controller

--- client/src/controllers/MessagesController.ts
+++ client/src/controllers/MessagesController.ts
@@ -1,29 +1,17 @@
+
 import { Controller } from '@hotwired/stimulus';
 
-/**
- * Adds the ability for a controlled element to pick an element from a template
- * and then clone that element, adding it to the container.
- * Additionally, it will allow for clearing all previously added elements.
- *
- * @example
- * <div
- *   data-controller="w-messages"
- *   data-action="w-messages:add@document->w-messages#add"
- *   data-w-messages-added-class="new"
- *   data-w-messages-show-class="appear"
- * >
- *   <ul data-w-messages-target="container"></ul>
- *   <template data-w-messages-target="template">
- *     <li data-message-status="error-or-success"><span></span></li>
- *  </template>
- * </div>
- */
-export class MessagesController extends Controller<HTMLElement> {
-  static classes = ['added', 'show'];
-  static targets = ['container', 'template'];
-  static values = {
-    showDelay: { default: 100, type: Number },
-  };
+declare global {
+  interface Window {
+    handleUnsavedEvent: (event: CustomEvent<{
+      formDirty: boolean;
+      commentsDirty: boolean;
+    }>) => void;
+  }
+}
+export default class extends Controller {
+  static targets = ["container", "template"];
+  static values = { clearDelay: Number ,clearDelayValue: Number};
 
   declare readonly addedClass: string;
   declare readonly containerTarget: HTMLElement;
@@ -35,56 +23,99 @@ export class MessagesController extends Controller<HTMLElement> {
   declare readonly templateTargets: HTMLTemplateElement[];
 
   declare showDelayValue: number;
+  declare element: HTMLElement;
+  declare clearDelayValue: number;
 
-  add(
-    event?: CustomEvent<{
-      /** Flag for clearing or stacking messages */
-      clear?: boolean;
-      /** Content for the message, HTML not supported. */
-      text?: string;
-      /** Message status level, based on Django's message types. */
-      type?: 'success' | 'error' | 'warning' | string;
-    }>,
-  ) {
-    const { clear = false, text = '', type } = event?.detail || {};
-
-    if (this.hasAddedClass) {
-      this.element.classList.add(this.addedClass);
-    }
-
-    if (clear) this.clear();
-
-    /** if no type provided, return the first template target, otherwise try to find
-     * a matching target, finally fall back on the first template target if nothing
-     * is found.
-     */
-    const template =
-      (type &&
-        this.templateTargets.find(({ dataset }) => dataset.type === type)) ||
-      this.templateTarget;
-
-    const content = template.content.firstElementChild?.cloneNode(true);
-
-    if (content instanceof HTMLElement) {
-      const textElement = content.lastElementChild;
-
-      if (textElement instanceof HTMLElement && text) {
-        textElement.textContent = text;
-      }
-
-      this.containerTarget.appendChild(content);
-
-      this.dispatch('added');
-
-      if (this.hasShowClass) {
-        setTimeout(() => {
-          this.element.classList.add(this.showClass);
-        }, this.showDelayValue);
-      }
-    }
+  static afterLoad() {
+    window.handleUnsavedEvent = (event) => {
+      const { formDirty, commentsDirty } = event.detail;
+      const anyDirty = formDirty || commentsDirty;
+      const allDirty = formDirty && commentsDirty;
+      const type = allDirty ? "all" : commentsDirty ? "comments" : "edits";
+      const detail = { type };
+      const eventName = anyDirty ? "w-unsaved:show" : "w-unsaved:hide";
+      document.dispatchEvent(new CustomEvent(eventName, { detail }));
+    };
   }
 
-  clear() {
-    this.containerTarget.innerHTML = '';
+add(
+  event?: CustomEvent<{
+    /** Flag for clearing or stacking messages */
+    clear?: boolean;
+    /** Content for the message, HTML not supported. */
+    text?: string;
+    /** Message status level, based on Django's message types. */
+    type?: 'success' | 'error' | 'warning' | string;
+  }>
+) {
+  const { clear = false, text = '', type } = event?.detail || {};
+
+  if (this.hasAddedClass) {
+    this.element!.classList.add(this.addedClass);
+  }
+
+  if (clear) {
+    setTimeout(() => {
+      this.clear().then(() => {
+        this.add(event);
+      });
+    }, this.clearDelayValue);
+    return;
+  }
+
+  const template =
+    (type &&
+      this.templateTargets.find(({ dataset }) => dataset.type === type)) ||
+    this.templateTarget;
+
+  const content = template.content.firstElementChild?.cloneNode(true);
+
+  if (content instanceof HTMLElement) {
+    const textElement = content.lastElementChild;
+
+    if (textElement instanceof HTMLElement && text) {
+      textElement.textContent = text;
+    }
+
+    this.containerTarget.appendChild(content);
+
+    this.dispatch('added');
+
+    if (this.hasShowClass) {
+      setTimeout(() => {
+        this.element.classList.add(this.showClass);
+      }, this.showDelayValue);
+    }
   }
 }
+
+
+  clear() {
+    return new Promise((resolve) => {
+      this.containerTarget.classList.remove("footer__container--visible");
+      setTimeout(() => {
+        this.containerTarget.innerHTML = "";
+        this.containerTarget.hidden = true;
+        resolve(undefined);
+      }, this.clearDelayValue);
+    });
+  }
+
+  show() {
+    this.containerTarget.hidden = false;
+    this.containerTarget.classList.add("footer__container--visible");
+  }
+
+  hide() {
+    this.containerTarget.classList.remove("footer__container--visible");
+    this.containerTarget.hidden = true;
+  }
+
+  dispatch(eventName: string, detail: any = {}) {
+    const customEvent = new CustomEvent(eventName, {
+      bubbles: true,
+      detail,
+    });
+
+    this.element.dispatchEvent(customEvent);
+  }}
