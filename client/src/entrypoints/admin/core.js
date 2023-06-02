887f89c626b9a63558c6cdadc0945d8da760c76b
LB Johnston <mail@lb.ee>
2023-06-02 19:24:46 +1000

    Refactor Stimulus util & module export
    - Accessing constructor was confusing and createController is not a core requirement
    - Avoid modifying the base Stimulus application class
    - Expose `window.StimulusModule` as the module output
    - Expose `window.wagtail.app` as the Wagtail Stimulus application instance
    - Rename root element variable to `root` in initStimulus (so we do not conflict with potential action option registration that uses `element` variable names)
    - Pull in the wagtail.components to the same core.js file and add JSDoc for exposed global
    - Relates to #10197

--- client/src/entrypoints/admin/core.js
+++ client/src/entrypoints/admin/core.js
@@ -1,11 +1,34 @@
 import $ from 'jquery';
+import * as StimulusModule from '@hotwired/stimulus';
 
+import { Icon, Portal } from '../..';
 import { coreControllerDefinitions } from '../../controllers';
 import { escapeHtml } from '../../utils/text';
 import { initStimulus } from '../../includes/initStimulus';
 
-/** initialise Wagtail Stimulus application with core controller definitions */
-window.Stimulus = initStimulus({ definitions: coreControllerDefinitions });
+/** Expose a global to allow for customisations and packages to build with Stimulus. */
+window.StimulusModule = StimulusModule;
+
+/**
+ * Wagtail global module, useful for debugging and as the exposed
+ * interface to access the Stimulus application instance and base
+ * React components.
+ *
+ * @type {Object} wagtail
+ * @property {Object} app - Wagtail's Stimulus application instance.
+ * @property {Object} components - Exposed components as globals for third-party reuse.
+ * @property {Object} components.Icon - Icon React component.
+ * @property {Object} components.Portal - Portal React component.
+ */
+const wagtail = window.wagtail || {};
+
+/** Initialise Wagtail Stimulus application with core controller definitions. */
+wagtail.app = initStimulus({ definitions: coreControllerDefinitions });
+
+/** Expose components as globals for third-party reuse. */
+wagtail.components = { Icon, Portal };
+
+window.wagtail = wagtail;
 
 window.escapeHtml = escapeHtml;
 
@@ -225,11 +248,3 @@ $(() => {
       $(this).removeClass('hovered');
     });
 });
-
-// =============================================================================
-// Wagtail global module, mainly useful for debugging.
-// =============================================================================
-
-const wagtail = window.wagtail || {};
-
-window.wagtail = wagtail;
