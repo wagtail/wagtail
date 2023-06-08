d073860dc2541d10d28391c2e33f60f6626c55d9
Matt Westcott <matt@west.co.tt>
2023-06-08 23:23:34 +0100

    Use results endpoints for user index search, instead of switching on x-requested-with

--- wagtail/users/urls/users.py
+++ wagtail/users/urls/users.py
@@ -5,6 +5,7 @@ from wagtail.users.views import users
 app_name = "wagtailusers_users"
 urlpatterns = [
     path("", users.Index.as_view(), name="index"),
+    path("results/", users.Index.as_view(results_only=True), name="index_results"),
     path("add/", users.Create.as_view(), name="add"),
     path("<str:pk>/", users.Edit.as_view(), name="edit"),
     path("<str:pk>/delete/", users.Delete.as_view(), name="delete"),
