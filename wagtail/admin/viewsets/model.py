e54a2f568a75ebbae6a413d43b45af97931ad264
Matt Westcott <matt@west.co.tt>
2023-06-08 22:39:19 +0100

    Set up search-as-you-type as a standard feature of generic IndexView

--- wagtail/admin/viewsets/model.py
+++ wagtail/admin/viewsets/model.py
@@ -40,6 +40,7 @@ class ModelViewSet(ViewSet):
             "model": self.model,
             "permission_policy": self.permission_policy,
             "index_url_name": self.get_url_name("index"),
+            "index_results_url_name": self.get_url_name("index_results"),
             "add_url_name": self.get_url_name("add"),
             "edit_url_name": self.get_url_name("edit"),
             "header_icon": self.icon,
