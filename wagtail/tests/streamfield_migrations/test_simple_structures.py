f2886cc647d4eac7503bd7a5ba79c750359ed41f
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2023-06-21 12:44:38 +0530

    Fix empty blocks created in migration operations

--- wagtail/tests/streamfield_migrations/test_simple_structures.py
+++ wagtail/tests/streamfield_migrations/test_simple_structures.py
@@ -107,6 +107,27 @@ class FieldChildBlockTest(TestCase):
             altered_raw_data[2]["value"][1]["value"], self.raw_data[2]["value"]
         )
 
+    def test_combine_to_listblock_no_existing_children(self):
+        """Combine all `simplestruct` blocks into a new ListBlock named `list1`
+        We have no `simplestruct` blocks in our existing data, so there should be no list1 blocks
+        created and the data should be intact.
+        """
+
+        altered_raw_data = apply_changes_to_raw_data(
+            raw_data=self.raw_data,
+            block_path_str="",
+            operation=StreamChildrenToListBlockOperation(
+                block_name="simplestruct", list_block_name="list1"
+            ),
+            streamfield=models.SampleModel.content,
+        )
+
+        self.assertEqual(len(altered_raw_data), 4)
+        self.assertEqual(altered_raw_data[0], self.raw_data[0])
+        self.assertEqual(altered_raw_data[1], self.raw_data[1])
+        self.assertEqual(altered_raw_data[2], self.raw_data[2])
+        self.assertEqual(altered_raw_data[3], self.raw_data[3])
+
     def test_combine_single_type_to_streamblock(self):
         """Combine all `char1` blocks as children of a new StreamBlock named `stream1`
 
@@ -165,6 +186,27 @@ class FieldChildBlockTest(TestCase):
         self.assertEqual(altered_raw_data[0]["value"][1], self.raw_data[1])
         self.assertEqual(altered_raw_data[0]["value"][2], self.raw_data[2])
         self.assertEqual(altered_raw_data[0]["value"][3], self.raw_data[3])
+
+    def test_combine_to_streamblock_no_existing_children(self):
+        """Combine all `simplestruct` blocks as children of a new StreamBlock named `stream1`
+        We have no `simplestruct` blocks in our existing data, so there should be no stream1 blocks
+        created and the data should be intact.
+        """
+
+        altered_raw_data = apply_changes_to_raw_data(
+            raw_data=self.raw_data,
+            block_path_str="",
+            operation=StreamChildrenToStreamBlockOperation(
+                block_names=["simplestruct"], stream_block_name="stream1"
+            ),
+            streamfield=models.SampleModel.content,
+        )
+
+        self.assertEqual(len(altered_raw_data), 4)
+        self.assertEqual(altered_raw_data[0], self.raw_data[0])
+        self.assertEqual(altered_raw_data[1], self.raw_data[1])
+        self.assertEqual(altered_raw_data[2], self.raw_data[2])
+        self.assertEqual(altered_raw_data[3], self.raw_data[3])
 
     def test_to_structblock(self):
         """Move each `char1` block inside a new StructBlock named `struct1`
