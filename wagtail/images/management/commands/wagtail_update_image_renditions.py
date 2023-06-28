3aee7dbf0d1d17fdc63f90188279bc3d31e51d00
Jake Howard <jake.howard@torchbox.com>
2023-06-28 15:08:11 +0100

    Remove duplication around operating on images and improve error reporting
    Now, if an image fails, the process will continue

--- wagtail/images/management/commands/wagtail_update_image_renditions.py
+++ wagtail/images/management/commands/wagtail_update_image_renditions.py
@@ -1,7 +1,11 @@
+import logging
+
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
 from wagtail.images import get_image_model
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -15,37 +19,41 @@ class Command(BaseCommand):
             action="store_true",
             help="Purge all image renditions without regenerating them",
         )
+        parser.add_argument(
+            "--chunk-size",
+            type=int,
+            default=50,
+            help="Operate in x size chunks (default: %(default)s)",
+        )
 
     def handle(self, *args, **options):
         Rendition = get_image_model().get_rendition_model()
 
         renditions = Rendition.objects.all()
 
+        purge_only = options["purge_only"]
+
         if not renditions.exists():
             self.stdout.write("No image renditions found.")
             return
 
-        if options["purge_only"]:
-            rendition_count = renditions.count()
-            renditions.delete()
+        rendition_ids = list(renditions.values_list("id", flat=True))
+
+        if purge_only:
             self.stdout.write(
-                self.style.SUCCESS(
-                    f"Successfully purged {rendition_count} image rendition(s)"
-                )
+                self.style.HTTP_INFO(f"Purging {len(rendition_ids)} rendition(s)")
             )
         else:
             self.stdout.write(
-                self.style.HTTP_INFO(f"Regenerating {renditions.count()} rendition(s)")
+                self.style.HTTP_INFO(f"Regenerating {len(rendition_ids)} rendition(s)")
             )
-            # Pre-calculate the ids of the renditions to change, otherwise `.iterator` never
-            # ends.
-            rendition_ids = list(renditions.values_list("id", flat=True))
 
-            for rendition in (
-                renditions.filter(id__in=rendition_ids)
-                .select_related("image")
-                .iterator(chunk_size=10)
-            ):
+        for rendition in (
+            renditions.filter(id__in=rendition_ids)
+            .select_related("image")
+            .iterator(chunk_size=options["chunk_size"])
+        ):
+            try:
                 with transaction.atomic():
                     rendition_filter = rendition.filter
                     rendition_image = rendition.image
@@ -53,10 +61,13 @@ class Command(BaseCommand):
                     # Delete the existing rendition
                     rendition.delete()
 
-                    # Create a new one
-                    rendition_image.get_rendition(rendition_filter)
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f"Successfully regenerated {len(rendition_ids)} image rendition(s)"
+                    if not purge_only:
+                        # Create a new one
+                        rendition_image.get_rendition(rendition_filter)
+            except:  # noqa:E722
+                logger.exception("Error operating on rendition %d", rendition.id)
+                self.stderr.write(
+                    self.style.ERROR(f"Failed to operate on rendition {rendition.id}")
                 )
-            )
+
+        self.stdout.write(self.style.SUCCESS("Success"))
