85bf7ba49437bdd784376cbf68437f3de9b5bfbe
Matt Westcott <matt@west.co.tt>
2023-06-28 01:37:00 +0100

    Use table component for page search results

--- wagtail/admin/ui/tables/pages.py
+++ wagtail/admin/ui/tables/pages.py
@@ -1,14 +1,22 @@
+from django.utils.safestring import mark_safe
+from django.utils.translation import gettext
+
 from wagtail.admin.ui.tables import BaseColumn, Column
 
 
 class PageTitleColumn(BaseColumn):
     cell_template_name = "wagtailadmin/pages/listing/_page_title_cell.html"
 
+    def __init__(self, *args, show_locale_labels=False, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.show_locale_labels = show_locale_labels
+
     def get_cell_context_data(self, instance, parent_context):
         context = super().get_cell_context_data(instance, parent_context)
         context["page_perms"] = instance.permissions_for_user(
             parent_context["request"].user
         )
+        context["show_locale_labels"] = self.show_locale_labels
         return context
 
 
@@ -21,3 +29,44 @@ class ParentPageColumn(Column):
 
 class PageStatusColumn(BaseColumn):
     cell_template_name = "wagtailadmin/pages/listing/_page_status_cell.html"
+
+
+class BulkActionsColumn(BaseColumn):
+    header_template_name = "wagtailadmin/bulk_actions/select_all_checkbox_cell.html"
+    cell_template_name = "wagtailadmin/bulk_actions/listing_checkbox_cell.html"
+
+    def get_header_context_data(self, parent_context):
+        context = super().get_header_context_data(parent_context)
+        parent_page = parent_context.get("parent_page")
+        if parent_page:
+            context["parent"] = parent_page.id
+        return context
+
+    def get_cell_context_data(self, instance, parent_context):
+        context = super().get_cell_context_data(instance, parent_context)
+        context.update(
+            {
+                "obj_type": "page",
+                "obj": instance,
+                "aria_labelledby_prefix": "page_",
+                "aria_labelledby": str(instance.pk),
+                "aria_labelledby_suffix": "_title",
+                "checkbox_aria_label": gettext("Select page"),
+            }
+        )
+        return context
+
+
+class NavigateToChildrenColumn(BaseColumn):
+    cell_template_name = "wagtailadmin/pages/listing/_navigation_explore.html"
+
+    def get_cell_context_data(self, instance, parent_context):
+        context = super().get_cell_context_data(instance, parent_context)
+        context["page"] = instance
+        context["page_perms"] = instance.permissions_for_user(
+            parent_context["request"].user
+        )
+        return context
+
+    def render_header_html(self, parent_context):
+        return mark_safe("<th></th>")
