13c35a8780307e970ac8bb1c2856a72fc11eb093
Matt Westcott <matt@west.co.tt>
2023-07-14 18:37:05 +0100

    Override construction of the host config list on Elasticsearch 8
    This allows us to omit the verify_certs and http_auth options, which are no longer valid

--- wagtail/search/backends/elasticsearch8.py
+++ wagtail/search/backends/elasticsearch8.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ImproperlyConfigured
+
 from wagtail.search.backends.elasticsearch7 import (
     Elasticsearch7AutocompleteQueryCompiler,
     Elasticsearch7Index,
@@ -34,6 +36,37 @@ class Elasticsearch8SearchBackend(Elasticsearch7SearchBackend):
     query_compiler_class = Elasticsearch8SearchQueryCompiler
     autocomplete_query_compiler_class = Elasticsearch8AutocompleteQueryCompiler
     results_class = Elasticsearch8SearchResults
+
+    def _get_host_config_from_url(self, url):
+        """Given a parsed URL, return the host configuration to be added to self.hosts"""
+        use_ssl = url.scheme == "https"
+        port = url.port or (443 if use_ssl else 80)
+
+        # the verify_certs and http_auth options are no longer valid in Elasticsearch 8
+        return {
+            "host": url.hostname,
+            "port": port,
+            "url_prefix": url.path,
+            "use_ssl": use_ssl,
+        }
+
+    def _get_options_from_host_urls(self, urls):
+        """Given a list of parsed URLs, return a dict of additional options to be passed into the
+        Elasticsearch constructor; necessary for options that aren't valid as part of the 'hosts' config"""
+        opts = super()._get_options_from_host_urls(urls)
+
+        basic_auth = (urls[0].username, urls[0].password)
+        # Ensure that all urls have the same credentials
+        if any((url.username, url.password) != basic_auth for url in urls):
+            raise ImproperlyConfigured(
+                "Elasticsearch host configuration is invalid. "
+                "Elasticsearch 8 does not support multiple hosts with differing authentication credentials."
+            )
+
+        if basic_auth != (None, None):
+            opts["basic_auth"] = basic_auth
+
+        return opts
 
 
 SearchBackend = Elasticsearch8SearchBackend
