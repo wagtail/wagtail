143e45d474ba394454bc04865d4028bfffb4ffa7
LB Johnston <mail@lb.ee>
2023-07-19 18:39:54 +1000

    Update Sphinx Wagtail Theme to v6.1.1 (#10699)

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ documentation_extras = [
     "sphinxcontrib-spelling>=5.4.0,<6",
     "Sphinx>=1.5.2",
     "sphinx-autobuild>=0.6.0",
-    "sphinx-wagtail-theme==6.0.0",
+    "sphinx-wagtail-theme==6.1.1",
     "myst_parser==0.18.1",
     "sphinx_copybutton>=0.5,<1.0",
 ]
