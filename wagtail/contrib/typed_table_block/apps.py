b0d69cf5f38e7b670ec1dc8944c25e283c30dbc1
Sage Abdullah <sage.abdullah@torchbox.com>
2023-08-16 15:16:22 +0100

    Add apps.py to wagtail.contrib.typed_table_block

---
+++ wagtail/contrib/typed_table_block/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class WagtailTableBlockAppConfig(AppConfig):
+    name = "wagtail.contrib.typed_table_block"
+    label = "wagtailtypedtableblock"
+    verbose_name = _("Wagtail typed table block")
