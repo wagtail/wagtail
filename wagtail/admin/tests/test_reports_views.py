79dfbb780ee7d947b6ef50277512b99ab64aca2e
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2023-09-01 10:47:21 -0300

    Add tests for the report

--- wagtail/admin/tests/test_reports_views.py
+++ wagtail/admin/tests/test_reports_views.py
@@ -5,14 +5,22 @@ from django.conf import settings
 from django.conf.locale import LANG_INFO
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
 from django.test.utils import override_settings
 from django.urls import reverse
 from django.utils import timezone, translation
 from openpyxl import load_workbook
 
 from wagtail.admin.views.mixins import ExcelDateFormatter
-from wagtail.models import GroupPagePermission, ModelLogEntry, Page, PageLogEntry
+from wagtail.admin.views.reports import page_types
+from wagtail.models import (
+    GroupPagePermission,
+    Locale,
+    ModelLogEntry,
+    Page,
+    PageLogEntry,
+)
+from wagtail.test.testapp.models import EventPage, EventPageSpeaker, SimplePage
 from wagtail.test.utils import WagtailTestUtils
 
 
@@ -529,3 +537,195 @@ class TestFilteredAgingPagesView(WagtailTestUtils, TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.aboutus_page.title)
         self.assertNotContains(response, self.home_page.title)
+
+
+class PageTypesReportViewTest(WagtailTestUtils, TestCase):
+    fixtures = ["test.json"]
+
+    def setUp(self):
+        self.user = self.login()
+
+    def get(self, params={}):
+        return self.client.get(reverse("wagtailadmin_reports:page_types"), params)
+
+    def test_simple(self):
+        response = self.get()
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "wagtailadmin/reports/page_types.html")
+
+    def test_displays_only_page_types(self):
+        """Asserts that the correct models are included in the queryset."""
+        response = self.get()
+        # Assert that the response contains page models:
+        self.assertContains(
+            response, ContentType.objects.get_for_model(Page).name.title()
+        )
+        self.assertContains(
+            response, ContentType.objects.get_for_model(EventPage).name.title()
+        )
+        # But it should not contain non-page models:
+        self.assertNotContains(
+            response, ContentType.objects.get_for_model(EventPageSpeaker).name.title()
+        )
+
+
+class PageTypesReportViewQuerysetTests(WagtailTestUtils, TestCase):
+    def setUp(self):
+        super().setUp()
+        self.view = page_types.PageTypesReportView()
+        self.view.request = RequestFactory().get(
+            reverse("wagtailadmin_reports:page_types")
+        )
+        self.root = Page.objects.first()
+        self.home = Page.objects.get(slug="home")
+        self.simple_page_a = SimplePage(title="Simple page A", content="hello")
+        self.simple_page_b = SimplePage(title="Simple page B", content="hello")
+        self.simple_page_c = SimplePage(title="Simple page C", content="hello")
+        Page.get_first_root_node().add_child(instance=self.simple_page_a)
+        Page.get_first_root_node().add_child(instance=self.simple_page_b)
+        Page.get_first_root_node().add_child(instance=self.simple_page_c)
+
+    def test_queryset_ordering(self):
+        """Asserts that the queryset is ordered by page model count."""
+        # Test that the queryset is correctly ordered by page model count
+        queryset = self.view.get_queryset()
+        # Convert queryset to list of ids to make it easier to test
+        queryset_list_pks = list(queryset.values_list("pk", flat=True))
+        # Get the positions of the content types in the list
+        simple_page_content_type = ContentType.objects.get_for_model(SimplePage)
+        simple_page_position = queryset_list_pks.index(simple_page_content_type.pk)
+        page_content_type = ContentType.objects.get_for_model(Page)
+        page_position = queryset_list_pks.index(page_content_type.pk)
+        # Assert that the SimplePage comes before Page, since it has more entries
+        self.assertTrue(simple_page_position < page_position)
+        # There should be 3 SimplePages
+        self.assertEqual(queryset.get(id=simple_page_content_type.pk).count, 3)
+        # There should be 2 Pages (Homepage and Root)
+        self.assertEqual(queryset.get(id=page_content_type.pk).count, 2)
+
+    def test_queryset_last_edited_page(self):
+        """Tests that the queryset correctly returns the last edited page."""
+        # Edit the first product page
+        revision = self.simple_page_a.save_revision()
+        revision.publish()
+        # Edit the second product page
+        revision = self.simple_page_b.save_revision()
+        revision.publish()
+        # Edit the third product page
+        revision = self.simple_page_c.save_revision()
+        revision.publish()
+        # Re-edit the first product page
+        revision = self.simple_page_a.save_revision()
+        revision.publish()
+        # Get the queryset:
+        queryset = self.view.decorate_paginated_queryset(self.view.get_queryset())
+        # Assert that the first product page is the last edited page
+        self.simple_page_a.refresh_from_db()
+        self.assertEqual(queryset[0].last_edited_page.specific, self.simple_page_a)
+
+    def test_queryset_last_edited_by(self):
+        """Tests that the queryset correctly returns the last edited by user."""
+        # Create some product pages:
+        # Create some users:
+        user_a = self.create_superuser(
+            username="user_a", first_name="John", last_name="Doe"
+        )
+        user_b = self.create_superuser(
+            username="user_b", first_name="Jane", last_name="Doe"
+        )
+        # Edit the first product page with user_a
+        revision = self.simple_page_a.save_revision(user=user_a)
+        revision.publish(user=user_a)
+        # Re-edit the first product page with user_b
+        revision = self.simple_page_a.save_revision(user=user_b)
+        revision.publish(user=user_b)
+        # Get the queryset:
+        queryset = self.view.decorate_paginated_queryset(self.view.get_queryset())
+        # Assert that the first product page is the last edited page
+        self.simple_page_a.refresh_from_db()
+        self.assertEqual(queryset[0].last_edited_page.specific, self.simple_page_a)
+        # Assert that the first product page was last edited by user b
+        self.assertEqual(queryset[0].last_edited_by, user_b.pk)
+        self.assertEqual(queryset[0].last_edited_by_user, user_b.get_username())
+
+
+@override_settings(LANGUAGE_CODE="en")
+class PageTypesReportFiltersTests(WagtailTestUtils, TestCase):
+    def setUp(self):
+        self.user = self.login()
+        self.default_locale = Locale.get_default()
+        self.fr_locale, _ = Locale.objects.get_or_create(language_code="fr")
+
+    def get(self, params={}):
+        return self.client.get(reverse("wagtailadmin_reports:page_types"), params)
+
+    def test_locale_filtering(self):
+        # Create a product page in default locale
+        page = Page(title="My Page")
+        simple_page = SimplePage(title="Simple Page", content="hello")
+        Page.get_first_root_node().add_child(instance=page)
+        Page.get_first_root_node().add_child(instance=simple_page)
+        # Translate pages to French
+        page.copy_for_translation(self.fr_locale)
+        simple_page.copy_for_translation(self.fr_locale)
+
+        # Edit the product page in English to make sure that it's the latest
+        simple_page.title = "Updated Simple Page English title"
+        revision = simple_page.save_revision()
+        simple_page.publish(revision)
+
+        response = self.get()
+        page_types = {
+            content_type.id: content_type
+            for content_type in response.context["object_list"]
+        }
+
+        page_row = page_types.get(ContentType.objects.get_for_model(Page).pk)
+        simple_page_row = page_types.get(
+            ContentType.objects.get_for_model(SimplePage).pk
+        )
+
+        self.assertEqual(page_row.count, 4)
+        self.assertEqual(simple_page_row.count, 2)
+        # The last edited page should be the French version
+        self.assertEqual(page_row.last_edited_page.locale, self.fr_locale)
+        # The last edited SimplePage should be the English version
+        self.assertEqual(simple_page_row.last_edited_page.locale, self.default_locale)
+
+        # Filter by French locale
+        response = self.get({"page_locale": self.fr_locale.language_code})
+        page_types = {
+            content_type.id: content_type
+            for content_type in response.context["object_list"]
+        }
+
+        page_row = page_types.get(ContentType.objects.get_for_model(Page).pk)
+        simple_page_row = page_types.get(
+            ContentType.objects.get_for_model(SimplePage).pk
+        )
+
+        # There should be 1 of each page (only the French locale ones)
+        self.assertEqual(page_row.count, 1)
+        self.assertEqual(simple_page_row.count, 1)
+        # The last edited page should be the French version (even though product page was later edited in English)
+        self.assertEqual(page_row.last_edited_page.locale, self.fr_locale)
+        self.assertEqual(simple_page_row.last_edited_page.locale, self.fr_locale)
+
+    @override_settings(
+        LANGUAGE_CODE="en",
+        WAGTAIL_CONTENT_LANGUAGES=[
+            ("en", "English"),
+            ("de", "German"),
+            ("fr", "French"),
+        ],
+    )
+    def test_get_locale_choices(self):
+        choices = page_types._get_locale_choices()
+
+        expected_choices = [
+            ("en", "English"),
+            ("de", "German"),
+            ("fr", "French"),
+        ]
+
+        self.assertCountEqual(choices, expected_choices)
