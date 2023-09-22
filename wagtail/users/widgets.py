240b65f3091cc23cd7f8c3457f3cca0056152d4a
Matt Westcott <matt@west.co.tt>
2023-09-22 11:34:17 +0100

    Use more concise code for appending to classname

--- wagtail/users/widgets.py
+++ wagtail/users/widgets.py
@@ -3,8 +3,5 @@ from wagtail.admin.widgets import Button
 
 class UserListingButton(Button):
     def __init__(self, label, url, classname="", **kwargs):
-        if classname:
-            classname += " button button-small"
-        else:
-            classname = "button button-small"
+        classname = f"{classname} button button-small".strip()
         super().__init__(label, url, classname=classname, **kwargs)
