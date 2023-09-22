2b4a206d7612797508814077f360469b12c59fa1
Sage Abdullah <sage.abdullah@torchbox.com>
2023-09-22 12:37:19 +0100

    Remove unused preview_url context variable in the create/edit view mixin
    Was mistakenly added in the PR that moved the side panels context variables to the view

--- wagtail/admin/views/generic/mixins.py
+++ wagtail/admin/views/generic/mixins.py
@@ -672,7 +672,6 @@ class CreateEditViewOptionalFeaturesMixin:
         context["draftstate_enabled"] = self.draftstate_enabled
         context["workflow_enabled"] = self.workflow_enabled
         context["live_last_updated_info"] = self.get_live_last_updated_info()
-        context["preview_url"] = self.get_preview_url()
         context["workflow_history_url"] = self.get_workflow_history_url()
         context[
             "confirm_workflow_cancellation_url"
