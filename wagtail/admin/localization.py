cab1aff7c40ca4e914f079d86a18a640f8e7937f
Bernhard Bliem <bernhard@bliem.xyz>
2023-10-17 18:30:50 +0200

    Activate user's preferred language in JavaScriptCatalog view

--- wagtail/admin/localization.py
+++ wagtail/admin/localization.py
@@ -1,7 +1,14 @@
+import types
+
+import l18n
 import pytz
 from django.conf import settings
 from django.utils.dates import MONTHS, WEEKDAYS, WEEKDAYS_ABBR
+from django.utils.timezone import override as override_tz
 from django.utils.translation import gettext as _
+from django.utils.translation import override
+
+from wagtail.log_actions import LogContext
 
 # Wagtail languages with >=90% coverage
 # This list is manually maintained
@@ -107,3 +114,41 @@ def get_available_admin_time_zones():
         return []
 
     return getattr(settings, "WAGTAIL_USER_TIME_ZONES", pytz.common_timezones)
+
+
+def get_localized_response(user, view_func, request, *args, **kwargs):
+    preferred_language = None
+    if hasattr(user, "wagtail_userprofile"):
+        preferred_language = user.wagtail_userprofile.get_preferred_language()
+        l18n.set_language(preferred_language)
+        time_zone = user.wagtail_userprofile.get_current_time_zone()
+    else:
+        time_zone = settings.TIME_ZONE
+    with override_tz(time_zone), LogContext(user=user):
+        if preferred_language:
+            with override(preferred_language):
+                response = view_func(request, *args, **kwargs)
+        else:
+            response = view_func(request, *args, **kwargs)
+
+        if hasattr(response, "render"):
+            # If the response has a render() method, Django treats it
+            # like a TemplateResponse, so we should do the same
+            # In this case, we need to guarantee that when the TemplateResponse
+            # is rendered, it is done within the override context manager
+            # or the user preferred_language/timezone will not be used
+            # (this could be replaced with simply rendering the TemplateResponse
+            # for simplicity but this does remove some of its middleware modification
+            # potential)
+            render = response.render
+
+            def overridden_render(response):
+                with override_tz(time_zone):
+                    if preferred_language:
+                        with override(preferred_language):
+                            return render()
+                    return render()
+
+            response.render = types.MethodType(overridden_render, response)
+            # decorate the response render method with the override context manager
+        return response
