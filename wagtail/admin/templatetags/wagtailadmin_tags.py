5cc9ba17bb63fa5df6848a2145924aa54907c4fa
Matt Westcott <matt@west.co.tt>
2023-10-27 15:43:31 +0100

    Remove UserPagePermissionsProxy from action_menu and wagtailadmin_tags

--- wagtail/admin/templatetags/wagtailadmin_tags.py
+++ wagtail/admin/templatetags/wagtailadmin_tags.py
@@ -53,7 +53,6 @@ from wagtail.models import (
     Locale,
     Page,
     PageViewRestriction,
-    UserPagePermissionsProxy,
 )
 from wagtail.permission_policies.pages import PagePermissionPolicy
 from wagtail.telepath import JSContext
@@ -149,18 +148,6 @@ def widgettype(bound_field):
             return ""
 
 
-def _get_user_page_permissions(context):
-    # RemovedInWagtail60Warning: Remove this function
-
-    # Create a UserPagePermissionsProxy object to represent the user's global permissions, and
-    # cache it in the context for the duration of the page request, if one does not exist already
-    if "user_page_permissions" not in context:
-        context["user_page_permissions"] = UserPagePermissionsProxy(
-            context["request"].user
-        )
-    return context["user_page_permissions"]
-
-
 @register.simple_tag(takes_context=True)
 def page_permissions(context, page):
     """
@@ -168,9 +155,6 @@ def page_permissions(context, page):
     Sets the variable 'page_perms' to a PagePermissionTester object that can be queried to find out
     what actions the current logged-in user can perform on the given page.
     """
-    # RemovedInWagtail60Warning: Keep the UserPagePermissionsProxy object in the context
-    # for backwards compatibility during the deprecation period, even though we don't use it
-    _get_user_page_permissions(context)
     return page.permissions_for_user(context["request"].user)
 
 
