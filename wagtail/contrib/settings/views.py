30c85808488cecab4647731d9259aa6f2058c4c5
Sage Abdullah <sage.abdullah@torchbox.com>
2023-11-06 13:00:47 +0000

    Continue refactoring contrib.settings.views.EditView to better reuse generic EditView
    And remove unused context variables

--- wagtail/contrib/settings/views.py
+++ wagtail/contrib/settings/views.py
@@ -1,32 +1,22 @@
 from functools import lru_cache
 
-# from typing import Optional
-# from django.core.exceptions import PermissionDenied
-from django.db import transaction
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
-
-# from django.template.response import TemplateResponse
 from django.utils.text import capfirst
 from django.utils.translation import gettext as _
 from django.utils.translation import gettext_lazy
 
-# from django.views import View
 from wagtail.admin import messages
 from wagtail.admin.panels import (
     ObjectList,
-    TabbedInterface,
     extract_panel_definitions_from_model_class,
 )
 from wagtail.admin.views import generic
-from wagtail.log_actions import log
 from wagtail.models import Site
 
 from .forms import SiteSwitchForm
 from .models import BaseGenericSetting, BaseSiteSetting
-
-# from .permissions import user_can_edit_setting_type
 from .registry import registry
 
 
@@ -90,6 +80,7 @@ def redirect_to_relevant_instance(request, app_name, model_name):
 
 class EditView(generic.EditView):
     template_name = "wagtailsettings/edit.html"
+    header_icon = "cogs"
     error_message = gettext_lazy("The setting could not be saved due to errors.")
 
     def setup(self, request, app_name, model_name, *args, **kwargs):
@@ -113,29 +104,22 @@ class EditView(generic.EditView):
     def get_edit_url(self):
         return reverse(
             "wagtailsettings:edit",
-            args=(
-                self.app_name,
-                self.model_name,
-                self.pk,
-            ),
+            args=(self.app_name, self.model_name, self.pk),
         )
+
+    def get_success_buttons(self):
+        return None
+
+    def get_page_subtitle(self):
+        return capfirst(self.model._meta.verbose_name)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        self.site = None
         site_switcher = None
-
-        if issubclass(self.model, BaseSiteSetting):
-            self.site = get_object_or_404(Site, pk=self.kwargs["pk"])
-            self.object = self.model.for_site(self.site)
-
-            # Show a site switcher form if there are multiple sites
-            if Site.objects.count() > 1:
-                site_switcher = SiteSwitchForm(self.site, self.model)
-                media = context.get("media") + site_switcher.media
-        else:
-            self.object = get_object_or_404(self.model, pk=self.kwargs["pk"])
+        if self.site and Site.objects.count() > 1:
+            site_switcher = SiteSwitchForm(self.site, self.model)
+            media = context.get("media") + site_switcher.media
 
         form = self.get_form()
 
@@ -147,17 +131,9 @@ class EditView(generic.EditView):
 
         context.update(
             {
-                "setting_type_name": self.model._meta.verbose_name,
-                "instance": self.object,
                 "edit_handler": edit_handler,
-                "site": self.site,
                 "site_switcher": site_switcher,
-                "tabbed": isinstance(
-                    get_setting_edit_handler(self.model), TabbedInterface
-                ),
                 "media": media,
-                "opts": self.model._meta,
-                "form_id": self.object.pk,
             }
         )
 
@@ -166,22 +142,7 @@ class EditView(generic.EditView):
     def get_success_url(self):
         return self.get_edit_url()
 
-    def form_valid(self, form):
-        with transaction.atomic():
-            form.save()
-            log(self.object, "wagtail.edit")
-
-            messages.success(
-                self.request,
-                _("%(setting_type)s updated.")
-                % {
-                    "setting_type": capfirst(self.model._meta.verbose_name),
-                    "instance": self.object,
-                },
-            )
-
-        return redirect(
-            "wagtailsettings:edit",
-            app_name=self.model._meta.app_label,
-            model_name=self.model._meta.model_name,
-        )
+    def get_success_message(self):
+        return _("%(setting_type)s updated.") % {
+            "setting_type": capfirst(self.model._meta.verbose_name)
+        }
