0f6e822587ed3edc24108900d0d12dac52bd01d1
Sage Abdullah <sage.abdullah@torchbox.com>
2023-11-21 13:41:30 +0000

    Create permissions policy registry in wagtail.permissions and register the defaults

--- wagtail/permissions.py
+++ wagtail/permissions.py
@@ -1,7 +1,16 @@
 from wagtail.models import Collection, Locale, Page, Site, Task, Workflow
 from wagtail.permission_policies import ModelPermissionPolicy
+from wagtail.permission_policies.base import BasePermissionPolicy
 from wagtail.permission_policies.collections import CollectionManagementPermissionPolicy
 from wagtail.permission_policies.pages import PagePermissionPolicy
+from wagtail.utils.registry import ObjectTypeRegistry
+
+policies_registry = ObjectTypeRegistry[BasePermissionPolicy]()
+
+
+def register_permission_policy(model, policy):
+    policies_registry.register(model, value=policy)
+
 
 page_permission_policy = PagePermissionPolicy(Page)
 site_permission_policy = ModelPermissionPolicy(Site)
@@ -9,3 +18,10 @@ collection_permission_policy = CollectionManagementPermissionPolicy(Collection)
 task_permission_policy = ModelPermissionPolicy(Task)
 workflow_permission_policy = ModelPermissionPolicy(Workflow)
 locale_permission_policy = ModelPermissionPolicy(Locale)
+
+register_permission_policy(Page, page_permission_policy)
+register_permission_policy(Site, site_permission_policy)
+register_permission_policy(Collection, collection_permission_policy)
+register_permission_policy(Task, task_permission_policy)
+register_permission_policy(Workflow, workflow_permission_policy)
+register_permission_policy(Locale, locale_permission_policy)
