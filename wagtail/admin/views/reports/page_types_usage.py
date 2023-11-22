a74d708209deec3ca14cb4f65d5aa2e2a7475ebc
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2023-11-22 07:48:02 -0300

    Refactor page types usage report annotations

--- wagtail/admin/views/reports/page_types_usage.py
+++ wagtail/admin/views/reports/page_types_usage.py
@@ -1,7 +1,6 @@
 import django_filters
 from django.conf import settings
-from django.contrib.auth import get_user_model
-from django.core.exceptions import PermissionDenied, ValidationError
+from django.core.exceptions import PermissionDenied
 from django.db.models import Count, F, OuterRef, Q, Subquery
 from django.utils.translation import gettext_lazy as _
 
@@ -10,7 +9,6 @@ from wagtail.admin.views.reports import ReportView
 from wagtail.coreutils import get_content_languages
 from wagtail.models import ContentType, Page, Site, get_page_models
 from wagtail.permission_policies.pages import PagePermissionPolicy
-from wagtail.users.utils import get_deleted_user_display_name
 
 
 def _get_locale_choices():
@@ -43,7 +41,6 @@ def _annotate_last_edit_info(queryset, language_code, site_root_path):
     queryset = queryset.annotate(
         count=Count("pages", filter=Q(**page_count_filter_kwargs)),
         last_edited_page_id=Subquery(latest_edited_page.values("pk")[:1]),
-        last_edited_page_owner_id=Subquery(latest_edited_page.values("owner_id")[:1]),
     )
 
     return queryset
@@ -106,51 +103,21 @@ class PageTypesUsageReportView(ReportView):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.user_model = get_user_model()
         self.page_models = [model.__name__.lower() for model in get_page_models()]
         self.i18n_enabled = getattr(settings, "WAGTAIL_I18N_ENABLED", False)
 
-    def user_id_to_python(self, user_id):
-        return self.user_model._meta.pk.to_python(user_id)
-
-    def add_last_edited_name_to_page_type(self, username_mapping, page_type):
-        if page_type.last_edited_page_owner_id:
-            # Giving the last_published_by annotation an explicit output_field type
-            # causes an issue with prefetch_workflow_states when the field is a
-            # ConvertedValueField. If the last user to publish the page has been
-            # deleted, we will render their user id in the template, so we call
-            # to_python on the value so that what's rendered matches the developer's
-            # expectation in the case of complex primary key types (e.g. UUIDField).
-            try:
-                user_id_value = self.user_id_to_python(
-                    page_type.last_edited_page_owner_id
-                )
-            except ValidationError:
-                user_id_value = page_type.last_edited_page_owner_id
-
-            last_edited_page_owner = username_mapping.get(
-                user_id_value, get_deleted_user_display_name(user_id=user_id_value)
-            )
-            page_type.last_edited_page_owner = last_edited_page_owner
-
-    def add_last_edited_page_to_page_type(self, pages_mapping, page_type):
-        if page_type.last_edited_page_id:
-            page_type.last_edited_page = pages_mapping.get(
-                page_type.last_edited_page_id
-            )
-
     def decorate_paginated_queryset(self, page_types):
-        page_ids = set(page_types.values_list("last_edited_page_id", flat=True))
-        pages = Page.objects.filter(pk__in=page_ids).select_related("owner")
+        pages_mapping = Page.objects.select_related("owner").in_bulk(
+            obj.last_edited_page_id for obj in page_types if obj.last_edited_page_id
+        )
 
-        pages_mapping = {page.pk: page for page in pages}
-        username_mapping = {
-            page.owner.pk: page.owner.get_username() for page in pages if page.owner
-        }
+        for item in page_types:
+            item.last_edited_page = (
+                pages_mapping[item.last_edited_page_id]
+                if item.last_updated_page_id
+                else None
+            )
 
-        for page_type in page_types:
-            self.add_last_edited_page_to_page_type(pages_mapping, page_type)
-            self.add_last_edited_name_to_page_type(username_mapping, page_type)
         return page_types
 
     def get_queryset(self):
