10c1e12285929624aab20207f8afb609b850a886
Tibor Leupold <tibor.leupold@torchbox.com>
2023-11-29 14:45:27 -0800

    Replace template components with standalone `laces` library (#11292)
    Fixes #11105

--- wagtail/admin/templatetags/wagtailadmin_tags.py
+++ wagtail/admin/templatetags/wagtailadmin_tags.py
@@ -19,11 +19,12 @@ from django.urls import reverse
 from django.urls.exceptions import NoReverseMatch
 from django.utils import timezone
 from django.utils.encoding import force_str
-from django.utils.html import avoid_wrapping, conditional_escape, json_script
+from django.utils.html import avoid_wrapping, json_script
 from django.utils.http import urlencode
 from django.utils.safestring import mark_safe
 from django.utils.timesince import timesince
 from django.utils.translation import gettext_lazy as _
+from laces.templatetags.laces import component
 
 from wagtail import hooks
 from wagtail.admin.admin_url_finder import AdminURLFinder
@@ -934,109 +935,6 @@ def resolve_url(url):
         return ""
 
 
-class ComponentNode(template.Node):
-    def __init__(
-        self,
-        component,
-        extra_context=None,
-        isolated_context=False,
-        fallback_render_method=None,
-        target_var=None,
-    ):
-        self.component = component
-        self.extra_context = extra_context or {}
-        self.isolated_context = isolated_context
-        self.fallback_render_method = fallback_render_method
-        self.target_var = target_var
-
-    def render(self, context: Context) -> str:
-        # Render a component by calling its render_html method, passing request and context from the
-        # calling template.
-        # If fallback_render_method is true, objects without a render_html method will have render()
-        # called instead (with no arguments) - this is to provide deprecation path for things that have
-        # been newly upgraded to use the component pattern.
-
-        component = self.component.resolve(context)
-
-        if self.fallback_render_method:
-            fallback_render_method = self.fallback_render_method.resolve(context)
-        else:
-            fallback_render_method = False
-
-        values = {
-            name: var.resolve(context) for name, var in self.extra_context.items()
-        }
-
-        if hasattr(component, "render_html"):
-            if self.isolated_context:
-                html = component.render_html(context.new(values))
-            else:
-                with context.push(**values):
-                    html = component.render_html(context)
-        elif fallback_render_method and hasattr(component, "render"):
-            html = component.render()
-        else:
-            raise ValueError(f"Cannot render {component!r} as a component")
-
-        if self.target_var:
-            context[self.target_var] = html
-            return ""
-        else:
-            if context.autoescape:
-                html = conditional_escape(html)
-            return html
-
-
-@register.tag(name="component")
-def component(parser, token):
-    bits = token.split_contents()[1:]
-    if not bits:
-        raise template.TemplateSyntaxError(
-            "'component' tag requires at least one argument, the component object"
-        )
-
-    component = parser.compile_filter(bits.pop(0))
-
-    # the only valid keyword argument immediately following the component
-    # is fallback_render_method
-    flags = token_kwargs(bits, parser)
-    fallback_render_method = flags.pop("fallback_render_method", None)
-    if flags:
-        raise template.TemplateSyntaxError(
-            "'component' tag only accepts 'fallback_render_method' as a keyword argument"
-        )
-
-    extra_context = {}
-    isolated_context = False
-    target_var = None
-
-    while bits:
-        bit = bits.pop(0)
-        if bit == "with":
-            extra_context = token_kwargs(bits, parser)
-        elif bit == "only":
-            isolated_context = True
-        elif bit == "as":
-            try:
-                target_var = bits.pop(0)
-            except IndexError:
-                raise template.TemplateSyntaxError(
-                    "'component' tag with 'as' must be followed by a variable name"
-                )
-        else:
-            raise template.TemplateSyntaxError(
-                "'component' tag received an unknown argument: %r" % bit
-            )
-
-    return ComponentNode(
-        component,
-        extra_context=extra_context,
-        isolated_context=isolated_context,
-        fallback_render_method=fallback_render_method,
-        target_var=target_var,
-    )
-
-
 class FragmentNode(template.Node):
     def __init__(self, nodelist, target_var):
         self.nodelist = nodelist
@@ -1247,3 +1145,8 @@ def human_readable_date(date, description=None, placement="top"):
         "description": description,
         "placement": placement,
     }
+
+
+# Shadow the laces `component` tag which was extracted from Wagtail. The shadowing
+# is useful to avoid having to update all the templates that use the `component` tag.
+register.tag("component", component)
