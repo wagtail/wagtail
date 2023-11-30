50a81a6187a194ccc4381d0f8631610d8fde77f8
Sage Abdullah <sage.abdullah@torchbox.com>
2023-11-30 13:00:11 +0000

    Update pagination tests on documents index to match Django standard

--- wagtail/documents/tests/test_admin_views.py
+++ wagtail/documents/tests/test_admin_views.py
@@ -65,11 +65,7 @@ class TestDocumentIndexView(WagtailTestUtils, TestCase):
         response = self.get({"p": "Hello World!"})
 
         # Check response
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "wagtaildocs/documents/index.html")
-
-        # Check that we got page one
-        self.assertEqual(response.context["page_obj"].number, 1)
+        self.assertEqual(response.status_code, 404)
 
     def test_pagination_out_of_range(self):
         self.make_docs()
@@ -77,14 +73,7 @@ class TestDocumentIndexView(WagtailTestUtils, TestCase):
         response = self.get({"p": 99999})
 
         # Check response
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "wagtaildocs/documents/index.html")
-
-        # Check that we got the last page
-        self.assertEqual(
-            response.context["page_obj"].number,
-            response.context["documents"].paginator.num_pages,
-        )
+        self.assertEqual(response.status_code, 404)
 
     def test_ordering(self):
         orderings = ["title", "-created_at"]
