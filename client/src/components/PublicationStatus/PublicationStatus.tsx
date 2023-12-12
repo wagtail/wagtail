384a51a6cf0f42730e0e301acb66464d8ae974e7
Thibaud Colas <thibaudcolas@gmail.com>
2023-12-12 17:30:10 +0000

    Combine o-pill and c-status styles, as they are always used together

--- client/src/components/PublicationStatus/PublicationStatus.tsx
+++ client/src/components/PublicationStatus/PublicationStatus.tsx
@@ -5,7 +5,7 @@ import React from 'react';
  * Displays the publication status of a page in a pill.
  */
 const PublicationStatus = ({ status }) => (
-  <span className={`o-pill c-status${status.live ? ' c-status--live' : ''}`}>
+  <span className={`c-status${status.live ? ' c-status--live' : ''}`}>
     {status.status}
   </span>
 );
