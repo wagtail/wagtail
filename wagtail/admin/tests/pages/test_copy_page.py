cbaabc30222698c862c9f8c5e3cc30e9f4bcf4c3
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2023-12-25 09:42:49 +0530

    Add ability to override a Page's copy form
    - Documentation added for Customising Copy Form with an auto-incrementing slug example
    - Add tests
    - Resolves #10147

--- wagtail/admin/tests/pages/test_copy_page.py
+++ wagtail/admin/tests/pages/test_copy_page.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 
 from wagtail.models import GroupPagePermission, Page
 from wagtail.test.testapp.models import (
+    CustomCopyFormPage,
     EventPage,
     EventPageSpeaker,
     PageWithExcludedCopyField,
@@ -781,3 +782,22 @@ class TestPageCopy(WagtailTestUtils, TestCase):
             event_page.speakers.first().translation_key,
             new_page.speakers.first().translation_key,
         )
+
+    def test_page_copy_with_custom_copy_form(self):
+        custom_copy_form_page = self.root_page.add_child(
+            instance=CustomCopyFormPage(
+                title="Hello world!",
+                slug="copy-form",
+                live=True,
+                has_unpublished_changes=False,
+            )
+        )
+
+        response = self.client.get(
+            reverse("wagtailadmin_pages:copy", args=(custom_copy_form_page.id,))
+        )
+        # Check response
+        self.assertEqual(response.status_code, 200)
+
+        # Check if slug is hello-world-2
+        self.assertContains(response, "copy-form-2")
