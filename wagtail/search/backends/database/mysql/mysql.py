8b914f4448ca79292f98c261c7e7c29d65c094f3
Jake Howard <jake.howard@torchbox.com>
2024-02-01 14:53:04 +0000

    Use _raw_delete when purging search index (#11565)
    This not only improves performance, but also reduces memory usage

--- wagtail/search/backends/database/mysql/mysql.py
+++ wagtail/search/backends/database/mysql/mysql.py
@@ -276,7 +276,7 @@ class Index:
             update_method(content_type_pk, indexers)
 
     def delete_item(self, item):
-        item.index_entries.using(self.db_alias).delete()
+        item.index_entries.all()._raw_delete(using=self.db_alias)
 
     def __str__(self):
         return self.name
@@ -662,7 +662,7 @@ class MySQLSearchBackend(BaseSearchBackend):
             for connection in connections.all()
             if connection.vendor == "mysql"
         ]:
-            IndexEntry._default_manager.using(connection.alias).delete()
+            IndexEntry._default_manager.all()._raw_delete(using=connection.alias)
 
     def add_type(self, model):
         pass  # Not needed.
