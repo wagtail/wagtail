35f1d909d0269e3b7475c4e5493b744b32d413ca
Joshua Munn <public@elysee-munn.family>
2024-02-13 22:38:10 +0000

    Move type sniffing logic to StreamBlock.normalize
    from StreamField._to_python

--- wagtail/exceptions.py
+++ wagtail/exceptions.py
@@ -7,3 +7,11 @@ class PageClassNotFoundError(ImportError):
     """
 
     pass
+
+
+class BlockNormalizationError(TypeError):
+    """
+    Raised by a block's `normalize' method when attempting to normalize a value
+    of an incorrect type.
+    """
+    pass
