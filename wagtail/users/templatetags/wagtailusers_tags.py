c2f6d67349f857774686f511ba4b9c2b2b1f49ba
Sage Abdullah <sage.abdullah@torchbox.com>
2024-02-14 14:16:59 +0000

    Prevent stale permissions from becoming the main permissions after a model rename
    This can happen after a RenameModel operation (changing the Python class
    name, not just the verbose_name).

--- wagtail/users/templatetags/wagtailusers_tags.py
+++ wagtail/users/templatetags/wagtailusers_tags.py
@@ -134,7 +134,10 @@ def format_permissions(permission_bound_field):
             # identify the main categories of permission, and assign to
             # the relevant dict key, else bung in the 'custom_perms' list
             permission_action = perm.codename.split("_")[0]
-            if permission_action in main_permission_names:
+            if (
+                permission_action in main_permission_names
+                and perm.codename == f"{permission_action}_{perm.content_type.model}"
+            ):
                 if permission_action in extra_perms_exist:
                     extra_perms_exist[permission_action] = True
                 content_perms_dict[permission_action] = {
