9966c68e58fcd24cf3b74aa03f2e5fed6c11a570
Chiemezuo <chiemezuoakujobi@gmail.com>
2024-02-22 14:25:25 +0100

    Add ability for Draftail to be initialized via a dispatched CustomEvent

--- client/src/entrypoints/admin/draftail.js
+++ client/src/entrypoints/admin/draftail.js
@@ -45,3 +45,18 @@ const entityTypes = [
 ];
 
 entityTypes.forEach((type) => draftail.registerPlugin(type, 'entityTypes'));
+
+/**
+ * Initialize a Draftail editor on a given element when the w-draftail:init event is fired.
+ */
+document.addEventListener('w-draftail:init', ({ detail = {}, target }) => {
+  const id = target.id;
+
+  if (!id) {
+    // eslint-disable-next-line no-console
+    console.error('`w-draftail:init` event must have a target with an id.');
+    return;
+  }
+
+  window.draftail.initEditor(`#${id}`, detail, document.currentScript);
+});
