17cf65173f3208737fd173b4b6ebcd3f5c099cf4
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-03-08 19:00:56 +0530

    added Keyboard Controller

---
+++ client/src/controllers/w_keyboard_controller.ts
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus"
+import Mousetrap from 'mousetrap';
+
+export default class extends Controller {
+    static values = { key: String };
+
+    declare keyValue: string;
+
+    handleKey(event: Event) {
+        event.preventDefault();
+        this.element.click();
+    }
+
+    connect() {
+        Mousetrap.bind(this.keyValue, this.handleKey.bind(this));
+    }
+
+    disconnect() {
+        Mousetrap.unbind(this.keyValue);
+    }
+
+    keyValueChanged(key, previousKey) {
+        // See https://stimulus.hotwired.dev/reference/values#change-callbacks
+    
+        if (previousKey && previousKey !== key) {
+          Mousetrap.unbind(previousKey, this.handleKey);
+        }
+    
+        Mousetrap.bind(key, this.handleKey);
+    }
+}
