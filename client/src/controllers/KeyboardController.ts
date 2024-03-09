5414b7bb0ea1f1b7792da8604de87870cc330a7d
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-03-09 23:36:10 +0530

    KeyboardController Test

---
+++ client/src/controllers/KeyboardController.ts
@@ -0,0 +1,23 @@
+import { Controller } from '@hotwired/stimulus';
+import Mousetrap from 'mousetrap';
+
+export class KeyboardController extends Controller<HTMLButtonElement> {
+  static values = { key: String };
+
+  handleKey(event: Event) {
+    event.preventDefault();
+    this.element.click();
+  }
+
+  initialize(): void {
+    this.handleKey = this.handleKey.bind(this);
+  }
+
+  keyValueChanged(key, previousKey) {
+    if (previousKey && previousKey !== key) {
+      Mousetrap.unbind(previousKey, this.handleKey);
+    }
+
+    Mousetrap.bind(key, this.handleKey);
+  }
+}
