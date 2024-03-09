e34f802fa1200fb34ea92c563808bcd841f2a6ec
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-03-09 11:10:05 +0530

    reformatted file

--- client/src/controllers/w_keyboard_controller.ts
+++ client/src/controllers/w_keyboard_controller.ts
@@ -1,31 +1,29 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 import Mousetrap from 'mousetrap';
 
 export default class extends Controller {
-    static values = { key: String };
+  static values = { key: String };
 
-    declare keyValue: string;
+  declare keyValue: string;
 
-    handleKey(event: Event) {
-        event.preventDefault();
-        this.element.click();
+  handleKey(event: Event) {
+    event.preventDefault();
+    this.element.click();
+  }
+
+  connect() {
+    Mousetrap.bind(this.keyValue, this.handleKey.bind(this));
+  }
+
+  disconnect() {
+    Mousetrap.unbind(this.keyValue);
+  }
+
+  keyValueChanged(key, previousKey) {
+    if (previousKey && previousKey !== key) {
+      Mousetrap.unbind(previousKey, this.handleKey);
     }
 
-    connect() {
-        Mousetrap.bind(this.keyValue, this.handleKey.bind(this));
-    }
-
-    disconnect() {
-        Mousetrap.unbind(this.keyValue);
-    }
-
-    keyValueChanged(key, previousKey) {
-        // See https://stimulus.hotwired.dev/reference/values#change-callbacks
-    
-        if (previousKey && previousKey !== key) {
-          Mousetrap.unbind(previousKey, this.handleKey);
-        }
-    
-        Mousetrap.bind(key, this.handleKey);
-    }
+    Mousetrap.bind(key, this.handleKey);
+  }
 }
