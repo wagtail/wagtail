a2ce4bca46ccd5b951da4706ca3e1d33ee4712db
Sage Abdullah <sage.abdullah@torchbox.com>
2024-03-20 22:34:52 +0700

    Completely reuse generic HistoryView template for page HistoryView

--- wagtail/admin/views/pages/history.py
+++ wagtail/admin/views/pages/history.py
@@ -2,9 +2,11 @@ import django_filters
 from django import forms
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
+from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy
 
 from wagtail.admin.views.generic import history
+from wagtail.admin.views.pages.utils import get_breadcrumbs_items_for_page
 from wagtail.models import Page, PageLogEntry
 from wagtail.permissions import page_permission_policy
 
@@ -50,7 +52,6 @@ class WorkflowHistoryDetailView(
 
 class PageHistoryView(history.HistoryView):
     template_name = "wagtailadmin/pages/history.html"
-    page_title = gettext_lazy("Page history")
     filterset_class = PageHistoryFilterSet
     model = Page
     pk_url_kwarg = "page_id"
@@ -63,23 +64,35 @@ class PageHistoryView(history.HistoryView):
         "lock",
         "unlock",
     }
+    history_url_name = "wagtailadmin_pages:history"
+    history_results_url_name = "wagtailadmin_pages:history_results"
+    edit_url_name = "wagtailadmin_pages:edit"
+    revisions_view_url_name = "wagtailadmin_pages:revisions_view"
+    revisions_revert_url_name = "wagtailadmin_pages:revisions_revert"
+    revisions_compare_url_name = "wagtailadmin_pages:revisions_compare"
+    revisions_unschedule_url_name = "wagtailadmin_pages:revisions_unschedule"
+    _show_breadcrumbs = True
 
     def get_object(self):
-        self.page = get_object_or_404(Page, id=self.pk).specific
-        return self.page
+        return get_object_or_404(Page, id=self.pk).specific
 
-    def get_context_data(self, *args, object_list=None, **kwargs):
-        context = super().get_context_data(*args, object_list=object_list, **kwargs)
-        context["page"] = self.page
-        context["subtitle"] = self.page.get_admin_display_title()
-        context["page_latest_revision"] = self.page.get_latest_revision()
-
-        return context
+    def get_page_subtitle(self):
+        return self.object.get_admin_display_title()
 
     def user_can_unschedule(self):
         return self.object.permissions_for_user(self.request.user).can_unschedule()
 
     def get_base_queryset(self):
-        return PageLogEntry.objects.filter(page=self.page).select_related(
-            "revision", "user", "user__wagtail_userprofile"
-        )
+        return self._annotate_queryset(PageLogEntry.objects.filter(page=self.object))
+
+    @cached_property
+    def breadcrumbs_items(self):
+        return get_breadcrumbs_items_for_page(self.object, self.request.user)
+
+    def get_breadcrumbs_items(self):
+        # The generic HistoryView will add an edit link for the current object as
+        # the second-to-last item, but we don't want that because we want to
+        # link to the explore view instead for consistency with how page
+        # breadcrumbs work elsewhere. So we only take the last item, which is
+        # the "self" (History) item.
+        return self.breadcrumbs_items + [super().get_breadcrumbs_items()[-1]]
