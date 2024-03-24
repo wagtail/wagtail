5cc22f3f7574f378cadc6671e080abd96dedc794
Chiemezuo <chiemezuoakujobi@gmail.com>
2024-03-24 13:13:36 +0100

    Add "description" field to AbstractImage
    - Baseline support for upcoming alt text capabilities

--- wagtail/images/tests/test_models.py
+++ wagtail/images/tests/test_models.py
@@ -145,6 +145,23 @@ class TestImage(TestCase):
         )
         self.assertIsNone(image.get_suggested_focal_point())
 
+    def test_default_with_description(self):
+        # Primary default should be description
+        image = Image.objects.create(
+            title="Test Image",
+            description="This is a test description",
+            file=get_test_image_file(),
+        )
+        self.assertEqual(image.default_alt_text, image.description)
+
+    def test_default_without_description(self):
+        # Secondary default should be title
+        image = Image.objects.create(
+            title="Test Image",
+            file=get_test_image_file(),
+        )
+        self.assertEqual(image.default_alt_text, image.title)
+
 
 class TestImageQuerySet(TransactionTestCase):
     fixtures = ["test_empty.json"]
