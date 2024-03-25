a6afab99895c3ed2930f55f7671bbf9848801bbe
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-03-25 08:51:00 +0530

    Formatted docunment and fixed linting errors

--- client/src/controllers/KeyboardController.stories.js
+++ client/src/controllers/KeyboardController.stories.js
@@ -20,18 +20,24 @@ const definitions = [
   },
 ];
 
-const Template = ({ debug = false }) => (
+const handleClick = () => {
+  const counterElement = document.getElementById('counter');
+  const currentCount = parseInt(counterElement.innerText.split(' ')[2], 10);
+  counterElement.innerText = `Click count: ${currentCount + 1}`;
+};
+
+const Template = ({ debug = false, count = 0 }) => (
   <StimulusWrapper debug={debug} definitions={definitions}>
     <button
       type="button"
       className="button button-small button-secondary"
       data-controller="w-kbd"
       data-w-kbd-key-value="mod+j"
-      onClick={() => console.log('clicked')}
+      onClick={handleClick}
     >
       Click Me
     </button>
-
+    <p id="counter">Click count: {count}</p>
     <p>
       {' '}
       Trigger with <kbd> Command ⌘</kbd> + <kbd>j</kbd> on macOS or{' '}
--- client/src/controllers/KeyboardController.test.js
+++ client/src/controllers/KeyboardController.test.js
@@ -97,7 +97,6 @@ describe('KeyboardController', () => {
 
     it('should call the click event when `mod+j` is pressed after being registered', async () => {
       expect(buttonClickMock).not.toHaveBeenCalled();
-      //clear the body
 
       await setup(
         `<button id="btn" data-controller="w-kbd" data-w-kbd-key-value="mod+j">Go</button>`,
@@ -116,15 +115,17 @@ describe('KeyboardController', () => {
   describe('aria keyshortcuts usage', () => {
     it('should take the aria-keyshortcut attribute if the data-w-kbd-key-value is not set', async () => {
       expect(buttonClickMock).not.toHaveBeenCalled();
-
       await setup(
         `<button id="btn" data-controller="w-kbd" aria-keyshortcut="j">Go</button>`,
       );
-
+      HTMLButtonElement.prototype.click = buttonClickMock;
       // Simulate the keydown event
       simulateKey({ key: 'j' });
 
       expect(buttonClickMock).toHaveBeenCalledTimes(1);
+      expect(buttonClickMock.mock.contexts).toEqual([
+        document.getElementById('btn'),
+      ]);
     });
   });
 });
