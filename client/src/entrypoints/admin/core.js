9e633f36ea1d2932006f911c7026bf7844757101
LB Johnston <mail@lb.ee>
2024-04-06 15:48:32 +1000

    Add deprecated backwards compatible window.URLify with release notes
    - Add legacy support for window.URLify with allowUnicode as a param
    - Add release notes details for both window.URLify (deprecated) & window.XRegExp (removed)

--- client/src/entrypoints/admin/core.js
+++ client/src/entrypoints/admin/core.js
@@ -3,10 +3,12 @@ import * as StimulusModule from '@hotwired/stimulus';
 
 import { Icon, Portal } from '../..';
 import { coreControllerDefinitions } from '../../controllers';
-import { escapeHtml } from '../../utils/text';
 import { InlinePanel } from '../../components/InlinePanel';
 import { MultipleChooserPanel } from '../../components/MultipleChooserPanel';
 import { initStimulus } from '../../includes/initStimulus';
+
+import { urlify } from '../../utils/urlify';
+import { escapeHtml } from '../../utils/text';
 
 /** Expose a global to allow for customisations and packages to build with Stimulus. */
 window.StimulusModule = StimulusModule;
@@ -37,6 +39,21 @@ window.escapeHtml = escapeHtml;
 window.InlinePanel = InlinePanel;
 
 window.MultipleChooserPanel = MultipleChooserPanel;
+
+/**
+ * Support legacy global URLify which can be called with `allowUnicode` as a third param.
+ * Was not documented and only used in modeladmin prepopulate JS.
+ *
+ * @deprecated RemovedInWagtail70
+ * @see https://github.com/django/django/blob/main/django/contrib/admin/static/admin/js/urlify.js#L156
+ *
+ * @param {string} str
+ * @param {number} numChars
+ * @param {boolean} allowUnicode
+ * @returns {string}
+ */
+window.URLify = (str, numChars = 255, allowUnicode = false) =>
+  urlify(str, { numChars, allowUnicode });
 
 $(() => {
   /* Dropzones */
