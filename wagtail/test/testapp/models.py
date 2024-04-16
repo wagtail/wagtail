2b14ad2a89bea391c6bc3d85b493d6218dce6a62
elhussein almasri <mhmdadgjp@gmail.com>
2024-04-16 07:59:09 +0000

    Add-unit-test

--- wagtail/test/testapp/models.py
+++ wagtail/test/testapp/models.py
@@ -217,12 +217,13 @@ class PageWithExcludedCopyField(Page):
 
     # Exclude this field from being copied
     special_field = models.CharField(blank=True, max_length=255, default="Very Special")
-    exclude_fields_in_copy = ["special_field"]
+    exclude_fields_in_copy = ["special_field", "related_manager"]
 
     content_panels = [
         FieldPanel("title", classname="title"),
         FieldPanel("special_field"),
         FieldPanel("content"),
+        MultipleChooserPanel("related_manager", chooser_field_name="image"),
     ]
 
 
@@ -2192,6 +2193,12 @@ class GalleryPage(Page):
 class GalleryPageImage(Orderable):
     page = ParentalKey(
         "tests.GalleryPage", related_name="gallery_images", on_delete=models.CASCADE
+    )
+    exclude_field = ParentalKey(
+        "tests.PageWithExcludedCopyField",
+        related_name="related_manager",
+        on_delete=models.CASCADE,
+        null=True,
     )
     image = models.ForeignKey(
         "wagtailimages.Image",
