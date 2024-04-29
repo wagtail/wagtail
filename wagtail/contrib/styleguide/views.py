c3a52a6fdb632a9e0be273d2e387210e95c5855f
Sage Abdullah <sage.abdullah@torchbox.com>
2024-04-29 17:56:48 +0700

    Fix missing static files in the styleguide

--- wagtail/contrib/styleguide/views.py
+++ wagtail/contrib/styleguide/views.py
@@ -94,7 +94,7 @@ class ExampleForm(forms.Form):
 
     @property
     def media(self):
-        return forms.Media(
+        return super().media + forms.Media(
             css={
                 "all": [versioned_static("wagtailstyleguide/css/animate-progress.css")]
             }
