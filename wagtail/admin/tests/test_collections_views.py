wagtail/wagtail

commit 45ac80bb8994dafe62e090c658030fe4438ff0b6

Make cross-link URLs on generic model views optional
Normally, generic model views will be spun up through wagtail.admin.viewsets, which takes care of configuring the add_url_name / edit_url_name etc attributes to ensure that they are cross-linked appropriately. But if you're defining one in isolation, that's an unnecessary hoop to jump through, and the error messages if you don't specify them are fairly opaque. Fix this so that:

1. Non-essential cross-links (e.g. the Add button on the index view, and the Delete button on the edit view) are gracefully omitted if the URLs are undefined;
2. Essential cross-links (e.g. the destination of the redirect after a successful form submission) raise an informative error telling the developer what needs to be defined.

wagtail/admin/templates/wagtailadmin/generic/edit.html
@@ -3,7 +3,7 @@
 
 {% block actions %}
     <input type="submit" value="{% trans 'Save' %}" class="button" />
-    {% if can_delete %}
+    {% if delete_url %}
         <a href="{{ delete_url }}" class="button button-secondary no">{{ delete_item_label }}</a>
     {% endif %}
 {% endblock %}

wagtail/admin/templates/wagtailadmin/generic/index.html
@@ -4,9 +4,8 @@
 {% block titletag %}{{ page_title }} {{ page_subtitle }}{% endblock %}
 
 {% block content %}
-    {% if can_add %}
-        {% url view.add_url_name as add_link %}
-        {% include "wagtailadmin/shared/header.html" with title=page_title subtitle=page_subtitle action_url=add_link action_text=view.add_item_label icon=header_icon only %}
+    {% if add_url %}
+        {% include "wagtailadmin/shared/header.html" with title=page_title subtitle=page_subtitle action_url=add_url action_text=add_item_label icon=header_icon only %}
     {% else %}
         {% include "wagtailadmin/shared/header.html" with title=page_title subtitle=page_subtitle icon=header_icon only %}
     {% endif %}

wagtail/admin/views/generic/models.py
@@ -1,4 +1,5 @@
 from django import VERSION as DJANGO_VERSION
+from django.core.exceptions import ImproperlyConfigured
 from django.db import transaction
 from django.forms import Form
 from django.http import HttpResponseRedirect
@@ -62,6 +63,7 @@ class IndexView(
     model = None
     index_url_name = None
     add_url_name = None
+    add_item_label = _("Add")
     edit_url_name = None
     template_name = "wagtailadmin/generic/index.html"
     context_object_name = None
@@ -141,6 +143,10 @@ class IndexView(
         if self.edit_url_name:
             return reverse(self.edit_url_name, args=(instance.pk,))
 
+    def get_add_url(self):
+        if self.add_url_name:
+            return reverse(self.add_url_name)
+
     def get_valid_orderings(self):
         orderings = []
         for col in self.columns:
@@ -169,6 +175,10 @@ class IndexView(
             self.permission_policy is None
             or self.permission_policy.user_has_permission(self.request.user, "add")
         )
+        if context["can_add"]:
+            context["add_url"] = self.get_add_url()
+            context["add_item_label"] = self.add_item_label
+
         context["table"] = table
         context["media"] = table.media
         context["index_url"] = index_url
@@ -201,9 +211,19 @@ class CreateView(
     submit_button_label = gettext_lazy("Create")
 
     def get_add_url(self):
+        if not self.add_url_name:
+            raise ImproperlyConfigured(
+                "Subclasses of wagtail.admin.views.generic.models.CreateView must provide an "
+                "add_url_name attribute or a get_add_url method"
+            )
         return reverse(self.add_url_name)
 
     def get_success_url(self):
+        if not self.index_url_name:
+            raise ImproperlyConfigured(
+                "Subclasses of wagtail.admin.views.generic.models.CreateView must provide an "
+                "index_url_name attribute or a get_success_url method"
+            )
         return reverse(self.index_url_name)
 
     def get_success_message(self, instance):
@@ -289,12 +309,23 @@ class EditView(
         return str(self.object)
 
     def get_edit_url(self):
+        if not self.edit_url_name:
+            raise ImproperlyConfigured(
+                "Subclasses of wagtail.admin.views.generic.models.EditView must provide an "
+                "edit_url_name attribute or a get_edit_url method"
+            )
         return reverse(self.edit_url_name, args=(self.object.id,))
 
     def get_delete_url(self):
-        return reverse(self.delete_url_name, args=(self.object.id,))
+        if self.delete_url_name:
+            return reverse(self.delete_url_name, args=(self.object.id,))
 
     def get_success_url(self):
+        if not self.index_url_name:
+            raise ImproperlyConfigured(
+                "Subclasses of wagtail.admin.views.generic.models.EditView must provide an "
+                "index_url_name attribute or a get_success_url method"
+            )
         return reverse(self.index_url_name)
 
     def save_instance(self):
@@ -350,12 +381,13 @@ class EditView(
         context = super().get_context_data(**kwargs)
         context["action_url"] = self.get_edit_url()
         context["submit_button_label"] = self.submit_button_label
-        context["delete_url"] = self.get_delete_url()
-        context["delete_item_label"] = self.delete_item_label
         context["can_delete"] = (
             self.permission_policy is None
             or self.permission_policy.user_has_permission(self.request.user, "delete")
         )
+        if context["can_delete"]:
+            context["delete_url"] = self.get_delete_url()
+            context["delete_item_label"] = self.delete_item_label
         return context
 
 
@@ -381,12 +413,22 @@ class DeleteView(
         return super().get_object(queryset)
 
     def get_success_url(self):
+        if not self.index_url_name:
+            raise ImproperlyConfigured(
+                "Subclasses of wagtail.admin.views.generic.models.DeleteView must provide an "
+                "index_url_name attribute or a get_success_url method"
+            )
         return reverse(self.index_url_name)
 
     def get_page_subtitle(self):
         return str(self.object)
 
     def get_delete_url(self):
+        if not self.index_url_name:
+            raise ImproperlyConfigured(
+                "Subclasses of wagtail.admin.views.generic.models.DeleteView must provide a "
+                "delete_url_name attribute or a get_delete_url method"
+            )
         return reverse(self.delete_url_name, args=(self.object.id,))
 
     def get_success_message(self):

commit 7e84879641307ce4a459a5a9bfa0951d1b44e18a

Fix delete button from showing up in collections edit view for users with insufficient permissions
Regression in 45ac80bb8994dafe62e090c658030fe4438ff0b6.
The template no longer checks for can_delete. Instead, it generates the
delete_url based on can_delete in the generic EditView's
get_context_data(). As a result, any modification to the can_delete
context value by a subclass is ignored.

Also add a small optimisation by using
user_has_any_permission_for_instance instead of making a query for
objects that the user has delete permission for and fetching the
object with the same ID.

wagtail/admin/templates/wagtailadmin/generic/edit.html
@@ -3,7 +3,7 @@
 
 {% block actions %}
     {{ block.super }}
-    {% if delete_url %}
+    {% if can_delete and delete_url %}
         <a href="{{ delete_url }}" class="button no">{{ delete_item_label }}</a>
     {% endif %}
 {% endblock %}

wagtail/admin/views/collections.py
@@ -140,12 +140,8 @@ class Edit(EditView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["can_delete"] = (
-            self.permission_policy.instances_user_has_permission_for(
-                self.request.user, "delete"
-            )
-            .filter(pk=self.object.pk)
-            .first()
+        context["can_delete"] = self.permission_policy.user_has_permission_for_instance(
+            self.request.user, "delete", self.object
         )
         return context
 

wagtail/admin/tests/test_collections_views.py
@@ -549,6 +549,17 @@ class TestEditCollection(CollectionInstanceTestUtils, WagtailTestUtils, TestCase
         # Retrieve edit form and check fields
         response = self.get(collection_id=self.marketing_sub_collection.id)
         self.assertNotContains(response, "Delete collection")
+        # Add delete permission to a different collection and try again,
+        # ensure that it checks against the tree structure, and not just a
+        # "delete collection" permission on any collection
+        # See https://github.com/wagtail/wagtail/issues/10084
+        GroupCollectionPermission.objects.create(
+            group=self.marketing_group,
+            collection=self.marketing_sub_collection_2,
+            permission=self.delete_permission,
+        )
+        response = self.get(collection_id=self.marketing_sub_collection.id)
+        self.assertNotContains(response, "Delete collection")
         # Add delete permission to parent collection and try again
         GroupCollectionPermission.objects.create(
             group=self.marketing_group,