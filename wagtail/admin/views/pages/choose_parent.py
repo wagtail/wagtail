148c97ba4132c0d875cf1c5b2ee66b4bef640142
Matthias Brück <hi@brueck.io>
2024-05-23 21:04:31 +1000

    Skip ChooseParentView if only one possible valid parent page available to the user

--- wagtail/admin/views/pages/choose_parent.py
+++ wagtail/admin/views/pages/choose_parent.py
@@ -6,10 +6,13 @@ from django.utils.text import capfirst
 from django.utils.translation import gettext as _
 from django.utils.translation import gettext_lazy
 from django.views.generic import FormView
+from django.contrib.contenttypes.models import ContentType
 
 from wagtail.admin.forms.pages import ParentChooserForm
 from wagtail.admin.views.generic.base import WagtailAdminTemplateMixin
 from wagtail.models import Page
+
+
 
 
 class ChooseParentView(WagtailAdminTemplateMixin, FormView):
@@ -17,6 +20,69 @@ class ChooseParentView(WagtailAdminTemplateMixin, FormView):
     model = Page
     index_url_name = None
     page_title = gettext_lazy("Choose parent")
+
+    def get_valid_parent_pages(self, user):
+        """
+        Identifies possible parent pages for the current user by first looking
+        at allowed_parent_page_models() on self.model to limit options to the
+        correct type of page, then checking permissions on those individual
+        pages to make sure we have permission to add a subpage to it.
+        """
+        # Get queryset of pages where this page type can be added
+        allowed_parent_page_content_types = list(
+            ContentType.objects.get_for_models(
+                *self.model.allowed_parent_page_models()
+            ).values()
+        )
+        allowed_parent_pages = Page.objects.filter(
+            content_type__in=allowed_parent_page_content_types
+        )
+
+        # Get queryset of pages where the user has permission to add subpages
+        if user.is_superuser:
+            pages_where_user_can_add = Page.objects.all()
+        else:
+            pages_where_user_can_add = Page.objects.none()
+
+            from wagtail.permission_policies.pages import PagePermissionPolicy
+
+            perms = {
+                perm
+                for perm in PagePermissionPolicy().get_cached_permissions_for_user(user)
+                if perm.permission.codename == "add_page"
+            }
+
+            for perm in perms:
+                # user has add permission on any subpage of perm.page
+                # (including perm.page itself)
+                pages_where_user_can_add |= Page.objects.descendant_of(
+                    perm.page, inclusive=True
+                )
+
+        # Combine them
+        return allowed_parent_pages & pages_where_user_can_add
+
+
+    def dispatch(self, request, *args, **kwargs):
+        parents = self.get_valid_parent_pages(request.user)
+
+        # There's only one available parent for this page type for this
+        # user, so we send them along with that as the chosen parent page
+        if len(parents) == 1:
+            parent = parents[0]
+            parent_id = quote(parent.pk)
+            model_opts = self.model._meta
+            # raise Exception(parent)
+            return redirect(
+                "wagtailadmin_pages:add",
+                model_opts.app_label,
+                model_opts.model_name,
+                parent_id,
+            )
+
+            # The page can be added in multiple places, so redirect to the
+            # choose_parent view so that the parent can be specified
+        return super().dispatch(request, *args, **kwargs)
 
     def get_form(self):
         if self.request.method == "POST":
