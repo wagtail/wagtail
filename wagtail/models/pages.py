8654aadb6380f2a86db04097a7bd08f6acb13817
smark-1 <75799735+smark-1@users.noreply.github.com>
2024-09-02 14:38:33 -0400

    Add method to set a default privacy option on Page models
    Fixes #3708

--- wagtail/models/pages.py
+++ wagtail/models/pages.py
@@ -1429,6 +1429,10 @@ class Page(AbstractPage, index.Indexed, ClusterableModel, metaclass=PageBase):
         except self.specific_class.DoesNotExist:
             return None
 
+    def get_default_privacy_setting(self, request: HttpRequest):
+        """Set the default privacy setting for a page."""
+        return {"type": BaseViewRestriction.NONE}
+
     @classmethod
     def clean_subpage_models(cls):
         """
