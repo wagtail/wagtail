d94b645f9b4b697ce1d289c76971ee6d11f0c05c
Sage Abdullah <sage.abdullah@torchbox.com>
2024-09-11 14:27:54 +0100

    Deprecate UserListingButton and SnippetListingButton in favor of Button

--- wagtail/users/widgets.py
+++ wagtail/users/widgets.py
@@ -1,5 +1,15 @@
+from warnings import warn
+
 from wagtail.admin.widgets import Button
+from wagtail.utils.deprecation import RemovedInWagtail70Warning
 
 
 class UserListingButton(Button):
-    pass
+    def __init__(self, *args, **kwargs):
+        warn(
+            "`UserListingButton` is deprecated. "
+            "Use `wagtail.admin.widgets.button.Button` "
+            "or `wagtail.admin.widgets.button.ListingButton` instead.",
+            category=RemovedInWagtail70Warning,
+        )
+        super().__init__(*args, **kwargs)
