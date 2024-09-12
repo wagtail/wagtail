wagtail/wagtail

commit b6fdd8c408047a8eaa2f06374b40004f95924cbf

Move UserSearchArea registration to UserViewSet
In a similar fashion to how we register the menu items

wagtail/users/views/users.py
@@ -17,6 +17,7 @@ from django.utils.translation import gettext_lazy
 
 from wagtail import hooks
 from wagtail.admin.filters import DateRangePickerWidget, WagtailFilterSet
+from wagtail.admin.search import SearchArea
 from wagtail.admin.ui.tables import (
     BulkActionsCheckboxColumn,
     Column,
@@ -43,7 +44,8 @@ User = get_user_model()
 
 # Typically we would check the permission 'auth.change_user' (and 'auth.add_user' /
 # 'auth.delete_user') for user management actions, but this may vary according to
-# the AUTH_USER_MODEL setting
+# the AUTH_USER_MODEL setting. These are no longer used in the codebase in favour
+# of ModelPermissionPolicy, but are kept here for backwards compatibility.
 add_user_perm = f"{AUTH_USER_APP_LABEL}.add_{AUTH_USER_MODEL_NAME.lower()}"
 change_user_perm = "{}.change_{}".format(
     AUTH_USER_APP_LABEL, AUTH_USER_MODEL_NAME.lower()
@@ -401,3 +403,29 @@ class UserViewSet(ModelViewSet):
         if for_update:
             return get_user_edit_form()
         return get_user_creation_form()
+
+    @cached_property
+    def search_area_class(self):
+        class UsersSearchArea(SearchArea):
+            def is_shown(search_area, request):
+                return self.permission_policy.user_has_any_permission(
+                    request.user, {"add", "change", "delete"}
+                )
+
+        return UsersSearchArea
+
+    def get_search_area(self):
+        return self.search_area_class(
+            gettext_lazy("Users"),
+            self.get_url_name("index"),
+            name="users",
+            icon_name="user",
+            order=600,
+        )
+
+    def register_search_area(self):
+        hooks.register("register_admin_search_area", self.get_search_area)
+
+    def on_register(self):
+        super().on_register()
+        self.register_search_area()

wagtail/users/wagtail_hooks.py
@@ -1,12 +1,8 @@
 from django.apps import apps
 from django.core.exceptions import ImproperlyConfigured
-from django.urls import reverse
 from django.utils.module_loading import import_string
-from django.utils.translation import gettext_lazy as _
 
 from wagtail import hooks
-from wagtail.admin.search import SearchArea
-from wagtail.compat import AUTH_USER_APP_LABEL, AUTH_USER_MODEL_NAME
 from wagtail.users.views.bulk_actions import (
     AssignRoleBulkAction,
     DeleteBulkAction,
@@ -33,38 +29,6 @@ def register_viewset():
         user_viewset_cls("wagtailusers_users", url_prefix="users"),
         group_viewset_cls("wagtailusers_groups", url_prefix="groups"),
     ]
-
-
-# Typically we would check the permission 'auth.change_user' (and 'auth.add_user' /
-# 'auth.delete_user') for user management actions, but this may vary according to
-# the AUTH_USER_MODEL setting
-add_user_perm = f"{AUTH_USER_APP_LABEL}.add_{AUTH_USER_MODEL_NAME.lower()}"
-change_user_perm = "{}.change_{}".format(
-    AUTH_USER_APP_LABEL, AUTH_USER_MODEL_NAME.lower()
-)
-delete_user_perm = "{}.delete_{}".format(
-    AUTH_USER_APP_LABEL, AUTH_USER_MODEL_NAME.lower()
-)
-
-
-class UsersSearchArea(SearchArea):
-    def is_shown(self, request):
-        return (
-            request.user.has_perm(add_user_perm)
-            or request.user.has_perm(change_user_perm)
-            or request.user.has_perm(delete_user_perm)
-        )
-
-
-@hooks.register("register_admin_search_area")
-def register_users_search_area():
-    return UsersSearchArea(
-        _("Users"),
-        reverse("wagtailusers_users:index"),
-        name="users",
-        icon_name="user",
-        order=600,
-    )
 
 
 for action_class in [AssignRoleBulkAction, DeleteBulkAction, SetActiveStateBulkAction]:

commit 38d368c60ba00d512843bb2809fdfb32d5b20dda

Fix link to Users search in the "Other searches" bar
Regression in b6fdd8c408047a8eaa2f06374b40004f95924cbf

wagtail/users/views/users.py
@@ -11,6 +11,7 @@ from django.core.exceptions import FieldDoesNotExist, PermissionDenied
 from django.db.models import Q
 from django.forms import CheckboxSelectMultiple
 from django.template import RequestContext
+from django.urls import reverse
 from django.utils.functional import cached_property
 from django.utils.translation import gettext as _
 from django.utils.translation import gettext_lazy
@@ -429,7 +430,7 @@ class UserViewSet(ModelViewSet):
     def get_search_area(self):
         return self.search_area_class(
             gettext_lazy("Users"),
-            self.get_url_name("index"),
+            reverse(self.get_url_name("index")),
             name="users",
             icon_name="user",
             order=600,

wagtail/users/tests/test_admin_views.py
@@ -226,6 +226,12 @@ class TestUserIndexView(AdminTemplateTestUtils, WagtailTestUtils, TestCase):
         response = self.get({"q": "Hello"})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context["query_string"], "Hello")
+        soup = self.get_soup(response.content)
+        filter_options = soup.select(".filter-options a")
+        self.assertIn(
+            ("Users", reverse("wagtailusers_users:index") + "?q=Hello"),
+            [(a.text.strip(), a.get("href")) for a in filter_options],
+        )
 
     def test_search_query_one_field(self):
         response = self.get({"q": "first name"})