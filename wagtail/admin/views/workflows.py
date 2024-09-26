49f9ae3a96dc9baf3e24ef6ad202b9a3472df19f
Sage Abdullah <sage.abdullah@torchbox.com>
2024-09-26 18:10:00 -0400

    Fix error in workflows listing with multiple snippets assigned on PostgreSQL (#12367)
    Fixes #12364

--- wagtail/admin/views/workflows.py
+++ wagtail/admin/views/workflows.py
@@ -4,7 +4,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import PermissionDenied
 from django.core.paginator import Paginator
 from django.db import transaction
-from django.db.models import Count, OuterRef, Prefetch
+from django.db.models import Count, Prefetch
 from django.db.models.functions import Lower
 from django.http import Http404, HttpResponseBadRequest
 from django.shortcuts import get_object_or_404, redirect, render
@@ -43,7 +43,6 @@ from wagtail.models import (
     Task,
     TaskState,
     Workflow,
-    WorkflowContentType,
     WorkflowState,
     WorkflowTask,
 )
@@ -151,10 +150,7 @@ class Index(IndexView):
 
     def get_base_queryset(self):
         queryset = super().get_base_queryset()
-        content_types = WorkflowContentType.objects.filter(
-            workflow=OuterRef("pk")
-        ).values_list("pk", flat=True)
-        queryset = queryset.annotate(content_types=Count(content_types))
+        queryset = queryset.annotate(content_types=Count("workflow_content_types"))
         return queryset.prefetch_related(
             "workflow_pages",
             "workflow_pages__page",
