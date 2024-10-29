883a95cb63e20da2bcb10b6c801ff08ad37df783
ashish-nagmoti <ashishnagmoti5@gmail.com>
2024-10-29 23:30:59 +0530

    added support for offline documentaion

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-@*uhf!dcb&2$-p=njasr07v5(ebu_&ocnajztt#^dgejp0o(6r"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
