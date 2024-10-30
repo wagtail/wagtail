a1909b0a6d5bd4f3f469ce06b16263ca5c24e6a3
Joel William <joelwilliam.work@gmail.com>
2024-10-30 14:39:23 +0530

    adding epub.unknown_mimetype suppression

--- docs/conf.py
+++ docs/conf.py
@@ -327,6 +327,7 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 # texinfo_no_detailmenu = False
 
+suppress_warnings = ['epub.unknown_project_files']
 
 def setup(app):
     app.add_js_file("js/banner.js")
