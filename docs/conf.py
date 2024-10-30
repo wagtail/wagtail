18e5071ab1dea711e87645635d4ed53449a79585
Sage Abdullah <sage.abdullah@torchbox.com>
2024-10-30 13:32:51 +0000

    Replace Torchbox with 'Wagtail core team and contributors' in docs config

--- docs/conf.py
+++ docs/conf.py
@@ -109,7 +109,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "Wagtail Documentation"
-copyright = f"{datetime.now().year}, Torchbox and contributors. BSD license"
+copyright = f"{datetime.now().year}, Wagtail core team and contributors. BSD license"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -275,7 +275,13 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    ("index", "Wagtail.tex", "Wagtail Documentation", "Torchbox", "manual"),
+    (
+        "index",
+        "Wagtail.tex",
+        "Wagtail Documentation",
+        "Wagtail core team and contributors",
+        "manual",
+    ),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -302,7 +308,15 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [("index", "wagtail", "Wagtail Documentation", ["Torchbox"], 1)]
+man_pages = [
+    (
+        "index",
+        "wagtail",
+        "Wagtail Documentation",
+        ["Wagtail core team and contributors"],
+        1,
+    )
+]
 
 # If true, show URL addresses after external links.
 # man_show_urls = False
@@ -317,9 +331,9 @@ texinfo_documents = [
         "index",
         "Wagtail",
         "Wagtail Documentation",
-        "Torchbox",
+        "Wagtail core team and contributors",
         "Wagtail",
-        "One line description of project.",
+        "An open source content management system built on Django.",
         "Miscellaneous",
     ),
 ]
