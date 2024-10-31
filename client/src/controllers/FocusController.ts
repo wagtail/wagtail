3c09da2fb8423b236c21611fe639768e68906883
LB <mail@lb.ee>
2024-10-31 21:39:04 +1000

    Enhance & rework FocusController
    - Update the Controller import & identifier to be `w-focus` not `w-skip-link`
    - Rework methods to be easier to maintain, pull out the focus event attachment handler to a function
    - Add JSDoc examples & refine description
    - Only add then remove the tabindex attribute if needed
    - Rework unit tests to be the emerging common structure, add unit test coverage & test for the preservation of existing tabindex attributes

--- client/src/controllers/FocusController.ts
+++ client/src/controllers/FocusController.ts
@@ -1,33 +1,56 @@
 import { Controller } from '@hotwired/stimulus';
 
+import { forceFocus } from '../utils/forceFocus';
+
 /**
- * Appears at the top left corner of the admin page with the tab button is clicked.
+ * Allows a target element (either via `href` or the targetValue as a selector) to be focused.
+ * If the element does not have a `tabindex` attribute, it will be added and removed when the element loses focus.
+ *
+ * @description
+ * Useful for the skip link functionality, which appears at the top left corner of the admin when the tab button is clicked.
  * Used to provide an accessible skip button for keyboard control so that users can
  * easily navigate to the main content without having to navigate a long list of navigation links.
- *
  * Inspired by https://github.com/selfthinker/dokuwiki_template_writr/blob/master/js/skip-link-focus-fix.js
+ *
+ * @example As an accessible skip link
+ * ```html
+ * <a href="#main" data-controller="w-focus" data-action="w-focus#focus:prevent">Skip to main content</a>
+ * ```
+ *
+ * @example As a button to skip to the top of a section
+ * ```html
+ * <section>
+ *   <div class="section-top"><h3>Section title</h3></div>
+ *   <p>...lots of content...</p>
+ *   <button type="button" data-controller="w-focus" data-w-focus-target-value=".section-top" data-action="w-focus#focus">Skip to top</button>
+ * </section>
+ * ```
  */
-export class SkipLinkController extends Controller<HTMLAnchorElement> {
-  skipToTarget?: HTMLElement | null;
+export class FocusController extends Controller<
+  HTMLAnchorElement | HTMLButtonElement
+> {
+  static values = {
+    target: String,
+  };
 
-  connect() {
-    this.skipToTarget = document.querySelector(
-      this.element.getAttribute('href') || 'main',
+  declare targetValue: string;
+
+  get target() {
+    const selector =
+      this.targetValue || this.element.getAttribute('href') || 'main';
+
+    return (
+      this.element.closest<HTMLElement>(selector) ||
+      document.querySelector<HTMLElement>(selector)
     );
   }
 
-  handleBlur() {
-    if (!this.skipToTarget) return;
-    this.skipToTarget.removeAttribute('tabindex');
-    this.skipToTarget.removeEventListener('blur', this.handleBlur);
-    this.skipToTarget.removeEventListener('focusout', this.handleBlur);
-  }
+  focus() {
+    const target = this.target;
 
-  skip() {
-    if (!this.skipToTarget) return;
-    this.skipToTarget.setAttribute('tabindex', '-1');
-    this.skipToTarget.addEventListener('blur', this.handleBlur);
-    this.skipToTarget.addEventListener('focusout', this.handleBlur);
-    this.skipToTarget.focus();
+    if (target) {
+      forceFocus(target);
+      this.dispatch('focused', { bubbles: true, cancelable: false, target });
+    }
   }
 }
