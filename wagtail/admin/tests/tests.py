e5966a75492470d6a0f0fb396a4d5d495b400b60
Aayushman Singh <aayushman2702@gmail.com>
2024-12-01 11:52:11 +0530

    Reverting changes to tests.py

--- wagtail/admin/tests/tests.py
+++ wagtail/admin/tests/tests.py
@@ -13,7 +13,6 @@ from taggit.models import Tag
 from wagtail.admin.auth import user_has_any_page_permission
 from wagtail.admin.mail import send_mail
 from wagtail.admin.menu import MenuItem
-from wagtail.admin.views.tags import WAGTAIL_TAGS_AUTOCOMPLETE_LIMIT
 from wagtail.models import Page
 from wagtail.test.testapp.models import RestaurantTag
 from wagtail.test.utils import WagtailTestUtils
