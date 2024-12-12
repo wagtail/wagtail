c6621c13b7b2deaa9cfb74767c914dfc7bc27627
Sage Abdullah <sage.abdullah@torchbox.com>
2024-12-12 15:21:40 +0000

    Move images and documents get_usage().count() call to view code
    The get_usage() method returns a ReferenceGroups instance that defines a
    __getitem__ method. Accessing get_usage().count() from the template
    means that Django tries to access the count via ["count"], which fails,
    then continues by using getattr(reference_groups, "count") before
    finally calling the count method.
    
    We have seen reports where the blocktranslate tag fails because the
    usage_count_val is not a number. We haven't got a reproducible example,
    but this would help surface any errors, as the exception would be raised
    from the Python code rather than the template.

--- wagtail/images/views/images.py
+++ wagtail/images/views/images.py
@@ -186,6 +186,7 @@ class EditView(generic.EditView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["next"] = self.next_url
+        context["usage_count_val"] = self.object.get_usage().count()
 
         try:
             context["filesize"] = self.object.get_file_size()
