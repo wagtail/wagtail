03ec49a55d0843cd2d9366c051d9cc85f95b99b7
onno <onno@code-on.be>
2024-12-26 10:50:07 +0100

    added char counter for the meta title and description. Improvement for screen readers prettier fix

--- client/src/entrypoints/admin/seo_char_count.js
+++ client/src/entrypoints/admin/seo_char_count.js
@@ -1,33 +1,32 @@
 (function () {
-    function updateCharCount(inputId) {
-        const input = document.getElementById(inputId);
-        if (!input) {
-            return;
-        }
-
-        const label = document.querySelector('label[for="' + inputId + '"]');
-        if (!label) {
-            return;
-        }
-
-        const originalText = label.textContent.trim();
-        if (!originalText) {
-            return;
-        }
-
-        const span = document.createElement('span');
-        span.setAttribute('aria-live', 'polite');
-        label.appendChild(span);
-
-        function refreshLabel() {
-            span.textContent = ` (${input.value.length})`;
-        }
-
-        refreshLabel();
-        input.addEventListener('input', refreshLabel);
+  function updateCharCount(inputId) {
+    const input = document.getElementById(inputId);
+    if (!input) {
+      return;
     }
 
-    updateCharCount('id_seo_title');
-    updateCharCount('id_search_description');
-})();
+    const label = document.querySelector('label[for="' + inputId + '"]');
+    if (!label) {
+      return;
+    }
 
+    const originalText = label.textContent.trim();
+    if (!originalText) {
+      return;
+    }
+
+    const span = document.createElement('span');
+    span.setAttribute('aria-live', 'polite');
+    label.appendChild(span);
+
+    function refreshLabel() {
+      span.textContent = ` (${input.value.length})`;
+    }
+
+    refreshLabel();
+    input.addEventListener('input', refreshLabel);
+  }
+
+  updateCharCount('id_seo_title');
+  updateCharCount('id_search_description');
+})();
