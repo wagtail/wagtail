3f91fcb3a31e5ce3f0a8cbff4540bce4587ea805
Matt Westcott <matt@west.co.tt>
2025-01-03 17:05:40 +0000

    Define base Page panels as placeholders within wagtail.models
    This ensures that code such as `content_panels = Page.content_panels + [...]` works as expected even if wagtail.admin has not been loaded.
    
    Fixes #12747

--- wagtail/admin/panels/page_utils.py
+++ wagtail/admin/panels/page_utils.py
@@ -1,50 +1,12 @@
-from django.conf import settings
 from django.utils.translation import gettext_lazy
 
 from wagtail.admin.forms.pages import WagtailAdminPageForm
 from wagtail.models import Page
 from wagtail.utils.decorators import cached_classmethod
 
-from .comment_panel import CommentPanel
-from .field_panel import FieldPanel
-from .group import MultiFieldPanel, ObjectList, TabbedInterface
-from .publishing_panel import PublishingPanel
-from .title_field_panel import TitleFieldPanel
+from .group import ObjectList, TabbedInterface
 
-
-def set_default_page_edit_handlers(cls):
-    cls.content_panels = [
-        TitleFieldPanel("title"),
-    ]
-
-    cls.promote_panels = [
-        MultiFieldPanel(
-            [
-                FieldPanel("slug"),
-                FieldPanel("seo_title"),
-                FieldPanel("search_description"),
-            ],
-            gettext_lazy("For search engines"),
-        ),
-        MultiFieldPanel(
-            [
-                FieldPanel("show_in_menus"),
-            ],
-            gettext_lazy("For site menus"),
-        ),
-    ]
-
-    cls.settings_panels = [
-        PublishingPanel(),
-    ]
-
-    if getattr(settings, "WAGTAILADMIN_COMMENTS_ENABLED", True):
-        cls.settings_panels.append(CommentPanel())
-
-    cls.base_form_class = WagtailAdminPageForm
-
-
-set_default_page_edit_handlers(Page)
+Page.base_form_class = WagtailAdminPageForm
 
 
 @cached_classmethod
