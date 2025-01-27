b7d7e18c58e3fbbb72e7b049ef461bd1203698aa
Matt Westcott <matt@west.co.tt>
2025-01-27 16:44:31 +0000

    Prevent database error when calling permission_order.register on app ready
    Fixes https://github.com/wagtail/wagtail/issues/12742
    
    Previously, `permission_order.register` performed a database lookup for the content type. This is invalid if called from an app `ready` method as the documentation suggests, because this may run before the database has been initialised. Instead, `register` now queues up the arguments it receives, and the content type lookup is constructed lazily on first call to `get_content_type_order_lookup` (which happens when the group edit view is requested).

--- wagtail/users/templatetags/wagtailusers_tags.py
+++ wagtail/users/templatetags/wagtailusers_tags.py
@@ -4,7 +4,7 @@ import re
 from django import template
 
 from wagtail import hooks
-from wagtail.users.permission_order import CONTENT_TYPE_ORDER
+from wagtail.users.permission_order import get_content_type_order_lookup
 
 register = template.Library()
 
@@ -42,9 +42,10 @@ def format_permissions(permission_bound_field):
     # get a distinct and ordered list of the content types that these permissions relate to.
     # relies on Permission model default ordering, dict.fromkeys() retaining that order
     # from the queryset, and the stability of sorted().
+    content_type_order = get_content_type_order_lookup()
     content_type_ids = sorted(
         dict.fromkeys(permissions.values_list("content_type_id", flat=True)),
-        key=lambda ct: CONTENT_TYPE_ORDER.get(ct, float("inf")),
+        key=lambda ct: content_type_order.get(ct, float("inf")),
     )
 
     # iterate over permission_bound_field to build a lookup of individual renderable
