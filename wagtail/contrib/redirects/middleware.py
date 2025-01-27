8ca9355ea4f1b2f0b8e5754cc06a4a6100f25962
Andy Babic <andy.babic@torchbox.com>
2025-01-27 20:22:52 +0000

    Rewrite middleware.get_redirect() to identify the best match using a single database query (and take into account that the provided 'path' should be an iri rather than a uri)

--- wagtail/contrib/redirects/middleware.py
+++ wagtail/contrib/redirects/middleware.py
@@ -1,35 +1,64 @@
 from urllib.parse import urlparse
 
 from django import http
+from django.db.models import Case, IntegerField, Q, When
 from django.utils.deprecation import MiddlewareMixin
-from django.utils.encoding import uri_to_iri
+from django.utils.encoding import iri_to_uri
 
 from wagtail.contrib.redirects import models
 from wagtail.models import Site
 
 
-def _get_redirect(request, path):
-    if (
-        "\0" in path
-    ):  # reject URLs with null characters, which crash on Postgres (#4496)
+def get_redirect(request, path):
+    if "\0" in path:
+        # reject paths with null characters, which crash on Postgres (#4496)
         return None
 
     site = Site.find_for_request(request)
-    try:
-        return models.Redirect.get_for_site(site).get(old_path=path)
-    except models.Redirect.MultipleObjectsReturned:
-        # We have a site-specific and a site-ambivalent redirect; prefer the specific one
-        return models.Redirect.objects.get(site=site, old_path=path)
-    except models.Redirect.DoesNotExist:
-        return None
 
+    encoded_path = iri_to_uri(path)
+    match_paths = [path, encoded_path]
+    path_without_query = urlparse(path).path
+    if path_without_query != path:
+        encoded_path_without_query = iri_to_uri(path_without_query)
+        match_paths.extend([path_without_query, encoded_path_without_query])
+    else:
+        encoded_path_without_query = encoded_path
 
-def get_redirect(request, path):
-    redirect = _get_redirect(request, path)
-    if not redirect:
-        # try unencoding the path
-        redirect = _get_redirect(request, uri_to_iri(path))
-    return redirect
+    queryset = models.Redirect.objects.filter(old_path__in=match_paths)
+    if site:
+        queryset = queryset.filter(Q(site=site) | Q(site__isnull=True)).annotate(
+            priority=Case(
+                # A full-url match for the current site is most favourable
+                # encoded matches less so, as they are likely to be less recent
+                When(site_id=site.id, old_path=path, then=1),
+                When(site_id=site.id, old_path=encoded_path, then=2),
+                # A site-ambivalent full-url is next best
+                # Again, encoded matches are slightly less favourable
+                When(site_id=None, old_path=path, then=3),
+                When(site_id=None, old_path=encoded_path, then=4),
+                # A path-only match for the current site is next best
+                # Again, encoded matches are slightly less favourable
+                When(site_id=site.id, old_path=path_without_query, then=5),
+                When(site_id=site.id, old_path=encoded_path_without_query, then=6),
+                # A decodeed site-ambivalent path-only match is next best
+                When(site_id=None, old_path=path_without_query, then=7),
+                # Anything else must be an encoded site-ambivalent path-only match
+                default=8,
+                output_field=IntegerField(),
+            )
+        )
+    else:
+        queryset = queryset.filter(site__isnull=True).annotate(
+            priority=Case(
+                When(old_path=path, then=1),
+                When(old_path=encoded_path, then=2),
+                When(old_path=path_without_query, then=3),
+                default=4,
+                output_field=IntegerField(),
+            )
+        )
+    return queryset.order_by("priority").first()
 
 
 # Originally pinched from: https://github.com/django/django/blob/main/django/contrib/redirects/middleware.py
