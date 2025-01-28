78decc21bf17ed77966a960238d5330f8765506c
Andy Babic <andy.babic@torchbox.com>
2025-01-28 08:57:43 +0000

    Update get_redirect() to assemble 'priority' cases dynamically, depending on the value of 'accept_path_only_matches'

--- wagtail/contrib/redirects/middleware.py
+++ wagtail/contrib/redirects/middleware.py
@@ -1,7 +1,7 @@
 from urllib.parse import urlparse
 
 from django import http
-from django.db.models import Case, IntegerField, Q, When
+from django.db.models import Case, IntegerField, When
 from django.utils.deprecation import MiddlewareMixin
 from django.utils.encoding import iri_to_uri
 
@@ -18,49 +18,73 @@ def get_redirect(request, path, accept_path_only_matches=False):
 
     encoded_path = iri_to_uri(path)
     match_paths = [path, encoded_path]
+    path_without_query = None
+    encoded_path_without_query = None
     if accept_path_only_matches:
         path_without_query = urlparse(path).path
         if path_without_query != path:
             encoded_path_without_query = iri_to_uri(path_without_query)
             match_paths.extend([path_without_query, encoded_path_without_query])
-    else:
-        path_without_query = path
-        encoded_path_without_query = encoded_path
 
-    queryset = models.Redirect.objects.filter(old_path__in=match_paths)
     if site:
-        queryset = queryset.filter(Q(site=site) | Q(site__isnull=True)).annotate(
-            priority=Case(
-                # A full-url match for the current site is most favourable
-                # encoded matches less so, as they are likely to be less recent
-                When(site_id=site.id, old_path=path, then=1),
-                When(site_id=site.id, old_path=encoded_path, then=2),
-                # A site-ambivalent full-url is next best
-                # Again, encoded matches are slightly less favourable
-                When(site_id=None, old_path=path, then=3),
-                When(site_id=None, old_path=encoded_path, then=4),
-                # A path-only match for the current site is next best
-                # Again, encoded matches are slightly less favourable
-                When(site_id=site.id, old_path=path_without_query, then=5),
-                When(site_id=site.id, old_path=encoded_path_without_query, then=6),
-                # A decodeed site-ambivalent path-only match is next best
-                When(site_id=None, old_path=path_without_query, then=7),
-                # Anything else must be an encoded site-ambivalent path-only match
-                default=8,
-                output_field=IntegerField(),
+        # Look for site-specific or site-ambivalent matches
+        queryset = models.Redirect.get_for_site(site)
+
+        # Deterimine default and cases for 'priority' annotation
+        priority_cases = [
+            # A full-url match for the current site is most favourable
+            # encoded matches less so, as they are likely to be less recent
+            When(site_id=site.id, old_path=path, then=1),
+            When(site_id=site.id, old_path=encoded_path, then=2),
+            # A site-ambivalent full-url is next best
+            # Again, encoded matches are slightly less favourable
+            When(site_id=None, old_path=path, then=3),
+        ]
+        default_priority = 4
+        if accept_path_only_matches and path_without_query != path:
+            priority_cases.extend(
+                [
+                    When(site_id=None, old_path=encoded_path, then=4),
+                    # A path-only match for the current site is next best
+                    # Again, encoded matches are slightly less favourable
+                    When(site_id=site.id, old_path=path_without_query, then=5),
+                    When(site_id=site.id, old_path=encoded_path_without_query, then=6),
+                    # A decodeed site-ambivalent path-only match is next best
+                    When(site_id=None, old_path=path_without_query, then=7),
+                ]
             )
-        )
+            default_priority = 8
     else:
-        queryset = queryset.filter(site__isnull=True).annotate(
+        # Look for site-ambivalent matches only
+        queryset = models.Redirect.objects.filter(site__isnull=True)
+
+        # Deterimine cases and default for 'priority' annotation
+        priority_cases = [
+            When(old_path=path, then=1),
+        ]
+        default_priority = 2
+        if accept_path_only_matches and path_without_query != path:
+            priority_cases.extend(
+                [
+                    When(old_path=encoded_path, then=2),
+                    When(old_path=path_without_query, then=3),
+                ]
+            )
+            default_priority = 4
+
+    # Filter, annotate 'priority', and return the best match (or None)
+    return (
+        queryset.filter(old_path__in=match_paths)
+        .annotate(
             priority=Case(
-                When(old_path=path, then=1),
-                When(old_path=encoded_path, then=2),
-                When(old_path=path_without_query, then=3),
-                default=4,
+                *priority_cases,
+                default=default_priority,
                 output_field=IntegerField(),
             )
         )
-    return queryset.order_by("priority").first()
+        .order_by("priority")
+        .first()
+    )
 
 
 # Originally pinched from: https://github.com/django/django/blob/main/django/contrib/redirects/middleware.py
