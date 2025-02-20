faaf5b4dc3c47928b6dd3054d932989fc1d95303
Matt Westcott <matt@west.co.tt>
2025-02-20 19:57:45 +0000

    Refactor page create/edit views to decouple identifying the action from performing it

--- wagtail/admin/views/pages/create.py
+++ wagtail/admin/views/pages/create.py
@@ -8,6 +8,7 @@ from django.shortcuts import get_object_or_404, redirect
 from django.template.response import TemplateResponse
 from django.urls import reverse
 from django.utils import timezone
+from django.utils.functional import cached_property
 from django.utils.translation import gettext as _
 from django.utils.translation import gettext_lazy
 from django.views.generic.base import View
@@ -150,19 +151,31 @@ class CreateView(WagtailAdminTemplateMixin, HookResponseMixin, View):
         else:
             return self.form_invalid(self.form)
 
-    def form_valid(self, form):
+    @cached_property
+    def action_name_and_method(self):
         if (
             bool(self.request.POST.get("action-publish"))
             and self.parent_page_perms.can_publish_subpage()
         ):
-            return self.publish_action()
+            return ("publish", self.publish_action)
         elif (
             bool(self.request.POST.get("action-submit"))
             and self.parent_page.has_workflow
         ):
-            return self.submit_action()
+            return ("submit", self.submit_action)
         else:
-            return self.save_action()
+            return ("save", self.save_action)
+
+    @property
+    def action_name(self):
+        return self.action_name_and_method[0]
+
+    @property
+    def action_method(self):
+        return self.action_name_and_method[1]
+
+    def form_valid(self, form):
+        return self.action_method()
 
     def get_page_subtitle(self):
         return self.page_class.get_verbose_name()
