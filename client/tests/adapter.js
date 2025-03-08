d187f3aee31a69d0559aca035e4a98afb4ff7dc4
LB <mail@lb.ee>
2025-03-09 01:49:50 +1000

    WIP

--- client/tests/adapter.js
+++ client/tests/adapter.js
@@ -4,3 +4,9 @@ import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({
   adapter: new Adapter(),
 });
+
+/** Mock window.scrollTo as not provided via JSDom */
+window.scrollTo = jest.fn();
+
+/** Mock scrollIntoView on elements, this is not provided by JSDom */
+Element.prototype.scrollIntoView = jest.fn();
