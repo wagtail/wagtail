de33a01abfdc430c88775661101eff5d4e7a195d
Jordan Teichmann <jordan.mht@gmail.com>
2025-03-12 23:59:35 -0400

    linting on paginator tests

--- wagtail/admin/tests/test_paginator.py
+++ wagtail/admin/tests/test_paginator.py
@@ -5,7 +5,6 @@ from wagtail.admin.paginator import WagtailPaginator, get_wagtail_paginator_clas
 
 
 class TestWagtailPaginator(TestCase):
-
     def test_elided_page_range(self):
         """
         Test the get_elided_page_range method of WagtailPaginator.
@@ -21,34 +20,32 @@ class TestWagtailPaginator(TestCase):
         test_cases = [
             # (total pages, current page, [expected elided page range])
             (1, 1, [1]),
-
             (3, 1, [1, 2, 3]),
             (3, 2, [1, 2, 3]),
             (3, 3, [1, 2, 3]),
-
             (10, 1, [1, 2, 3, 4, ellipsis, 10]),
             (10, 6, [1, ellipsis, 6, 7, ellipsis, 10]),
             (10, 10, [1, ellipsis, 7, 8, 9, 10]),
-
             # Exmaples of test cases that should fail
             # (3, 1, [1, 2]), # Too few pages
             # (10, 1, [1, 2, 3, 4, ellipsis, 9, 10]), # Too many pages
             # (10, 6, [1, ellipsis, 5, 6, ellipsis, 10]), # Wrong middle position
             # (10, 10, [1, ellipsis, 7, 8, 9]), # Too few pages
         ]
-        
+
         for total_pages, current_page, expected_elided_page_range in test_cases:
-            paginator = WagtailPaginator(list(range(total_pages)), 1) # 1 object per page
+            paginator = WagtailPaginator(
+                list(range(total_pages)), 1
+            )  # 1 object per page
             elided_page_range = paginator.get_elided_page_range(current_page)
             self.assertSequenceEqual(
                 list(elided_page_range),
                 expected_elided_page_range,
-                f"Elided page range failed for total_pages={total_pages}, current_page={current_page}"
+                f"Elided page range failed for total_pages={total_pages}, current_page={current_page}",
             )
 
 
 class TestGetWagtailPaginatorClass(TestCase):
-
     def test_default_paginator_class(self):
         # Test that the default paginator is WagtailPaginator when no setting is provided
         self.assertIs(get_wagtail_paginator_class(), WagtailPaginator)
