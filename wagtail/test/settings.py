cdb03b00d7526a5371b09f657c185f5cc2cfdba5
Sage Abdullah <sage.abdullah@torchbox.com>
2025-03-27 17:48:31 +0700

    Add RangeFilterAdapter to display active RangeFilters

--- wagtail/test/settings.py
+++ wagtail/test/settings.py
@@ -171,6 +171,7 @@ INSTALLED_APPS = [
     "wagtail",
     "taggit",
     "rest_framework",
+    "django_filters",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
