ae467698200109d136d3fc036702ca1fc15e06f1
Sylvain Boissel <sylvain@boissel.dev>
2025-03-31 11:05:54 +0200

    typo and fix value

--- wagtail/project_template/home/apps.py
+++ wagtail/project_template/home/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
+    default_auto_field = "django.db.models.AutoField"
     name = "home"
