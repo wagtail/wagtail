6765cdd0e74f6361b933491334fceff21f635110
Matt Westcott <matt@west.co.tt>
2025-04-02 18:32:59 +0100

    Refactor custom user viewset tests to use post-6.2 mechanism
    Rather than just patching WAGTAIL_USER_EDIT_FORM / WAGTAIL_USER_CREATION_FORM (which are imminently to be removed) when we come to test custom forms, we set up the viewset for CustomUser as per https://docs.wagtail.org/en/stable/releases/6.2.html#deprecation-of-wagtail-user-edit-form-wagtail-user-creation-form-and-wagtail-user-custom-fields-settings so that the custom forms are active whenever CustomUser is (i.e. we're not running the tests with the --emailuser switch). This means we have to adjust the regular create/edit view tests to pass the additional fields `country` and `attachment` when using CustomUser.
    
    We also remove the tests for the get_user_creation_form / get_user_edit_form helpers, as the only purpose of these is to accommodate the deprecated WAGTAIL_USER_EDIT_FORM / WAGTAIL_USER_CREATION_FORM settings.

--- wagtail/users/tests/__init__.py
+++ wagtail/users/tests/__init__.py
@@ -1,13 +1,7 @@
 from .test_admin_views import (
     CustomGroupViewSet,
-    CustomUserCreationForm,
-    CustomUserEditForm,
-    CustomUserViewSet,
 )
 
 __all__ = [
     "CustomGroupViewSet",
-    "CustomUserViewSet",
-    "CustomUserCreationForm",
-    "CustomUserEditForm",
 ]
