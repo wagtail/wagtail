1485d2f328be358453b595ff2fe7753f6bd13871
Jordan Teichmann <jordan.mht@gmail.com>
2025-04-09 12:51:29 -0400

    add num_page_buttons and flexible range length, update tests

--- wagtail/admin/tests/test_paginator.py
+++ wagtail/admin/tests/test_paginator.py
@@ -18,30 +18,72 @@ class TestWagtailPaginator(TestCase):
         ellipsis = WagtailPaginator.ELLIPSIS
 
         test_cases = [
-            # (total pages, current page, [expected elided page range])
-            (1, 1, [1]),
-            (3, 1, [1, 2, 3]),
-            (3, 2, [1, 2, 3]),
-            (3, 3, [1, 2, 3]),
-            (10, 1, [1, 2, 3, 4, ellipsis, 10]),
-            (10, 6, [1, ellipsis, 6, 7, ellipsis, 10]),
-            (10, 10, [1, ellipsis, 7, 8, 9, 10]),
-            # Exmaples of test cases that should fail
-            # (3, 1, [1, 2]), # Too few pages
-            # (10, 1, [1, 2, 3, 4, ellipsis, 9, 10]), # Too many pages
-            # (10, 6, [1, ellipsis, 5, 6, ellipsis, 10]), # Wrong middle position
-            # (10, 10, [1, ellipsis, 7, 8, 9]), # Too few pages
+            # Format: (total pages, current page, num_page_buttons, [expected elided page range])
+            # Exmaples of test cases that should fail:
+            # (3, 1, 6, [1, 2]), # Too few pages
+            # (10, 1, 6, [1, 2, 3, 4, ellipsis, 9, 10]), # Too many pages
+            # (10, 6, 6, [1, ellipsis, 5, 6, ellipsis, 10]), # Wrong middle position
+            # (10, 10, 6, [1, ellipsis, 7, 8, 9]), # Too few pages
+            # num_page_buttons=4
+            # If the number of page buttons is less than 5, the elided range should be empty.
+            (10, 3, 4, []),
+            # num_page_buttons=5
+            (1, 1, 5, [1]),
+            (3, 1, 5, [1, 2, 3]),
+            (3, 3, 5, [1, 2, 3]),
+            (5, 2, 5, [1, 2, 3, 4, 5]),
+            (10, 1, 5, [1, 2, 3, ellipsis, 10]),
+            (10, 6, 5, [1, ellipsis, 6, ellipsis, 10]),
+            (10, 10, 5, [1, ellipsis, 8, 9, 10]),
+            # Our default number of page buttons.
+            # num_page_buttons=6
+            (1, 1, 6, [1]),
+            (3, 1, 6, [1, 2, 3]),
+            (3, 3, 6, [1, 2, 3]),
+            (6, 2, 6, [1, 2, 3, 4, 5, 6]),
+            (6, 6, 6, [1, 2, 3, 4, 5, 6]),
+            (10, 1, 6, [1, 2, 3, 4, ellipsis, 10]),
+            (10, 6, 6, [1, ellipsis, 6, 7, ellipsis, 10]),
+            (10, 10, 6, [1, ellipsis, 7, 8, 9, 10]),
+            # num_page_buttons=7
+            (7, 2, 7, [1, 2, 3, 4, 5, 6, 7]),
+            (10, 1, 7, [1, 2, 3, 4, 5, ellipsis, 10]),
+            (10, 6, 7, [1, ellipsis, 5, 6, 7, ellipsis, 10]),
+            (10, 10, 7, [1, ellipsis, 6, 7, 8, 9, 10]),
+            # num_page_buttons=8
+            (8, 3, 8, [1, 2, 3, 4, 5, 6, 7, 8]),
+            (10, 1, 8, [1, 2, 3, 4, 5, 6, ellipsis, 10]),
+            (20, 6, 8, [1, ellipsis, 5, 6, 7, 8, ellipsis, 20]),
+            (10, 10, 8, [1, ellipsis, 5, 6, 7, 8, 9, 10]),
+            # num_page_buttons=9
+            (9, 3, 9, [1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            (10, 1, 9, [1, 2, 3, 4, 5, 6, 7, ellipsis, 10]),
+            (10, 6, 9, [1, ellipsis, 4, 5, 6, 7, 8, 9, 10]),
+            (20, 6, 9, [1, ellipsis, 4, 5, 6, 7, 8, ellipsis, 20]),
+            (10, 10, 9, [1, ellipsis, 4, 5, 6, 7, 8, 9, 10]),
+            # num_page_buttons=10
+            (10, 3, 10, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+            (10, 1, 10, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+            (10, 6, 10, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+            (20, 6, 10, [1, ellipsis, 4, 5, 6, 7, 8, 9, ellipsis, 20]),
+            (10, 10, 10, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
         ]
 
-        for total_pages, current_page, expected_elided_page_range in test_cases:
+        for (
+            total_pages,
+            current_page,
+            num_page_buttons,
+            expected_elided_page_range,
+        ) in test_cases:
             paginator = WagtailPaginator(
                 list(range(total_pages)), 1
             )  # 1 object per page
+            paginator.num_page_buttons = num_page_buttons
             elided_page_range = paginator.get_elided_page_range(current_page)
             self.assertSequenceEqual(
                 list(elided_page_range),
                 expected_elided_page_range,
-                f"Elided page range failed for total_pages={total_pages}, current_page={current_page}",
+                f"Elided page range failed for total_pages={total_pages}, current_page={current_page}, num_page_buttons={num_page_buttons}",
             )
 
 
