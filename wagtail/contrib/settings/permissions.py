2e56425a7a1c47a652451e3a265acf7b0bd15527
Matt Westcott <matt@west.co.tt>
2025-06-09 19:26:18 +0100

    Apply sites permission policy for site setting views

--- wagtail/contrib/settings/permissions.py
+++ wagtail/contrib/settings/permissions.py
@@ -1,3 +1,21 @@
+from wagtail.permission_policies import ModelPermissionPolicy
+from wagtail.permission_policies.sites import SitePermissionPolicy
+
+
+def get_permission_policy(model):
+    """
+    Get the permission policy for a given model.
+    If the model is a BaseSiteSetting, return SitePermissionPolicy.
+    Otherwise, return ModelPermissionPolicy.
+    """
+    from .models import BaseSiteSetting
+
+    if issubclass(model, BaseSiteSetting):
+        return SitePermissionPolicy(model)
+    else:
+        return ModelPermissionPolicy(model)
+
+
 def user_can_edit_setting_type(user, model):
     """Check if a user has permission to edit this setting type"""
     return user.has_perm(f"{model._meta.app_label}.change_{model._meta.model_name}")
