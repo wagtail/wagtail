f85fd2db4f795565bc562e3f5aee51600d4a3dac
Giovana Fontes <149431855+GiovanaFontesS@users.noreply.github.com>
2025-06-18 14:29:03 -0300

    Add test for user_can_cancel function

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-01u9a27(b@dabzi)rcd%y9awbsxz4**b6k@71+00v6e=13m7jg"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
