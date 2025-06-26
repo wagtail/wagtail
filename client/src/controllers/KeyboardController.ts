e1cf61cc4e6f762af6daaec038855f743ebae13d
Dhruvi Patel <dp7070dp@gmail.com>
2025-06-25 23:12:02 -0400

    Update KeyboardController to support a target element

--- client/src/controllers/KeyboardController.ts
+++ client/src/controllers/KeyboardController.ts
@@ -32,10 +32,20 @@ import { WAGTAIL_CONFIG } from '../config/wagtailConfig';
  * ```html
  * <button type="button" data-controller="w-kbd" aria-keyshortcuts="alt+p">Trigger me with alt+p.</button>
  * ```
+ *
+ * @example - use a target element to trigger the click on a different element
+ * ```html
+ * <section type="button" data-controller="w-kbd" data-w-kbd-key="[">
+ *   Trigger my button with the <kbd>[</kbd> key.
+ *   <button type="button" id="my-button" data-w-kbd-target="element">My Button</button>
+ * </section>
+ * ```
  */
 export class KeyboardController extends Controller<
   HTMLButtonElement | HTMLAnchorElement
 > {
+  static targets = ['element'];
+
   static values = {
     key: { default: '', type: String },
     scope: { default: '', type: String },
@@ -46,6 +56,9 @@ export class KeyboardController extends Controller<
   /** Scope of the keyboard shortcut, defaults to the normal MouseTrap (non-input) scope. */
   declare scopeValue: '' | 'global';
 
+  /** Allow an explicit target to be the element that the keyboard activates instead of the controlled element. */
+  declare readonly elementTargets: (HTMLButtonElement | HTMLAnchorElement)[];
+
   /**
    * If custom keyboard shortcuts are disabled by user in settings then controller will not be loaded
    */
@@ -53,29 +66,47 @@ export class KeyboardController extends Controller<
     return !!WAGTAIL_CONFIG.KEYBOARD_SHORTCUTS_ENABLED;
   }
 
+  /**
+   * Set up the handleKey binding and check for an aria-keyshortcuts attribute to
+   * use as a fallback if the key value is not set.
+   */
   initialize() {
     this.handleKey = this.handleKey.bind(this);
+
     if (!this.keyValue) {
-      const ariaKeyShortcuts = this.element.getAttribute('aria-keyshortcuts');
+      const ariaKeyShortcuts = [...this.elementTargets, this.element]
+        .find((element) => element.hasAttribute('aria-keyshortcuts'))
+        ?.getAttribute('aria-keyshortcuts');
+
       if (ariaKeyShortcuts) {
         this.keyValue = ariaKeyShortcuts;
       }
     }
   }
 
+  /**
+   * Handle the key event by preventing the default action and triggering a click on the target element.
+   */
   handleKey(event: Event) {
     if (event.preventDefault) event.preventDefault();
-    this.element.click();
+    [
+      this.elementTargets.length > 0 ? this.elementTargets[0] : this.element,
+    ].forEach((target) => {
+      target.click();
+    });
   }
 
   /**
    * When a key is set or changed, bind the handler to the keyboard shortcut.
    * This will override the shortcut, if already set.
+   * Allow key to be empty, so it can be removed from the attributes to unbind the shortcuts
    */
   keyValueChanged(key: string, previousKey: string) {
     if (previousKey && previousKey !== key) {
       Mousetrap.unbind(previousKey);
     }
+
+    if (!key) return;
 
     if (this.scopeValue === 'global') {
       Mousetrap.bindGlobal(key, this.handleKey);
