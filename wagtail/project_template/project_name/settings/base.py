a04e05d3e65fb9a1acb502f43e0ce07b747441fa
Sumair Khokhar <sumair.tanveer@arbisoft.com>
2025-06-27 14:59:26 +0500

    Update base.py

--- wagtail/project_template/project_name/settings/base.py
+++ wagtail/project_template/project_name/settings/base.py
@@ -148,12 +148,8 @@ STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
-    # ManifestStaticFilesStorage is recommended in production, to prevent
-    # outdated JavaScript / CSS assets being served from cache
-    # (e.g. after a Wagtail upgrade).
-    # See https://docs.djangoproject.com/en/{{ docs_version }}/ref/contrib/staticfiles/#manifeststaticfilesstorage
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
