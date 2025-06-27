4846fb40e0c1433af0356052ee3d38a63025e163
Sumair Khokhar <sumair.tanveer@arbisoft.com>
2025-06-27 14:59:29 +0500

    Update production.py

--- wagtail/project_template/project_name/settings/production.py
+++ wagtail/project_template/project_name/settings/production.py
@@ -6,3 +6,14 @@ try:
     from .local import *
 except ImportError:
     pass
+
+
+STORAGES = {
+    # ManifestStaticFilesStorage is recommended in production, to prevent
+    # outdated JavaScript / CSS assets being served from cache
+    # (e.g. after a Wagtail upgrade).
+    # See https://docs.djangoproject.com/en/{{ docs_version }}/ref/contrib/staticfiles/#manifeststaticfilesstorage
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+    },
+}
