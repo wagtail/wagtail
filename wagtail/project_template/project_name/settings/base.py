5ab1204dfda4d1c4614869e90e5328b1623f2bc4
Brylie Christopher Oxley <brylie@protonmail.com>
2025-07-02 22:10:51 +0300

    Reorder middleware to place SecurityMiddleware at the top
    - Fixes #13198
    - See https://docs.djangoproject.com/en/stable/topics/http/middleware/#activating-middleware

--- wagtail/project_template/project_name/settings/base.py
+++ wagtail/project_template/project_name/settings/base.py
@@ -49,13 +49,13 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
     "wagtail.contrib.redirects.middleware.RedirectMiddleware",
 ]
 
