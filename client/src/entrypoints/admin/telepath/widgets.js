149c26d4fc6d83620a2562f077cc9e9aab9d7e4a
Matt Westcott <matt@west.co.tt>
2025-07-09 21:59:44 +0100

    Register datetime widgets with telepath inside date-time-chooser.js instead of widgets.js
    This puts them in the same file as the window.init* functions that they depend on, so that we aren't reliant on getting the import order of these two files correct.

--- client/src/entrypoints/admin/telepath/widgets.js
+++ client/src/entrypoints/admin/telepath/widgets.js
@@ -5,22 +5,10 @@ import {
   Select,
 } from '../../../components/Widget';
 
-import {
-  AdminDateInput,
-  AdminTimeInput,
-  AdminDateTimeInput,
-} from '../../../components/DateTimeWidget';
-
 window.telepath.register('wagtail.widgets.Widget', Widget);
 window.telepath.register('wagtail.widgets.CheckboxInput', CheckboxInput);
 window.telepath.register('wagtail.widgets.RadioSelect', RadioSelect);
 window.telepath.register('wagtail.widgets.Select', Select);
-window.telepath.register('wagtail.widgets.AdminDateInput', AdminDateInput);
-window.telepath.register('wagtail.widgets.AdminTimeInput', AdminTimeInput);
-window.telepath.register(
-  'wagtail.widgets.AdminDateTimeInput',
-  AdminDateTimeInput,
-);
 
 class ValidationError {
   constructor(messages) {
