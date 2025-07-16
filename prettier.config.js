7685384b0e06ac02730a553bedcedbd7efd23935
LB <mail@lb.ee>
2025-07-17 07:41:44 +1000

    Update core JS config files with ESM & types
    - Stylelint - move to `.mjs` for opt in ESM & add type
    - Prettier - move to ESM supported with `.js` & add type
    - Tailwind - move to ESM supported with `.js` & add type
    - Eslint - add type (ESM not supported until next version)

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,8 @@
 /**
  * @see https://prettier.io/docs/en/options.html
+ * @type {import("prettier").Config}
  */
-module.exports = {
+const config = {
   arrowParens: 'always',
   bracketSameLine: false,
   bracketSpacing: true,
@@ -15,3 +16,5 @@ module.exports = {
   semi: true,
   singleQuote: true,
 };
+
+export default config;
