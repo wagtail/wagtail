12b9a0361c4151248785f531f95c1479fcf6fdf1
hericlibong <heric.afrimages@gmail.com>
2025-07-18 16:00:40 +0200

    Convert test_tmp_storages.py to Django TestCase, format with ruff

--- wagtail/contrib/redirects/tests/test_tmp_storages.py
+++ wagtail/contrib/redirects/tests/test_tmp_storages.py
@@ -1,20 +1,20 @@
-import pytest
 from django.core.cache import cache
+from django.test import TestCase
 
 from wagtail.contrib.redirects.tmp_storages import CacheStorage
 
 
-@pytest.mark.django_db
-def test_cache_storage_save_and_remove():
-    name = "testfile.txt"
-    content = b"hello world"
-    storage = CacheStorage(name)
+class CacheStorageTests(TestCase):
+    def test_cache_storage_save_and_remove(self):
+        name = "testfile.txt"
+        content = b"hello world"
+        storage = CacheStorage(name)
 
-    # Adds to cache
-    storage.save(content)
-    key = storage.CACHE_PREFIX + storage.name
-    assert cache.get(key) == content
+        # Adds to cache
+        storage.save(content)
+        key = storage.CACHE_PREFIX + storage.name
+        self.assertEqual(cache.get(key), content)
 
-    # Removes from cache
-    storage.remove()
-    assert cache.get(key) is None
+        # Removes from cache
+        storage.remove()
+        self.assertIsNone(cache.get(key))
