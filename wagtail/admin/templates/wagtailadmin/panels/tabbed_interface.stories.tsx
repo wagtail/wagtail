878bba08de9c44e24afb1688514baf84d08d621b
Sage Abdullah <sage.abdullah@torchbox.com>
2025-07-21 10:47:00 +0100

    Update stories for TabbedInterface to reflect new changes
    - render_as_object replaced with render_html in 5521e3b59f45af830ebac3c5686e092616eb82e4
    - show_panel_furniture added in 9a1606c809b2daee005591d98e9e2058e4823c79
    - reflect new Stimulus-based implementation with data-controller="w-tabs"
      instead of data-tabs

--- wagtail/admin/templates/wagtailadmin/panels/tabbed_interface.stories.tsx
+++ wagtail/admin/templates/wagtailadmin/panels/tabbed_interface.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Pattern, generateDocs } from 'storybook-django/src/react';
 import template from './tabbed_interface.html';
+import { TabsController } from '../../../../../client/src/controllers/TabsController';
+import { StimulusWrapper } from '../../../../../client/storybook/StimulusWrapper';
 
 const { docs, argTypes } = generateDocs(template);
 
@@ -10,94 +12,123 @@ export default {
   argTypes: { ...argTypes },
 };
 
-const Template = (args) => <Pattern filename={__filename} context={args} />;
+const Template = ({ debug = false, ...args }) => (
+  <StimulusWrapper debug={debug} definitions={definitions}>
+    <Pattern filename={__filename} context={args} />
+  </StimulusWrapper>
+);
+
+const baseAttrs = {
+  'data-controller': 'w-tabs',
+  'data-action': 'popstate@window->w-tabs#select',
+  'data-w-tabs-use-location-value': 'true',
+};
+
+const buildTabs = (tabbedInterface) => ({
+  ...tabbedInterface,
+  attrs: baseAttrs,
+  visible_children_with_identifiers:
+    tabbedInterface.visible_children_with_identifiers.map(
+      ([child, identifier]) => [
+        {
+          ...child,
+          show_panel_furniture: true,
+        },
+        identifier,
+      ],
+    ),
+});
+
+const definitions = [
+  { identifier: 'w-tabs', controllerConstructor: TabsController },
+];
 
 export const Base = Template.bind({});
 
 Base.args = {
-  self: {
+  self: buildTabs({
     visible_children_with_identifiers: [
       [
         {
           heading: 'Content',
-          render_as_object: `<h2>Content</h2><div>Content Body</div>`,
+          render_html: `<h2>Content</h2><div>Content Body</div>`,
         },
         'content',
       ],
       [
         {
           heading: 'Promote',
-          render_as_object: `<h2>Promote</h2><div>Promote Body</div>`,
+          render_html: `<h2>Promote</h2><div>Promote Body</div>`,
         },
         'promote',
       ],
       [
         {
           heading: 'Settings',
-          render_as_object: `<h2>Settings</h2><div>Settings Body</div>`,
+          render_html: `<h2>Settings</h2><div>Settings Body</div>`,
         },
         'settings',
       ],
     ],
-  },
+  }),
 };
 
 export const Single = Template.bind({});
 
 Single.args = {
-  self: {
+  self: buildTabs({
     visible_children_with_identifiers: [
       [
         {
           heading: 'Tab1',
-          render_as_object: `<h2>Title1</h2><div>Body Text</div>`,
+          render_html: `<h2>Title1</h2><div>Body Text</div>`,
         },
         '1',
       ],
     ],
-  },
+  }),
 };
 
 export const Multiple = Template.bind({});
 
 Multiple.args = {
-  self: {
+  self: buildTabs({
     visible_children_with_identifiers: [
       [
         {
           heading: 'Tab1',
-          render_as_object: `<h1>Title 1</h1><div>Body Text</div>`,
+          render_html: `<h1>Title 1</h1><div>Body Text</div>`,
         },
         '1',
       ],
       [
         {
           heading: 'Tab2',
-          render_as_object: `<h1>Title 2</h1><div>Body Text</div>`,
+          render_html: `<h1>Title 2</h1><div>Body Text</div>`,
         },
         '2',
       ],
       [
         {
           heading: 'Tab3',
-          render_as_object: `<h1>Title 3</h1><div>Body Text</div>`,
+          render_html: `<h1>Title 3</h1><div>Body Text</div>`,
         },
         '3',
       ],
       [
         {
           heading: 'Tab4',
-          render_as_object: `<h1>Title 4</h1><div>Body Text</div>`,
+          render_html: `<h1>Title 4</h1><div>Body Text</div>`,
         },
         '4',
       ],
       [
         {
           heading: 'Tab5',
-          render_as_object: `<h1>Title 5</h1><div>Body Text</div>`,
+          render_html: `<h1>Title 5</h1><div>Body Text</div>`,
         },
         '5',
       ],
     ],
-  },
+  }),
 };
