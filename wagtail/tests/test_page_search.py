dd16618ce8ff0254fd10f0bd1c0bd03f8fdf4171
Matthew Westcott <matthew@west.co.tt>
2025-09-09 22:33:33 +0100

    Move remaining page-specific search tests into wagtail.tests

--- wagtail/tests/test_page_search.py
+++ wagtail/tests/test_page_search.py
@@ -1,14 +1,41 @@
+from unittest import mock
+
 from django.conf import settings
 from django.db.models import F
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 from wagtail.models import Page
+from wagtail.search import index
 from wagtail.search.backends import get_search_backend
 from wagtail.search.backends.base import (
     BaseSearchQueryCompiler,
     BaseSearchResults,
     OrderByFieldError,
 )
+from wagtail.test.testapp.models import SimplePage
+from wagtail.test.utils import WagtailTestUtils
+
+
+@mock.patch("wagtail.tests.DummySearchBackend", create=True)
+@override_settings(
+    WAGTAILSEARCH_BACKENDS={"default": {"BACKEND": "wagtail.tests.DummySearchBackend"}}
+)
+class TestInsertOrUpdateObject(WagtailTestUtils, TestCase):
+    def test_converts_to_specific_page(self, backend):
+        root_page = Page.objects.get(id=1)
+        page = root_page.add_child(
+            instance=SimplePage(title="test", slug="test", content="test")
+        )
+
+        # Convert page into a generic "Page" object and add it into the index
+        unspecific_page = page.page_ptr
+
+        backend().reset_mock()
+
+        index.insert_or_update_object(unspecific_page)
+
+        # It should be automatically converted back to the specific version
+        backend().add.assert_called_with(page)
 
 
 class PageSearchTests:
