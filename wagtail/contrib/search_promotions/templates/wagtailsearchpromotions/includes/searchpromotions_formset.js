1d04e09af1324f8ddf063fd1960041c7576e23e7
Sage Abdullah <sage.abdullah@torchbox.com>
2025-10-16 10:57:45 +0100

    Refactor search promotions inline panel to use FormsetController

--- wagtail/contrib/search_promotions/templates/wagtailsearchpromotions/includes/searchpromotions_formset.js
+++
@@ -1,9 +0,0 @@
-/* global InlinePanel */
-
-$(function () {
-  var panel = new InlinePanel({
-    formsetPrefix: 'id_{{ formset.prefix }}',
-    emptyChildFormPrefix: '{{ formset.empty_form.prefix }}',
-    canOrder: true,
-  });
-});
