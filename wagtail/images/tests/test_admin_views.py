8b1a7a99c6c04eb58d52bc05c51315398eaa1e5c
Sage Abdullah <sage.abdullah@torchbox.com>
2025-11-14 17:15:35 +0000

    Do not prefill image and document title if there is an existing title (#13574)

--- wagtail/images/tests/test_admin_views.py
+++ wagtail/images/tests/test_admin_views.py
@@ -948,7 +948,7 @@ class TestImageAddView(AdminTemplateTestUtils, WagtailTestUtils, TestCase):
         self.assertIsNotNone(file_input)
         expected_attributes = {
             "data-controller": "w-sync",
-            "data-action": "change->w-sync#apply",
+            "data-action": "input->w-sync#apply",
             "data-w-sync-bubbles-param": "true",
             "data-w-sync-name-value": "wagtail:images-upload",
             "data-w-sync-normalize-value": "true",
@@ -2026,7 +2026,7 @@ class TestImageChooserView(WagtailTestUtils, TestCase):
         self.assertIsNotNone(file_input)
         expected_attributes = {
             "data-controller": "w-sync",
-            "data-action": "change->w-sync#apply",
+            "data-action": "input->w-sync#apply",
             "data-w-sync-bubbles-param": "true",
             "data-w-sync-name-value": "wagtail:images-upload",
             "data-w-sync-normalize-value": "true",
