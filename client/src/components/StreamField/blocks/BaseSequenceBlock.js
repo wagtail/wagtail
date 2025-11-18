d8d1bfb38cd5789a968270b95b42c0f095e5c850
Sage Abdullah <sage.abdullah@torchbox.com>
2025-11-18 16:48:12 +0000

    Move StreamField ActionButton classes to a separate file

--- client/src/components/StreamField/blocks/BaseSequenceBlock.js
+++ client/src/components/StreamField/blocks/BaseSequenceBlock.js
@@ -10,113 +10,15 @@ import {
   toggleCollapsiblePanel,
 } from '../../../includes/panels';
 import { range } from '../../../utils/range';
+import {
+  MoveUpButton,
+  MoveDownButton,
+  DragButton,
+  DuplicateButton,
+  DeleteButton,
+} from './ActionButtons';
 import { CollapsiblePanel } from './CollapsiblePanel';
 import { StructBlockDefinition } from './StructBlock';
-import { gettext } from '../../../utils/gettext';
-
-class ActionButton {
-  constructor(sequenceChild) {
-    this.sequenceChild = sequenceChild;
-  }
-
-  render(container) {
-    this.dom = $(`
-      <button type="button" class="button button--icon text-replace white" data-streamfield-action="${this.labelIdentifier}" title="${h(this.label)}">
-        <svg class="icon icon-${h(this.icon)}" aria-hidden="true">
-          <use href="#icon-${h(this.icon)}"></use>
-        </svg>
-      </button>
-    `);
-
-    this.dom.on('click', () => {
-      if (this.onClick) this.onClick();
-      return false; // don't propagate to header's onclick event (which collapses the block)
-    });
-
-    $(container).append(this.dom);
-
-    if (this.enableEvent) {
-      this.sequenceChild.addEventListener(this.enableEvent, () => {
-        this.enable();
-      });
-    }
-
-    if (this.disableEvent) {
-      this.sequenceChild.addEventListener(this.disableEvent, () => {
-        this.disable();
-      });
-    }
-
-    if (this.initiallyDisabled) {
-      this.disable();
-    }
-  }
-
-  enable() {
-    this.dom.removeAttr('disabled');
-  }
-
-  disable() {
-    this.dom.attr('disabled', 'true');
-  }
-}
-
-class MoveUpButton extends ActionButton {
-  enableEvent = 'enableMoveUp';
-  disableEvent = 'disableMoveUp';
-  initiallyDisabled = true;
-  icon = 'arrow-up';
-  labelIdentifier = 'MOVE_UP';
-  label = gettext('Move up');
-
-  onClick() {
-    this.sequenceChild.moveUp();
-  }
-}
-
-class MoveDownButton extends ActionButton {
-  enableEvent = 'enableMoveDown';
-  disableEvent = 'disableMoveDown';
-  initiallyDisabled = true;
-  icon = 'arrow-down';
-  labelIdentifier = 'MOVE_DOWN';
-  label = gettext('Move down');
-
-  onClick() {
-    this.sequenceChild.moveDown();
-  }
-}
-
-class DragButton extends ActionButton {
-  enableEvent = 'enableDrag';
-  disableEvent = 'disableDrag';
-  initiallyDisabled = false;
-  icon = 'grip';
-  labelIdentifier = 'DRAG';
-  label = gettext('Drag');
-}
-
-class DuplicateButton extends ActionButton {
-  enableEvent = 'enableDuplication';
-  disableEvent = 'disableDuplication';
-  icon = 'copy';
-  labelIdentifier = 'DUPLICATE';
-  label = gettext('Duplicate');
-
-  onClick() {
-    this.sequenceChild.duplicate({ animate: true });
-  }
-}
-
-class DeleteButton extends ActionButton {
-  icon = 'bin';
-  labelIdentifier = 'DELETE';
-  label = gettext('Delete');
-
-  onClick() {
-    this.sequenceChild.delete({ animate: true });
-  }
-}
 
 export class BaseSequenceChild extends EventTarget {
   constructor(
