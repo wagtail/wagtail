bd4a31b26a8883f3d2bb785db6faf374e3517504
akshatggupta <guptakshat333@gmail.com>
2025-12-02 00:30:50 +0400

    Reapply "WIP: docker-template changes (save before restoring main)"
    This reverts commit fd9bfb00ef3548da23634d04a50531f266dd3e0c.

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-*li(!z7$cb&_k^*)6&8dp4j1)%(##&aqi+h7i&4pi$lepg6d(a"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ mysite/mysite/settings/production.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = False
+
+# ManifestStaticFilesStorage is recommended in production, to prevent
+# outdated JavaScript / CSS assets being served from cache
+# (e.g. after a Wagtail upgrade).
+# See https://docs.djangoproject.com/en/5.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
+STORAGES["staticfiles"][
+    "BACKEND"
+] = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
+try:
+    from .local import *
+except ImportError:
+    pass
