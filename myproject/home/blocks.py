94886c9f08b432e42b26058eb8d92bf5f5563e83
Ali Asad <aliasad0443@gmail.com>
2025-12-27 19:32:34 +0500

    Add IntChoiceBlock and TestPage with StreamField

---
+++ myproject/home/blocks.py
@@ -0,0 +1,20 @@
+from django import forms
+from wagtail.blocks import ChoiceBlock
+
+
+class IntChoiceBlock(ChoiceBlock):
+    def get_field(self, **kwargs):
+        choices = kwargs.pop("choices")
+
+        return forms.TypedChoiceField(
+            choices=choices,
+            coerce=int,
+            **kwargs,
+        )
+
+    def to_python(self, value):
+        if value is None:
+            return None
+        return int(value)
+
+
---
+++ myproject/home/models.py
@@ -0,0 +1,38 @@
+from wagtail import blocks
+from wagtail.models import Page
+from wagtail.fields import StreamField
+from wagtail.admin.panels import FieldPanel
+from .blocks import IntChoiceBlock  # your custom block
+
+# StructBlock using the custom IntChoiceBlock
+class TestBlock(blocks.StructBlock):
+    number = IntChoiceBlock(
+        choices=[
+            (1, "One"),
+            (2, "Two"),
+            (3, "Three"),
+        ],
+        default=1,
+    )
+
+    class Meta:
+        icon = "placeholder"
+
+# HomePage definition
+class HomePage(Page):
+    pass
+
+# Page using the TestBlock
+class TestPage(Page):
+    body = StreamField([
+        ('test', TestBlock()),
+    ], use_json_field=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
+
+    parent_page_types = [HomePage]  # direct reference to HomePage
+
+    class Meta:
+        verbose_name = "Test Page"
