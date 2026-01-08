e9b7b0adb1c6cdf5bdec6b06b3640490cb6ad89c
Matthew Westcott <matthew@west.co.tt>
2026-01-08 01:21:58 +0000

    Add get_field_updates_for_resave method for identifying field updates on child object creation

--- wagtail/admin/tests/test_forms.py
+++ wagtail/admin/tests/test_forms.py
@@ -3,7 +3,9 @@ from django.test import SimpleTestCase, TestCase
 
 from wagtail.admin.forms.auth import LoginForm, PasswordResetForm
 from wagtail.admin.forms.models import WagtailAdminModelForm
+from wagtail.test.snippets.models import MultiSectionRichTextSnippet
 from wagtail.test.testapp.models import Advert
+from wagtail.test.utils.form_data import inline_formset, nested_form_data, rich_text
 
 
 class CustomLoginForm(LoginForm):
@@ -66,3 +68,75 @@ class TestDeferRequiredFields(TestCase):
         form.defer_required_fields()
         form.restore_required_fields()
         self.assertFalse(form.is_valid())
+
+
+class SnippetForm(WagtailAdminModelForm):
+    class Meta:
+        model = MultiSectionRichTextSnippet
+        fields = []
+        formsets = {
+            "sections": {
+                "fields": ["body"],
+            }
+        }
+
+
+class TestGetFieldUpdatesForResave(TestCase):
+    def test_get_field_updates_for_resave_on_create(self):
+        form = SnippetForm(
+            nested_form_data(
+                {
+                    "sections": inline_formset(
+                        [
+                            {"id": "", "body": rich_text("<p>Section 1 body</p>")},
+                            {"id": "", "body": rich_text("<p>Section 2 body</p>")},
+                        ]
+                    )
+                }
+            )
+        )
+        self.assertTrue(form.is_valid())
+        snippet = form.save()
+        section_1 = snippet.sections.get(body__contains="Section 1 body")
+        section_2 = snippet.sections.get(body__contains="Section 2 body")
+        self.assertCountEqual(
+            form.get_field_updates_for_resave(),
+            [
+                ("sections-INITIAL_FORMS", "2"),
+                ("sections-0-id", str(section_1.id)),
+                ("sections-1-id", str(section_2.id)),
+            ],
+        )
+
+    def test_get_field_updates_for_resave_on_update(self):
+        snippet = MultiSectionRichTextSnippet()
+        section_1 = snippet.sections.create(body="<p>Initial body 1</p>")
+        snippet.save()
+
+        form = SnippetForm(
+            nested_form_data(
+                {
+                    "sections": inline_formset(
+                        [
+                            {
+                                "id": str(section_1.id),
+                                "body": rich_text("<p>Section 1 body</p>"),
+                            },
+                            {"id": "", "body": rich_text("<p>Section 2 body</p>")},
+                        ],
+                        initial=1,
+                    )
+                }
+            ),
+            instance=snippet,
+        )
+        self.assertTrue(form.is_valid())
+        form.save()
+        section_2 = snippet.sections.get(body__contains="Section 2 body")
+        self.assertCountEqual(
+            form.get_field_updates_for_resave(),
+            [
+                ("sections-INITIAL_FORMS", "2"),
+                ("sections-1-id", str(section_2.id)),
+            ],
+        )
