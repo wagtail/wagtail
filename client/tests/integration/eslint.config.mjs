7e0d15f5a2cb814bb386c294d06b271c509aa1f3
Sage Abdullah <sage.abdullah@torchbox.com>
2026-02-10 13:54:34 +0000

    Upgrade ESLint to v9

--- client/tests/integration/eslint.config.mjs
+++ client/tests/integration/eslint.config.mjs
@@ -1,21 +1,30 @@
 /**
- * Overrides to our base ESLint configuration specifically for our integration tests.
+ * ESLint configuration for integration tests
+ * This extends the base flat config with test-specific overrides
  */
-export default {
-  rules: {
-    // To support code running in Node without transpiling.
-    '@typescript-eslint/no-var-requires': 'off',
-    'no-underscore-dangle': ['error', { allow: ['__BROWSER_GLOBAL__'] }],
-    // So we can lint the code without resolving imports, in case the sub-package isn’t installed.
-    'import/no-unresolved': 'off',
+
+import globals from 'globals';
+import baseConfig from '../../../eslint.config.mjs';
+
+export default [
+  ...baseConfig,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        page: 'readonly',
+        TEST_ORIGIN: 'readonly',
+        __BROWSER_GLOBAL__: 'readonly',
+      },
+    },
+    rules: {
+      // To support code running in Node without transpiling
+      '@typescript-eslint/no-require-imports': 'off',
+      'no-underscore-dangle': ['error', { allow: ['__BROWSER_GLOBAL__'] }],
+      // So we can lint the code without resolving imports, in case the sub-package isn't installed
+      'import-x/no-extraneous-dependencies': 'off',
+      'import-x/no-relative-packages': 'off',
+      'import-x/no-unresolved': 'off',
+    },
   },
-  env: {
-    jest: true,
-    browser: true,
-    node: true,
-  },
-  globals: {
-    page: 'readonly',
-    TEST_ORIGIN: 'readonly',
-  },
-};
+];
