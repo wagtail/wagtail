7dfb3431149b8fa7ac94b9f5073ef32bc5d9dc30
Matthew Westcott <matthew@west.co.tt>
2026-02-12 15:50:36 +0000

    Update doc8 to 1.1.2

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ testing_extras = [
     "django-pattern-library>=0.7",
     # For coverage and PEP8 linting
     "coverage>=3.7.0",
-    "doc8==0.8.1",
+    "doc8==1.1.2",
     "ruff==0.1.5",
     # For enforcing string formatting mechanism in source files
     "semgrep==1.40.0",
