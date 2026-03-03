12168f381e108a3f918e093a753678a097b4b119
S​age Abdullah <sage.abdullah@torchbox.com>
2026-03-03 12:04:08 +0000

    Prevent dropdowns from closing after a successful autosave (#13917)
    Co-authored-by: Thibaud Colas <thibaudcolas@gmail.com>

--- wagtail/admin/tests/viewsets/test_model_viewset.py
+++ wagtail/admin/tests/viewsets/test_model_viewset.py
@@ -1831,7 +1831,16 @@ class TestHeaderButtons(WagtailTestUtils, TestCase):
         response = self.client.get(self.edit_url)
         self.assertEqual(response.status_code, 200)
         soup = self.get_soup(response.content)
-        header_buttons = soup.select(".w-slim-header .w-dropdown a")
+        dropdown = soup.select_one(".w-slim-header .w-dropdown")
+        self.assertIsNotNone(dropdown)
+        self.assertLessEqual(
+            {
+                "w-breadcrumbs:opened@document->w-dropdown#hide",
+                "w-breadcrumbs:closed@document->w-dropdown#hide",
+            },
+            set(dropdown.get("data-action").split()),
+        )
+        header_buttons = dropdown.select("a")
         expected_buttons = [
             ("Copy", self.copy_url),
             ("Delete", self.delete_url),
