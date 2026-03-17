adbe3b378dd6dbfdc8b2585001d00716cc2e509a
Jake Howard <jake.howard@torchbox.com>
2026-03-17 15:25:45 +0000

    Exclude view-restricted collections from document and images API

--- wagtail/images/api/v2/views.py
+++ wagtail/images/api/v2/views.py
@@ -1,5 +1,6 @@
 from wagtail.api.v2.filters import FieldsFilter, OrderingFilter, SearchFilter
 from wagtail.api.v2.views import BaseAPIViewSet
+from wagtail.models import CollectionViewRestriction
 
 from ... import get_image_model
 from .serializers import ImageSerializer
@@ -21,3 +22,13 @@ class ImagesAPIViewSet(BaseAPIViewSet):
     ]
     name = "images"
     model = get_image_model()
+
+    def get_queryset(self):
+        # Exclude images which aren't in visible collections
+        restricted_collection_ids = {
+            restriction.collection_id
+            for restriction in CollectionViewRestriction.objects.all()
+            if not restriction.accept_request(self.request)
+        }
+
+        return super().get_queryset().exclude(collection__in=restricted_collection_ids)
