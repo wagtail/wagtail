8db4b4216cf44396495e488a9cb1a86e56dd8467
MOHD ADEEB <codewithdewolf@gmail.com>
2026-03-24 02:05:57 +0530

    fix(tests): Rename testapp migration to 0062 to fix conflict with wagtail:main

--- wagtail/test/testapp/migrations/0062_tofieldcategory_tofieldpost.py
+++ wagtail/test/testapp/migrations/0062_tofieldcategory_tofieldpost.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("tests", "0060_eventpagespeakeraward_certificate"),
+        ("tests", "0061_revisablecluster_revisableclusterchild"),
     ]
 
     operations = [
