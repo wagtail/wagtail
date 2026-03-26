5a237ef16783f5aa1625d9b67ac4a7472ca38b27
Matthew Westcott <matthew@west.co.tt>
2026-03-26 17:41:48 +0000

    Revert "Update django-treebeard dependency to >4.8,<6 (#13888)"
    This reverts commit 4bae5f3aef8c9138264e0d889c4b862d008169a4.

--- wagtail/actions/delete_page.py
+++ wagtail/actions/delete_page.py
@@ -29,11 +29,21 @@ class DeletePageAction:
     def _delete_page(self, page, *args, **kwargs):
         from wagtail.models import Page
 
-        for child in page.get_descendants().specific().iterator():
-            self.log_deletion(child)
-        self.log_deletion(page.specific)
+        # Ensure that deletion always happens on an instance of Page, not a specific subclass. This
+        # works around a bug in treebeard <= 3.0 where calling SpecificPage.delete() fails to delete
+        # child pages that are not instances of SpecificPage
+        if type(page) is Page:
+            for child in page.get_descendants().specific().iterator():
+                self.log_deletion(child)
+            self.log_deletion(page.specific)
 
-        return super(Page, page.specific).delete(*args, **kwargs)
+            # this is a Page instance, so carry on as we were
+            return super(Page, page).delete(*args, **kwargs)
+        else:
+            # retrieve an actual Page instance and delete that instead of page
+            return DeletePageAction(
+                Page.objects.get(id=page.id), user=self.user
+            ).execute(*args, **kwargs)
 
     def execute(self, *args, skip_permission_checks=False, **kwargs):
         self.check(skip_permission_checks=skip_permission_checks)
