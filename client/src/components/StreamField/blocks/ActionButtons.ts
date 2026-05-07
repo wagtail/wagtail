a10b559fd3d2690ecb55760bd061d1b691afe662
Saloni Kashyap <kashyapsaloni68292@gmail.com>
2026-05-08 00:55:09 +0530

    Add aria-label to icon-only action buttons

--- client/src/components/StreamField/blocks/ActionButtons.ts
+++ client/src/components/StreamField/blocks/ActionButtons.ts
@@ -21,7 +21,13 @@ export abstract class ActionButton {
 
   render(container, position = 'after') {
     this.dom = $(`
-      <button type="button" class="button button--icon text-replace white" data-streamfield-action="${this.labelIdentifier}" title="${h(this.label)}">
+      <button
+      type="button"
+      class="button button--icon text-replace white"
+      data-streamfield-action="${this.labelIdentifier}"
+      title="${h(this.label)}"
+      aria-label="${h(this.label)}"
+      >
         <svg class="icon icon-${h(this.icon)}" aria-hidden="true">
           <use href="#icon-${h(this.icon)}"></use>
         </svg>
