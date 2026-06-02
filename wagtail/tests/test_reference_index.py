wagtail/wagtail

commit 11f8575d184591c803fb9edbc26df552cf1d9490

Handle nested inline models when displaying object usage information
Co-authored-by: Kacper Walęga <80758209+non4me21@users.noreply.github.com>
Co-authored-by: Tian Jie Wong <wtj.wongtianjie@gmail.com>

wagtail/models/reference_index.py
@@ -808,10 +808,21 @@ class ReferenceIndex(models.Model):
         field = self.source_field
         model_path_components = self.model_path.split(".")
 
-        # ManyToOneRel (reverse accessor for ParentalKey) does not have a verbose name. So get the name of the child field instead
-        if isinstance(field, models.ManyToOneRel):
-            child_field = field.related_model._meta.get_field(model_path_components[2])
-            return capfirst(child_field.verbose_name)
+        # ManyToOneRel (reverse accessor for ParentalKey) does not have a verbose name,
+        # so get the name of the child field instead. While we don't yet support
+        # ManyToManyRel, check for ForeignObjectRel (common base class of both
+        # ManyToOneRel and ManyToManyRel) to future-proof this.
+        if isinstance(field, models.ForeignObjectRel):
+            labels = []
+            idx = 0
+            child_field = field
+            while isinstance(child_field, models.ForeignObjectRel):
+                related_model_opts = child_field.related_model._meta
+                labels.append(capfirst(related_model_opts.verbose_name))
+                idx += 2
+                child_field = related_model_opts.get_field(model_path_components[idx])
+            labels.append(capfirst(child_field.verbose_name))
+            return " → ".join(labels)
         elif isinstance(field, StreamField):
             label = f"{capfirst(field.verbose_name)}"
             block = field.stream_block

wagtail/tests/test_reference_index.py
@@ -21,6 +21,7 @@ from wagtail.test.testapp.models import (
     EventPageCarouselItem,
     EventPageRelatedLink,
     EventPageSpeaker,
+    EventPageSpeakerAward,
     GenericSnippetNoFieldIndexPage,
     GenericSnippetNoIndexPage,
     GenericSnippetPage,
@@ -319,27 +320,25 @@ class TestDescribeOnDelete(TestCase):
         management.call_command("rebuild_references_index", stdout=StringIO())
 
     def setUp(self):
-        field = VariousOnDeleteModel._meta.get_field("stream_field")
-        advertisement_content = field.stream_block.child_blocks["advertisement_content"]
-        captioned_advert = advertisement_content.child_blocks["captioned_advert"]
-
         self.advert = Advert.objects.create(text="An advertisement")
+        self.page = EventPage.objects.first()
         self.advert_uuid = AdvertWithCustomUUIDPrimaryKey.objects.create(
             text="A UUID advertisement"
         )
-
-        self.page = EventPage.objects.first()
-        page_link = f'<p>Link to <a id="{self.page.id}" linktype="page">a page</a></p>'
-
         self.image = get_image_model().objects.create(
             title="My image",
             file=get_test_image_file(),
         )
-
         self.document = get_document_model().objects.create(
             title="My document",
             file=get_test_document_file(),
         )
+
+    def test_describe_source_field_and_on_delete(self):
+        field = VariousOnDeleteModel._meta.get_field("stream_field")
+        advertisement_content = field.stream_block.child_blocks["advertisement_content"]
+        captioned_advert = advertisement_content.child_blocks["captioned_advert"]
+        page_link = f'<p>Link to <a id="{self.page.id}" linktype="page">a page</a></p>'
 
         # Each case is a tuple of (
         #   VariousOnDeleteModel init kwargs,
@@ -486,7 +485,6 @@ class TestDescribeOnDelete(TestCase):
             ),
         ]
 
-    def test_describe_source_field_and_on_delete(self):
         for (
             init_kwargs,
             referred_object,
@@ -524,7 +522,10 @@ class TestDescribeOnDelete(TestCase):
         self.assertEqual(usage.count(), 2)
         for _, references in usage:
             reference = references[0]
-            self.assertEqual(reference.describe_source_field(), "Advert")
+            self.assertEqual(
+                reference.describe_source_field(),
+                "Advert placement → Advert",
+            )
             self.assertEqual(
                 reference.describe_on_delete(),
                 "the advert placement will also be deleted",
@@ -545,7 +546,10 @@ class TestDescribeOnDelete(TestCase):
             event_page.save()
         refs = ReferenceIndex.get_references_to(root_page)
         self.assertEqual(refs.count(), 1)
-        self.assertEqual(refs[0].describe_source_field(), "Link page")
+        self.assertEqual(
+            refs[0].describe_source_field(),
+            "Event page speaker → Link page",
+        )
         self.assertEqual(
             refs[0].describe_on_delete(),
             "the event page speaker will also be deleted",
@@ -566,6 +570,32 @@ class TestDescribeOnDelete(TestCase):
         )
         with self.assertRaises(FieldDoesNotExist):
             reference.describe_source_field()
+
+    def test_foreign_key_in_nested_child_model(self):
+        root_page = Page.objects.get(id=2)
+        speaker = EventPageSpeaker(
+            first_name="Willie", last_name="Wagtail", link_page=root_page
+        )
+        event_page = EventPage.objects.first()
+        event_page.speakers.add(speaker)
+        award = EventPageSpeakerAward(
+            speaker=speaker,
+            name="Best Speaker",
+            certificate=self.document,
+        )
+        speaker.awards.add(award)
+        with self.captureOnCommitCallbacks(execute=True):
+            event_page.save()
+        refs = ReferenceIndex.get_references_to(self.document)
+        self.assertEqual(refs.count(), 1)
+        self.assertEqual(
+            refs[0].describe_source_field(),
+            "Event page speaker → Event page speaker award → Certificate",
+        )
+        self.assertEqual(
+            refs[0].describe_on_delete(),
+            "the event page speaker will also be deleted",
+        )
 
 
 class TestBulkFetch(TestCase):

wagtail/snippets/tests/test_usage.py
@@ -156,7 +156,7 @@ class TestSnippetUsageView(WagtailTestUtils, TestCase):
         self.assertContains(response, "<td>Page</td>", html=True)
         self.assertContains(response, "<th>Field</th>", html=True)
         self.assertNotContains(response, "<th>If you confirm deletion</th>", html=True)
-        self.assertContains(response, "<li>Advert</li>", html=True)
+        self.assertContains(response, "<li>Advert placement → Advert</li>", html=True)
 
     def test_usage_with_describe_on_delete_cascade(self):
         with self.captureOnCommitCallbacks(execute=True):

wagtail/test/testapp/migrations/0060_eventpagespeakeraward_certificate.py
@@ -1,0 +1,26 @@
+# Generated by Django 5.2.7 on 2026-01-28 10:09
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("tests", "0059_nopromotepage"),
+        ("wagtaildocs", "0014_alter_document_file_size"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="eventpagespeakeraward",
+            name="certificate",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="event_speaker_awards",
+                to="wagtaildocs.document",
+            ),
+        ),
+    ]

wagtail/test/testapp/models.py
@@ -339,8 +339,15 @@ class EventPageSpeakerAward(TranslatableMixin, Orderable, models.Model):
     )
     name = models.CharField("Award name", max_length=255)
     date_awarded = models.DateField(null=True, blank=True)
+    certificate = models.ForeignKey(
+        "wagtaildocs.Document",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="event_speaker_awards",
+    )
 
-    panels = ["name", "date_awarded"]
+    panels = ["name", "date_awarded", "certificate"]
 
     class Meta(TranslatableMixin.Meta, Orderable.Meta):
         pass

commit f98a72b26a98faebe8353e66da68b6484738d7a6

Prevent error in usage views when using gettext_lazy for a model's verbose_name
Regression in 11f8575.

wagtail/models/reference_index.py
@@ -7,6 +7,7 @@ from django.core.exceptions import FieldDoesNotExist
 from django.db import connection, models
 from django.db.models import CharField, Count, OuterRef, Subquery
 from django.db.models.functions import Cast, Coalesce
+from django.utils.encoding import force_str
 from django.utils.functional import cached_property
 from django.utils.text import capfirst
 from django.utils.translation import gettext_lazy as _
@@ -836,7 +837,7 @@ class ReferenceIndex(models.Model):
                 idx += 2
                 child_field = related_model_opts.get_field(model_path_components[idx])
             labels.append(capfirst(child_field.verbose_name))
-            return " → ".join(labels)
+            return " → ".join(force_str(label) for label in labels)
         elif isinstance(field, StreamField):
             label = f"{capfirst(field.verbose_name)}"
             block = field.stream_block

wagtail/tests/test_reference_index.py
@@ -1,11 +1,12 @@
 from io import StringIO
+from unittest import mock
 
 from django.contrib.contenttypes.models import ContentType
 from django.core import management
 from django.core.exceptions import FieldDoesNotExist
 from django.db import models
 from django.test import TestCase
-from django.utils.functional import SimpleLazyObject
+from django.utils.functional import SimpleLazyObject, lazystr
 
 from wagtail.blocks import StreamValue, StructValue
 from wagtail.documents import get_document_model
@@ -530,6 +531,21 @@ class TestDescribeOnDelete(TestCase):
                 reference.describe_on_delete(),
                 "the advert placement will also be deleted",
             )
+
+    @mock.patch(
+        "wagtail.test.testapp.models.AdvertPlacement._meta.verbose_name",
+        lazystr("lazy advert placement"),
+    )
+    def test_describe_source_field_parental_key_with_lazy_verbose_name(self):
+        # Validate that lazy string verbose names (e.g. gettext_lazy) on the
+        # related model are correctly resolved in the reference index.
+        advert = Advert.objects.first()
+        usage = ReferenceIndex.get_references_to(advert).group_by_source_object()
+        reference = usage[0][1][0]
+        self.assertEqual(
+            reference.describe_source_field(),
+            "Lazy advert placement → Advert",
+        )
 
     def test_parental_key_with_related_query_name(self):
         # EventPageSpeaker has a ParentalKey to EventPage with a