ef50772dd6287bd15dd64f98d15d9c961e259502
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2026-06-12 15:13:19 +0100

    update permission policy declarations

--- wagtail/documents/permissions.py
+++ wagtail/documents/permissions.py
@@ -1,8 +1,10 @@
 from wagtail.documents import get_document_model_string
+from wagtail.permission_policies import override_permission_policy
 from wagtail.permission_policies.collections import CollectionOwnershipPermissionPolicy
 
-permission_policy = CollectionOwnershipPermissionPolicy(
+permission_policy = override_permission_policy("document",CollectionOwnershipPermissionPolicy(
     get_document_model_string(),
     auth_model="wagtaildocs.Document",
     owner_field_name="uploaded_by_user",
+    ),
 )
--- wagtail/permissions.py
+++ wagtail/permissions.py
@@ -1,11 +1,11 @@
 from wagtail.models import Collection, Locale, Page, Site, Task, Workflow
-from wagtail.permission_policies import ModelPermissionPolicy
+from wagtail.permission_policies import ModelPermissionPolicy, override_permission_policy
 from wagtail.permission_policies.collections import CollectionManagementPermissionPolicy
 from wagtail.permission_policies.pages import PagePermissionPolicy
 
-page_permission_policy = PagePermissionPolicy(Page)
-site_permission_policy = ModelPermissionPolicy(Site)
-collection_permission_policy = CollectionManagementPermissionPolicy(Collection)
-task_permission_policy = ModelPermissionPolicy(Task)
-workflow_permission_policy = ModelPermissionPolicy(Workflow)
-locale_permission_policy = ModelPermissionPolicy(Locale)
+page_permission_policy = override_permission_policy("page", PagePermissionPolicy(Page))
+site_permission_policy = override_permission_policy("site", ModelPermissionPolicy(Site))
+collection_permission_policy = override_permission_policy("collection", CollectionManagementPermissionPolicy(Collection))
+task_permission_policy = override_permission_policy("task", ModelPermissionPolicy(Task))
+workflow_permission_policy = override_permission_policy("workflow", ModelPermissionPolicy(Workflow))
+locale_permission_policy = override_permission_policy("locale", ModelPermissionPolicy(Locale))
