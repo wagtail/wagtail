79e0772872e0a1ec26692c0b348a02e286724a55
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2026-06-12 15:12:21 +0100

    Add override_permission_policy method

--- wagtail/permission_policies/__init__.py
+++ wagtail/permission_policies/__init__.py
@@ -1,1 +1,13 @@
+from django.conf import settings
 from .base import *  # NOQA: F403
+
+def override_permission_policy(policy: str, default):
+    """
+    Override permission policy from settings
+    """
+    permission_policy_overrides = getattr(settings, "WAGTAIL_PERMISSION_POLICY_OVERRIDES", {})
+    permission_policy = permission_policy_overrides.get(policy)
+    if permission_policy:
+        return permission_policy
+    return default
+     
