0e1d21d90f9ccee9785f0f3b449c753c467343ab
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2026-06-15 12:51:57 +0100

    use import string

--- wagtail/permission_policies/__init__.py
+++ wagtail/permission_policies/__init__.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.utils.module_loading import import_string
 from .base import *  # NOQA: F403
 
 def override_permission_policy(policy: str, default):
@@ -8,6 +9,6 @@ def override_permission_policy(policy: str, default):
     permission_policy_overrides = getattr(settings, "WAGTAIL_PERMISSION_POLICY_OVERRIDES", {})
     permission_policy = permission_policy_overrides.get(policy)
     if permission_policy:
-        return permission_policy
+        return import_string(permission_policy)
     return default
      
