c4c9ce2de1cb2d9668d42989f3c0554b10180cd8
Matthew Westcott <matthew@west.co.tt>
2026-06-25 15:31:41 +0100

    Use swappable page model for foreign keys in migrations

--- wagtail/contrib/redirects/migrations/0001_initial.py
+++ wagtail/contrib/redirects/migrations/0001_initial.py
@@ -1,4 +1,5 @@
 from django.db import migrations, models
+import swapper
 
 
 class Migration(migrations.Migration):
@@ -49,7 +50,7 @@ class Migration(migrations.Migration):
                         blank=True,
                         null=True,
                         verbose_name="Redirect to a page",
-                        to="wagtailcore.Page",
+                        to=swapper.get_model_name("wagtailcore", "Page"),
                     ),
                 ),
                 (
