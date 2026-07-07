36144308137c57d8ddaf7ec4a37c4fefd8bf99ef
Matthew Westcott <matthew@west.co.tt>
2026-07-07 18:36:01 +0100

    Make wagtailcore.Page references swappable in test models and migrations

--- wagtail/test/testapp/migrations/0048_requireddatepage.py
+++ wagtail/test/testapp/migrations/0048_requireddatepage.py
@@ -2,6 +2,7 @@
 
 import django.db.models.deletion
 from django.db import migrations, models
+import swapper
 
 
 class Migration(migrations.Migration):
@@ -23,7 +24,7 @@ class Migration(migrations.Migration):
                         parent_link=True,
                         primary_key=True,
                         serialize=False,
-                        to="wagtailcore.page",
+                        to=swapper.get_model_name("wagtailcore", "Page"),
                     ),
                 ),
                 ("deadline", models.DateField()),
@@ -31,6 +32,6 @@ class Migration(migrations.Migration):
             options={
                 "abstract": False,
             },
-            bases=("wagtailcore.page",),
+            bases=(swapper.get_model_name("wagtailcore", "Page"),),
         ),
     ]
