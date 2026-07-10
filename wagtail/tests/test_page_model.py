c22e9dcde9ca3bfd06a293a6dc90c7f5753edf45
Matthew Westcott <matthew@west.co.tt>
2026-07-10 13:37:44 +0100

    Remove unnecessary reference to show_in_menus from TestPageWithContentJSON

--- wagtail/tests/test_page_model.py
+++ wagtail/tests/test_page_model.py
@@ -3813,7 +3813,6 @@ class TestPageWithContentJSON(PageFixturesMixin, TestCase):
             locked_at="2000-01-01T00:00:00Z",
             has_unpublished_changes=not original_page.has_unpublished_changes,
             content_type=eventpage_content_type.id,
-            show_in_menus=not original_page.show_in_menus,
             owner=1,
         )
 
@@ -3821,7 +3820,7 @@ class TestPageWithContentJSON(PageFixturesMixin, TestCase):
         updated_page = original_page.with_content_json(content)
 
         # The following attributes values should have changed
-        for attr_name in ("title", "slug", "content", "url_path", "show_in_menus"):
+        for attr_name in ("title", "slug", "content", "url_path"):
             self.assertNotEqual(
                 getattr(original_page, attr_name), getattr(updated_page, attr_name)
             )
