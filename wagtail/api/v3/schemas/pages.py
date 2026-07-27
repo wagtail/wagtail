64a15ae2deabca7b3c480ce4b0d1b25c71dfe5ac
Sage Abdullah <sage.abdullah@torchbox.com>
2026-07-27 16:02:54 +0100

    Make meta.type optional in page update API

--- wagtail/api/v3/schemas/pages.py
+++ wagtail/api/v3/schemas/pages.py
@@ -84,9 +84,9 @@ class PageCreateBaseSchema(Schema):
 
 
 class PageUpdateMetaSchema(Schema):
-    type: str
+    type: str | None = None
     action: Literal["publish"] | None = None
 
 
 class PageUpdateBaseSchema(Schema):
-    meta: PageUpdateMetaSchema
+    meta: PageUpdateMetaSchema = PageUpdateMetaSchema()
