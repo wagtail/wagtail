962f4e7be994aeb1ec32ec2041df7151f490a901
Sage Abdullah <sage.abdullah@torchbox.com>
2026-07-28 13:04:14 +0100

    Add ancestor_of filter to pages list API v3 router

--- wagtail/api/v3/tests/test_pages.py
+++ wagtail/api/v3/tests/test_pages.py
@@ -219,6 +219,51 @@ class TestV3PageListingFilters(TestV3PageListingBase):
             errors=[{"type": "literal_error", "loc": ["query", "filters", "type", 0]}],
         )
 
+    def test_ancestor_of_filter(self):
+        response = self.get_response(ancestor_of=10)
+        content = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.get_page_id_list(content), [2, 6])
+
+    def test_ancestor_of_with_type(self):
+        response = self.get_response(type="demosite.EventIndexPage", ancestor_of=8)
+        content = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.get_page_id_list(content), [4])
+
+    def test_ancestor_of_unknown_page_gives_error(self):
+        response = self.get_response(ancestor_of=1000)
+        self.assert_problem_response(
+            response,
+            status_code=404,
+            detail_contains="No Page matches the given query.",
+        )
+
+    def test_ancestor_of_not_positive_integer_gives_error(self):
+        response = self.get_response(ancestor_of="abc")
+        self.assert_problem_response(
+            response,
+            status_code=422,
+            detail_contains="Validation failed",
+            errors=[
+                {
+                    "type": "int_parsing",
+                    "loc": ["query", "ancestor_of"],
+                    "msg": "Input should be a valid integer, unable to parse string as an integer",
+                },
+            ],
+        )
+
+    def test_ancestor_of_home_page_ignores_root(self):
+        # Root page is not in any site, so pretend it doesn't exist
+        response = self.get_response(ancestor_of=2)
+        content = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.get_page_id_list(content), [])
+
     def test_child_of_filter(self):
         response = self.get_response(child_of=5)
         content = response.json()
