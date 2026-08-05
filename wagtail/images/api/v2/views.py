b8a2040dfcdb97498b746254ad8bb4e47e21358c
Matthew Westcott <matthew@west.co.tt>
2026-08-05 23:53:12 +0100

    Exclude items in descendants of private collections from document/image API endpoints

--- wagtail/images/api/v2/views.py
+++ wagtail/images/api/v2/views.py
@@ -1,6 +1,6 @@
 from wagtail.api.v2.filters import FieldsFilter, OrderingFilter, SearchFilter
+from wagtail.api.v2.utils import get_restricted_collection_ids
 from wagtail.api.v2.views import BaseAPIViewSet
-from wagtail.models import CollectionViewRestriction
 
 from ... import get_image_model
 from .serializers import ImageSerializer
@@ -25,10 +25,8 @@ class ImagesAPIViewSet(BaseAPIViewSet):
 
     def get_queryset(self):
         # Exclude images which aren't in visible collections
-        restricted_collection_ids = {
-            restriction.collection_id
-            for restriction in CollectionViewRestriction.objects.all()
-            if not restriction.accept_request(self.request)
-        }
-
-        return super().get_queryset().exclude(collection__in=restricted_collection_ids)
+        return (
+            super()
+            .get_queryset()
+            .exclude(collection__in=get_restricted_collection_ids(self.request))
+        )
