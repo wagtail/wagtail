a909f1caa81568f0acce2958f297fd2cfcf5f44f
rvkeerthi24 <rvkeerthi99@gmail.com>
2026-09-18 00:39:24 +0530

    Remove jQuery usage in ExpandingFormset

--- client/src/components/ExpandingFormset/index.js
+++ client/src/components/ExpandingFormset/index.js
@@ -1,4 +1,4 @@
-import $ from 'jquery';
+import { runInlineScripts } from '../../utils/runInlineScripts';
 
 /**
  * Usage of this class directly is deprecated for admin core code use.
@@ -11,9 +11,9 @@ import $ from 'jquery';
 export class ExpandingFormset {
   constructor(prefix, opts = {}, initControls = true) {
     this.opts = opts;
-    const addButton = $('#' + prefix + '-ADD');
-    this.formContainer = $('#' + prefix + '-FORMS');
-    this.totalFormsInput = $('#' + prefix + '-TOTAL_FORMS');
+    const addButton = document.getElementById(prefix + '-ADD');
+    this.formContainer = document.getElementById(prefix + '-FORMS');
+    this.totalFormsInput = document.getElementById(prefix + '-TOTAL_FORMS');
 
     const emptyFormElement = document.getElementById(
       prefix + '-EMPTY_FORM_TEMPLATE',
@@ -28,14 +28,14 @@ export class ExpandingFormset {
         }
       }
 
-      addButton.on('click', () => {
+      addButton?.addEventListener('click', () => {
         this.addForm();
       });
     }
   }
 
   get formCount() {
-    return parseInt(this.totalFormsInput.val(), 10);
+    return parseInt(this.totalFormsInput.value, 10);
   }
 
   /**
@@ -49,9 +49,14 @@ export class ExpandingFormset {
       formIndex + '$1',
     );
 
-    this.formContainer.append(newFormHtml);
+    const template = document.createElement('template');
+    template.innerHTML = newFormHtml;
+    const fragment = template.content;
 
-    this.totalFormsInput.val(this.formCount + 1);
+    this.formContainer.appendChild(fragment);
+    runInlineScripts(this.formContainer);
+
+    this.totalFormsInput.value = this.formCount + 1;
 
     if (!('runCallbacks' in opts) || opts.runCallbacks) {
       if (this.opts.onAdd) this.opts.onAdd(formIndex);
